6baeb47706667bbaba5c50b9673beb11004eb7ea
Greg Meyer <greg.meyer@datadoghq.com>
2016-09-16 11:16:11 -0400

    starts adding cassandra, doesn't work yet

---
+++ cassandra/ci/start-docker.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# A script to create the Cassandra containers.
+
+set -e
+
+NAME='dd-test-cassandra'
+PORT=7199
+
+# CASSANDRA_VERSION=${CASSANDRA_VERSION-2.1.14}
+CASSANDRA_VERSION=${CASSANDRA_VERSION-2.0.17}
+
+if docker ps -a | grep dd-test-cassandra >/dev/null; then
+  echo 'the containers already exist, we have to remove them'
+  bash cassandra/ci/stop-docker.sh
+fi
+
+docker create --expose $PORT -p $PORT:$PORT -p 9042:9042 -p 7000:7000 -p 7001:7001 -p 9160:9160 -e JMX_PORT=7199 -e LOCAL_JMX=no --name $NAME cassandra:$CASSANDRA_VERSION
+ # -e JVM_OPTS=-Djava.rmi.server.hostname=0.0.0.0
+# docker ps
+# docker ps -a
+# docker logs $NAME
+
+# docker cp ./cassandra/ci/cassandra_${CASSANDRA_VERSION}.yaml $NAME:/etc/cassandra/cassandra.yaml
+# docker ps
+# docker ps -a
+# docker logs $NAME
+
+docker cp ./cassandra/ci/cassandra-env_${CASSANDRA_VERSION}.sh $NAME:/etc/cassandra/cassandra-env.sh
+# docker ps
+# docker ps -a
+# docker logs $NAME
+
+cp ./cassandra/ci/jmxremote.password ./cassandra/ci/jmxremote.password.tmp
+chmod 400 ./cassandra/ci/jmxremote.password.tmp
+docker cp ./cassandra/ci/jmxremote.password.tmp $NAME:/etc/cassandra/jmxremote.password
+rm -f ./cassandra/ci/jmxremote.password.tmp
+
+docker start $NAME
+docker ps
+docker ps -a
+docker logs $NAME
+
+
+IP_ADDR=$(docker inspect ${NAME} | grep '"IPAddress"' | cut -d':' -f2 | cut -d'"' -f2)
+IP_ADDR=$(echo $IP_ADDR | cut -d " " -f2)
+
+
+echo "Your cassandra container is running at $IP_ADDR"
+
+echo 'running'
