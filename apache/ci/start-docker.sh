cd20171c46a43e0d0e6658457cf3193ddab4a17e
Greg Meyer <greg.meyer@datadoghq.com>
2016-10-07 13:45:38 -0400

    more debugging

--- apache/ci/start-docker.sh
+++ apache/ci/start-docker.sh
@@ -14,15 +14,22 @@ if docker ps -a | grep dd-test-apache >/dev/null; then
   bash apache/ci/stop-docker.sh
 fi
 
-docker create --expose 8180 -p $PORT:$PORT --name $NAME httpd:$APACHE_VERSION
+docker create --expose $PORT -p $PORT:$PORT --name $NAME httpd:$APACHE_VERSION
 docker cp ./apache/ci/httpd.conf $NAME:/usr/local/apache2/conf/httpd.conf
 docker start $NAME
 
 IP_ADDR=$(docker inspect ${NAME} | grep '"IPAddress"' | cut -d':' -f2 | cut -d'"' -f2)
 IP_ADDR=$(echo $NAME | cut -d " " -f2)
 
+echo `docker ps`
 echo 'running'
 
-echo `docker logs $NAME`
+for i in `seq 1 10`; do
+  echo `docker logs $NAME`
+  echo `docker ps`
+  sleep 2
+done
+
+echo `docker ps`
 
 echo `curl http://localhost:8180 || true`
