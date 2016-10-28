45eea3e5bfa71a561c4f646e60d97f75d217d0db
Greg Meyer <greg.meyer@datadoghq.com>
2016-10-28 17:30:58 -0400

    fixes nginx for 1.6

--- nginx/ci/start-docker.sh
+++ nginx/ci/start-docker.sh
@@ -10,15 +10,28 @@ PORT2=44442
 NGINX_VERSION=${FLAVOR_VERSION-1.7.11}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if docker ps | grep $NAME >/dev/null; then
+if docker ps -a | grep $NAME >/dev/null; then
   echo 'the containers already exist, we have to remove them'
   bash $DIR/stop-docker.sh
 fi
 
 if [[ "$NGINX_VERSION" == "1.6.2" ]]; then
-  DOCKER_REPO="simtech/nginx-1.6.2"
+  # DOCKER_REPO="simtech/nginx-1.6.2"
+  DOCKER_REPO="centos/nginx-16-centos7"
+  VOLUMES="-v $DIR/nginx.conf:/opt/rh/nginx16/root/etc/nginx/nginx.conf -v $DIR/testing.crt:/opt/rh/nginx16/root/etc/nginx/testing.crt -v $DIR/testing.key:/opt/rh/nginx16/root/etc/nginx/testing.key"
+  # docker run -d -p $PORT1:$PORT1 -p $PORT2:$PORT2 --name $NAME -v $DIR/nginx.conf:/opt/rh/nginx16/root/etc/nginx/nginx.conf -v $DIR/testing.crt:/opt/rh/nginx16/root/etc/nginx/testing.crt -v $DIR/testing.key:/opt/rh/nginx16/root/etc/nginx/testing.key $DOCKER_REPO
+  docker create -p $PORT1:$PORT1 -p $PORT2:$PORT2 --name $NAME $DOCKER_REPO
+  docker cp $DIR/nginx.conf $NAME:/opt/rh/nginx16/root/etc/nginx/nginx.conf
+  docker cp $DIR/testing.key $NAME:/opt/rh/nginx16/root/etc/nginx/testing.key
+  docker cp $DIR/testing.crt $NAME:/opt/rh/nginx16/root/etc/nginx/testing.crt
+
+  docker start $NAME
 else
   DOCKER_REPO="nginx:1.7.11"
+  VOLUMES="-v $DIR/nginx.conf:/etc/nginx/nginx.conf -v $DIR/testing.crt:/etc/nginx/testing.crt -v $DIR/testing.key:/etc/nginx/testing.key"
+  docker run -d -p $PORT1:$PORT1 -p $PORT2:$PORT2 --name $NAME -v $DIR/nginx.conf:/etc/nginx/nginx.conf -v $DIR/testing.crt:/etc/nginx/testing.crt -v $DIR/testing.key:/etc/nginx/testing.key $DOCKER_REPO
 fi
 
-docker run -d -p $PORT1:$PORT1 -p $PORT2:$PORT2 --name $NAME -v $DIR/nginx.conf:/etc/nginx/nginx.conf -v $DIR/testing.crt:/etc/nginx/testing.crt -v $DIR/testing.key:/etc/nginx/testing.key $DOCKER_REPO
+
+docker logs $NAME
+# docker run -d -p $PORT1:$PORT1 -p $PORT2:$PORT2 --name $NAME $DOCKER_REPO
--- nginx/ci/stop-docker.sh
+++ nginx/ci/stop-docker.sh
@@ -7,9 +7,9 @@ set -e
 NAME=dd-test-nginx
 
 if docker ps -a | grep $NAME >/dev/null; then
-  containers=$(docker ps --format '{{.Names}}' --filter name=$NAME)
-  stopped_containers=$(docker ps -a --format '{{.Names}}' --filter name=$NAME)
+  containers=$(docker ps -q --filter name=$NAME)
+  stopped_containers=$(docker ps -a -q --filter name=$NAME)
 
-  docker kill $containers>/dev/null
-  docker rm $stopped_containers>/dev/null
+  docker kill $containers>/dev/null || true
+  docker rm $stopped_containers>/dev/null || true
 fi
