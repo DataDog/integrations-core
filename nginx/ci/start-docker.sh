633bcbed87d0bd76128e249ddecd19f38beb268f
Greg Meyer <greg.meyer@datadoghq.com>
2016-10-28 17:42:15 -0400

    cleanup

--- nginx/ci/start-docker.sh
+++ nginx/ci/start-docker.sh
@@ -16,10 +16,8 @@ if docker ps -a | grep $NAME >/dev/null; then
 fi
 
 if [[ "$NGINX_VERSION" == "1.6.2" ]]; then
-  # DOCKER_REPO="simtech/nginx-1.6.2"
   DOCKER_REPO="centos/nginx-16-centos7"
   VOLUMES="-v $DIR/nginx.conf:/opt/rh/nginx16/root/etc/nginx/nginx.conf -v $DIR/testing.crt:/opt/rh/nginx16/root/etc/nginx/testing.crt -v $DIR/testing.key:/opt/rh/nginx16/root/etc/nginx/testing.key"
-  # docker run -d -p $PORT1:$PORT1 -p $PORT2:$PORT2 --name $NAME -v $DIR/nginx.conf:/opt/rh/nginx16/root/etc/nginx/nginx.conf -v $DIR/testing.crt:/opt/rh/nginx16/root/etc/nginx/testing.crt -v $DIR/testing.key:/opt/rh/nginx16/root/etc/nginx/testing.key $DOCKER_REPO
   docker create -p $PORT1:$PORT1 -p $PORT2:$PORT2 --name $NAME $DOCKER_REPO
   docker cp $DIR/nginx.conf $NAME:/opt/rh/nginx16/root/etc/nginx/nginx.conf
   docker cp $DIR/testing.key $NAME:/opt/rh/nginx16/root/etc/nginx/testing.key
@@ -31,7 +29,3 @@ else
   VOLUMES="-v $DIR/nginx.conf:/etc/nginx/nginx.conf -v $DIR/testing.crt:/etc/nginx/testing.crt -v $DIR/testing.key:/etc/nginx/testing.key"
   docker run -d -p $PORT1:$PORT1 -p $PORT2:$PORT2 --name $NAME -v $DIR/nginx.conf:/etc/nginx/nginx.conf -v $DIR/testing.crt:/etc/nginx/testing.crt -v $DIR/testing.key:/etc/nginx/testing.key $DOCKER_REPO
 fi
-
-
-docker logs $NAME
-# docker run -d -p $PORT1:$PORT1 -p $PORT2:$PORT2 --name $NAME $DOCKER_REPO
