5e146f5bd8c747cd9f28ef44bf8f87b5f0da069d
Greg Meyer <greg.meyer@datadoghq.com>
2016-11-04 19:00:36 -0400

    fixes regex

--- rabbitmq/ci/start-docker.sh
+++ rabbitmq/ci/start-docker.sh
@@ -11,7 +11,7 @@ VERSION=${FLAVOR_VERSION-3.5.0}
 docker run -d --name $NAME -p 15672:15672 -p 5672:5672 rabbitmq:$VERSION-management
 
 COUNT=1
-until [[ `docker logs $NAME 2>&1` =~ "*Server startup complete" || $COUNT -eq 20 ]]; do
+until [[ `docker logs $NAME 2>&1` =~ .*"Server startup complete" || $COUNT -eq 20 ]]; do
   sleep 2
   COUNT=$(($COUNT+1))
   echo `docker logs $NAME`
