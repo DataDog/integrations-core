81d1d0036961c5c8aa758806b59497c03ae7418b
Greg Meyer <greg.meyer@datadoghq.com>
2017-01-05 18:00:13 -0500

    finishes this

--- linux_proc_extras/test_linux_proc_extras.py
+++ linux_proc_extras/test_linux_proc_extras.py
@@ -2,8 +2,12 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 
+# 3p
+from mock import mock_open, patch
+
 # project
 from tests.checks.common import AgentCheckTest
+from shared.test.common import Fixtures
 
 class TestCheckLinuxProcExtras(AgentCheckTest):
     CHECK_NAME = 'linux_proc_extras'
@@ -30,10 +34,35 @@ class TestCheckLinuxProcExtras(AgentCheckTest):
 
     # Really a basic check to see if all metrics are there
     def test_check(self):
-        self.run_check({'instances': []})
+
+        self.load_check({'instances': []})
+
+        m = mock_open(read_data=Fixtures.read_file('entropy_avail'))
+        with patch('__builtin__.open', m):
+            self.check.get_entropy_info()
+
+        m = mock_open(read_data=Fixtures.read_file('inode-nr'))
+        with patch('__builtin__.open', m):
+            self.check.get_inode_info()
+
+        m = mock_open(read_data=Fixtures.read_file('proc-stat'))
+        with patch('__builtin__.open', m):
+            self.check.get_stat_info()
+            self.check.get_stat_info()
+
+        with patch('check.get_subprocess_output', return_value=(Fixtures.read_file('process_stats'), "", 0)):
+            self.check.get_process_states()
+
+        self.metrics = self.check.get_metrics()
+        self.events = self.check.get_events()
+        self.service_checks = self.check.get_service_checks()
+        self.service_metadata = []
+        self.warnings = self.check.get_warnings()
+
+        self.check.log.info(self.metrics)
 
         # Assert metrics
         for metric in self.PROC_COUNTS + self.INODE_GAUGES + self.ENTROPY_GAUGES + self.PROCESS_STATS_GAUGES:
-            self.assertMetric(metric, tags=[])
+            self.assertMetric(metric)
 
         self.coverage_report()
