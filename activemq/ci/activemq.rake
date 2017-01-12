f40ec0fce689c254e10ade32ec9c504259e2a9ab
Greg Meyer <greg.meyer@datadoghq.com>
2017-01-12 15:39:34 -0500

    splits activemq checks

--- activemq/ci/activemq.rake
+++ activemq/ci/activemq.rake
@@ -1,56 +1,25 @@
 require 'ci/common'
 
 def activemq_version
-  ENV['FLAVOR_VERSION'] || '5.11.1'
+  ENV['FLAVOR_VERSION'] || 'latest'
 end
 
 def activemq_rootdir
-  if ENV['TRAVIS']
-    return "#{ENV['INTEGRATIONS_DIR']}/activemq_#{activemq_version}"
-  end
-  if ENV['VOLATILE_DIR']
-    "#{ENV['VOLATILE_DIR']}/activemq"
-  else
-    "/tmp/integration-sdk-testing/activemq"
-  end
+  "#{ENV['INTEGRATIONS_DIR']}/activemq_#{activemq_version}"
 end
-
-container_name = 'dd-test-activemq'
-admin_port = 8161
-listen_port = 61616
 
 namespace :ci do
   namespace :activemq do |flavor|
-    task before_install: ['ci:common:before_install'] do
-      sh %(docker stop dd-test-activemq || true)
-      sh %(docker rm dd-test-activemq || true)
-      sh %(mkdir -p #{activemq_rootdir}/data)
-
-      sh %(curl -s -L\
-       -o #{activemq_rootdir}/kahadb.tar.gz\
-       https://s3.amazonaws.com/dd-agent-tarball-mirror/apache-activemq-kahadb.tar.gz)
-      sh %(tar zxf #{activemq_rootdir}/kahadb.tar.gz\
-       -C #{activemq_rootdir}/data)
-      sh %(rm -rf #{activemq_rootdir}/data/kahadb/lock)
-      # I don't like this but it encounters a permissions error if I don't put the chmod here.
-      sh %(chmod -R a+rwxX #{activemq_rootdir})
-    end
+    task before_install: ['ci:common:before_install']
 
     task install: ['ci:common:install'] do
       use_venv = in_venv
       install_requirements('activemq/requirements.txt',
                            "--cache-dir #{ENV['PIP_CACHE']}",
                            "#{ENV['VOLATILE_DIR']}/ci.log", use_venv)
-
-      sh %(docker run -d \
-        -p #{admin_port}:#{admin_port} \
-        -p #{listen_port}:#{listen_port} \
-        --name #{container_name} \
-        -v #{activemq_rootdir}/data:/var/activemq/data \
-        -e ACTIVEMQ_DATA=/var/activemq/data \
-        rmohr/activemq:#{activemq_version})
-
-      Wait.for 'http://localhost:8161'
+      # sample docker usage
+      # sh %(docker create -p XXX:YYY --name activemq source/activemq:activemq_version)
+      # sh %(docker start activemq)
     end
 
     task before_script: ['ci:common:before_script']
@@ -64,11 +33,12 @@ namespace :ci do
 
     task before_cache: ['ci:common:before_cache']
 
-    task cleanup: ['ci:common:cleanup'] do
-      sh %(rm -rf #{activemq_rootdir})
-      sh %(docker stop dd-test-activemq || true)
-      sh %(docker rm dd-test-activemq || true)
-    end
+    task cleanup: ['ci:common:cleanup']
+    # sample cleanup task
+    # task cleanup: ['ci:common:cleanup'] do
+    #   sh %(docker stop activemq)
+    #   sh %(docker rm activemq)
+    # end
 
     task :execute do
       exception = nil
--- activemq/test_activemq.py
+++ activemq/test_activemq.py
@@ -3,80 +3,36 @@
 # Licensed under Simplified BSD License (see LICENSE)
 
 # stdlib
-from itertools import product
-
-# 3rd
 from nose.plugins.attrib import attr
+
+# 3p
 
 # project
 from tests.checks.common import AgentCheckTest
 
 
+instance = {
+    'host': 'localhost',
+    'port': 26379,
+    'password': 'datadog-is-devops-best-friend'
+}
+
+
+# NOTE: Feel free to declare multiple test classes if needed
+
 @attr(requires='activemq')
-class ActiveMQTestCase(AgentCheckTest):
-    CHECK_NAME = 'activemq_xml'
-
-    CONFIG = {
-        'url': "http://localhost:8161",
-        'username': "admin",
-        'password': "admin"
-    }
-
-    GENERAL_METRICS = [
-        "activemq.subscriber.count",
-        "activemq.topic.count",
-        "activemq.queue.count",
-    ]
-
-    QUEUE_METRICS = [
-        "activemq.queue.consumer_count",
-        "activemq.queue.dequeue_count",
-        "activemq.queue.enqueue_count",
-        "activemq.queue.size",
-    ]
-
-    SUBSCRIBER_METRICS = [
-        "activemq.subscriber.pending_queue_size",
-        "activemq.subscriber.dequeue_counter",
-        "activemq.subscriber.enqueue_counter",
-        "activemq.subscriber.dispatched_queue_size",
-        "activemq.subscriber.dispatched_counter",
-    ]
-
-    TOPIC_METRICS = [
-        "activemq.topic.consumer_count",
-        "activemq.topic.dequeue_count",
-        "activemq.topic.enqueue_count",
-        "activemq.topic.size",
-    ]
+class TestActivemq(AgentCheckTest):
+    """Basic Test for activemq integration."""
+    CHECK_NAME = 'activemq'
 
     def test_check(self):
         """
-        Collect ActiveMQ metrics
+        Testing Activemq check.
         """
-        config = {
-            'instances': [self.CONFIG]
-        }
+        self.load_check({}, {})
 
-        self.run_check(config)
+        # run your actual tests...
 
-        tags = ["url:{0}".format(self.CONFIG['url'])]
-
-        # Test metrics
-        for mname in self.GENERAL_METRICS:
-            self.assertMetric(mname, count=1, tags=tags)
-
-        for mname in self.QUEUE_METRICS:
-            self.assertMetric(mname, count=1, tags=tags + ["queue:my_queue"])
-
-        for mname, tname in product(self.TOPIC_METRICS,
-                                    ["my_topic", "ActiveMQ.Advisory.MasterBroker"]):
-            self.assertMetric(mname, count=1, tags=tags + ["topic:{0}".format(tname)])
-
-        for mname in self.SUBSCRIBER_METRICS:
-            subscriber_tags = tags + \
-                ["clientId:my_client", "connectionId:NOTSET", "subscriptionName:my_subscriber",
-                 "destinationName:my_topic", "selector:jms_selector", "active:no"]
-            self.assertMetric(mname, count=1, tags=subscriber_tags)
-
+        self.assertTrue(True)
+        # Raises when COVERAGE=true and coverage < 100%
         self.coverage_report()
