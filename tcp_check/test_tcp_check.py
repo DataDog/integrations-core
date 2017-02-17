79795b276385712e013697efe5dd5bcaa97e7381
Hippolyte HENRY <hippolyte.henry@dadadoghq.com>
2017-02-17 16:43:23 -0500

    Add test for the response_time metric

--- tcp_check/test_tcp_check.py
+++ tcp_check/test_tcp_check.py
@@ -27,6 +27,13 @@ CONFIG = {
         'port': 80,
         'timeout': 1,
         'name': 'UpService'
+    }, {
+        'host': 'datadoghq.com',
+        'port': 80,
+        'timeout': 1,
+        'name': 'response_time',
+        'tags': ['test3'],
+        'collect_response_time': True
     }]
 }
 
@@ -61,14 +68,14 @@ class TCPCheckTest(AgentCheckTest):
         self.run_check(CONFIG)
 
         # Overrides self.service_checks attribute when values are available
-        self.warnings = self.wait_for_async('get_warnings', 'warnings', 3)
+        self.warnings = self.wait_for_async('get_warnings', 'warnings', len(CONFIG['instances']))
 
         # Assess warnings
         self.assertWarning(
             "Using events for service checks is deprecated in "
             "favor of monitors and will be removed in future versions of the "
             "Datadog Agent.",
-            count=3
+            count=len(CONFIG['instances'])
         )
 
     def test_check(self):
@@ -79,15 +86,21 @@ class TCPCheckTest(AgentCheckTest):
         self.run_check(CONFIG)
 
         # Overrides self.service_checks attribute when values are available
-        self.service_checks = self.wait_for_async('get_service_checks', 'service_checks', 3)
+        self.service_checks = self.wait_for_async('get_service_checks', 'service_checks', len(CONFIG['instances']))
 
         expected_tags = ["instance:DownService", "target_host:127.0.0.1", "port:65530"]
         self.assertServiceCheckCritical("tcp.can_connect", tags=expected_tags)
 
-        expected_tags = ["instance:DownService2", "target_host:126.0.0.1", "port:65530"]
+        expected_tags = ["instance:DownService2", "target_host:126.0.0.1", "port:65530", "test1"]
         self.assertServiceCheckCritical("tcp.can_connect", tags=expected_tags)
 
-        expected_tags = ["instance:UpService", "target_host:datadoghq.com", "port:80"]
+        expected_tags = ["instance:UpService", "target_host:datadoghq.com", "port:80", "test2"]
         self.assertServiceCheckOK("tcp.can_connect", tags=expected_tags)
+
+        expected_tags = ["instance:response_time", "target_host:datadoghq.com", "port:80", "test3"]
+        self.assertServiceCheckOK("tcp.can_connect", tags=expected_tags)
+
+        expected_tags = ["instance:response_time", "url:datadoghq.com:80", "test3"]
+        self.assertMetric("network.tcp.response_time", tags=expected_tags)
 
         self.coverage_report()
