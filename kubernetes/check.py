6663246095d0425c3b4c05117af4fb23f29f423d
Daniel Turner <daniel.turner@shopify.com>
2017-03-01 10:51:04 -0600

    collect_events true should disable all other checks since dd-agent with collect_events false is likely going also be deployed as a ds on all hosts, so we'll get duplicate metrics

--- kubernetes/check.py
+++ kubernetes/check.py
@@ -149,18 +149,19 @@ class Kubernetes(AgentCheck):
 
         pods_list = self.kubeutil.retrieve_pods_list()
 
-        # kubelet health checks
-        self._perform_kubelet_checks(self.kubeutil.kube_health_url)
-
-        # kubelet metrics
-        self._update_metrics(instance, pods_list)
-
         # kubelet events
         if _is_affirmative(instance.get('collect_events', DEFAULT_COLLECT_EVENTS)):
             try:
                 self._process_events(instance, pods_list)
             except Exception as ex:
                 self.log.error("Event collection failed: %s" % str(ex))
+        else:
+            # kubelet health checks
+            self._perform_kubelet_checks(self.kubeutil.kube_health_url)
+
+            # kubelet metrics
+            self._update_metrics(instance, pods_list)
+
 
     def _publish_raw_metrics(self, metric, dat, tags, depth=0):
         if depth >= self.max_depth:
