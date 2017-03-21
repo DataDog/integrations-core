a4e23c839c235bc2bb7592c09640faf11c4e02a1
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2017-03-21 13:59:30 -0400

    Add labels service and stack

--- docker_daemon/check.py
+++ docker_daemon/check.py
@@ -18,7 +18,9 @@ from utils.dockerutil import (DockerUtil,
                               MountException,
                               BogusPIDException,
                               SWARM_SVC_LABEL,
-                              RANCHER_CONTAINER_NAME)
+                              RANCHER_CONTAINER_NAME,
+                              RANCHER_SVC_NAME,
+                              RANCHER_STACK_NAME)
 from utils.kubernetes import KubeUtil
 from utils.platform import Platform
 from utils.service_discovery.sd_backend import get_sd_backend
@@ -373,8 +375,13 @@ class DockerDaemon(AgentCheck):
             self.collect_labels_as_tags.append(KubeUtil.POD_NAME_LABEL)
 
         # Collect container names as tags on rancher
-        if Platform.is_rancher() and RANCHER_CONTAINER_NAME not in self.collect_labels_as_tags:
-            self.collect_labels_as_tags.append(RANCHER_CONTAINER_NAME)
+        if Platform.is_rancher():
+            if RANCHER_CONTAINER_NAME not in self.collect_labels_as_tags:
+                self.collect_labels_as_tags.append(RANCHER_CONTAINER_NAME)
+            if RANCHER_SVC_NAME not in self.collect_labels_as_tags:
+                self.collect_labels_as_tags.append(RANCHER_SVC_NAME)
+            if RANCHER_STACK_NAME not in self.collect_labels_as_tags:
+                self.collect_labels_as_tags.append(RANCHER_STACK_NAME)
 
         if entity is not None:
             pod_name = None
@@ -405,7 +412,13 @@ class DockerDaemon(AgentCheck):
                                 tags.append("swarm_service:%s" % v)
                         elif k == RANCHER_CONTAINER_NAME and Platform.is_rancher():
                             if v:
+                                tags.append('rancher_container:%s' % v)
+                        elif k == RANCHER_SVC_NAME and Platform.is_rancher():
+                            if v:
                                 tags.append('rancher_service:%s' % v)
+                        elif k == RANCHER_STACK_NAME and Platform.is_rancher():
+                            if v:
+                                tags.append('rancher_stack:%s' % v)
 
                         elif not v:
                             tags.append(k)
