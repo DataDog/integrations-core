0bd1669b793ef4f8c9e062ca736e620cd1164cfd
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-03-23 11:27:19 -0400

    [mysql] default is blocking fashion to match former behavior.

--- mysql/check.py
+++ mysql/check.py
@@ -529,7 +529,7 @@ class MySql(AgentCheck):
         if _is_affirmative(options.get('replication', False)):
             # Get replica stats
             results.update(self._get_replica_stats(db))
-            nonblocking = _is_affirmative(options.get('replication_non_blocking_status', True))
+            nonblocking = _is_affirmative(options.get('replication_non_blocking_status', False))
             results.update(self._get_slave_status(db, above_560, nonblocking))
             metrics.update(REPLICA_VARS)
 
