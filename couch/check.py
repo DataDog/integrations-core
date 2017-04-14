9f168f0734b05c58a8ec3769e8cf87fcf7b9f7f2
Bernhard Gschwantner <bernharduw@users.noreply.github.com>
2017-04-14 14:35:18 +0200

    [couch] escaped the database names (fixes #214) (#268)
    * escaped the database names (fixes #214)
    
    To support forward slashes in db names, the names have to be URI encoded.
    
    * Directly imported quote from urllib

--- couch/check.py
+++ couch/check.py
@@ -4,6 +4,7 @@
 
 # stdlib
 from urlparse import urljoin
+from urllib import quote
 
 # 3rd party
 import requests
@@ -119,7 +120,7 @@ class CouchDb(AgentCheck):
             databases = list(databases)[:self.MAX_DB]
 
         for dbName in databases:
-            url = urljoin(server, dbName)
+            url = urljoin(server, quote(dbName, safe = ''))
             try:
                 db_stats = self._get_stats(url, instance)
             except requests.exceptions.HTTPError as e:
