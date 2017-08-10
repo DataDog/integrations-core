7d4a69c9b14ce9d443732685a38aab0ba9843abe
Haissam Kaj <haissam.kaj@gmail.com>
2017-08-10 15:42:55 +0000

    [kubernetes] Introduce kubelet check

---
+++ kubelet/CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG - kubelet
+
+1.0.0 / Unreleased
+==================
+
+
+### Changes
+
+* [FEATURE] add kubelet integration.
--- kubernetes/datadog_checks/kubernetes/kubernetes.py
+++ kubernetes/datadog_checks/kubernetes/kubernetes.py
@@ -62,31 +62,11 @@ K8S_ALERT_MAP = {
     'Normal': 'info'
 }
 
-# Suffixes per
-# https://github.com/kubernetes/kubernetes/blob/8fd414537b5143ab039cb910590237cabf4af783/pkg/api/resource/suffix.go#L108
-FACTORS = {
-    'n': float(1)/(1000*1000*1000),
-    'u': float(1)/(1000*1000),
-    'm': float(1)/1000,
-    'k': 1000,
-    'M': 1000*1000,
-    'G': 1000*1000*1000,
-    'T': 1000*1000*1000*1000,
-    'P': 1000*1000*1000*1000*1000,
-    'E': 1000*1000*1000*1000*1000*1000,
-    'Ki': 1024,
-    'Mi': 1024*1024,
-    'Gi': 1024*1024*1024,
-    'Ti': 1024*1024*1024*1024,
-    'Pi': 1024*1024*1024*1024*1024,
-    'Ei': 1024*1024*1024*1024*1024*1024,
-}
-
 QUANTITY_EXP = re.compile(r'[-+]?\d+[\.]?\d*[numkMGTPE]?i?')
 
 
 class Kubernetes(AgentCheck):
-    """ Collect metrics and events from kubelet """
+    """ Collect metrics and events from Kubernetes """
 
     pod_names_by_container = {}
 
@@ -138,7 +118,7 @@ class Kubernetes(AgentCheck):
                 if line.find('hostname') != -1:
                     continue
 
-                matches = re.match('\[(.)\]([^\s]+) (.*)?', line)
+                matches = re.match(r'\[(.)\]([^\s]+) (.*)?', line)
                 if not matches or len(matches.groups()) < 2:
                     continue
 
@@ -228,7 +208,7 @@ class Kubernetes(AgentCheck):
             except Exception as err:
                 self.log.warning("Error while getting performance metrics: %s" % str(err))
 
-        # kubelet events
+        # kubernetes events
         if self.event_retriever is not None:
             try:
                 events = self.event_retriever.get_event_array()
@@ -348,7 +328,6 @@ class Kubernetes(AgentCheck):
         try:
             cont_labels = subcontainer['spec']['labels']
         except KeyError:
-            self.log.debug("Subcontainer, doesn't have any labels")
             cont_labels = {}
 
         # Collect pod names, namespaces, rc...
@@ -388,16 +367,6 @@ class Kubernetes(AgentCheck):
         return tags
 
     def _update_metrics(self, instance, pods_list):
-        def parse_quantity(s):
-            number = ''
-            unit = ''
-            for c in s:
-                if c.isdigit() or c == '.':
-                    number += c
-                else:
-                    unit += c
-            return float(number) * FACTORS.get(unit, 1)
-
         metrics = self.kubeutil.retrieve_metrics()
 
         excluded_labels = instance.get('excluded_labels')
@@ -450,7 +419,7 @@ class Kubernetes(AgentCheck):
                 # limits
                 try:
                     for limit, value_str in container['resources']['limits'].iteritems():
-                        values = [parse_quantity(s) for s in QUANTITY_EXP.findall(value_str)]
+                        values = [self.kubeutil.parse_quantity(s) for s in QUANTITY_EXP.findall(value_str)]
                         if len(values) != 1:
                             self.log.warning("Error parsing limits value string: %s", value_str)
                             continue
@@ -461,7 +430,7 @@ class Kubernetes(AgentCheck):
                 # requests
                 try:
                     for request, value_str in container['resources']['requests'].iteritems():
-                        values = [parse_quantity(s) for s in QUANTITY_EXP.findall(value_str)]
+                        values = [self.kubeutil.parse_quantity(s) for s in QUANTITY_EXP.findall(value_str)]
                         if len(values) != 1:
                             self.log.warning("Error parsing requests value string: %s", value_str)
                             continue
@@ -484,7 +453,7 @@ class Kubernetes(AgentCheck):
 
     def _update_pods_metrics(self, instance, pods):
         """
-        Reports the number of running pods, tagged by service and creator
+        Reports the number of running pods on this node, tagged by service and creator
 
         We go though all the pods, extract tags then count them by tag list, sorted and
         serialized in a pipe-separated string (it is an illegar character for tags)
