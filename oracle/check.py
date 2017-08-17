64a0646903f9065cb119a87a4d917cd10b041093
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-08-17 12:42:15 +0000

    [oracle] improve connection string generaiton.

--- oracle/check.py
+++ oracle/check.py
@@ -60,12 +60,12 @@ class Oracle(AgentCheck):
             return
 
         self.log.debug('Running cx_Oracle version {0}'.format(cx_Oracle.version))
-        server, user, password, tags = self._get_config(instance)
+        server, user, password, service, tags = self._get_config(instance)
 
         if not server or not user:
             raise Exception("Oracle host and user are needed")
 
-        con = self._get_connection(server, user, password)
+        con = self._get_connection(server, user, password, service)
 
         self._get_sys_metrics(con, tags)
 
@@ -75,14 +75,15 @@ class Oracle(AgentCheck):
         self.server = instance.get('server', None)
         user = instance.get('user', None)
         password = instance.get('password', None)
+        service = instance.get('service_name', None)
         tags = instance.get('tags', None)
-        return (self.server, user, password, tags)
+        return (self.server, user, password, service, tags)
 
-    def _get_connection(self, server, user, password):
+    def _get_connection(self, server, user, password, service):
         self.service_check_tags = [
             'server:%s' % server
         ]
-        connect_string = '{0}/{1}@{2}'.format(user, password, server)
+        connect_string = '{0}/{1}@//{2}/{3}'.format(user, password, server, service)
         try:
             con = cx_Oracle.connect(connect_string)
             self.log.debug("Connected to Oracle DB")
