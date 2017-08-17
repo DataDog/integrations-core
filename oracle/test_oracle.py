705d3644749251d5d38e14849924e11da99396b8
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-08-17 14:59:18 +0200

    [oracle][test] actually assert something.

--- oracle/test_oracle.py
+++ oracle/test_oracle.py
@@ -14,17 +14,8 @@ from tests.checks.common import AgentCheckTest
 logging.basicConfig()
 
 """
-Uses Oracle instance running in VM from:
-https://atlas.hashicorp.com/woznial/boxes/centos-6.3-oracle-xe
-
-Include following line in your Vagrantfile:
-
-config.vm.network "forwarded_port", guest: 1521, host: 8521
-
 Using the "system" user as permission granting not available
 for default "system" user
-
-Install oracle instant client in /opt/oracle
 
 Set up Oracle instant client:
 http://jasonstitt.com/cx_oracle_on_os_x
@@ -44,6 +35,34 @@ CONFIG = {
     }]
 }
 
+SERVICE_CHECK_NAME = 'oracle.can_connect'
+METRICS = [
+    'oracle.buffer_cachehit_ratio',
+    'oracle.cursor_cachehit_ratio',
+    'oracle.library_cachehit_ratio',
+    'oracle.shared_pool_free',
+    'oracle.physical_reads',
+    'oracle.physical_writes',
+    'oracle.enqueue_timeouts',
+    'oracle.gc_cr_receive_time',
+    'oracle.cache_blocks_corrupt',
+    'oracle.cache_blocks_lost',
+    'oracle.logons',
+    'oracle.active_sessions',
+    'oracle.long_table_scans',
+    'oracle.service_response_time',
+    'oracle.user_rollbacks',
+    'oracle.sorts_per_user_call',
+    'oracle.rows_per_sort',
+    'oracle.disk_sorts',
+    'oracle.memroy_sorts_ratio',
+    'oracle.database_wait_time_ratio',
+    'oracle.enqueue_timeouts',
+    'oracle.session_limit_usage',
+    'oracle.session_count',
+    'oracle.temp_space_used',
+]
+
 @attr(requires='oracle')
 class TestOracle(AgentCheckTest):
     """Basic Test for oracle integration."""
@@ -51,4 +70,9 @@ class TestOracle(AgentCheckTest):
 
     def testOracle(self):
         self.run_check_twice(CONFIG)
+
+        for m in METRICS:
+            self.assertMetric(m, at_least=1)
+
+        self.assertServiceCheck(SERVICE_CHECK_NAME)
         self.coverage_report()
