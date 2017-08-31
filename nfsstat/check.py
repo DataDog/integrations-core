9d7e30077c132a6cda825d9485a5371692ff8ef6
Greg Meyer <greg.meyer@datadoghq.com>
2017-08-31 01:01:40 -0400

    use the new nfsstat

--- nfsstat/check.py
+++ nfsstat/check.py
@@ -18,120 +18,84 @@ class NfsStatCheck(AgentCheck):
     metric_prefix = 'system.nfs.'
 
     def check(self, instance):
-        # get the stats from nfsiostat-sysstat
-        stat_out, _, _ = get_subprocess_output("nfsiostat-sysstat", self.log)
-        all_devices = [l.strip().split() for l in stat_out.splitlines()]
-        devices_raw = [l for l in all_devices[1:] if l]
+        # if they set the path, use that
+        if instance.get("nfsiostat_path"):
+            nfs_cmd = instance.get("nfsiostat_path")
+        else:
+            # if not, check if it's installed in the opt dir, if so use that
+            if os.path.exists("/opt/datadog-agent/embedded/sbin/nfsiostat"):
+                nfs_cmd = "/opt/datadog-agent/embedded/sbin/nfsiostat"
+            # if not, then check if it is in the default place
+            elif os.path.exists("/usr/local/sbin/nfsiostat"):
+                nfs_cmd = "/usr/local/sbin/nfsiostat"
 
-        all_mounts = self.get_mounts()
-
-        devices = []
-        metadata = []
-        for d in devices_raw:
-            if d[0] == "Filesystem:":
-                # if the first element is the metadata,
-                # set it as the metadata and continue
-                metadata = d
+        stat_out, err, _ = get_subprocess_output(nfs_cmd, self.log)
+        all_devices = []
+        this_device = []
+        for l in stat_out.splitlines():
+            if not l:
                 continue
-            device = Device(d, metadata, all_mounts, self.log)
-            devices.append(device)
+            elif l.find("mounted on") >= 0 and len(this_device) > 0:
+                # if it's a new device, create the device and add it to the array
+                device = Device(this_device, self.log)
+                all_devices.append(device)
+                this_device = []
+            this_device.append(l.strip().split())
 
-        for device in devices:
-            self.gauge(self.metric_prefix + 'read_per_sec',
-                        device.read, tags=device.tags)
-            self.gauge(self.metric_prefix + 'writes_per_sec',
-                        device.writes, tags=device.tags)
-            self.gauge(self.metric_prefix + 'read_direct_per_sec',
-                        device.read_direct, tags=device.tags)
-            self.gauge(self.metric_prefix + 'writes_direct_per_sec',
-                        device.writes_direct, tags=device.tags)
-            self.gauge(self.metric_prefix + 'read_from_server_per_sec',
-                        device.read_from_server, tags=device.tags)
-            self.gauge(self.metric_prefix + 'written_to_server_per_sec',
-                        device.written_to_server, tags=device.tags)
-            self.gauge(self.metric_prefix + 'ops_per_sec',
-                        device.ops, tags=device.tags)
-            self.gauge(self.metric_prefix + 'read_ops_per_sec',
-                        device.read_ops, tags=device.tags)
-            self.gauge(self.metric_prefix + 'write_ops_per_sec',
-                        device.write_ops, tags=device.tags)
+        # Add the last device into the array
+        device = Device(this_device, self.log)
+        all_devices.append(device)
 
-    def get_mounts(self):
-        mounts_raw = self.read_mounts()
-        all_mounts = [l.strip().split() for l in mounts_raw]
-        return all_mounts
+        for device in all_devices:
+            device.send_metrics(self.gauge)
 
-    def read_mounts(self):
-        # get the mounts, to get some additional metadata for tags
-        with open("/proc/mounts") as f:
-            mounts_raw = f.readlines()
-        return mounts_raw
-
-
-# Each NFS device is different
-# but it all requires essentially the same processing
-# Making each device an object class makes the
-# processing of the data a bit more straightforward
 class Device(object):
-    attrs = ['',
-            'read',
-            'writes',
-            'read_direct',
-            'writes_direct',
-            'read_from_server',
-            'written_to_server']
 
-    def __init__(self, device, metadata, all_mounts, log):
+    def __init__(self, device_data, log):
         self.log = log
 
-        self._device_data = device
-        self._metadata = metadata
-        self.device_name = device[0]
-        self.ops = float(device[7])
-        self.read_ops = float(device[8])
-        self.write_ops = float(device[9])
+        self._device_data = device_data
+        self._parse_device_header()
+        self._parse_tags()
+
+        self._parse_ops()
+        self._parse_read_data()
+        self._parse_write_data()
+
+    def _parse_device_header(self):
+        self._device_header = self._device_data[0]
+        self.log.info(self._device_header)
+        self.device_name = self._device_header[0]
+        self.mount = self._device_header[-1][:-1]
         self.nfs_server = self.device_name.split(":")[0]
         self.nfs_export = self.device_name.split(":")[1]
 
-        # all mounts are not related to the device, only the one it is mounted on
-        self._set_mount(all_mounts)
-        self._parse_data()
-        self._parse_tags()
+    def _parse_ops(self):
+        ops = self._device_data[2]
+        self.ops = float(ops[0])
+        self.rpc_bklog = float(ops[1])
 
+    def _parse_read_data(self):
+        read_data = self._device_data[4]
+        self.read_ops = float(read_data[0])
+        self.read_kb = float(read_data[1])
+        self.read_kb_per_s = float(read_data[2])
+        self.read_retrans = float(read_data[3])
+        self.read_retrans_pct = read_data[4].strip("(").strip(")").strip("%")
+        self.read_retrans_pct = float(self.read_retrans_pct)
+        self.read_avg_rtt = float(read_data[5])
+        self.read_avg_exe = float(read_data[6])
 
-    def _parse_data(self):
-        for i, m in enumerate(self._metadata):
-            if i == 0:
-                continue
-            if i > 6:
-                break
-
-            # Blk, kB and MB are the possible options for how these are
-            # displayed as detailed in the man page.
-            # The rates are displayed either as blocks, kilobytes, or megabytes
-            if m.find("Blk") >= 0:
-                dat = self._convert_blk(self._device_data[i])
-                setattr(self, self.attrs[i], dat)
-            elif m.find("kB") >= 0:
-                dat = self._convert_kB(self._device_data[i])
-                setattr(self, self.attrs[i], dat)
-            elif m.find("MB") >= 0:
-                dat = self._convert_MB(self._device_data[i])
-                setattr(self, self.attrs[i], dat)
-
-    def _convert_blk(self, number):
-        # a block is either 1K bytes or,
-        # if the environment variable POSIXLY_CORRECT is set, 512 bytes
-        convert = float(1000)
-        if os.getenv('POSIXLY_CORRECT', False):
-            convert = float(512)
-        return float(number) * convert
-
-    def _convert_kB(self, number):
-        return float(number) * float(1024)
-
-    def _convert_MB(self, number):
-        return float(number) * float(1048576)
+    def _parse_write_data(self):
+        write_data = self._device_data[6]
+        self.write_ops = float(write_data[0])
+        self.write_kb = float(write_data[1])
+        self.write_kb_per_s = float(write_data[2])
+        self.write_retrans = float(write_data[3])
+        self.write_retrans_pct = write_data[4].strip("(").strip(")").strip("%")
+        self.write_retrans_pct = float(self.write_retrans_pct)
+        self.write_avg_rtt = float(write_data[5])
+        self.write_avg_exe = float(write_data[6])
 
     def _parse_tags(self):
         self.tags = []
@@ -139,9 +103,25 @@ class Device(object):
         self.tags.append("nfs_export:{0}".format(self.nfs_export))
         self.tags.append("nfs_mount:{0}".format(self.mount))
 
-    def _set_mount(self, mounts):
-        # for now, only the mount point is relevant
-        for m in mounts:
-            if m[0] == self.device_name:
-                self.mount = m[1]
-                break
+    def send_metrics(self, gauge):
+        metric_prefix = 'system.nfs.'
+        gauge(metric_prefix + "ops", self.ops, tags=self.tags)
+        gauge(metric_prefix + "rpc_bklog", self.rpc_bklog, tags=self.tags)
+
+        read_metric_prefix = metric_prefix + "read"
+        gauge(read_metric_prefix + ".ops", self.read_ops, tags=self.tags)
+        gauge(read_metric_prefix, self.read_kb, tags=self.tags)
+        gauge(read_metric_prefix + "_per_s", self.read_kb_per_s, tags=self.tags)
+        gauge(read_metric_prefix + ".retrans", self.read_retrans, tags=self.tags)
+        gauge(read_metric_prefix + ".retrans.pct", self.read_retrans_pct, tags=self.tags)
+        gauge(read_metric_prefix + ".rtt", self.read_avg_rtt, tags=self.tags)
+        gauge(read_metric_prefix + ".exe", self.read_avg_exe, tags=self.tags)
+
+        write_metric_prefix = metric_prefix + "write"
+        gauge(write_metric_prefix + ".ops", self.write_ops, tags=self.tags)
+        gauge(write_metric_prefix, self.write_kb, tags=self.tags)
+        gauge(write_metric_prefix + "_per_s", self.write_kb_per_s, tags=self.tags)
+        gauge(write_metric_prefix + ".retrans", self.write_retrans, tags=self.tags)
+        gauge(write_metric_prefix + ".retrans.pct", self.write_retrans_pct, tags=self.tags)
+        gauge(write_metric_prefix + ".rtt", self.write_avg_rtt, tags=self.tags)
+        gauge(write_metric_prefix + ".exe", self.write_avg_exe, tags=self.tags)
--- nfsstat/test_nfsstat.py
+++ nfsstat/test_nfsstat.py
@@ -13,15 +13,22 @@ import mock
 from tests.checks.common import AgentCheckTest, Fixtures
 
 metrics = [
-    'system.nfs.read_per_sec',
-    'system.nfs.writes_per_sec',
-    'system.nfs.read_direct_per_sec',
-    'system.nfs.writes_direct_per_sec',
-    'system.nfs.read_from_server_per_sec',
-    'system.nfs.written_to_server_per_sec',
-    'system.nfs.ops_per_sec',
-    'system.nfs.read_ops_per_sec',
-    'system.nfs.write_ops_per_sec',
+    'system.nfs.ops',
+    'system.nfs.rpc_bklog',
+    'system.nfs.read',
+    'system.nfs.read.ops',
+    'system.nfs.read_per_s',
+    'system.nfs.read.retrans',
+    'system.nfs.read.retrans.pct',
+    'system.nfs.read.rtt',
+    'system.nfs.read.exe',
+    'system.nfs.write',
+    'system.nfs.write.ops',
+    'system.nfs.write_per_s',
+    'system.nfs.write.retrans',
+    'system.nfs.write.retrans.pct',
+    'system.nfs.write.rtt',
+    'system.nfs.write.exe',
 ]
 
 FIXTURE_DIR = os.path.join(os.path.dirname(__file__), 'ci')
@@ -46,9 +53,8 @@ class TestNfsstat(AgentCheckTest):
         self.load_check({'instances': [self.CONFIG]})
 
     @mock.patch('_nfsstat.get_subprocess_output',
-                return_value=(Fixtures.read_file('nfsiostat-sysstat', sdk_dir=FIXTURE_DIR), "", 0))
-    @mock.patch('_nfsstat.NfsStatCheck.read_mounts', return_value=(mock_mounts()))
-    def test_check(self, mock_dfs, mock_mounts):
+                return_value=(Fixtures.read_file('nfsiostat', sdk_dir=FIXTURE_DIR), "", 0))
+    def test_check(self, nfsiostat_mocks):
         """
         Testing Nfsstat check.
         """
@@ -56,11 +62,14 @@ class TestNfsstat(AgentCheckTest):
             'instances': [{}]
         }
         self.run_check(config)
+
         nfs_server_tag = 'nfs_server:192.168.34.1'
         nfs_export_tag = 'nfs_export:/exports/nfs/datadog/{0}'
         nfs_mount_tag = 'nfs_mount:/mnt/datadog/{0}'
 
         folder_names = ['one', 'two', 'three', 'four', 'five']
+
+        # self.assertTrue(False)
 
         for metric in metrics:
             for folder in folder_names:
