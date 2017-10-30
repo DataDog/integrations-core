d412c518b67d0dea576f9ab118bc7aab63b2713e
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-10-30 13:48:37 +0100

    [rabbitmq] fix tests for wheels

--- rabbitmq/test/test_rabbitmq.py
+++ rabbitmq/test/test_rabbitmq.py
@@ -217,7 +217,8 @@ class TestRabbitMQ(AgentCheckTest):
 
     def test__get_data(self):
         with mock.patch('check.requests') as r:
-            from check import RabbitMQ, RabbitMQException  # pylint: disable=import-error,no-name-in-module
+            from datadog.rabbitmq import RabbitMQ  # pylint: disable=import-error,no-name-in-module
+            from datadog.rabbitmq.rabbitmq import RabbitMQException  # pylint: disable=import-error,no-name-in-module
             check = RabbitMQ('rabbitmq', {}, {"instances": [{"rabbitmq_api_url": "http://example.com"}]})
             r.get.side_effect = [requests.exceptions.HTTPError, ValueError]
             self.assertRaises(RabbitMQException, check._get_data, '')
@@ -273,6 +274,6 @@ class TestRabbitMQ(AgentCheckTest):
         self.assertEqual(sc[1]['status'], AgentCheck.CRITICAL)
 
         # in case of connection errors, this check should stay silent
-        from check import RabbitMQException  # pylint: disable=import-error,no-name-in-module
+        from datadog.rabbitmq.rabbitmq import RabbitMQException  # pylint: disable=import-error,no-name-in-module
         self.check._get_data.side_effect = RabbitMQException
         self.assertRaises(RabbitMQException, self.check._get_vhosts, instances['instances'][0], '')
