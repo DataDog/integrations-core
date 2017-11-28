4ef393c5b0b28fb912b4341ab286fd6db2bb50c0
Trishank K Kuppusamy <trishank.kuppusamy@datadoghq.com>
2017-11-28 14:21:03 -0500

    Add exception handling.

--- make-wheels.py
+++ make-wheels.py
@@ -19,19 +19,33 @@ def main():
                   and not name.startswith('.'))
     # Get the current working directory.
     pwd = os.getcwd()
+
     # For each directory:
     for name in dirs:
         # Get the next working directory.
         nwd = os.path.join(pwd, name)
         # Change to the next working directory.
         os.chdir(nwd)
+
         # If the next working directory has 'setup.py', then we can build.
         if os.path.isfile('setup.py'):
             print('Trying to build {}...'.format(name), end='')
             # https://packaging.python.org/tutorials/distributing-packages/#wheels
             cmd = 'python setup.py bdist_wheel'.split()
-            proc = subprocess.run(cmd, check=True, stdout=subprocess.PIPE)
-            print('done.'.format(name))
+
+            # Try building.
+            try:
+                proc = subprocess.run(cmd, check=True, stdout=subprocess.PIPE,
+                                      universal_newlines=True)
+            # If something went wrong, raise an exception.
+            except:
+                print(proc.stderr)
+                raise
+            # Otherwise, check that all went well, and print output.
+            else:
+                proc.check_returncode()
+                print(proc.stdout)
+                print()
 
 
 if __name__ == '__main__':
