434e77a9ff21f28801ebaccd8258fe6e23f513ea
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2017-12-22 17:06:17 +0100

    [squid] Add integration (#937)
    * Add squid check
    
    * Add tests
    
    * Address review comments
    
    * Update setup.py
    
    * Update directory structure
    
    * Add cachemgr to the metric namespace

---
+++ squid/setup.py
@@ -0,0 +1,99 @@
+# Always prefer setuptools over distutils
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+# module version
+from datadog_checks.squid import __version__
+
+import json
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+runtime_reqs = ['datadog-checks-base']
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    for line in f.readlines():
+        req = line.rpartition('#')
+        if not len(req[1]):
+            if '--hash=' in req[2]:
+                tokens = req[2].split()
+                if len(tokens) > 1:
+                    runtime_reqs.append(tokens[0])
+            elif ';' in req[2]:
+                runtime_reqs.append(req[2])
+
+version = __version__
+manifest_version = None
+with open(path.join(here, 'manifest.json'), encoding='utf-8') as f:
+    manifest = json.load(f)
+    manifest_version = manifest.get('version')
+
+if version != manifest_version:
+    raise Exception("Inconsistent versioning in module and manifest - aborting wheel build")
+
+setup(
+    name='datadog-squid',
+    version=version,
+    description='The Squid check',
+    long_description=long_description,
+    keywords='datadog agent squid check',
+
+    # The project's main homepage.
+    url='https://github.com/DataDog/integrations-core',
+
+    # Author details
+    author='Datadog',
+    author_email='packages@datadoghq.com',
+
+    # License
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Monitoring',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # The package we're going to ship
+    packages=['datadog_checks.squid'],
+
+    # Run-time dependencies
+    install_requires=list(set(runtime_reqs)),
+
+    # Development dependencies, run with:
+    # $ pip install -e .[dev]
+    extras_require={
+        'dev': [
+            'check-manifest',
+            'datadog_agent_tk>=5.15',
+        ],
+    },
+
+    # Testing setup and dependencies
+    tests_require=[
+        'nose',
+        'coverage',
+        'datadog_agent_tk>=5.15',
+    ],
+    test_suite='nose.collector',
+
+    # Extra files to ship with the wheel package
+    package_data={b'datadog_checks.squid': ['squid.yaml.example']},
+    include_package_data=True,
+
+    # The entrypoint to run the check manually without an agent
+    entry_points={
+        'console_scripts': [
+            'squid=datadog_checks.squid:main',
+        ],
+    },
+)
---
+++ squid/test/test_squid.py
@@ -0,0 +1,153 @@
+# (C) Datadog, Inc. 2010-2016
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
+# stdlib
+from nose.plugins.attrib import attr
+
+# 3p
+
+# project
+from tests.checks.common import AgentCheckTest
+
+expected_metrics = [
+    "client_http.requests",
+    "client_http.hits",
+    "client_http.errors",
+    "client_http.kbytes_in",
+    "client_http.kbytes_out",
+    "client_http.hit_kbytes_out",
+    "server.all.requests",
+    "server.all.errors",
+    "server.all.kbytes_in",
+    "server.all.kbytes_out",
+    "server.http.requests",
+    "server.http.errors",
+    "server.http.kbytes_in",
+    "server.http.kbytes_out",
+    "server.ftp.requests",
+    "server.ftp.errors",
+    "server.ftp.kbytes_in",
+    "server.ftp.kbytes_out",
+    "server.other.requests",
+    "server.other.errors",
+    "server.other.kbytes_in",
+    "server.other.kbytes_out",
+    "icp.pkts_sent",
+    "icp.pkts_recv",
+    "icp.queries_sent",
+    "icp.replies_sent",
+    "icp.queries_recv",
+    "icp.replies_recv",
+    "icp.query_timeouts",
+    "icp.replies_queued",
+    "icp.kbytes_sent",
+    "icp.kbytes_recv",
+    "icp.q_kbytes_sent",
+    "icp.r_kbytes_sent",
+    "icp.q_kbytes_recv",
+    "icp.r_kbytes_recv",
+    "icp.times_used",
+    "cd.times_used",
+    "cd.msgs_sent",
+    "cd.msgs_recv",
+    "cd.memory",
+    "cd.local_memory",
+    "cd.kbytes_sent",
+    "cd.kbytes_recv",
+    "unlink.requests",
+    "page_faults",
+    "select_loops",
+    "cpu_time",
+    "swap.outs",
+    "swap.ins",
+    "swap.files_cleaned",
+    "aborted_requests",
+]
+
+@attr(requires="squid")
+class TestSquidIntegration(AgentCheckTest):
+    """Integration tests for squid."""
+    CHECK_NAME = "squid"
+
+    def test_check_ok(self):
+        conf = {
+            "init_config": {},
+            "instances": [{
+                "name": "ok_instance",
+                "tags": ["custom_tag"]
+            }]
+        }
+        self.run_check_twice(conf)
+        self.assertServiceCheckOK("squid.can_connect", tags=["name:ok_instance", "custom_tag"])
+        for metric in expected_metrics:
+            self.assertMetric("squid.cachemgr." + metric, tags=["name:ok_instance", "custom_tag"])
+        self.coverage_report()
+
+    def test_check_fail(self):
+        conf = {
+            "init_config": {},
+            "instances": [{
+                "name": "fail_instance",
+                "host": "bad_host"
+            }]
+        }
+        self.assertRaises(Exception, self.run_check_twice, conf)
+        self.assertServiceCheckCritical("squid.can_connect", tags=["name:fail_instance"])
+
+class TestSquidUnit(AgentCheckTest):
+    """Unit tests for squid"""
+    CHECK_NAME = "squid"
+
+    def test_parse_counter(self):
+        self.load_check({}, {})
+
+        # Good format 
+        line = "counter = 0\n"
+        counter, value = self.check.parse_counter(line)
+        self.assertEquals(counter, "counter")
+        self.assertEquals(value, "0")
+
+        # Bad format
+        line = "counter: value\n"
+        counter, value = self.check.parse_counter(line)
+        self.assertEquals(counter, None)
+        self.assertEquals(value, None)
+
+    def test_parse_instance(self):
+        self.load_check({}, {})
+
+        # instance with defaults
+        instance = {
+            "name": "ok_instance"
+        }
+        name, host, port, cachemgr_user, cachemgr_passwd, custom_tags = self.check.parse_instance(instance)
+        self.assertEquals(name, "ok_instance")
+        self.assertEquals(host, "localhost")
+        self.assertEquals(port, 3128)
+        self.assertEquals(cachemgr_passwd, "")
+        self.assertEquals(cachemgr_user, "")
+        self.assertEquals(custom_tags, [])
+
+        # instance no defaults
+        instance = {
+            "name": "ok_instance",
+            "host": "host",
+            "port": 1234,
+            "cachemgr_username": "datadog",
+            "cachemgr_password": "pass",
+            "tags": ["foo:bar"],
+        }
+        name, host, port, cachemgr_user, cachemgr_passwd, custom_tags = self.check.parse_instance(instance)
+        self.assertEquals(name, "ok_instance")
+        self.assertEquals(host, "host")
+        self.assertEquals(port, 1234)
+        self.assertEquals(cachemgr_user, "datadog")
+        self.assertEquals(cachemgr_passwd, "pass")
+        self.assertEquals(custom_tags, ["foo:bar"])
+
+        # instance no name
+        instance = {
+            "host": "host"
+        }
+        self.assertRaises(Exception, self.check.parse_instance)
