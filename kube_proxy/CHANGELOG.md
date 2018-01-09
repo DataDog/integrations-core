d15bdcdc713cca23a55559471a9fda016b48e84e
Pierre <mfpierre@gmail.com>
2018-01-09 14:04:15 +0100

    Add kube proxy integration

---
+++ kube_proxy/CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG - Kube_proxy
+
+0.1.0/ Unreleased
+==================
+
+### Changes
+
+* [FEATURE] adds kube_proxy integration.
---
+++ kube_proxy/check.py
@@ -0,0 +1,13 @@
+# (C) Datadog, Inc. 2010-2018
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
+# stdlib
+
+# 3rd party
+
+# project
+from checks.generic_prometheus_check import GenericPrometheusCheck
+
+class KubeProxyCheck(GenericPrometheusCheck):
+    pass
