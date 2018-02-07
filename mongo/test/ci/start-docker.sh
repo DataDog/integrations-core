82575a4d1c9bad194f9805b0cfbaa4d5d0fbb4be
Greg Meyer <greg.meyer@datadoghq.com>
2018-02-07 13:45:51 -0500

    remove some comments

--- mongo/test/ci/start-docker.sh
+++ mongo/test/ci/start-docker.sh
@@ -45,26 +45,20 @@ do
     sleep 2
 done
 
-# echo 'docker exec -it $NAME mongo --eval "rs.initiate();" localhost:$PORT'
 docker exec -it $NAME mongo --eval "rs.initiate();" --host localhost --port $PORT
 
-# echo 'docker exec -it $NAME mongo --eval cfg = rs.conf(); cfg.members[0].host = "localhost:$PORT"; rs.reconfig(cfg); printjson(rs.conf()); localhost:$PORT'
-# echo "cfg = rs.conf(); cfg.members[0].host = '$SHARD00_IP:$PORT'; rs.reconfig(cfg); printjson(rs.conf());"
 docker exec -it $NAME mongo --eval "cfg = rs.conf(); cfg.members[0].host = '$SHARD00_IP:$PORT'; rs.reconfig(cfg); printjson(rs.conf());" --host localhost --port $PORT
 
 sleep 2
 
-# echo 'docker exec -it $NAME mongo --eval "printjson(rs.add(\'$SHARD01_IP:$PORT1\\')); printjson(rs.status());" localhost:$PORT'
 docker exec -it $NAME mongo --eval "printjson(rs.add('$SHARD01_IP:$PORT1')); printjson(rs.status());" --host localhost --port $PORT
-
-# echo 'docker exec -it $NAME mongo --eval "printjson(rs.add(\'$SHARD02_IP:$PORT2\\')); printjson(rs.status());" localhost:$PORT'
 docker exec -it $NAME mongo --eval "printjson(rs.add('$SHARD02_IP:$PORT2')); printjson(rs.status());" --host localhost --port $PORT
-
 docker exec -it $NAME bash -l -c "mongo --eval 'db.getMongo().getDBNames()' --port $PORT --host localhost test"
 docker exec -it $NAME bash -l -c "mongo --eval 'db.getCollectionNames()' --port $PORT --host localhost test"
 
 sleep 2
 
+# Don't print the commands within the loop, there's too much output.
 set +x
 echo "Checking if shards are initialized and then waiting until they are initialized"
 until docker exec -it $NAME mongo --eval "printjson(rs.status());" --host localhost --port $PORT | grep '"stateStr" : "SECONDARY"' >/dev/null;
@@ -75,8 +69,6 @@ set -x
 
 echo "The shards have been initialized"
 
-# echo 'docker exec -it $NAME mongo --eval cfg = rs.conf(); cfg.members[0].host = "localhost:$PORT"; rs.reconfig(cfg); printjson(rs.conf()); localhost:$PORT'
-# echo "cfg = rs.conf(); cfg.members[0].host = '$SHARD00_IP:$PORT'; rs.reconfig(cfg); printjson(rs.conf());"
 docker exec -it $NAME mongo --eval "cfg = rs.conf(); cfg.members[0].host = '$SHARD00_IP:$PORT'; rs.reconfig(cfg); printjson(rs.conf());" --host localhost --port $PORT
 
 echo "Setting test user"
