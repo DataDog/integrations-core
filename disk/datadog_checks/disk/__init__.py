be0a078aa004470a450dddfa5a8e770b2e0ad97c
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-02-07 03:41:13 +0000

    [Disk] Fix flake8 issues and ignore unused

--- disk/datadog_checks/disk/__init__.py
+++ disk/datadog_checks/disk/__init__.py
@@ -1,9 +1,9 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from .__about__ import __version__
-from .disk import Disk
+from .__about__ import __version__  # NOQA F401
+from .disk import Disk  # NOQA F401
 
 all = [
-  '__version__', 'Disk'
+    '__version__', 'Disk'
 ]
