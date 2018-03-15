595eb9bf202c8a3c97c27683b7bb947cce2dd388
Ofek Lev <ofekmeister@gmail.com>
2018-03-15 15:19:50 -0400

    finish script

--- upgrage-dep.py
+++ upgrage-dep.py
@@ -18,7 +18,7 @@ def chdir(d, cwd=None):
         os.chdir(origin)
 
 
-def ensure_reqs_declared(reqs_txt, reqs_in):
+def ensure_deps_declared(reqs_txt, reqs_in):
     if os.path.isfile(reqs_txt) and not os.path.isfile(reqs_in):
         declacred_lines = []
 
@@ -30,8 +30,18 @@ def ensure_reqs_declared(reqs_txt, reqs_in):
             if line and not line.startswith('#'):
                 declacred_lines.append(line + '\n')
 
-        with open(reqs_in, 'w', encoding='utf-8') as f:
+        with open(reqs_in, 'w', encoding='utf-8', newline='\n') as f:
             f.writelines(declacred_lines)
+
+
+def compile_check_deps(check_dir):
+    with chdir(check_dir):
+        return subprocess.check_output([
+            'pip-compile',
+            '--generate-hashes',
+            '--output-file', 'requirements.txt',
+            'requirements.in'
+        ], stderr=subprocess.STDOUT)
 
 
 def main():
@@ -50,12 +60,12 @@ def main():
 
     package, version = args[:2]
 
-    for path in os.listdir(HERE):
-        check_dir = os.path.join(HERE, path)
+    for check_name in sorted(os.listdir(HERE)):
+        check_dir = os.path.join(HERE, check_name)
         reqs_in = os.path.join(check_dir, 'requirements.in')
         reqs_txt = os.path.join(check_dir, 'requirements.txt')
 
-        ensure_reqs_declared(reqs_txt, reqs_in)
+        ensure_deps_declared(reqs_txt, reqs_in)
 
         if os.path.isfile(reqs_in):
             with open(reqs_in, 'r', encoding='utf-8') as f:
@@ -71,20 +81,21 @@ def main():
             else:
                 continue
 
-            print(repr(lines[i]), pkg, reqs_in)
+            if verbose:
+                print('Check `{}:'.format(check_name))
+                print('    Old: `{}`'.format(lines[i].strip()))
+
+            lines[i] = '{}=={}\n'.format(package, version)
+
+            with open(reqs_in, 'w', encoding='utf-8', newline='\n') as f:
+                f.writelines(lines)
+
+            if verbose:
+                print('    New: `{}`'.format(lines[i].strip()))
+                print('Locking dependencies...')
+
+            compile_check_deps(check_dir)
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
