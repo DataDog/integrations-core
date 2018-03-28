f56a47d3078b4dee3a25dbf5abdbc892970312ab
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-28 16:39:22 -0400

    adds real headers

--- datadog-checks-base/datadog_checks/stubs/_util.py
+++ datadog-checks-base/datadog_checks/stubs/_util.py
@@ -5,14 +5,3 @@
 
 def subprocess_output(*args, **kwargs):
     pass
-
-def headers(*args, **kwargs):
-    # Build the request headers
-    res = {
-        'User-Agent': 'Datadog Agent/%s' % 'test',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'text/html, */*',
-    }
-    if 'http_host' in kwargs:
-        res['Host'] = kwargs['http_host']
-    return res
