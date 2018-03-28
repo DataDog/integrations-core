88064df86dee61bcfa62f369059a8384aa5c1d55
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-28 16:14:57 -0400

    adds util

---
+++ datadog-checks-base/datadog_checks/util.py
@@ -0,0 +1,14 @@
+# (C) Datadog, Inc. 2010-2018
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
+def headers(agentConfig, **kwargs):
+    # Build the request headers
+    res = {
+        'User-Agent': 'Datadog Agent/%s' % agentConfig['version'],
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'text/html, */*',
+    }
+    if 'http_host' in kwargs:
+        res['Host'] = kwargs['http_host']
+    return res
