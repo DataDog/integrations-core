e8105bdc5870d1b5cd831e55300229328d453d62
Greg Meyer <greg.meyer@datadoghq.com>
2018-03-30 11:45:39 -0400

    adds a cleanup task

--- tasks.py
+++ tasks.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, unicode_literals
 
 import os
+import fnmatch
+import shutil
 from io import open
 
 from invoke import task
@@ -139,3 +141,36 @@ def upgrade(ctx, package=None, version=None, verbose=False):
                     'requirements.in',
                     hide='both'
                 )
+
+@task(help={})
+def cleanup(ctx):
+    """
+    Cleanup all temporary files and directories
+
+    Example invocation:
+        inv cleanup
+    """
+
+    should_delete_directories = [
+        ".eggs",
+        ".pytest_cache",
+        ".tox",
+        "__pycache__"
+    ]
+    should_delete_directories_globs = [
+        "*.egg-info"
+    ]
+
+    for path, dirnames, filenames in os.walk('.'):
+        for filename in fnmatch.filter(filenames, '*.pyc'):
+            file_path = os.path.join(HERE, path, filename)
+            os.remove(file_path)
+        for dirname in dirnames:
+            if dirname in should_delete_directories:
+                dir_path = os.path.join(HERE, path, dirname)
+                shutil.rmtree(dir_path)
+
+        for dir_glob in should_delete_directories_globs:
+            for dirname in fnmatch.filter(dirnames, dir_glob):
+                dir_path = os.path.join(HERE, path, dirname)
+                shutil.rmtree(dir_path)
