2928a7aa0902dcf9e9781cc194fd1d4730540132
Ofek Lev <ofekmeister@gmail.com>
2018-04-02 19:02:29 -0400

    fix style

--- ssh_check/datadog_checks/ssh_check/ssh_check.py
+++ ssh_check/datadog_checks/ssh_check/ssh_check.py
@@ -78,19 +78,24 @@ class CheckSSH(AgentCheck):
         try:
             # Try to connect to check status of SSH
             try:
-                client.connect(conf.host, port=conf.port, username=conf.username,
-                    password=conf.password, pkey=private_key)
-                self.service_check(self.SSH_SERVICE_CHECK_NAME, AgentCheck.OK, tags=tags,
-                    message=exception_message)
+                client.connect(
+                    conf.host, port=conf.port, username=conf.username,
+                    password=conf.password, pkey=private_key
+                )
+                self.service_check(
+                    self.SSH_SERVICE_CHECK_NAME, AgentCheck.OK, tags=tags, message=exception_message
+                )
 
             except Exception as e:
                 exception_message = str(e)
                 status = AgentCheck.CRITICAL
-                self.service_check(self.SSH_SERVICE_CHECK_NAME, status, tags=tags,
-                    message=exception_message)
+                self.service_check(
+                    self.SSH_SERVICE_CHECK_NAME, status, tags=tags, message=exception_message
+                )
                 if conf.sftp_check:
-                    self.service_check(self.SFTP_SERVICE_CHECK_NAME, status, tags=tags,
-                        message=exception_message)
+                    self.service_check(
+                        self.SFTP_SERVICE_CHECK_NAME, status, tags=tags, message=exception_message
+                    )
                 raise
 
             # Open sftp session on the existing connection to check status of SFTP
@@ -112,8 +117,9 @@ class CheckSSH(AgentCheck):
                 if exception_message is None:
                     exception_message = "No errors occured"
 
-                self.service_check(self.SFTP_SERVICE_CHECK_NAME, status, tags=tags,
-                    message=exception_message)
+                self.service_check(
+                    self.SFTP_SERVICE_CHECK_NAME, status, tags=tags, message=exception_message
+                )
         finally:
             # Always close the client, failure to do so leaks one thread per connection left open
             client.close()
