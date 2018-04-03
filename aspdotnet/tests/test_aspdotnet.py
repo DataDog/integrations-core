0a6e808ee49df4ed722a48b5fd01d024fb7e69f2
Derek Brown <derek.brown@datadoghq.com>
2018-04-03 16:50:18 -0700

    Make sure to include the fixtures in the common wheel

--- aspdotnet/tests/test_aspdotnet.py
+++ aspdotnet/tests/test_aspdotnet.py
@@ -2,12 +2,10 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-import mock
 import os
 import pytest
 from datadog_checks.stubs import aggregator
 from datadog_checks.aspdotnet import AspdotnetCheck
-#import datadog_checks_tests_helper.datadog_test_libs.win
 from datadog_test_libs.win.pdh_mocks import pdh_mocks_fixture, initialize_pdh_tests
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -25,7 +23,6 @@ INSTANCE_WITH_TAGS = {
 def Aggregator():
     aggregator.reset()
     return aggregator
-
 
 
 CHECK_NAME = 'aspdotnet'
@@ -67,6 +64,8 @@ ASP_APP_INSTANCES = (
     "_LM_W3SVC_2_ROOT_owa",
     "_LM_W3SVC_1_ROOT_owa",
 )
+
+
 @pytest.mark.usefixtures("pdh_mocks_fixture")
 def test_basic_check(Aggregator, pdh_mocks_fixture):
     initialize_pdh_tests()
@@ -82,6 +81,7 @@ def test_basic_check(Aggregator, pdh_mocks_fixture):
             Aggregator.assert_metric(metric, tags=["instance:%s" % i], count=1)
 
     assert Aggregator.metrics_asserted_pct == 100.0
+
 
 def test_with_tags(Aggregator, pdh_mocks_fixture):
     initialize_pdh_tests()
--- datadog-checks-tests-helper/setup.py
+++ datadog-checks-tests-helper/setup.py
@@ -49,6 +49,7 @@ setup(
 
     packages=find_packages(),
 
+    include_package_data=True,
     install_requires=get_requirements('requirements.in'),
 
     #setup_requires=['pytest-runner', ],
