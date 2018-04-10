e22c26cb8d4c6a0db88ba55a331b6079ff1bc82e
Roman Sheydvasser <s.roman@gmail.com>
2018-04-10 14:18:48 -0400

    [marathon] add task counts metrics for groups
    Specifies 'apps.counts' parameter to embed all task counts (tasksStaged, tasksRunning, tasksHealthy, tasksUnhealthy). Adds support for task count metrics when filtering Marathon metrics by group. Also future-proofs class against when task counts are not embedded by default in plain 'apps' API call which we make when not filtering by groups.

--- marathon/datadog_checks/marathon/marathon.py
+++ marathon/datadog_checks/marathon/marathon.py
@@ -151,9 +151,9 @@ class Marathon(AgentCheck):
     def process_apps(self, url, timeout, auth, acs_url, ssl_verify, tags=None, group=None):
         # Marathon apps
         if group is None:
-            marathon_path = urljoin(url, "v2/apps")
+            marathon_path = urljoin(url, "v2/apps?embed=apps.counts")
         else:
-            marathon_path = urljoin(url, "v2/groups/{}".format(group))
+            marathon_path = urljoin(url, "v2/groups/{}?embed=group.apps.counts".format(group))
         response = self.get_json(marathon_path, timeout, auth, acs_url, ssl_verify, tags)
         if response is not None:
             self.gauge('marathon.apps', len(response['apps']), tags=tags)
