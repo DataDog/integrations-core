1504400ca801608826740defc10a253d6498054f
Derek Brown <derek.brown@datadoghq.com>
2018-04-12 11:28:35 -0700

    [windows] [dotnetclr] Update to new wheel/testing infrastructure

--- dotnetclr/setup.py
+++ dotnetclr/setup.py
@@ -4,35 +4,22 @@ from setuptools import setup
 from codecs import open
 from os import path
 
-import json
-import re
-
 here = path.abspath(path.dirname(__file__))
 
-def parse_req_line(line):
-    line = line.strip()
-    if not line or line.startswith('--hash') or line[0] == '#':
-        return None
-    req = line.rpartition('#')
-    if len(req[1]) == 0:
-        line = req[2].strip()
-    else:
-        line = req[1].strip()
+HERE = path.dirname(path.abspath(__file__))
 
-    if '--hash=' in line:
-        line = line[:line.find('--hash=')].strip()
-    if ';' in line:
-        line = line[:line.find(';')].strip()
-    if '\\' in line:
-        line = line[:line.find('\\')].strip()
-
-    return line
+# Get version info
+ABOUT = {}
+with open(path.join(HERE, 'datadog_checks', 'dotnetclr', '__about__.py')) as f:
+    exec(f.read(), ABOUT)
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+
 # Parse requirements
+<<<<<<< HEAD
 runtime_reqs = ['datadog_checks_base']
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     for line in f.readlines():
@@ -62,13 +49,19 @@ with open(path.join(here, 'manifest.json'), encoding='utf-8') as f:
 
 if version != manifest_version:
     raise Exception("Inconsistent versioning in module and manifest - aborting wheel build")
+=======
+def get_requirements(fpath):
+    with open(path.join(HERE, fpath), encoding='utf-8') as f:
+        return f.readlines()
+
+>>>>>>> [windows] [dotnetclr] Update to new wheel/testing infrastructure
 
 setup(
     name='datadog-dotnetclr',
-    version=version,
-    description='The DotNetclr check',
+    version=ABOUT["__version__"],
+    description='The .NET CLR check',
     long_description=long_description,
-    keywords='datadog agent dotnetclr check',
+    keywords='datadog agent .NET CLR check',
 
     # The project's main homepage.
     url='https://github.com/DataDog/integrations-core',
@@ -78,7 +71,7 @@ setup(
     author_email='packages@datadoghq.com',
 
     # License
-    license='MIT',
+    license='BSD',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -95,26 +88,15 @@ setup(
     packages=['datadog_checks.dotnetclr'],
 
     # Run-time dependencies
-    install_requires=list(set(runtime_reqs)),
-
-    # Development dependencies, run with:
-    # $ pip install -e .[dev]
-    extras_require={
-        'dev': [
-            'check-manifest',
-            'datadog_agent_tk>=5.15',
-        ],
-    },
+    install_requires=get_requirements('requirements.in')+[
+        'datadog-checks-base',
+    ],
 
     # Testing setup and dependencies
-    tests_require=[
-        'nose',
-        'coverage',
-        'datadog_agent_tk>=5.15',
-    ],
-    test_suite='nose.collector',
+    setup_requires=['pytest-runner'],
+    tests_require=get_requirements('requirements-dev.txt'),
 
     # Extra files to ship with the wheel package
-    package_data={b'datadog_checks.dotnetclr': ['conf.yaml.example']},
+    package_data={'datadog_checks.dotnetclr': ['conf.yaml.example']},
     include_package_data=True,
 )
