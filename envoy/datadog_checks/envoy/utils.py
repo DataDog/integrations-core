760900bf2ab111cfdf5bc1821b8619667334aaf5
Ofek Lev <ofekmeister@gmail.com>
2018-04-14 15:44:30 -0400

    finalize tree design

--- envoy/datadog_checks/envoy/utils.py
+++ envoy/datadog_checks/envoy/utils.py
@@ -1,34 +1,37 @@
-from collections import defaultdict
-
-
-def tree():
-    return defaultdict(tree)
-
-
 def make_metric_tree(metrics):
-    metric_tree = tree()
+    metric_tree = {}
 
     for metric in metrics:
-        # We make `mapping` reference the root of the tree
+        # We make `tree` reference the root of the tree
         # at every iteration to create the new branches.
-        mapping = metric_tree
+        tree = metric_tree
 
         # Separate each full metric name into its constituent parts.
         parts = metric.split('.')
 
         for i, part in enumerate(parts):
+            # Create branch if necessary.
+            if part not in tree:
+                tree[part] = {}
+
+            # Move to the next branch.
+            tree = tree[part]
+
+            # Create a list for our possible tag configurations if necessary.
+            if '|_tags_|' not in tree:
+                tree['|_tags_|'] = []
+
+            # Get the tag configuration for the current branch's metric part.
             tags = metrics[metric]['tags'][i]
-            num_tags = len(tags)
 
-            # We'll create keys as they are referenced. See:
-            # https://en.wikipedia.org/wiki/Autovivification
-            mapping = mapping[part]
-            mapping['tag_length_map'][num_tags] = tags
+            # Ensure the tag configuration doesn't already exist.
+            if tags not in tree['|_tags_|']:
+                # Each metric part can be proceeded by a differing number of tags.
+                tree['|_tags_|'].append(tags)
 
-            # Pre-compute the minimum number of tags it will take
-            # for the parser to proceed to the subsequent part.
-            minumum_num_tags = mapping.get('minumum_num_tags', 0)
-            if num_tags < minumum_num_tags:
-                mapping['minumum_num_tags'] = num_tags
+                # Sort possible tag configurations by length in reverse order.
+                tree['|_tags_|'] = sorted(
+                    tree['|_tags_|'], key=lambda t: len(t), reverse=True
+                )
 
     return metric_tree
--- envoy/tests/test_utils.py
+++ envoy/tests/test_utils.py
@@ -3,15 +3,50 @@ from datadog_checks.envoy.utils import make_metric_tree
 
 def test_make_metric_tree():
     metrics = {
-        'http.admin.downstream_cx_total': '',
-        'http.admin.rs_too_large': '',
-    }
-
-    assert make_metric_tree(metrics) == {
-        'http': {
-            'admin': {
-                'downstream_cx_total': {},
-                'rs_too_large': {},
-            },
+        'http.dynamodb.error': {
+            'tags': (
+                ('stat_prefix', ),
+                (),
+                ('table_name', 'error_type', ),
+            ),
+            'method': 'count',
+        },
+        'http.dynamodb.error.BatchFailureUnprocessedKeys': {
+            'tags': (
+                ('stat_prefix', ),
+                (),
+                ('table_name', ),
+                (),
+            ),
+            'method': 'count',
         },
     }
+
+    exp = {
+        'http': {
+            'dynamodb': {
+                'error': {
+                    'BatchFailureUnprocessedKeys': {
+                        '|_tags_|': [
+                            (),
+                        ],
+                    },
+                    '|_tags_|': [
+                        ('table_name', 'error_type', ),
+                        ('table_name', ),
+                    ],
+                },
+                '|_tags_|': [
+                    (),
+                ],
+            },
+            '|_tags_|': [
+                ('stat_prefix', ),
+            ],
+        },
+    }
+
+    mt = make_metric_tree(metrics)
+    print(mt)
+    print(exp)
+    assert mt==exp
