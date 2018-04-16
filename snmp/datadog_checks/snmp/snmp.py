86d5f18ca7df372baeb2d936ce02526c3c9f2f36
Scott Dixon <dixon.scottr@gmail.com>
2018-04-16 13:03:29 -0700

    Fix whitespace

--- snmp/datadog_checks/snmp/snmp.py
+++ snmp/datadog_checks/snmp/snmp.py
@@ -301,12 +301,12 @@ class SnmpCheck(NetworkCheck):
                         message = "{0} for instance {1}".format(error_status.prettyPrint(),
                                                                 instance["ip_address"])
                         instance["service_check_error"] = message
-                        
+
                         # submit CRITICAL service check if we can't connect to device
                         if 'unknownUserName' in message:
                             instance["service_check_severity"] = Status.CRITICAL
                             self.log.error(message)
-                        else:    
+                        else:
                             self.warning(message)
 
                     for table_row in var_binds_table:
