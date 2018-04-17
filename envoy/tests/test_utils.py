fa714c772290cc00c7a3c232aaf53bf97af61a82
Ofek Lev <ofekmeister@gmail.com>
2018-04-17 05:05:24 -0400

    [envoy] improve metric parser (#1404)

--- envoy/tests/test_utils.py
+++ envoy/tests/test_utils.py
@@ -3,15 +3,45 @@ from datadog_checks.envoy.utils import make_metric_tree
 
 def test_make_metric_tree():
     metrics = {
-        'http.admin.downstream_cx_total': '',
-        'http.admin.rs_too_large': '',
+        'http.dynamodb.error': {
+            'tags': (
+                ('stat_prefix', ),
+                (),
+                ('table_name', 'error_type', ),
+            ),
+            'method': 'count',
+        },
+        'http.dynamodb.error.BatchFailureUnprocessedKeys': {
+            'tags': (
+                ('stat_prefix', ),
+                (),
+                ('table_name', ),
+                (),
+            ),
+            'method': 'count',
+        },
     }
 
     assert make_metric_tree(metrics) == {
         'http': {
-            'admin': {
-                'downstream_cx_total': {},
-                'rs_too_large': {},
+            'dynamodb': {
+                'error': {
+                    'BatchFailureUnprocessedKeys': {
+                        '|_tags_|': [
+                            (),
+                        ],
+                    },
+                    '|_tags_|': [
+                        ('table_name', 'error_type', ),
+                        ('table_name', ),
+                    ],
+                },
+                '|_tags_|': [
+                    (),
+                ],
             },
+            '|_tags_|': [
+                ('stat_prefix', ),
+            ],
         },
     }
