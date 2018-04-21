69c9ba4b3106b3dd335c8c716b4630a311970b96
Ofek Lev <ofekmeister@gmail.com>
2018-04-21 01:52:33 -0400

    add util

---
+++ datadog_checks_base/datadog_checks/utils/common.py
@@ -0,0 +1,10 @@
+# (C) Datadog, Inc. 2018
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+
+from six.moves.urllib.parse import urlparse
+
+
+def get_docker_hostname():
+    return urlparse(os.getenv('DOCKER_HOST')).hostname or 'localhost'
