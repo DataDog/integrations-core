6a3c5edbbcb2f8ddf262f9526e47664fc555fa40
Ofek Lev <ofekmeister@gmail.com>
2018-04-23 16:29:14 -0400

    fix network check porting

--- network/setup.py
+++ network/setup.py
@@ -22,15 +22,6 @@ def get_requirements(fpath):
     with open(path.join(HERE, fpath), encoding='utf-8') as f:
         return f.readlines()
 
-
-manifest_version = None
-with open(path.join(HERE, 'manifest.json'), encoding='utf-8') as f:
-    manifest = json.load(f)
-    manifest_version = manifest.get('version')
-
-if ABOUT["__version__"] != manifest_version:
-    raise Exception("Inconsistent versioning in module and manifest - aborting wheel build")
-
 setup(
     name='datadog-network',
     version=ABOUT["__version__"],
