cc2714e61ca67dfe360a139045cebc14e6f315db
Massimiliano Pippi <mpippi@gmail.com>
2018-04-24 18:56:34 +0200

    added integration tests for vts module

--- nginx/tests/test_nginx.py
+++ nginx/tests/test_nginx.py
@@ -12,7 +12,7 @@ import pytest
 import requests
 
 from datadog_checks.stubs import aggregator as _aggregator
-from datadog_checks.nginx import Nginx
+from datadog_checks.nginx import Nginx, VTS_METRIC_MAP
 
 
 HERE = os.path.dirname(os.path.abspath(__file__))
@@ -23,24 +23,41 @@ NGINX_PORT = '8080'
 NGINX_PORT_SSL = '8081'
 TAGS = ['foo', 'bar']
 
+DOCKER_COMPOSE_ARGS = [
+    "docker-compose",
+    "-f", os.path.join(HERE, 'docker-compose.yaml')
+]
+
 
 @pytest.fixture(scope="session")
 def nginx():
     env = os.environ
     env['NGINX_CONFIG_FOLDER'] = os.path.join(HERE, 'nginx')
 
-    args = [
-        "docker-compose",
-        "-f", os.path.join(HERE, 'docker-compose.yaml')
-    ]
-    subprocess.check_call(args + ["up", "-d"], env=env)
+    start_nginx(env)
+    yield
+    subprocess.check_call(DOCKER_COMPOSE_ARGS + ["down"], env=env)
+
+
+@pytest.fixture(scope="session")
+def nginx_vts():
+    env = os.environ
+    env['NGINX_CONFIG_FOLDER'] = os.path.join(HERE, 'nginx_vts')
+
+    start_nginx(env)
+    yield
+    subprocess.check_call(DOCKER_COMPOSE_ARGS + ["down"], env=env)
+
+
+def start_nginx(env):
+    subprocess.check_call(DOCKER_COMPOSE_ARGS + ["up", "-d"], env=env)
 
     sys.stderr.write("Waiting for NGINX to boot...")
 
     attempts = 1
     while True:
         if attempts >= 10:
-            subprocess.check_call(args + ["down"], env=env)
+            subprocess.check_call(DOCKER_COMPOSE_ARGS + ["down"], env=env)
             raise Exception("NGINX failed to boot...")
 
         try:
@@ -50,10 +67,6 @@ def nginx():
         except Exception:
             attempts += 1
             time.sleep(1)
-
-    yield
-
-    subprocess.check_call(args + ["down"], env=env)
 
 
 @pytest.fixture
@@ -75,6 +88,15 @@ def instance_ssl():
     return {
         'nginx_status_url': 'https://{}:{}/nginx_status'.format(NGINX_HOST, NGINX_PORT_SSL),
         'tags': TAGS,
+    }
+
+
+@pytest.fixture
+def instance_vts():
+    return {
+        'nginx_status_url': 'http://{}:{}/vts_status'.format(NGINX_HOST, NGINX_PORT),
+        'tags': TAGS,
+        'use_vts': True,
     }
 
 
@@ -125,7 +147,6 @@ def mocked_perform_request(*args, **kwargs):
     """
     response = mock.MagicMock()
     url = args[1]
-    print(url)
 
     if "/2/nginx" in url:
         file_contents = open(os.path.join(FIXTURES_PATH, 'plus_api_nginx.json')).read()
@@ -192,3 +213,38 @@ def test_nest_payload(check):
     }
 
     assert result == expected
+
+
+@pytest.mark.skipif('nginx-vts' not in os.environ.get('NGINX_IMAGE', ''), reason="Not using VTS")
+def test_vts(check, instance_vts, aggregator, nginx_vts):
+    check.check(instance_vts)
+
+    # skip metrics that are difficult to reproduce in a test environment
+    skip_metrics = [
+        'nginx.upstream.peers.responses.1xx',
+        'nginx.upstream.peers.responses.2xx',
+        'nginx.upstream.peers.responses.3xx',
+        'nginx.upstream.peers.responses.4xx',
+        'nginx.upstream.peers.responses.5xx',
+        'nginx.upstream.peers.requests',
+        'nginx.upstream.peers.received',
+        'nginx.server_zone.received',
+        'nginx.server_zone.responses.1xx',
+        'nginx.server_zone.responses.2xx',
+        'nginx.server_zone.responses.3xx',
+        'nginx.server_zone.responses.4xx',
+        'nginx.server_zone.responses.5xx',
+        'nginx.server_zone.requests',
+        'nginx.server_zone.sent',
+        'nginx.upstream.peers.sent',
+        'nginx.upstream.peers.health_checks.last_passed',
+        'nginx.upstream.peers.weight',
+        'nginx.upstream.peers.backup',
+    ]
+
+    print(aggregator.metric_names)
+
+    for vts, mapped in VTS_METRIC_MAP.items():
+        if mapped in skip_metrics:
+            continue
+        aggregator.assert_metric(mapped, tags=TAGS)
