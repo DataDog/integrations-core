d45e2e0cd5a4d29d2ffdd801ed0da822ca7aa368
Christine Chen <ChristineTChen@users.noreply.github.com>
2018-04-24 07:39:26 -0400

    [nginx] test migration (#1320)
    * Migrate test framework
    
    * Add import headers
    
    * add docker
    
    * add docker stuff
    
    * rebased on master + fixes
    
    * finishing porting tests
    
    * cosmetics

--- nginx/datadog_checks/nginx/__about__.py
+++ nginx/datadog_checks/nginx/__about__.py
@@ -1,10 +1,4 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from .__about__ import __version__
-from .nginx import Nginx
-
-__all__ = [
-    '__version__',
-    'Nginx'
-]
+__version__ = '2.0.0'
