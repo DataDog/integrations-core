d0b35bf4cb6db05069b7a91d27a9888c81b7adbd
Roman Sheydvasser <s.roman@gmail.com>
2018-04-27 15:12:00 -0400

    add groups.json fixture to test file

--- marathon/test/test_marathon.py
+++ marathon/test/test_marathon.py
@@ -68,6 +68,8 @@ class MarathonCheckTest(AgentCheckTest):
                 return Fixtures.read_json_file("deployments.json", sdk_dir=ci_dir)
             elif "v2/queue" in url:
                 return Fixtures.read_json_file("queue.json", sdk_dir=ci_dir)
+            elif "v2/groups" in url:
+                return Fixtures.read_json_file("groups.json", sdk_dir=ci_dir)
             else:
                 raise Exception("unknown url:" + url)
 
