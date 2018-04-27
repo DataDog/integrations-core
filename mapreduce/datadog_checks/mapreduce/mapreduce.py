10667219806e30e33a35f954c039e60c68fc4936
Greg Meyer <greg.meyer@datadoghq.com>
2018-04-27 14:51:01 -0400

    adds verify to mapreduce

--- mapreduce/datadog_checks/mapreduce/mapreduce.py
+++ mapreduce/datadog_checks/mapreduce/mapreduce.py
@@ -478,7 +478,7 @@ class MapReduceCheck(AgentCheck):
             url = urljoin(url, '?' + query)
 
         try:
-            response = requests.get(url, timeout=self.default_integration_http_timeout)
+            response = requests.get(url, timeout=self.default_integration_http_timeout, verify=ssl_verify)
             response.raise_for_status()
             response_json = response.json()
 
