75b654f61fd946537a19cc6029c56d7beff26fb5
Ofek Lev <ofekmeister@gmail.com>
2018-05-04 12:08:08 -0400

    finish function

--- datadog_checks_base/datadog_checks/utils/common.py
+++ datadog_checks_base/datadog_checks/utils/common.py
@@ -11,4 +11,22 @@ def get_docker_hostname():
     return urlparse(os.getenv('DOCKER_HOST', '')).hostname or 'localhost'
 
 
-def pattern_filter(items, patterns)
+def pattern_filter(items, patterns, exclude=False, key=None):
+    if not patterns:
+        return items
+
+    key = key or __return_self
+    matches = {
+        item for pattern in patterns
+        for item in items
+        if re.search(pattern, key(item))
+    }
+
+    if exclude:
+        return [item for item in items if item not in matches]
+    else:
+        return [item for item in items if item in matches]
+
+
+def __return_self(obj):
+    return obj
