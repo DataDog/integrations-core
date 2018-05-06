097751a46c4ae1e440b0b6e764c27cdeb127f74e
Ofek Lev <ofekmeister@gmail.com>
2018-05-06 16:54:13 -0400

    add filter chain

--- datadog_checks_base/datadog_checks/utils/common.py
+++ datadog_checks_base/datadog_checks/utils/common.py
@@ -32,5 +32,32 @@ def pattern_filter(items, patterns, exclude=False, key=None):
         return [item for item in items if key(item) in matches]
 
 
+def pattern_filter_chain(items, whitelist=None, blacklist=None, key=None):
+    """This filters `items` by a regular expression `whitelist` and/or
+    `blacklist`, with the `whitelist` taking precedence. An optional `key`
+    function can be provided that will be passed each item.
+    """
+    if not (whitelist or blacklist):
+        return items
+
+    key = key or __return_self
+    whitelist = whitelist or []
+    blacklist = blacklist or []
+    whitelisted = set()
+    blacklisted = set()
+
+    for item in items:
+        item_key = key(item)
+        whitelisted.update(item_key for pattern in whitelist if re.search(pattern, item_key))
+        blacklisted.update(item_key for pattern in blacklist if re.search(pattern, item_key))
+
+    if blacklist:
+        # Remove any whitelisted items from the blacklist.
+        blacklisted.difference_update(whitelisted)
+        return [item for item in items if key(item) not in blacklisted]
+    else:
+        return [item for item in items if key(item) in whitelisted]
+
+
 def __return_self(obj):
     return obj
--- datadog_checks_base/tests/test_utils.py
+++ datadog_checks_base/tests/test_utils.py
@@ -1,10 +1,24 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from datadog_checks.utils.common import pattern_filter
+from datadog_checks.utils.common import pattern_filter, pattern_filter_chain
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        return self.name == other.name
 
 
 class TestPatternFilter:
+    def test_no_items(self):
+        items = []
+        patterns = ['mock']
+
+        assert pattern_filter(items, patterns) == []
+
     def test_no_patterns(self):
         items = ['mock']
         patterns = []
@@ -24,16 +38,49 @@ class TestPatternFilter:
         assert pattern_filter(items, patterns, exclude=True) == ['ghi']
 
     def test_key_function(self):
-        class Item:
-            def __init__(self, name):
-                self.name = name
-
-            def __eq__(self, other):
-                return self.name == other.name
-
         items = [Item('abc'), Item('def'), Item('abcdef'), Item('ghi')]
         patterns = ['abc', 'def']
 
         assert pattern_filter(items, patterns, key=lambda item: item.name) == [
+            Item('abc'), Item('def'), Item('abcdef')
+        ]
+
+
+class TestPatternFilterChain:
+    def test_no_items(self):
+        items = []
+        whitelist = ['mock']
+
+        assert pattern_filter_chain(items, whitelist=whitelist) == []
+
+    def test_no_patterns(self):
+        items = ['mock']
+
+        assert pattern_filter_chain(items) is items
+
+    def test_multiple_matches_whitelist(self):
+        items = ['abc', 'def', 'abcdef', 'ghi']
+        whitelist = ['abc', 'def']
+
+        assert pattern_filter_chain(items, whitelist=whitelist) == ['abc', 'def', 'abcdef']
+
+    def test_multiple_matches_blacklist(self):
+        items = ['abc', 'def', 'abcdef', 'ghi']
+        blacklist = ['abc', 'def']
+
+        assert pattern_filter_chain(items, blacklist=blacklist) == ['ghi']
+
+    def test_whitelist_override(self):
+        items = ['abc', 'def', 'abcdef', 'ghi']
+        whitelist = ['def']
+        blacklist = ['abc', 'def']
+
+        assert pattern_filter_chain(items, whitelist=whitelist, blacklist=blacklist) == ['def', 'abcdef', 'ghi']
+
+    def test_key_function(self):
+        items = [Item('abc'), Item('def'), Item('abcdef'), Item('ghi')]
+        whitelist = ['abc', 'def']
+
+        assert pattern_filter_chain(items, whitelist=whitelist, key=lambda item: item.name) == [
             Item('abc'), Item('def'), Item('abcdef')
         ]
