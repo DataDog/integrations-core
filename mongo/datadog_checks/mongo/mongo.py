0c69a1a4a3f23876155091cf2183f6eec7e9529f
Ofek Lev <ofekmeister@gmail.com>
2018-05-07 16:54:56 -0400

    address review

--- mongo/datadog_checks/mongo/mongo.py
+++ mongo/datadog_checks/mongo/mongo.py
@@ -777,7 +777,7 @@ class MongoDb(AgentCheck):
         # Handle replica data, if any
         # See
         # http://www.mongodb.org/display/DOCS/Replica+Set+Commands#ReplicaSetCommands-replSetGetStatus  # noqa
-        if not instance.get('disable_replica_check'):
+        if _is_affirmative(instance.get('replica_check')):
             try:
                 data = {}
                 dbnames = []
