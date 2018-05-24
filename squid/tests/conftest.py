f021836d4cded05e235fde1f23654204eae73d4c
Christine <cchen7196@gmail.com>
2018-05-24 15:11:16 -0400

    Update with review feedback

--- squid/tests/conftest.py
+++ squid/tests/conftest.py
@@ -3,6 +3,7 @@
 # Licensed under Simplified BSD License (see LICENSE)
 import pytest
 import os
+import sys
 import subprocess
 import requests
 import time
@@ -26,16 +27,19 @@ def spin_up_squid():
         "docker-compose",
         "-f", os.path.join(common.HERE, 'compose', 'squid.yaml')
     ]
-
-    subprocess.check_call(args + ["down"], env=env)
     subprocess.check_call(args + ["up", "-d"], env=env)
-
-    for _ in xrange(30):
+    for _ in xrange(10):
         try:
             res = requests.get(common.URL)
+            print "!!!!!!!", res
             res.raise_for_status()
             break
         except Exception:
             time.sleep(1)
+            sys.stderr.write("Waiting for Squid to boot...")
+    else:
+        subprocess.check_call(args + ["down"], env=env)
+        raise Exception("Squid failed to boot...")
+
     yield
     subprocess.check_call(args + ["down"], env=env)
