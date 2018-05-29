04c15c528aa56dcd9776e3e11301e73005e8eea3
Ofek Lev <ofekmeister@gmail.com>
2018-05-28 20:48:15 -0400

    oops...

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -4,12 +4,12 @@ set -ex
 
 case $CHECK in
     couchbase|mcache)
-        sudo service postgresql stop
-        while sudo lsof -Pi :5432 -sTCP:LISTEN -t; do sleep 1; done
-        ;;
-    pgbouncer|postgres)
         sudo service memcached stop
         while sudo lsof -Pi :11211 -sTCP:LISTEN -t; do sleep 1; done
+        ;;
+    pgbouncer|postgres)
+        sudo service postgresql stop
+        while sudo lsof -Pi :5432 -sTCP:LISTEN -t; do sleep 1; done
         ;;
 esac
 
