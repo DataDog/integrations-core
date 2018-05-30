cec7dfb31560b1a59b8de19c87e0eb6f3b711b99
Ofek Lev <ofekmeister@gmail.com>
2018-05-29 22:42:37 -0400

    try

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -3,15 +3,14 @@
 set -ex
 
 # Stop any services that bind to ports we need.
-case $CHECK in
-    couchbase|mcache|"")
-        sudo service memcached stop
-        while sudo lsof -Pi :11211 -sTCP:LISTEN -t; do sleep 1; done
-        ;;
-    pgbouncer|postgres|"")
-        sudo service postgresql stop
-        while sudo lsof -Pi :5432 -sTCP:LISTEN -t; do sleep 1; done
-        ;;
-esac
+if [[ "$CHECK" =~ "^$|couchbase|mcache" ]]; then
+    sudo service memcached stop
+    while sudo lsof -Pi :11211 -sTCP:LISTEN -t; do sleep 1; done
+fi
+
+if [[ "$CHECK" =~ "^$|pgbouncer|postgres" ]]; then
+    sudo service postgresql stop
+    while sudo lsof -Pi :5432 -sTCP:LISTEN -t; do sleep 1; done
+fi
 
 set +ex
