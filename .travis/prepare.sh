f43d0bc3e394c5a79366aa0850369399a8cd97ca
Ofek Lev <ofekmeister@gmail.com>
2018-05-29 22:28:22 -0400

    .

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -8,7 +8,7 @@ case $CHECK in
         sudo service memcached stop
         while sudo lsof -Pi :11211 -sTCP:LISTEN -t; do sleep 1; done
         ;;
-    pgbouncer|postgres|)
+    pgbouncer|postgres|"")
         sudo service postgresql stop
         while sudo lsof -Pi :5432 -sTCP:LISTEN -t; do sleep 1; done
         ;;
