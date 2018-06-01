42333bc49a09cec1ad7da06c7ec9f2a0344545fd
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-05-31 21:23:22 -0400

    Remove unnecessary exports from init

--- openstack/datadog_checks/openstack/__init__.py
+++ openstack/datadog_checks/openstack/__init__.py
@@ -1,17 +1,10 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from .openstack import OpenStackCheck, OpenStackProjectScope, OpenStackUnscoped, KeystoneCatalog
-from .openstack import IncompleteConfig, IncompleteAuthScope, IncompleteIdentity
+from .openstack import OpenStackCheck
 from .__about__ import __version__
 
 __all__ = [
     '__version__',
-    'OpenStackCheck',
-    'OpenStackProjectScope',
-    'OpenStackUnscoped',
-    'KeystoneCatalog',
-    'IncompleteConfig',
-    'IncompleteAuthScope',
-    'IncompleteIdentity'
+    'OpenStackCheck'
 ]
