c0b2aa11598863f650f1049d187a3f04fe5aa7cc
Rishabh Moudgil <rishabh.moudgil@datadoghq.com>
2018-06-06 08:57:20 -0400

    Add support for HTTP authentication (#1677)

--- hdfs_namenode/tests/test_hdfs_namenode.py
+++ hdfs_namenode/tests/test_hdfs_namenode.py
@@ -5,8 +5,8 @@
 from datadog_checks.hdfs_namenode import HDFSNameNode
 
 from .common import (
-    HDFS_NAMENODE_CONFIG, HDFS_NAMESYSTEM_METRIC_TAGS, HDFS_NAMESYSTEM_METRICS_VALUES,
-    HDFS_NAMESYSTEM_STATE_METRICS_VALUES, HDFS_NAMESYSTEM_MUTUAL_METRICS_VALUES
+    HDFS_NAMENODE_CONFIG, HDFS_NAMENODE_AUTH_CONFIG, CUSTOM_TAGS, HDFS_NAMESYSTEM_METRIC_TAGS,
+    HDFS_NAMESYSTEM_METRICS_VALUES, HDFS_NAMESYSTEM_STATE_METRICS_VALUES, HDFS_NAMESYSTEM_MUTUAL_METRICS_VALUES
 )
 
 
@@ -16,13 +16,28 @@ def test_check(aggregator, mocked_request):
     # Run the check once
     hdfs_namenode.check(HDFS_NAMENODE_CONFIG['instances'][0])
 
+    aggregator.assert_service_check(
+        HDFSNameNode.JMX_SERVICE_CHECK, HDFSNameNode.OK, tags=HDFS_NAMESYSTEM_METRIC_TAGS + CUSTOM_TAGS, count=1
+    )
+
     for metric, value in HDFS_NAMESYSTEM_STATE_METRICS_VALUES.iteritems():
-        aggregator.assert_metric(metric, value=value, tags=HDFS_NAMESYSTEM_METRIC_TAGS, count=1)
+        aggregator.assert_metric(metric, value=value, tags=HDFS_NAMESYSTEM_METRIC_TAGS + CUSTOM_TAGS, count=1)
 
     for metric, value in HDFS_NAMESYSTEM_METRICS_VALUES.iteritems():
-        aggregator.assert_metric(metric, value=value, tags=HDFS_NAMESYSTEM_METRIC_TAGS, count=1)
+        aggregator.assert_metric(metric, value=value, tags=HDFS_NAMESYSTEM_METRIC_TAGS + CUSTOM_TAGS, count=1)
 
     for metric, value in HDFS_NAMESYSTEM_MUTUAL_METRICS_VALUES.iteritems():
-        aggregator.assert_metric(metric, value=value, tags=HDFS_NAMESYSTEM_METRIC_TAGS, count=2)
+        aggregator.assert_metric(metric, value=value, tags=HDFS_NAMESYSTEM_METRIC_TAGS + CUSTOM_TAGS, count=2)
 
     aggregator.assert_all_metrics_covered()
+
+
+def test_auth(aggregator, mocked_auth_request):
+    hdfs_namenode = HDFSNameNode('hdfs_namenode', {}, {})
+
+    # Run the check once
+    hdfs_namenode.check(HDFS_NAMENODE_AUTH_CONFIG['instances'][0])
+
+    aggregator.assert_service_check(
+        HDFSNameNode.JMX_SERVICE_CHECK, HDFSNameNode.OK, tags=HDFS_NAMESYSTEM_METRIC_TAGS + CUSTOM_TAGS, count=1
+    )
