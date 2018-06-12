626b7f6c2a2b099bba22795130fabf3a88dd3690
Rishabh Moudgil <rishabh.moudgil@datadoghq.com>
2018-06-12 17:13:32 -0400

    Add Couchbase health and cluster membership service checks and alerts (#1593)
    * Add health and cluster membership status checks
    * Add rebalancing events, we need to keep track of previous states
    * Add service checks metadata
    * Add documentation about service checks and events
    * Add to Couchbase tests

--- couchbase/datadog_checks/couchbase/couchbase.py
+++ couchbase/datadog_checks/couchbase/couchbase.py
@@ -4,7 +4,9 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 # stdlib
+import time
 import re
+from collections import defaultdict
 
 # 3rd party
 import requests
@@ -12,6 +14,7 @@ import requests
 # project
 from datadog_checks.checks import AgentCheck
 from datadog_checks.utils.headers import headers
+from datadog_checks.utils.containers import hash_mutable
 
 # Constants
 COUCHBASE_STATS_PATH = '/pools/default'
@@ -20,10 +23,32 @@ DEFAULT_TIMEOUT = 10
 
 
 class Couchbase(AgentCheck):
-    """Extracts stats from Couchbase via its REST API
+    """
+    Extracts stats from Couchbase via its REST API
     http://docs.couchbase.com/couchbase-manual-2.0/#using-the-rest-api
     """
+
+    # Service Checks
     SERVICE_CHECK_NAME = 'couchbase.can_connect'
+    NODE_CLUSTER_SERVICE_CHECK_NAME = 'couchbase.by_node.cluster_membership'
+    NODE_HEALTH_SERVICE_CHECK_NAME = 'couchbase.by_node.health'
+
+    NODE_MEMBERSHIP_TRANSLATION = {
+        'active': AgentCheck.OK,
+        'inactiveAdded': AgentCheck.WARNING,
+        'activeFailed': AgentCheck.CRITICAL,
+        None: AgentCheck.UNKNOWN,
+    }
+
+    NODE_HEALTH_TRANSLATION = {
+        'healthy': AgentCheck.OK,
+        'warmup': AgentCheck.OK,
+        'unhealthy': AgentCheck.CRITICAL,
+        None: AgentCheck.UNKNOWN
+    }
+
+    # Events
+    SOURCE_TYPE_NAME = 'couchbase'
 
     # Selected metrics to send amongst all the bucket stats, after name normalization
     BUCKET_STATS = set([
@@ -232,32 +257,49 @@ class Couchbase(AgentCheck):
 
     seconds_value_pattern = re.compile('(\d+(\.\d+)?)(\D+)')
 
-    def _create_metrics(self, data, tags=None):
+    class CouchbaseInstanceState(object):
+        def __init__(self):
+            self.previous_status = None
+
+    def __init__(self, name, init_config, agentConfig, instances=None):
+        AgentCheck.__init__(self, name, init_config, agentConfig, instances)
+
+        # Keep track of all instances
+        self._instance_states = defaultdict(lambda: self.CouchbaseInstanceState())
+
+    def _create_metrics(self, data, instance_state, server, tags=None):
+        # Get storage metrics
         storage_totals = data['stats']['storageTotals']
         for key, storage_type in storage_totals.items():
             for metric_name, val in storage_type.items():
                 if val is not None:
-                    metric_name = '.'.join(['couchbase', key, self.camel_case_to_joined_lower(metric_name)])
+                    metric_name = 'couchbase.{}.{}'.format(key, self.camel_case_to_joined_lower(metric_name))
                     self.gauge(metric_name, val, tags=tags)
 
+        # Get bucket metrics
         for bucket_name, bucket_stats in data['buckets'].items():
+            metric_tags = list(tags)
+            metric_tags.append('bucket:{}'.format(bucket_name))
             for metric_name, val in bucket_stats.items():
                 if val is not None:
                     norm_metric_name = self.camel_case_to_joined_lower(metric_name)
                     if norm_metric_name in self.BUCKET_STATS:
-                        full_metric_name = '.'.join(['couchbase', 'by_bucket', norm_metric_name])
-                        metric_tags = list(tags)
-                        metric_tags.append('bucket:%s' % bucket_name)
+                        full_metric_name = 'couchbase.by_bucket.{}'.format(norm_metric_name)
                         self.gauge(full_metric_name, val[0], tags=metric_tags, device_name=bucket_name)
 
+        # Get node metrics
         for node_name, node_stats in data['nodes'].items():
+            metric_tags = list(tags)
+            metric_tags.append('node:{}'.format(node_name))
             for metric_name, val in node_stats['interestingStats'].items():
                 if val is not None:
-                    metric_name = '.'.join(['couchbase', 'by_node', self.camel_case_to_joined_lower(metric_name)])
-                    metric_tags = list(tags)
-                    metric_tags.append('node:%s' % node_name)
+                    metric_name = 'couchbase.by_node.{}'.format(self.camel_case_to_joined_lower(metric_name))
                     self.gauge(metric_name, val, tags=metric_tags, device_name=node_name)
 
+            # Get cluster health data
+            self._process_cluster_health_data(node_name, node_stats, tags)
+
+        # Get query metrics
         for metric_name, val in data['query'].items():
             if val is not None:
                 norm_metric_name = self.camel_case_to_joined_lower(metric_name)
@@ -266,13 +308,89 @@ class Couchbase(AgentCheck):
                     if isinstance(val, basestring):
                         val = self.extract_seconds_value(val)
 
-                    full_metric_name = '.'.join(['couchbase', 'query',
-                                                self.camel_case_to_joined_lower(norm_metric_name)])
+                    full_metric_name = 'couchbase.query.{}'.format(self.camel_case_to_joined_lower(norm_metric_name))
                     self.gauge(full_metric_name, val, tags=tags)
 
+        # Get tasks, we currently only care about 'rebalance' tasks
+        rebalance_status, rebalance_msg = data['tasks'].get('rebalance', (None, None))
+
+        # Only fire an event when the state has changed
+        if rebalance_status is not None and instance_state.previous_status != rebalance_status:
+            rebalance_event = None
+
+            # If we get an error, we create an error event with the msg we receive
+            if rebalance_status == 'error':
+                msg_title = 'Encountered an error while rebalancing'
+                msg = rebalance_msg
+
+                rebalance_event = self._create_event('error', msg_title, msg, server, tags=tags)
+
+            # We only want to fire a 'completion' of a rebalance so make sure we're not firing an event on first run
+            elif rebalance_status == 'notRunning' and instance_state.previous_status is not None:
+                msg_title = 'Stopped rebalancing'
+                msg = 'stopped rebalancing.'
+                rebalance_event = self._create_event('info', msg_title, msg, server, tags=tags)
+
+            # If a rebalance task is running, fire an event. This will also fire an event if a rebalance task was
+            #   already running when the check first runs.
+            elif rebalance_status == 'gracefulFailover':
+                msg_title = 'Failing over gracefully'
+                msg = 'is failing over gracefully.'
+                rebalance_event = self._create_event('info', msg_title, msg, server, tags=tags)
+            elif rebalance_status == 'rebalance':
+                msg_title = 'Rebalancing'
+                msg = 'is rebalancing.'
+                rebalance_event = self._create_event('info', msg_title, msg, server, tags=tags)
+
+            # Send the event
+            if rebalance_event is not None:
+                self.event(rebalance_event)
+
+            # Update the status of this instance
+            instance_state.previous_status = rebalance_status
+
+    def _process_cluster_health_data(self, node_name, node_stats, tags):
+        """
+        Process and send cluster health data (i.e. cluster membership status and node health
+        """
+
+        # Tags for service check
+        cluster_health_tags = list(tags) + ['node:{}'.format(node_name)]
+
+        # Get the membership status of the node
+        cluster_membership = node_stats.get('clusterMembership', None)
+        membership_status = self.NODE_MEMBERSHIP_TRANSLATION.get(cluster_membership, AgentCheck.UNKNOWN)
+        self.service_check(self.NODE_CLUSTER_SERVICE_CHECK_NAME, membership_status, tags=cluster_health_tags)
+
+        # Get the health status of the node
+        health = node_stats.get('status', None)
+        health_status = self.NODE_HEALTH_TRANSLATION.get(health, AgentCheck.UNKNOWN)
+        self.service_check(self.NODE_HEALTH_SERVICE_CHECK_NAME, health_status, tags=cluster_health_tags)
+
+    def _create_event(self, alert_type, msg_title, msg, server, tags=None):
+        """
+        Create an event object
+        """
+        msg_title = 'Couchbase {}: {}'.format(server, msg_title)
+        msg = 'Couchbase instance {} {}'.format(server, msg)
+
+        return {
+            'timestamp': int(time.time()),
+            'event_type': 'couchbase_rebalance',
+            'msg_text': msg,
+            'msg_title': msg_title,
+            'alert_type': alert_type,
+            'source_type_name': self.SOURCE_TYPE_NAME,
+            'aggregation_key': server,
+            'tags': tags
+        }
+
     def _get_stats(self, url, instance):
-        """ Hit a given URL and return the parsed json. """
-        self.log.debug('Fetching Couchbase stats at url: %s' % url)
+        """
+        Hit a given URL and return the parsed json.
+        """
+
+        self.log.debug('Fetching Couchbase stats at url: {}'.format(url))
 
         timeout = float(instance.get('timeout', DEFAULT_TIMEOUT))
 
@@ -285,6 +403,8 @@ class Couchbase(AgentCheck):
         return r.json()
 
     def check(self, instance):
+        instance_state = self._instance_states[hash_mutable(instance)]
+
         server = instance.get('server', None)
         if server is None:
             raise Exception("The server must be specified")
@@ -295,9 +415,9 @@ class Couchbase(AgentCheck):
             tags = []
         else:
             tags = list(set(tags))
-        tags.append('instance:%s' % server)
+        tags.append('instance:{}'.format(server))
         data = self.get_data(server, instance)
-        self._create_metrics(data, tags=list(set(tags)))
+        self._create_metrics(data, instance_state, server, tags=list(set(tags)))
 
     def get_data(self, server, instance):
         # The dictionary to be returned.
@@ -306,10 +426,11 @@ class Couchbase(AgentCheck):
             'buckets': {},
             'nodes': {},
             'query': {},
+            'tasks': {}
         }
 
         # build couchbase stats entry point
-        url = '%s%s' % (server, COUCHBASE_STATS_PATH)
+        url = '{}{}'.format(server, COUCHBASE_STATS_PATH)
 
         # Fetch initial stats and capture a service check based on response.
         service_check_tags = instance.get('tags', [])
@@ -317,12 +438,12 @@ class Couchbase(AgentCheck):
             service_check_tags = []
         else:
             service_check_tags = list(set(service_check_tags))
-        service_check_tags.append('instance:%s' % server)
+        service_check_tags.append('instance:{}'.format(server))
         try:
             overall_stats = self._get_stats(url, instance)
             # No overall stats? bail out now
             if overall_stats is None:
-                raise Exception("No data returned from couchbase endpoint: %s" % url)
+                raise Exception("No data returned from couchbase endpoint: {}".format(url))
         except requests.exceptions.HTTPError as e:
             self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.CRITICAL,
                                tags=service_check_tags, message=str(e.message))
@@ -345,7 +466,7 @@ class Couchbase(AgentCheck):
         # Next, get all buckets .
         endpoint = overall_stats['buckets']['uri']
 
-        url = '%s%s' % (server, endpoint)
+        url = '{}{}'.format(server, endpoint)
         buckets = self._get_stats(url, instance)
 
         if buckets is not None:
@@ -354,12 +475,12 @@ class Couchbase(AgentCheck):
 
                 # Fetch URI for the stats bucket
                 endpoint = bucket['stats']['uri']
-                url = '%s%s' % (server, endpoint)
+                url = '{}{}'.format(server, endpoint)
 
                 try:
                     bucket_stats = self._get_stats(url, instance)
                 except requests.exceptions.HTTPError:
-                    url_backup = '%s/pools/nodes/buckets/%s/stats' % (server, bucket_name)
+                    url_backup = '{}/pools/nodes/buckets/{}/stats'.format(server, bucket_name)
                     bucket_stats = self._get_stats(url_backup, instance)
 
                 bucket_samples = bucket_stats['op']['samples']
@@ -370,13 +491,41 @@ class Couchbase(AgentCheck):
         query_monitoring_url = instance.get('query_monitoring_url', None)
         if query_monitoring_url is not None:
             try:
-                url = '%s%s' % (query_monitoring_url, COUCHBASE_VITALS_PATH)
+                url = '{}{}'.format(query_monitoring_url, COUCHBASE_VITALS_PATH)
                 query = self._get_stats(url, instance)
                 if query is not None:
                     couchbase['query'] = query
             except requests.exceptions.HTTPError:
-                self.log.error("Error accessing the endpoint %s, make sure you're running at least "
-                               "couchbase 4.5 to collect the query monitoring metrics", url)
+                self.log.error("Error accessing the endpoint {}, make sure you're running at least "
+                               "couchbase 4.5 to collect the query monitoring metrics".format(url))
+
+        # Next, get all the tasks
+        tasks_url = '{}{}/tasks'.format(server, COUCHBASE_STATS_PATH)
+        try:
+            tasks = self._get_stats(tasks_url, instance)
+            for task in tasks:
+                task_type = task['type']
+
+                # We only care about rebalance statuses
+                if task_type != 'rebalance':
+                    continue
+
+                # Format the status so it's easier to understand
+                if 'errorMessage' in task:
+                    couchbase['tasks'][task_type] = ('error', task['errorMessage'])
+                elif task['status'] == 'notRunning':
+                    couchbase['tasks'][task_type] = (task['status'], None)
+
+                # If the status is 'running', we want to differentiate between a regular rebalance and a graceful
+                #   failover rebalance, so we use the subtype
+                elif task['status'] == 'running':
+                    couchbase['tasks'][task_type] = (task['subtype'], None)
+
+                # Should only be 1 rebalance
+                break
+
+        except requests.exceptions.HTTPError:
+            self.log.error("Error accessing the endpoint {}".format(url))
 
         return couchbase
 
