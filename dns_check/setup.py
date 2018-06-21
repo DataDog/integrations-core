294b018e597490fa2daac91a81ee0835dfcfcc15
Gregory Zussa <gzussa@users.noreply.github.com>
2018-06-21 10:33:53 -0400

    [dns_check] Port over dns_check tests to pytest (#1757)

--- dns_check/setup.py
+++ dns_check/setup.py
@@ -1,23 +1,23 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+
 from setuptools import setup
-from codecs import open  # To use a consistent encoding
+from codecs import open
 from os import path
 
 HERE = path.dirname(path.abspath(__file__))
-
-# Get version info
-ABOUT = {}
-with open(path.join(HERE, 'datadog_checks', 'dns_check', '__about__.py')) as f:
-    exec(f.read(), ABOUT)
 
 # Get the long description from the README file
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# Get version info
+ABOUT = {}
+with open(path.join(HERE, "datadog_checks", "dns_check", "__about__.py")) as f:
+    exec(f.read(), ABOUT)
 
-# Parse requirements
+
 def get_requirements(fpath):
     with open(path.join(HERE, fpath), encoding='utf-8') as f:
         return f.readlines()
@@ -55,14 +55,12 @@ setup(
     packages=['datadog_checks.dns_check'],
 
     # Run-time dependencies
-    install_requires=get_requirements('requirements.in')+[
+    install_requires=get_requirements('requirements.in') + [
         'datadog_checks_base',
     ],
-
-    # Testing setup and dependencies
     tests_require=get_requirements('requirements-dev.txt'),
 
     # Extra files to ship with the wheel package
-    package_data={'datadog_checks.dns_check': ['conf.yaml.example']},
+    package_data={b'datadog_checks.dns_check': ['conf.yaml.example']},
     include_package_data=True,
 )
