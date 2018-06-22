fb7901491d2bdc10fda8bda94ed81d45463d6f3a
Ofek Lev <ofekmeister@gmail.com>
2018-06-21 22:51:28 -0400

    refactor

--- tasks/dep.py
+++ tasks/dep.py
@@ -8,11 +8,14 @@ import re
 from collections import defaultdict
 from io import open
 
+from colorama import Fore, Style, init
 from invoke import task
 from invoke.exceptions import Exit
 from six import iteritems
 
 from .constants import ROOT
+
+init(autoreset=True)
 
 
 DEP_PATTERN = re.compile(r'^([^=#]+)(?:==(\S+))?')
@@ -34,8 +37,7 @@ def ensure_deps_declared(reqs_txt, reqs_in):
             f.writelines(declacred_lines)
 
 
-@task
-def dep_check(ctx):
+def collect_packages(check=False):
     packages = defaultdict(lambda: defaultdict(list))
 
     for check_name in sorted(os.listdir(ROOT)):
@@ -46,30 +48,61 @@ def dep_check(ctx):
                     match = DEP_PATTERN.match(line.strip())
                     if match:
                         package, version = match.groups()
-                        packages[package.lower()][version].append(check_name)
+
+                        if check and version is None:
+                            raise Exit(
+                                '{}{}Unpinned dependency `{}` in the `{}` check.'.format(
+                                    Fore.RED, Style.BRIGHT, package, check_name
+                                )
+                            )
+
+                        versions = packages[package.lower()]
+                        versions[version].append(check_name)
+
+                        if check and len(versions) > 1:
+                            raise Exit(
+                                '{}{}Multiple dependency versions for `{}` in the {} and {} checks.'.format(
+                                    Fore.RED, Style.BRIGHT, package, versions.popitem()[1], versions.popitem()[1]
+                                )
+                            )
+
+    return packages
+
+
+@task
+def dep_check(ctx):
+    packages = collect_packages()
 
     output = ''
     for package, versions in sorted(iteritems(packages)):
         if len(versions) > 1:
-            output += 'Multiple versions found for package `{}`:\n'.format(package)
+            output += '{}{}Multiple versions found for package `{}`:\n'.format(Fore.RED, Style.BRIGHT, package)
             for version, checks in sorted(iteritems(versions)):
                 if len(checks) == 1:
-                    output += '    {}: {}\n'.format(version, checks[0])
+                    output += '{}    {}: {}\n'.format(Style.BRIGHT, version, checks[0])
                 elif len(checks) == 2:
-                    output += '    {}: {} and {}\n'.format(version, checks[0], checks[1])
+                    output += '{}    {}: {} and {}\n'.format(Style.BRIGHT, version, checks[0], checks[1])
                 else:
                     remaining = len(checks) - 2
-                    output += '    {}: {}, {}, and {} other{}\n'.format(
+                    output += '{}    {}: {}, {}, and {} other{}\n'.format(
+                        Style.BRIGHT,
                         version,
                         checks[0],
                         checks[1],
                         remaining,
                         's' if remaining > 1 else ''
                     )
+        else:
+            version, checks = versions.items()[0]
+            if version is None:
+                output += (
+                    '{}{}Unpinned dependency `{}` in the `{}` check.\n'
+                    ''.format(Fore.RED, Style.BRIGHT, package, checks[0])
+                )
 
     if output:
         print(output[:-1])
-        Exit(1)
+        raise Exit(1)
 
 
 @task(help={
