b1a1da283a239f0b7e117080b6a026728608ac3c
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-06-26 10:34:57 -0400

    Fixes cherry-pick merge issues

--- kubernetes_state/datadog_checks/kubernetes_state/__about__.py
+++ kubernetes_state/datadog_checks/kubernetes_state/__about__.py
@@ -2,8 +2,4 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-<<<<<<< HEAD
-__version__ = '2.5.0'
-=======
 __version__ = '2.7.0'
->>>>>>> 6d7e62d3... [ci skip] Bumped kubernetes_state version to 2.7.0 (#1803)
