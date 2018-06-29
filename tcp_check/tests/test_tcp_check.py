5ebf2649ddb1a36db53557fe153be2653f0d0e2e
Massimiliano Pippi <mpippi@gmail.com>
2018-06-29 16:18:58 +0200

    fixed tests

--- tcp_check/tests/test_tcp_check.py
+++ tcp_check/tests/test_tcp_check.py
@@ -45,27 +45,64 @@ def aggregator():
     return aggregator
 
 
-def test_check(aggregator):
+@pytest.fixture
+def check():
+    return TCPCheck(CHECK_NAME, {}, {})
+
+
+@pytest.fixture
+def instance_ko():
+    return {
+        'host': '127.0.0.1',
+        'port': 65530,
+        'timeout': 1.5,
+        'name': 'DownService',
+        'tags': ["foo:bar"],
+    }
+
+
+@pytest.fixture
+def instance():
+    return {
+        'host': 'datadoghq.com',
+        'port': 80,
+        'timeout': 1.5,
+        'name': 'UpService',
+        'tags': ["foo:bar"]
+    }
+
+
+def test_down(aggregator, check, instance_ko):
     """
-    Check coverage.
+    Service expected to be down
     """
-    check = TCPCheck(CHECK_NAME, {}, {})
-    # Run the check
-    for instance in CONFIG["instances"]:
-        check.check(instance)
-    expected_tags = ["instance:DownService", "target_host:127.0.0.1", "port:65530"]
+    check.check(instance_ko)
+    expected_tags = ["instance:DownService", "target_host:127.0.0.1", "port:65530", "foo:bar"]
     aggregator.assert_service_check('tcp.can_connect', status=check.CRITICAL, tags=expected_tags)
 
-    expected_tags = ["instance:DownService2", "target_host:126.0.0.1", "port:65530", "test1"]
+
+def test_up(aggregator, check, instance):
+    """
+    Service expected to be up
+    """
+    check.check(instance)
+    expected_tags = ["instance:UpService", "target_host:datadoghq.com", "port:80", "foo:bar"]
     aggregator.assert_service_check('tcp.can_connect', status=check.OK, tags=expected_tags)
 
-    expected_tags = ["instance:UpService", "target_host:datadoghq.com", "port:80", "test2"]
+
+def test_response_time(aggregator, check, instance):
+    """
+    Test the response time from a server expected to be up
+    """
+    instance['collect_response_time'] = True
+    instance['name'] = 'instance:response_time'
+    check.check(instance)
+
+    # service check
+    expected_tags = ['foo:bar', 'target_host:datadoghq.com', 'port:80', 'instance:instance:response_time']
     aggregator.assert_service_check('tcp.can_connect', status=check.OK, tags=expected_tags)
 
-    expected_tags = ["instance:response_time", "target_host:datadoghq.com", "port:80", "test3"]
-    aggregator.assert_service_check('tcp.can_connect', status=check.OK, tags=expected_tags)
-
-    expected_tags = ["instance:response_time", "url:datadoghq.com:80", "test3"]
-    aggregator.assert_metric('network.tcp.response_time', status=check.OK, tags=expected_tags)
-
+    # response time metric
+    expected_tags = ['url:datadoghq.com:80', 'instance:instance:response_time', 'foo:bar']
+    aggregator.assert_metric('network.tcp.response_time', tags=expected_tags)
     aggregator.assert_all_metrics_covered()
