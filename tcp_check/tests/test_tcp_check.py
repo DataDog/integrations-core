e0d87ba752be60d65d72106196d20aa8e751e7c7
Massimiliano Pippi <mpippi@gmail.com>
2018-06-30 11:38:27 +0200

    cleanup

--- tcp_check/tests/test_tcp_check.py
+++ tcp_check/tests/test_tcp_check.py
@@ -4,37 +4,6 @@
 from datadog_checks.tcp_check import TCPCheck
 import pytest
 
-RESULTS_TIMEOUT = 20
-
-CONFIG = {
-    'init_config': {},
-    'instances': [{
-        'host': '127.0.0.1',
-        'port': 65530,
-        'timeout': 1.5,
-        'name': 'DownService'
-    }, {
-        'host': '126.0.0.1',
-        'port': 65530,
-        'timeout': 1.5,
-        'name': 'DownService2',
-        'tags': ['test1']
-    }, {
-        'host': 'datadoghq.com',
-        'port': 80,
-        'timeout': 1.5,
-        'name': 'UpService',
-        'tags': ['test2']
-    }, {
-        'host': 'datadoghq.com',
-        'port': 80,
-        'timeout': 1,
-        'name': 'response_time',
-        'tags': ['test3'],
-        'collect_response_time': True
-    }]
-}
-
 CHECK_NAME = 'tcp_check'
 
 
