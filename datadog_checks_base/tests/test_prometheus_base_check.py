7ba5302dc33a0b488b11677ac1a645a82b454252
Antoine Pouille <antoine.pouille@datadoghq.com>
2018-07-02 15:53:09 +0200

    Adding some other tests for negative values + GenericPrometheusCheck

--- datadog_checks_base/tests/test_prometheus_base_check.py
+++ datadog_checks_base/tests/test_prometheus_base_check.py
@@ -14,3 +14,22 @@ def test_rate_override():
     processed_type_overrides = check.scrapers_map[endpoint].type_overrides
     assert cmp(expected_type_overrides, processed_type_overrides) == 0
     assert ["test_rate"] == check.scrapers_map[endpoint].rate_metrics
+
+
+def test_timeout_override():
+    endpoint = "none"
+    default_instance = {
+        'default_namespace': {
+            'prometheus_url': endpoint,
+            'metrics': [{"test_rate": "test.rate"}],
+            'prometheus_timeout': -1,
+        }
+    }
+
+    instance = {'prometheus_url': endpoint, 'namespace': 'default_namespace'}
+    check = GenericPrometheusCheck('prometheus_check', {}, {}, [instance], default_instance, default_namespace="foo")
+    assert check.get_scraper(instance).prometheus_timeout == -1
+
+    instance = {'prometheus_url': endpoint, 'namespace': 'default_namespace', 'prometheus_timeout': 5}
+    check = GenericPrometheusCheck('prometheus_check', {}, {}, [instance], default_instance, default_namespace="foo")
+    assert check.get_scraper(instance).prometheus_timeout == 5
