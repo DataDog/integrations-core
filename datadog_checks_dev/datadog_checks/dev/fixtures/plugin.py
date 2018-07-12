25198626992cdbe92769e537dc929d1b14b94fb7
Ofek Lev <ofekmeister@gmail.com>
2018-07-11 23:18:49 -0400

    raise error

--- datadog_checks_dev/datadog_checks/dev/fixtures/plugin.py
+++ datadog_checks_dev/datadog_checks/dev/fixtures/plugin.py
@@ -14,4 +14,4 @@ try:
 except ImportError:
     @pytest.fixture
     def aggregator():
-        return 'datadog_checks_base is not installed!'
+        raise ImportError('datadog_checks_base is not installed!')
