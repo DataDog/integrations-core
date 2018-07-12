7203571c0a25f92165cda4181c162e55465b1367
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-07-12 19:14:40 -0400

    Clean up variables

--- datadog_checks_base/tests/test_proxy.py
+++ datadog_checks_base/tests/test_proxy.py
@@ -6,8 +6,8 @@ import mock
 from datadog_checks.checks import AgentCheck
 
 PROXY_SETTINGS = {
-    "http": 'nffjn',
-    "https": 'Noniergui3rngne',
+    "http": 'http(s)://user:password@proxy_for_http:port',
+    "https": 'http(s)://user:password@proxy_for_https:port',
     "no": None
 }
 
@@ -18,16 +18,16 @@ NO_PROXY_SETTINGS = {
 def test_use_proxy():
     with mock.patch('datadog_checks.checks.AgentCheck._get_requests_proxy', return_value=PROXY_SETTINGS):
         check = AgentCheck()
-        assert check.get_instance_proxy({}, 'endpoint1') == PROXY_SETTINGS
+        assert check.get_instance_proxy({}, 'uri1/health') == PROXY_SETTINGS
 
 
 def test_skip_proxy():
     with mock.patch('datadog_checks.checks.AgentCheck._get_requests_proxy', return_value=PROXY_SETTINGS):
         check = AgentCheck()
-        assert check.get_instance_proxy({'skip_proxy': True}, 'endpoint2') == NO_PROXY_SETTINGS
+        assert check.get_instance_proxy({'skip_proxy': True}, 'uri/health') == NO_PROXY_SETTINGS
 
 
 def test_deprecated_no_proxy():
     with mock.patch('datadog_checks.checks.AgentCheck._get_requests_proxy', return_value=PROXY_SETTINGS):
         check = AgentCheck()
-        assert check.get_instance_proxy({'no_proxy': True}, 'endpoint2') == NO_PROXY_SETTINGS
+        assert check.get_instance_proxy({'no_proxy': True}, 'uri/health') == NO_PROXY_SETTINGS
