f1f31001295a61efd2973047d265d66f37fa9677
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-07-12 18:33:21 -0400

    Add test for get_instance_proxy_functionality

---
+++ datadog_checks_base/tests/test_proxy.py
@@ -0,0 +1,26 @@
+# (C) Datadog, Inc. 2018
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import mock
+
+from datadog_checks.checks import AgentCheck
+
+PROXY_SETTINGS = {
+    "http": 'nffjn',
+    "https": 'Noniergui3rngne',
+    "no": None
+}
+
+NO_PROXY_SETTINGS = {
+    "no": None
+}
+
+def test_use_proxy():
+    with mock.patch('datadog_checks.checks.AgentCheck._get_requests_proxy', return_value=PROXY_SETTINGS):
+        check = AgentCheck()
+        assert check.get_instance_proxy({}, 'endpoint1') == PROXY_SETTINGS
+
+def test_skip_proxy():
+    with mock.patch('datadog_checks.checks.AgentCheck._get_requests_proxy', return_value=PROXY_SETTINGS):
+        check = AgentCheck()
+        assert check.get_instance_proxy({'skip_proxy': True}, 'endpoint2') == NO_PROXY_SETTINGS
