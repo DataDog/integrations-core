792d263603a71858561fab26423493192056d421
Greg Zussa <gregory.zussa@datadoghq.com>
2018-07-16 18:14:25 +0200

    fine you win

--- haproxy/tests/conftest.py
+++ haproxy/tests/conftest.py
@@ -78,21 +78,18 @@ def haproxy_container():
         subprocess.check_call(args + ["down"], env=env)
         subprocess.check_call(args + ["up", "-d"], env=env)
         wait_for_haproxy()
-        # os.chown(host_socket_path, os.getuid(), os.getgid())
-        os.chmod(host_socket_path, 0777)
-        # try:
-        #     if Platform.is_linux():
-        #         # on linux this needs access to the socket
-        #         # it won't work without access
-        #         chown_args = []
-        #         user = getpass.getuser()
-        #         if user != 'root':
-        #             chown_args += ['sudo']
-        #         chown_args += ["chown", user, host_socket_path]
-        #         subprocess.check_call(chown_args, env=env)
-        # except subprocess.CalledProcessError:
-        #     # it's not always bad if this fails
-        #     pass
+        try:
+            # on linux this needs access to the socket
+            # it won't work without access
+            chown_args = []
+            user = getpass.getuser()
+            if user != 'root':
+                chown_args += ['sudo']
+            chown_args += ["chown", user, host_socket_path]
+            subprocess.check_call(chown_args, env=env)
+        except subprocess.CalledProcessError:
+            # it's not always bad if this fails
+            pass
         time.sleep(20)
         yield host_socket_path
         subprocess.check_call(args + ["down"], env=env)
