ba1079b7da9f6c227e7b444ad8742e7960d8bb29
Ofek Lev <ofek.lev@datadoghq.com>
2018-07-17 15:29:23 -0400

    refactor temp dir creation

--- openldap/tests/conftest.py
+++ openldap/tests/conftest.py
@@ -1,13 +1,13 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-
 import os
-import pytest
-import tempfile
 
+import pytest
+
+from datadog_checks.dev import docker_run, temp_dir
+from datadog_checks.dev.utils import create_file
 from datadog_checks.openldap import OpenLDAP
-from datadog_checks.dev import docker_run
 from .common import HERE
 
 
@@ -18,14 +18,14 @@ def check():
 
 @pytest.fixture(scope="session")
 def openldap_server():
-
-    host_socket_dir = os.path.realpath(tempfile.mkdtemp())
-    host_socket_path = os.path.join(host_socket_dir, "ldapi")
-
-    with docker_run(
-        compose_file=os.path.join(HERE, "compose", "docker-compose.yaml"),
-        env_vars={"HOST_SOCKET_DIR": host_socket_dir},
-        log_patterns="slapd starting",
-    ):
+    with temp_dir() as d:
+        host_socket_path = os.path.join(d, "ldapi")
+        create_file(host_socket_path)
         os.chmod(host_socket_path, 0777)
-        yield host_socket_path
+
+        with docker_run(
+            compose_file=os.path.join(HERE, "compose", "docker-compose.yaml"),
+            env_vars={"HOST_SOCKET_DIR": d},
+            log_patterns="slapd starting",
+        ):
+            yield host_socket_path
