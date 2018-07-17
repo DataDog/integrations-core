675d703cb148a97d41ec482ae81f525f31306f18
Ofek Lev <ofekmeister@gmail.com>
2018-07-17 13:44:00 -0400

    docs

--- datadog_checks_dev/datadog_checks/dev/subprocess.py
+++ datadog_checks_dev/datadog_checks/dev/subprocess.py
@@ -17,6 +17,28 @@ SubprocessResult = namedtuple('SubprocessResult', ('stdout', 'stderr', 'code'))
 
 
 def run_command(command, capture=None, check=False, encoding='utf-8', shell=False, env=None):
+    """This function provides a convenient API wrapping subprocess.Popen. Captured output
+    is guaranteed not to deadlock, but will still reside in memory in the end.
+
+    :param command: The command to run in a subprocess.
+    :type command: ``str`` or ``list`` of ``str``
+    :param capture: The method of output capture:
+                    stdout => 'stdout' or 'out'
+                    stderr => 'stderr' or 'err'
+                    both   => ``True`` or 'both'
+                    none   => ``False`` or ``None``
+    :type capture: ``bool`` or ``str``
+    :param check: Whether or not to raise an exception on non-zero exit codes.
+    :type check: ``bool``
+    :param encoding: Method of decoding output.
+    :type encoding: ``str``
+    :param shell: Whether to use the shell as the program to execute. Use
+                  'detect' to attempt the right thing in a cross-platform
+                  manner. You should never need to use this argument.
+    :type shell: ``bool`` or ``str``
+    :param env: The environment to replace ``os.environ`` with in the subprocess.
+    :type env: ``dict``
+    """
     if shell == 'detect':
         shell = NEED_SHELL
 
