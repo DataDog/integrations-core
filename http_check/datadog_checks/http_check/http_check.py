81d43173721a5b495643b5fae5b0fd027ad03aaf
Robert van Veelen <robert.vanveelen@gmail.com>
2018-07-18 19:44:45 +0100

    Pass request_context to _new_pool

--- http_check/datadog_checks/http_check/http_check.py
+++ http_check/datadog_checks/http_check/http_check.py
@@ -104,7 +104,7 @@ class WeakCiphersHTTPSConnectionPool(urllib3.connectionpool.HTTPSConnectionPool)
 
 class WeakCiphersPoolManager(urllib3.poolmanager.PoolManager):
 
-    def _new_pool(self, scheme, host, port):
+    def _new_pool(self, scheme, host, port, request_context=None):
         if scheme == 'https':
             return WeakCiphersHTTPSConnectionPool(host, port, **(self.connection_pool_kw))
         return super(WeakCiphersPoolManager, self)._new_pool(scheme, host, port)
