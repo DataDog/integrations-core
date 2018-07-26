f64a9410a3dc3c2d8530a59cc11f8a567f9ed698
Ofek Lev <ofekmeister@gmail.com>
2018-07-26 13:09:07 -0400

    Make sure all checks' versions are exposed (#1945)

--- teamcity/datadog_checks/teamcity/__init__.py
+++ teamcity/datadog_checks/teamcity/__init__.py
@@ -1,7 +1,7 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from __about__ import __version__
+from .__about__ import __version__
 from .teamcity import TeamCityCheck
 
 __all__ = [
