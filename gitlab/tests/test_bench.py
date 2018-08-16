e55a7f0a4329ba58352efe4cbd37343d9b49ef21
Ofek Lev <ofekmeister@gmail.com>
2018-08-16 18:24:59 -0400

    Add benchmarks to gitlab check (#2065)

---
+++ gitlab/tests/test_bench.py
@@ -0,0 +1,15 @@
+# (C) Datadog, Inc. 2018
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.gitlab import GitlabCheck
+from .common import CONFIG
+
+
+def test_run(benchmark):
+    instance = CONFIG['instances'][0]
+    c = GitlabCheck('gitlab', CONFIG['init_config'], {})
+
+    # Run once to get any initialization out of the way.
+    c.check(instance)
+
+    benchmark(c.check, instance)
