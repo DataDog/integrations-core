b2923534220d4c28d4bf88a4da7393017c08c870
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-08-20 10:38:06 -0400

    Put behind instance config flag

--- datadog_checks_base/datadog_checks/utils/decorator.py
+++ datadog_checks_base/datadog_checks/utils/decorator.py
@@ -2,11 +2,14 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 
+from datadog_checks.config import is_affirmative
 from ddtrace import tracer
-patch(requests=True)
 
 def trace_func(func):
     def function_wrapper(*args, **kwargs):
-        with tracer.trace(func.__name__, service=args[0].__class__.__name__) as span:
+        if is_affirmative(args[1].get('trace_check', False)):
+            with tracer.trace(func.__name__, service=args[0].__class__.__name__) as span:
+                return func(*args, **kwargs)
+        else:
             return func(*args, **kwargs)
     return function_wrapper
