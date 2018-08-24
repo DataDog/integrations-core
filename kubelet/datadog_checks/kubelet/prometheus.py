b1e6b82664597782375bd0e2216ca924fd47cc79
Pierre <mfpierre@gmail.com>
2018-08-24 16:11:19 +0200

    Drop protobuf in kubelet check

--- kubelet/datadog_checks/kubelet/prometheus.py
+++ kubelet/datadog_checks/kubelet/prometheus.py
@@ -4,12 +4,14 @@
 
 # project
 from copy import deepcopy
+from datadog_checks.checks.openmetrics import OpenMetricsBaseCheck
 from tagger import get_tags
 
 # check
 from .common import is_static_pending_pod, get_pod_by_uid
 
 METRIC_TYPES = ['counter', 'gauge', 'summary']
+
 # container-specific metrics should have all these labels
 CONTAINER_LABELS = ['container_name', 'namespace', 'pod_name', 'name', 'image', 'id']
 
@@ -96,11 +98,10 @@ class CadvisorPrometheusScraperMixin(object):
         """
         for lbl in CONTAINER_LABELS:
             if lbl == 'container_name':
-                for ml in labels:
-                    if ml.name == lbl:
-                        if ml.value == '' or ml.value == 'POD':
+                if lbl in labels:
+                    if labels[lbl] == '' or labels[lbl] == 'POD':
                             return False
-            if lbl not in [ml.name for ml in labels]:
+            if lbl not in labels:
                 return False
         return True
 
@@ -113,15 +114,16 @@ class CadvisorPrometheusScraperMixin(object):
         :param metric
         :return bool
         """
-        for ml in labels:
-            if ml.name == 'container_name' and ml.value == 'POD':
+        if 'container_name' in labels:
+            if labels['container_name'] == 'POD':
                 return True
-            # container_cpu_usage_seconds_total has an id label that is a cgroup path
-            # eg: /kubepods/burstable/pod531c80d9-9fc4-11e7-ba8b-42010af002bb
-            # FIXME: this was needed because of a bug:
-            # https://github.com/kubernetes/kubernetes/pull/51473
-            # starting from k8s 1.8 we can remove this
-            elif ml.name == 'id' and ml.value.split('/')[-1].startswith('pod'):
+        # container_cpu_usage_seconds_total has an id label that is a cgroup path
+        # eg: /kubepods/burstable/pod531c80d9-9fc4-11e7-ba8b-42010af002bb
+        # FIXME: this was needed because of a bug:
+        # https://github.com/kubernetes/kubernetes/pull/51473
+        # starting from k8s 1.8 we can remove this
+        if 'id' in labels:
+            if labels['id'].split('/')[-1].startswith('pod'):
                 return True
         return False
 
@@ -133,9 +135,8 @@ class CadvisorPrometheusScraperMixin(object):
         :param l_name: str
         :return: str or None
         """
-        for label in labels:
-            if label.name == l_name:
-                return label.value
+        if l_name in labels:
+            return labels[l_name]
 
     def _get_container_id(self, labels):
         """
@@ -218,42 +219,48 @@ class CadvisorPrometheusScraperMixin(object):
         return []
 
     @staticmethod
-    def _sum_values_by_context(message, uid_from_labels):
+    def _sum_values_by_context(metric, uid_from_labels):
         """
-        Iterates over all metrics in a message and sums the values
+        Iterates over all metrics in a metric and sums the values
         matching the same uid. Modifies the metric family in place.
-        :param message: prometheus metric family
+        :param metric: prometheus metric family
         :param uid_from_labels: function mapping a metric.label to a unique context id
         :return: dict with uid as keys, metric object references as values
         """
         seen = {}
-        metric_type = METRIC_TYPES[message.type]
-        for metric in message.metric:
-            uid = uid_from_labels(metric.label)
+        for sample in metric.samples:
+            uid = uid_from_labels(sample[OpenMetricsBaseCheck.SAMPLE_LABELS])
             if not uid:
-                metric.Clear()  # Ignore this metric message
+                # TODO
+                # metric.Clear()  # Ignore this metric message
                 continue
             # Sum the counter value accross all contexts
             if uid not in seen:
-                seen[uid] = metric
+                seen[uid] = sample
             else:
-                getattr(seen[uid], metric_type).value += getattr(metric, metric_type).value
-                metric.Clear()  # Ignore this metric message
+                # We have to create a new tuple
+                seen[uid] = (
+                    seen[uid][OpenMetricsBaseCheck.SAMPLE_NAME],
+                    seen[uid][OpenMetricsBaseCheck.SAMPLE_LABELS],
+                    seen[uid][OpenMetricsBaseCheck.SAMPLE_VALUE] + sample[OpenMetricsBaseCheck.SAMPLE_VALUE]
+                )
+                # TODO
+                # metric.Clear()  # Ignore this metric message
 
         return seen
 
-    def _process_container_rate(self, metric_name, message, scraper_config):
+    def _process_container_rate(self, metric_name, metric, scraper_config):
         """
         Takes a simple metric about a container, reports it as a rate.
         If several series are found for a given container, values are summed before submission.
         """
-        if message.type >= len(METRIC_TYPES):
-            self.log.error("Metric type %s unsupported for metric %s" % (message.type, message.name))
+        if metric.type not in METRIC_TYPES:
+            self.log.error("Metric type %s unsupported for metric %s" % (metric.type, metric.name))
             return
 
-        metrics = self._sum_values_by_context(message, self._get_container_id_if_container_metric)
-        for c_id, metric in metrics.iteritems():
-            pod_uid = self._get_pod_uid(metric.label)
+        samples = self._sum_values_by_context(metric, self._get_container_id_if_container_metric)
+        for c_id, sample in samples.iteritems():
+            pod_uid = self._get_pod_uid(sample[self.SAMPLE_LABELS])
             if self.pod_list_utils.is_excluded(c_id, pod_uid):
                 continue
 
@@ -262,49 +269,49 @@ class CadvisorPrometheusScraperMixin(object):
 
             # FIXME we are forced to do that because the Kubelet PodList isn't updated
             # for static pods, see https://github.com/kubernetes/kubernetes/pull/59948
-            pod = self._get_pod_by_metric_label(metric.label)
+            pod = self._get_pod_by_metric_label(sample[self.SAMPLE_LABELS])
             if pod is not None and is_static_pending_pod(pod):
                 tags += get_tags('kubernetes_pod://%s' % pod["metadata"]["uid"], True)
-                tags += self._get_kube_container_name(metric.label)
+                tags += self._get_kube_container_name(sample[self.SAMPLE_LABELS])
                 tags = list(set(tags))
 
-            val = getattr(metric, METRIC_TYPES[message.type]).value
+            val = sample[self.SAMPLE_VALUE]
 
             self.rate(metric_name, val, tags)
 
-    def _process_pod_rate(self, metric_name, message, scraper_config):
+    def _process_pod_rate(self, metric_name, metric, scraper_config):
         """
         Takes a simple metric about a pod, reports it as a rate.
         If several series are found for a given pod, values are summed before submission.
         """
-        if message.type >= len(METRIC_TYPES):
-            self.log.error("Metric type %s unsupported for metric %s" % (message.type, message.name))
+        if metric.type not in METRIC_TYPES:
+            self.log.error("Metric type %s unsupported for metric %s" % (metric.type, metric.name))
             return
 
-        metrics = self._sum_values_by_context(message, self._get_pod_uid_if_pod_metric)
-        for pod_uid, metric in metrics.iteritems():
+        samples = self._sum_values_by_context(metric, self._get_pod_uid_if_pod_metric)
+        for pod_uid, sample in samples.iteritems():
             if '.network.' in metric_name and self._is_pod_host_networked(pod_uid):
                 continue
             tags = get_tags('kubernetes_pod://%s' % pod_uid, True)
             tags += scraper_config['custom_tags']
-            val = getattr(metric, METRIC_TYPES[message.type]).value
+            val = sample[self.SAMPLE_VALUE]
             self.rate(metric_name, val, tags)
 
-    def _process_usage_metric(self, m_name, message, cache, scraper_config):
+    def _process_usage_metric(self, m_name, metric, cache, scraper_config):
         """
-        Takes a metrics message, a metric name, and a cache dict where it will store
+        Takes a metric object, a metric name, and a cache dict where it will store
         container_name --> (value, tags) so that _process_limit_metric can compute usage_pct
         it also submit said value and tags as a gauge.
         """
         # track containers that still exist in the cache
         seen_keys = {k: False for k in cache}
 
-        metrics = self._sum_values_by_context(message, self._get_container_id_if_container_metric)
-        for c_id, metric in metrics.iteritems():
-            c_name = self._get_container_label(metric.label, 'name')
+        samples = self._sum_values_by_context(metric, self._get_container_id_if_container_metric)
+        for c_id, sample in samples.iteritems():
+            c_name = self._get_container_label(sample[self.SAMPLE_LABELS], 'name')
             if not c_name:
                 continue
-            pod_uid = self._get_pod_uid(metric.label)
+            pod_uid = self._get_pod_uid(sample[self.SAMPLE_LABELS])
             if self.pod_list_utils.is_excluded(c_id, pod_uid):
                 continue
 
@@ -313,13 +320,13 @@ class CadvisorPrometheusScraperMixin(object):
 
             # FIXME we are forced to do that because the Kubelet PodList isn't updated
             # for static pods, see https://github.com/kubernetes/kubernetes/pull/59948
-            pod = self._get_pod_by_metric_label(metric.label)
+            pod = self._get_pod_by_metric_label(sample[self.SAMPLE_LABELS])
             if pod is not None and is_static_pending_pod(pod):
                 tags += get_tags('kubernetes_pod://%s' % pod["metadata"]["uid"], True)
-                tags += self._get_kube_container_name(metric.label)
+                tags += self._get_kube_container_name(sample[self.SAMPLE_LABELS])
                 tags = list(set(tags))
 
-            val = getattr(metric, METRIC_TYPES[message.type]).value
+            val = sample[self.SAMPLE_VALUE]
             cache[c_name] = (val, tags)
             seen_keys[c_name] = True
             self.gauge(m_name, val, tags)
@@ -329,16 +336,16 @@ class CadvisorPrometheusScraperMixin(object):
             if not seen:
                 del cache[k]
 
-    def _process_limit_metric(self, m_name, message, cache, scraper_config, pct_m_name=None):
+    def _process_limit_metric(self, m_name, metric, cache, scraper_config, pct_m_name=None):
         """
         Reports limit metrics if m_name is not an empty string,
         and optionally checks in the given cache if there's a usage
-        for each metric in the message and reports the usage_pct
+        for each sample in the metric and reports the usage_pct
         """
-        metrics = self._sum_values_by_context(message, self._get_container_id_if_container_metric)
-        for c_id, metric in metrics.iteritems():
-            limit = getattr(metric, METRIC_TYPES[message.type]).value
-            pod_uid = self._get_pod_uid(metric.label)
+        samples = self._sum_values_by_context(metric, self._get_container_id_if_container_metric)
+        for c_id, sample in samples.iteritems():
+            limit = sample[self.SAMPLE_VALUE]
+            pod_uid = self._get_pod_uid(sample[self.SAMPLE_LABELS])
             if self.pod_list_utils.is_excluded(c_id, pod_uid):
                 continue
 
@@ -349,7 +356,7 @@ class CadvisorPrometheusScraperMixin(object):
                 self.gauge(m_name, limit, tags)
 
             if pct_m_name and limit > 0:
-                c_name = self._get_container_label(metric.label, 'name')
+                c_name = self._get_container_label(sample[self.SAMPLE_LABELS], 'name')
                 if not c_name:
                     continue
                 usage, tags = cache.get(c_name, (None, None))
@@ -359,79 +366,81 @@ class CadvisorPrometheusScraperMixin(object):
                     self.log.debug("No corresponding usage found for metric %s and "
                                    "container %s, skipping usage_pct for now." % (pct_m_name, c_name))
 
-    def container_cpu_usage_seconds_total(self, message, scraper_config):
+    def container_cpu_usage_seconds_total(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.cpu.usage.total'
 
-        for metric in message.metric:
-            # Convert cores in nano cores
-            metric.counter.value *= 10. ** 9
-        self._process_container_rate(metric_name, message, scraper_config)
+        for i, sample in enumerate(metric.samples):
+            # Replacing the sample tuple to convert cores in nano cores
+            metric.samples[i] = (sample[self.SAMPLE_NAME], sample[self.SAMPLE_LABELS],
+                                 sample[self.SAMPLE_VALUE] * 10. ** 9)
 
-    def container_fs_reads_bytes_total(self, message, scraper_config):
+        self._process_container_rate(metric_name, metric, scraper_config)
+
+    def container_fs_reads_bytes_total(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.io.read_bytes'
-        self._process_container_rate(metric_name, message, scraper_config)
+        self._process_container_rate(metric_name, metric, scraper_config)
 
-    def container_fs_writes_bytes_total(self, message, scraper_config):
+    def container_fs_writes_bytes_total(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.io.write_bytes'
-        self._process_container_rate(metric_name, message, scraper_config)
+        self._process_container_rate(metric_name, metric, scraper_config)
 
-    def container_network_receive_bytes_total(self, message, scraper_config):
+    def container_network_receive_bytes_total(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.network.rx_bytes'
-        self._process_pod_rate(metric_name, message, scraper_config)
+        self._process_pod_rate(metric_name, metric, scraper_config)
 
-    def container_network_transmit_bytes_total(self, message, scraper_config):
+    def container_network_transmit_bytes_total(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.network.tx_bytes'
-        self._process_pod_rate(metric_name, message, scraper_config)
+        self._process_pod_rate(metric_name, metric, scraper_config)
 
-    def container_network_receive_errors_total(self, message, scraper_config):
+    def container_network_receive_errors_total(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.network.rx_errors'
-        self._process_pod_rate(metric_name, message, scraper_config)
+        self._process_pod_rate(metric_name, metric, scraper_config)
 
-    def container_network_transmit_errors_total(self, message, scraper_config):
+    def container_network_transmit_errors_total(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.network.tx_errors'
-        self._process_pod_rate(metric_name, message, scraper_config)
+        self._process_pod_rate(metric_name, metric, scraper_config)
 
-    def container_network_transmit_packets_dropped_total(self, message, scraper_config):
+    def container_network_transmit_packets_dropped_total(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.network.tx_dropped'
-        self._process_pod_rate(metric_name, message, scraper_config)
+        self._process_pod_rate(metric_name, metric, scraper_config)
 
-    def container_network_receive_packets_dropped_total(self, message, scraper_config):
+    def container_network_receive_packets_dropped_total(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.network.rx_dropped'
-        self._process_pod_rate(metric_name, message, scraper_config)
+        self._process_pod_rate(metric_name, metric, scraper_config)
 
-    def container_fs_usage_bytes(self, message, scraper_config):
+    def container_fs_usage_bytes(self, metric, scraper_config):
         """
         Number of bytes that are consumed by the container on this filesystem.
         """
         metric_name = scraper_config['namespace'] + '.filesystem.usage'
-        if message.type >= len(METRIC_TYPES):
-            self.log.error("Metric type %s unsupported for metric %s" % (message.type, message.name))
+        if metric.type not in METRIC_TYPES:
+            self.log.error("Metric type %s unsupported for metric %s" % (metric.type, metric.name))
             return
-        self._process_usage_metric(metric_name, message, self.fs_usage_bytes, scraper_config)
+        self._process_usage_metric(metric_name, metric, self.fs_usage_bytes, scraper_config)
 
-    def container_fs_limit_bytes(self, message, scraper_config):
+    def container_fs_limit_bytes(self, metric, scraper_config):
         """
         Number of bytes that can be consumed by the container on this filesystem.
         This method is used by container_fs_usage_bytes, it doesn't report any metric
         """
         pct_m_name = scraper_config['namespace'] + '.filesystem.usage_pct'
-        if message.type >= len(METRIC_TYPES):
-            self.log.error("Metric type %s unsupported for metric %s" % (message.type, message.name))
+        if metric.type not in METRIC_TYPES:
+            self.log.error("Metric type %s unsupported for metric %s" % (metric.type, metric.name))
             return
-        self._process_limit_metric('', message, self.fs_usage_bytes, scraper_config, pct_m_name)
+        self._process_limit_metric('', metric, self.fs_usage_bytes, scraper_config, pct_m_name)
 
-    def container_memory_usage_bytes(self, message, scraper_config):
+    def container_memory_usage_bytes(self, metric, scraper_config):
         """TODO: add swap, cache, failcnt and rss"""
         metric_name = scraper_config['namespace'] + '.memory.usage'
-        if message.type >= len(METRIC_TYPES):
-            self.log.error("Metric type %s unsupported for metric %s" % (message.type, message.name))
+        if metric.type not in METRIC_TYPES:
+            self.log.error("Metric type %s unsupported for metric %s" % (metric.type, metric.name))
             return
-        self._process_usage_metric(metric_name, message, self.mem_usage_bytes, scraper_config)
+        self._process_usage_metric(metric_name, metric, self.mem_usage_bytes, scraper_config)
 
-    def container_spec_memory_limit_bytes(self, message, scraper_config):
+    def container_spec_memory_limit_bytes(self, metric, scraper_config):
         metric_name = scraper_config['namespace'] + '.memory.limits'
         pct_m_name = scraper_config['namespace'] + '.memory.usage_pct'
-        if message.type >= len(METRIC_TYPES):
-            self.log.error("Metric type %s unsupported for metric %s" % (message.type, message.name))
+        if metric.type not in METRIC_TYPES:
+            self.log.error("Metric type %s unsupported for metric %s" % (metric.type, metric.name))
             return
-        self._process_limit_metric(metric_name, message, self.mem_usage_bytes, scraper_config, pct_m_name=pct_m_name)
+        self._process_limit_metric(metric_name, metric, self.mem_usage_bytes, scraper_config, pct_m_name=pct_m_name)
--- kubelet/tests/test_prometheus.py
+++ kubelet/tests/test_prometheus.py
@@ -4,7 +4,6 @@
 import json
 import os
 import sys
-from collections import namedtuple
 
 import pytest
 import mock
@@ -18,13 +17,11 @@ pytestmark = pytest.mark.skipif(sys.platform == 'win32', reason='tests for linux
 # Constants
 HERE = os.path.abspath(os.path.dirname(__file__))
 
-Label = namedtuple('Label', 'name value')
-
 
 class MockMetric(object):
     def __init__(self, name, labels=None, value=None):
         self.name = name
-        self.label = labels if labels else []
+        self.label = labels if labels else {}
         self.value = value
 
 
@@ -62,24 +59,24 @@ def test_is_container_metric():
         MockMetric('foo', []),
         MockMetric(
             'bar',
-            [
-                Label(name='container_name', value='POD'),  # POD --> False
-                Label(name='namespace', value='default'),
-                Label(name='pod_name', value='pod0'),
-                Label(name='name', value='foo'),
-                Label(name='image', value='foo'),
-                Label(name='id', value='deadbeef'),
-            ]
+            {
+                'container_name': 'POD',
+                'namespace': 'default',
+                'pod_name': 'pod0',
+                'name': 'foo',
+                'image': 'foo',
+                'id': 'deadbeef',
+            }
         ),
         MockMetric(  # missing container_name
             'foobar',
-            [
-                Label(name='namespace', value='default'),
-                Label(name='pod_name', value='pod0'),
-                Label(name='name', value='foo'),
-                Label(name='image', value='foo'),
-                Label(name='id', value='deadbeef'),
-            ]
+            {
+                'namespace': 'default',
+                'pod_name': 'pod0',
+                'name': 'foo',
+                'image': 'foo',
+                'id': 'deadbeef',
+            }
         ),
     ]
     for metric in false_metrics:
@@ -87,31 +84,32 @@ def test_is_container_metric():
 
     true_metric = MockMetric(
         'foo',
-        [
-            Label(name='container_name', value='ctr0'),
-            Label(name='namespace', value='default'),
-            Label(name='pod_name', value='pod0'),
-            Label(name='name', value='foo'),
-            Label(name='image', value='foo'),
-            Label(name='id', value='deadbeef'),
-        ]
+        {
+            'container_name': 'ctr0',
+            'namespace': 'default',
+            'pod_name': 'pod0',
+            'name': 'foo',
+            'image': 'foo',
+            'id': 'deadbeef',
+        }
     )
     assert CadvisorPrometheusScraperMixin._is_container_metric(true_metric.label) is True
 
 
 def test_is_pod_metric():
     false_metrics = [
-        MockMetric('foo', []),
-        MockMetric('bar', [Label(name='container_name', value='ctr0')]),
-        MockMetric('foobar', [Label(name='container_name', value='ctr0'), Label(name='id', value='deadbeef')]),
+        MockMetric('foo', {}),
+        MockMetric('bar', {'container_name': 'ctr0'}),
+        MockMetric('foobar', {'container_name': 'ctr0', 'id': 'deadbeef'}),
     ]
 
     true_metrics = [
-        MockMetric('foo', [Label(name='container_name', value='POD')]),
-        MockMetric('bar', [Label(name='id', value='/kubepods/burstable/pod531c80d9-9fc4-11e7-ba8b-42010af002bb')]),
-        MockMetric('foobar', [
-            Label(name='container_name', value='POD'),
-            Label(name='id', value='/kubepods/burstable/pod531c80d9-9fc4-11e7-ba8b-42010af002bb')]),
+        MockMetric('foo', {'container_name': 'POD'}),
+        MockMetric('bar', {'id': '/kubepods/burstable/pod531c80d9-9fc4-11e7-ba8b-42010af002bb'}),
+        MockMetric('foobar', {
+            'container_name': 'POD',
+            'id': '/kubepods/burstable/pod531c80d9-9fc4-11e7-ba8b-42010af002bb'
+        }),
     ]
 
     for metric in false_metrics:
@@ -122,32 +120,32 @@ def test_is_pod_metric():
 
 
 def test_get_container_label():
-    labels = [
-        Label("container_name", value="POD"),
-        Label("id", value="/kubepods/burstable/pod531c80d9-9fc4-11e7-ba8b-42010af002bb"),
-    ]
+    labels = {
+        "container_name": "POD",
+        "id": "/kubepods/burstable/pod531c80d9-9fc4-11e7-ba8b-42010af002bb",
+    }
     assert CadvisorPrometheusScraperMixin._get_container_label(labels, "container_name") == "POD"
-    assert CadvisorPrometheusScraperMixin._get_container_label([], "not-in") is None
+    assert CadvisorPrometheusScraperMixin._get_container_label({}, "not-in") is None
 
 
 def test_get_container_id(cadvisor_scraper):
-    labels = [
-        Label("container_name", value="datadog-agent"),
-        Label("namespace", value="default"),
-        Label("pod_name", value="datadog-agent-pbqt2"),
-        Label("container_name", value="datadog-agent"),
-    ]
+    labels = {
+        "container_name": "datadog-agent",
+        "namespace": "default",
+        "pod_name": "datadog-agent-pbqt2",
+        "container_name": "datadog-agent",
+    }
     container_id = cadvisor_scraper._get_container_id(labels)
     assert container_id == "containerd://51cba2ca229069039575750d44ed3a67e9b5ead651312ba7ff218dd9202fde64"
     assert cadvisor_scraper._get_container_id([]) is None
 
 
 def test_get_pod_uid(cadvisor_scraper):
-    labels = [
-        Label("container_name", value="POD"),
-        Label("namespace", value="default"),
-        Label("pod_name", value="datadog-agent-pbqt2"),
-    ]
+    labels = {
+        "container_name": "POD",
+        "namespace": "default",
+        "pod_name": "datadog-agent-pbqt2",
+    }
     assert cadvisor_scraper._get_pod_uid(labels) == "b66c40af-997d-11e8-96a3-42010a840157"
     assert cadvisor_scraper._get_pod_uid([]) is None
 
@@ -161,33 +159,31 @@ def test_is_pod_host_networked(cadvisor_scraper):
 
 def test_get_pod_by_metric_label(cadvisor_scraper):
     assert len(cadvisor_scraper.pod_list) == 4
-    kube_proxy = cadvisor_scraper._get_pod_by_metric_label([
-        Label("container_name", value="POD"),
-        Label("namespace", value="kube-system"),
-        Label("pod_name", value="kube-proxy-2d2bq")
-    ])
-    fluentd = cadvisor_scraper._get_pod_by_metric_label([
-        Label("container_name", value="POD"),
-        Label("namespace", value="kube-system"),
-        Label("pod_name", value="fluentd-gcp-v3.0.0-z55q5")
-    ])
+    kube_proxy = cadvisor_scraper._get_pod_by_metric_label({
+        "container_name": "POD",
+        "namespace": "kube-system",
+        "pod_name": "kube-proxy-2d2bq",
+    })
+    fluentd = cadvisor_scraper._get_pod_by_metric_label({
+        "container_name": "POD",
+        "namespace": "kube-system",
+        "pod_name": "fluentd-gcp-v3.0.0-z55q5",
+    })
     assert kube_proxy["metadata"]["uid"] == "8abf1ed0-94c4-11e8-96a3-42010a840157"
     assert fluentd["metadata"]["uid"] == "fe3d57c4-94c4-11e8-96a3-42010a840157"
 
 
 def test_get_kube_container_name():
-    tags = CadvisorPrometheusScraperMixin._get_kube_container_name([
-        Label("container_name", value="datadog-agent"),
-        Label("id",
-              value="/kubepods/burstable/"
-                    "podc2319815-10d0-11e8-bd5a-42010af00137/"
-                    "a335589109ce5506aa69ba7481fc3e6c943abd23c5277016c92dac15d0f40479"),
-        Label("image",
-              value="datadog/agent-dev@sha256:894fb66f89be0332a47388d7219ab8b365520ff0e3bbf597bd0a378b19efa7ee"),
-        Label("name", value="k8s_datadog-agent_datadog-agent-jbm2k_default_c2319815-10d0-11e8-bd5a-42010af00137_0"),
-        Label("namespace", value="default"),
-        Label("pod_name", value="datadog-agent-jbm2k"),
-    ])
+    tags = CadvisorPrometheusScraperMixin._get_kube_container_name({
+        "container_name": "datadog-agent",
+        "id": "/kubepods/burstable/"
+              "podc2319815-10d0-11e8-bd5a-42010af00137/"
+              "a335589109ce5506aa69ba7481fc3e6c943abd23c5277016c92dac15d0f40479",
+        "image": "datadog/agent-dev@sha256:894fb66f89be0332a47388d7219ab8b365520ff0e3bbf597bd0a378b19efa7ee",
+        "name": "k8s_datadog-agent_datadog-agent-jbm2k_default_c2319815-10d0-11e8-bd5a-42010af00137_0",
+        "namespace": "default",
+        "pod_name": "datadog-agent-jbm2k",
+    })
     assert tags == ["kube_container_name:datadog-agent"]
 
     tags = CadvisorPrometheusScraperMixin._get_kube_container_name([])
