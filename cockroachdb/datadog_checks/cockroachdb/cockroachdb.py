434d7c6db4367f85a4da57069a7afb14a83fbbc0
Ofek Lev <ofekmeister@gmail.com>
2018-08-30 01:47:00 -0400

    add disk space service check

--- cockroachdb/datadog_checks/cockroachdb/cockroachdb.py
+++ cockroachdb/datadog_checks/cockroachdb/cockroachdb.py
@@ -1,6 +1,8 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from __future__ import division
+
 from collections import defaultdict
 
 from datadog_checks.checks.openmetrics import OpenMetricsBaseCheck
@@ -9,6 +11,8 @@ from .metrics import METRIC_MAP, TRACKED_METRICS
 
 
 class CockroachdbCheck(OpenMetricsBaseCheck):
+    SERVICE_CHECK_DISK_SPACE = 'cockroachdb.disk_space'
+
     def __init__(self, name, init_config, agentConfig, instances=None):
         super(CockroachdbCheck, self).__init__(
             name,
@@ -52,9 +56,34 @@ class CockroachdbCheck(OpenMetricsBaseCheck):
             metric_transformers={metric: self.track_metric for metric in TRACKED_METRICS}
         )
 
+        tags = instance.get('tags', [])
+        self.check_disk_space(tracked_metrics, instance, tags)
+
+    def check_disk_space(self, tracked_metrics, instance, tags):
+        capacity_total = tracked_metrics.get('capacity')
+        capacity_available = tracked_metrics.get('capacity_available')
+
+        if capacity_total is None or capacity_available is None:
+            self.log.info(
+                'Missing `capacity` and `capacity_available` metrics, skipping disk space check...'
+            )
+            return
+
+        percent_remaining = capacity_available[0] / capacity_total[0] * 100
+
+        disk_space_critical = int(instance.get('disk_space_critical', 5))
+        if disk_space_critical <= percent_remaining:
+            self.service_check(self.SERVICE_CHECK_DISK_SPACE, self.CRITICAL, tags=tags)
+            return
+
+        disk_space_warning = int(instance.get('disk_space_warning', 15))
+        if disk_space_warning <= percent_remaining:
+            self.service_check(self.SERVICE_CHECK_DISK_SPACE, self.WARNING, tags=tags)
+            return
+
+        self.service_check(self.SERVICE_CHECK_DISK_SPACE, self.OK, tags=tags)
+
     def track_metric(self, metric, scraper_config):
-        scraper_config['_tracked_metrics'][metric.name].extend(
-            sample[self.SAMPLE_VALUE] for sample in metric.samples
-        )
+        scraper_config['_tracked_metrics'][metric.name].append(metric.samples[0][self.SAMPLE_VALUE])
 
         self._submit(TRACKED_METRICS[metric.name], metric, scraper_config)
