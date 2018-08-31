5dc1d65686a3968e8fb17271265597a2c621489c
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-08-30 20:02:05 -0400

    Move patch

--- datadog_checks_base/datadog_checks/__init__.py
+++ datadog_checks_base/datadog_checks/__init__.py
@@ -2,9 +2,6 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
-from ddtrace import patch
-
-patch(requests=True)
 
 __all__ = [
     '__version__'
