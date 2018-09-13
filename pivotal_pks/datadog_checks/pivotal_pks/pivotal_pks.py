291d5be13070b5ad2a728f7f9e8f1afbea5f890c
Ofek Lev <ofekmeister@gmail.com>
2018-09-13 12:13:02 -0400

    Remove extra files

--- pivotal_pks/datadog_checks/pivotal_pks/pivotal_pks.py
+++
@@ -1,9 +0,0 @@
-# (C) Datadog, Inc. 2018
-# All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
-from datadog_checks.checks import AgentCheck
-
-
-class PivotalPksCheck(AgentCheck):
-    def check(self, instance):
-        pass
