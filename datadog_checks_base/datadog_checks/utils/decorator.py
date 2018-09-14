67bd02bf1b035561aac516cf5b39b6b9a9de200e
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-09-14 16:29:46 -0400

    Use datadog_agent config

--- datadog_checks_base/datadog_checks/utils/decorator.py
+++ datadog_checks_base/datadog_checks/utils/decorator.py
@@ -6,6 +6,7 @@ from datadog_checks.config import is_affirmative
 from ddtrace import tracer
 from functools import wraps
 from inspect import getargspec
+import datadog_agent
 
 def trace_func(func):
     @wraps(func)
@@ -15,7 +16,7 @@ def trace_func(func):
             instance_index = getargspec(func).args.index('instance')
         except ValueError:
             return func(*args, **kwargs)
-        if is_affirmative(args[instance_index].get('trace_check', False)):
+        if is_affirmative(args[instance_index].get('trace_check', False)) and datadog_agent.get_config('integration_tracing'):
             with tracer.trace('integration.check', service='integrations-tracing', resource=args[0].name):
                 return func(*args, **kwargs)
         return func(*args, **kwargs)
