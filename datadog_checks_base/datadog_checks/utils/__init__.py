5dc4ae71eeba4518721f4cb153f3b53f6207a1bd
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-09-15 14:08:29 -0400

    Patch libraries behind a6 global config flag

--- datadog_checks_base/datadog_checks/utils/__init__.py
+++ datadog_checks_base/datadog_checks/utils/__init__.py
@@ -1,6 +1,12 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from ddtrace import patch
 
-patch(requests=True)
+from ddtrace import patch
+try:
+    import datadog_agent
+    if datadog_agent.get_config('integration_tracing'):
+        patch(requests=True)
+except ImportError:
+    # Tracing Integrations is only available with Agent 6
+    pass
--- datadog_checks_base/datadog_checks/utils/decorator.py
+++ datadog_checks_base/datadog_checks/utils/decorator.py
@@ -6,17 +6,25 @@ from datadog_checks.config import is_affirmative
 from ddtrace import tracer
 from functools import wraps
 from inspect import getargspec
-import datadog_agent
+
+try:
+    import datadog_agent
+except ImportError:
+    # Integration Tracing is only available with Agent 6
+    pass
 
 def trace_func(func):
     @wraps(func)
     def function_wrapper(*args, **kwargs):
+        if datadog_agent is None:
+            return func(*args, **kwargs)
+
         # Get instance config to see if tracing is enabled
         try:
             instance_index = getargspec(func).args.index('instance')
         except ValueError:
             return func(*args, **kwargs)
-        if is_affirmative(args[instance_index].get('trace_check', False)) and datadog_agent.get_config('integration_tracing'):
+        if is_affirmative(args[instance_index].get('trace_check', False)) and datadog_agent.get_config('integration_tracing', False):
             with tracer.trace('integration.check', service='integrations-tracing', resource=args[0].name):
                 return func(*args, **kwargs)
         return func(*args, **kwargs)
