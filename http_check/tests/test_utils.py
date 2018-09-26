9e59f42e81f9e77008bec2b9b34507157103e36a
Massimiliano Pippi <mpippi@gmail.com>
2018-09-26 18:52:05 -0400

    address PR comments

--- http_check/tests/test_utils.py
+++ http_check/tests/test_utils.py
@@ -1,14 +1,13 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-import tempfile
-import shutil
 import os
 import sys
 
 import pytest
 import mock
 
+from datadog_checks.dev import temp_dir
 from datadog_checks.http_check.utils import get_ca_certs_path, _get_ca_certs_paths
 
 
@@ -22,24 +21,21 @@ def test_get_ca_certs_path():
 def test__get_ca_certs_paths():
     with mock.patch('datadog_checks.http_check.utils.os.path.dirname') as dirname:
         # create a tmp `embeddeded` folder
-        tmp = tempfile.mkdtemp()
-        target = os.path.join(tmp, 'embedded')
-        os.mkdir(target)
-        # point `dirname()` there
-        dirname.return_value = target
+        with temp_dir() as tmp:
+            target = os.path.join(tmp, 'embedded')
+            os.mkdir(target)
+            # point `dirname()` there
+            dirname.return_value = target
 
-        # tornado not found
-        paths = _get_ca_certs_paths()
-        assert len(paths) == 2
-        assert paths[0].startswith(target)
-        assert paths[1] == '/etc/ssl/certs/ca-certificates.crt'
+            # tornado not found
+            paths = _get_ca_certs_paths()
+            assert len(paths) == 2
+            assert paths[0].startswith(target)
+            assert paths[1] == '/etc/ssl/certs/ca-certificates.crt'
 
-        # mock tornado's presence
-        sys.modules['tornado'] = mock.MagicMock(__file__='.')
-        paths = _get_ca_certs_paths()
-        print(paths)
-        assert len(paths) == 3
-        assert paths[1].endswith('ca-certificates.crt')
-        assert paths[2] == '/etc/ssl/certs/ca-certificates.crt'
-
-        shutil.rmtree(target)
+            # mock tornado's presence
+            sys.modules['tornado'] = mock.MagicMock(__file__='.')
+            paths = _get_ca_certs_paths()
+            assert len(paths) == 3
+            assert paths[1].endswith('ca-certificates.crt')
+            assert paths[2] == '/etc/ssl/certs/ca-certificates.crt'
