840dc3c9310e2cf01fc7b22a9ab261a013cba1aa
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-09-26 16:15:40 -0400

    Trace and Pin Openstack Check (#2306)
    * Add tracing to OpenStack
    
    * Pin datadog_checks_base

--- openstack/setup.py
+++ openstack/setup.py
@@ -25,7 +25,7 @@ def get_requirements(fpath):
         return f.readlines()
 
 
-CHECKS_BASE_REQ = 'datadog_checks_base'
+CHECKS_BASE_REQ = 'datadog_checks_base >= 3.0.0,<4.0.0'
 
 setup(
     name='datadog-openstack',
