631e228c317dda6e80c0768a1fa549718dbc874c
Ofek Lev <ofekmeister@gmail.com>
2018-09-27 17:29:45 -0400

    Make windows_service use scm api instead of wmi (#2305)
    * Make windows_service use scm api instead of wmi
    
    * address review, update readme

--- windows_service/tests/test_windows_service.py
+++ windows_service/tests/test_windows_service.py
@@ -1,150 +1,39 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-
 import pytest
 
-from datadog_checks.windows_service import WindowsService
+
+def test_bad_config(check, instance_bad_config):
+    with pytest.raises(ValueError):
+        check.check(instance_bad_config)
 
 
-@pytest.fixture
-def check():
-    return WindowsService("windows_service", {}, {}, None)
-
-
-INSTANCE = {
-    'host': '.',
-    'services': ['EventLog', 'Dnscache', 'NonExistingService'],
-    'tags': ['optional:tag1']
-}
-
-INVALID_HOST_INSTANCE = {
-    'host': 'nonexistinghost',
-    'services': ['EventLog'],
-}
-
-WILDCARD_INSTANCE = {
-    'host': '.',
-    'services': ['Event%', 'Dns%'],
-}
-
-ALL_INSTANCE = {
-    'host': '.',
-    'services': ['ALL'],
-}
-
-ALL_STATES_INSTANCE = {
-    "host": ".",
-    "services": [
-        "StoppedService", "StartPendingService", "StopPendingService", "RunningService",
-        "ContinuePendingService", "PausePendingService", "PausedService", "UnknownService"
-    ]
-}
-
-
-class mock_sampler():
-    def __init__(self):
-        self._wmi_objects = []
-
-    def __getitem__(self, i):
-        return self._wmi_objects[i]
-
-    def sample(self):
-        self._wmi_objects = [
-            {
-                "Name": "StoppedService",
-                "state": "Stopped"
-            },
-            {
-                "Name": "StartPendingService",
-                "state": "Start Pending"
-            },
-            {
-                "Name": "StopPendingService",
-                "state": "Stop Pending"
-            },
-            {
-                "Name": "RunningService",
-                "state": "Running"
-            },
-            {
-                "Name": "ContinuePendingService",
-                "state": "Continue Pending"
-            },
-            {
-                "Name": "PausePendingService",
-                "state": "Pause Pending"
-            },
-            {
-                "Name": "PausedService",
-                "state": "Paused"
-            },
-            {
-                "Name": "UnknownService",
-                "state": "Unknown"
-            },
-        ]
-
-    def reset(self):
-        self._wmi_objects = []
-
-
-def test_basic_check(aggregator, check):
-    check.check(INSTANCE)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.OK,
+def test_basic(aggregator, check, instance_basic):
+    check.check(instance_basic)
+    aggregator.assert_service_check(check.SERVICE_CHECK_NAME, status=check.OK,
                                     tags=['service:EventLog', 'optional:tag1'], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.OK,
+    aggregator.assert_service_check(check.SERVICE_CHECK_NAME, status=check.OK,
                                     tags=['service:Dnscache', 'optional:tag1'], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.CRITICAL,
-                                    tags=['service:NonExistingService', 'optional:tag1'], count=1)
-    aggregator.assert_all_metrics_covered()
+    aggregator.assert_service_check(check.SERVICE_CHECK_NAME, status=check.CRITICAL,
+                                    tags=['service:NonExistentService', 'optional:tag1'], count=1)
 
 
-def test_invalid_host(aggregator, check):
-    check.check(INVALID_HOST_INSTANCE)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.CRITICAL,
-                                    tags=['host:nonexistinghost', 'service:EventLog'], count=1)
-    aggregator.assert_all_metrics_covered()
-
-
-def test_wildcard(aggregator, check):
-    check.check(WILDCARD_INSTANCE)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.OK,
+def test_wildcard(aggregator, check, instance_wildcard):
+    check.check(instance_wildcard)
+    aggregator.assert_service_check(check.SERVICE_CHECK_NAME, status=check.OK,
                                     tags=['service:EventLog'], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.OK,
+    aggregator.assert_service_check(check.SERVICE_CHECK_NAME, status=check.OK,
                                     tags=['service:EventSystem'], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.OK,
+    aggregator.assert_service_check(check.SERVICE_CHECK_NAME, status=check.OK,
                                     tags=['service:Dnscache'], count=1)
-    aggregator.assert_all_metrics_covered()
 
 
-def test_all(aggregator, check):
-    check.check(ALL_INSTANCE)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.OK,
+def test_all(aggregator, check, instance_all):
+    check.check(instance_all)
+    aggregator.assert_service_check(check.SERVICE_CHECK_NAME, status=check.OK,
                                     tags=['service:EventLog'], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.OK,
+    aggregator.assert_service_check(check.SERVICE_CHECK_NAME, status=check.OK,
                                     tags=['service:Dnscache'], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.OK,
+    aggregator.assert_service_check(check.SERVICE_CHECK_NAME, status=check.OK,
                                     tags=['service:EventSystem'], count=1)
-
-
-def test_service_states(aggregator, check, mocker):
-    mocker.patch("datadog_checks.windows_service.WindowsService._get_wmi_sampler", return_value=mock_sampler())
-    check.check(ALL_STATES_INSTANCE)
-
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.CRITICAL,
-                                    tags=["service:StoppedService"], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.WARNING,
-                                    tags=["service:StartPendingService"], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.WARNING,
-                                    tags=["service:StopPendingService"], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.OK,
-                                    tags=["service:RunningService"], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.WARNING,
-                                    tags=["service:ContinuePendingService"], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.WARNING,
-                                    tags=["service:PausePendingService"], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.WARNING,
-                                    tags=["service:PausedService"], count=1)
-    aggregator.assert_service_check(WindowsService.SERVICE_CHECK_NAME, status=WindowsService.UNKNOWN,
-                                    tags=["service:UnknownService"], count=1)
