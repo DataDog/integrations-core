610019a93ee1997b0d37e311185ce3cdd8c5529e
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-10-01 11:12:15 -0400

    Start redirecting imports

---
+++ datadog_checks_base/datadog_checks/utils/containers.py
@@ -0,0 +1,5 @@
+# (C) Datadog, Inc. 2010-2016
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
+from datadog_checks.base.utils.containers import freeze, hash_mutable
---
+++ datadog_checks_base/datadog_checks/utils/prometheus/functions.py
@@ -0,0 +1,5 @@
+# (C) Datadog, Inc. 2016
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
+from datadog_checks.base.utils.prometheus.functions import parse_metric_family
