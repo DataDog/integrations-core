2c34c0635aa65e9ed6f75535a7bf76564668bdbe
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-10-02 13:10:59 -0400

    Fix __init__ files

---
+++ datadog_checks_base/datadog_checks/checks/openmetrics/__init__.py
@@ -0,0 +1,4 @@
+# (C) Datadog, Inc. 2018
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from ...base.checks.openmetrics import *
---
+++ datadog_checks_base/datadog_checks/utils/prometheus/__init__.py
@@ -0,0 +1,4 @@
+# (C) Datadog, Inc. 2016
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+from ...base.utils.prometheus import *
