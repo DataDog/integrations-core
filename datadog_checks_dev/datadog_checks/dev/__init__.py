987e8c5525c1b147be51ccba2bf85651419e69ae
Ofek Lev <ofekmeister@gmail.com>
2018-10-03 18:44:05 -0400

    Add generic environment runner

--- datadog_checks_dev/datadog_checks/dev/__init__.py
+++ datadog_checks_dev/datadog_checks/dev/__init__.py
@@ -3,6 +3,7 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
 from .docker import docker_run, get_docker_hostname
+from .env import environment_run
 from .errors import RetryError
 from .structures import EnvVars, LazyFunction
 from .subprocess import run_command
