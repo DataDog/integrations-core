98d1ab9b4751d159f209a00021febaaa7b011db0
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2018-10-04 12:16:14 +0200

    Use check fixture

--- snmp/tests/test_bench.py
+++ snmp/tests/test_bench.py
@@ -1,20 +1,17 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from datadog_checks.snmp import SnmpCheck
 from .common import TABULAR_OBJECTS, generate_instance_config
 
 
-def test_tabular_enforce(benchmark):
+def test_tabular_enforce(benchmark, check):
     instance = generate_instance_config(TABULAR_OBJECTS)
-    c = SnmpCheck('snmp', {}, {}, {})
 
-    benchmark(c.check, instance)
+    benchmark(check.check, instance)
 
 
-def test_tabular_no_enforce(benchmark):
+def test_tabular_no_enforce(benchmark, check):
     instance = generate_instance_config(TABULAR_OBJECTS)
     instance["enforce_mib_constraints"] = False
-    c = SnmpCheck('snmp', {}, {}, {})
 
-    benchmark(c.check, instance)
+    benchmark(check.check, instance)
