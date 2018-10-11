16f5d455697bfe0720f2d111ec9f8d041b58c704
Massimiliano Pippi <mpippi@gmail.com>
2018-10-11 09:50:06 +0200

    Move config parser to its own module (#2370)

--- elastic/tests/test_bench.py
+++ elastic/tests/test_bench.py
@@ -5,13 +5,10 @@ import time
 
 import requests
 
-from datadog_checks.elastic import ESCheck
-from .common import CHECK_NAME, CONFIG, PASSWORD, URL, USER
+from .common import CONFIG, PASSWORD, URL, USER
 
 
-def test_check(benchmark):
-    elastic_check = ESCheck(CHECK_NAME, {}, {})
-
+def test_check(benchmark, elastic_cluster, elastic_check):
     for _ in range(3):
         try:
             elastic_check.check(CONFIG)
@@ -21,22 +18,18 @@ def test_check(benchmark):
     benchmark(elastic_check.check, CONFIG)
 
 
-def test_pshard_metrics(benchmark):
+def test_pshard_metrics(benchmark, elastic_cluster, elastic_check):
     elastic_latency = 10
     config = {'url': URL, 'pshard_stats': True, 'username': USER, 'password': PASSWORD}
 
     requests.put(URL + '/_settings', data='{"index": {"number_of_replicas": 1}}')
     requests.put(URL + '/testindex/testtype/2', data='{"name": "Jane Doe", "age": 27}')
     requests.put(URL + '/testindex/testtype/1', data='{"name": "John Doe", "age": 42}')
-
     time.sleep(elastic_latency)
-    elastic_check = ESCheck(CHECK_NAME, {}, {})
 
     benchmark(elastic_check.check, config)
 
 
-def test_index_metrics(benchmark):
+def test_index_metrics(benchmark, elastic_cluster, elastic_check):
     config = {'url': URL, 'index_stats': True, 'username': USER, 'password': PASSWORD}
-    elastic_check = ESCheck(CHECK_NAME, {}, {})
-
     benchmark(elastic_check.check, config)
