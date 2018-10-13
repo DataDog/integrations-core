5fa1bb2ec93e76a97ddc55f471b5e885da9c3728
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2018-10-12 20:13:06 -0400

    Fix empty event issue with Agent 6 (#2348)
    * Fix Agent 6 Nagios event issues
    
    * lower fields
    
    * No need to lower, and fix tests
    
    * Fix Nagios via json
    
    * Update the fix to work with backend changes

--- nagios/datadog_checks/nagios/nagios.py
+++ nagios/datadog_checks/nagios/nagios.py
@@ -2,12 +2,11 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-# stdlib
 from collections import namedtuple
+import json
 import re
 from inspect import getargspec
 
-# project
 from datadog_checks.checks import AgentCheck
 from datadog_checks.utils.tailfile import TailFile
 
@@ -61,6 +60,8 @@ EVENT_FIELDS = {
 # Regex for the Nagios event log
 RE_LINE_REG = re.compile('^\[(\d+)\] EXTERNAL COMMAND: (\w+);(.*)$')
 RE_LINE_EXT = re.compile('^\[(\d+)\] ([^:]+): (.*)$')
+
+SOURCE_TYPE_NAME = 'Nagios'
 
 
 class Nagios(AgentCheck):
@@ -331,14 +332,36 @@ class NagiosEventLogTailer(NagiosTailer):
     def create_event(self, timestamp, event_type, hostname, fields):
         """Factory method called by the parsers
         """
-        d = fields._asdict()
-        d.update({'timestamp': timestamp, 'event_type': event_type})
+        # Agent6 expects a specific set of fields, so we need to place all
+        # extra fields in the msg_title and let the Datadog backend separate them
+        # Any remaining fields that aren't a part of the datadog-agent payload
+        # specification will be dropped.
+        event_payload = fields._asdict()
+
+        msg_text = {
+            'event_type': event_payload.pop('event_type', None),
+            'event_soft_hard': event_payload.pop('event_soft_hard', None),
+            'check_name': event_payload.pop('check_name', None),
+            'event_state': event_payload.pop('event_state', None),
+            'payload': event_payload.pop('payload', None),
+            'ack_author': event_payload.pop('ack_author', None)
+        }
+
+        msg_text = json.dumps(msg_text)
+        self.log.info("Nagios Event pack: {}".format(msg_text))
+
+        event_payload.update({
+                'timestamp': timestamp,
+                'event_type': event_type,
+                'msg_text': msg_text,
+                'source_type_name': SOURCE_TYPE_NAME,
+        })
 
         # if host is localhost, turn that into the internal host name
-        host = d.get('host', None)
+        host = event_payload.get('host', None)
         if host == "localhost":
-            d["host"] = hostname
-        return d
+            event_payload["host"] = hostname
+        return event_payload
 
 
 class NagiosPerfDataTailer(NagiosTailer):
