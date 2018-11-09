4c8bedb823a7d68aa61001236ddad735d8a45630
Daniel Maher <phrawzty+github@gmail.com>
2018-11-09 11:41:45 +0100

    Update datadog_checks_base/datadog_checks/base/utils/proxy.py
    Co-Authored-By: masci <mpippi@gmail.com>

--- datadog_checks_base/datadog_checks/base/utils/proxy.py
+++ datadog_checks_base/datadog_checks/base/utils/proxy.py
@@ -11,7 +11,7 @@ def config_proxy_skip(proxies, uri, skip_proxy=False):
     it will disable the proxy if the uri provided is to be reached directly.
     :param proxies dict with existing proxies: 'https', 'http', 'no' as pontential keys
     :param uri URI to determine if a proxy is necessary or not.
-    :param skip_proxy if True, the proxy dictionary returned will disable all proxies
+    :param skip_proxy If `True`, the returned proxy dictionary will disable all proxies.
     """
     parsed_uri = urlparse(uri)
 
