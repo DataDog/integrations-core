8433ae6d59ddb27753fa5015722f314c8ad19006
Gregory <gregory.zussa@datadoghq.com>
2018-11-20 18:27:10 +0100

    wip

--- openstack_controller/datadog_checks/openstack_controller/api.py
+++ openstack_controller/datadog_checks/openstack_controller/api.py
@@ -195,7 +195,7 @@ class KeystoneApi(AbstractApi):
         headers = {'X-Auth-Token': project_token}
         try:
             r = self._make_request(url, headers)
-            self.logger.debug("url: %s || response: %s", url, str(r['projects']))
+            self.logger.debug("url: %s || response: %s", url, json.dumps(r['projects']))
             return r['projects']
 
         except Exception as e:
