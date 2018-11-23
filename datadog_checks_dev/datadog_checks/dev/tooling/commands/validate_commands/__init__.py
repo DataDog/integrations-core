40e200ddd2dddadce2e382d11cdda8326e5ad244
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2018-11-23 16:14:37 +0100

    Add agent_req validation step (#2642)

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate_commands/__init__.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate_commands/__init__.py
@@ -5,10 +5,12 @@ from .dep import dep
 from .manifest import manifest
 from .metadata import metadata
 from .service_checks import service_checks
+from .agent_reqs import agent_reqs
 
 ALL_COMMANDS = (
     dep,
     manifest,
     metadata,
     service_checks,
+    agent_reqs,
 )
