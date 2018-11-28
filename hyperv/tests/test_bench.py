667a1a55484441cded312dc35565b030c191e3c3
Ofek Lev <ofekmeister@gmail.com>
2018-11-28 00:43:10 -0500

    more benchmarks

--- hyperv/tests/test_bench.py
+++ hyperv/tests/test_bench.py
@@ -1,10 +1,25 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from copy import deepcopy
+
 from datadog_checks.hyperv import HypervCheck
 
 
-def test_run(benchmark, instance):
+def test_refresh_counters(benchmark, instance):
+    instance = deepcopy(instance)
+    instance['refresh_counters'] = True
+    check = HypervCheck('hyperv', {}, {}, [instance])
+
+    # Run once to get any PDH setup out of the way.
+    check.check(instance)
+
+    benchmark(check.check, instance)
+
+
+def test_no_refresh_counters(benchmark, instance):
+    instance = deepcopy(instance)
+    instance['refresh_counters'] = False
     check = HypervCheck('hyperv', {}, {}, [instance])
 
     # Run once to get any PDH setup out of the way.
