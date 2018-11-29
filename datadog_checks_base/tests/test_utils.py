4f47b30f7e4106909cf12a460566f9a005cb929a
Ofek Lev <ofek.lev@datadoghq.com>
2018-11-29 15:43:44 -0500

    Update test_utils.py

--- datadog_checks_base/tests/test_utils.py
+++ datadog_checks_base/tests/test_utils.py
@@ -4,7 +4,6 @@
 
 from datadog_checks.utils.common import pattern_filter
 from datadog_checks.utils.limiter import Limiter
-from datadog_checks.utils.null_context_manager import NullContextManager
 
 
 class Item:
@@ -106,9 +105,3 @@ class TestLimiter():
         assert limiter.get_status() == (0, 10, False)
         assert limiter.is_reached("dummy1") is False
         assert limiter.get_status() == (1, 10, False)
-
-
-class TestNullContextManager():
-    def test_thing(self):
-        with NullContextManager("dummy") as dummy:
-            assert dummy == "dummy"
