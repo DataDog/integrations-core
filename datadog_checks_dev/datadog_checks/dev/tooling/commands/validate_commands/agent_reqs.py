d91a59ef28575d7bb01a7fdd5b0de9ecea5160f1
Ofek Lev <ofekmeister@gmail.com>
2018-11-29 21:42:10 -0500

    Handle unreleased checks for agent reqs validation (#2664)
    * Handle unreleased checks for agent reqs validation
    
    * change end echo
    
    * here too

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate_commands/agent_reqs.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate_commands/agent_reqs.py
@@ -4,7 +4,7 @@
 import click
 import os
 
-from ..utils import CONTEXT_SETTINGS, abort, echo_failure, echo_info, echo_success
+from ..utils import CONTEXT_SETTINGS, abort, echo_failure, echo_info, echo_success, echo_warning
 from ...constants import get_root, AGENT_REQ_FILE, AGENT_V5_ONLY, NOT_CHECKS
 from ...utils import get_valid_checks, parse_agent_req_file, get_version_string
 from ...release import get_package_name
@@ -22,24 +22,29 @@ def agent_reqs():
     root = get_root()
     echo_info("Validating requirements-agent-release.txt...")
     agent_reqs_content = parse_agent_req_file(read_file(os.path.join(root, AGENT_REQ_FILE)))
-    failed_checks = 0
     ok_checks = 0
+    unreleased_checks = 0
+    failed_checks = 0
     for check_name in get_valid_checks():
         if check_name not in AGENT_V5_ONLY | NOT_CHECKS:
+            package_name = get_package_name(check_name)
             check_version = get_version_string(check_name)
-            pinned_version = agent_reqs_content.get(get_package_name(check_name))
-            if check_version != pinned_version:
+            pinned_version = agent_reqs_content.get(package_name)
+            if package_name not in agent_reqs_content:
+                unreleased_checks += 1
+                echo_warning('{} has not yet been released'.format(check_name))
+            elif check_version != pinned_version:
                 failed_checks += 1
-                if pinned_version:
-                    echo_failure("{} has version {} but is pinned to {}".format(
-                        check_name, check_version,
-                    ))
-                else:
-                    echo_failure("{} has version {} but is not pinned".format(check_name, check_version))
+                echo_failure("{} has version {} but is pinned to {}".format(
+                    check_name, check_version, pinned_version
+                ))
             else:
                 ok_checks += 1
+
     if ok_checks:
         echo_success("{} correctly pinned checks".format(ok_checks))
+    if unreleased_checks:
+        echo_warning("{} unreleased checks".format(unreleased_checks))
     if failed_checks:
         echo_failure("{} checks out of sync".format(failed_checks))
         abort()
