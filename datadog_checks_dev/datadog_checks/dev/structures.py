750ce9096592a63a43a677810a6eb40f5bdb3e6f
Ofek Lev <ofekmeister@gmail.com>
2018-12-13 02:57:11 -0500

    Add ability to pass state to e2e tear down (#2724)

--- datadog_checks_dev/datadog_checks/dev/structures.py
+++ datadog_checks_dev/datadog_checks/dev/structures.py
@@ -3,8 +3,12 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import abc
 import os
+from shutil import rmtree
+from tempfile import mkdtemp
 
 import six
+
+from ._env import e2e_active, get_env_vars, remove_env_vars, set_env_vars, tear_down_env
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -35,3 +39,45 @@ class EnvVars(dict):
     def __exit__(self, exc_type, exc_value, traceback):
         os.environ.clear()
         os.environ.update(self.old_env)
+
+
+class TempDir(object):
+    all_names = set()
+
+    def __init__(self, name='default'):
+        key = None
+        directory = None
+
+        if e2e_active():
+            name = name.lower()
+
+            if name in TempDir.all_names:
+                raise Exception(
+                    'Temporary directory name {} has already been used, choose a different one.'.format(name)
+                )
+            TempDir.all_names.add(name)
+
+            key = 'temp_dir_{}'.format(name)
+            env_vars = get_env_vars()
+
+            if key in env_vars:
+                directory = env_vars[key]
+            else:
+                directory = mkdtemp()
+                set_env_vars({key: directory})
+
+        self.name = name
+        self.key = key
+        self.directory = os.path.realpath(directory or mkdtemp())
+
+    def __enter__(self):
+        return self.directory
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if e2e_active():
+            if tear_down_env():
+                TempDir.all_names.discard(self.name)
+                remove_env_vars([self.key])
+                rmtree(self.directory)
+        else:
+            rmtree(self.directory)
