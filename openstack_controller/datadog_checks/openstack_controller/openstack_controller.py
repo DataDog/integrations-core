8828b5abadb2bdf62fe467ca75666591d9645519
Gregory <gregory.zussa@datadoghq.com>
2018-12-13 22:58:53 +0100

    Fixing NoneType error arount hypervisor hostname

--- openstack_controller/datadog_checks/openstack_controller/openstack_controller.py
+++ openstack_controller/datadog_checks/openstack_controller/openstack_controller.py
@@ -270,16 +270,14 @@ class OpenStackControllerCheck(AgentCheck):
         Submits stats for all hypervisors registered to this control plane
         Raises specific exceptions based on response code
         """
-        resp = self.get_os_hypervisors_detail()
-        hypervisors = resp.get('hypervisors', [])
+        hypervisors = self.get_os_hypervisors_detail()
         for hyp in hypervisors:
             self.get_stats_for_single_hypervisor(hyp, custom_tags=custom_tags,
                                                  use_shortname=use_shortname,
                                                  collect_hypervisor_metrics=collect_hypervisor_metrics,
                                                  collect_hypervisor_load=collect_hypervisor_load)
-
-        if not hypervisors:
-            self.log.warn("Unable to collect any hypervisors from Nova response: {}".format(resp))
+        else:
+            self.log.warn("Unable to collect any hypervisors from Nova response.")
 
     def get_stats_for_single_hypervisor(self, hyp, custom_tags=None,
                                         use_shortname=False,
@@ -796,6 +794,9 @@ class OpenStackControllerCheck(AgentCheck):
 
     def _get_host_aggregate_tag(self, hyp_hostname, use_shortname=False):
         tags = []
+        if not hyp_hostname:
+            return tags
+
         hyp_hostname = hyp_hostname.split('.')[0] if use_shortname else hyp_hostname
         if hyp_hostname in self._get_and_set_aggregate_list():
             tags.append('aggregate:{}'.format(self._aggregate_list[hyp_hostname].get('aggregate', "unknown")))
