201158513c218acefb2300addf6787596e0e443f
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2018-12-13 14:18:01 +0100

    use subprocess

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/py3.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/py3.py
@@ -3,7 +3,7 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import click
 import os
-from pylint import epylint as lint
+import subprocess
 
 from ..utils import CONTEXT_SETTINGS, abort, echo_failure, echo_info, echo_success, echo_warning
 from ...constants import get_root, NOT_CHECKS
@@ -25,24 +25,24 @@ def py3(check):
     elif check in get_valid_checks() and check not in NOT_CHECKS:
         path_to_module = os.path.join(root, check, 'datadog_checks', check)
     else:
-        if not os.path.exists(check):
-            abort("{} does not exist.".format(check))
         path_to_module = check
+
+    if not os.path.exists(path_to_module):
+        abort("{} does not exist.".format(path_to_module))
+
     echo_info("Validating python3 compatibility of {}".format(check))
-    out, _ = lint.py_run("{} --py3k --disable=no-absolute-import".format(path_to_module), return_std=True)
-    lines = out.read().strip().splitlines()
-    if len(lines) == 2:
-        # No errors found by pylint
-        echo_success("{} is compatible with python3".format(check))
-    elif len(lines) == 0:
-        # No python files found by pylint
-        echo_warning("Pylint found no python files to check for {}.".format(check))
-        echo_warning("Be sure to specify the name of an integration, or a path to a python module")
-    else:
+    try:
+        # Disable `no-absolute-import`, which checks for a behaviour that's already part of python 2.7
+        # cf https://www.python.org/dev/peps/pep-0328/
+        out = subprocess.check_output(["pylint", path_to_module, "--py3k", "--disable=no-absolute-import"])
+    except subprocess.CalledProcessError as e:
         echo_failure("Incompatibilities were found for {}:".format(check))
         # The last 3 lines are for the pylint score
-        for line in lines[:-3]:
+        for line in e.output.splitlines():
             line = line.strip()
+            if line.startswith("----"):
+                # No more errors
+                break
             if line.startswith("*************"):
                 # Line for the module where the issue is found
                 echo_info("  {}".format(line))
@@ -50,3 +50,10 @@ def py3(check):
                 # Line describing the issue
                 echo_failure("  {}".format(line))
         abort()
+
+    if len(out.strip()) == 0:
+        # No python files found by pylint
+        echo_warning("Pylint found no python files to check for {}.".format(check))
+        echo_warning("Be sure to specify the name of an integration, or a path to a python module")
+    else:
+        echo_success("{} is compatible with python3".format(check))
