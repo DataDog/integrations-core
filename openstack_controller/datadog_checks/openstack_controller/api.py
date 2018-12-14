3b5059ee06f62687465c6582a9c5f497f2aae760
Gregory <gregory.zussa@datadoghq.com>
2018-12-14 15:15:23 +0100

    clean up

--- openstack_controller/datadog_checks/openstack_controller/api.py
+++ openstack_controller/datadog_checks/openstack_controller/api.py
@@ -5,7 +5,7 @@ import requests
 import simplejson as json
 from six.moves.urllib.parse import urljoin
 from .settings import (DEFAULT_API_REQUEST_TIMEOUT, DEFAULT_KEYSTONE_API_VERSION, DEFAULT_NEUTRON_API_VERSION,
-                       DEFAULT_PAGINATED_LIMIT)
+                       DEFAULT_PAGINATED_LIMIT, DEFAULT_RETRY)
 from .exceptions import (InstancePowerOffFailure, AuthenticationNeeded, KeystoneUnreachable)
 
 
@@ -121,9 +121,9 @@ class ComputeApi(AbstractApi):
             query_params['marker'] = resp[-1]['id']
             query_params['limit'] = self.paginated_limit
             retry = 0
-            while retry < 3:
+            while retry < DEFAULT_RETRY:
                 # get_flavors_detail is an expensive call, if it fails,
-                # If it fails, we retry 3 times while reducing the `limit` param
+                # If it fails, we retry DEFAULT_RETRY times while reducing the `limit` param
                 # otherwise we will backoff
                 try:
                     resp = self._make_request(url, self.headers, params=query_params)
@@ -133,7 +133,7 @@ class ComputeApi(AbstractApi):
                 except Exception as e:
                     query_params['limit'] /= 2
                     retry += 1
-                    if retry == 3:
+                    if retry == DEFAULT_RETRY:
                         raise e
 
         return result
