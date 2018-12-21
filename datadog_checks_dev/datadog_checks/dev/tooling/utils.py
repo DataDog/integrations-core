541d26d0def0041a7ada12d57c9c74e7c4340902
Massimiliano Pippi <mpippi@gmail.com>
2018-12-21 11:40:35 +0100

    fix example in docstring

--- datadog_checks_dev/datadog_checks/dev/tooling/utils.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/utils.py
@@ -163,7 +163,7 @@ def parse_agent_req_file(contents):
     Returns a dictionary mapping {check-package-name --> pinned_version} from the
     given file contents. We can assume lines are in the form:
 
-        active-directory==1.1.1; sys_platform == 'win32'
+        datadog-active-directory==1.1.1; sys_platform == 'win32'
 
     """
     catalog = OrderedDict()
