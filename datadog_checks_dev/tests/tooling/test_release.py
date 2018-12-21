4c478e11b31d90b0edc5cf5a8d72d26b93e04d35
Massimiliano Pippi <mpippi@gmail.com>
2018-12-21 11:40:14 +0100

    remove unused import

--- datadog_checks_dev/tests/tooling/test_release.py
+++ datadog_checks_dev/tests/tooling/test_release.py
@@ -1,8 +1,6 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-import pytest
-
 from datadog_checks.dev.tooling.release import get_package_name, get_folder_name
 
 
