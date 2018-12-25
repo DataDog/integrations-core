5a74bebf65d11f8aa7f0dbbf2ef38f2b4a4bae63
Ofek Lev <ofekmeister@gmail.com>
2018-12-24 21:38:48 -0500

    Support Kerberos auth

--- hdfs_namenode/datadog_checks/hdfs_namenode/hdfs_namenode.py
+++ hdfs_namenode/datadog_checks/hdfs_namenode/hdfs_namenode.py
@@ -1,46 +1,22 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import os
 
-
-"""
-HDFS NameNode Metrics
----------------------
-hdfs.namenode.capacity_total                    Total disk capacity in bytes
-hdfs.namenode.capacity_used                     Disk usage in bytes
-hdfs.namenode.capacity_remaining                Remaining disk space left in bytes
-hdfs.namenode.total_load                        Total load on the file system
-hdfs.namenode.fs_lock_queue_length              Lock queue length
-hdfs.namenode.blocks_total                      Total number of blocks
-hdfs.namenode.max_objects                       Maximum number of files HDFS supports
-hdfs.namenode.files_total                       Total number of files
-hdfs.namenode.pending_replication_blocks        Number of blocks pending replication
-hdfs.namenode.under_replicated_blocks           Number of under replicated blocks
-hdfs.namenode.scheduled_replication_blocks      Number of blocks scheduled for replication
-hdfs.namenode.pending_deletion_blocks           Number of pending deletion blocks
-hdfs.namenode.num_live_data_nodes               Total number of live data nodes
-hdfs.namenode.num_dead_data_nodes               Total number of dead data nodes
-hdfs.namenode.num_decom_live_data_nodes         Number of decommissioning live data nodes
-hdfs.namenode.num_decom_dead_data_nodes         Number of decommissioning dead data nodes
-hdfs.namenode.volume_failures_total             Total volume failures
-hdfs.namenode.estimated_capacity_lost_total     Estimated capacity lost in bytes
-hdfs.namenode.num_decommissioning_data_nodes    Number of decommissioning data nodes
-hdfs.namenode.num_stale_data_nodes              Number of stale data nodes
-hdfs.namenode.num_stale_storages                Number of stale storages
-hdfs.namenode.missing_blocks                    Number of missing blocks
-hdfs.namenode.corrupt_blocks                    Number of corrupt blocks
-"""
-
-# stdlib
-from urlparse import urljoin
-
-# 3rd party
 import requests
+import requests_kerberos
 from requests.exceptions import Timeout, HTTPError, InvalidURL, ConnectionError
 from simplejson import JSONDecodeError
+from six import iteritems
+from six.moves.urllib.parse import urljoin
 
-# Project
-from datadog_checks.checks import AgentCheck
+from datadog_checks.base import AgentCheck, is_affirmative
+
+KERBEROS_STRATEGIES = {
+    'required': requests_kerberos.REQUIRED,
+    'optional': requests_kerberos.OPTIONAL,
+    'disabled': requests_kerberos.DISABLED,
+}
 
 
 class HDFSNameNode(AgentCheck):
@@ -99,20 +75,11 @@ class HDFSNameNode(AgentCheck):
         tags.append("namenode_url:{}".format(jmx_address))
         tags = list(set(tags))
 
-        # Authenticate our connection to JMX endpoint if required
-        username = instance.get('username')
-        password = instance.get('password')
-        auth = None
-        if username is not None and password is not None:
-            auth = (username, password)
-
-        # Get data from JMX
-        disable_ssl_validation = instance.get('disable_ssl_validation', False)
         hdfs_system_state_beans = self._get_jmx_data(
-            jmx_address, auth, disable_ssl_validation, self.HDFS_NAME_SYSTEM_STATE_BEAN, tags
+            instance, jmx_address, self.HDFS_NAME_SYSTEM_STATE_BEAN, tags
         )
         hdfs_system_beans = self._get_jmx_data(
-            jmx_address, auth, disable_ssl_validation, self.HDFS_NAME_SYSTEM_BEAN, tags
+            instance, jmx_address, self.HDFS_NAME_SYSTEM_BEAN, tags
         )
 
         # Process the JMX data and send out metrics
@@ -130,13 +97,13 @@ class HDFSNameNode(AgentCheck):
             message="Connection to {} was successful".format(jmx_address),
         )
 
-    def _get_jmx_data(self, jmx_address, auth, disable_ssl_validation, bean_name, tags):
+    def _get_jmx_data(self, instance, jmx_address, bean_name, tags):
         """
         Get namenode beans data from JMX endpoint
         """
 
         response = self._rest_request_to_json(
-            jmx_address, auth, disable_ssl_validation, self.JMX_PATH, {"qry": bean_name}, tags=tags
+            instance, jmx_address, self.JMX_PATH, {"qry": bean_name}, tags=tags
         )
         beans = response.get("beans", [])
         return beans
@@ -151,7 +118,7 @@ class HDFSNameNode(AgentCheck):
         if bean_name != bean_name:
             raise Exception("Unexpected bean name {}".format(bean_name))
 
-        for metric, (metric_name, metric_type) in metrics.iteritems():
+        for metric, (metric_name, metric_type) in iteritems(metrics):
             metric_value = bean.get(metric)
 
             if metric_value is not None:
@@ -174,20 +141,44 @@ class HDFSNameNode(AgentCheck):
         else:
             self.log.error('Metric type "{}" unknown'.format(metric_type))
 
-    def _rest_request_to_json(self, address, auth, disable_ssl_validation, object_path, query_params, tags=None):
+    def _rest_request_to_json(self, instance, url, object_path, query_params, tags=None):
         """
         Query the given URL and return the JSON response
         """
-        response_json = None
-        url = address
-
         if object_path:
             url = self._join_url_dir(url, object_path)
 
         # Add query_params as arguments
         if query_params:
-            query = '&'.join(['{}={}'.format(key, value) for key, value in query_params.iteritems()])
+            query = '&'.join(['{}={}'.format(key, value) for key, value in iteritems(query_params)])
             url = urljoin(url, '?' + query)
+
+        auth = None
+
+        # Authenticate our connection to JMX endpoint if required
+        kerberos = instance.get('kerberos')
+        username = instance.get('username')
+        password = instance.get('password')
+        if username is not None and password is not None:
+            auth = (username, password)
+        elif kerberos is not None:
+            if kerberos not in KERBEROS_STRATEGIES:
+                raise Exception('Invalid Kerberos strategy `{}`'.format(kerberos))
+
+            auth = requests_kerberos.HTTPKerberosAuth(
+                mutual_authentication=KERBEROS_STRATEGIES[kerberos],
+                delegate=is_affirmative(instance.get('kerberos_delegate', False)),
+                force_preemptive=is_affirmative(instance.get('kerberos_force_initiate', False)),
+                hostname_override=instance.get('kerberos_hostname'),
+                principal=instance.get('kerberos_principal')
+            )
+
+        disable_ssl_validation = instance.get('disable_ssl_validation', False)
+
+        old_keytab_path = None
+        if 'kerberos_keytab' in instance:
+            old_keytab_path = os.getenv('KRB5_CLIENT_KTNAME')
+            os.environ['KRB5_CLIENT_KTNAME'] = instance['kerberos_keytab']
 
         self.log.debug('Attempting to connect to "{}"'.format(url))
 
@@ -223,9 +214,15 @@ class HDFSNameNode(AgentCheck):
             self.service_check(self.JMX_SERVICE_CHECK, AgentCheck.CRITICAL, tags=tags, message=str(e))
             raise
 
-        return response_json
+        else:
+            return response_json
 
-    def _join_url_dir(self, url, *args):
+        finally:
+            if old_keytab_path is not None:
+                os.environ['KRB5_CLIENT_KTNAME'] = old_keytab_path
+
+    @classmethod
+    def _join_url_dir(cls, url, *args):
         """
         Join a URL with multiple directories
         """
