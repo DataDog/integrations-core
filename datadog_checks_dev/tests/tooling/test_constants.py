9e1a2c2c11b79a8635c5df06b9a6bd34dcd39a81
Massimiliano Pippi <mpippi@gmail.com>
2019-01-04 00:16:36 +0100

    add functions to get full path to notable files in the repo

--- datadog_checks_dev/tests/tooling/test_constants.py
+++ datadog_checks_dev/tests/tooling/test_constants.py
@@ -5,7 +5,8 @@ import os
 import mock
 
 from datadog_checks.dev.tooling.constants import (
-    get_agent_release_requirements, get_agent_requirements, get_root, set_root
+    get_agent_release_requirements, get_agent_requirements, get_root, set_root,
+    get_agent_changelog, get_agent_integrations_file
 )
 
 
@@ -21,6 +22,22 @@ def test_get_agent_requirements():
             'foo', 'datadog_checks_base', 'datadog_checks', 'base', 'data', 'agent_requirements.in'
         )
         assert get_agent_requirements() == expected
+
+
+def test_get_agent_integrations_file():
+    with mock.patch('datadog_checks.dev.tooling.constants.get_root', return_value='foo'):
+        expected = os.path.join(
+            'foo', 'AGENT_INTEGRATIONS.md'
+        )
+        assert get_agent_integrations_file() == expected
+
+
+def test_get_agent_changelog():
+    with mock.patch('datadog_checks.dev.tooling.constants.get_root', return_value='foo'):
+        expected = os.path.join(
+            'foo', 'AGENT_CHANGELOG.md'
+        )
+        assert get_agent_changelog() == expected
 
 
 def test_get_root():
