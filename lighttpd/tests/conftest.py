babaddc3793c8acad9a7b6a48081d754626e453a
Gregory <gregory.zussa@datadoghq.com>
2019-01-11 17:01:38 -0500

    Incorporate Feedback

--- lighttpd/tests/conftest.py
+++ lighttpd/tests/conftest.py
@@ -8,29 +8,32 @@ from six.moves.urllib.request import urlopen
 from six.moves.urllib import error
 from copy import deepcopy
 
-from datadog_checks.dev import docker_run
+from datadog_checks.dev import docker_run, WaitFor
 from datadog_checks.lighttpd import Lighttpd
 
 from . import common
 
 
+def wait_for_lighttpd():
+    attempts = 0
+    while True:
+        if attempts > 10:
+            return False
+
+        try:
+            urlopen(common.STATUS_URL).read()
+        except error.HTTPError:
+            # endpoint is secured, we do expect 401
+            break
+        except error.URLError:
+            attempts += 1
+            time.sleep(1)
+    return True
+
+
 @pytest.fixture(scope="session")
 def dd_environment():
-    with docker_run(common.COMPOSE_FILE):
-        attempts = 0
-        while True:
-            if attempts > 10:
-                raise Exception("lighttpd boot timed out!")
-
-            try:
-                urlopen(common.STATUS_URL).read()
-            except error.HTTPError:
-                # endpoint is secured, we do expect 401
-                break
-            except error.URLError:
-                attempts += 1
-                time.sleep(1)
-
+    with docker_run(common.COMPOSE_FILE, conditions=[WaitFor(wait_for_lighttpd)]):
         instance = deepcopy(common.INSTANCE)
         yield instance
 
