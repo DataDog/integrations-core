fbeb34dcb2867cd6c996e51145db6d33f6e4b21c
Gregory <gregory.zussa@datadoghq.com>
2019-01-14 16:56:07 -0500

    incorporate feedback

--- squid/tests/conftest.py
+++ squid/tests/conftest.py
@@ -2,37 +2,21 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 import os
-import sys
-import time
 import pytest
-import requests
 
 from copy import deepcopy
 
-from datadog_checks.dev import WaitFor, docker_run
+from datadog_checks.dev import docker_run
 from datadog_checks.squid import SquidCheck
 
 from . import common
 
 
-def wait_for_squid():
-    try:
-        res = requests.get(common.URL)
-        res.raise_for_status()
-        return True
-    except Exception:
-        time.sleep(1)
-        sys.stderr.write("Waiting for Squid to boot...")
-        return False
-
-
 @pytest.fixture(scope='session')
 def dd_environment():
-    env = os.environ
     with docker_run(
-            compose_file=os.path.join(common.HERE, 'compose', 'squid.yaml'),
-            env_vars=env,
-            conditions=[WaitFor(wait_for_squid)],
+        compose_file=os.path.join(common.HERE, 'compose', 'squid.yaml'),
+        endpoints=[common.URL],
     ):
         yield common.CHECK_CONFIG
 
