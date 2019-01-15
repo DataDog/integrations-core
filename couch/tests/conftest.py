a67ee267a619f757ce8c85773cfce48500a518fc
Gregory <gregory.zussa@datadoghq.com>
2019-01-15 10:51:21 -0500

    fix flake

--- couch/tests/conftest.py
+++ couch/tests/conftest.py
@@ -10,7 +10,7 @@ from time import sleep
 from collections import defaultdict
 from copy import deepcopy
 
-from datadog_checks.dev import WaitFor, docker_run
+from datadog_checks.dev import docker_run
 from datadog_checks.couch import CouchDb
 
 from . import common
