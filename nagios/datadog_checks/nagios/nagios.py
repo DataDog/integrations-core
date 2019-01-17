470a373d88dfc6e47ff4779062144ec32df0e0c1
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-01-17 18:39:13 -0500

    Import division

--- nagios/datadog_checks/nagios/nagios.py
+++ nagios/datadog_checks/nagios/nagios.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from __future__ import division
 
 from collections import namedtuple
 import json
