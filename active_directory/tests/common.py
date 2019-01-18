2bc0a171c7b623e993560a365567f626579a76d7
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-01-18 13:34:11 -0500

    Support Python 3 (#2975)

---
+++ active_directory/tests/common.py
@@ -0,0 +1,8 @@
+# (C) Datadog, Inc. 2010-2016
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+MINIMAL_INSTANCE = {
+    'host': '.',
+}
+
+CHECK_NAME = 'active_directory'
