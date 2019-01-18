b7ac34de668469a08a459df83a6f9d70ba16385b
Tyler Benson <tyler.benson@datadoghq.com>
2019-01-18 11:30:18 -0500

    Rename to jboss_wildfly

--- jboss_wildfly/datadog_checks/jboss_wildfly/__init__.py
+++ jboss_wildfly/datadog_checks/jboss_wildfly/__init__.py
@@ -2,9 +2,9 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
-from .jboss import JbossCheck
+from .jboss_wildfly import JBossWildflyCheck
 
 __all__ = [
     '__version__',
-    'JbossCheck'
+    'JBossWildflyCheck'
 ]
