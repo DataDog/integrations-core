30e97d72c0f7255202e09ae974ae8fd8b7f7900d
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2019-01-18 14:38:17 -0500

    Add e2e support

--- activemq_xml/tests/conftest.py
+++ activemq_xml/tests/conftest.py
@@ -3,28 +3,20 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 import os
-import subprocess
-from shutil import rmtree
 import stat
 import tarfile
 import tempfile
-from time import sleep
+from shutil import rmtree
 
 import pytest
-import requests
 
-from .common import HERE, URL
+from datadog_checks.dev import docker_run
 
-
-@pytest.fixture
-def aggregator():
-    from datadog_checks.stubs import aggregator
-    aggregator.reset()
-    return aggregator
+from .common import HERE, URL, CONFIG
 
 
 @pytest.fixture(scope="session", autouse=True)
-def activemq_xml_container():
+def dd_environment():
     # use os.path.realpath to avoid mounting issues of symlinked /var -> /private/var in Docker on macOS
     tmp_dir = os.path.realpath(tempfile.mkdtemp())
     activemq_data_dir = os.path.join(tmp_dir, "data")
@@ -37,40 +29,11 @@ def activemq_xml_container():
     os.chmod(os.path.join(activemq_data_dir, "kahadb", "db.data"), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
     os.chmod(os.path.join(activemq_data_dir, "kahadb", "db.redo"), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
 
-    env = os.environ
-    env["ACTIVEMQ_DATA_DIR"] = activemq_data_dir
-
-    args = [
-        "docker-compose",
-        "-f", os.path.join(HERE, 'compose', 'docker-compose.yaml')
-    ]
-
-    subprocess.check_call(args + ["down"])
-    subprocess.check_call(args + ["up", "-d"])
-
-    # wait for the cluster to be up before yielding
-    if not wait_for_container():
-        subprocess.check_call(args + ["logs"])
-        subprocess.check_call(args + ["down"])
-        raise Exception("activemq_xml container boot timed out!")
-
-    yield
+    with docker_run(
+        compose_file=os.path.join(HERE, 'compose', 'docker-compose.yaml'),
+        env_vars={"ACTIVEMQ_DATA_DIR": activemq_data_dir},
+        endpoints=URL,
+    ):
+        yield CONFIG
 
     rmtree(tmp_dir, ignore_errors=True)
-    subprocess.check_call(args + ["down"])
-
-
-def wait_for_container():
-    """
-    Wait for the activemq_xml container to be reachable
-    """
-    for i in range(30):
-        print("Waiting for service to come up")
-        try:
-            requests.get(URL).raise_for_status()
-            return True
-        except Exception as e:
-            print(e)
-            sleep(1)
-
-    return False
