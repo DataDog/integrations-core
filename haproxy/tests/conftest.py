2334ff6048cd00df324ba5f0d18227d2c7fe40a4
Gregory <gregory.zussa@datadoghq.com>
2019-01-21 17:08:57 +0100

    Incorporate feedback

--- haproxy/tests/conftest.py
+++ haproxy/tests/conftest.py
@@ -2,7 +2,6 @@ import pytest
 import os
 import subprocess
 import requests
-import time
 import logging
 import mock
 import getpass
@@ -10,17 +9,17 @@ import getpass
 from copy import deepcopy
 
 from datadog_checks.dev import TempDir, WaitFor, docker_run
-from datadog_checks.dev.utils import create_file, file_exists
 from datadog_checks.haproxy import HAProxy
 
-from .common import HERE, CHECK_CONFIG, USERNAME, PASSWORD, STATS_URL, STATS_URL_OPEN
+from .common import HERE, CHECK_CONFIG, USERNAME, PASSWORD, STATS_URL, STATS_URL_OPEN, platform_supports_sockets
 
 log = logging.getLogger('test_haproxy')
 
 
 def wait_for_haproxy():
-    res = requests.get(STATS_URL, auth=(USERNAME, PASSWORD))
-    res.raise_for_status()
+    if platform_supports_sockets:
+        res = requests.get(STATS_URL, auth=(USERNAME, PASSWORD))
+        res.raise_for_status()
 
     res_open = requests.get(STATS_URL_OPEN)
     res_open.raise_for_status()
@@ -29,37 +28,45 @@ def wait_for_haproxy():
 @pytest.fixture(scope='session')
 def dd_environment():
     env = {}
-    with TempDir() as temp_dir:
-        host_socket_path = os.path.join(temp_dir, 'datadog-haproxy-stats.sock')
-
-        env['HAPROXY_CONFIG_DIR'] = os.path.join(HERE, 'compose')
-        env['HAPROXY_CONFIG'] = os.path.join(HERE, 'compose', 'haproxy.cfg')
-        if os.environ.get('HAPROXY_VERSION', '1.5.11').split('.')[:2] >= ['1', '7']:
-            env['HAPROXY_CONFIG'] = os.path.join(HERE, 'compose', 'haproxy-1_7.cfg')
-        env['HAPROXY_CONFIG_OPEN'] = os.path.join(HERE, 'compose', 'haproxy-open.cfg')
-        env['HAPROXY_SOCKET_DIR'] = temp_dir
-
+    env['HAPROXY_CONFIG_DIR'] = os.path.join(HERE, 'compose')
+    env['HAPROXY_CONFIG_OPEN'] = os.path.join(HERE, 'compose', 'haproxy-open.cfg')
+    if not platform_supports_sockets:
         with docker_run(
             compose_file=os.path.join(HERE, 'compose', 'haproxy.yaml'),
             env_vars=env,
+            service_name="haproxy-open",
             conditions=[WaitFor(wait_for_haproxy)],
         ):
-            try:
-                # on linux this needs access to the socket
-                # it won't work without access
-                chown_args = []
-                user = getpass.getuser()
-                if user != 'root':
-                    chown_args += ['sudo']
-                chown_args += ["chown", user, host_socket_path]
-                subprocess.check_call(chown_args, env=env)
-            except subprocess.CalledProcessError:
-                # it's not always bad if this fails
-                pass
-            config = deepcopy(CHECK_CONFIG)
-            unixsocket_url = 'unix://{0}'.format(host_socket_path)
-            config['unixsocket_url'] = unixsocket_url
-            yield config
+            yield deepcopy(CHECK_CONFIG)
+    else:
+        with TempDir() as temp_dir:
+            host_socket_path = os.path.join(temp_dir, 'datadog-haproxy-stats.sock')
+            env['HAPROXY_CONFIG'] = os.path.join(HERE, 'compose', 'haproxy.cfg')
+            if os.environ.get('HAPROXY_VERSION', '1.5.11').split('.')[:2] >= ['1', '7']:
+                env['HAPROXY_CONFIG'] = os.path.join(HERE, 'compose', 'haproxy-1_7.cfg')
+            env['HAPROXY_SOCKET_DIR'] = temp_dir
+
+            with docker_run(
+                compose_file=os.path.join(HERE, 'compose', 'haproxy.yaml'),
+                env_vars=env,
+                conditions=[WaitFor(wait_for_haproxy)],
+            ):
+                try:
+                    # on linux this needs access to the socket
+                    # it won't work without access
+                    chown_args = []
+                    user = getpass.getuser()
+                    if user != 'root':
+                        chown_args += ['sudo']
+                    chown_args += ["chown", user, host_socket_path]
+                    subprocess.check_call(chown_args, env=env)
+                except subprocess.CalledProcessError:
+                    # it's not always bad if this fails
+                    pass
+                config = deepcopy(CHECK_CONFIG)
+                unixsocket_url = 'unix://{0}'.format(host_socket_path)
+                config['unixsocket_url'] = unixsocket_url
+                yield config
 
 
 @pytest.fixture
--- haproxy/tests/test_haproxy.py
+++ haproxy/tests/test_haproxy.py
@@ -6,26 +6,29 @@ import pytest
 import os
 import copy
 
-from datadog_checks.utils.platform import Platform
 from datadog_checks.haproxy import HAProxy
 
-from . import common
+from .common import (BACKEND_SERVICES, BACKEND_LIST, BACKEND_TO_ADDR, BACKEND_CHECK_GAUGES, FRONTEND_CHECK_GAUGES,
+                     FRONTEND_CHECK_GAUGES_POST_1_4, BACKEND_CHECK_GAUGES_POST_1_5, BACKEND_CHECK_GAUGES_POST_1_7,
+                     FRONTEND_CHECK_RATES, FRONTEND_CHECK_RATES_POST_1_4, BACKEND_CHECK_RATES_POST_1_4,
+                     BACKEND_CHECK_RATES, requires_socket_support, SERVICE_CHECK_NAME, STATS_URL, CHECK_CONFIG_OPEN,
+                     STATS_URL_OPEN, CONFIG_TCPSOCKET, STATS_SOCKET, CONFIG_UNIXSOCKET)
 
 
 def _test_frontend_metrics(aggregator, shared_tag):
     frontend_tags = shared_tag + ['type:FRONTEND', 'service:public']
-    for gauge in common.FRONTEND_CHECK_GAUGES:
+    for gauge in FRONTEND_CHECK_GAUGES:
         aggregator.assert_metric(gauge, tags=frontend_tags, count=1)
 
     if os.environ.get('HAPROXY_VERSION', '1.5.11').split('.')[:2] >= ['1', '4']:
-        for gauge in common.FRONTEND_CHECK_GAUGES_POST_1_4:
+        for gauge in FRONTEND_CHECK_GAUGES_POST_1_4:
             aggregator.assert_metric(gauge, tags=frontend_tags, count=1)
 
-    for rate in common.FRONTEND_CHECK_RATES:
+    for rate in FRONTEND_CHECK_RATES:
         aggregator.assert_metric(rate, tags=frontend_tags, count=1)
 
     if os.environ.get('HAPROXY_VERSION', '1.5.11').split('.')[:2] >= ['1', '4']:
-        for rate in common.FRONTEND_CHECK_RATES_POST_1_4:
+        for rate in FRONTEND_CHECK_RATES_POST_1_4:
             aggregator.assert_metric(rate, tags=frontend_tags, count=1)
 
 
@@ -33,57 +36,58 @@ def _test_backend_metrics(aggregator, shared_tag, services=None, add_addr_tag=Fa
     backend_tags = shared_tag + ['type:BACKEND']
     haproxy_version = os.environ.get('HAPROXY_VERSION', '1.5.11').split('.')[:2]
     if not services:
-        services = common.BACKEND_SERVICES
+        services = BACKEND_SERVICES
     for service in services:
-        for backend in common.BACKEND_LIST:
+        for backend in BACKEND_LIST:
             tags = backend_tags + ['service:' + service, 'backend:' + backend]
 
             if add_addr_tag and haproxy_version >= ['1', '7']:
                 tags.append('server_address:{}'.format(
-                    common.BACKEND_TO_ADDR[backend]))
+                    BACKEND_TO_ADDR[backend]))
 
-            for gauge in common.BACKEND_CHECK_GAUGES:
+            for gauge in BACKEND_CHECK_GAUGES:
                 aggregator.assert_metric(gauge, tags=tags, count=1)
 
             if haproxy_version >= ['1', '5']:
-                for gauge in common.BACKEND_CHECK_GAUGES_POST_1_5:
+                for gauge in BACKEND_CHECK_GAUGES_POST_1_5:
                     aggregator.assert_metric(gauge, tags=tags, count=1)
 
             if haproxy_version >= ['1', '7']:
-                for gauge in common.BACKEND_CHECK_GAUGES_POST_1_7:
+                for gauge in BACKEND_CHECK_GAUGES_POST_1_7:
                     aggregator.assert_metric(gauge, tags=tags, count=1)
 
-            for rate in common.BACKEND_CHECK_RATES:
+            for rate in BACKEND_CHECK_RATES:
                 aggregator.assert_metric(rate, tags=tags, count=1)
 
             if haproxy_version >= ['1', '4']:
-                for rate in common.BACKEND_CHECK_RATES_POST_1_4:
+                for rate in BACKEND_CHECK_RATES_POST_1_4:
                     aggregator.assert_metric(rate, tags=tags, count=1)
 
 
 def _test_service_checks(aggregator, services=None):
     if not services:
-        services = common.BACKEND_SERVICES
+        services = BACKEND_SERVICES
     for service in services:
-        for backend in common.BACKEND_LIST:
+        for backend in BACKEND_LIST:
             tags = ['service:' + service, 'backend:' + backend]
-            aggregator.assert_service_check(common.SERVICE_CHECK_NAME,
+            aggregator.assert_service_check(SERVICE_CHECK_NAME,
                                             status=HAProxy.UNKNOWN,
                                             count=1,
                                             tags=tags)
         tags = ['service:' + service, 'backend:BACKEND']
-        aggregator.assert_service_check(common.SERVICE_CHECK_NAME,
+        aggregator.assert_service_check(SERVICE_CHECK_NAME,
                                         status=HAProxy.OK,
                                         count=1,
                                         tags=tags)
 
 
+@requires_socket_support
 @pytest.mark.usefixtures('dd_environment')
 @pytest.mark.integration
 def test_check(aggregator, check, instance):
     check.check(instance)
 
-    shared_tag = ["instance_url:{0}".format(common.STATS_URL)]
+    shared_tag = ["instance_url:{0}".format(STATS_URL)]
 
     _test_frontend_metrics(aggregator, shared_tag + ['active:false'])
     _test_backend_metrics(aggregator, shared_tag + ['active:false'])
@@ -97,13 +101,14 @@ def test_check(aggregator, check, instance):
     aggregator.assert_all_metrics_covered()
 
 
+@requires_socket_support
 @pytest.mark.usefixtures('dd_environment')
 @pytest.mark.integration
 def test_check_service_filter(aggregator, check, instance):
     instance['services_include'] = ['datadog']
     instance['services_exclude'] = ['.*']
     check.check(instance)
-    shared_tag = ["instance_url:{0}".format(common.STATS_URL)]
+    shared_tag = ["instance_url:{0}".format(STATS_URL)]
 
     _test_backend_metrics(aggregator, shared_tag + ['active:false'], ['datadog'])
 
@@ -112,6 +117,7 @@ def test_check_service_filter(aggregator, check, instance):
     aggregator.assert_all_metrics_covered()
 
 
+@requires_socket_support
 @pytest.mark.usefixtures('dd_environment')
 @pytest.mark.integration
 def test_wrong_config(aggregator, check, instance):
@@ -126,9 +132,9 @@ def test_wrong_config(aggregator, check, instance):
 @pytest.mark.usefixtures('dd_environment')
 @pytest.mark.integration
 def test_open_config(aggregator, check):
-    check.check(common.CHECK_CONFIG_OPEN)
+    check.check(CHECK_CONFIG_OPEN)
 
-    shared_tag = ["instance_url:{0}".format(common.STATS_URL_OPEN)]
+    shared_tag = ["instance_url:{0}".format(STATS_URL_OPEN)]
 
     _test_frontend_metrics(aggregator, shared_tag)
     _test_backend_metrics(aggregator, shared_tag)
@@ -137,15 +143,16 @@ def test_open_config(aggregator, check):
     aggregator.assert_all_metrics_covered()
 
 
+@requires_socket_support
 @pytest.mark.usefixtures('dd_environment')
 @pytest.mark.integration
 @pytest.mark.skipif(os.environ.get('HAPROXY_VERSION', '1.5.11').split('.')[:2] < ['1', '7'],
                     reason='Sockets with operator level are only available with haproxy 1.7')
 def test_tcp_socket(aggregator, check):
-    config = copy.deepcopy(common.CONFIG_TCPSOCKET)
+    config = copy.deepcopy(CONFIG_TCPSOCKET)
     check.check(config)
 
-    shared_tag = ["instance_url:{0}".format(common.STATS_SOCKET)]
+    shared_tag = ["instance_url:{0}".format(STATS_SOCKET)]
 
     _test_frontend_metrics(aggregator, shared_tag)
     _test_backend_metrics(aggregator, shared_tag, add_addr_tag=True)
@@ -154,11 +161,11 @@ def test_tcp_socket(aggregator, check):
     aggregator.assert_all_metrics_covered()
 
 
+@requires_socket_support
 @pytest.mark.usefixtures('dd_environment')
 @pytest.mark.integration
-@pytest.mark.skipif(not Platform.is_linux(), reason='Windows sockets are not file handles')
 def test_unixsocket_config(aggregator, check, dd_environment):
-    config = copy.deepcopy(common.CONFIG_UNIXSOCKET)
+    config = copy.deepcopy(CONFIG_UNIXSOCKET)
     unixsocket_url = dd_environment["unixsocket_url"]
     config['url'] = unixsocket_url
     check.check(config)
