e6efc8ee5984c0ec4615af48e1051b289eac9049
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2019-01-21 11:54:20 +0100

    Add e2e support

--- kyototycoon/tests/conftest.py
+++ kyototycoon/tests/conftest.py
@@ -3,67 +3,38 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 import os
-import subprocess
-from time import sleep
 
 import pytest
 import requests
 
-from common import (
-    HERE, URL
+from datadog_checks.dev import docker_run
+
+from .common import (
+    HERE, URL, DEFAULT_INSTANCE
 )
 
 
-@pytest.fixture
-def aggregator():
-    from datadog_checks.stubs import aggregator
-    aggregator.reset()
-    return aggregator
-
-
 @pytest.fixture(scope="session")
-def kyototycoon():
+def dd_environment():
     """
     Spin up a kyototycoon docker image
     """
-    args = [
-        'docker-compose',
-        '-f', os.path.join(HERE, 'compose', 'compose_kyototycoon.yaml')
-    ]
 
-    subprocess.check_call(args + ["up", "-d"])
+    with docker_run(
+        compose_file=os.path.join(HERE, 'compose', 'compose_kyototycoon.yaml'),
+        endpoints='{}/rpc/report'.format(URL),
+    ):
 
-    # wait for kyototycoon to be up
-    if not wait_for_kyototycoon():
-        raise Exception("kyototycoon container boot timed out!")
+        # Generate a test database
+        data = {
+            'dddd': 'dddd'
+        }
+        headers = {
+            'X-Kt-Mode': 'set'
+        }
 
-    # Generate a test database
-    data = {
-        'dddd': 'dddd'
-    }
-    headers = {
-        'X-Kt-Mode': 'set'
-    }
+        for i in xrange(100):
+            requests.put(URL, data=data, headers=headers)
+            requests.get(URL)
 
-    for i in xrange(100):
-        requests.put(URL, data=data, headers=headers)
-        requests.get(URL)
-
-    yield
-
-    subprocess.check_call(args + ["down"])
-
-
-def wait_for_kyototycoon():
-    """
-    Wait for the kyototycoon container to be reachable
-    """
-    for i in xrange(40):
-        sleep(1)
-        try:
-            requests.get('{0}/rpc/report'.format(URL)).raise_for_status()
-            return True
-        except Exception:
-            pass
-
-    return False
+        yield DEFAULT_INSTANCE
