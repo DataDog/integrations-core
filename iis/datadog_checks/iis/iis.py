091a09241084a3fc72435163b38e345dbd90c7c8
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-01-22 18:38:40 -0500

    Ensure unicode and cleanup primts

--- iis/datadog_checks/iis/iis.py
+++ iis/datadog_checks/iis/iis.py
@@ -3,7 +3,7 @@
 # Licensed under Simplified BSD License (see LICENSE)
 from six import iteritems
 
-from datadog_checks.base import AgentCheck, PDHBaseCheck
+from datadog_checks.base import AgentCheck, PDHBaseCheck, ensure_unicode
 from datadog_checks.utils.containers import hash_mutable
 
 
@@ -59,14 +59,14 @@ class IIS(PDHBaseCheck):
         if "_Total" not in expected_sites:
             expected_sites.add("_Total")
 
-        self.log.debug("expected sites is %s" % str(expected_sites))
+        self.log.debug("expected sites is {}".format(str(expected_sites)))
         key = hash_mutable(instance)
         for inst_name, dd_name, metric_func, counter in self._metrics[key]:
             try:
                 try:
                     vals = counter.get_all_values()
                 except Exception as e:
-                    self.log.error("Failed to get_all_values %s %s" % (inst_name, dd_name))
+                    self.log.error("Failed to get_all_values {} {}".format(inst_name, dd_name))
                     continue
 
                 for sitename, val in iteritems(vals):
@@ -78,21 +78,21 @@ class IIS(PDHBaseCheck):
                         if not counter.is_single_instance():
                             # Skip any sites we don't specifically want.
                             if not sites:
-                                tags.append("site:{0}".format(self.normalize(sitename)))
+                                tags.append("site:{0}".format(ensure_unicode(self.normalize(sitename))))
                             # always report total
                             elif sitename == "_Total":
-                                tags.append("site:{0}".format(self.normalize(sitename)))
+                                tags.append("site:{0}".format(ensure_unicode(self.normalize(sitename))))
                             elif sitename not in sites:
                                 continue
                             else:
-                                tags.append("site:{0}".format(self.normalize(sitename)))
+                                tags.append("site:{0}".format(ensure_unicode(self.normalize(sitename))))
                     except Exception as e:
-                        self.log.error("Caught exception %s setting tags" % str(e))
+                        self.log.error("Caught exception {} setting tags".format(str(e)))
 
                     try:
                         metric_func(dd_name, val, tags)
                     except Exception as e:
-                        self.log.error("metric_func: %s %s %s" % (dd_name, str(val), str(e)))
+                        self.log.error("metric_func: {} {} {}".format(dd_name, str(val), str(e)))
                         pass
 
                     if dd_name == "iis.uptime":
@@ -100,19 +100,19 @@ class IIS(PDHBaseCheck):
                         status = AgentCheck.CRITICAL if uptime == 0 else AgentCheck.OK
                         self.service_check(self.SERVICE_CHECK, status, tags)
                         if sitename in expected_sites:
-                            self.log.debug("Removing %s from expected sites" % sitename)
+                            self.log.debug("Removing {} from expected sites".format(sitename))
                             expected_sites.remove(sitename)
                         else:
-                            self.log.warning("site not in expected_sites %s" % sitename)
+                            self.log.warning("site not in expected_sites {}".format(sitename))
 
             except Exception as e:
                 # don't give up on all of the metrics because one failed
-                self.log.error("IIS Failed to get metric data for %s %s: %s" % (inst_name, dd_name, str(e)))
+                self.log.error("IIS Failed to get metric data for {} {}: {}" .format(inst_name, dd_name, str(e)))
                 pass
 
         for site in expected_sites:
             tags = []
             if key in self._tags:
                 tags = list(self._tags[key])
-            tags.append("site:{0}".format(self.normalize(site)))
+            tags.append("site:{}".format(self.normalize(site)))
             self.service_check(self.SERVICE_CHECK, AgentCheck.CRITICAL, tags)
