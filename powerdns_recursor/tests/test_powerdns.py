12b48bf423d66da1545f31307923d4647908cde8
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2019-01-23 11:28:29 -0500

    Add e2e support (#2992)

--- powerdns_recursor/tests/test_powerdns.py
+++ powerdns_recursor/tests/test_powerdns.py
@@ -3,81 +3,22 @@
 # Licensed under Simplified BSD License (see LICENSE)
 
 import pytest
-import os
-import subprocess
-import requests
-import time
-import logging
-
-from datadog_checks.powerdns_recursor import PowerDNSRecursorCheck
 
 from . import common, metrics
 
-CHECK_NAME = 'powerdns_recursor'
-
-log = logging.getLogger('test_apache')
-
-
-def wait_for_powerdns():
-    base_url = "http://{}:{}".format(common.HOST, common.PORT)
-    for _ in range(0, 100):
-        res = None
-        try:
-            res = requests.get(base_url)
-            res.raise_for_status
-            return
-        except Exception as e:
-            log.info("exception: {0} res: {1}".format(e, res))
-            time.sleep(2)
-    raise Exception("Cannot start up apache")
+pytestmark = [
+    pytest.mark.integration,
+    pytest.mark.usefixtures("dd_environment"),
+]
 
 
-@pytest.fixture(scope="session")
-def spin_up_powerdns():
-    env = os.environ
-    pdns_version = env['POWERDNS_VERSION']
-    pdns_underscore_version = pdns_version.replace('.', '_')
-    pdns_tag = "powerdns_recursor_{0}".format(pdns_underscore_version)
-    powerdns_image = "datadog/docker-library:{0}".format(pdns_tag)
-    env['POWERDNS_IMAGE'] = powerdns_image
-
-    env['POWERDNS_CONFIG'] = os.path.join(common.HERE, 'compose', 'recursor.conf')
-    args = [
-        "docker-compose",
-        "-f", os.path.join(common.HERE, 'compose', 'powerdns.yaml')
-    ]
-    subprocess.check_call(args + ["up", "-d", "--build"], env=env)
-    wait_for_powerdns()
-    time.sleep(20)
-    yield
-    subprocess.check_call(args + ["down"], env=env)
-
-
-@pytest.fixture
-def aggregator():
-    from datadog_checks.stubs import aggregator
-    aggregator.reset()
-    return aggregator
-
-
-def _get_pdns_version():
-    headers = {"X-API-Key": common.CONFIG['api_key']}
-    url = "http://{}:{}/api/v1/servers/localhost/statistics".format(common.HOST, common.PORT)
-    request = requests.get(url, headers=headers)
-    if request.status_code == 404:
-        return 3
-    else:
-        return 4
-
-
-def test_check(aggregator, spin_up_powerdns):
+def test_check(aggregator, check):
     service_check_tags = common._config_sc_tags(common.CONFIG)
 
     # get version and test v3 first.
-    version = _get_pdns_version()
-    pdns_check = PowerDNSRecursorCheck(CHECK_NAME, {}, {})
+    version = common._get_pdns_version()
     if version == 3:
-        pdns_check.check(common.CONFIG)
+        check.check(common.CONFIG)
 
         # Assert metrics
         for metric in metrics.GAUGE_METRICS:
@@ -86,13 +27,11 @@ def test_check(aggregator, spin_up_powerdns):
         for metric in metrics.RATE_METRICS:
             aggregator.assert_metric(metrics.METRIC_FORMAT.format(metric), tags=[], count=1)
 
-        aggregator.assert_service_check('powerdns.recursor.can_connect',
-                                        status=PowerDNSRecursorCheck.OK,
-                                        tags=service_check_tags)
-        assert aggregator.metrics_asserted_pct == 100.0
+        aggregator.assert_service_check('powerdns.recursor.can_connect', status=check.OK, tags=service_check_tags)
+        aggregator.assert_all_metrics_covered()
 
     elif version == 4:
-        pdns_check.check(common.CONFIG_V4)
+        check.check(common.CONFIG_V4)
 
         # Assert metrics
         for metric in metrics.GAUGE_METRICS + metrics.GAUGE_METRICS_V4:
@@ -101,26 +40,20 @@ def test_check(aggregator, spin_up_powerdns):
         for metric in metrics.RATE_METRICS + metrics.RATE_METRICS_V4:
             aggregator.assert_metric(metrics.METRIC_FORMAT.format(metric), tags=[], count=1)
 
-        aggregator.assert_service_check('powerdns.recursor.can_connect',
-                                        status=PowerDNSRecursorCheck.OK,
-                                        tags=service_check_tags)
-        assert aggregator.metrics_asserted_pct == 100.0
+        aggregator.assert_service_check('powerdns.recursor.can_connect', status=check.OK, tags=service_check_tags)
+        aggregator.assert_all_metrics_covered()
     else:
-        print("powerdns_recursor unknown version.")
-        aggregator.assert_service_check('powerdns.recursor.can_connect',
-                                        status=PowerDNSRecursorCheck.CRITICAL,
-                                        tags=service_check_tags)
+        aggregator.assert_service_check('powerdns.recursor.can_connect', status=check.CRITICAL, tags=service_check_tags)
 
 
-def test_tags(aggregator, spin_up_powerdns):
-    version = _get_pdns_version()
+def test_tags(aggregator, check):
+    version = common._get_pdns_version()
 
-    pdns_check = PowerDNSRecursorCheck(CHECK_NAME, {}, {})
     tags = ['foo:bar']
     if version == 3:
         config = common.CONFIG.copy()
         config['tags'] = ['foo:bar']
-        pdns_check.check(config)
+        check.check(config)
 
         # Assert metrics v3
         for metric in metrics.GAUGE_METRICS:
@@ -132,7 +65,7 @@ def test_tags(aggregator, spin_up_powerdns):
     elif version == 4:
         config = common.CONFIG_V4.copy()
         config['tags'] = ['foo:bar']
-        pdns_check.check(config)
+        check.check(config)
 
         # Assert metrics v3
         for metric in metrics.GAUGE_METRICS + metrics.GAUGE_METRICS_V4:
@@ -142,41 +75,15 @@ def test_tags(aggregator, spin_up_powerdns):
             aggregator.assert_metric(metrics.METRIC_FORMAT.format(metric), tags=tags, count=1)
 
     service_check_tags = common._config_sc_tags(common.CONFIG)
-    aggregator.assert_service_check('powerdns.recursor.can_connect',
-                                    status=PowerDNSRecursorCheck.OK,
-                                    tags=service_check_tags+tags)
+    aggregator.assert_service_check('powerdns.recursor.can_connect', status=check.OK, tags=service_check_tags + tags)
 
     aggregator.assert_all_metrics_covered()
 
 
-def test_bad_config(aggregator):
-    pdns_check = PowerDNSRecursorCheck(CHECK_NAME, {}, {})
+def test_bad_api_key(aggregator, check):
     with pytest.raises(Exception):
-        pdns_check.check(common.BAD_CONFIG)
-
-    service_check_tags = common._config_sc_tags(common.BAD_CONFIG)
-    aggregator.assert_service_check('powerdns.recursor.can_connect',
-                                    status=PowerDNSRecursorCheck.CRITICAL,
-                                    tags=service_check_tags)
-    assert len(aggregator._metrics) == 0
-
-
-def test_bad_api_key(aggregator, spin_up_powerdns):
-    pdns_check = PowerDNSRecursorCheck(CHECK_NAME, {}, {})
-    with pytest.raises(Exception):
-        pdns_check.check(common.BAD_API_KEY_CONFIG)
+        check.check(common.BAD_API_KEY_CONFIG)
 
     service_check_tags = common._config_sc_tags(common.BAD_API_KEY_CONFIG)
-    aggregator.assert_service_check('powerdns.recursor.can_connect',
-                                    status=PowerDNSRecursorCheck.CRITICAL,
-                                    tags=service_check_tags)
-    assert len(aggregator._metrics) == 0
-
-
-def test_very_bad_config(aggregator):
-    pdns_check = PowerDNSRecursorCheck(CHECK_NAME, {}, {})
-    for config in [{}, {"host": "localhost"}, {"port": 1000}, {"host": "localhost", "port": 1000}]:
-        with pytest.raises(Exception):
-            pdns_check.check(common.BAD_API_KEY_CONFIG)
-
+    aggregator.assert_service_check('powerdns.recursor.can_connect', status=check.CRITICAL, tags=service_check_tags)
     assert len(aggregator._metrics) == 0
