154990c53c7f3cddab7e0898612eef761db04646
Gregory <gregory.zussa@datadoghq.com>
2019-01-23 18:46:11 +0100

    Incorporate feedback

--- riak/tests/conftest.py
+++ riak/tests/conftest.py
@@ -5,12 +5,12 @@
 import pytest
 import os
 import requests
-import time
 import logging
 
 from copy import deepcopy
 from datadog_checks.riak import Riak
-from datadog_checks.dev import WaitFor, docker_run
+from datadog_checks.dev import docker_run
+from datadog_checks.dev.conditions import CheckEndpoints
 
 from . import common
 
@@ -18,33 +18,14 @@ from . import common
 log = logging.getLogger('test_riak')
 
 
-def wait_for_riak():
-    can_access = False
-    for _ in range(0, 10):
-        res = None
-        try:
-            res = requests.get("{0}/riak/bucket".format(common.BASE_URL))
-            log.info("response: {0}".format(res))
-            log.info("status code: {0}, text: {1}".format(res.status_code, res.text))
-            res.raise_for_status
-            can_access = True
-            break
-        except Exception as e:
-            log.info("exception: {0}, response: {1}".format(e, res))
-            time.sleep(5)
-    if not can_access:
-        raise Exception("Cannot access Riak")
-
-
 def populate():
-    data = 'herzlich willkommen'
-    headers = {"Content-Type": "text/plain"}
     for _ in range(0, 10):
         res = requests.post(
             "{0}/riak/bucket/german".format(common.BASE_URL),
-            headers=headers,
-            data=data)
+            headers={"Content-Type": "text/plain"},
+            data='herzlich willkommen')
         res.raise_for_status
+
         res = requests.get("{0}/riak/bucket/german".format(common.BASE_URL))
         res.raise_for_status
 
@@ -55,7 +36,7 @@ def dd_environment():
     with docker_run(
         compose_file=os.path.join(common.HERE, 'compose', 'riak.yaml'),
         env_vars=env,
-        conditions=[WaitFor(wait_for_riak), populate],
+        conditions=[CheckEndpoints(['{}/riak/bucket'.format(common.BASE_URL)]), populate],
         sleep=10,  # some stats require a bit of time before the test will capture them
     ):
         yield common.INSTANCE
