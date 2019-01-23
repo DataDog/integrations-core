206c74430d40bee5cfd2e8c284c20e67a5fda0c5
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2019-01-23 12:35:27 -0500

    Add e2e support (#2989)

--- kyototycoon/tests/test_kyototycoon.py
+++ kyototycoon/tests/test_kyototycoon.py
@@ -2,28 +2,23 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
+from copy import deepcopy
+
 from datadog_checks.kyototycoon import KyotoTycoonCheck
 
 from .common import (
-    URL
+    DEFAULT_INSTANCE, TAGS
 )
 
 
-def test_check(aggregator, kyototycoon):
+def test_check(aggregator, dd_environment):
     """
     Testing Kyototycoon check.
     """
     kt = KyotoTycoonCheck('kyototycoon', {}, {})
 
-    TAGS = ['optional:tag1']
-
-    instance = {
-        'report_url': '{0}/rpc/report'.format(URL),
-        'tags': TAGS
-    }
-
     # run the check
-    kt.check(instance)
+    kt.check(deepcopy(DEFAULT_INSTANCE))
 
     GAUGES = KyotoTycoonCheck.GAUGES.values()
     DB_GAUGES = KyotoTycoonCheck.DB_GAUGES.values()
@@ -37,16 +32,15 @@ def test_check(aggregator, kyototycoon):
     # no replications, so ignore kyototycoon.replication.delay
     for mname in GAUGES:
         if mname != 'replication.delay':
-            aggregator.assert_metric('kyototycoon.{0}'.format(mname), tags=TAGS, count=1)
+            aggregator.assert_metric('kyototycoon.{}'.format(mname), tags=TAGS, count=1)
 
     for mname in DB_GAUGES:
-        aggregator.assert_metric('kyototycoon.{0}'.format(mname), tags=TAGS + ['db:0'], count=1)
+        aggregator.assert_metric('kyototycoon.{}'.format(mname), tags=TAGS + ['db:0'], count=1)
 
     for mname in ALL_RATES:
-        aggregator.assert_metric('kyototycoon.{0}_per_s'.format(mname), tags=TAGS, count=1)
+        aggregator.assert_metric('kyototycoon.{}_per_s'.format(mname), tags=TAGS, count=1)
 
     # service check
-    aggregator.assert_service_check(
-        KyotoTycoonCheck.SERVICE_CHECK_NAME, status=KyotoTycoonCheck.OK, tags=TAGS, count=1)
+    aggregator.assert_service_check(KyotoTycoonCheck.SERVICE_CHECK_NAME, status=KyotoTycoonCheck.OK, tags=TAGS, count=1)
 
     aggregator.assert_all_metrics_covered()
