bd4b28bfa3fd24ff6537810b1884d1d4b471980c
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2019-01-30 19:25:24 +0100

    Extend test with added verification

--- snmp/tests/test_unit.py
+++ snmp/tests/test_unit.py
@@ -52,14 +52,75 @@ def test_parse_metrics(hlapi_mock, check):
     hlapi_mock.ObjectIdentity.assert_called_once_with("foo_mib", "foo")
     hlapi_mock.reset_mock()
 
-    # MIB with table
+    # MIB with table, no symbols
     metrics = [{
         "MIB": "foo_mib",
         "table": "foo",
     }]
+    with pytest.raises(Exception):
+        check.parse_metrics(metrics, False)
+
+    # MIB with table and symbols
+    metrics = [{
+        "MIB": "foo_mib",
+        "table": "foo",
+        "symbols": ["foo", "bar"],
+    }]
     table, raw, mibs = check.parse_metrics(metrics, True)
     assert raw == []
     assert mibs == set()
-    assert len(table) == 1
-    hlapi_mock.ObjectIdentity.assert_called_once_with("foo_mib", "foo")
+    assert len(table) == 2
+    hlapi_mock.ObjectIdentity.assert_any_call("foo_mib", "foo")
+    hlapi_mock.ObjectIdentity.assert_any_call("foo_mib", "bar")
+    hlapi_mock.reset_mock()
+
+    # MIB with table, symbols, bad metrics_tags
+    metrics = [{
+        "MIB": "foo_mib",
+        "table": "foo",
+        "symbols": ["foo", "bar"],
+        "metric_tags": [{}]
+    }]
+    with pytest.raises(Exception):
+        check.parse_metrics(metrics, False)
+
+    # MIB with table, symbols, bad metrics_tags
+    metrics = [{
+        "MIB": "foo_mib",
+        "table": "foo",
+        "symbols": ["foo", "bar"],
+        "metric_tags": [{"tag": "foo"}]
+    }]
+    with pytest.raises(Exception):
+        check.parse_metrics(metrics, False)
+
+    # MIB with table, symbols, metrics_tags index
+    metrics = [{
+        "MIB": "foo_mib",
+        "table": "foo",
+        "symbols": ["foo", "bar"],
+        "metric_tags": [{"tag": "foo", "index": "1"}]
+    }]
+    table, raw, mibs = check.parse_metrics(metrics, True)
+    assert raw == []
+    assert mibs == set()
+    assert len(table) == 2
+    hlapi_mock.ObjectIdentity.assert_any_call("foo_mib", "foo")
+    hlapi_mock.ObjectIdentity.assert_any_call("foo_mib", "bar")
+    hlapi_mock.reset_mock()
+
+    # MIB with table, symbols, metrics_tags column
+    metrics = [{
+        "MIB": "foo_mib",
+        "table": "foo",
+        "symbols": ["foo", "bar"],
+        "metric_tags": [{"tag": "foo", "column": "baz"}]
+    }]
+    table, raw, mibs = check.parse_metrics(metrics, True)
+    assert raw == []
+    assert mibs == set()
+    assert len(table) == 3
+    hlapi_mock.ObjectIdentity.assert_any_call("foo_mib", "foo")
+    hlapi_mock.ObjectIdentity.assert_any_call("foo_mib", "bar")
+    hlapi_mock.ObjectIdentity.assert_any_call("foo_mib", "baz")
     hlapi_mock.reset_mock()
