2d9f95e2c59cdfe54c153b0a010f971fe6bdb387
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2019-01-31 17:27:50 +0100

    Make flake8 happy

--- datadog_checks_base/tests/test_pdhbasecheck.py
+++ datadog_checks_base/tests/test_pdhbasecheck.py
@@ -3,25 +3,14 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 import pytest
-from datadog_checks.stubs import aggregator
 
 try:
     from datadog_checks.checks.win.winpdh_base import PDHBaseCheck
-
-    # for reasons unknown, flake8 says that pdh_mocks_fixture is unused, even though
-    # it's used below.  noqa to suppress that error.
-    from datadog_test_libs.win.pdh_mocks import pdh_mocks_fixture, initialize_pdh_tests  # noqa: F401
-
-except ImportError:  # noqa: E722
+    from datadog_test_libs.win import pdh_mocks
+except ImportError:
     pass
 
 from .utils import requires_windows
-
-
-@pytest.fixture
-def Aggregator():
-    aggregator.reset()
-    return aggregator
 
 
 DEFAULT_INSTANCE = {'host': '.'}
@@ -38,48 +27,48 @@ MULTI_INSTANCE_COUNTER_WITH_INSTANCES = [
 ]
 
 
-@requires_windows  # noqa: F811
-def test_single_instance_counter(Aggregator, pdh_mocks_fixture):
-    initialize_pdh_tests()
+@requires_windows
+def test_single_instance_counter(aggregator, pdh_mocks_fixture):
+    pdh_mocks.initialize_pdh_tests()
     instance = DEFAULT_INSTANCE
     c = PDHBaseCheck("testcheck", {}, {}, [instance], SINGLE_INSTANCE_COUNTER)
     c.check(instance)
 
-    Aggregator.assert_metric("test.system.mem.available", tags=None, count=1)
+    aggregator.assert_metric("test.system.mem.available", tags=None, count=1)
 
 
-@requires_windows  # noqa: F811
-def test_single_instance_counter_with_instance(Aggregator, pdh_mocks_fixture):
-    initialize_pdh_tests()
+@requires_windows
+def test_single_instance_counter_with_instance(aggregator, pdh_mocks_fixture):
+    pdh_mocks.initialize_pdh_tests()
     instance = DEFAULT_INSTANCE
     with pytest.raises(AttributeError):
         PDHBaseCheck("testcheck", {}, {}, [instance], INSTANCE_OF_SINGLE_INSTANCE_COUNTER)
 
 
-@requires_windows  # noqa: F811
-def test_multi_instance_counter(Aggregator, pdh_mocks_fixture):
-    initialize_pdh_tests()
+@requires_windows
+def test_multi_instance_counter(aggregator, pdh_mocks_fixture):
+    pdh_mocks.initialize_pdh_tests()
     instance = DEFAULT_INSTANCE
     c = PDHBaseCheck("testcheck", {}, {}, [instance], MULTI_INSTANCE_COUNTER)
     c.check(instance)
     for t in ['instance:0', 'instance:1', 'instance:_Total']:
-        Aggregator.assert_metric("test.processor_time", tags=['%s' % t], count=1)
-    assert Aggregator.metrics_asserted_pct == 100.0
+        aggregator.assert_metric("test.processor_time", tags=['%s' % t], count=1)
+    assert aggregator.metrics_asserted_pct == 100.0
 
 
-@requires_windows  # noqa: F811
-def test_multi_instance_counter_specific_instances(Aggregator, pdh_mocks_fixture):
-    initialize_pdh_tests()
+@requires_windows
+def test_multi_instance_counter_specific_instances(aggregator, pdh_mocks_fixture):
+    pdh_mocks.initialize_pdh_tests()
     instance = DEFAULT_INSTANCE
     c = PDHBaseCheck("testcheck", {}, {}, [instance], MULTI_INSTANCE_COUNTER_WITH_INSTANCES)
     c.check(instance)
     for t in ['test.processor_time_0', 'test.processor_time_1']:
-        Aggregator.assert_metric(t, tags=None, count=1)
-    assert Aggregator.metrics_asserted_pct == 100.0
+        aggregator.assert_metric(t, tags=None, count=1)
+    assert aggregator.metrics_asserted_pct == 100.0
 
 
-@requires_windows  # noqa: F811
-def test_returns_partial_metrics(Aggregator, pdh_mocks_fixture):
+@requires_windows
+def test_returns_partial_metrics(aggregator, pdh_mocks_fixture):
     COUNTER_LIST = [
         ["NTDS", None, "LDAP Client Sessions", "active_directory.ldap.client_sessions", "gauge"],
         ["NTDS", None, "LDAP Bind Time", "active_directory.ldap.bind_time", "gauge"],
@@ -89,13 +78,13 @@ def test_returns_partial_metrics(Aggregator, pdh_mocks_fixture):
         ["NTDS", None, "Kerberos Authentications/sec", "active_directory.kerberos.auths_persec", "gauge"],
         ["NTDS", None, "NTLM Authentications/sec", "active_directory.ntlm.auths_persec", "gauge"],
     ]
-    initialize_pdh_tests()
+    pdh_mocks.initialize_pdh_tests()
     instance = DEFAULT_INSTANCE
     c = PDHBaseCheck("testcheck", {}, {}, [instance], COUNTER_LIST)
     c.check(instance)
 
-    Aggregator.assert_metric("active_directory.ldap.client_sessions", tags=None, count=1)
-    Aggregator.assert_metric("active_directory.ldap.bind_time", tags=None, count=1)
-    Aggregator.assert_metric("active_directory.ldap.successful_binds_persec", tags=None, count=1)
-    Aggregator.assert_metric("active_directory.ldap.searches_persec", tags=None, count=1)
-    assert Aggregator.metrics_asserted_pct == 100.0
+    aggregator.assert_metric("active_directory.ldap.client_sessions", tags=None, count=1)
+    aggregator.assert_metric("active_directory.ldap.bind_time", tags=None, count=1)
+    aggregator.assert_metric("active_directory.ldap.successful_binds_persec", tags=None, count=1)
+    aggregator.assert_metric("active_directory.ldap.searches_persec", tags=None, count=1)
+    assert aggregator.metrics_asserted_pct == 100.0
--- datadog_checks_base/tests/test_winpdh.py
+++ datadog_checks_base/tests/test_winpdh.py
@@ -5,29 +5,17 @@
 import pytest
 import datadog_checks.stubs.datadog_agent as logger
 from collections import defaultdict
-from datadog_checks.stubs import aggregator
 
 try:
     from datadog_checks.checks.win.winpdh import WinPDHCounter, SINGLE_INSTANCE_KEY
-
-    # for reasons unknown, flake8 says that pdh_mocks_fixture is unused, even though
-    # it's used below.  noqa to suppress that error.
-    from datadog_test_libs.win.pdh_mocks import pdh_mocks_fixture, initialize_pdh_tests  # noqa: F401
-    from datadog_test_libs.win.pdh_mocks import pdh_mocks_fixture_bad_perf_strings  # noqa: F401
-
-except:  # noqa: E722
+    from datadog_test_libs.win import pdh_mocks
+except ImportError:
     import platform
 
     if platform.system() != 'Windows':
         pass
 
 from .utils import requires_windows
-
-
-@pytest.fixture
-def Aggregator():
-    aggregator.reset()
-    return aggregator
 
 
 '''
@@ -38,9 +26,9 @@ the interface to the OS API.
 '''
 
 
-@requires_windows  # noqa: F811
+@requires_windows
 def test_winpdhcounter_bad_strings_english(pdh_mocks_fixture_bad_perf_strings):
-    initialize_pdh_tests()
+    pdh_mocks.initialize_pdh_tests()
     counter = WinPDHCounter('System', 'Processor Queue Length', logger)
 
     vals = counter.get_all_values()
@@ -48,9 +36,9 @@ def test_winpdhcounter_bad_strings_english(pdh_mocks_fixture_bad_perf_strings):
     assert SINGLE_INSTANCE_KEY in vals
 
 
-@requires_windows  # noqa: F811
+@requires_windows
 def test_winpdhcounter_throws_on_bad_input(pdh_mocks_fixture):
-    initialize_pdh_tests()
+    pdh_mocks.initialize_pdh_tests()
     with pytest.raises(AttributeError):
         WinPDHCounter('Ssystem', 'Processor Queue Length', logger)
 
@@ -58,9 +46,9 @@ def test_winpdhcounter_throws_on_bad_input(pdh_mocks_fixture):
         WinPDHCounter('System', 'PProcessor Queue Length', logger)
 
 
-@requires_windows  # noqa: F811
+@requires_windows
 def test_winpdhcounter_throws_on_bad_input_with_bad_strings(pdh_mocks_fixture_bad_perf_strings):
-    initialize_pdh_tests()
+    pdh_mocks.initialize_pdh_tests()
     with pytest.raises(AttributeError):
         WinPDHCounter('Ssystem', 'Processor Queue Length', logger)
 
@@ -68,12 +56,12 @@ def test_winpdhcounter_throws_on_bad_input_with_bad_strings(pdh_mocks_fixture_ba
         WinPDHCounter('System', 'PProcessor Queue Length', logger)
 
 
-@requires_windows  # noqa: F811
+@requires_windows
 def test_winpdhcounter_bad_strings_not_english(pdh_mocks_fixture_bad_perf_strings):
     WinPDHCounter._use_en_counter_names = False
     WinPDHCounter.pdh_counter_dict = defaultdict(list)
 
-    initialize_pdh_tests(lang="se-sv")
+    pdh_mocks.initialize_pdh_tests(lang="se-sv")
     '''
     expectation is that the initialization will fail.  We attempt to fall
     back to english counters if the strings database isn't present; however,
@@ -83,11 +71,11 @@ def test_winpdhcounter_bad_strings_not_english(pdh_mocks_fixture_bad_perf_string
         WinPDHCounter('System', 'Processor Queue Length', logger)
 
 
-@requires_windows  # noqa: F811
+@requires_windows
 def test_winpdhcounter_non_english(pdh_mocks_fixture):
     WinPDHCounter._use_en_counter_names = False
     WinPDHCounter.pdh_counter_dict = defaultdict(list)
-    initialize_pdh_tests(lang="se-sv")
+    pdh_mocks.initialize_pdh_tests(lang="se-sv")
     counter = WinPDHCounter('System', 'Processor Queue Length', logger)
 
     vals = counter.get_all_values()
