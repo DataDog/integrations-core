5a2169b2b2a706713cdaf7c41949083fe6f4bea8
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-02-04 10:44:21 -0500

    address review

--- tokumx/datadog_checks/tokumx/vendor/__init__.py
+++ tokumx/datadog_checks/tokumx/vendor/__init__.py
@@ -1,10 +1,7 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from .import bson, pymongo
-
-
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+from . import bson, pymongo
 
 __all__ = [
     'bson',
