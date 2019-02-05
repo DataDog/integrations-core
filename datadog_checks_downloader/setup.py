166430221282ed38516e221a5888b4cc4f72af11
Trishank K Kuppusamy <trishank.kuppusamy@datadoghq.com>
2019-02-05 15:49:20 -0500

    incorporate @ofek feedback

--- datadog_checks_downloader/setup.py
+++ datadog_checks_downloader/setup.py
@@ -43,6 +43,8 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: System :: Monitoring',
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
