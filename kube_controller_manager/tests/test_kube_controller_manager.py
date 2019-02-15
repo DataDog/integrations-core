4b49701f25576069fbb78bddf4701d570328bf06
Xavier Vello <xavier.vello@datadoghq.com>
2019-02-15 10:20:45 +0100

    fix mock coding style

--- kube_controller_manager/tests/test_kube_controller_manager.py
+++ kube_controller_manager/tests/test_kube_controller_manager.py
@@ -25,30 +25,28 @@ def mock_metrics():
     f_name = os.path.join(os.path.dirname(__file__), 'fixtures', 'metrics.txt')
     with open(f_name, 'r') as f:
         text_data = f.read()
-    mocked = mock.patch(
+    with mock.patch(
         'requests.get',
         return_value=mock.MagicMock(
             status_code=200,
             iter_lines=lambda **kwargs: text_data.split("\n"),
             headers={'Content-Type': "text/plain"}
         ),
-    )
-    yield mocked.start()
-    mocked.stop()
+    ):
+        yield
 
 
 @pytest.fixture()
 def mock_leader():
     # Inject a fake object in the leader-election monitoring logic
-    mocked = mock.patch(
+    with mock.patch(
         'datadog_checks.kube_controller_manager.KubeControllerManagerCheck._get_record',
         return_value=ElectionRecord(
             '{"holderIdentity":"pod1","leaseDurationSeconds":15,"leaderTransitions":3,' +
             '"acquireTime":"2018-12-19T18:23:24Z","renewTime":"2019-01-02T16:30:07Z"}'
         ),
-    )
-    yield mocked.start()
-    mocked.stop()
+    ):
+        yield
 
 
 def test_check_metrics(aggregator, mock_metrics, mock_leader):
