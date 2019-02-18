852087d4e50d91428b5ba240c2a9343872c221b2
Ofek Lev <ofekmeister@gmail.com>
2019-02-17 23:49:49 -0500

    Revert "Performance improvement in tracking signed files in git (#2872)"
    This reverts commit 3a958fc8bbb6d3a52e38011e7c7aa7f8ea3bd8ac.

--- datadog_checks_dev/datadog_checks/dev/tooling/signing.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/signing.py
@@ -32,13 +32,12 @@ class YubikeyException(Exception):
 
 
 class UntrackedFileException(Exception):
-    def __init__(self, stderr):
-        self.stderr = stderr
+    def __init__(self, filename):
+        self.filename = filename
 
 
     def __str__(self):
-        # https://stackoverflow.com/a/18389510
-        return '\n\t{}'.format('\t'.join(self.stderr.splitlines(True)))
+        return '{} has not been tracked by git!'.format(self.filename)
 
 
 def read_gitignore_patterns():
@@ -118,10 +117,10 @@ def update_link_metadata(checks):
             tag = json.load(tag_json)
             products = tag['signed']['products']
 
-        stderr = git_ls_files(products)
-        if stderr:
-            os.remove(tag_link)
-            raise UntrackedFileException(stderr)
+        for product in products:
+            if not git_ls_files(product):
+                os.remove(tag_link)
+                raise UntrackedFileException(product)
 
         # Tell pipeline which tag link metadata to use.
         write_file(metadata_file_tracker, tag_link)
