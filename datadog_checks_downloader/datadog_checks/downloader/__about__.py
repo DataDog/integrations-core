0c2fb0c877e2f5a901df2a020d168e1ec4840db1
Trishank K Kuppusamy <trishank.kuppusamy@datadoghq.com>
2019-02-22 14:22:28 -0500

    fix version in datadog-checks-downloader about

--- datadog_checks_downloader/datadog_checks/downloader/__about__.py
+++ datadog_checks_downloader/datadog_checks/downloader/__about__.py
@@ -2,8 +2,4 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-<<<<<<< HEAD
-__version__ = "0.1.0"
-=======
 __version__ = "0.3.0"
->>>>>>> e83ebb09... [Release] Bumped datadog_checks_downloader version to 0.3.0 (#3187)
