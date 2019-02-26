fbdf7523376b28fd8f9f2d6d7eed9de74cd0915d
Gregory <gregory.zussa@datadoghq.com>
2019-02-26 18:53:13 +0100

    incorporate feedback

--- mesos_slave/tests/conftest.py
+++ mesos_slave/tests/conftest.py
@@ -1,8 +1,8 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+
 import os
-import copy
 import pytest
 
 from datadog_checks.dev import docker_run
@@ -14,10 +14,14 @@ from . import common
 def dd_environment(instance):
     compose_file = os.path.join(common.HERE, 'compose', 'docker-compose.yml')
 
-    with docker_run(compose_file):
+    with docker_run(
+        compose_file,
+        service_name="mesos-slave",
+        log_patterns=['New master detected at master'],
+    ):
         yield instance
 
 
 @pytest.fixture(scope='session')
 def instance():
-    return copy.deepcopy(common.INSTANCE)
+    return common.INSTANCE
