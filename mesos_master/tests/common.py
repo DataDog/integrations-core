00d20765b5e735c5d40ffcc9cd86b4baaa3fee9b
Gregory <gregory.zussa@datadoghq.com>
2019-02-26 18:44:16 +0100

    incorporate feedback

--- mesos_master/tests/common.py
+++ mesos_master/tests/common.py
@@ -1,14 +1,10 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-import os
+from datadog_checks.dev import get_here
 
-from datadog_checks.dev import get_docker_hostname
 
-HOST = get_docker_hostname()
-
-HERE = os.path.dirname(os.path.abspath(__file__))
-ROOT = os.path.dirname(os.path.dirname(HERE))
+HERE = get_here()
 
 INSTANCE = {
     'url': 'http://localhost:5050',
