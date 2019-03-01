8645d1673f8fed93c5daaaef6c9860cbcffd0d47
Marc Ian Bucad <ian.bucad@gmail.com>
2019-03-01 10:57:22 +0000

    Use execute instead of callproc if using (py)odbc (#3236)

--- sqlserver/datadog_checks/sqlserver/sqlserver.py
+++ sqlserver/datadog_checks/sqlserver/sqlserver.py
@@ -540,7 +540,14 @@ class SQLServer(AgentCheck):
 
             try:
                 self.log.debug("Calling Stored Procedure : {}".format(proc))
-                cursor.callproc(proc)
+                if self._get_connector(instance) == 'adodbapi':
+                    cursor.callproc(proc)
+                else:
+                    # pyodbc does not support callproc; use execute instead.
+                    # Reference: https://github.com/mkleehammer/pyodbc/wiki/Calling-Stored-Procedures
+                    call_proc = '{{CALL {}}}'.format(proc)
+                    cursor.execute(call_proc)
+
                 rows = cursor.fetchall()
                 self.log.debug("Row count ({}) : {}".format(proc, cursor.rowcount))
 
