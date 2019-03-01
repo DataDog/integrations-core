ab40f63e46c78be18113c485bfc40b16329c06f1
Gregory <gregory.zussa@datadoghq.com>
2019-03-01 19:03:01 +0100

    Incorporate feedback

--- snmp/datadog_checks/snmp/snmp.py
+++ snmp/datadog_checks/snmp/snmp.py
@@ -72,10 +72,10 @@ class SnmpCheck(NetworkCheck):
         tags = instance.get("tags", [])
         ip_address = instance["ip_address"]
         metrics = []
-        if self.init_config is not None:
+        if instance.get('use_global_metrics', True) and self.init_config is not None:
             metrics = self.init_config.get('metrics', [])
         if instance.get('metrics') is not None:
-            metrics = instance.get('metrics', [])
+            metrics.extend(instance.get('metrics', []))
         timeout = int(instance.get('timeout', self.DEFAULT_TIMEOUT))
         retries = int(instance.get('retries', self.DEFAULT_RETRIES))
         enforce_constraints = _is_affirmative(instance.get('enforce_mib_constraints', True))
