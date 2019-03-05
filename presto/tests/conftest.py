b59e74def3db2fc5eef7ce041cb5a3c89a8ed6d2
Fanny Jiang <jiangf@bu.edu>
2019-03-05 14:16:52 -0500

    add docker-compose.yaml and tests for e2e

--- presto/tests/conftest.py
+++ presto/tests/conftest.py
@@ -1,17 +1,19 @@
 # (C) Datadog, Inc. 2019
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import os
 import pytest
 
 from datadog_checks.dev.utils import load_jmx_config
+from datadog_checks.dev import docker_run, get_here
 
 
 @pytest.fixture(scope='session')
-def dd_environment():
-    yield load_jmx_config()
+def dd_environment(instance):
+    with docker_run(os.path.join(get_here(), 'docker','docker-compose.yaml')):
+        yield instance
 
-
-@pytest.fixture
+@pytest.fixture(scope='session', autouse=True)
+@pytest.mark.usefixtures('dd_environment')
 def instance():
-
-    return {}
+    return load_jmx_config()
