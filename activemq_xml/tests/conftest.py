666ff7adf7382f211eb749aea4364bbc783ed84e
Paul Coignet <paul.coignet@datadoghq.com>
2019-03-11 17:50:45 +0100

    fix mkdir when dir already exists

--- activemq_xml/tests/conftest.py
+++ activemq_xml/tests/conftest.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
+import errno
 import os
 import stat
 import tarfile
@@ -19,7 +20,11 @@ def dd_environment():
     with TempDir() as tmp_dir:
         activemq_data_dir = os.path.join(tmp_dir, "data")
         fixture_archive = os.path.join(HERE, "fixtures", "apache-activemq-kahadb.tar.gz")
-        os.mkdir(activemq_data_dir)
+        try:
+            os.mkdir(activemq_data_dir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+               raise 
         with tarfile.open(fixture_archive, "r:gz") as f:
             f.extractall(path=activemq_data_dir)
         os.chmod(os.path.join(activemq_data_dir, "kahadb"), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
