a7995129153ef9327c7077daf3a4094a001d7d41
Paul Coignet <paul.coignet@datadoghq.com>
2019-03-12 10:28:10 +0100

    address review

--- activemq_xml/tests/conftest.py
+++ activemq_xml/tests/conftest.py
@@ -2,7 +2,6 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-import errno
 import os
 import stat
 import tarfile
@@ -20,11 +19,10 @@ def dd_environment():
     with TempDir() as tmp_dir:
         activemq_data_dir = os.path.join(tmp_dir, "data")
         fixture_archive = os.path.join(HERE, "fixtures", "apache-activemq-kahadb.tar.gz")
-        try:
+
+        if not os.path.exists(activemq_data_dir):
             os.mkdir(activemq_data_dir)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
+
         with tarfile.open(fixture_archive, "r:gz") as f:
             f.extractall(path=activemq_data_dir)
         os.chmod(os.path.join(activemq_data_dir, "kahadb"), stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
