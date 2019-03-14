f49e6f7ec0c002c5ea2443f9711ceaaa6d9da34b
Paul Coignet <paul.coignet@datadoghq.com>
2019-03-14 17:26:53 +0100

    remove docker-cmpose

--- directory/tests/conftest.py
+++ directory/tests/conftest.py
@@ -1,12 +1,8 @@
 # (C) Datadog, Inc. 2019
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
-
-import os
-
 import pytest
 
-from datadog_checks.dev import docker_run
 from datadog_checks.directory import DirectoryCheck
 
 from . import common
@@ -14,11 +10,7 @@ from . import common
 
 @pytest.fixture(scope="session")
 def dd_environment():
-    compose_file = os.path.join(common.HERE, "compose", "docker-compose.yml")
-    with docker_run(
-        compose_file=compose_file,
-    ):
-        yield common.get_config_stubs(".")[0]
+    yield common.get_config_stubs(".")[0]
 
 
 @pytest.fixture
