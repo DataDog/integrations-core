096720bc5fd9bf23c489c7d823555b06d2324f79
Thomas Hervé <thomas.herve@datadoghq.com>
2019-03-20 16:45:46 +0100

    Add e2e

---
+++ prometheus/tests/conftest.py
@@ -0,0 +1,55 @@
+import mock
+import os
+import pytest
+
+from prometheus_client import generate_latest, CollectorRegistry, Gauge, Counter
+
+from datadog_checks.base import ensure_unicode
+from datadog_checks.dev import docker_run, get_docker_hostname
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+HOST = get_docker_hostname()
+
+INSTANCE = {
+    'prometheus_url': 'http://{}:9090/metrics'.format(HOST),
+    'namespace': 'prometheus',
+    'metrics': [
+        {'prometheus_target_interval_length_seconds': 'target_interval_seconds'},
+        'go_memstats_mallocs_total',
+    ]
+}
+
+
+@pytest.fixture(scope="session")
+def dd_environment():
+    compose_file = os.path.join(HERE, 'docker', 'docker-compose.yaml')
+    log_patterns = 'Server is ready to receive web requests'
+
+    with docker_run(compose_file, log_patterns=log_patterns, sleep=10):
+        yield INSTANCE
+
+
+@pytest.fixture
+def poll_mock():
+    registry = CollectorRegistry()
+    # pylint: disable=E1123,E1101
+    g1 = Gauge('metric1', 'processor usage', ['matched_label', 'node', 'flavor'], registry=registry)
+    g1.labels(matched_label="foobar", node="host1", flavor="test").set(99.9)
+    g2 = Gauge('metric2', 'memory usage', ['matched_label', 'node', 'timestamp'], registry=registry)
+    g2.labels(matched_label="foobar", node="host2", timestamp="123").set(12.2)
+    c1 = Counter('counter1', 'hits', ['node'], registry=registry)
+    c1.labels(node="host2").inc(42)
+    g3 = Gauge('metric3', 'memory usage', ['matched_label', 'node', 'timestamp'], registry=registry)
+    g3.labels(matched_label="foobar", node="host2", timestamp="456").set(float('inf'))
+
+    poll_mock_patch = mock.patch(
+        'requests.get',
+        return_value=mock.MagicMock(
+            status_code=200,
+            iter_lines=lambda **kwargs: ensure_unicode(generate_latest(registry)).split("\n"),
+            headers={'Content-Type': "text/plain"}
+        )
+    )
+    yield poll_mock_patch.start()
+    poll_mock_patch.stop()
