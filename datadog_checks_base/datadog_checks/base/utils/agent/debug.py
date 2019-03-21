71a06d460678d3e4ae93320e6c0389fb735e1281
Ofek Lev <ofekmeister@gmail.com>
2019-03-21 12:40:55 -0400

    Handle edge case for finding the first and last lines (#3342)

--- datadog_checks_base/datadog_checks/base/utils/agent/debug.py
+++ datadog_checks_base/datadog_checks/base/utils/agent/debug.py
@@ -36,27 +36,47 @@ def enter_pdb(f, line=0, args=(), kwargs=None):
     file_name = inspect.getfile(f)
     lines, def_line = inspect.getsourcelines(f)
 
+    start = def_line + 1
+    stop = def_line + len(lines)
+
+    # Get rid of the declaration (assuming it spans only one line)
+    del lines[:1]
+
     # Start of the function
     if line == 0:
-        line = def_line + 1
+        line = get_first_statement(lines, start)
     # End of the function
     elif line == -1:
-        line = def_line + len(lines) - 1
+        line = abs(get_first_statement(reversed(lines), -(stop - 1)))
     # Specific line number of the file where the function is defined
     else:
-        start = def_line + 1
-        stop = def_line + len(lines)
-
         # Ensure the line is actually within the function
         if line not in range(start, stop):
             raise ValueError('Line {} is not part of the check method.'.format(line))
 
-        # Ensure there is something to run or else the function will proceed normally
-        elif not lines[line - def_line].strip():
-            raise ValueError('Line {} is a blank line and therefore there is nothing to execute.'.format(line))
+        elif not is_statement(lines[line - def_line - 1]):
+            raise ValueError('There is nothing to execute on line {}.'.format(line))
 
     debugger = Debugger()
     debugger.set_break(file_name, line)
     debugger.set_trace()
 
     f(*args, **kwargs)
+
+
+def get_first_statement(lines, start):
+    for i, line in enumerate(lines, start):
+        if is_statement(line):
+            return i
+    else:
+        raise ValueError('There is nothing to execute on line {}.'.format(abs(start)))
+
+
+def is_statement(line):
+    line = line.strip()
+
+    # Blank lines, comments, or docstrings
+    if not line or line.startswith(('#', '"', "'")):
+        return False
+
+    return True
