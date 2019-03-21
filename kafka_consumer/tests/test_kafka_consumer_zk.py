6833fc6f14ac6f2f2bb28154385db64b01f31d65
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-03-21 10:45:29 +0100

    Properly cache zookeeper connection strings (#3333)

--- kafka_consumer/tests/test_kafka_consumer_zk.py
+++ kafka_consumer/tests/test_kafka_consumer_zk.py
@@ -2,12 +2,14 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 import copy
+import time
 
 import pytest
 from six import iteritems
 
+from datadog_checks.base.utils.containers import hash_mutable
 from datadog_checks.kafka_consumer import KafkaCheck
-from .common import HOST, PARTITIONS, TOPICS, is_supported
+from .common import HOST, PARTITIONS, TOPICS, ZK_CONNECT_STR, is_supported
 
 pytestmark = pytest.mark.skipif(
     not is_supported('zookeeper'),
@@ -97,3 +99,15 @@ def test_check_nogroups_zk(aggregator, zk_instance):
         else:
             for mname in BROKER_METRICS + CONSUMER_METRICS:
                 aggregator.assert_metric(mname, at_least=1)
+
+
+def test_should_zk():
+    check = KafkaCheck('kafka_consumer', {}, {})
+    # Kafka Consumer Offsets set to True and we have a zk_connect_str that hasn't been run yet
+    assert (check._should_zk([ZK_CONNECT_STR, ZK_CONNECT_STR], 10, True) is True)
+    # Kafka Consumer Offsets is set to False, should immediately ZK
+    assert (check._should_zk(ZK_CONNECT_STR, 10, False) is True)
+    # Last time we checked ZK_CONNECT_STR was less than interval ago, shouldn't ZK
+    zk_connect_hash = hash_mutable(ZK_CONNECT_STR)
+    check._zk_last_ts[zk_connect_hash] = time.time()
+    assert (check._should_zk(ZK_CONNECT_STR, 100, True) is False)
