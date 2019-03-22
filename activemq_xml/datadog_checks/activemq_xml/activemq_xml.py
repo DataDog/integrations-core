18b6da688101221224148f14d9774e285ed42f67
Christine <cchen7196@gmail.com>
2019-03-22 18:22:39 -0400

    update conf.yaml and http wrapper

--- activemq_xml/datadog_checks/activemq_xml/activemq_xml.py
+++ activemq_xml/datadog_checks/activemq_xml/activemq_xml.py
@@ -61,7 +61,7 @@ class ActiveMQXML(AgentCheck):
         url = "%s%s" % (base_url, xml_url)
         self.log.debug("ActiveMQ Fetching queue data from: %s" % url)
         try:
-            r = requests.get(url, auth=auth)
+            r = self.http.get(url, auth=auth)
             r.raise_for_status()
         except requests.exceptions.ConnectionError:
             if suppress_errors:
