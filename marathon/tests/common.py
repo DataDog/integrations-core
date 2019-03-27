b16b04f4641d46a824ea4a1e5e12b6bb4fee0048
Paul Coignet <paul.coignet@datadoghq.com>
2019-03-27 14:27:54 +0100

    add marathon e2e

--- marathon/tests/common.py
+++ marathon/tests/common.py
@@ -1,11 +1,27 @@
-# (C) Datadog, Inc. 2010-2017
+# (C) Datadog, Inc. 2010-2019
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
-import os
-
 from datadog_checks.utils.common import get_docker_hostname
+from datadog_checks.dev import get_here
 
 
-HERE = os.path.dirname(os.path.abspath(__file__))
+HERE = get_here()
 HOST = get_docker_hostname()
 PORT = 8080
+
+INSTANCE_INTEGRATION = {
+    'url': 'http://{}:{}'.format(HOST, PORT),
+    'tags': ['optional:tag1'],
+    'label_tags': ['LABEL_NAME'],
+    'enable_deployment_metrics': True,
+}
+
+EXPECTED_METRICS = [
+    'marathon.apps',
+    'marathon.deployments',
+    'marathon.queue.size',
+]
+
+EXPECTED_TAGS = [
+    'optional:tag1'
+]
