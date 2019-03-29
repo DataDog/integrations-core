73d93bf5afb1618ab859988780a609cbe6378bc8
Thomas Hervé <thomas.herve@datadoghq.com>
2019-03-29 16:31:17 +0100

    JBoss/WildFly JMX Integration (#3320)
    * Jboss/Wildfly JMX Integration
    
    * Rename to jboss_wildfly
    
    * Add Logos
    
    * Fix validation
    
    * Remove SVG sources
    
    * Remove unused classifier
    
    * Remove broken import
    
    * Add testing facilities
    
    * Apply suggestions from code review
    
    Co-Authored-By: therve <thomas.herve@datadoghq.com>
    
    * Some more naming cleanups
    
    * Other doc changes
    
    * Get back to jmx remote
    
    * Fix manifest
    
    * Use new e2e metadata
    
    * Update docker setup

---
+++ jboss_wildfly/CHANGELOG.md
@@ -0,0 +1,2 @@
+# CHANGELOG - JBoss/WildFly
+
---
+++ jboss_wildfly/tests/conftest.py
@@ -0,0 +1,18 @@
+# (C) Datadog, Inc. 2010-2018
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
+import os
+
+import pytest
+
+from datadog_checks.dev import docker_run
+from datadog_checks.dev.utils import load_jmx_config
+
+from .common import HERE
+
+
+@pytest.fixture(scope="session")
+def dd_environment():
+    with docker_run(os.path.join(HERE, 'docker', 'docker-compose.yml')):
+        yield load_jmx_config(), {'use_jmx': True}
