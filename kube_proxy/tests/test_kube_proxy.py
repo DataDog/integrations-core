7a6c224de55cad0041fe507d655933ae59791333
Ofek Lev <ofekmeister@gmail.com>
2019-04-07 00:53:04 -0400

    Adhere to code style

--- kube_proxy/tests/test_kube_proxy.py
+++ kube_proxy/tests/test_kube_proxy.py
@@ -3,17 +3,17 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 import os
-import pytest
-import mock
 
-# 3p
+import mock
+import pytest
 
 # project
 from datadog_checks.kube_proxy import KubeProxyCheck
 
-instance = {
-    'prometheus_url': 'http://localhost:10249/metrics',
-}
+# 3p
+
+
+instance = {'prometheus_url': 'http://localhost:10249/metrics'}
 
 # Constants
 CHECK_NAME = 'kube_proxy'
@@ -23,6 +23,7 @@ NAMESPACE = 'kubeproxy'
 @pytest.fixture
 def aggregator():
     from datadog_checks.stubs import aggregator
+
     aggregator.reset()
     return aggregator
 
@@ -35,10 +36,8 @@ def mock_iptables():
     mock_iptables = mock.patch(
         'requests.get',
         return_value=mock.MagicMock(
-            status_code=200,
-            iter_lines=lambda **kwargs: text_data.split("\n"),
-            headers={'Content-Type': "text/plain"}
-        )
+            status_code=200, iter_lines=lambda **kwargs: text_data.split("\n"), headers={'Content-Type': "text/plain"}
+        ),
     )
     yield mock_iptables.start()
     mock_iptables.stop()
@@ -52,10 +51,8 @@ def mock_userspace():
     mock_userspace = mock.patch(
         'requests.get',
         return_value=mock.MagicMock(
-            status_code=200,
-            iter_lines=lambda **kwargs: text_data.split("\n"),
-            headers={'Content-Type': "text/plain"}
-        )
+            status_code=200, iter_lines=lambda **kwargs: text_data.split("\n"), headers={'Content-Type': "text/plain"}
+        ),
     )
     yield mock_userspace.start()
     mock_userspace.stop()
@@ -71,12 +68,15 @@ def test_check_iptables(aggregator, mock_iptables):
     aggregator.assert_metric(NAMESPACE + '.cpu.time')
     aggregator.assert_metric(NAMESPACE + '.mem.resident')
     aggregator.assert_metric(NAMESPACE + '.mem.virtual')
-    aggregator.assert_metric(NAMESPACE + '.client.http.requests',
-                             tags=['method:GET', 'code:200', 'host:127.0.0.1:8080'])
-    aggregator.assert_metric(NAMESPACE + '.client.http.requests',
-                             tags=['method:POST', 'code:201', 'host:127.0.0.1:8080'])
-    aggregator.assert_metric(NAMESPACE + '.client.http.requests',
-                             tags=['method:GET', 'code:404', 'host:127.0.0.1:8080'])
+    aggregator.assert_metric(
+        NAMESPACE + '.client.http.requests', tags=['method:GET', 'code:200', 'host:127.0.0.1:8080']
+    )
+    aggregator.assert_metric(
+        NAMESPACE + '.client.http.requests', tags=['method:POST', 'code:201', 'host:127.0.0.1:8080']
+    )
+    aggregator.assert_metric(
+        NAMESPACE + '.client.http.requests', tags=['method:GET', 'code:404', 'host:127.0.0.1:8080']
+    )
     aggregator.assert_metric(NAMESPACE + '.sync_rules.latency.count')
     aggregator.assert_metric(NAMESPACE + '.sync_rules.latency.sum')
     aggregator.assert_all_metrics_covered()
@@ -91,12 +91,16 @@ def test_check_userspace(aggregator, mock_userspace):
     aggregator.assert_metric(NAMESPACE + '.cpu.time')
     aggregator.assert_metric(NAMESPACE + '.mem.resident')
     aggregator.assert_metric(NAMESPACE + '.mem.virtual')
-    aggregator.assert_metric(NAMESPACE + '.client.http.requests',
-                             tags=['method:GET', 'host:127.0.0.1:8080', 'code:200'])
-    aggregator.assert_metric(NAMESPACE + '.client.http.requests',
-                             tags=['method:POST', 'host:127.0.0.1:8080', 'code:201'])
-    aggregator.assert_metric(NAMESPACE + '.client.http.requests',
-                             tags=['method:GET', 'host:127.0.0.1:8080', 'code:200'])
-    aggregator.assert_metric(NAMESPACE + '.client.http.requests',
-                             tags=['method:POST', 'host:127.0.0.1:8080', 'code:201'])
+    aggregator.assert_metric(
+        NAMESPACE + '.client.http.requests', tags=['method:GET', 'host:127.0.0.1:8080', 'code:200']
+    )
+    aggregator.assert_metric(
+        NAMESPACE + '.client.http.requests', tags=['method:POST', 'host:127.0.0.1:8080', 'code:201']
+    )
+    aggregator.assert_metric(
+        NAMESPACE + '.client.http.requests', tags=['method:GET', 'host:127.0.0.1:8080', 'code:200']
+    )
+    aggregator.assert_metric(
+        NAMESPACE + '.client.http.requests', tags=['method:POST', 'host:127.0.0.1:8080', 'code:201']
+    )
     aggregator.assert_all_metrics_covered()
