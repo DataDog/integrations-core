c1698d58727a93013ccb82de4bd2d95905da720f
Ofek Lev <ofekmeister@gmail.com>
2019-04-07 00:44:50 -0400

    Adhere to code style

--- istio/datadog_checks/istio/__init__.py
+++ istio/datadog_checks/istio/__init__.py
@@ -2,10 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-from .istio import Istio # noqa
-from .__about__ import __version__ # noqa
+from .__about__ import __version__  # noqa
+from .istio import Istio  # noqa
 
-__all__ = [
-    'Istio'
-    '__version__'
-]
+__all__ = ['Istio' '__version__']
