3d0ca34ffd85d0360a4981a2c54528a631e355f6
Ofek Lev <ofekmeister@gmail.com>
2019-04-07 00:05:41 -0400

    Adhere to code style

--- aspdotnet/datadog_checks/aspdotnet/aspdotnet.py
+++ aspdotnet/datadog_checks/aspdotnet/aspdotnet.py
@@ -4,8 +4,10 @@
 try:
     from datadog_checks.base import PDHBaseCheck
 except ImportError:
+
     class PDHBaseCheck:
         pass
+
 
 EVENT_TYPE = SOURCE_TYPE_NAME = 'aspdotnet'
 
@@ -13,18 +15,40 @@ DEFAULT_COUNTERS = [
     # counterset, instance of counter, counter name, metric name
     # This set is from the Microsoft recommended counters to monitor exchange:
     # https://technet.microsoft.com/en-us/library/dn904093%28v=exchg.150%29.aspx?f=255&MSPPError=-2147217396
-
     # ASP.Net
-    ["ASP.NET",              None, "Application Restarts",          "aspdotnet.application_restarts",    "gauge"],
-    ["ASP.NET",              None, "Worker Process Restarts",       "aspdotnet.worker_process_restarts", "gauge"],
-    ["ASP.NET",              None, "Request Wait Time",             "aspdotnet.request.wait_time",       "gauge"],
-
+    ["ASP.NET", None, "Application Restarts", "aspdotnet.application_restarts", "gauge"],
+    ["ASP.NET", None, "Worker Process Restarts", "aspdotnet.worker_process_restarts", "gauge"],
+    ["ASP.NET", None, "Request Wait Time", "aspdotnet.request.wait_time", "gauge"],
     # ASP.Net Applications
-    ["ASP.NET Applications", None, "Requests In Application Queue", "aspdotnet.applications.requests.in_queue",       "gauge"],  # noqa: E501
-    ["ASP.NET Applications", None, "Requests Executing",            "aspdotnet.applications.requests.executing",      "gauge"],  # noqa: E501
-    ["ASP.NET Applications", None, "Requests/Sec",                  "aspdotnet.applications.requests.persec",         "gauge"],  # noqa: E501
-    ["ASP.NET Applications", None, "Forms Authentication Failure", "aspdotnet.applications.forms_authentication.failure",       "gauge"],  # noqa: E501
-    ["ASP.NET Applications", None, "Forms Authentication Success", "aspdotnet.applications.forms_authentication.successes",       "gauge"],  # noqa: E501
+    [
+        "ASP.NET Applications",
+        None,
+        "Requests In Application Queue",
+        "aspdotnet.applications.requests.in_queue",
+        "gauge",
+    ],  # noqa: E501
+    [
+        "ASP.NET Applications",
+        None,
+        "Requests Executing",
+        "aspdotnet.applications.requests.executing",
+        "gauge",
+    ],  # noqa: E501
+    ["ASP.NET Applications", None, "Requests/Sec", "aspdotnet.applications.requests.persec", "gauge"],  # noqa: E501
+    [
+        "ASP.NET Applications",
+        None,
+        "Forms Authentication Failure",
+        "aspdotnet.applications.forms_authentication.failure",
+        "gauge",
+    ],  # noqa: E501
+    [
+        "ASP.NET Applications",
+        None,
+        "Forms Authentication Success",
+        "aspdotnet.applications.forms_authentication.successes",
+        "gauge",
+    ],  # noqa: E501
 ]
 
 
