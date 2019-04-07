3975b40635ade62945498bf994274b5b53695c43
Ofek Lev <ofekmeister@gmail.com>
2019-04-07 00:06:45 -0400

    Adhere to code style

--- cassandra_nodetool/tests/conftest.py
+++ cassandra_nodetool/tests/conftest.py
@@ -2,16 +2,18 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-import subprocess
-import stat
-import os
 import logging
+import os
+import stat
+import subprocess
+
 import pytest
 
-from . import common
-from datadog_checks.dev import docker_run, TempDir
-from datadog_checks.dev.utils import copy_path
+from datadog_checks.dev import TempDir, docker_run
 from datadog_checks.dev.docker import get_container_ip
+from datadog_checks.dev.utils import copy_path
+
+from . import common
 
 log = logging.getLogger(__file__)
 
@@ -35,23 +37,22 @@ def dd_environment():
         env['JMX_PASS_FILE'] = temp_jmx_file
         os.chmod(temp_jmx_file, stat.S_IRWXU)
         with docker_run(
-            compose_file,
-            service_name=common.CASSANDRA_CONTAINER_NAME,
-            log_patterns=['Listening for thrift clients']
+            compose_file, service_name=common.CASSANDRA_CONTAINER_NAME, log_patterns=['Listening for thrift clients']
         ):
             cassandra_seed = get_container_ip("{}".format(common.CASSANDRA_CONTAINER_NAME))
             env['CASSANDRA_SEEDS'] = cassandra_seed
             with docker_run(
-                compose_file,
-                service_name=common.CASSANDRA_CONTAINER_NAME_2,
-                log_patterns=['All sessions completed']
+                compose_file, service_name=common.CASSANDRA_CONTAINER_NAME_2, log_patterns=['All sessions completed']
             ):
-                subprocess.check_call([
-                    "docker",
-                    "exec", common.CASSANDRA_CONTAINER_NAME,
-                    "cqlsh",
-                    "-e",
-                    "CREATE KEYSPACE IF NOT EXISTS test \
-                    WITH REPLICATION={'class':'SimpleStrategy', 'replication_factor':2}"
-                ])
+                subprocess.check_call(
+                    [
+                        "docker",
+                        "exec",
+                        common.CASSANDRA_CONTAINER_NAME,
+                        "cqlsh",
+                        "-e",
+                        "CREATE KEYSPACE IF NOT EXISTS test \
+                    WITH REPLICATION={'class':'SimpleStrategy', 'replication_factor':2}",
+                    ]
+                )
                 yield common.CONFIG_INSTANCE, 'local'
--- cassandra_nodetool/tests/test_integration.py
+++ cassandra_nodetool/tests/test_integration.py
@@ -4,8 +4,9 @@
 
 import pytest
 
-from . import common
 from datadog_checks.cassandra_nodetool import CassandraNodetoolCheck
+
+from . import common
 
 
 @pytest.mark.integration
@@ -16,8 +17,14 @@ def test_integration(aggregator, dd_environment):
     integration = CassandraNodetoolCheck(common.CHECK_NAME, {}, {})
     integration.check(common.CONFIG_INSTANCE)
 
-    aggregator.assert_metric('cassandra.nodetool.status.replication_availability', value=200,
-                             tags=['keyspace:test', 'datacenter:datacenter1', 'foo', 'bar'])
+    aggregator.assert_metric(
+        'cassandra.nodetool.status.replication_availability',
+        value=200,
+        tags=['keyspace:test', 'datacenter:datacenter1', 'foo', 'bar'],
+    )
 
-    aggregator.assert_metric('cassandra.nodetool.status.replication_factor', value=2,
-                             tags=['keyspace:test', 'datacenter:datacenter1', 'foo', 'bar'])
+    aggregator.assert_metric(
+        'cassandra.nodetool.status.replication_factor',
+        value=2,
+        tags=['keyspace:test', 'datacenter:datacenter1', 'foo', 'bar'],
+    )
