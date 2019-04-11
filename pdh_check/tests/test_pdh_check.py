7dfed941b23e721420bfd565e2ec7dc0473113e7
Ofek Lev <ofekmeister@gmail.com>
2019-04-11 12:10:28 -0400

    Adhere to code style (#3553)
    * Adhere to code style
    
    * fix

--- pdh_check/tests/test_pdh_check.py
+++ pdh_check/tests/test_pdh_check.py
@@ -3,10 +3,9 @@
 # Licensed under Simplified BSD License (see LICENSE)
 
 import pytest
+from datadog_test_libs.win.pdh_mocks import initialize_pdh_tests, pdh_mocks_fixture  # noqa: F401
 
 from datadog_checks.pdh_check import PDHCheck
-
-from datadog_test_libs.win.pdh_mocks import pdh_mocks_fixture, initialize_pdh_tests  # noqa: F401
 
 CHECK_NAME = 'pdh_check'
 
@@ -15,13 +14,10 @@ INSTANCE = {
     'metrics': [
         ['File Read Operations/sec', 'pdh.system.file_read_per_sec', 'gauge'],
         ['File Write Bytes/sec', 'pdh.system.file_write_bytes_sec', 'gauge'],
-    ]
+    ],
 }
 
-INSTANCE_METRICS = [
-    'pdh.system.file_read_per_sec',
-    'pdh.system.file_write_bytes_sec',
-]
+INSTANCE_METRICS = ['pdh.system.file_read_per_sec', 'pdh.system.file_write_bytes_sec']
 
 
 @pytest.mark.usefixtures('pdh_mocks_fixture')
@@ -30,9 +26,7 @@ def test_basic_check(aggregator):
     Returns the right metrics and service checks
     """
     # Set up & run the check
-    config = {
-        'instances': [INSTANCE]
-    }
+    config = {'instances': [INSTANCE]}
     initialize_pdh_tests()
     c = PDHCheck(CHECK_NAME, {}, {}, config['instances'])
     c.check(config['instances'][0])
