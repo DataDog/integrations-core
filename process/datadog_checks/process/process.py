c3fb7c1e6c0162a6fa7c14a99390e5fe1c73f29e
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-04-15 09:39:32 +0200

    Remove win32 & solaris check

--- process/datadog_checks/process/process.py
+++ process/datadog_checks/process/process.py
@@ -180,9 +180,7 @@ class ProcessCheck(AgentCheck):
             result = {}
 
         # Ban certain method that we know fail
-        if method == 'memory_info' and ProcessCheck._is_win32() or Platform.is_solaris():
-            return result
-        elif method == 'num_fds' and not Platform.is_unix():
+        if method == 'num_fds' and not Platform.is_unix():
             return result
         elif method == 'num_handles' and not Platform.is_win32():
             return result
@@ -493,9 +491,3 @@ class ProcessCheck(AgentCheck):
                 pass
 
         return filtered_pids
-
-    @staticmethod
-    def _is_win32():
-        is_64bits = sys.maxsize > 2 ** 32
-        # Python returns 'win32' for all versions of Windows for compatibility reasons
-        return Platform.is_win32() and not is_64bits
