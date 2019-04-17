39d3fc0904b7ee4f56f565af78a7d31438fdcfca
Ofek Lev <ofekmeister@gmail.com>
2019-04-17 17:41:53 -0400

    Adhere to code style (#3487)

--- cassandra_nodetool/tests/conftest.py
+++ cassandra_nodetool/tests/conftest.py
@@ -2,13 +2,15 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-import subprocess
 import os
+import subprocess
+
 import pytest
 
-from . import common
 from datadog_checks.dev import docker_run
 from datadog_checks.dev.docker import get_container_ip
+
+from . import common
 
 
 @pytest.fixture(scope="session")
@@ -21,23 +23,22 @@ def dd_environment():
     env['CONTAINER_PORT'] = common.PORT
 
     with docker_run(
-        compose_file,
-        service_name=common.CASSANDRA_CONTAINER_NAME,
-        log_patterns=['Listening for thrift clients']
+        compose_file, service_name=common.CASSANDRA_CONTAINER_NAME, log_patterns=['Listening for thrift clients']
     ):
         cassandra_seed = get_container_ip("{}".format(common.CASSANDRA_CONTAINER_NAME))
         env['CASSANDRA_SEEDS'] = cassandra_seed
         with docker_run(
-            compose_file,
-            service_name=common.CASSANDRA_CONTAINER_NAME_2,
-            log_patterns=['All sessions completed']
+            compose_file, service_name=common.CASSANDRA_CONTAINER_NAME_2, log_patterns=['All sessions completed']
         ):
-            subprocess.check_call([
-                "docker",
-                "exec", common.CASSANDRA_CONTAINER_NAME,
-                "cqlsh",
-                "-e",
-                "CREATE KEYSPACE IF NOT EXISTS test \
-                WITH REPLICATION={'class':'SimpleStrategy', 'replication_factor':2}"
-            ])
+            subprocess.check_call(
+                [
+                    "docker",
+                    "exec",
+                    common.CASSANDRA_CONTAINER_NAME,
+                    "cqlsh",
+                    "-e",
+                    "CREATE KEYSPACE IF NOT EXISTS test \
+                WITH REPLICATION={'class':'SimpleStrategy', 'replication_factor':2}",
+                ]
+            )
             yield common.CONFIG_INSTANCE, 'local'
