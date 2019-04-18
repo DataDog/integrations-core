709882cc06be1ff52e47ae5e6acc4e1832cc4999
Ofek Lev <ofekmeister@gmail.com>
2019-04-18 04:41:17 -0400

    Adhere to code style (#3502)
    * Adhere to code style
    
    * remove

--- dotnetclr/tests/common.py
+++ dotnetclr/tests/common.py
@@ -1,9 +1,7 @@
 # (C) Datadog, Inc. 2010-2016
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
-MINIMAL_INSTANCE = {
-    'host': '.',
-}
+MINIMAL_INSTANCE = {'host': '.'}
 
 CHECK_NAME = 'active_directory'
 
--- dotnetclr/tests/test_dotnetclr.py
+++ dotnetclr/tests/test_dotnetclr.py
@@ -2,16 +2,12 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 import pytest
+from datadog_test_libs.win.pdh_mocks import initialize_pdh_tests, pdh_mocks_fixture  # noqa: F401
 
-from .common import (
-    MINIMAL_INSTANCE,
-    CHECK_NAME,
-    INSTANCES,
-)
 from datadog_checks.dotnetclr import DotnetclrCheck
 from datadog_checks.dotnetclr.dotnetclr import DEFAULT_COUNTERS
 
-from datadog_test_libs.win.pdh_mocks import pdh_mocks_fixture, initialize_pdh_tests  # noqa: F401
+from .common import CHECK_NAME, INSTANCES, MINIMAL_INSTANCE
 
 
 @pytest.mark.usefixtures('pdh_mocks_fixture')
