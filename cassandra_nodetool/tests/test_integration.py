3f725d205ab09df02e8b34df9dd810cbf9495262
Thomas Hervé <thomas.herve@datadoghq.com>
2019-04-19 11:15:16 +0200

    Run tests with Cassandra 3.0 (#3641)

--- cassandra_nodetool/tests/test_integration.py
+++ cassandra_nodetool/tests/test_integration.py
@@ -2,6 +2,8 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
+import time
+
 import pytest
 
 from datadog_checks.cassandra_nodetool import CassandraNodetoolCheck
@@ -15,7 +17,20 @@ def test_integration(aggregator, dd_environment):
     Testing Cassandra Nodetool Integration
     """
     integration = CassandraNodetoolCheck(common.CHECK_NAME, {}, {})
-    integration.check(common.CONFIG_INSTANCE)
+    # Starting with recent Cassandra versions, replication takes some time to
+    # warm up, let's retry a few times.
+    for _ in range(20):
+        integration.check(common.CONFIG_INSTANCE)
+        try:
+            aggregator.assert_metric(
+                'cassandra.nodetool.status.replication_availability',
+                value=200,
+                tags=['keyspace:test', 'datacenter:datacenter1', 'foo', 'bar'],
+            )
+        except AssertionError:
+            time.sleep(5)
+        else:
+            break
 
     aggregator.assert_metric(
         'cassandra.nodetool.status.replication_availability',
