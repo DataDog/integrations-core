eec5efad6a3868616d863617d4442370deb8fe14
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2019-04-24 15:29:48 +0200

    filter golang metrics

--- kube_metrics_server/datadog_checks/kube_metrics_server/kube_metrics_server.py
+++ kube_metrics_server/datadog_checks/kube_metrics_server/kube_metrics_server.py
@@ -26,35 +26,10 @@ GAUGES = {
     'process_virtual_memory_bytes': 'process.virtual_memory_bytes',
 }
 
-GO_METRICS = {
-    'go_gc_duration_seconds': 'go.gc_duration_seconds',
-    'go_goroutines': 'go.goroutines',
-    'go_memstats_alloc_bytes': 'go.memstats.alloc_bytes',
-    'go_memstats_alloc_bytes_total': 'go.memstats.alloc_bytes_total',
-    'go_memstats_buck_hash_sys_bytes': 'go.memstats.buck_hash_sys_bytes',
-    'go_memstats_frees_total': 'go.memstats.frees_total',
-    'go_memstats_gc_sys_bytes': 'go.memstats.gc_sys_bytes',
-    'go_memstats_heap_alloc_bytes': 'go.memstats.heap_alloc_bytes',
-    'go_memstats_heap_idle_bytes': 'go.memstats.heap_idle_bytes',
-    'go_memstats_heap_inuse_bytes': 'go.memstats.heap_inuse_bytes',
-    'go_memstats_heap_objects': 'go.memstats.heap_objects',
-    'go_memstats_heap_released_bytes_total': 'go.memstats.heap_released_bytes_total',
-    'go_memstats_heap_sys_bytes': 'go.memstats.heap_sys_bytes',
-    'go_memstats_last_gc_time_seconds': 'go.memstats.last_gc_time_seconds',
-    'go_memstats_lookups_total': 'go.memstats.lookups_total',
-    'go_memstats_mallocs_total': 'go.memstats.mallocs_total',
-    'go_memstats_mcache_inuse_bytes': 'go.memstats.mcache_inuse_bytes',
-    'go_memstats_mcache_sys_bytes': 'go.memstats.mcache_sys_bytes',
-    'go_memstats_mspan_inuse_bytes': 'go.memstats.mspan_inuse_bytes',
-    'go_memstats_mspan_sys_bytes': 'go.memstats.mspan_sys_bytes',
-    'go_memstats_next_gc_bytes': 'go.memstats.next_gc_bytes',
-    'go_memstats_other_sys_bytes': 'go.memstats.other_sys_bytes',
-    'go_memstats_stack_inuse_bytes': 'go.memstats.stack_inuse_bytes',
-    'go_memstats_stack_sys_bytes': 'go.memstats.stack_sys_bytes',
-    'go_memstats_sys_bytes': 'go.memstats.sys_bytes',
-}
+GO_METRICS = {'go_gc_duration_seconds': 'go.gc_duration_seconds', 'go_goroutines': 'go.goroutines'}
 
 IGNORED_METRICS = [
+    # Should be reported via API Server Check
     'http_requests_total',
     'http_request_size_bytes',
     'http_response_size_bytes',
@@ -65,11 +40,36 @@ IGNORED_METRICS = [
     'apiserver_storage_data_key_generation_failures_total',
     'apiserver_storage_data_key_generation_latencies_microseconds',
     'apiserver_storage_envelope_transformation_cache_misses_total',
+    # Should be reported via ETCD Check
     'etcd_helper_cache_entry_count',
     'etcd_helper_cache_hit_count',
     'etcd_helper_cache_miss_count',
     'etcd_request_cache_add_latencies_summary',
     'etcd_request_cache_get_latencies_summary',
+    # Should be reported via Golang Check
+    'go_memstats_alloc_bytes',
+    'go_memstats_alloc_bytes_total',
+    'go_memstats_buck_hash_sys_bytes',
+    'go_memstats_frees_total',
+    'go_memstats_gc_sys_bytes',
+    'go_memstats_heap_alloc_bytes',
+    'go_memstats_heap_idle_bytes',
+    'go_memstats_heap_inuse_bytes',
+    'go_memstats_heap_objects',
+    'go_memstats_heap_released_bytes_total',
+    'go_memstats_heap_sys_bytes',
+    'go_memstats_last_gc_time_seconds',
+    'go_memstats_lookups_total',
+    'go_memstats_mallocs_total',
+    'go_memstats_mcache_inuse_bytes',
+    'go_memstats_mcache_sys_bytes',
+    'go_memstats_mspan_inuse_bytes',
+    'go_memstats_mspan_sys_bytes',
+    'go_memstats_next_gc_bytes',
+    'go_memstats_other_sys_bytes',
+    'go_memstats_stack_inuse_bytes',
+    'go_memstats_stack_sys_bytes',
+    'go_memstats_sys_bytes',
 ]
 
 
