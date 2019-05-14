1605851dc8a04322611b54c9358477a18e9c5e5d
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-05-14 10:33:26 +0200

    Add e2e tests to cacti

---
+++ cacti/tests/common.py
@@ -0,0 +1,19 @@
+# (C) Datadog, Inc. 2019
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+from datadog_checks.dev import get_docker_hostname, get_here
+from datadog_checks.cacti import CactiCheck
+
+HERE = get_here()
+HOST = get_docker_hostname()
+
+# ID
+CONTAINER_NAME = "dd-test-cacti"
+
+RRD_PATH = '/var/www/html/cacti/rra'
+
+INSTANCE_INTEGRATION = {'resourcemanager_uri': HOST, 'collect_task_metrics': True}
+
+EXPECTED_METRICS = {
+}
---
+++ cacti/tests/conftest.py
@@ -0,0 +1,37 @@
+# (C) Datadog, Inc. 2019
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+import os
+import pytest
+from copy import deepcopy
+
+from datadog_checks.dev import docker_run
+from datadog_checks.dev.conditions import WaitFor
+from datadog_checks.cacti import CactiCheck
+
+from .common import (
+    HOST,
+    INSTANCE_INTEGRATION
+)
+
+
+@pytest.fixture(scope="session")
+def dd_environment():
+    env = {'HOSTNAME': HOST}
+    with docker_run(
+        compose_file=os.path.join(HERE, "compose", "docker-compose.yaml"),
+        #conditions=[WaitFor(setup_mapreduce, attempts=240, wait=5)],
+        env_vars=env,
+    ):
+        yield INSTANCE_INTEGRATION
+
+
+@pytest.fixture
+def check():
+    return CactiCheck('mapreduce', {}, {})
+
+
+@pytest.fixture
+def instance():
+    return deepcopy(INSTANCE_INTEGRATION)
