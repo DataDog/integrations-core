0c8b2909346742c22648ab6ad893a4d1b48acb71
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-05-22 16:14:08 +0200

    Add install script

--- nagios/tests/common.py
+++ nagios/tests/common.py
@@ -10,9 +10,51 @@ HOST = get_docker_hostname()
 
 CONTAINER_NAME = "dd-test-nagios"
 
-INSTANCE_INTEGRATION = {'nagios_conf': '/usr/local/nagios/etc/nagios.cfg',
-                        'collect_host_performance_data': True,
-                        'collect_task_metrics': True}
+INSTANCE_INTEGRATION = {
+    'nagios_conf': '/usr/local/nagios/etc/nagios.cfg',
+    'collect_host_performance_data': True,
+    'collect_task_metrics': True
+}
+
+E2E_METADATA = {
+    'start_commands': [
+        # Install prerequistes
+        'apt-get update',
+        'apt-get install wget build-essential apache2 php php-gd libgd-dev sendmail unzip libapache2-mod-php7.3 -y',
+        # User and group configuration
+        'useradd nagios',
+        'groupadd nagcmd',
+        'usermod -a -G nagcmd nagios',
+        'usermod -a -G nagios,nagcmd www-data',
+        # Download and extract nagios core
+        'wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.2.0.tar.gz',
+        'tar -xzf nagios*.tar.gz',
+        # Compile nagios
+        '(cd nagios-4.2.0; ./configure --with-nagios-group=nagios --with-command-group=nagcmd)',
+        '(cd nagios-4.2.0; make all)'
+        '(cd nagios-4.2.0; make install && make install-commandmode &&  make install-init && make install-config)',
+        '(cd nagios-4.2.0; install -c -m 644 sample-config/httpd.conf /etc/apache2/sites-available/nagios.conf)',
+        '(cd nagios-4.2.0; cp -R contrib/eventhandlers/ /usr/local/nagios/libexec/)',
+        'chown -R nagios:nagios /usr/local/nagios/libexec/eventhandlers',
+        # Install the Nagios plugins
+        'wget https://nagios-plugins.org/download/nagios-plugins-2.1.2.tar.gz',
+        'tar -xzf nagios-plugins*.tar.gz',
+        '(cd nagios-plugins-2.1.2/; bash configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl)',
+        '(cd nagios-plugins-2.1.2/; make && make install)',
+        # Configure nagios
+        "sed -i 's/#cfg_dir=\/usr\/local\/nagios\/etc\/servers/cfg_dir=\/usr\/local\/nagios\/etc\/servers/' /usr/local/nagios/etc/nagios.cfg"  # Uncomment cfg_dir,
+        'mkdir -p /usr/local/nagios/etc/servers',
+        # Configure Apache
+        # Enable apache modules
+        'a2enmod rewrite',
+        'a2enmod cgi',
+        # Enable nagios virtualhost
+        'ln -s /etc/apache2/sites-available/nagios.conf /etc/apache2/sites-enabled/',
+        # Restart Apache and Nagios
+        'service apache2 restart'
+        'service nagios start'
+    ]
+}
 
 EXPECTED_METRICS = ["rta", "pl"]
 
--- nagios/tests/conftest.py
+++ nagios/tests/conftest.py
@@ -2,17 +2,14 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-import os
 import pytest
 from copy import deepcopy
 
-from datadog_checks.dev import docker_run
 from datadog_checks.nagios import NagiosCheck
 
 from .common import (
-    HERE,
-    HOST,
-    INSTANCE_INTEGRATION
+    INSTANCE_INTEGRATION,
+    E2E_METADATA
 )
 
 
@@ -26,12 +23,7 @@ def aggregator():
 
 @pytest.fixture(scope="session")
 def dd_environment():
-    env = {'HOST': HOST}
-    with docker_run(
-        compose_file=os.path.join(HERE, "compose", "docker-compose.yaml"),
-        env_vars=env,
-    ):
-        yield INSTANCE_INTEGRATION
+    return INSTANCE_INTEGRATION#, E2E_METADATA
 
 
 @pytest.fixture
