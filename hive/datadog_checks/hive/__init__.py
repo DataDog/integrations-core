7684f4bcd184e2538a9a0f2714b928dc39060ae1
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-06-04 14:31:36 +0200

    fix hive init file

--- hive/datadog_checks/hive/__init__.py
+++ hive/datadog_checks/hive/__init__.py
@@ -2,9 +2,5 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
-from .hive import HiveCheck
 
-__all__ = [
-    '__version__',
-    'HiveCheck'
-]
+__all__ = ['__version__']
