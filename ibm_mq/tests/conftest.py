f2fa9fada80a1392e0d0dc9e7db858e56f3a97c4
Ofek Lev <ofekmeister@gmail.com>
2019-06-07 12:49:58 -0400

    Support Windows
    See https://github.com/docker/compose/issues/6527

--- ibm_mq/tests/conftest.py
+++ ibm_mq/tests/conftest.py
@@ -9,6 +9,7 @@ import pytest
 from six.moves import range
 
 from datadog_checks.dev import docker_run
+from datadog_checks.dev.conditions import CheckDockerLogs
 from datadog_checks.ibm_mq import IbmMqCheck
 
 from . import common
@@ -113,5 +114,10 @@ def dd_environment():
 
     env = {'COMPOSE_DIR': common.COMPOSE_DIR}
 
-    with docker_run(common.COMPOSE_FILE_PATH, env_vars=env, log_patterns=log_pattern, sleep=10):
+    with docker_run(
+        common.COMPOSE_FILE_PATH,
+        env_vars=env,
+        conditions=[CheckDockerLogs('ibm_mq', log_pattern)],
+        sleep=10,
+    ):
         yield common.INSTANCE, common.E2E_METADATA
