535d41b81c9dce592285498eae2c9129aec097dc
florian.veaux <florian.veaux@datadoghq.com>
2019-06-11 13:07:49 -0400

    Address review

--- gunicorn/tests/conftest.py
+++ gunicorn/tests/conftest.py
@@ -13,7 +13,7 @@ import pytest
 
 from datadog_checks.dev import docker_run, temp_dir
 
-from .common import FIXTURES, HERE, INSTANCE, PROC_NAME
+from .common import COMPOSE, INSTANCE, PROC_NAME
 
 log = logging.getLogger('test_gunicorn')
 
@@ -21,7 +21,7 @@ log = logging.getLogger('test_gunicorn')
 @pytest.fixture(scope='session')
 def dd_environment():
     os.environ['PROC_NAME'] = PROC_NAME
-    compose_file = os.path.join(HERE, 'compose', 'docker-compose.yaml')
+    compose_file = os.path.join(COMPOSE, 'docker-compose.yaml')
     with docker_run(compose_file, log_patterns=['Booting worker with pid']):
         yield INSTANCE
 
@@ -85,14 +85,14 @@ def install_pip_packages(venv_bin_path):
 
 
 def copy_config_files(conf_file, app_dir):
-    shutil.copyfile(os.path.join(FIXTURES, 'conf.py'), conf_file)
+    shutil.copyfile(os.path.join(COMPOSE, 'conf.py'), conf_file)
 
     with open(conf_file, 'a') as f:
         f.write('chdir = "{}"'.format(app_dir))
 
     app_file = os.path.join(app_dir, 'app.py')
 
-    shutil.copyfile(os.path.join(FIXTURES, 'app.py'), app_file)
+    shutil.copyfile(os.path.join(COMPOSE, 'app.py'), app_file)
 
 
 def start_gunicorn(venv_bin_path, conf_file):
