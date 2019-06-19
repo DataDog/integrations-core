4149d3d92358aaee0831eea9da5dbff544303d3a
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-06-19 16:34:52 +0200

    [ibm_mq] fix lint

--- ibm_mq/datadog_checks/ibm_mq/config.py
+++ ibm_mq/datadog_checks/ibm_mq/config.py
@@ -52,8 +52,10 @@ class IBMMQConfig:
         self.auto_discover_queues = is_affirmative(instance.get('auto_discover_queues', False))
 
         if int(self.auto_discover_queues) + int(bool(self.queue_patterns)) + int(bool(self.queue_regex)) > 1:
-            log.warning("Configurations auto_discover_queues, queue_patterns and queue_regex are not intended to be "
-                        "used together")
+            log.warning(
+                "Configurations auto_discover_queues, queue_patterns and queue_regex are not intended to be used "
+                "together."
+            )
 
         self.channels = instance.get('channels', [])
 
