69e9fcc5bdbf53ab067b8adcf1125e48710646ee
Marc Ian Bucad <ian.bucad@gmail.com>
2019-06-20 15:27:09 +1000

    apply suggestion

--- postgres/tests/test_custom_metrics.py
+++ postgres/tests/test_custom_metrics.py
@@ -43,6 +43,7 @@ def test_custom_queries(aggregator, pg_instance):
                     'metric_prefix': 'custom',
                     'query': "SELECT letter, num FROM (VALUES (97, 'a'), (98, 'b'), (99, 'c')) AS t (num,letter)",
                     'columns': [{'name': 'customtag', 'type': 'tag'}, {'name': 'num', 'type': 'gauge'}],
+                    'tags': ['query:custom'],
                 },
                 {
                     'metric_prefix': 'another_custom_one',
@@ -67,5 +68,5 @@ def test_custom_queries(aggregator, pg_instance):
         custom_tags = ['customtag:{}'.format(tag)]
         custom_tags.extend(tags)
 
-        aggregator.assert_metric('custom.num', value=value, tags=custom_tags)
+        aggregator.assert_metric('custom.num', value=value, tags=custom_tags + ['query:custom'])
         aggregator.assert_metric('another_custom_one.num', value=value, tags=custom_tags + ['query:another_custom_one'])
