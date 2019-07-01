93ec11908b85d2bf7872116f95e8d71580f14246
Ofek Lev <ofekmeister@gmail.com>
2019-07-01 13:29:36 -0400

    Update setup.py

--- datadog_checks_base/setup.py
+++ datadog_checks_base/setup.py
@@ -61,8 +61,8 @@ setup(
     packages=['datadog_checks'],
     include_package_data=True,
     extras_require={
-        'deps': get_requirements('requirements.in', exclude=['kubernetes', 'requests-kerberos', 'requests_ntlm']),
-        'auth': get_requirements('requirements.in', only=['requests-kerberos', 'requests_ntlm']),
+        'deps': get_requirements('requirements.in', exclude=['kubernetes', 'pysocks', 'requests-kerberos', 'requests_ntlm', 'win-inet-pton']),
+        'http': get_requirements('requirements.in', only=['pysocks', 'requests-kerberos', 'requests_ntlm', 'win-inet-pton']),
         'kube': get_requirements('requirements.in', only=['kubernetes']),
     },
 )
