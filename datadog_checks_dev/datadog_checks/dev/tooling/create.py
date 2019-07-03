6965e9872d857154c4cefad01ef2049fd0fd7abd
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-07-03 10:53:36 -0400

    Apply suggestions from code review
    Co-Authored-By: Ofek Lev <ofekmeister@gmail.com>

--- datadog_checks_dev/datadog_checks/dev/tooling/create.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/create.py
@@ -55,11 +55,11 @@ def construct_template_fields(integration_name, repo_choice, **kwargs):
             '1. Install the [developer toolkit]'
             '(https://docs.datadoghq.com/developers/integrations/new_check_howto/#developer-toolkit)'
             ' on any machine.\n'
-            '2. Run ddev release build {normalized_integration_name} to build the package.\n'
+            '2. Run `ddev release build {normalized_integration_name}` to build the package.\n'
             '3. [Download the Datadog Agent](https://app.datadoghq.com/account/settings#agent).\n'
             '4. Upload the build artifact to any host with an Agent and'
-            'run datadog-agent integration install -w'
-            ' path/to/{normalized_integration_name}/dist/<ARTIFACT_NAME>.whl.\n'.format(
+            'run `datadog-agent integration install -w'
+            ' path/to/{normalized_integration_name}/dist/<ARTIFACT_NAME>.whl`.'.format(
                 integration_name=integration_name, normalized_integration_name=normalized_integration_name
             )
         )
