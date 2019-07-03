e73dc7d3bdd806b30d4d9eab009b4f48799b1acb
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-07-03 09:04:34 -0400

    Update datadog_checks_dev/datadog_checks/dev/tooling/create.py
    Co-Authored-By: FlorianVeaux <florian.veaux@datadoghq.com>

--- datadog_checks_dev/datadog_checks/dev/tooling/create.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/create.py
@@ -54,7 +54,7 @@ def construct_template_fields(integration_name, repo_choice, **kwargs):
             'To install the {integration_name} check on your host:\n\n'
             '    1. Install the [developer toolkit](https://docs.datadoghq.com/developers/integrations/new_check_howto/#developer-toolkit)'
             ' on any machine.\n'
-            '    2. Run ddev release build aqua to build the package.\n'
+            '    2. Run ddev release build {integration_name} to build the package.\n'.format(integration_name=integration_name)
             '    3. [Download the Datadog Agent](https://app.datadoghq.com/account/settings#agent).\n'
             '    4. Upload the build artifact to any host with an Agent and'
             'run datadog-agent integration install -w path/to/{integration_name}/dist/<ARTIFACT_NAME>.whl.\n'
