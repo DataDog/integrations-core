d59d9e380201fa34150f499d3f1848a99572f00e
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2019-07-03 00:27:50 -0400

    Add back links

--- datadog_checks_dev/datadog_checks/dev/tooling/create.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/create.py
@@ -52,11 +52,12 @@ def construct_template_fields(integration_name, repo_choice, **kwargs):
         email = email_packages = 'friend@datadog.community'
         install_info = (
             'To install the {integration_name} check on your host:\n\n'
-            '    1. Install the developer toolkit on any machine.\n'
+            '    1. Install the [developer toolkit](https://docs.datadoghq.com/developers/integrations/new_check_howto/#developer-toolkit)'
+            ' on any machine.\n'
             '    2. Run ddev release build aqua to build the package.\n'
-            '    3. Download the Datadog Agent.\n'
+            '    3. [Download the Datadog Agent](https://app.datadoghq.com/account/settings#agent).\n'
             '    4. Upload the build artifact to any host with an Agent and'
-            'run datadog-agent integration install -w path/to/{integration_name}}/dist/<ARTIFACT_NAME>.whl.\n'
+            'run datadog-agent integration install -w path/to/{integration_name}/dist/<ARTIFACT_NAME>.whl.\n'
             .format(integration_name=integration_name)
         )
         license_header = ''
