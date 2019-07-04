1f638cb2cae2c979d8cdf0cb7648d8676f55cb5b
Ofek Lev <ofekmeister@gmail.com>
2019-07-04 03:18:33 -0400

    o

--- .azure-pipelines/scripts/run.py
+++ .azure-pipelines/scripts/run.py
@@ -2,7 +2,6 @@ import os
 import re
 import subprocess
 import sys
-import time
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,10 +11,7 @@ def main():
         return
 
     checks = sys.argv[1].strip()
-    print(f'Checks chosen: {checks}')
-
     if checks == 'changed':
-        print('Detecting changed checks...')
         result = subprocess.run(['ddev', 'test', '--list'], encoding='utf-8', capture_output=True, check=True)
         checks = sorted(c.strip('`') for c in re.findall('^`[^`]+`', result.stdout, re.M))
     else:
@@ -26,17 +22,9 @@ def main():
         if not os.path.isdir(scripts_path):
             continue
 
-        print(f'Setting up: {check}')
-
         scripts = sorted(os.listdir(scripts_path))
         for script in scripts:
             script_file = os.path.join(scripts_path, script)
-            display_header = f'Running: {script_file}'
-            print(f'\n{display_header}\n{"-" * len(display_header)}\n')
-
-            # Sometimes the scripts output before our header
-            time.sleep(10)
-
             subprocess.run([script_file], shell=True, check=True)
 
 
