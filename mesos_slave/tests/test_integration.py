4581ae5ade9fb8e14b9f8ce747f1c7a32c33706d
Paul Coignet <paul.coignet@datadoghq.com>
2019-07-05 16:55:13 +0200

    Uncomment integration test

---
+++ mesos_slave/tests/test_integration.py
@@ -0,0 +1,40 @@
+# (C) Datadog, Inc. 2019
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import platform
+
+import pytest
+from six import iteritems
+
+pytestmark = pytest.mark.integration
+
+
+@pytest.mark.integration
+# Linux only: https://github.com/docker/for-mac/issues/1031
+@pytest.mark.skipif(platform.system() != 'Linux', reason="Only runs on Unix systems")
+@pytest.mark.usefixtures("dd_environment")
+def test_integration(check, instance, aggregator):
+    check.check(instance)
+    metrics = {}
+    for d in (
+        check.SLAVE_TASKS_METRICS,
+        check.SYSTEM_METRICS,
+        check.SLAVE_RESOURCE_METRICS,
+        check.SLAVE_EXECUTORS_METRICS,
+        check.STATS_METRICS,
+    ):
+        metrics.update(d)
+
+    for _, v in iteritems(check.TASK_METRICS):
+        aggregator.assert_metric(v[0])
+    for _, v in iteritems(metrics):
+        aggregator.assert_metric(v[0])
+
+    service_check_tags = [
+        'instance:mytag1',
+        'mesos_cluster:test',
+        'mesos_node:slave',
+        'mesos_pid:slave(1)@127.0.0.1:5051',
+        'task_name:hello',
+    ]
+    aggregator.assert_service_check('hello.ok', tags=service_check_tags, count=1, status=check.OK)
