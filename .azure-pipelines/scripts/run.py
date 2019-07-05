c8cbd7342af9589e6bada31a7e507bd9c8d65673
Ofek Lev <ofekmeister@gmail.com>
2019-07-05 00:34:04 -0400

    Ss (#11)
    * i
    
    * update perms of 05_increase_docker_memory.sh
    
    * update perms of 01_disable_hugepages.sh
    
    * update perms of 01_install_odbc.sh
    
    * update perms of 01_install_client.sh
    
    * update perms of 01_install_kerberos.sh
    
    * i
    
    * Update run.py
    
    * Update run.py
    
    * o

--- .azure-pipelines/scripts/run.py
+++ .azure-pipelines/scripts/run.py
@@ -5,32 +5,52 @@ import subprocess
 import sys
 
 HERE = os.path.dirname(os.path.abspath(__file__))
-WINDOWS = platform.system() == 'Windows'
+PLATFORM = (
+    'windows' if platform.system() == 'Windows'
+    else 'macos' if platform.system() == 'Darwin'
+    else 'linux'
+)
+
+
+def display_action(script_file):
+    display_header = f'Running: {script_file}'
+    print(f'\n{display_header}\n{"-" * len(display_header)}\n')
 
 
 def main():
     if len(sys.argv) == 1:
         return
 
-    checks = sys.argv[1].strip()
-    if checks == 'changed':
+    checks = [c.strip() for c in sys.argv[1:]]
+    print(f'Checks chosen: {repr(checks).strip("[]")}')
+
+    if 'changed' in checks:
+        print('Detecting changed checks...')
         result = subprocess.run(['ddev', 'test', '--list'], encoding='utf-8', capture_output=True, check=True)
         checks = sorted(c.strip('`') for c in re.findall('^`[^`]+`', result.stdout, re.M))
     else:
-        checks = sorted(c for c in checks.split() if c and not c.startswith('-'))
+        checks = sorted(c for c in checks if c and not c.startswith('-'))
 
     for check in checks:
-        scripts_path = os.path.join(HERE, check)
-        if not os.path.isdir(scripts_path):
+        check_path = os.path.join(HERE, check)
+        if not os.path.isdir(check_path):
             continue
 
-        scripts = sorted(os.listdir(scripts_path))
-        for script in scripts:
-            if WINDOWS and script.endswith('.sh'):
-                continue
+        print(f'\nSetting up: {check}')
 
-            script_file = os.path.join(scripts_path, script)
-            subprocess.run([script_file], shell=True, check=True)
+        contents = os.listdir(check_path)
+        if 'run.py' in contents:
+            script_file = os.path.join(check_path, 'run.py')
+            display_action(script_file)
+            subprocess.run([sys.executable, script_file], check=True)
+        elif PLATFORM in contents:
+            scripts_path = os.path.join(check_path, PLATFORM)
+            scripts = sorted(os.listdir(scripts_path))
+
+            for script in scripts:
+                script_file = os.path.join(scripts_path, script)
+                display_action(script_file)
+                subprocess.run([script_file], shell=True, check=True)
 
 
 if __name__ == '__main__':
