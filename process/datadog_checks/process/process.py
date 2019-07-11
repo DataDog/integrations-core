c4b1b69819cbf27263f5228a1db5ddb514dd1d5d
Paul Coignet <paul.coignet@datadoghq.com>
2019-07-11 10:04:58 +0200

    Remove useless comment

--- process/datadog_checks/process/process.py
+++ process/datadog_checks/process/process.py
@@ -457,7 +457,6 @@ class ProcessCheck(AgentCheck):
             warning = bounds.get('warning', [1, float('inf')])
             critical = bounds.get('critical', [1, float('inf')])
 
-            # -1 disable the boundary
             if warning[1] < nb_procs or nb_procs < warning[0]:
                 status = AgentCheck.WARNING
             if critical[1] < nb_procs or nb_procs < critical[0]:
