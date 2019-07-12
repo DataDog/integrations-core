2828ca0bbdbdcb6bdc52c7066d23aafdaa8b9348
Ofek Lev <ofekmeister@gmail.com>
2019-07-11 21:21:30 -0400

    Remove unused import (#4104)

--- disk/datadog_checks/disk/disk.py
+++ disk/datadog_checks/disk/disk.py
@@ -19,13 +19,6 @@ try:
 except ImportError:
     psutil = None
 
-try:
-    import datadog_agent  # noqa: F401
-
-    is_agent_6 = True
-except ImportError:
-    is_agent_6 = False
-
 # See: https://github.com/DataDog/integrations-core/pull/1109#discussion_r167133580
 IGNORE_CASE = re.I if platform.system() == 'Windows' else 0
 
