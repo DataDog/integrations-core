c0643f62a62b26f0329fb4c481c278903d09bff6
Thomas Hervé <thomas.herve@datadoghq.com>
2019-07-23 17:54:46 +0200

    Save output result

--- openstack_controller/tests/conftest.py
+++ openstack_controller/tests/conftest.py
@@ -15,13 +15,9 @@ def dd_environment():
     if not os.environ.get('TF_VAR_account_json'):
         pytest.skip('TF_VAR_account_json not set')
     with terraform_run(os.path.join(get_here(), 'terraform')) as outputs:
-        if not outputs:
-            # We're stopping the environment, we need fake values
-            ip = internal_ip = private_key = ''
-        else:
-            ip = outputs['ip']['value']
-            internal_ip = outputs['internal_ip']['value']
-            private_key = outputs['ssh_private_key']['value']
+        ip = outputs['ip']['value']
+        internal_ip = outputs['internal_ip']['value']
+        private_key = outputs['ssh_private_key']['value']
         instance = {
             'name': 'test',
             'keystone_server_url': 'http://{}/identity/v3'.format(internal_ip),
@@ -29,8 +25,6 @@ def dd_environment():
             'ssl_verify': False,
         }
         with socks_proxy(ip, 'ubuntu', private_key) as socks:
-            if not socks:
-                socks = '', 0
             socks_ip, socks_port = socks
             agent_config = {'proxy': {'http': 'socks5://{}:{}'.format(socks_ip, socks_port)}}
             yield instance, agent_config
