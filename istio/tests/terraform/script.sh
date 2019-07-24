fd1e565966e54ff441341bafa9648dc050cb68e7
Thomas Hervé <thomas.herve@datadoghq.com>
2019-07-24 18:21:22 +0200

    Tweak kubeconfig

--- istio/tests/terraform/script.sh
+++ istio/tests/terraform/script.sh
@@ -2,14 +2,6 @@
 # Download istio
 curl -L https://git.io/getLatestIstio | ISTIO_VERSION=$ISTIO_VERSION sh -
 export PATH=$PWD/istio-$ISTIO_VERSION/bin:$PATH
-echo "$CA_CERTIFICATE" > ca.crt
-
-# Create kubectl context
-kubectl config --kubeconfig=ci set-cluster k8s --server=$K8S_SERVER --certificate-authority=ca.crt
-kubectl config --kubeconfig=ci set-credentials admin --username=$K8S_USERNAME --password=$K8S_PASSWORD
-kubectl config --kubeconfig=ci set-context k8s-ci --cluster=k8s --namespace=default --user=admin
-kubectl config --kubeconfig=ci use-context k8s-ci
-export KUBECONFIG=ci
 
 kubectl create serviceaccount --namespace kube-system tiller || true
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller || true
