1a7b93cfecb898c4423e208b3693cb761f3773c0
Christine Chen <ChristineTChen@users.noreply.github.com>
2019-07-30 15:32:05 -0400

    Update tls_verify default to False
    Co-Authored-By: FlorianVeaux <florian.veaux@datadoghq.com>

--- mesos_master/datadog_checks/mesos_master/mesos_master.py
+++ mesos_master/datadog_checks/mesos_master/mesos_master.py
@@ -135,7 +135,7 @@ class MesosMaster(AgentCheck):
         'master/valid_status_updates': ('mesos.cluster.valid_status_updates', GAUGE),
     }
 
-    HTTP_CONFIG_REMAPPER = {'disable_ssl_validation': {'name': 'tls_verify', 'invert': True}}
+    HTTP_CONFIG_REMAPPER = {'disable_ssl_validation': {'name': 'tls_verify', 'invert': True, 'default: False}}
 
     def __init__(self, name, init_config, agentConfig, instances=None):
         AgentCheck.__init__(self, name, init_config, agentConfig, instances)
