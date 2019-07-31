3836785cc5b86f998c8065f144c50b6fdb7e42bb
ChristineTChen <christine.chen@datadoghq.com>
2019-07-31 14:13:47 -0400

    Update config

--- vault/datadog_checks/vault/vault.py
+++ vault/datadog_checks/vault/vault.py
@@ -29,15 +29,16 @@ class Vault(AgentCheck):
         'ssl_private_key': {'name': 'tls_private_key'},
         'ssl_ca_cert': {'name': 'tls_ca_cert'},
         'ssl_ignore_warning': {'name': 'tls_ignore_warning'},
-        'timeout': {'name': 'timeout', 'default': 20},
     }
 
-    def __init__(self, name, init_config, agentConfig, instances=None):
-        super(Vault, self).__init__(name, init_config, agentConfig, instances)
+    def __init__(self, name, init_config, instances):
+        super(Vault, self).__init__(name, init_config, instances)
         self.api_versions = {
             '1': {'functions': {'check_leader': self.check_leader_v1, 'check_health': self.check_health_v1}}
         }
         self.config = {}
+        if 'client_token' in self.instance:
+            self.http.options['headers']['X-Vault-Token'] = self.instance['client_token']
 
     def check(self, instance):
         config = self.get_config(instance)
@@ -130,31 +131,6 @@ class Vault(AgentCheck):
                 self.log.error('Vault configuration setting `api_url` is required')
                 return
 
-            client_token = instance.get('client_token')
-            config['headers'] = {'X-Vault-Token': client_token} if client_token else None
-
-            username = instance.get('username')
-            password = instance.get('password')
-            config['auth'] = (username, password) if username and password else None
-
-            ssl_cert = instance.get('ssl_cert')
-            ssl_private_key = instance.get('ssl_private_key')
-            if isinstance(ssl_cert, string_types):
-                if isinstance(ssl_private_key, string_types):
-                    config['ssl_cert'] = (ssl_cert, ssl_private_key)
-                else:
-                    config['ssl_cert'] = ssl_cert
-            else:
-                config['ssl_cert'] = None
-
-            if isinstance(instance.get('ssl_ca_cert'), string_types):
-                config['ssl_verify'] = instance['ssl_ca_cert']
-            else:
-                config['ssl_verify'] = is_affirmative(instance.get('ssl_verify', True))
-
-            config['ssl_ignore_warning'] = is_affirmative(instance.get('ssl_ignore_warning', False))
-            config['proxies'] = self.get_instance_proxy(instance, config['api_url'])
-            config['timeout'] = int(instance.get('timeout', 20))
             config['tags'] = instance.get('tags', [])
 
             # Keep track of the previous cluster leader to detect changes.
