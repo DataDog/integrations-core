551cf471d1ecbc76c2d0e36668952779afb39b6b
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-08-01 11:54:06 +0200

    revert test_minimal_config

--- mysql/tests/test_mysql.py
+++ mysql/tests/test_mysql.py
@@ -36,8 +36,6 @@ def test_minimal_config(aggregator, instance_basic):
     for mname in testable_metrics:
         aggregator.assert_metric(mname, at_least=0)
 
-    aggregator.assert_all_metrics_covered()
-
 
 @pytest.mark.usefixtures('dd_environment')
 def test_complex_config(aggregator, instance_complex):
