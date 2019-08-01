001d1995738bf0b9676e7e1d15b4100e4faf4727
Christine Chen <ChristineTChen@users.noreply.github.com>
2019-08-01 12:10:17 -0400

    Add requests wrapper to vault (#4259)
    * Add config remapper
    
    * Update config
    
    * Fix style
    
    * Fix style in conf.yaml
    
    * fix indent
    
    * fix test signature
    
    * Fix style

--- vault/datadog_checks/vault/vault.py
+++ vault/datadog_checks/vault/vault.py
@@ -1,13 +1,11 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-import warnings
+
 from time import time as timestamp
 
 import requests
 from simplejson import JSONDecodeError
-from six import string_types
-from urllib3.exceptions import InsecureRequestWarning
 
 from datadog_checks.checks import AgentCheck
 from datadog_checks.config import is_affirmative
@@ -24,12 +22,22 @@ class Vault(AgentCheck):
     SERVICE_CHECK_UNSEALED = 'vault.unsealed'
     SERVICE_CHECK_INITIALIZED = 'vault.initialized'
 
-    def __init__(self, name, init_config, agentConfig, instances=None):
-        super(Vault, self).__init__(name, init_config, agentConfig, instances)
+    HTTP_CONFIG_REMAPPER = {
+        'ssl_verify': {'name': 'tls_verify'},
+        'ssl_cert': {'name': 'tls_cert'},
+        'ssl_private_key': {'name': 'tls_private_key'},
+        'ssl_ca_cert': {'name': 'tls_ca_cert'},
+        'ssl_ignore_warning': {'name': 'tls_ignore_warning'},
+    }
+
+    def __init__(self, name, init_config, instances):
+        super(Vault, self).__init__(name, init_config, instances)
         self.api_versions = {
             '1': {'functions': {'check_leader': self.check_leader_v1, 'check_health': self.check_health_v1}}
         }
         self.config = {}
+        if 'client_token' in self.instance:
+            self.http.options['headers']['X-Vault-Token'] = self.instance['client_token']
 
     def check(self, instance):
         config = self.get_config(instance)
@@ -122,31 +130,6 @@ class Vault(AgentCheck):
                 self.log.error('Vault configuration setting `api_url` is required')
                 return
 
-            client_token = instance.get('client_token')
-            config['headers'] = {'X-Vault-Token': client_token} if client_token else None
-
-            username = instance.get('username')
-            password = instance.get('password')
-            config['auth'] = (username, password) if username and password else None
-
-            ssl_cert = instance.get('ssl_cert')
-            ssl_private_key = instance.get('ssl_private_key')
-            if isinstance(ssl_cert, string_types):
-                if isinstance(ssl_private_key, string_types):
-                    config['ssl_cert'] = (ssl_cert, ssl_private_key)
-                else:
-                    config['ssl_cert'] = ssl_cert
-            else:
-                config['ssl_cert'] = None
-
-            if isinstance(instance.get('ssl_ca_cert'), string_types):
-                config['ssl_verify'] = instance['ssl_ca_cert']
-            else:
-                config['ssl_verify'] = is_affirmative(instance.get('ssl_verify', True))
-
-            config['ssl_ignore_warning'] = is_affirmative(instance.get('ssl_ignore_warning', False))
-            config['proxies'] = self.get_instance_proxy(instance, config['api_url'])
-            config['timeout'] = int(instance.get('timeout', 20))
             config['tags'] = instance.get('tags', [])
 
             # Keep track of the previous cluster leader to detect changes.
@@ -159,21 +142,9 @@ class Vault(AgentCheck):
 
     def access_api(self, url, config, tags):
         try:
-            with warnings.catch_warnings():
-                if config['ssl_ignore_warning']:
-                    warnings.simplefilter('ignore', InsecureRequestWarning)
-
-                response = requests.get(
-                    url,
-                    auth=config['auth'],
-                    cert=config['ssl_cert'],
-                    verify=config['ssl_verify'],
-                    proxies=config['proxies'],
-                    timeout=config['timeout'],
-                    headers=config['headers'],
-                )
-                response.raise_for_status()
-                json_data = response.json()
+            response = self.http.get(url)
+            response.raise_for_status()
+            json_data = response.json()
         except requests.exceptions.HTTPError:
             msg = 'The Vault endpoint `{}` returned {}.'.format(url, response.status_code)
             self.service_check(self.SERVICE_CHECK_CONNECT, AgentCheck.CRITICAL, message=msg, tags=tags)
@@ -185,7 +156,7 @@ class Vault(AgentCheck):
             self.log.exception(msg)
             raise ApiUnreachable
         except requests.exceptions.Timeout:
-            msg = 'Vault endpoint `{}` timed out after {} seconds'.format(url, config['timeout'])
+            msg = 'Vault endpoint `{}` timed out after {} seconds'.format(url, self.http.options['timeout'])
             self.service_check(self.SERVICE_CHECK_CONNECT, AgentCheck.CRITICAL, message=msg, tags=tags)
             self.log.exception(msg)
             raise ApiUnreachable
