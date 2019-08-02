8c0dcff6c68f608180fb2afeaa2eb2adda69a1c3
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-08-02 12:00:30 +0200

    Fix style

--- nagios/tests/test_e2e.py
+++ nagios/tests/test_e2e.py
@@ -11,7 +11,7 @@ LOGGER = logging.getLogger(__name__)
 @pytest.mark.e2e
 def test_e2e(dd_agent_check, instance):
     # This integration is based on tailing logs but because there is an agent running
-    # there is an agent running in the agent image we cannot guarantee that we will 
+    # there is an agent running in the agent image we cannot guarantee that we will
     # pick specific metrics on a test.
     # The test will fail if the integration cannot read nagios config or related files
     dd_agent_check(instance, rate=True)
