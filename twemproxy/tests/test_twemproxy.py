641360baae4dd6a5531510ecdc87ed58f59d66cd
Julia <611228+hithwen@users.noreply.github.com>
2019-08-05 10:29:37 +0200

    Add e2e test to twemproxy (#4248)
    * Add e2e test to twemproxy

--- twemproxy/tests/test_twemproxy.py
+++ twemproxy/tests/test_twemproxy.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
 
 from datadog_checks.twemproxy import Twemproxy
 
@@ -9,18 +10,40 @@ from . import common, metrics
 SC_TAGS = ['host:{}'.format(common.HOST), 'port:{}'.format(common.PORT), 'optional:tag1']
 
 
-def test_check(check, dd_environment, setup_request, aggregator):
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_integration(check, dd_environment, setup_request, aggregator):
     check.check(common.INSTANCE)
 
-    for stat in metrics.GLOBAL_STATS:
-        aggregator.assert_metric("twemproxy.{}".format(stat), at_least=0)
     for stat in metrics.POOL_STATS:
         aggregator.assert_metric("twemproxy.{}".format(stat), count=1)
     for stat in metrics.SERVER_STATS:
         aggregator.assert_metric("twemproxy.{}".format(stat), count=2)
+    for stat in metrics.SERVER_STATS_2:
+        aggregator.assert_metric("twemproxy.{}".format(stat), count=2)
+    for stat in metrics.POOL_STATS_2:
+        aggregator.assert_metric("twemproxy.{}".format(stat), count=1)
+    aggregator.assert_all_metrics_covered()
 
     # Test service check
     aggregator.assert_service_check('twemproxy.can_connect', status=Twemproxy.OK, tags=SC_TAGS, count=1)
 
-    # Raises when COVERAGE=true and coverage < 100%
+
+@pytest.mark.e2e
+def test_e2e(dd_agent_check, setup_request, instance):
+    aggregator = dd_agent_check(instance, rate=True)
+
+    for stat in metrics.POOL_STATS:
+        aggregator.assert_metric("twemproxy.{}".format(stat), count=2)
+    for stat in metrics.SERVER_STATS:
+        aggregator.assert_metric("twemproxy.{}".format(stat), count=4)
+
+    for stat in metrics.SERVER_STATS_2:
+        aggregator.assert_metric("twemproxy.{}".format(stat), count=2)
+    for stat in metrics.POOL_STATS_2:
+        aggregator.assert_metric("twemproxy.{}".format(stat), count=1)
+
     aggregator.assert_all_metrics_covered()
+
+    # Test service check
+    aggregator.assert_service_check('twemproxy.can_connect', status=Twemproxy.OK, tags=SC_TAGS, count=2)
