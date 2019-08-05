81792d0e48f8083fb288a411662ba4f1a39ba894
Thomas Hervé <thomas.herve@datadoghq.com>
2019-08-05 13:40:49 +0200

    Add e2e test (#4290)
    * Add e2e test
    
    This adds a e2e test for the default environment.

--- envoy/tests/test_envoy.py
+++ envoy/tests/test_envoy.py
@@ -8,80 +8,85 @@ from datadog_checks.envoy.metrics import METRIC_PREFIX, METRICS
 
 from .common import INSTANCES, response
 
+CHECK_NAME = 'envoy'
 
-class TestEnvoy:
-    CHECK_NAME = 'envoy'
 
-    def test_success(self, aggregator):
-        instance = INSTANCES['main']
-        c = Envoy(self.CHECK_NAME, {}, [instance])
+def test_success(aggregator):
+    instance = INSTANCES['main']
+    c = Envoy(CHECK_NAME, {}, [instance])
+    c.check(instance)
+
+    metrics_collected = 0
+    for metric in METRICS:
+        metrics_collected += len(aggregator.metrics(METRIC_PREFIX + metric))
+
+    assert metrics_collected >= 250
+
+
+def test_success_fixture(aggregator):
+    instance = INSTANCES['main']
+    c = Envoy(CHECK_NAME, {}, [instance])
+
+    with mock.patch('requests.get', return_value=response('multiple_services')):
         c.check(instance)
 
-        metrics_collected = 0
-        for metric in METRICS.keys():
-            metrics_collected += len(aggregator.metrics(METRIC_PREFIX + metric))
+    metrics_collected = 0
+    for metric in METRICS:
+        metrics_collected += len(aggregator.metrics(METRIC_PREFIX + metric))
 
-        assert metrics_collected >= 250
+    num_metrics = len(response('multiple_services').content.decode().splitlines())
+    num_metrics -= sum(c.unknown_metrics.values()) + sum(c.unknown_tags.values())
+    assert 4186 <= metrics_collected == num_metrics
 
-    def test_success_fixture(self, aggregator):
-        instance = INSTANCES['main']
-        c = Envoy(self.CHECK_NAME, {}, [instance])
 
-        with mock.patch('requests.get', return_value=response('multiple_services')):
-            c.check(instance)
+def test_success_fixture_whitelist(aggregator):
+    instance = INSTANCES['whitelist']
+    c = Envoy(CHECK_NAME, {}, [instance])
 
-        metrics_collected = 0
-        for metric in METRICS.keys():
-            metrics_collected += len(aggregator.metrics(METRIC_PREFIX + metric))
+    with mock.patch('requests.get', return_value=response('multiple_services')):
+        c.check(instance)
 
-        num_metrics = len(response('multiple_services').content.decode().splitlines())
-        num_metrics -= sum(c.unknown_metrics.values()) + sum(c.unknown_tags.values())
-        assert 4186 <= metrics_collected == num_metrics
+    for metric in aggregator.metric_names:
+        assert metric.startswith('envoy.cluster.')
 
-    def test_success_fixture_whitelist(self, aggregator):
-        instance = INSTANCES['whitelist']
-        c = Envoy(self.CHECK_NAME, {}, [instance])
 
-        with mock.patch('requests.get', return_value=response('multiple_services')):
-            c.check(instance)
+def test_success_fixture_blacklist(aggregator):
+    instance = INSTANCES['blacklist']
+    c = Envoy(CHECK_NAME, {}, [instance])
 
-        for metric in aggregator.metric_names:
-            assert metric.startswith('envoy.cluster.')
+    with mock.patch('requests.get', return_value=response('multiple_services')):
+        c.check(instance)
 
-    def test_success_fixture_blacklist(self, aggregator):
-        instance = INSTANCES['blacklist']
-        c = Envoy(self.CHECK_NAME, {}, [instance])
+    for metric in aggregator.metric_names:
+        assert not metric.startswith('envoy.cluster.')
 
-        with mock.patch('requests.get', return_value=response('multiple_services')):
-            c.check(instance)
 
-        for metric in aggregator.metric_names:
-            assert not metric.startswith('envoy.cluster.')
+def test_success_fixture_whitelist_blacklist(aggregator):
+    instance = INSTANCES['whitelist_blacklist']
+    c = Envoy(CHECK_NAME, {}, [instance])
 
-    def test_success_fixture_whitelist_blacklist(self, aggregator):
-        instance = INSTANCES['whitelist_blacklist']
-        c = Envoy(self.CHECK_NAME, {}, [instance])
+    with mock.patch('requests.get', return_value=response('multiple_services')):
+        c.check(instance)
 
-        with mock.patch('requests.get', return_value=response('multiple_services')):
-            c.check(instance)
+    for metric in aggregator.metric_names:
+        assert metric.startswith("envoy.cluster.") and not metric.startswith("envoy.cluster.out")
 
-        for metric in aggregator.metric_names:
-            assert metric.startswith("envoy.cluster.") and not metric.startswith("envoy.cluster.out")
 
-    def test_service_check(self, aggregator):
-        instance = INSTANCES['main']
-        c = Envoy(self.CHECK_NAME, {}, [instance])
+def test_service_check(aggregator):
+    instance = INSTANCES['main']
+    c = Envoy(CHECK_NAME, {}, [instance])
 
-        with mock.patch('requests.get', return_value=response('multiple_services')):
-            c.check(instance)
+    with mock.patch('requests.get', return_value=response('multiple_services')):
+        c.check(instance)
 
-        assert aggregator.service_checks(Envoy.SERVICE_CHECK_NAME)[0].status == Envoy.OK
+    assert aggregator.service_checks(Envoy.SERVICE_CHECK_NAME)[0].status == Envoy.OK
 
-    def test_unknown(self):
-        instance = INSTANCES['main']
-        c = Envoy(self.CHECK_NAME, {}, [instance])
 
-        with mock.patch('requests.get', return_value=response('unknown_metrics')):
-            c.check(instance)
+def test_unknown():
+    instance = INSTANCES['main']
+    c = Envoy(CHECK_NAME, {}, [instance])
 
-        assert sum(c.unknown_metrics.values()) == 5
+    with mock.patch('requests.get', return_value=response('unknown_metrics')):
+        c.check(instance)
+
+    assert sum(c.unknown_metrics.values()) == 5
