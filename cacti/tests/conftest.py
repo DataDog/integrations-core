a227e696996832bb36753ef541a14bdd9e36beb9
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-08-06 18:19:14 +0200

    Use docker lib

--- cacti/tests/conftest.py
+++ cacti/tests/conftest.py
@@ -9,19 +9,19 @@ import pytest
 
 from datadog_checks.cacti import CactiCheck
 from datadog_checks.dev import TempDir, WaitFor, docker_run, run_command
+from datadog_checks.dev.docker import run_in_container
 
 from .common import CONTAINER_NAME, E2E_METADATA, HERE, INSTANCE_INTEGRATION, RRD_PATH
 
 
 def set_up_cacti():
     commands = [
-        ['/sbin/restore'],
-        ['mysql', '-u', 'root', '-e', "flush privileges;"],
-        ['php', '/opt/cacti/lib/poller.php', '--force'],
+        '/sbin/restore',
+        'mysql -u root -e "flush privileges;"',
+        'php /opt/cacti/lib/poller.php --force',
     ]
     for c in commands:
-        command = ['docker', 'exec', CONTAINER_NAME] + c
-        run_command(command, capture=True, check=True)
+        run_in_container(CONTAINER_NAME, c)
 
 
 @pytest.fixture(scope="session")
