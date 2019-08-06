284b58aeb392893182a49937cc2efefce3d427d8
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-08-06 14:11:39 +0200

    Remove duplicated code

--- tcp_check/tests/test_e2e.py
+++ tcp_check/tests/test_e2e.py
@@ -3,13 +3,12 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
 
-from datadog_checks.tcp_check import TCPCheck
+from . import common
 
 
 @pytest.mark.e2e
 def test_e2e(dd_agent_check, instance):
-    aggregator = dd_agent_check(instance, rate=True)
+    aggregator = dd_agent_check(instance)
+    common._test_check(aggregator)
 
-    expected_tags = ['foo:bar', 'target_host:datadoghq.com', 'port:80', 'instance:UpService']
-    aggregator.assert_metric('network.tcp.can_connect', value=1, tags=expected_tags)
-    aggregator.assert_service_check('tcp.can_connect', status=TCPCheck.OK, tags=expected_tags)
+
