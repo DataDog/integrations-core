c1a9d04842e4a88f9eede052a88c27149fe5ebca
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-08-13 15:31:18 +0200

    Remove duplicate test

--- gearmand/tests/test_integration.py
+++ gearmand/tests/test_integration.py
@@ -10,22 +10,6 @@ from . import common
 
 
 @pytest.mark.usefixtures("dd_environment")
-def test_service_check(check, aggregator):
-    tags = ['server:{}'.format(common.HOST), 'port:{}'.format(common.PORT)] + common.TAGS2
-
-    check.check(common.INSTANCE2)
-
-    aggregator.assert_metric('gearman.unique_tasks', value=0.0, tags=tags, count=1)
-    aggregator.assert_metric('gearman.running', value=0.0, tags=tags, count=1)
-    aggregator.assert_metric('gearman.queued', value=0.0, tags=tags, count=1)
-    aggregator.assert_metric('gearman.workers', value=0.0, tags=tags, count=1)
-
-    aggregator.assert_service_check('gearman.can_connect', status=Gearman.OK, tags=tags, count=1)
-
-    aggregator.assert_all_metrics_covered()
-
-
-@pytest.mark.usefixtures("dd_environment")
 def test_service_check_broken(check, aggregator):
     tags = ['server:{}'.format(common.HOST), 'port:{}'.format(common.BAD_PORT)] + common.TAGS2
 
