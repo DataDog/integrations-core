fdbb3806f8e9a2a92504339307f2b0a9fc5e537a
Kylian Serrania <kylian.serrania@datadoghq.com>
2019-08-14 18:03:57 +0200

    isort formatting

--- datadog_checks_base/datadog_checks/checks/network_checks.py
+++ datadog_checks_base/datadog_checks/checks/network_checks.py
@@ -3,4 +3,4 @@
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
 # Copyright 2016-2019 Datadog, Inc.
 # flake8: noqa
-from . import NetworkCheck, Status, EventType
+from . import EventType, NetworkCheck, Status
