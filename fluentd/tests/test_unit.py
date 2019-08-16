84e57f8ced2767ac789cf47f99702768a6eee1c1
ChristineTChen <christine.chen@datadoghq.com>
2019-08-16 16:31:16 -0400

    fix style

--- fluentd/tests/test_unit.py
+++ fluentd/tests/test_unit.py
@@ -7,7 +7,7 @@ from datadog_checks.fluentd import Fluentd
 
 def test_default_timeout(instance):
     # test default timeout
-    check = Fluentd(CHECK_NAME, {}, instance)
+    check = Fluentd(CHECK_NAME, {}, [instance])
     check.check(instance)
 
     assert check.http.options['timeout'] == (5, 5)
@@ -15,14 +15,14 @@ def test_default_timeout(instance):
 
 def test_init_config_old_timeout(instance):
     # test init_config timeout
-    check = Fluentd(CHECK_NAME, {'default_timeout': 2}, instance)
+    check = Fluentd(CHECK_NAME, {'default_timeout': 2}, [instance])
     check.check(instance)
     assert check.http.options['timeout'] == (2, 2)
 
 
 def test_init_config_timeout(instance):
     # test init_config timeout
-    check = Fluentd(CHECK_NAME, {'timeout': 7}, instance)
+    check = Fluentd(CHECK_NAME, {'timeout': 7}, [instance])
     check.check(instance)
 
     assert check.http.options['timeout'] == (7, 7)
@@ -31,7 +31,7 @@ def test_init_config_timeout(instance):
 def test_instance_old_timeout(instance):
     # test instance default_timeout
     instance['default_timeout'] = 13
-    check =  Fluentd(CHECK_NAME, {'default_timeout': 9}, instance)
+    check = Fluentd(CHECK_NAME, {'default_timeout': 9}, [instance])
     check.check(instance)
 
     assert check.http.options['timeout'] == (13, 13)
@@ -40,7 +40,7 @@ def test_instance_old_timeout(instance):
 def test_instance_timeout(instance):
     # test instance timeout
     instance['timeout'] = 15
-    check = Fluentd(CHECK_NAME, {}, instance)
+    check = Fluentd(CHECK_NAME, {}, [instance])
     check.check(instance)
 
     assert check.http.options['timeout'] == (15, 15)
