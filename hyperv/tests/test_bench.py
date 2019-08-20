26b32ccd203d5a6275ede6f66b29855990f5fa4d
Ofek Lev <ofekmeister@gmail.com>
2019-08-20 10:04:03 -0400

    Add Hyper-V e2e

--- hyperv/tests/test_bench.py
+++ hyperv/tests/test_bench.py
@@ -1,28 +1,22 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from copy import deepcopy
-
 from datadog_checks.hyperv import HypervCheck
 
 
-def test_refresh_counters(benchmark, instance):
-    instance = deepcopy(instance)
-    instance['refresh_counters'] = True
-    check = HypervCheck('hyperv', {}, {}, [instance])
+def test_refresh_counters(benchmark, instance_refresh):
+    check = HypervCheck('hyperv', {}, {}, [instance_refresh])
 
     # Run once to get any PDH setup out of the way.
-    check.check(instance)
+    check.check(instance_refresh)
 
-    benchmark(check.check, instance)
+    benchmark(check.check, instance_refresh)
 
 
-def test_no_refresh_counters(benchmark, instance):
-    instance = deepcopy(instance)
-    instance['refresh_counters'] = False
-    check = HypervCheck('hyperv', {}, {}, [instance])
+def test_no_refresh_counters(benchmark, instance_no_refresh):
+    check = HypervCheck('hyperv', {}, {}, [instance_no_refresh])
 
     # Run once to get any PDH setup out of the way.
-    check.check(instance)
+    check.check(instance_no_refresh)
 
-    benchmark(check.check, instance)
+    benchmark(check.check, instance_no_refresh)
