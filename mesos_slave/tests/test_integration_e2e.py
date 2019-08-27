dbdb600d81ba590001931f92b25532b8a3efc5c4
Christine Chen <ChristineTChen@users.noreply.github.com>
2019-08-27 14:17:43 -0400

    Add e2e to mesos_slave (#4407)
    * Update test
    
    * fix style
    
    * Refactor assertion logic
    
    * fix check syntax
    
    * remove fixture
    
    * Fix style

--- mesos_slave/tests/test_integration_e2e.py
+++ mesos_slave/tests/test_integration_e2e.py
@@ -8,6 +8,8 @@ from six import iteritems
 
 from datadog_checks.mesos_slave import MesosSlave
 
+from .common import CHECK_NAME
+
 
 @pytest.mark.integration
 # Linux only: https://github.com/docker/for-mac/issues/1031
@@ -16,6 +18,20 @@ from datadog_checks.mesos_slave import MesosSlave
 def test_integration(instance, aggregator):
     check = MesosSlave('mesos_slave', {}, [instance])
     check.check(instance)
+    check.check(instance)
+    assert_metrics_covered(aggregator)
+
+
+@pytest.mark.e2e
+# Linux only: https://github.com/docker/for-mac/issues/1031
+@pytest.mark.skipif(platform.system() != 'Linux', reason="Only runs on Unix systems")
+def test_e2e(dd_agent_check, instance):
+    aggregator = dd_agent_check(instance, rate=True)
+    assert_metrics_covered(aggregator)
+
+
+def assert_metrics_covered(aggregator):
+    check = MesosSlave(CHECK_NAME, {}, [{}])
     metrics = {}
     for d in (
         check.SLAVE_TASKS_METRICS,
@@ -31,4 +47,4 @@ def test_integration(instance, aggregator):
 
     aggregator.assert_all_metrics_covered()
 
-    aggregator.assert_service_check('mesos_slave.can_connect', count=1, status=check.OK)
+    aggregator.assert_service_check('mesos_slave.can_connect', status=check.OK, count=2)
