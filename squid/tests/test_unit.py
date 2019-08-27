5de1858d4a97a7cefc31d60d5db4f3fac47c88ce
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-08-27 11:01:04 +0200

    Update squid/tests/test_unit.py
    Co-Authored-By: Ofek Lev <ofekmeister@gmail.com>

--- squid/tests/test_unit.py
+++ squid/tests/test_unit.py
@@ -1,7 +1,6 @@
 # (C) Datadog, Inc. 2010-2016
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
-from collections import OrderedDict
 from copy import deepcopy
 
 import mock
