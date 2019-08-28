34d43599d408bd496ed5aa3f2777783cad94dae2
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-08-28 16:22:20 +0200

    Remove unused import

--- mesos_slave/tests/test_unit.py
+++ mesos_slave/tests/test_unit.py
@@ -1,7 +1,6 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from collections import namedtuple
 from copy import deepcopy
 
 import mock
