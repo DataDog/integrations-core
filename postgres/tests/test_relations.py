3ce10644a9528af23a36805d8410fe20bfb34976
Johan Fischer <johan.fischer@loricahealth.com>
2019-08-31 03:40:49 +0000

    Fix the missing schema public expected in the size tags.

--- postgres/tests/test_relations.py
+++ postgres/tests/test_relations.py
@@ -97,6 +97,7 @@ def test_relations_metrics2(aggregator, pg_instance):
             'port:{}'.format(pg_instance['port']),
             'db:%s' % pg_instance['dbname'],
             'table:{}'.format(relation.lower()),
+            'schema:public',
         ]
 
     for relation in relations:
