039046fe0199a3c31f67d075977a008cdb07f242
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-09-06 19:49:28 +0200

    Fix test

--- datadog_checks_base/tests/stubs/test_aggregator_no_duplicate.py
+++ datadog_checks_base/tests/stubs/test_aggregator_no_duplicate.py
@@ -29,11 +29,10 @@ Duplicate metrics found:
   - check.metric.dup2
       MetricStub(name='check.metric.dup2', type=0, value=3.0, tags=['aa'], hostname='')
       MetricStub(name='check.metric.dup2', type=0, value=4.0, tags=['aa'], hostname='')
-assert 2 == 0
 '''
     print("\n===\n{}\n===\n".format(expected_msg.strip()))
     print("\n===\n{}\n===\n".format(actual_msg.strip()))
-    assert expected_msg.strip() == actual_msg.strip()
+    assert expected_msg.strip() == actual_msg.split("assert")[0].strip()
 
 
 @pytest.mark.parametrize(
