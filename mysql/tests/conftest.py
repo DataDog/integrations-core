1647a91cf6a14aab3b2b07f39a8b8189f19066ba
florian.veaux <florian.veaux@datadoghq.com>
2019-09-17 17:19:07 -0400

    Test sleep

--- mysql/tests/conftest.py
+++ mysql/tests/conftest.py
@@ -32,6 +32,7 @@ def dd_environment(instance_basic):
             CheckDockerLogs('mysql-slave', ["ready for connections", "mariadb successfully initialized"]),
             populate_database,
         ],
+        sleep=10
     ):
         yield instance_basic
 
