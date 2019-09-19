bdb820a574da23bbb3dbac46331a1beedcb6f1fe
Marc Ian Bucad <ian.bucad@gmail.com>
2019-09-19 19:47:19 -0400

    initial commit - adds kerberos docker env

---
+++ datadog_checks_base/tests/compose/kerberos-nginx/setupkeytab.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+echo "domain: $DOMAIN"
+echo "realm: $KRB5_REALM"
+
+usernameplusdomain="$KRB5_USER@$DOMAIN"
+
+echo "User name: $usernameplusdomain"
+
+set -x
+
+## Load principals into cache
+kinit -kt $KRB5_KEYTAB ${KRB5_SVC}/$HOSTNAME.$DOMAIN@$KRB5_REALM -V
+# kinit -kt $KRB5_KEYTAB $KRB5_USER@$KRB5_REALM -V
+
+## List principals in cache
+echo "Principals in keytab..."
+echo "----------------------"
+klist -ket $KRB5_KEYTAB
+echo "======================"
+echo "Principals in cache..."
+echo "----------------------"
+klist
+echo "======================"
+
+set +x
