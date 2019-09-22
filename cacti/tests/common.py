c1cb2c9c20e00abee4ceb40ca3da4d25186e5220
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-09-22 16:29:39 +0200

    Fix style

--- cacti/tests/common.py
+++ cacti/tests/common.py
@@ -2,7 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-from datadog_checks.dev import get_here, get_docker_hostname
+from datadog_checks.dev import get_docker_hostname, get_here
 
 HERE = get_here()
 HOST = get_docker_hostname()
