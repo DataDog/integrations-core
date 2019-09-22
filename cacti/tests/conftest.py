bf3187a30dd657b3fd402c5626ee70c6a45e6a38
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-09-22 18:00:15 +0200

    Fix style

--- cacti/tests/conftest.py
+++ cacti/tests/conftest.py
@@ -10,7 +10,7 @@ import pytest
 from datadog_checks.cacti import CactiCheck
 from datadog_checks.dev import TempDir, WaitFor, docker_run, run_command
 
-from .common import CONTAINER_NAME, E2E_METADATA, HERE, INSTANCE_INTEGRATION, RRD_PATH, MYSQL_USERNAME, MYSQL_PASSWORD
+from .common import CONTAINER_NAME, E2E_METADATA, HERE, INSTANCE_INTEGRATION, MYSQL_PASSWORD, MYSQL_USERNAME, RRD_PATH
 
 SQL_SETUP = '''
 CREATE USER '{user}'@'localhost' IDENTIFIED BY '{password}';
@@ -18,7 +18,9 @@ GRANT ALL PRIVILEGES ON *.* TO '{user}'@'localhost' WITH GRANT OPTION;
 CREATE USER '{user}'@'%' IDENTIFIED BY '{password}';
 GRANT ALL PRIVILEGES ON *.* TO '{user}'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-'''.format(user=MYSQL_USERNAME, password=MYSQL_PASSWORD)
+'''.format(
+    user=MYSQL_USERNAME, password=MYSQL_PASSWORD
+)
 
 
 def set_up_cacti():
