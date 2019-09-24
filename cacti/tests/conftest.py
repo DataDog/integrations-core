140481c9aeae47dcc27df924457b63ca0cd5f12e
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-09-24 09:11:36 +0200

    Fix cacti flaky e2e

--- cacti/tests/conftest.py
+++ cacti/tests/conftest.py
@@ -5,33 +5,55 @@
 import os
 from copy import deepcopy
 
+import pymysql
 import pytest
 
 from datadog_checks.cacti import CactiCheck
 from datadog_checks.dev import TempDir, WaitFor, docker_run, run_command
 
+from . import common
 from .common import CONTAINER_NAME, E2E_METADATA, HERE, INSTANCE_INTEGRATION, MYSQL_PASSWORD, MYSQL_USERNAME, RRD_PATH
 
 SQL_SETUP = '''
+DROP USER IF EXISTS '{user}'@'localhost';
+DROP USER IF EXISTS '{user}'@'%';
+
 CREATE USER '{user}'@'localhost' IDENTIFIED BY '{password}';
 GRANT ALL PRIVILEGES ON *.* TO '{user}'@'localhost' WITH GRANT OPTION;
 CREATE USER '{user}'@'%' IDENTIFIED BY '{password}';
 GRANT ALL PRIVILEGES ON *.* TO '{user}'@'%' WITH GRANT OPTION;
+
 FLUSH PRIVILEGES;
 '''.format(
     user=MYSQL_USERNAME, password=MYSQL_PASSWORD
 )
 
 
-def set_up_cacti():
-    commands = [
-        ['/sbin/restore'],
-        ['mysql', '-u', 'root', '-e', SQL_SETUP],
-        ['php', '/opt/cacti/lib/poller.php', '--force'],
-    ]
-    for c in commands:
-        command = ['docker', 'exec', CONTAINER_NAME] + c
-        run_command(command, capture=True, check=True)
+def setup_db():
+    run_docker_command(['mysql', '-u', 'root', '-e', SQL_SETUP])
+
+
+def setup_data():
+    run_docker_command(['/sbin/restore'])
+
+    conn = pymysql.connect(
+        host=common.HOST, user=common.MYSQL_USERNAME, passwd=common.MYSQL_PASSWORD, db=common.DATABASE
+    )
+    c = conn.cursor()
+
+    c.execute('select count(*) from data_local')
+    data_local_count = c.fetchone()[0]
+
+    if data_local_count == 0:
+        raise Exception("Exception data_local to be populated but found 0 entries.")
+
+
+def poll_cacti():
+    run_docker_command(['php', '/opt/cacti/lib/poller.php', '--force'])
+
+
+def run_docker_command(command):
+    run_command(['docker', 'exec', CONTAINER_NAME] + command, capture=True, check=True)
 
 
 @pytest.fixture(scope="session")
@@ -41,7 +63,7 @@ def dd_environment():
         e2e_metadata['docker_volumes'] = ['{}:{}'.format(rrd_path, RRD_PATH)]
 
         with docker_run(
-            conditions=[WaitFor(set_up_cacti)],
+            conditions=[WaitFor(setup_db), WaitFor(setup_data), WaitFor(poll_cacti)],
             compose_file=os.path.join(HERE, "compose", "docker-compose.yaml"),
             env_vars={'RRD_PATH': rrd_path},
             build=True,
