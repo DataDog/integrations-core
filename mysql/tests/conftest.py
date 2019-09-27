313644c1f4a88745899a46ae995836b5124c565a
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-09-27 19:47:52 +0200

    Replace image with alexandreyangdatadog/mysql-replication

--- mysql/tests/conftest.py
+++ mysql/tests/conftest.py
@@ -125,7 +125,7 @@ def _wait_for_it_script():
 def _mysql_docker_repo():
     if MYSQL_FLAVOR == 'mysql':
         if MYSQL_VERSION == '5.5':
-            return 'bergerx/mysql-replication'
+            return 'alexandreyangdatadog/mysql-replication'
         elif MYSQL_VERSION in ('5.6', '5.7'):
             return 'bergerx/mysql-replication'
         elif MYSQL_VERSION == '8.0':
