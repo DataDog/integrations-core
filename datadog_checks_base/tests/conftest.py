190ba908b85406673034dae94bef770794de1657
florian.veaux <florian.veaux@datadoghq.com>
2019-10-07 16:03:53 +0200

    fix style

--- datadog_checks_base/tests/conftest.py
+++ datadog_checks_base/tests/conftest.py
@@ -3,7 +3,6 @@ import os
 import pytest
 
 from datadog_checks.dev import docker_run, get_here
-from datadog_checks.dev.conditions import CheckDockerLogs
 
 HERE = get_here()
 
@@ -11,7 +10,5 @@ HERE = get_here()
 @pytest.fixture(scope="session")
 def socks5_proxy():
     compose_file = os.path.join(HERE, "compose", "socks5-proxy.yaml")
-    with docker_run(
-        compose_file=compose_file, log_patterns=['Start listening proxy service on port']
-    ):
+    with docker_run(compose_file=compose_file, log_patterns=['Start listening proxy service on port']):
         yield "proxy_user:proxy_password@localhost:1080"
