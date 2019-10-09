7bf0a5e8a665d75d3ac57c7880d4ea3fb0a90ee2
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-10-09 18:18:00 +0200

    Fix lint flake8-logging-format command

--- datadog_checks_dev/datadog_checks/dev/plugin/tox.py
+++ datadog_checks_dev/datadog_checks/dev/plugin/tox.py
@@ -65,7 +65,7 @@ def add_style_checker(config, sections, make_envconfig, reader):
                 'isort --check-only --diff --recursive .',
                 'python -c "print(\'\\n[WARNING] Complying with following lint rules is recommended, '
                 'but not mandatory, yet.\')"',
-                '- flake8 --enable-extensions=G --select=G .',  # lint `flake8-logging-format`
+                '- flake8 --config=../.flake8 --enable-extensions=G --select=G .',  # lint `flake8-logging-format`
             ]
         ),
     }
