67ff7b022dacc011f313ba33fe87ef609482488e
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-10-11 13:42:20 +0200

    Repeat on all tests

--- varnish/tests/test_varnish.py
+++ varnish/tests/test_varnish.py
@@ -11,9 +11,7 @@ pytestmark = [pytest.mark.usefixtures('dd_environment'), pytest.mark.integration
 
 
 def test_check(aggregator, check, instance):
-    check.check(instance)
-    time.sleep(2)
-    check.check(instance)
+    _check_repeat(check, instance)
     for mname in common.COMMON_METRICS:
         aggregator.assert_metric(mname, at_least=1, tags=['cluster:webs', 'varnish_name:default'])
 
@@ -21,10 +19,10 @@ def test_check(aggregator, check, instance):
 def test_inclusion_filter(aggregator, check, instance):
     instance['metrics_filter'] = ['SMA.*']
 
-    check.check(instance)
+    _check_repeat(check, instance)
     for mname in common.COMMON_METRICS:
         if 'SMA.' in mname:
-            aggregator.assert_metric(mname, count=1, tags=['cluster:webs', 'varnish_name:default'])
+            aggregator.assert_metric(mname, at_least=1, tags=['cluster:webs', 'varnish_name:default'])
         else:
             aggregator.assert_metric(mname, count=0, tags=['cluster:webs', 'varnish_name:default'])
 
@@ -32,9 +30,16 @@ def test_inclusion_filter(aggregator, check, instance):
 def test_exclusion_filter(aggregator, check, instance):
     instance['metrics_filter'] = ['^SMA.Transient.c_req']
 
-    check.check(instance)
+    _check_repeat(check, instance)
     for mname in common.COMMON_METRICS:
         if 'SMA.Transient.c_req' in mname:
             aggregator.assert_metric(mname, count=0, tags=['cluster:webs', 'varnish_name:default'])
         elif 'varnish.uptime' not in mname:
-            aggregator.assert_metric(mname, count=1, tags=['cluster:webs', 'varnish_name:default'])
+            aggregator.assert_metric(mname, at_least=1, tags=['cluster:webs', 'varnish_name:default'])
+
+
+def _check_repeat(check, instance):
+    # Ensure all metrics are generated
+    check.check(instance)
+    time.sleep(2)
+    check.check(instance)
