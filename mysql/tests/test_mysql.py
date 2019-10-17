e1008c12696e07b5e2e153f9f209b1d56ede706a
mathildapurr <23065951+mathildapurr@users.noreply.github.com>
2019-10-17 13:54:08 -0400

    added build to metadata and adjusted logic

--- mysql/tests/test_mysql.py
+++ mysql/tests/test_mysql.py
@@ -254,14 +254,14 @@ def test_version_metadata(instance_basic, version_metadata):
     # mysql_check.check(instance_basic)
     # mysql_check.check_id = 'test:123'
     mysql_check = MySql(common.CHECK_NAME, {}, instances=[instance_basic])
+    mysql_check.check_id = 'test:123' 
 
     with mock.patch('datadog_checks.base.stubs.datadog_agent.set_check_metadata') as m:
         mysql_check.check(instance_basic)
-        mysql_check.check_id = 'test:123' 
-
+        
         for name, value in version_metadata.items():
             m.assert_any_call('test:123', name, value)
-
+        
         assert m.call_count == len(version_metadata)
 
 # def test_flavor_metadata(flavor_metadata):
