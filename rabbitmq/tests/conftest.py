2ce342b389468ea87cc5b045bea206a3efd83181
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-10-18 17:05:28 +0200

    Fix rabbitmq test envs

--- rabbitmq/tests/conftest.py
+++ rabbitmq/tests/conftest.py
@@ -25,8 +25,8 @@ def dd_environment():
 
     env = {}
 
-    if not os.environ.get('RABBITMQ_VERSION'):
-        env['RABBITMQ_VERSION'] = '3.6.0'
+    if 'RABBITMQ_VERSION' not in os.environ:
+        pytest.exit('RABBITMQ_VERSION not available')
 
     compose_file = os.path.join(HERE, 'compose', 'docker-compose.yaml')
 
