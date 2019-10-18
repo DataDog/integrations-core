1cc9e8f97f7955d13203b90a38d5c941e5579ce4
Christine Chen <ChristineTChen@users.noreply.github.com>
2019-10-18 10:36:20 -0400

    Ensure the correct tls_ca_cert value is used (#4819)
    * Ensure instance_ca_cert uses tls_ca_cert value
    
    * Fix style

--- http_check/datadog_checks/http_check/config.py
+++ http_check/datadog_checks/http_check/config.py
@@ -52,7 +52,7 @@ def from_instance(instance, default_ca_certs=None):
         raise ConfigurationError("The url {} must start with the scheme http or https".format(url))
     include_content = is_affirmative(instance.get('include_content', False))
     ssl_expire = is_affirmative(instance.get('check_certificate_expiration', True))
-    instance_ca_certs = instance.get('ca_certs', default_ca_certs)
+    instance_ca_certs = instance.get('tls_ca_cert', instance.get('ca_certs', default_ca_certs))
     weakcipher = is_affirmative(instance.get('weakciphers', False))
     check_hostname = is_affirmative(instance.get('check_hostname', True))
     allow_redirects = is_affirmative(instance.get('allow_redirects', True))
