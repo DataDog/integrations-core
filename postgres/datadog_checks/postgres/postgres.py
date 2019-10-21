fb9784aaffb6c374114bbb8fe4c78e319a3174fb
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-10-21 13:59:08 +0200

    Add missing argument

--- postgres/datadog_checks/postgres/postgres.py
+++ postgres/datadog_checks/postgres/postgres.py
@@ -177,7 +177,7 @@ class PostgreSql(AgentCheck):
     def _is_10_or_above(self, db):
         return self._is_above(db, [10, 0, 0])
 
-    def _get_instance_metrics(self, database_size_metrics, collect_default_db):
+    def _get_instance_metrics(self, db, database_size_metrics, collect_default_db):
         """
         Add NEWER_92_METRICS to the default set of COMMON_METRICS when server
         version is 9.2 or later.
@@ -193,7 +193,7 @@ class PostgreSql(AgentCheck):
 
         if metrics is None:
             # select the right set of metrics to collect depending on postgres version
-            if self._is_9_2_or_above():
+            if self._is_9_2_or_above(db):
                 self.instance_metrics = dict(COMMON_METRICS, **NEWER_92_METRICS)
             else:
                 self.instance_metrics = dict(COMMON_METRICS)
@@ -516,7 +516,7 @@ class PostgreSql(AgentCheck):
         If custom_metrics is not an empty list, gather custom metrics defined in postgres.yaml
         """
 
-        db_instance_metrics = self._get_instance_metrics(collect_database_size_metrics, collect_default_db)
+        db_instance_metrics = self._get_instance_metrics(db, collect_database_size_metrics, collect_default_db)
         bgw_instance_metrics = self._get_bgw_metrics(db)
         archiver_instance_metrics = self._get_archiver_metrics(db)
 
