3fb0bb6e959577b7e6568a658e0a5b3864672f42
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-10-22 15:46:07 +0200

    Make integration test work on OSX (#4766)

--- mesos_slave/tests/test_integration_e2e.py
+++ mesos_slave/tests/test_integration_e2e.py
@@ -11,9 +11,11 @@ from datadog_checks.mesos_slave import MesosSlave
 
 from .common import CHECK_NAME
 
+# Does not work on windows. The zookeeper image are not compatible with windows architecture.
+# Error: "no matching manifest for windows/amd64 10.0.17763 in the manifest list entries"
+pytest.mark.skipif(platform.system() == 'Windows', reason="Docker images not compatible with windows architecture")
 
-# Linux only: https://github.com/docker/for-mac/issues/1031
-@pytest.mark.skipif(platform.system() != 'Linux', reason='Only runs on Unix systems')
+
 @pytest.mark.integration
 @pytest.mark.usefixtures("dd_environment")
 def test_check_integration(instance, aggregator):
