bab41b872f0e9fcfcbcf331a8570df42abae9141
Julia Simon <611228+hithwen@users.noreply.github.com>
2019-10-23 11:59:28 +0200

    Remove OSX metrics and sort expected metrics

--- disk/tests/test_e2e.py
+++ disk/tests/test_e2e.py
@@ -14,7 +14,3 @@ def test_check(dd_agent_check):
     aggregator = dd_agent_check()
     for metric in common.EXPECTED_METRICS:
         aggregator.assert_metric(metric['metric'], metric_type=aggregator.GAUGE, device=metric['device'])
-    if sys.platform == 'darwin':
-        for metric in common.OSX_METRICS:
-            aggregator.assert_metric(metric['metric'], metric_type=aggregator.GAUGE, device=metric['device'])
-    aggregator.assert_all_metrics_covered()
