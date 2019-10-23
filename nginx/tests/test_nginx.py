6fb8df66901d765b3afe5d3cdd5aa2ec1994525a
Danni <23065951+mathildapurr@users.noreply.github.com>
2019-10-23 03:39:17 -0400

    Submit version metadata (#4736)
    * adding version as metadata
    
    * add version env var
    
    * added logging when no version is available and tests
    
    * added metadata test for nginx
    
    * add metadata test
    
    * added function to get version metadata
    
    * modified test to accomodate vts
    
    * fix style
    
    * remove debugging
    
    * parse formatting

--- nginx/tests/test_nginx.py
+++ nginx/tests/test_nginx.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2018
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import mock
 import pytest
 import requests
 
@@ -36,3 +37,17 @@ def test_connect_ssl(check, instance_ssl, aggregator):
         instance_ssl['ssl_validation'] = True
         check_ssl = check(instance_ssl)
         check_ssl.check(instance_ssl)
+
+
+@pytest.mark.usefixtures('dd_environment')
+def test_metadata(check, instance, aggregator, version_metadata):
+    nginx_check = check(instance)
+    nginx_check.check_id = 'test:123'
+
+    with mock.patch('datadog_checks.base.stubs.datadog_agent.set_check_metadata') as m:
+        nginx_check.check(instance)
+
+        for name, value in version_metadata.items():
+            m.assert_any_call('test:123', name, value)
+
+        assert m.call_count == len(version_metadata)
