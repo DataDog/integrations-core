ba1646160fa0cf39b30f8e1290dea0be6283a699
mathildapurr <23065951+mathildapurr@users.noreply.github.com>
2019-10-29 17:43:17 -0400

    added comment

--- mesos_slave/tests/test_integration_e2e.py
+++ mesos_slave/tests/test_integration_e2e.py
@@ -42,4 +42,5 @@ def assert_metrics_covered(aggregator):
         aggregator.assert_metric(v[0])
 
     aggregator.assert_all_metrics_covered()
+    # We should submit 2 service checks per check run because 2 endpoints were tried
     aggregator.assert_service_check('mesos_slave.can_connect', status=AgentCheck.OK, count=4)
