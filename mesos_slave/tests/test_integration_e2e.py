6c974fbfd926027e369e717eb3ac0d8393b13c68
mathildapurr <23065951+mathildapurr@users.noreply.github.com>
2019-10-29 10:49:23 -0400

    add rate back

--- mesos_slave/tests/test_integration_e2e.py
+++ mesos_slave/tests/test_integration_e2e.py
@@ -22,7 +22,7 @@ def test_check_integration(instance, aggregator):
 
 @pytest.mark.e2e
 def test_check_e2e(dd_agent_check, instance):
-    aggregator = dd_agent_check(instance)
+    aggregator = dd_agent_check(instance, rate=True)
     assert_metrics_covered(aggregator)
 
 
