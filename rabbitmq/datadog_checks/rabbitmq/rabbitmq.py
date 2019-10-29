2daaae29c353de7b2ef8ded7638a17b92b200a69
gianpaul.rachiele <gianpaul.rachiele@COMP10546.local>
2019-10-29 11:45:52 -0400

    Add version metadata to RabbitMQ check

--- rabbitmq/datadog_checks/rabbitmq/rabbitmq.py
+++ rabbitmq/datadog_checks/rabbitmq/rabbitmq.py
@@ -200,6 +200,50 @@ class RabbitMQ(AgentCheck):
 
         return base_url, max_detailed, specified, custom_tags, suppress_warning
 
+    def _collect_metadata(self, base_url):
+        # Versioning in RabbitMQ seems to be standard semantic versioning.
+        # https://www.rabbitmq.com/changelog.html
+
+        # The /overview endpoint from the RabbitMQ status API outputs
+        # JSON in the following format which includes versioning information
+        # more information here: https://pulse.mozilla.org/doc/stats.html
+        '''
+        {
+          "management_version": "3.7.19",
+          "rates_mode": "basic",
+          "sample_retention_policies": {
+            "global": [
+              600,
+              3600,
+              28800,
+              86400
+            ],
+            "basic": [...],
+            "detailed": [...]
+          },
+          "exchange_types": [...],
+          "rabbitmq_version": "3.7.19",
+          "cluster_name": "rabbitmqtest",
+          "erlang_version": "22.1.4",
+          "erlang_full_version": "Erlang/OTP 22 [erts-10.5.3] [source] [64-bit] [smp:4:4]...",
+          "message_stats": {...},
+            "confirm": 10,
+            "confirm_details": {...},
+            "deliver": 0,
+            ...
+        }
+        '''
+        overview_url = urljoin(base_url, 'overview')
+        overview_response = self._get_data(overview_url)
+
+        # the repsonse is has unicode in it so converting to a string below
+        version = str(overview_response['rabbitmq_version'])
+        if version:
+            self.set_metadata('version', version)
+            self.log.debug(u"found rabbitmq version {}".format(version))
+        else:
+            self.log.warning(u"could not retrieve rabbitmq version information")
+
     def _get_vhosts(self, instance, base_url):
         vhosts = instance.get('vhosts')
 
@@ -217,7 +261,8 @@ class RabbitMQ(AgentCheck):
             with warnings.catch_warnings():
                 vhosts = self._get_vhosts(instance, base_url)
                 self.cached_vhosts[base_url] = vhosts
-
+                # this collects and sets versioning metadata
+                self._collect_metadata(base_url)
                 limit_vhosts = []
                 if self._limit_vhosts(instance):
                     limit_vhosts = vhosts
