17d57f00e7284f010f997388dd681803e7b30bd6
ChristineTChen <christine.chen@datadoghq.com>
2019-11-08 13:16:10 -0500

    Working terraform setup

--- cilium/tests/terraform/script.py
+++ cilium/tests/terraform/script.py
@@ -27,23 +27,7 @@ check_call(
     ]
 )
 
-
-# We don't care about the platform as we only use yaml files
-check_call(
-    [
-        "curl",
-        "-o",
-        "cilium.tar.gz",  # Need to update with versions where possible
-        "-L",
-        "https://github.com/cilium/cilium/archive/{version}.tar.gz".format(version=version),
-    ]
-)
-check_call(["tar", "xf", "{version}.tar.gz".format(version=version)])
-
-
 check_call(["kubectl", "create", "ns", "cilium"])
 
 check_call(["kubectl", "create", "-f", config])
 
-# Restart pods
-check_call(["kubectl", "delete", "pods", "-n", "cilium"])
