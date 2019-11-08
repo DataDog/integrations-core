d31aa1723006d0b3f7413e387876f4820acd2712
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-11-08 11:03:48 +0100

    Add note to migrate unit test into e2e

--- postfix/tests/test_unit.py
+++ postfix/tests/test_unit.py
@@ -7,12 +7,12 @@ from datadog_checks.postfix import PostfixCheck
 
 MOCK_VERSION = '1.3.1'
 
-
 @mock.patch(
     'datadog_checks.postfix.postfix.get_subprocess_output',
     return_value=('mail_version = {}'.format(MOCK_VERSION), None, None),
 )
 def test_collect_metadata(aggregator, datadog_agent):
+    # TODO: Migrate this test as e2e test when it's possible to retrieve the metadata from the Agent
     check = PostfixCheck('postfix', {}, [{}])
     check.check_id = 'test:123'
 
