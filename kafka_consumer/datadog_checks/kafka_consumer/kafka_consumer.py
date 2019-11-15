3dc4aca7d0ba7b0cb35a8300cd6d872cdcacd7d6
Marc Ian Bucad <ian.bucad@gmail.com>
2019-11-15 11:46:25 +1100

    fix style

--- kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
+++ kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
@@ -129,8 +129,8 @@ class KafkaCheck(AgentCheck):
             bootstrap_servers=kafka_connect_str,
             client_id='dd-agent',
             request_timeout_ms=self.init_config.get('kafka_timeout', DEFAULT_KAFKA_TIMEOUT) * 1000,
-            # There is a bug with kafka-python where pinning api_version for KafkaAdminClient raises an IncompatibleBrokerVersion. Change to api_version=self._kafka_version once fixed.
-            # api_version=self._kafka_version,
+            # There is a bug with kafka-python where pinning api_version for KafkaAdminClient raises an
+            # `IncompatibleBrokerVersion`. Change to `api_version=self._kafka_version` once fixed upstream.
             api_version=None,
             # While we check for SASL/SSL params, if not present they will default to the kafka-python values for
             # plaintext connections
