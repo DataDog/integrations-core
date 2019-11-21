e80eced7daba434b5a652cefd372a42c5688bca0
Thomas Hervé <thomas.herve@datadoghq.com>
2019-11-21 15:59:20 +0100

    Handle total_time_to_temporal_percent missing (#5055)
    total_time_to_temporal_percent is only going to be in 6.16, let's
    provide a fallback for now.

--- snmp/datadog_checks/snmp/snmp.py
+++ snmp/datadog_checks/snmp/snmp.py
@@ -20,9 +20,17 @@ from six import iteritems
 
 from datadog_checks.base import AgentCheck, ConfigurationError, is_affirmative
 from datadog_checks.base.errors import CheckException
-from datadog_checks.base.utils.common import total_time_to_temporal_percent
 
 from .config import InstanceConfig
+
+try:
+    from datadog_checks.base.utils.common import total_time_to_temporal_percent
+except ImportError:
+
+    # Provide fallback for agent < 6.16
+    def total_time_to_temporal_percent(total_time, scale=1000):
+        return total_time / scale * 100
+
 
 try:
     from datadog_agent import get_config, read_persistent_cache, write_persistent_cache
