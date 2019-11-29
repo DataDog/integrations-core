10a387ca6d29a4cef0ac3e98cea3073ec181398f
Xavier Lucas <xavier.lucas@datadoghq.com>
2019-11-29 17:33:49 +0100

    Handle scheme in prometheus_url instead of separate option (#4913)

--- kube_apiserver_metrics/datadog_checks/kube_apiserver_metrics/kube_apiserver_metrics.py
+++ kube_apiserver_metrics/datadog_checks/kube_apiserver_metrics/kube_apiserver_metrics.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from copy import deepcopy
+from re import match
 
 from six import iteritems
 
@@ -75,8 +76,15 @@ class KubeAPIServerMetricsCheck(OpenMetricsBaseCheck):
         """
         kube_apiserver_metrics_instance = deepcopy(instance)
         endpoint = instance.get('prometheus_url')
-        scheme = instance.get('scheme', self.DEFAULT_SCHEME)
-        kube_apiserver_metrics_instance['prometheus_url'] = "{0}://{1}".format(scheme, endpoint)
+        prometheus_url = endpoint
+
+        # Allow using a proper URL without introducing a breaking change since
+        # the scheme option is deprecated.
+        if not match('^https?://.*$', endpoint):
+            scheme = instance.get('scheme', self.DEFAULT_SCHEME)
+            prometheus_url = "{0}://{1}".format(scheme, endpoint)
+
+        kube_apiserver_metrics_instance['prometheus_url'] = prometheus_url
 
         # Most set ups are using self signed certificates as the APIServer can be used as a CA.
         ssl_verify = instance.get('ssl_verify', self.DEFAULT_SSL_VERIFY)
