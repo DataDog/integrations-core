a4c3a92b2672225efde5692b46c9843cef35aefc
Florimond Manca <florimond.manca@datadoghq.com>
2019-12-06 12:21:09 +0100

    Add comment about map_count computation

--- .azure-pipelines/scripts/sap_hana/linux/50_increase_docker_memory.sh
+++ .azure-pipelines/scripts/sap_hana/linux/50_increase_docker_memory.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
-sudo sysctl -w vm.max_map_count=$(expr 6 \* 65536)
+# 6GB is proven to be enough for SAP HANA to run.
+# The default number of memory addresses is 65536, i.e. 512MB (Linux 64-bit).
+# => To get 6GB, we multiply that amount by 12.
+sudo sysctl -w vm.max_map_count=$(expr 12 \* 65536)
 
 set +ex
