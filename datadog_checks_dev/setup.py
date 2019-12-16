32098e37216ebb43a8d13162eb01f907c9c77e4a
Alexandre Yang <alexandre.yang@datadoghq.com>
2019-12-16 22:58:43 +0100

    Fix coverage

--- datadog_checks_dev/setup.py
+++ datadog_checks_dev/setup.py
@@ -22,7 +22,7 @@ with open(path.join(HERE, 'README.md'), 'r', encoding='utf-8') as f:
 
 
 REQUIRES = [
-    'coverage>=4.5.1',
+    'coverage==4.5.4',
     'mock',
     'psutil',
     'PyYAML>=5.1',
