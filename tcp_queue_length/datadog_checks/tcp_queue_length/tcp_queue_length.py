6a36dd31482012e08451d5b2fc09d11aa97969f3
Lénaïc Huard <lenaic.huard@datadoghq.com>
2019-12-17 15:31:07 +0100

    First draft

--- tcp_queue_length/datadog_checks/tcp_queue_length/tcp_queue_length.py
+++ tcp_queue_length/datadog_checks/tcp_queue_length/tcp_queue_length.py
@@ -1,9 +1,38 @@
 # (C) Datadog, Inc. 2019
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import requests_unixsocket
+
 from datadog_checks.base import AgentCheck
 
-
 class TcpQueueLengthCheck(AgentCheck):
+
+    NAMESPACE = "tcp_queue"
+
+    def __init__(self, *args, **kwargs):
+        super(AgentCheck, self).__init__(*args, **kwargs)
+        self.session = requests_unixsocket.Session()
+
     def check(self, instance):
-        pass
+        try:
+            r = self.session.get('http+unix://%2Fopt%2Fdatadog-agent%2Frun%2Fsysprobe.sock/probe/tcp_queue_length', timeout=10)
+            r.raise_for_status()
+        except Exception as e:
+            self.log.warning('GET on system-probe socket failed: {}'.format(str(e)))
+            return
+
+        instance_tags = instance.get('tags', [])
+
+        for line in r.json():
+            try:
+                tags = ['saddr:{}'.format(line['Conn']['Saddr']),
+                        'daddr:{}'.format(line['Conn']['Daddr']),
+                        'sport:{}'.format(line['Conn']['Sport']),
+                        'dport:{}'.format(line['Conn']['Dport'])]
+
+                self.gauge(self.NAMESPACE + '.rqueue.min', float(line['Stats']['Rqueue']['Min']), instance_tags + tags)
+                self.gauge(self.NAMESPACE + '.rqueue.max', float(line['Stats']['Rqueue']['Max']), instance_tags + tags)
+                self.gauge(self.NAMESPACE + '.wqueue.min', float(line['Stats']['Wqueue']['Min']), instance_tags + tags)
+                self.gauge(self.NAMESPACE + '.wqueue.max', float(line['Stats']['Wqueue']['Max']), instance_tags + tags)
+            except KeyError as e:
+                self.log.error('Invalid line received from `/probe/tcp_queue_length`: {}'.format(e))
