136d6624b2533897adc4b130af3624112375dcaa
Ofek Lev <ofekmeister@gmail.com>
2019-12-18 02:00:21 -0500

    cleaner

--- redisdb/tests/test_auth.py
+++ redisdb/tests/test_auth.py
@@ -3,6 +3,8 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from __future__ import unicode_literals
 
+import re
+
 import pytest
 
 from datadog_checks.redisdb import Redis
@@ -29,16 +31,8 @@ def test_redis_auth_empty_pass(redis_auth):
     redis = Redis('redisdb', {}, {})
     instance = {'host': HOST, 'port': PORT, 'password': ''}
 
-    try:
+    with pytest.raises(Exception, match=re.compile('authentication required|operation not permitted', re.I)):
         redis.check(instance)
-    except Exception as e:
-        error = str(e).lower()
-        pre28_err = 'noauth authentication required'
-        post28_err = 'operation not permitted'
-        post33_err = 'authentication required'
-        assert pre28_err in error or post28_err in error or post33_err in error
-    else:
-        raise AssertionError('Check should raise an exception')
 
 
 @pytest.mark.integration
