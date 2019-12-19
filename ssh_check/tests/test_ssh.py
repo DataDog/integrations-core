1445e1febd05f2eddf435870bbfc7b265650d14d
gianpaulrachiele <42185750+gianpaulrachiele@users.noreply.github.com>
2019-12-19 09:10:05 -0500

    Add version metadata (#5016)
    * Collect version metadata
    Co-Authored-By: Alexandre Yang <alexandre.yang@datadoghq.com>

--- ssh_check/tests/test_ssh.py
+++ ssh_check/tests/test_ssh.py
@@ -2,12 +2,16 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import threading
+from collections import namedtuple
 
 import pytest
+from mock import MagicMock
 
 from datadog_checks.ssh_check import CheckSSH
 
 from . import common
+
+pytestmark = pytest.mark.unit
 
 
 def test_ssh(aggregator):
@@ -44,3 +48,61 @@ def test_ssh_bad_config(aggregator):
     # Check that we've closed all connections, if not we're leaking threads
     common.wait_for_threads()
     assert nb_threads == threading.active_count()
+
+
+@pytest.mark.parametrize(
+    'version, metadata',
+    [
+        (
+            'OpenSSH_for_Windows_7.7p1, LibreSSL 2.6.5',
+            {
+                'version.major': '7',
+                'version.minor': '7',
+                'version.release': 'p1',
+                'version.scheme': 'ssh_check',
+                'version.raw': 'OpenSSH_for_Windows_7.7p1, LibreSSL 2.6.5',
+                'flavor': 'OpenSSH',
+            },
+        ),
+        (
+            'SSH-2.0-OpenSSH_8.1',
+            {
+                'version.major': '8',
+                'version.minor': '1',
+                'version.scheme': 'ssh_check',
+                'version.raw': 'SSH-2.0-OpenSSH_8.1',
+                'flavor': 'OpenSSH',
+            },
+        ),
+        (
+            'SSH-2.0-OpenSSH_7.4p1 Debian-10+deb9u2',
+            {
+                'version.major': '7',
+                'version.minor': '4',
+                'version.release': 'p1',
+                'version.scheme': 'ssh_check',
+                'version.raw': 'SSH-2.0-OpenSSH_7.4p1 Debian-10+deb9u2',
+                'flavor': 'OpenSSH',
+            },
+        ),
+    ],
+)
+def test_collect_metadata(version, metadata, datadog_agent):
+    client = MagicMock()
+    client.get_transport = MagicMock(return_value=namedtuple('Transport', ['remote_version'])(version))
+
+    ssh = CheckSSH('ssh_check', {}, {}, list(common.INSTANCES.values()))
+    ssh.check_id = 'test:123'
+    ssh._collect_metadata(client)
+    datadog_agent.assert_metadata('test:123', metadata)
+
+
+def test_collect_bad_metadata(datadog_agent):
+    client = MagicMock()
+    client.get_transport = MagicMock(return_value=namedtuple('Transport', ['remote_version'])('Cannot parse this'))
+
+    ssh = CheckSSH('ssh_check', {}, {}, list(common.INSTANCES.values()))
+    ssh.check_id = 'test:123'
+    ssh._collect_metadata(client)
+    datadog_agent.assert_metadata_count(1)
+    datadog_agent.assert_metadata('test:123', {'flavor': 'unknown'})
