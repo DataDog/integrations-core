5df8c9ac180e5cfc603fb80fb5d1397bd43ce46a
Mike Garabedian <mike.garabedian@datadoghq.com>
2019-12-19 14:56:08 -0500

    Increase CI to 3GB

--- .azure-pipelines/scripts/activemq/linux/50_increase_docker_memory.sh
+++ .azure-pipelines/scripts/activemq/linux/50_increase_docker_memory.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-# Allocate 2GB for activemq
+# Allocate 3GB for activemq
 # The default number of memory addresses is 65536, i.e. 512MB (Linux 64-bit).
-# => To get 2GB, we multiply that amount by 4.
-sudo sysctl -w vm.max_map_count=$(expr 4 \* 65536)
+# => To get 3GB, we multiply that amount by 6.
+sudo sysctl -w vm.max_map_count=$(expr 6 \* 65536)
 
 set +ex
