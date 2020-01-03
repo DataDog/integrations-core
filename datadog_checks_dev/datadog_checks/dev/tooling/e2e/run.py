fb7ded4356467c509659e6ce70e0e38f37af71b0
Mike Garabedian <mike.garabedian@datadoghq.com>
2020-01-03 17:13:16 -0500

    Add debug option to base ddev command

--- datadog_checks_dev/datadog_checks/dev/tooling/e2e/run.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/e2e/run.py
@@ -4,8 +4,25 @@
 from ..._env import E2E_ENV_VAR_PREFIX, E2E_SET_UP, E2E_TEAR_DOWN
 from ...subprocess import run_command
 from ...utils import chdir, get_ci_env_vars, path_join
+from ..commands.console import echo_debug
 from ..constants import get_root
 from .format import parse_config_from_result
+
+
+def _execute(check, command, env_vars):
+    if E2E_TEAR_DOWN in env_vars:
+        operation = 'starting'
+    else:
+        operation = 'stopping'
+
+    with chdir(path_join(get_root(), check), env_vars=env_vars):
+        echo_debug('%s env with env_vars: %s' % (operation, env_vars), cr=True, indent=True)
+        echo_debug('%s env with command: %s' % (operation, command), indent=True)
+        result = run_command(command, capture=True)
+        echo_debug('command result stdout: %s' % result.stdout, indent=True)
+        echo_debug('command result stderr: %s' % result.stderr, indent=True)
+
+    return result
 
 
 def start_environment(check, env):
@@ -16,9 +33,7 @@ def start_environment(check, env):
         'TOX_TESTENV_PASSENV': '{} USERNAME PYTEST_ADDOPTS {}'.format(E2E_TEAR_DOWN, ' '.join(get_ci_env_vars())),
     }
 
-    with chdir(path_join(get_root(), check), env_vars=env_vars):
-        result = run_command(command, capture=True)
-
+    result = _execute(check, command, env_vars)
     return parse_config_from_result(env, result)
 
 
@@ -33,7 +48,5 @@ def stop_environment(check, env, metadata=None):
     }
     env_vars.update((metadata or {}).get('env_vars', {}))
 
-    with chdir(path_join(get_root(), check), env_vars=env_vars):
-        result = run_command(command, capture=True)
-
+    result = _execute(check, command, env_vars)
     return parse_config_from_result(env, result)
