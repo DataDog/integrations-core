9089e0b88690ded4a6c1629f73eed495daa2bc24
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-01-29 15:29:37 +0100

    Refactor traced decorator

--- datadog_checks_base/datadog_checks/base/utils/tracing.py
+++ datadog_checks_base/datadog_checks/base/utils/tracing.py
@@ -1,8 +1,8 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
+import functools
 
-import wrapt
 from ddtrace import tracer
 
 from ..config import is_affirmative
@@ -14,16 +14,18 @@ except ImportError:
     datadog_agent = None
 
 
-@wrapt.decorator
-def traced(wrapped, instance, args, kwargs):
-    if datadog_agent is None:
-        return wrapped(*args, **kwargs)
+def traced(fn):
+    @functools.wraps(fn)
+    def traced_wrapper(instance, *args, **kwargs):
+        if datadog_agent is None:
+            return fn(instance, *args, **kwargs)
 
-    trace_check = is_affirmative(instance.init_config.get('trace_check'))
-    integration_tracing = is_affirmative(datadog_agent.get_config('integration_tracing'))
+        trace_check = is_affirmative(instance.init_config.get('trace_check'))
+        integration_tracing = is_affirmative(datadog_agent.get_config('integration_tracing'))
 
-    if integration_tracing and trace_check:
-        with tracer.trace('integration.check', service='integrations-tracing', resource=instance.name):
-            return wrapped(*args, **kwargs)
+        if integration_tracing and trace_check:
+            with tracer.trace('integration.check', service='integrations-tracing', resource=instance.name):
+                return fn(instance, *args, **kwargs)
+        return fn(instance, *args, **kwargs)
 
-    return wrapped(*args, **kwargs)
+    return traced_wrapper
