b7b2b31f93fcf01b79d148d2296726de73d4b1e8
Ofek Lev <ofekmeister@gmail.com>
2020-01-31 09:46:13 -0500

    Fix "Allow help options to ddev run command" (#5605)
    * Revert "Allow help options to ddev run command (#5602)"
    
    This reverts commit 446823f1e50412c2a27b72e72a5cb2ea7b6d5399.
    
    * meh, not a fan

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/run.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/run.py
@@ -14,7 +14,7 @@ from .console import UNKNOWN_OPTIONS
 @click.pass_context
 def run(ctx, args):
     """Run commands in the proper repo."""
-    if not args or '-h' in args or '--help' in args:
+    if not args or (len(args) == 1 and args[0] in ('-h', '--help')):
         click.echo(ctx.get_help())
         return
 
