40c8543f41d9abe75642959b8735aec754c21d5a
FlorianVeaux <florian.veaux@datadoghq.com>
2020-02-03 14:56:16 -0500

    Remap vsphere filename (#5628)

--- vsphere/datadog_checks/vsphere/__init__.py
+++ vsphere/datadog_checks/vsphere/__init__.py
@@ -2,6 +2,6 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 from .__about__ import __version__
-from .vsphere_new import VSphereCheck
+from .vsphere import VSphereCheck
 
 __all__ = ['__version__', 'VSphereCheck']
