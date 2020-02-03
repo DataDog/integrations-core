52e2ffb86c962ab630a0534a4b2d320cbfcc3ad8
florian.veaux <florian.veaux@datadoghq.com>
2020-02-03 12:59:36 -0500

    Keep commit history

--- vsphere/datadog_checks/vsphere/__init__.py
+++ vsphere/datadog_checks/vsphere/__init__.py
@@ -2,6 +2,6 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 from .__about__ import __version__
-from .vsphere import VSphereCheck
+from .vsphere_new import VSphereCheck
 
 __all__ = ['__version__', 'VSphereCheck']
