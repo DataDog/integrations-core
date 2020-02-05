3d231e61d8c3d2f47dafd837b6a35a6bad4c0d5e
sarah-witt <sarah.witt@COMP11958.local>
2020-02-05 10:55:32 -0500

    Fix user typo

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/release/testable.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/release/testable.py
@@ -71,7 +71,7 @@ def create_jira_issue(client, teams, pr_title, pr_url, pr_body, dry_run, pr_auth
 def pick_card_member(config, author, team):
     """Return a member to assign to the created issue.
     In practice, it returns one jira user which is not the PR author, for the given team.
-    For it to work, you need a `jira_user_$team` table in your ddev configuration,
+    For it to work, you need a `jira_users_$team` table in your ddev configuration,
     with keys being github users and values being their corresponding jira IDs (not names).
 
     For example::
