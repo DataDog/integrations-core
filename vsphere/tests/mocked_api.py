545a79e724dc8238325b757da19a70a07f6590e5
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-02-11 12:25:00 +0100

    Add test case

--- vsphere/tests/mocked_api.py
+++ vsphere/tests/mocked_api.py
@@ -124,6 +124,9 @@ class MockedRestAPI(VSphereRestAPI):
             TagAssociation.TagToObjects(
                 tag_id='tag_id_2', object_ids=[DynamicID(type='HostSystem', id='10.0.0.104-1')]
             ),
+            TagAssociation.TagToObjects(
+                tag_id='tag_id_2', object_ids=[DynamicID(type='Datastore', id='NFS-Share-1')]
+            ),
         ]
 
     def smart_connect(self):
--- vsphere/tests/test_check.py
+++ vsphere/tests/test_check.py
@@ -147,6 +147,11 @@ def test_collect_tags(aggregator, dd_run_check, realtime_instance):
         tags=['my_cat_name_2:my_tag_name_2', 'vcenter_server:FAKE'],
         hostname='10.0.0.104',
     )
+    aggregator.assert_metric(
+        'vsphere.datastore.maxTotalLatency.latest',
+        tags=['my_cat_name_2:my_tag_name_2', 'vcenter_server:FAKE'],
+        hostname='10.0.0.104',
+    )
 
 
 @pytest.mark.usefixtures('mock_type', 'mock_threadpool', 'mock_api')
