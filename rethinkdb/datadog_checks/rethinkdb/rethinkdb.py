324b4730def4ed7a4aeea5b7c4f67208737008f3
Florimond Manca <florimond.manca@datadoghq.com>
2020-02-12 12:19:54 +0100

    Add initial type hints

--- rethinkdb/datadog_checks/rethinkdb/rethinkdb.py
+++ rethinkdb/datadog_checks/rethinkdb/rethinkdb.py
@@ -1,9 +1,12 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from typing import Any, Dict
+
 from datadog_checks.base import AgentCheck
 
 
 class RethinkdbCheck(AgentCheck):
     def check(self, instance):
+        # type: (Dict[str, Any]) -> None
         pass
--- rethinkdb/tests/test_rethinkdb.py
+++ rethinkdb/tests/test_rethinkdb.py
@@ -1,10 +1,14 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from typing import Any, Dict
+
+from datadog_checks.base.stubs.aggregator import AggregatorStub
 from datadog_checks.rethinkdb import RethinkdbCheck
 
 
 def test_check(aggregator, instance):
+    # type: (AggregatorStub, Dict[str, Any]) -> None
     check = RethinkdbCheck('rethinkdb', {}, {})
     check.check(instance)
 
