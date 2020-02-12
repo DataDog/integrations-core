aaecdac7db0ceda30396c49961e133f207bad794
Florimond Manca <florimond.manca@gmail.com>
2020-02-12 16:17:47 +0100

    Fix usage of legacy signature in check integration test (#5718)

--- datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/tests/test_{check_name}.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/tests/test_{check_name}.py
@@ -6,7 +6,7 @@ from datadog_checks.{check_name} import {check_class}
 
 def test_check(aggregator, instance):
     # type: (AggregatorStub, Dict[str, Any]) -> None
-    check = {check_class}('{check_name}', {{}}, {{}})
+    check = {check_class}('{check_name}', {{}}, [instance])
     check.check(instance)
 
     aggregator.assert_all_metrics_covered()
