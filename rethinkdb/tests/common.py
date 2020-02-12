3ae186536d3f47bf0eacd608571c6c69844388f0
Florimond Manca <florimond.manca@datadoghq.com>
2020-02-12 15:02:33 +0100

    Setup Docker Compose

---
+++ rethinkdb/tests/common.py
@@ -0,0 +1,17 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+
+from datadog_checks.utils.common import get_docker_hostname
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+ROOT = os.path.dirname(os.path.dirname(HERE))
+
+CHECK_NAME = 'rethinkdb'
+
+IMAGE = 'rethinkdb:2.4.0'
+CONTAINER_NAME = 'rethinkdb'
+
+HOST = get_docker_hostname()
+PORT = 28015
--- rethinkdb/tests/conftest.py
+++ rethinkdb/tests/conftest.py
@@ -1,14 +1,28 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+from typing import Any, Dict, Iterator
+
 import pytest
+
+from datadog_checks.dev import docker_run
+
+from .common import CONTAINER_NAME, HERE, IMAGE, PORT
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    yield
+    # type: () -> Iterator[dict]
+    compose_file = os.path.join(HERE, 'compose', 'docker-compose.yaml')
 
+    env_vars = {
+        'RETHINKDB_PORT': str(PORT),
+        'RETHINKDB_IMAGE': IMAGE,
+        'RETHINKDB_CONTAINER_NAME': CONTAINER_NAME,
+    }
 
-@pytest.fixture
-def instance():
-    return {}
+    with docker_run(compose_file, env_vars=env_vars, log_patterns=[r'Server ready.*']):
+        instance = {}  # type: Dict[str, Any]
+        config = {'instances': [instance]}
+        yield config
