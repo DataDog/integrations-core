15edf74b71a2665aaa245b18bb05bfbf95a9f851
Florimond Manca <florimond.manca@datadoghq.com>
2020-02-12 11:04:52 +0100

    Add type checking to integration check template

--- datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/datadog_checks/{check_name}/{check_name}.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/datadog_checks/{check_name}/{check_name}.py
@@ -1,6 +1,9 @@
-{license_header}from datadog_checks.base import AgentCheck
+{license_header}from typing import Any, Dict
+
+from datadog_checks.base import AgentCheck
 
 
 class {check_class}(AgentCheck):
     def check(self, instance):
+        # type: (Dict[str, Any]) -> None
         pass
