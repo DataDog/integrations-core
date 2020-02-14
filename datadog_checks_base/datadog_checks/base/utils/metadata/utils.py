7f27809ad8aa3e7f3e3a34ee899fb17157d34c10
Julia <611228+hithwen@users.noreply.github.com>
2020-02-14 16:02:55 +0100

    Update utils.py

--- datadog_checks_base/datadog_checks/base/utils/metadata/utils.py
+++ datadog_checks_base/datadog_checks/base/utils/metadata/utils.py
@@ -3,8 +3,6 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from six import binary_type, text_type
 
-from datadog_checks.base import AgentCheck
-
 
 def is_primitive(obj):
     # https://github.com/python/cpython/blob/4f82a53c5d34df00bf2d563c2417f5e2638d1004/Lib/json/encoder.py#L357-L377
