f874ddb2dd8c7d795d27342f8bb2bb6bf904a363
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-02-14 10:50:04 +0100

    Add metadata collection decorator

--- datadog_checks_base/datadog_checks/base/utils/metadata/utils.py
+++ datadog_checks_base/datadog_checks/base/utils/metadata/utils.py
@@ -7,3 +7,15 @@ from six import binary_type, text_type
 def is_primitive(obj):
     # https://github.com/python/cpython/blob/4f82a53c5d34df00bf2d563c2417f5e2638d1004/Lib/json/encoder.py#L357-L377
     return obj is None or isinstance(obj, (binary_type, bool, float, int, text_type))
+
+
+def is_metadata_collection_enabled(func):
+    """
+    Use this decorator to avoid making unnecesary api calls when metadata collection is disabled
+    :param func:
+    :return:
+    """
+    def _decorator(self, *args, **kwargs):
+        if self.agentConfig.get('enable_metadata_collection', True):
+            return func(self, *args, **kwargs)
+    return _decorator
