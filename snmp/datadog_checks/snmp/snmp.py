115792a9004902be9fb02c8e8e6db9813a849e0a
Florimond Manca <florimond.manca@datadoghq.com>
2020-02-18 14:35:30 +0100

    Use all matching profiles instead of only the most specific one.

--- snmp/datadog_checks/snmp/snmp.py
+++ snmp/datadog_checks/snmp/snmp.py
@@ -7,6 +7,7 @@ import json
 import threading
 import time
 from collections import defaultdict
+from typing import Dict, List, Optional
 
 import pysnmp.proto.rfc1902 as snmp_type
 from pyasn1.codec.ber import decoder
@@ -68,7 +69,7 @@ class SnmpCheck(AgentCheck):
         self.ignore_nonincreasing_oid = is_affirmative(init_config.get('ignore_nonincreasing_oid', False))
 
         self.profiles = init_config.get('profiles', {})
-        self.profiles_by_oid = {}
+        self.profiles_by_oid = {}  # type: Dict[str, str]
         self._load_profiles()
 
         self.instance['name'] = self._get_instance_key(self.instance)
@@ -116,6 +117,7 @@ class SnmpCheck(AgentCheck):
         return key
 
     def discover_instances(self):
+        # type: () -> None
         config = self._config
         discovery_interval = config.instance.get('discovery_interval', 3600)
         while self._running:
@@ -129,19 +131,23 @@ class SnmpCheck(AgentCheck):
                 instance['ip_address'] = host
 
                 host_config = self._build_config(instance)
+
                 try:
                     sys_object_oid = self.fetch_sysobject_oid(host_config)
                 except Exception as e:
                     self.log.debug("Error scanning host %s: %s", host, e)
                     continue
+
                 try:
-                    profile = self._profile_for_sysobject_oid(sys_object_oid)
+                    profiles = self._profiles_for_sysobject_oid(sys_object_oid)
                 except ConfigurationError:
                     if not (host_config.all_oids or host_config.bulk_oids):
                         self.log.warning("Host %s didn't match a profile for sysObjectID %s", host, sys_object_oid)
                         continue
                 else:
-                    host_config.refresh_with_profile(self.profiles[profile], self.warning, self.log)
+                    for profile in profiles:
+                        host_config.refresh_with_profile(self.profiles[profile], self.warning, self.log)
+
                 config.discovered_instances[host] = host_config
 
                 write_persistent_cache(self.check_id, json.dumps(list(config.discovered_instances)))
@@ -273,17 +279,15 @@ class SnmpCheck(AgentCheck):
         self.log.debug('Returned vars: %s', var_binds)
         return var_binds[0][1].prettyPrint()
 
-    def _profile_for_sysobject_oid(self, sys_object_oid):
-        """Return, if any, a matching profile for sys_object_oid.
-
-        If several profiles match, it will return the longer match, ie the
-        closest one to the sys_object_oid.
-        """
+    def _profiles_for_sysobject_oid(self, sys_object_oid):
+        # type: (str) -> List[str]
+        """Return all profiles that match the given sys_object_id."""
         oids = [oid for oid in self.profiles_by_oid if fnmatch.fnmatch(sys_object_oid, oid)]
-        oids.sort()
+
         if not oids:
             raise ConfigurationError('No profile matching sysObjectID {}'.format(sys_object_oid))
-        return self.profiles_by_oid[oids[-1]]
+
+        return [self.profiles_by_oid[oid] for oid in oids]
 
     def _consume_binds_iterator(self, binds_iterator, config):
         all_binds = []
@@ -350,14 +354,16 @@ class SnmpCheck(AgentCheck):
             self._check_with_config(config)
 
     def _check_with_config(self, config):
+        # type: (InstanceConfig) -> Optional[str]
         # Reset errors
         instance = config.instance
         error = results = None
         try:
             if not (config.all_oids or config.bulk_oids):
                 sys_object_oid = self.fetch_sysobject_oid(config)
-                profile = self._profile_for_sysobject_oid(sys_object_oid)
-                config.refresh_with_profile(self.profiles[profile], self.warning, self.log)
+                profiles = self._profiles_for_sysobject_oid(sys_object_oid)
+                for profile in profiles:
+                    config.refresh_with_profile(self.profiles[profile], self.warning, self.log)
 
             if config.all_oids or config.bulk_oids:
                 self.log.debug('Querying device %s', config.ip_address)
