fece109864dc1aa42ef06d67bc1427fe7e8c1209
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-02-18 17:17:49 +0100

    add touch

--- datadog_checks_dev/datadog_checks/dev/tooling/e2e/docker.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/e2e/docker.py
@@ -188,7 +188,7 @@ class DockerInterface(object):
 
     def update_os_version(self):
         command = ['docker', 'exec', self.container_name]
-        command.extend(["bash", "-c", "'echo 10 > /etc/debian_version'"])
+        command.extend(["bash", "-c", "'touch /etc/debian_version && echo 10 > /etc/debian_version'"])
         run_command(command, capture=True, check=True)
 
     def update_base_package(self):
