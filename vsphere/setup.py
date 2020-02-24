a4d5db323910852ad701f9cd9953c3bedb63ae1d
Thomas Hervé <thomas.herve@datadoghq.com>
2020-02-24 19:04:54 +0100

    Bump base version (#5842)
    We need it for the extra_headers feature

--- vsphere/setup.py
+++ vsphere/setup.py
@@ -24,7 +24,7 @@ def get_requirements(fpath):
         return f.readlines()
 
 
-CHECKS_BASE_REQ = 'datadog_checks_base>=6.4.0'
+CHECKS_BASE_REQ = 'datadog_checks_base>=11.0.0'
 
 setup(
     name='datadog-vsphere',
