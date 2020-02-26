2e982e23e13615284f8a4fba5b5a8d1c4e008e0d
florian.veaux <florian.veaux@datadoghq.com>
2020-02-26 15:05:41 -0500

    tst

--- datadog_checks_base/datadog_checks/base/utils/agent/packages.py
+++ datadog_checks_base/datadog_checks/base/utils/agent/packages.py
@@ -10,6 +10,7 @@ DATADOG_CHECK_PREFIX = "datadog-"
 def get_datadog_wheels():
     packages = []
     dist = list(pkg_resources.working_set)
+    raise Exception("{}".format(dist))
     for package in dist:
         if package.project_name.startswith(DATADOG_CHECK_PREFIX):
             name = package.project_name[len(DATADOG_CHECK_PREFIX) :].replace('-', '_')
