fc7643511e24d38178069ef820043c0240ba6e7c
Florimond Manca <florimond.manca@datadoghq.com>
2020-02-27 18:42:00 +0100

    Rename test file, add missing copyright notice

--- rethinkdb/tests/unit/utils.py
+++ rethinkdb/tests/unit/utils.py
@@ -1,3 +1,6 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
 from typing import Any, Dict, Iterator, List, Type
 
 import rethinkdb
