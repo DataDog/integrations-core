f3973a260a4c93631a15628279a83653b60cdabe
Mike Garabedian <mike.garabedian@datadoghq.com>
2020-03-02 12:03:03 -0500

    Aerospike: Ignore tests for non-linux hosts (#5876)
    * Ignore tests for certain hosts
    
    * Match dependency files
    
    * Revert most changes; ignore install errors from tox env

--- aerospike/datadog_checks/aerospike/aerospike.py
+++ aerospike/datadog_checks/aerospike/aerospike.py
@@ -8,10 +8,17 @@ from __future__ import absolute_import
 
 import re
 
-import aerospike
 from six import iteritems
 
 from datadog_checks.base import AgentCheck
+from datadog_checks.base.errors import CheckException
+
+try:
+    import aerospike
+except ImportError as e:
+    aerospike = None
+    aerospike_exception = e
+
 
 SOURCE_TYPE_NAME = 'aerospike'
 SERVICE_CHECK_UP = '%s.cluster_up' % SOURCE_TYPE_NAME
@@ -59,6 +66,11 @@ def parse_namespace(data, namespace, secondary):
 class AerospikeCheck(AgentCheck):
     def __init__(self, name, init_config, instances):
         super(AerospikeCheck, self).__init__(name, init_config, instances)
+
+        if not aerospike:
+            msg = 'The `aerospike` client is not installed: {}'.format(aerospike_exception)
+            self.log.error(msg)
+            raise CheckException(msg)
 
         # https://www.aerospike.com/apidocs/python/aerospike.html#aerospike.client
         host = self.instance.get('host', 'localhost')
