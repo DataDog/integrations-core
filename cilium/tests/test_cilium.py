1d260b498b340154aaca619840bf70ba2b600904
Christine Chen <ChristineTChen@users.noreply.github.com>
2020-03-02 12:50:14 -0500

    Add Cilium version metadata (#5408)
    * Add Cilium version metadata
    
    * Fix style
    
    * Update to latest cilium version
    
    * Use fixture

--- cilium/tests/test_cilium.py
+++ cilium/tests/test_cilium.py
@@ -3,7 +3,7 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from datadog_checks.cilium import CiliumCheck
 
-from .common import ADDL_AGENT_METRICS, AGENT_DEFAULT_METRICS, OPERATOR_AWS_METRICS, OPERATOR_METRICS
+from .common import ADDL_AGENT_METRICS, AGENT_DEFAULT_METRICS, CILIUM_VERSION, OPERATOR_AWS_METRICS, OPERATOR_METRICS
 
 
 def test_agent_check(aggregator, agent_instance, mock_agent_data):
@@ -22,3 +22,20 @@ def test_operator_check(aggregator, operator_instance, mock_operator_data):
     for m in OPERATOR_METRICS + OPERATOR_AWS_METRICS:
         aggregator.assert_metric(m)
     aggregator.assert_all_metrics_covered()
+
+
+def test_version_metadata(datadog_agent, agent_instance, mock_agent_data):
+    check = CiliumCheck('cilium', {}, [agent_instance])
+    check.check_id = 'test:123'
+    check.check(agent_instance)
+
+    major, minor, patch = CILIUM_VERSION.split('.')
+    version_metadata = {
+        'version.scheme': 'semver',
+        'version.major': major,
+        'version.minor': minor,
+        'version.patch': patch,
+        'version.raw': CILIUM_VERSION,
+    }
+
+    datadog_agent.assert_metadata('test:123', version_metadata)
