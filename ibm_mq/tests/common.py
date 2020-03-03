dc3ec314ef9b8771b6c1fe32efa257cd134d3e6b
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-03-03 13:21:38 +0100

    Style fix

--- ibm_mq/tests/common.py
+++ ibm_mq/tests/common.py
@@ -91,7 +91,7 @@ E2E_METADATA = {
         'mkdir /opt/mqm',
         'curl -L -o /opt/mqm/mq-client.tar.gz '
         # TODO: change to S3
-        'https://www.dropbox.com/s/9wmhpwq8blb1na3/9.1.0.4-IBM-MQC-Redist-LinuxX64.tar.gz\?dl\=0',
+        'https://www.dropbox.com/s/9wmhpwq8blb1na3/9.1.0.4-IBM-MQC-Redist-LinuxX64.tar.gz\?dl\=0',  # noqa: W605
         'tar -C /opt/mqm -xf /opt/mqm/mq-client.tar.gz',
     ],
     'env_vars': {'LD_LIBRARY_PATH': '/opt/mqm/lib64:/opt/mqm/lib', 'C_INCLUDE_PATH': '/opt/mqm/inc'},
