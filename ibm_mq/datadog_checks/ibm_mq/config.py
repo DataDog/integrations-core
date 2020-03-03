2567c3b19f9e602e5fdb4d7d33b6f21cf67552f5
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-03-03 14:18:52 +0100

    Change ConfigurationError import

--- ibm_mq/datadog_checks/ibm_mq/config.py
+++ ibm_mq/datadog_checks/ibm_mq/config.py
@@ -7,10 +7,9 @@ import re
 
 from six import iteritems
 
-from datadog_checks.base import AgentCheck
+from datadog_checks.base import AgentCheck, ConfigurationError
 from datadog_checks.base.constants import ServiceCheck
 from datadog_checks.config import is_affirmative
-from datadog_checks.errors import ConfigurationError
 
 try:
     import pymqi
