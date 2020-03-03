13c9c444b5bfec1af41971e977b783093d3d3fb5
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-03-03 14:07:39 +0100

    Move all tarbals to azure

--- ibm_mq/tests/common.py
+++ ibm_mq/tests/common.py
@@ -85,13 +85,14 @@ INSTANCE_QUEUE_REGEX_TAG = {
 }
 
 E2E_METADATA = {
+    # This script makes the necessary setup to be able to compile pymqi on the agent machine
     'start_commands': [
         'apt-get update',
-        'apt-get install gcc -y',  # Needed when bumping pymqi to be able to build it on the E2E
+        'apt-get install gcc -y',
         'mkdir /opt/mqm',
         'curl -L -o /opt/mqm/mq-client.tar.gz '
         # TODO: change to S3
-        'https://www.dropbox.com/s/9wmhpwq8blb1na3/9.1.0.4-IBM-MQC-Redist-LinuxX64.tar.gz\?dl\=0',  # noqa: W605
+        'https://ddintegrations.blob.core.windows.net/ibm-mq/9.1.0.4-IBM-MQC-Redist-LinuxX64.tar.gz',
         'tar -C /opt/mqm -xf /opt/mqm/mq-client.tar.gz',
     ],
     'env_vars': {'LD_LIBRARY_PATH': '/opt/mqm/lib64:/opt/mqm/lib', 'C_INCLUDE_PATH': '/opt/mqm/inc'},
