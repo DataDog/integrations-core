8188d09c408ff027857ca11c970a38a92cd9bf15
KeriPMiller <miller.keripatricia@gmail.com>
2020-03-03 11:42:38 -0500

    cleaning up code for PR

--- kafka/tests/conftest.py
+++ kafka/tests/conftest.py
@@ -8,20 +8,10 @@ import pytest
 from datadog_checks.dev import docker_run
 from datadog_checks.dev.utils import load_jmx_config
 
-from .common import HERE, HOST_IP
+from .common import HERE
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    """
-    Start a kafka cluster and wait for it to be up and running.
-    """
-    with docker_run(
-        os.path.join(HERE, 'compose', 'docker-compose.yml'),
-        env_vars={
-            # Advertising the hostname doesn't work on docker:dind so we manually
-            # resolve the IP address. This seems to also work outside docker:dind
-            'KAFKA_HOST': HOST_IP
-        },
-    ):
+    with docker_run(os.path.join(HERE, 'compose', 'docker-compose.yml')):
         yield load_jmx_config(), {'use_jmx': True}
