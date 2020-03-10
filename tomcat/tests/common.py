5fe363d9eaddae157937ad8bb3c64a46e74fa0b7
KeriPMiller <miller.keripatricia@gmail.com>
2020-03-10 14:01:48 -0400

    added comment of untestable metrics to add when supported

--- tomcat/tests/common.py
+++ tomcat/tests/common.py
@@ -1,20 +1,31 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
-import socket
-
-from datadog_checks.dev import get_docker_hostname, get_here
+from datadog_checks.dev import get_here
 
 CHECK_NAME = "tomcat"
 
 HERE = get_here()
-HOST = get_docker_hostname()
-HOST_IP = socket.gethostbyname(HOST)
-
 
 """
-Rate type metrics that do not work in our e2e:
+These metrics cannot be added to E2E yet because they are rate and count types.
+They are visiable in the console but not to this test.
+
+Metrics To add to e2e test:
+    "tomcat.bytes_sent",
+    "tomcat.bytes_rcvd",
+    "tomcat.error_count",
+    "tomcat.request_count",
+    "tomcat.processing_time",
+    "tomcat.servlet.processing_time",
+    "tomcat.servlet.error_count",
+    "tomcat.servlet.request_count",
+    "tomcat.cache.access_count",
+    "tomcat.cache.hits_count",
+    "tomcat.jsp.count",
+    "tomcat.jsp.reload_count",
 """
+
 
 TOMCAT_E2E_METRICS = [
     # Tomcat
