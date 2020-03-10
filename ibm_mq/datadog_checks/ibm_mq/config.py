ccfaa2de3253e3e2f1155a8dd28f80eec408bb60
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-03-10 13:23:11 +0100

    Simplify tag calculation

--- ibm_mq/datadog_checks/ibm_mq/config.py
+++ ibm_mq/datadog_checks/ibm_mq/config.py
@@ -89,17 +89,12 @@ class IBMMQConfig:
         )  # type: Dict[str, str]
 
         custom_tags = instance.get('tags', [])  # type: List[str]
-        self.tags = [
-            "queue_manager:{}".format(self.queue_manager_name),
-            "mq_host:{}".format(self.host),  # 'host' is reserved and 'mq_host' is used instead
-            "port:{}".format(self.port),
-            "channel:{}".format(self.channel),
-        ] + custom_tags  # type: List[str]
         self.tags_no_channel = [
             "queue_manager:{}".format(self.queue_manager_name),
             "mq_host:{}".format(self.host),  # 'host' is reserved and 'mq_host' is used instead
             "port:{}".format(self.port),
         ] + custom_tags  # type: List[str]
+        self.tags = self.tags_no_channel + ["channel:{}".format(self.channel)]  # type: List[str]
 
         self.ssl = is_affirmative(instance.get('ssl_auth', False))  # type: bool
         self.ssl_cipher_spec = instance.get('ssl_cipher_spec', 'TLS_RSA_WITH_AES_256_CBC_SHA')  # type: str
