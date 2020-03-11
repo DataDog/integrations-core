31b6d5baff4a9649e8ba2ce92b7545e57cbe4eaa
Florimond Manca <florimond.manca@datadoghq.com>
2020-03-11 17:48:50 +0100

    Add types to compat module

--- snmp/datadog_checks/snmp/compat.py
+++ snmp/datadog_checks/snmp/compat.py
@@ -1,9 +1,14 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+from typing import Any
+
 try:
     from datadog_checks.base.utils.common import total_time_to_temporal_percent
 except ImportError:
 
     # Provide fallback for agent < 6.16
-    def total_time_to_temporal_percent(total_time, scale=1000):
+    def total_time_to_temporal_percent(total_time, scale=1000):  # type: ignore
         return total_time / scale * 100
 
 
@@ -12,10 +17,13 @@ try:
 except ImportError:
 
     def get_config(value):
+        # type: (Any) -> str
         return ''
 
     def write_persistent_cache(value, key):
+        # type: (Any, Any) -> None
         pass
 
     def read_persistent_cache(value):
+        # type: (Any) -> str
         return ''
