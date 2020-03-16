ee96f5419fa92fa35ef1eb48698aeec9b1b2ee3e
sarah-witt <sarah.witt@COMP11958.local>
2020-03-16 13:39:24 -0400

    Use for all checks and add to pipeline

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/codeowners.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/codeowners.py
@@ -3,26 +3,48 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import json
 import os
+import re
 
 import click
 
 from ....utils import file_exists, read_file, write_file
 from ...constants import get_root
-from ...utils import get_valid_integrations, load_manifest, get_codeowners, get_valid_checks
-from ..console import CONTEXT_SETTINGS, abort, echo_failure, echo_info, echo_success
+from ...utils import get_codeowners, get_valid_checks, get_valid_integrations, load_manifest
+from ..console import CONTEXT_SETTINGS, echo_failure, echo_success
+
+DIRECTORY_REGEX = re.compile(r"\/(.*)\/$")
 
 
-
-@click.command('codeowners', context_settings=CONTEXT_SETTINGS, short_help='Validate `CODEOWNERS` file has an entry for each integration')
-def codeowners():
-    """Validate all `CODEOWNERS` file."""
-
-    all_checks = get_valid_checks()
-
+def get_all_checks_with_codeowners():
     codeowners = get_codeowners()
 
-    for i in codeowners:
-        print("hi" + i)
-    #for check in all_checks:
+    checks_with_codeowners = set()
+
+    for entry in codeowners:
+        parts = [part for part in entry.split(" ") if part != ""]
+        match = DIRECTORY_REGEX.match(parts[0])
+        if match and match.groups:
+            check = match.group(1)
+            checks_with_codeowners.add(check)
+
+    return checks_with_codeowners
 
 
+@click.command(
+    'codeowners',
+    context_settings=CONTEXT_SETTINGS,
+    short_help='Validate `CODEOWNERS` file has an entry for each integration',
+)
+def codeowners():
+    """Validate all `CODEOWNERS` file."""
+    all_checks_with_codeowners = get_all_checks_with_codeowners()
+    all_checks = get_valid_integrations()
+    has_failed = False
+
+    for check in all_checks:
+        if check not in all_checks_with_codeowners:
+            has_failed = True
+            echo_failure(f"Integration {check} does not have a valid `CODEOWNERS` entry.")
+
+    if not has_failed:
+        echo_success("All integrations have codeowners.")
