3d7d49abefedf53294fb5ac4e949548d634634d3
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-03-17 17:48:55 +0100

    Remove ignore

--- vsphere/datadog_checks/vsphere/vsphere.py
+++ vsphere/datadog_checks/vsphere/vsphere.py
@@ -186,7 +186,7 @@ class VSphereCheck(AgentCheck):
                 # Hosts are not considered as parents of the VMs they run, we use the `runtime.host` property
                 # to get the name of the ESXi host
                 runtime_host = properties.get("runtime.host")
-                runtime_host_props = infrastructure_data.get(runtime_host, {})  # type: ignore
+                runtime_host_props = infrastructure_data[runtime_host] if runtime_host else {}
                 runtime_hostname = to_native_string(runtime_host_props.get("name", "unknown"))
                 tags.append('vsphere_host:{}'.format(runtime_hostname))
 
