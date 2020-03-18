c889ac9bd197a9b0e2db3139b78d51c441092ee6
Sarah Witt <sarah.witt@datadoghq.com>
2020-03-18 13:28:59 -0400

    Change check to integration

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/codeowners.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/codeowners.py
@@ -11,24 +11,27 @@ from ..console import CONTEXT_SETTINGS, echo_failure, echo_success
 DIRECTORY_REGEX = re.compile(r"\/(.*)\/$")
 
 
-def get_all_checks_with_codeowners():
+def get_all_integrations_with_codeowners():
+    """ Returns a list of all integrations that have a codeowner"""
     codeowners = get_codeowners()
 
-    checks_with_codeowners = set()
-    checks_with_only_entries = set()
+    integrations_with_codeowners = set()
+    integrations_with_only_entries = set()
 
+    # each valid entry looks something like:
+    # /containerd/                              @DataDog/container-integrations @DataDog/agent-integrations
     for entry in codeowners:
         parts = [part for part in entry.split(" ") if part != "" and part != "\t"]
         if not parts:
             continue
         match = DIRECTORY_REGEX.match(parts[0])
         if match and match.group(1):
-            check = match.group(1)
+            integration = match.group(1)
             if len(parts) != 2:
-                checks_with_only_entries.add(check)
+                integrations_with_only_entries.add(integration)
             else:
-                checks_with_codeowners.add(check)
-    return checks_with_codeowners, checks_with_only_entries
+                integrations_with_codeowners.add(integration)
+    return integrations_with_codeowners, integrations_with_only_entries
 
 
 @click.command(
@@ -38,17 +41,17 @@ def get_all_checks_with_codeowners():
 )
 def codeowners():
     """Validate that every integration has an entry in the `CODEOWNERS` file."""
-    all_checks_with_codeowners, checks_with_only_entries = get_all_checks_with_codeowners()
-    all_checks = get_valid_integrations()
+    all_integrations_with_codeowners, integrations_with_only_entries = get_all_integrations_with_codeowners()
+    all_integrations = get_valid_integrations()
     has_failed = False
 
-    for check in all_checks:
-        if check in checks_with_only_entries:
+    for integration in all_integrations:
+        if integration in integrations_with_only_entries:
             has_failed = True
-            echo_failure(f"Integration {check} has a `CODEOWNERS` entry, but the codeowner is empty.")
-        elif check not in all_checks_with_codeowners:
+            echo_failure(f"Integration {integration} has a `CODEOWNERS` entry, but the codeowner is empty.")
+        elif integration not in all_integrations_with_codeowners:
             has_failed = True
-            echo_failure(f"Integration {check} does not have a valid `CODEOWNERS` entry.")
+            echo_failure(f"Integration {integration} does not have a valid `CODEOWNERS` entry.")
 
     if not has_failed:
         echo_success("All integrations have codeowners.")
