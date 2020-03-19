ac47ebb13f7d67ef2b0d92f27dfb8cbb259067d5
Sarah Witt <witt.s@husky.neu.edu>
2020-03-19 17:46:17 -0400

    Add script to resolve username from email (#6099)
    * Add script to resolve username from email
    
    * Fix styling
    
    * Apply fixes
    
    * remove comma

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/scripts/__init__.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/scripts/__init__.py
@@ -4,11 +4,12 @@
 import click
 
 from ...console import CONTEXT_SETTINGS
+from .github_user import email2ghuser
 from .metrics2md import metrics2md
 from .remove_labels import remove_labels
 from .upgrade_python import upgrade_python
 
-ALL_COMMANDS = (metrics2md, remove_labels, upgrade_python)
+ALL_COMMANDS = (email2ghuser, metrics2md, remove_labels, upgrade_python)
 
 
 @click.group(context_settings=CONTEXT_SETTINGS, short_help='Miscellaneous scripts that may be useful')
