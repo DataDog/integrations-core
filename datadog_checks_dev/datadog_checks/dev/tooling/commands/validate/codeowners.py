d3c80991eeeb43220a42f465cfd4869b1f0d55a1
Sarah Witt <sarah.witt@datadoghq.com>
2020-03-20 15:20:33 -0400

    Update script to check for emails

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/codeowners.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/codeowners.py
@@ -11,7 +11,7 @@ from ..console import CONTEXT_SETTINGS, echo_failure, echo_success
 DIRECTORY_REGEX = re.compile(r"\/(.*)\/$")
 
 
-IGNORE_EXTRAS_TILES = {
+IGNORE_TILES = {
     'bluematador',
     'bonsai',
     'buddy',
@@ -26,65 +26,45 @@ IGNORE_EXTRAS_TILES = {
 }
 
 
-def get_all_integrations_with_codeowners():
-    """ Returns a list of all integrations that have a codeowner"""
+def create_codeowners_map():
+    """Creates a mapping of integrations to codeowners entries"""
     codeowners = get_codeowners()
-
-    integrations_with_codeowners = set()
-    integrations_with_only_entries = set()
-    integrations_with_email_only = set()
+    all_integrations = sorted(get_valid_integrations())
+    codeowner_map = dict.fromkeys(all_integrations)
 
     # each valid entry looks something like:
     # /containerd/                              @DataDog/container-integrations @DataDog/agent-integrations
     for entry in codeowners:
         parts = [part for part in entry.split(" ") if part != "" and part != "\t"]
-        if not parts:
-            continue
         match = DIRECTORY_REGEX.match(parts[0])
         if match and match.group(1):
-            integration = match.group(1)
-            print(integration, parts[0])
             if len(parts) < 2:
-                integrations_with_only_entries.add(integration)
+                codeowner_map[match.group(1)] = "empty"
             else:
-                owner = parts[1]
-                if not owner.startswith("@") or integration in IGNORE_EXTRAS_TILES:
-                    integrations_with_email_only.add(integration)
-                else:
-                    integrations_with_codeowners.add(integration)
+                codeowner_map[match.group(1)] = parts[1]
 
-    return integrations_with_codeowners, integrations_with_email_only, integrations_with_only_entries
+    return codeowner_map
 
 
 @click.command(
     context_settings=CONTEXT_SETTINGS, short_help='Validate `CODEOWNERS` file has an entry for each integration'
 )
-@click.pass_context
-def codeowners(ctx):
+def codeowners():
     """Validate that every integration has an entry in the `CODEOWNERS` file."""
-    repo_choice = ctx.obj['repo_choice']
-    if repo_choice == 'extras':
-        IGNORE_EXTRAS_TILES
 
-    (
-        all_integrations_with_codeowners,
-        integrations_with_email_only,
-        integrations_with_only_entries,
-    ) = get_all_integrations_with_codeowners()
-
-    all_integrations = sorted(get_valid_integrations())
     has_failed = False
+    codeowner_map = create_codeowners_map()
 
-    for integration in all_integrations:
-        if integration in integrations_with_only_entries:
-            has_failed = True
-            echo_failure(f"Integration {integration} has a `CODEOWNERS` entry, but the codeowner is empty.")
-        elif integration in integrations_with_email_only:
-            has_failed = True
-            echo_failure(f"Integration {integration} has a `CODEOWNERS` entry, but the codeowner is not a username or team.")
-        elif integration not in all_integrations_with_codeowners:
+    for integration, codeowner in codeowner_map.items():
+        if not codeowner:
             has_failed = True
             echo_failure(f"Integration {integration} does not have a valid `CODEOWNERS` entry.")
+        elif codeowner == "empty":
+            has_failed = True
+            echo_failure(f"Integration {integration} has a `CODEOWNERS` entry, but the codeowner is empty.")
+        elif not codeowner.startswith("@") and integration not in IGNORE_TILES:
+            has_failed = True
+            echo_failure(f"Integration {integration} has a `CODEOWNERS` entry, but the codeowner is not a username or team.")
 
     if not has_failed:
         echo_success("All integrations have codeowners.")
