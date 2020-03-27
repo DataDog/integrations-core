491257bd19c76893fc59b2d0c4b93a7c62f10780
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-03-27 17:51:06 +0100

    Add comments

--- datadog_checks_base/datadog_checks/stubs/aggregator.py
+++ datadog_checks_base/datadog_checks/stubs/aggregator.py
@@ -2,4 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 # flake8: noqa
+
+# Module moved to datadog_checks_dev, this import is for backward compatibility
+
 from ..base.stubs.aggregator import *
