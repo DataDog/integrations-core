2900b34ab677d973abf6778cea6498f4002c01fc
Thomas Hervé <thomas.herve@datadoghq.com>
2020-03-30 11:27:13 +0200

    Use a condition

--- ignite/tests/conftest.py
+++ ignite/tests/conftest.py
@@ -7,6 +7,7 @@ import os
 import pytest
 
 from datadog_checks.dev import docker_run, get_docker_hostname, get_here, run_command
+from datadog_checks.dev.conditions import WaitFor
 from datadog_checks.dev.utils import load_jmx_config
 
 E2E_METADATA = {
@@ -16,11 +17,18 @@ E2E_METADATA = {
 
 @pytest.fixture(scope="session")
 def dd_environment():
-    with docker_run(os.path.join(get_here(), 'compose', 'docker-compose.yml'), log_patterns="Ignite node started OK"):
-        result = run_command("docker exec dd-ignite /opt/ignite/apache-ignite/bin/control.sh --activate", capture=True)
-        if result.stderr:
-            raise Exception(result.stderr)
+    with docker_run(
+        os.path.join(get_here(), 'compose', 'docker-compose.yml'),
+        conditions=[WaitFor(setup_ignite)],
+        log_patterns="Ignite node started OK",
+    ):
         instance = load_jmx_config()
         instance['instances'][0]['port'] = 49112
         instance['instances'][0]['host'] = get_docker_hostname()
         yield instance, E2E_METADATA
+
+
+def setup_ignite():
+    result = run_command("docker exec dd-ignite /opt/ignite/apache-ignite/bin/control.sh --activate", capture=True)
+    if result.stderr:
+        raise Exception(result.stderr)
