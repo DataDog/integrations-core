a93230ea865d062cd931ea1dca203cb75c5ecb81
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-03-30 11:00:25 +0200

    Blacklist mcp_source.request_acks_total

--- istio/tests/common.py
+++ istio/tests/common.py
@@ -242,7 +242,6 @@ GALLEY_METRICS = [
     'istio.galley.process.virtual_memory_bytes',
     'istio.galley.endpoint_no_pod',
     'istio.galley.mcp_source.clients_total',
-    'istio.galley.mcp_source.request_acks_total',
     'istio.galley.runtime_processor.event_span_duration_milliseconds.count',
     'istio.galley.runtime_processor.event_span_duration_milliseconds.sum',
     'istio.galley.runtime_processor.events_processed_total',
