cd6144122f40c8535a4df00989e75cd9153cd204
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-01 19:04:32 +0200

    refactor default returned metrics

--- confluent_platform/tests/common.py
+++ confluent_platform/tests/common.py
@@ -7,14 +7,16 @@ from datadog_checks.dev import get_docker_hostname, get_here, load_jmx_config
 HERE = get_here()
 HOST = get_docker_hostname()
 
+DEFAULT_MAX_RETURNED_METRICS = 2000
+
 INSTANCES = [
-    {'host': 'localhost', 'port': '31000', 'max_returned_metrics': 2000},  # confluentinc/cp-zookeeper
-    {'host': 'localhost', 'port': '31001', 'max_returned_metrics': 2000},  # confluentinc/cp-server
-    {'host': 'localhost', 'port': '31002', 'max_returned_metrics': 2000},  # cnfldemos/cp-server-connect-datagen
-    {'host': 'localhost', 'port': '31004', 'max_returned_metrics': 2000},  # confluentinc/cp-ksql-server
-    {'host': 'localhost', 'port': '31006', 'max_returned_metrics': 2000},  # confluentinc/cp-kafka-rest
-    {'host': 'localhost', 'port': '31007', 'max_returned_metrics': 2000},  # confluentinc/cp-schema-registry
-    {'host': 'localhost', 'port': '31008', 'max_returned_metrics': 2000},  # confluentinc/cp-enterprise-replicator
+    {'host': 'localhost', 'port': '31000', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-zookeeper
+    {'host': 'localhost', 'port': '31001', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-server
+    {'host': 'localhost', 'port': '31002', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # cnfldemos/cp-server-connect-datagen
+    {'host': 'localhost', 'port': '31004', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-ksql-server
+    {'host': 'localhost', 'port': '31006', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-kafka-rest
+    {'host': 'localhost', 'port': '31007', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-schema-registry
+    {'host': 'localhost', 'port': '31008', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-enterprise-replicator
 ]
 
 CHECK_CONFIG = load_jmx_config()
