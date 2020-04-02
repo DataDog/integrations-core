52d509d8e1ee4050e0b2cc813d1a553170b71d6d
Florimond Manca <florimond.manca@gmail.com>
2020-04-02 14:14:57 +0200

    Trigger CI if contents of tests/ changes (#6223)

--- datadog_checks_dev/datadog_checks/dev/tooling/constants.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/constants.py
@@ -82,7 +82,7 @@ NOT_TILES = [
 
 # If a file changes in a PR with any of these file extensions,
 # a test will run against the check containing the file
-TESTABLE_FILE_EXTENSIONS = ('.py', '.ini', '.in', '.txt', '.yml', '.yaml')
+TESTABLE_FILE_PATTERNS = ('*.py', '*.ini', '*.in', '*.txt', '*.yml', '*.yaml', '**/tests/*')
 NON_TESTABLE_FILES = ('auto_conf.yaml', 'metrics.yaml', 'agent_requirements.in')
 
 REQUIREMENTS_IN = 'requirements.in'
