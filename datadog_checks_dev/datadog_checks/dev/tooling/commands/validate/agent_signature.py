7c8c427ef0792c3934b3f63034bc58e9f6ab4d05
Sarah Witt <sarah.witt@datadoghq.com>
2020-04-03 09:38:27 -0400

    Update messages

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/agent_signature.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/agent_signature.py
@@ -28,8 +28,11 @@ def legacy_signature(check):
         if check_failed:
             has_failed = True
             failed_file, failed_num = check_failed
-            echo_failure(f'Check {check} uses legacy agent signature in {failed_file} on line {failed_num}')
+            echo_failure(f"Check `{check}` uses legacy agent signature in `{failed_file}` on line {failed_num}")
 
     if not has_failed:
-        echo_success(f'All checks use the new agent signature.')
+        if check:
+            echo_success(f"Check `{check}` uses the new agent signature.")
+        else:
+            echo_success(f'All checks use the new agent signature.')
     return
