ca34b27785d365689ef7bb46285ee7c27f3f8072
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-03 14:26:30 +0200

    Fix style

--- confluent_platform/tests/common.py
+++ confluent_platform/tests/common.py
@@ -10,13 +10,41 @@ HOST = get_docker_hostname()
 DEFAULT_MAX_RETURNED_METRICS = 2000
 
 INSTANCES = [
-    {'host': 'localhost', 'port': '31000', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-zookeeper
-    {'host': 'localhost', 'port': '31001', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-server
-    {'host': 'localhost', 'port': '31002', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # cnfldemos/cp-server-connect-datagen
-    {'host': 'localhost', 'port': '31004', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-ksql-server
-    {'host': 'localhost', 'port': '31006', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-kafka-rest
-    {'host': 'localhost', 'port': '31007', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-schema-registry
-    {'host': 'localhost', 'port': '31008', 'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS},  # confluentinc/cp-enterprise-replicator
+    {
+        'host': 'localhost',
+        'port': '31000',
+        'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS,
+    },  # confluentinc/cp-zookeeper
+    {
+        'host': 'localhost',
+        'port': '31001',
+        'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS,
+    },  # confluentinc/cp-server
+    {
+        'host': 'localhost',
+        'port': '31002',
+        'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS,
+    },  # cnfldemos/cp-server-connect-datagen
+    {
+        'host': 'localhost',
+        'port': '31004',
+        'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS,
+    },  # confluentinc/cp-ksql-server
+    {
+        'host': 'localhost',
+        'port': '31006',
+        'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS,
+    },  # confluentinc/cp-kafka-rest
+    {
+        'host': 'localhost',
+        'port': '31007',
+        'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS,
+    },  # confluentinc/cp-schema-registry
+    {
+        'host': 'localhost',
+        'port': '31008',
+        'max_returned_metrics': DEFAULT_MAX_RETURNED_METRICS,
+    },  # confluentinc/cp-enterprise-replicator
 ]
 
 CHECK_CONFIG = load_jmx_config()
