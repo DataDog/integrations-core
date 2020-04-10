0a0c1da7de388be65203b80c55c5233102fff25e
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-10 15:50:29 +0200

    Update datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/config.py
    Co-Authored-By: Florimond Manca <florimond.manca@gmail.com>

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/config.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/config.py
@@ -113,7 +113,7 @@ def config(ctx, check, sync, verbose):
         if check_display_queue or verbose:
             echo_info(f'{check}:')
             if verbose:
-                check_display_queue.append(lambda **kwargs: echo_info(f'Valid spec', **kwargs))
+                check_display_queue.append(lambda **kwargs: echo_info('Valid spec', **kwargs))
             for display in check_display_queue:
                 display(indent=True)
 
