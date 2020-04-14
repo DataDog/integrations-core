fa02bfb650ad5b48a0b63327b9fb509d86d6d4a7
ChristineTChen <christine.chen@datadoghq.com>
2020-04-14 17:32:14 -0400

    Move constants

---
+++ istio/datadog_checks/istio/constants.py
@@ -0,0 +1,13 @@
+# (C) Datadog, Inc. 2020 - Present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
+# Istio namespaces
+MIXER_NAMESPACE = 'istio.mixer'
+MESH_NAMESPACE = 'istio.mesh'
+PILOT_NAMESPACE = 'istio.pilot'
+GALLEY_NAMESPACE = 'istio.galley'
+CITADEL_NAMESPACE = 'istio.citadel'
+ISTIOD_NAMESPACE = 'istio.istiod'
+
+DEFAULT_METRIC_LIMIT = 0
