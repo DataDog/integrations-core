50c172e4ab493aeea646cdfa74b33757179f3fe5
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-16 10:37:35 +0200

    Test CI Pipeline release manual

--- .gitlab/tagger/build-packages.sh
+++ .gitlab/tagger/build-packages.sh
@@ -3,9 +3,13 @@
 set -euxo pipefail
 IFS=$'\n\t'
 
+echo "Start build-packages.sh"
+
 if [[ -n "$DD_TEST_CMD" ]]; then
     echo "Triggering build pipeline..."
-    eval "$DD_TEST_CMD"
+    echo "$DD_TEST_CMD"
 else
     echo "Skipping, DD_TEST_CMD is not set"
 fi
+
+echo "End build-packages.sh"
