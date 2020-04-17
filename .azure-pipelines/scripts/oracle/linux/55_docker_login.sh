b77f4197437cb6d4ef2d514a559947f662b02d57
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-17 16:25:22 +0200

    revert .azure-pipelines/scripts/oracle/linux/55_docker_login.sh

--- .azure-pipelines/scripts/oracle/linux/55_docker_login.sh
+++ .azure-pipelines/scripts/oracle/linux/55_docker_login.sh
@@ -3,12 +3,6 @@ set -euo pipefail
 
 set +x
 
-n=0
-until [ $n -ge 3 ]
-do
-   echo "$ORACLE_DOCKER_PASSWORD" | docker login container-registry.oracle.com --username "$ORACLE_DOCKER_USERNAME" --password-stdin && break
-   n=$((n+1))
-   sleep 5
-done
+echo "$ORACLE_DOCKER_PASSWORD" | docker login container-registry.oracle.com --username "$ORACLE_DOCKER_USERNAME" --password-stdin
 
 set -x
