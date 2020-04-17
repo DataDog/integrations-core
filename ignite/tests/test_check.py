c51a229285749566fd6a13e5fbbf1eb26eac317c
Thomas Hervé <thomas.herve@datadoghq.com>
2020-04-17 09:50:32 +0200

    Add metadata assert

--- ignite/tests/test_check.py
+++ ignite/tests/test_check.py
@@ -4,6 +4,8 @@
 
 import pytest
 
+from datadog_checks.dev.utils import get_metadata_metrics
+
 from .common import COUNTS, GAUGES
 
 
@@ -19,5 +21,8 @@ def test_e2e(dd_agent_check):
         aggregator.assert_metric(metric, metric_type=aggregator.MONOTONIC_COUNT)
 
     aggregator.assert_all_metrics_covered()
+
+    jvm_metrics = [metric for metric in GAUGES if metric.startswith('jvm')]
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_metric_type=False, exclude=jvm_metrics)
 
     aggregator.assert_service_check("ignite.can_connect")
