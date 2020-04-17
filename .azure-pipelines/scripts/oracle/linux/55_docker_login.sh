5c0d1034b9983c9df5d345d16bebf3ecee5285ad
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-17 16:22:40 +0200

    Fix flacky docker login

--- .azure-pipelines/scripts/oracle/linux/55_docker_login.sh
+++ .azure-pipelines/scripts/oracle/linux/55_docker_login.sh
@@ -3,6 +3,12 @@ set -euo pipefail
 
 set +x
 
-echo "$ORACLE_DOCKER_PASSWORD" | docker login container-registry.oracle.com --username "$ORACLE_DOCKER_USERNAME" --password-stdin
+n=0
+until [ $n -ge 3 ]
+do
+   echo "$ORACLE_DOCKER_PASSWORD" | docker login container-registry.oracle.com --username "$ORACLE_DOCKER_USERNAME" --password-stdin && break
+   n=$((n+1))
+   sleep 5
+done
 
 set -x
