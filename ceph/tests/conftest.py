cb3fe19d04cf082825817ca80b55d8ea61619925
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-04-17 13:35:47 +0200

    flaky ceph

--- ceph/tests/conftest.py
+++ ceph/tests/conftest.py
@@ -26,16 +26,18 @@ E2E_METADATA = {
 def dd_environment():
     compose_file = os.path.join(HERE, 'compose', 'docker-compose.yaml')
     # We need a custom condition to wait a bit longer
-    condition = CheckDockerLogs(compose_file, 'spawning ceph --cluster ceph -w', wait=5)
     with docker_run(
         compose_file=compose_file,
-        conditions=[condition],
-        sleep=5,  # Let's wait just a little bit after ceph got spawned to remove flakyness
+        conditions=[
+            CheckDockerLogs(compose_file, 'spawning ceph --cluster ceph -w', wait=5),
+            CheckDockerLogs(compose_file, 'Running on http://0.0.0.0:5000/')
+        ],
     ):
         # Clean the disk space warning
         run_command(
             ['docker', 'exec', 'dd-test-ceph', 'ceph', 'tell', 'mon.*', 'injectargs', '--mon_data_avail_warn', '5']
         )
         # Wait a bit for the change to take effect
-        time.sleep(5)
+        condition = CheckDockerLogs(compose_file, 'Cluster is now healthy')
+        condition()
         yield BASIC_CONFIG, E2E_METADATA
