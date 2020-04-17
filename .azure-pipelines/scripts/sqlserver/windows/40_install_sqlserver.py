f30e605116088baf45d56d968c7dedb73b2bab52
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-17 17:52:29 +0200

    Fix flaky CI by retrying sql-server install

---
+++ .azure-pipelines/scripts/sqlserver/windows/40_install_sqlserver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import subprocess
+
+from retrying import retry
+
+
+@retry(stop_max_attempt_number=5, wait_exponential_multiplier=1000)
+def install_sqlserver():
+    """
+    Install with TCP/IP enabled, see: https://chocolatey.org/packages/sql-server-2017
+    """
+    subprocess.run(["choco", "install", "sql-server-2017", "--params", "'/TCPENABLED:1'"], check=True)
+
+
+install_sqlserver()
