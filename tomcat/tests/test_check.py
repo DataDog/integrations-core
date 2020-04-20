445473004e70588786c5f49ce97d04283fd6a4f2
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-20 12:24:43 +0200

    Assert metrics

--- tomcat/tests/test_check.py
+++ tomcat/tests/test_check.py
@@ -6,6 +6,51 @@ import pytest
 
 from .common import TOMCAT_E2E_METRICS
 
+JVM_METRICS = [
+    'jvm.buffer_pool.direct.capacity',
+    'jvm.buffer_pool.direct.count',
+    'jvm.buffer_pool.direct.used',
+    'jvm.buffer_pool.mapped.capacity',
+    'jvm.buffer_pool.mapped.count',
+    'jvm.buffer_pool.mapped.used',
+    'jvm.cpu_load.process',
+    'jvm.cpu_load.system',
+    'jvm.gc.cms.count',
+    'jvm.gc.eden_size',
+    'jvm.gc.old_gen_size',
+    'jvm.gc.parnew.time',
+    'jvm.gc.survivor_size',
+    'jvm.heap_memory_committed',
+    'jvm.heap_memory_init',
+    'jvm.heap_memory_max',
+    'jvm.heap_memory',
+    'jvm.loaded_classes',
+    'jvm.non_heap_memory_committed',
+    'jvm.non_heap_memory_init',
+    'jvm.non_heap_memory_max',
+    'jvm.non_heap_memory',
+    'jvm.os.open_file_descriptors',
+    'jvm.thread_count',
+]
+
+COUNTER_METRICS = [
+    # TODO: JMXFetch `counter` is reporter the wrong type.
+    'tomcat.bytes_rcvd',
+    'tomcat.bytes_sent',
+    'tomcat.error_count',
+    'tomcat.jsp.count',
+    'tomcat.jsp.reload_count',
+    'tomcat.processing_time',
+    'tomcat.request_count',
+    'tomcat.servlet.error_count',
+    'tomcat.servlet.processing_time',
+    'tomcat.servlet.request_count',
+    'tomcat.string_cache.access_count',
+    'tomcat.string_cache.hit_count',
+    'tomcat.web.cache.hit_count',
+    'tomcat.web.cache.lookup_count',
+]
+
 
 @pytest.mark.e2e
 def test(dd_agent_check):
@@ -16,3 +61,6 @@ def test(dd_agent_check):
         aggregator.assert_metric(metric)
 
     aggregator.assert_all_metrics_covered()
+
+    from datadog_checks.dev.utils import get_metadata_metrics
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), exclude=JVM_METRICS + COUNTER_METRICS)
