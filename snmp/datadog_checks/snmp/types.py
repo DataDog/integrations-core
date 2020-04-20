995b02b390b78b6d51a8bc1988cb144c57bbdf30
Thomas Hervé <thomas.herve@datadoghq.com>
2020-04-20 09:36:52 +0200

    Fix debug output (#6400)
    We don't have repr on the debug printer.

--- snmp/datadog_checks/snmp/types.py
+++ snmp/datadog_checks/snmp/types.py
@@ -7,7 +7,8 @@ Type declarations, for type checking purposes only.
 from typing import Literal, NamedTuple, Tuple, TypedDict
 
 MetricDefinition = TypedDict(
-    'MetricDefinition', {'type': Literal['gauge', 'rate', 'counter', 'monotonic_count'], 'value': float}
+    'MetricDefinition',
+    {'type': Literal['gauge', 'rate', 'counter', 'monotonic_count', 'monotonic_count_and_rate'], 'value': float},
 )
 
 MIBSymbol = NamedTuple('MIBSymbol', [('name', str), ('prefix', Tuple[str, ...])])
