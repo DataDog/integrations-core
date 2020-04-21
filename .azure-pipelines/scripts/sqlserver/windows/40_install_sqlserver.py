15b379135d3c400456737aba46813d2aded99a0a
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-21 20:06:39 +0200

    5 attempts

--- .azure-pipelines/scripts/sqlserver/windows/40_install_sqlserver.py
+++ .azure-pipelines/scripts/sqlserver/windows/40_install_sqlserver.py
@@ -7,7 +7,7 @@ from retrying import retry
 # Install with TCP/IP enabled, see: https://chocolatey.org/packages/sql-server-2017
 
 
-@retry(stop_max_attempt_number=2, wait_exponential_multiplier=1000)
+@retry(stop_max_attempt_number=5, wait_exponential_multiplier=1000)
 def install_sqlserver():
     subprocess.run(["choco", "install", "sql-server-2017", "--params=\"'/TCPENABLED:1'\""], check=True)
 
