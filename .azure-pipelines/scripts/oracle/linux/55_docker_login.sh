003cc2dbdcc0eebfbef827ef23550c6a731a9246
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-21 15:19:03 +0200

    Add some logs

--- .azure-pipelines/scripts/oracle/linux/55_docker_login.sh
+++ .azure-pipelines/scripts/oracle/linux/55_docker_login.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 
 set +x
 
+echo "[INFO] Docker login"
 for i in 2 4 8 16 32; do
   echo "$ORACLE_DOCKER_PASSWORD" | docker login container-registry.oracle.com --username "$ORACLE_DOCKER_USERNAME" --password-stdin && break
+  echo "[INFO] Wait $i seconds and retry docker login"
   sleep $i
 done
 
