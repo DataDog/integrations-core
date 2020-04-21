7ed57a2f9642433e175211427a3749ec7a18ece3
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-21 20:08:22 +0200

    should fail

--- .azure-pipelines/scripts/sqlserver/windows/40_install_sqlserver.py
+++ .azure-pipelines/scripts/sqlserver/windows/40_install_sqlserver.py
@@ -9,7 +9,7 @@ from retrying import retry
 
 @retry(stop_max_attempt_number=5, wait_exponential_multiplier=1000)
 def install_sqlserver():
-    subprocess.run(["choco", "install", "sql-server-2017", "--params=\"'/TCPENABLED:1'\""], check=True)
+    subprocess.run(["choco", "install", "sql-server-201xxx7", "--params=\"'/TCPENABLED:1'\""], check=True)
 
 
 install_sqlserver()
