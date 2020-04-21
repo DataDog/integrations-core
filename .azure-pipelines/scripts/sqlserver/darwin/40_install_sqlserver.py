dd570793529dabba56c060e742b7083282ebfbcd
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-21 19:52:10 +0200

    Shell False

--- .azure-pipelines/scripts/sqlserver/darwin/40_install_sqlserver.py
+++ .azure-pipelines/scripts/sqlserver/darwin/40_install_sqlserver.py
@@ -7,7 +7,7 @@ from retrying import retry
 
 @retry(stop_max_attempt_number=2, wait_exponential_multiplier=1000)
 def install_sqlserver():
-    subprocess.run(["choco", "install", "sql-server-2017", "--params=\"'/TCPENABLED:1'\""], shell=True, check=True)
+    subprocess.run(["choco", "install", "sql-server-2017", "--params=\"'/TCPENABLED:1'\""], check=True)
 
 
 install_sqlserver()
