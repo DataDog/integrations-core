0da962c4d6cacedcccd64eee37f1267baf3ebb99
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-21 14:46:03 +0200

    use exponential backup

--- .azure-pipelines/scripts/oracle/linux/55_docker_login.sh
+++ .azure-pipelines/scripts/oracle/linux/55_docker_login.sh
@@ -3,12 +3,9 @@ set -euo pipefail
 
 set +x
 
-n=0
-until [ $n -ge 3 ]
-do
-   echo "$ORACLE_DOCKER_PASSWORD" | docker login container-registry.oracle.com --username "$ORACLE_DOCKER_USERNAME" --password-stdin && break
-   n=$((n+1))
-   sleep 5
+for i in 2 4 8 16 32; do
+  echo "$ORACLE_DOCKER_PASSWORD" | docker login container-registry.oracle.com --username "$ORACLE_DOCKER_USERNAME" --password-stdin && break
+  sleep $i
 done
 
 set -x
