bddc966110decb229cf7a838026d1bfc0f0dd14e
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-24 18:19:43 +0200

    Add missing metric and assert metrics

--- ibm_mq/tests/test_ibm_mq_e2e.py
+++ ibm_mq/tests/test_ibm_mq_e2e.py
@@ -4,6 +4,7 @@
 
 import pytest
 
+from datadog_checks.dev.utils import get_metadata_metrics
 from .common import assert_all_metrics
 
 
@@ -12,3 +13,4 @@ def test_e2e_check_all(dd_agent_check, instance_collect_all):
     aggregator = dd_agent_check(instance_collect_all, rate=True)
 
     assert_all_metrics(aggregator)
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
