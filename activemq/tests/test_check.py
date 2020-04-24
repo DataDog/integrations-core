06efaee8f7dfc5c941bebec2df8ab2d078ddbafc
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-24 19:07:08 +0200

    Assert jvm metrics

--- activemq/tests/test_check.py
+++ activemq/tests/test_check.py
@@ -5,7 +5,7 @@
 import pytest
 
 from datadog_checks.dev.utils import get_metadata_metrics
-from .common import ACTIVEMQ_E2E_METRICS
+from .common import ACTIVEMQ_E2E_METRICS, JVM_E2E_METRICS
 
 
 @pytest.mark.e2e
@@ -13,8 +13,8 @@ def test(dd_agent_check):
     instance = {}
     aggregator = dd_agent_check(instance)
 
-    for metric in ACTIVEMQ_E2E_METRICS:
+    for metric in ACTIVEMQ_E2E_METRICS + JVM_E2E_METRICS:
         aggregator.assert_metric(metric)
 
     aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), exclude=JVM_E2E_METRICS)
