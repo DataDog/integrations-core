3845446d8386be449a4ff3800b054dcea7954ab3
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-24 16:17:34 +0200

    revert

--- .azure-pipelines/scripts/sqlserver/windows/40_install_sqlserver.py
+++ .azure-pipelines/scripts/sqlserver/windows/40_install_sqlserver.py
@@ -9,7 +9,7 @@ def install_sqlserver():
     Install with TCP/IP enabled, see: https://chocolatey.org/packages/sql-server-2017
     """
     print("Install sql-server-2017 ...")
-    subprocess.run(["choco", "install", "xxsql-server-2017", "--no-progress", "--params", "'/TCPENABLED:1'"], check=True)
+    subprocess.run(["choco", "install", "sql-server-2017", "--no-progress", "--params", "'/TCPENABLED:1'"], check=True)
 
 
 install_sqlserver()
