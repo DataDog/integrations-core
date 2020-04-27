04215eb8ed90051890ca1504647d70119021c728
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-27 12:03:11 +0200

    refactor tests

---
+++ solr/tests/common.py
@@ -0,0 +1,39 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
+
+JVM_METRICS = [
+    "jvm.thread_count",
+    "jvm.gc.cms.count",
+    "jvm.gc.parnew.time",
+    "jvm.buffer_pool.mapped.count",
+    "jvm.buffer_pool.mapped.capacity",
+    "jvm.buffer_pool.mapped.used",
+    "jvm.gc.cms.count",
+    "jvm.gc.parnew.time",
+    "jvm.gc.survivor_size",
+    "jvm.gc.old_gen_size",
+    "jvm.buffer_pool.direct.count",
+    "jvm.buffer_pool.direct.capacity",
+    "jvm.buffer_pool.direct.used",
+    "jvm.os.open_file_descriptors",
+    "jvm.cpu_load.system",
+    "jvm.cpu_load.process",
+    "jvm.loaded_classes",
+    "jvm.gc.eden_size",
+    "jvm.heap_memory_init",
+    "jvm.heap_memory_committed",
+    "jvm.heap_memory_max",
+    "jvm.heap_memory",
+    "jvm.non_heap_memory_init",
+    "jvm.non_heap_memory_committed",
+    "jvm.non_heap_memory_max",
+    "jvm.non_heap_memory",
+]
+
+SOLR_METRICS = [
+    "solr.searcher.maxdocs",
+    "solr.searcher.numdocs",
+    "solr.searcher.warmup",
+]
--- solr/tests/test_check.py
+++ solr/tests/test_check.py
@@ -7,45 +7,15 @@ import pytest
 from datadog_checks.base.stubs.aggregator import AggregatorStub
 from datadog_checks.dev.utils import get_metadata_metrics
 
-JVM_METRICS = [
-    "jvm.thread_count",
-    "jvm.gc.cms.count",
-    "jvm.gc.parnew.time",
-    "jvm.buffer_pool.mapped.count",
-    "jvm.buffer_pool.mapped.capacity",
-    "jvm.buffer_pool.mapped.used",
-    "jvm.gc.cms.count",
-    "jvm.gc.parnew.time",
-    "jvm.gc.survivor_size",
-    "jvm.gc.old_gen_size",
-    "jvm.buffer_pool.direct.count",
-    "jvm.buffer_pool.direct.capacity",
-    "jvm.buffer_pool.direct.used",
-    "jvm.os.open_file_descriptors",
-    "jvm.cpu_load.system",
-    "jvm.cpu_load.process",
-    "jvm.loaded_classes",
-    "jvm.gc.eden_size",
-    "jvm.heap_memory_init",
-    "jvm.heap_memory_committed",
-    "jvm.heap_memory_max",
-    "jvm.heap_memory",
-    "jvm.non_heap_memory_init",
-    "jvm.non_heap_memory_committed",
-    "jvm.non_heap_memory_max",
-    "jvm.non_heap_memory",
-]
+from .common import JVM_METRICS, SOLR_METRICS
+
 
 @pytest.mark.e2e
 def test_e2e(dd_agent_check):
     instance = {}
     aggregator = dd_agent_check(instance)  # type: AggregatorStub
-    metrics = [
-        "solr.searcher.maxdocs",
-        "solr.searcher.numdocs",
-        "solr.searcher.warmup",
-    ]
-    for metric in metrics + JVM_METRICS:
+
+    for metric in SOLR_METRICS + JVM_METRICS:
         aggregator.assert_metric(metric)
 
     aggregator.assert_all_metrics_covered()
