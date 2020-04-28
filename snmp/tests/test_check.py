91e21ad1fb2a324b89ec9e2d0a135686c56d368c
Thomas Hervé <thomas.herve@datadoghq.com>
2020-04-28 10:07:31 +0200

    Try to fix CI

--- snmp/tests/test_check.py
+++ snmp/tests/test_check.py
@@ -679,7 +679,7 @@ def test_discovery(aggregator):
             aggregator.reset()
     finally:
         check._running = False
-        check._thread.join()
+        del check
 
     for metric in common.SUPPORTED_METRIC_TYPES:
         metric_name = "snmp." + metric['name']
