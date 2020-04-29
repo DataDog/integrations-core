d58afb6db20d3665b77a0be4d8af6a774ef5cff4
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-29 18:27:01 +0200

    Revert

--- hazelcast/tests/common.py
+++ hazelcast/tests/common.py
@@ -6,7 +6,7 @@ from datadog_checks.dev import get_docker_hostname, get_here
 HERE = get_here()
 HOST = get_docker_hostname()
 
-MEMBER_PORT = 1098
+MEMBER_PORT = 1099
 MC_PORT = 9999
 MC_HEALTH_CHECK_ENDPOINT = 'http://{}:8081/health'.format(HOST)
 
