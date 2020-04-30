56167b7a83f519867f77a0de7948f5e589a8dac0
ChristineTChen <christine.chen@datadoghq.com>
2020-04-30 17:00:22 -0400

    add upsOutletGroupStatusGroupState metric

--- snmp/tests/test_profiles.py
+++ snmp/tests/test_profiles.py
@@ -1106,5 +1106,5 @@ def test_apc_ups(aggregator):
 
     for metric in metrics:
         aggregator.assert_metric('snmp.{}'.format(metric), metric_type=aggregator.GAUGE, tags=tags, count=1)
-
+    aggregator.assert_metric('snmp.upsOutletGroupStatusGroupState', metric_type=aggregator.GAUGE, tags=['outlet_group_name:test_outlet'] + profile_tags)
     aggregator.assert_all_metrics_covered()
