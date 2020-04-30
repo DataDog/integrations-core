7e54306165e1ec69c20ebfcc51aff9db2c986b3d
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-04-30 10:39:04 +0200

    Fix flaky tests by adding retry to docker_run (#6515)

---
+++ oracle/tests/scripts/install_instant_client.sh
@@ -0,0 +1,16 @@
+# This script makes the necessary setup to be able to compile pymqi on the agent machine
+
+INSTANT_CLIENT_URL="https://ddintegrations.blob.core.windows.net/oracle/instantclient-basiclite-linux.x64-19.3.0.0.0dbru.zip"
+
+mkdir /opt/oracle
+apt-get update
+apt-get install unzip
+
+# Retry necessary due to flaky download that might trigger:
+# curl: (56) OpenSSL SSL_read: SSL_ERROR_SYSCALL, errno 110
+for i in 2 4 8 16 32; do
+  curl -o /opt/oracle/instantclient.zip $INSTANT_CLIENT_URL && break
+  sleep $i
+done
+
+unzip /opt/oracle/instantclient.zip -d /opt/oracle
