ad75704fa2a0c1c21c3d23173c73b33c58f343b5
Sarah Witt <sarah.witt@datadoghq.com>
2020-04-29 23:16:16 -0400

    Add assert metrics using metadata

--- presto/tests/test_presto.py
+++ presto/tests/test_presto.py
@@ -3,6 +3,9 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
 
+from datadog_checks.dev.jmx import JVM_E2E_METRICS
+from datadog_checks.dev.utils import get_metadata_metrics
+
 from .common import METRICS
 
 
@@ -11,7 +14,8 @@ def test(dd_agent_check):
     instance = {}
     aggregator = dd_agent_check(instance, rate=True)
 
-    for metric in METRICS:
+    for metric in METRICS + JVM_E2E_METRICS:
         aggregator.assert_metric(metric)
 
     aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), exclude=JVM_E2E_METRICS)
