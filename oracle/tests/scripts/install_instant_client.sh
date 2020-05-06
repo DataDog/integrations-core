582fd45b4aa92e44af2a0ee7cdebd7a68170ea26
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-05-06 10:00:57 +0200

    Make startup commands less flaky

--- oracle/tests/scripts/install_instant_client.sh
+++ oracle/tests/scripts/install_instant_client.sh
@@ -1,14 +1,19 @@
 # This script makes the necessary setup to be able to compile pymqi on the agent machine
 
+set -x  # Show executed commands
+set -e  # Stop on first failure
+
 INSTANT_CLIENT_URL="https://ddintegrations.blob.core.windows.net/oracle/instantclient-basiclite-linux.x64-19.3.0.0.0dbru.zip"
 
 mkdir /opt/oracle
-apt-get update
-apt-get install unzip
 
-# Retry necessary due to flaky download that might trigger:
-# curl: (56) OpenSSL SSL_read: SSL_ERROR_SYSCALL, errno 110
+# Retry:
+# - Retry `apt-get`: we might not be able to fetch deps from debian
+# - Retry of curl donwload needed due to:
+#   curl: (56) OpenSSL SSL_read: SSL_ERROR_SYSCALL, errno 110
 for i in 2 4 8 16 32; do
+  apt-get update
+  apt-get install -y unzip
   curl -o /opt/oracle/instantclient.zip $INSTANT_CLIENT_URL && break
   sleep $i
 done
