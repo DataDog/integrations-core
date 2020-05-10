8e25d846036fabe6379e1092ac973359a3567844
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-05-10 19:35:13 +0200

    clean up constants

--- marklogic/datadog_checks/marklogic/constants.py
+++ marklogic/datadog_checks/marklogic/constants.py
@@ -1,13 +1,6 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from typing import Any, Dict
-
-from six import iteritems
-from pprint import pprint
-
-from datadog_checks.base import AgentCheck, ConfigurationError
-from datadog_checks.marklogic.api import MarkLogicApi
 
 RESOURCE_TYPES = {
     'cluster': {
