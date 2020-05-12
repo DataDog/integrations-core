152bbb01d5414c681a161e4ee20adf50d658b612
Mike Garabedian <mike.garabedian@datadoghq.com>
2020-05-12 16:12:59 -0400

    Fix style

--- datadog_checks_dev/datadog_checks/dev/tooling/trello.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/trello.py
@@ -1,7 +1,6 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from collections import defaultdict
 
 import requests
 
@@ -52,7 +51,7 @@ class TrelloClient:
             '55d1fe4cd3192ab85fa0f7ea': 'In Progress',  # INPROGRESS
             '58f0c271cbf2d534bd626916': 'Issues Found',  # HAVE BUGS
             '5d5a8a50ca7a0189ae8ac5ac': 'Awaiting Build',  # WAITING
-            '5dfb4eef503607473af708ab': 'Done'
+            '5dfb4eef503607473af708ab': 'Done',
         }
 
     def create_card(self, team, name, body, member=None):
@@ -94,17 +93,13 @@ class TrelloClient:
         Gather statistics for each category in the Trello board.
 
         """
-        columns = requests.get(self.LISTS_ENDPOINT, params=self.auth)
-        column_map = {c['id']: c['name'] for c in columns.json()}
-
         map_label = {v: k for k, v in self.label_map.items()}
         map_team_list = {v: k for k, v in self.team_list_map.items()}
 
-        counts = {k: {'Total': 0,
-                      'In Progress': 0,
-                      'Issues Found': 0,
-                      'Awaiting Build': 0,
-                      'Done': 0} for k in map_label.values()}
+        counts = {
+            k: {'Total': 0, 'In Progress': 0, 'Issues Found': 0, 'Awaiting Build': 0, 'Done': 0}
+            for k in map_label.values()
+        }
 
         cards = requests.get(self.BOARD_ENDPOINT, params=self.auth)
         for card in cards.json():
