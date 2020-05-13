d4f0d34c470d07b4b095f50702ea4e2841597402
mmcniece <TheMcGoose@users.noreply.github.com>
2020-05-13 12:59:22 -0400

    update tests to no expect calculate_text_stack_trace

--- clickhouse/tests/test_unit.py
+++ clickhouse/tests/test_unit.py
@@ -32,7 +32,7 @@ def test_config(instance):
             sync_request_timeout=10,
             compression=False,
             secure=False,
-            settings={'calculate_text_stack_trace': False},
+            settings={},
             client_name='datadog-test-clickhouse',
         )
 
