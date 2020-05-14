925b65caf5cc48929fe06ed3a826ec9f7b4eb3a3
Ofek Lev <ofekmeister@gmail.com>
2020-05-14 12:29:21 -0400

    address

--- win32_event_log/datadog_checks/win32_event_log/check.py
+++ win32_event_log/datadog_checks/win32_event_log/check.py
@@ -10,10 +10,10 @@ class Win32EventLogCheck(AgentCheck):
     def __new__(cls, name, init_config, instances):
         instance = instances[0]
 
-        if not is_affirmative(instance.get('use_wmi', True)):
-            return super(Win32EventLogCheck, cls).__new__(cls)
-        else:
+        if is_affirmative(instance.get('use_wmi', True)):
             return Win32EventLogWMI(name, init_config, instances)
+        else:
+            return super(Win32EventLogCheck, cls).__new__(cls)
 
     def __init__(self, name, init_config, instances):
         super(Win32EventLogCheck, self).__init__(name, init_config, instances)
