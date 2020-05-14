0e7e93d25c77642b66083ca370dc867d05999e32
Keshav Garg <keshav.garg@nutanix.com>
2020-05-14 11:03:57 +0530

    [ENG-308606][BM] Add support query execution from yaml config.
    Reviewers               : Aayush, Atreyee, Gurleen, Adnan, Hussain.
    Tickets resolved        : ENG-308606
    Tickets related         :
    Tests run               : Manuall
    Target release          : master, 5.18
    Breaks rolling upgrade  :

--- sqlserver/datadog_checks/sqlserver/sqlserver.py
+++ sqlserver/datadog_checks/sqlserver/sqlserver.py
@@ -61,6 +61,10 @@ DM_OS_WAIT_STATS_TABLE = "sys.dm_os_wait_stats"
 DM_OS_MEMORY_CLERKS_TABLE = "sys.dm_os_memory_clerks"
 DM_OS_VIRTUAL_FILE_STATS = "sys.dm_io_virtual_file_stats"
 
+# Note : This is not actually table, This is a place holder for all other table.
+# goverened by the query.
+DM_CUSTOME_METRIC = "custom_metric_table"
+
 class SQLConnectionError(Exception):
     """
     Exception raised for SQL instance connection issues
@@ -223,8 +227,13 @@ class SQLServer(AgentCheck):
                     if user_type is None:
                         sql_type, base_name = self.get_sql_type(instance, row['counter_name'])
                 except Exception:
-                    self.log.warning("Can't load the metric %s, ignoring", row['name'], exc_info=True)
-                    continue
+                    if row.get('sql_query') is None:
+                        self.log.warning("Can't load the metric %s, ignoring", row['name'], exc_info=True)
+                        continue
+                    else:
+                        sql_type = PERF_COUNTER_LARGE_RAWCOUNT
+                        db_table = DM_CUSTOME_METRIC
+                        base_name = None
 
                 metrics_to_collect.append(self.typed_metric(instance,
                                                             row,
@@ -303,7 +312,8 @@ class SQLServer(AgentCheck):
             table_type_mapping = {
                 DM_OS_WAIT_STATS_TABLE: (self.gauge, SqlOsWaitStat),
                 DM_OS_MEMORY_CLERKS_TABLE: (self.gauge, SqlOsMemoryClerksStat),
-                DM_OS_VIRTUAL_FILE_STATS: (self.gauge, SqlIoVirtualFileStat)
+                DM_OS_VIRTUAL_FILE_STATS: (self.gauge, SqlIoVirtualFileStat),
+                DM_CUSTOME_METRIC: (self.gauge, SqlComplexMetric)
             }
             metric_type, cls = table_type_mapping[table]
 
@@ -483,6 +493,9 @@ class SQLServer(AgentCheck):
                             metric.fetch_metric(cursor, vfs_rows, vfs_cols, custom_tags)
                         elif type(metric) is SqlOsMemoryClerksStat:
                             metric.fetch_metric(cursor, clerk_rows, clerk_cols, custom_tags)
+                        elif type(metric) is SqlComplexMetric:
+                            rows, cols = metric.fetch_all_values(cursor, metric.cfg_instance["sql_query"] , self.log)
+                            metric.fetch_metric(cursor, rows, cols, custom_tags)
 
                     except Exception as e:
                         self.log.warning("Could not fetch metric %s: %s" % (metric.datadog_name, e))
@@ -652,6 +665,44 @@ class SqlServerMetric(object):
     def fetch_metrics(self, cursor, tags):
         raise NotImplementedError
 
+class SqlComplexMetric(SqlServerMetric):
+
+    @classmethod
+    def fetch_all_values(cls, cursor, query, logger):
+        # This method detch all the metric based on the complex metric passed.
+        logger.debug("query : %s", query)
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        columns = [i[0] for i in cursor.description]
+        return rows, columns
+
+    def fetch_metric(self, cursor, rows, columns, tags):
+        # This method deals with publishing metric in data dog.
+        tagby_index = -1
+
+        if self.cfg_instance != None :
+            attribute_name_list = self.cfg_instance.get("attribute",[])
+        attribute_index_list = []
+        for index in range(len(columns)):
+            if (self.tag_by is not None) and (columns[index] in [self.tag_by]):
+                tagby_index = index
+            if columns[index] in attribute_name_list:
+                attribute_index_list.append(index)
+
+        for row in rows:
+            metric_tags = list(tags)
+            if tagby_index != -1:
+                metric_tags.append('{}:{}'.format(columns[tagby_index],str(row[tagby_index]).strip()))
+            for index in range(len(row)):
+                if (index == tagby_index) and (tagby_index not in attribute_index_list):
+                    continue
+                report_value = row[index]
+                metric_name = ""
+                if index in attribute_index_list:
+                    metric_name = '{}.{}.{}'.format(self.datadog_name, "attribute", columns[index])
+                else :
+                    metric_name = '{}.{}.{}'.format(self.datadog_name, "metric", columns[index])
+                self.report_function(metric_name, report_value, tags=metric_tags)
 
 class SqlSimpleMetric(SqlServerMetric):
 
