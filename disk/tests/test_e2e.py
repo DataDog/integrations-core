a535a89545db29280025d665d231e332e56941c5
ChristineTChen <christine.chen@datadoghq.com>
2020-05-14 23:23:02 -0400

    Use variable

--- disk/tests/test_e2e.py
+++ disk/tests/test_e2e.py
@@ -12,4 +12,4 @@ def test_check(dd_agent_check):
     aggregator = dd_agent_check()
     for metric in common.EXPECTED_METRICS:
         for device in common.EXPECTED_DEVICE:
-            aggregator.assert_metric(metric['metric'], metric_type=aggregator.GAUGE, device=metric['device'])
+            aggregator.assert_metric(metric, metric_type=aggregator.GAUGE, device=device)
