2149dd1e4673b131f59965dde60d89f9782dc5e3
ChristineTChen <christine.chen@datadoghq.com>
2020-05-16 16:32:51 -0400

    Update scraper config with instance

--- datadog_checks_base/datadog_checks/base/checks/openmetrics/mixins.py
+++ datadog_checks_base/datadog_checks/base/checks/openmetrics/mixins.py
@@ -3,6 +3,7 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from __future__ import division
 
+import copy
 from fnmatch import translate
 from math import isinf, isnan
 from os.path import isfile
@@ -59,7 +60,8 @@ class OpenMetricsScraperMixin(object):
             instance = {}
 
         # Create an empty configuration
-        config = {}
+        # Supports new configuration options
+        config = copy.deepcopy(instance)
 
         # Set the endpoint
         endpoint = instance.get('prometheus_url')
