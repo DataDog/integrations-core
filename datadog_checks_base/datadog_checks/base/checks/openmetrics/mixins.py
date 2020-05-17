99c90ce93ec32108455d5fca0d87530d76bb26f6
ChristineTChen <christine.chen@datadoghq.com>
2020-05-16 22:41:21 -0400

    clean up docstring

--- datadog_checks_base/datadog_checks/base/checks/openmetrics/mixins.py
+++ datadog_checks_base/datadog_checks/base/checks/openmetrics/mixins.py
@@ -371,8 +371,9 @@ class OpenMetricsScraperMixin(object):
 
     def parse_metric_family(self, response, scraper_config):
         """
-        Parse the MetricFamily from a valid requests.Response object to provide a MetricFamily object (see [0])
+        Parse the MetricFamily from a valid `requests.Response` object to provide a MetricFamily object.
         The text format uses iter_lines() generator.
+
         :param response: requests.Response
         :return: core.Metric
         """
@@ -415,7 +416,7 @@ class OpenMetricsScraperMixin(object):
 
     def scrape_metrics(self, scraper_config):
         """
-        Poll the data from prometheus and return the metrics as a generator.
+        Poll the data from Prometheus and return the metrics as a generator.
         """
         response = self.poll(scraper_config)
         if scraper_config['telemetry']:
@@ -467,10 +468,10 @@ class OpenMetricsScraperMixin(object):
 
     def process(self, scraper_config, metric_transformers=None):
         """
-        Polls the data from prometheus and pushes them as gauges
+        Polls the data from Prometheus and pushes them as gauges.
         `endpoint` is the metrics endpoint to use to poll metrics from Prometheus
 
-        Note that if the instance has a 'tags' attribute, it will be pushed
+        Note that if the instance has a `tags` attribute, it will be pushed
         automatically as additional custom tags and added to the metrics
         """
         transformers = scraper_config['_default_metric_transformers'].copy()
@@ -596,10 +597,10 @@ class OpenMetricsScraperMixin(object):
 
     def process_metric(self, metric, scraper_config, metric_transformers=None):
         """
-        Handle a prometheus metric according to the following flow:
-            - search scraper_config['metrics_mapper'] for a prometheus.metric <--> datadog.metric mapping
+        Handle a Prometheus metric according to the following flow:
+            - search `scraper_config['metrics_mapper']` for a prometheus.metric to datadog.metric mapping
             - call check method with the same name as the metric
-            - log some info if none of the above worked
+            - log info if none of the above worked
 
         `metric_transformers` is a dict of <metric name>:<function to run when the metric name is encountered>
         """
@@ -658,12 +659,12 @@ class OpenMetricsScraperMixin(object):
 
     def poll(self, scraper_config, headers=None):
         """
-        Custom headers can be added to the default headers.
-
-        Returns a valid requests.Response, raise requests.HTTPError if the status code of the requests.Response
-        isn't valid - see response.raise_for_status()
+        Returns a valid requests.Response, otherwise raise requests.HTTPError if the status code of the
+        requests.Response isn't valid - see `response.raise_for_status()`
 
         The caller needs to close the requests.Response
+
+        Custom headers can be added to the default headers.
 
         :param endpoint: string url endpoint
         :param headers: extra headers
@@ -715,14 +716,14 @@ class OpenMetricsScraperMixin(object):
     def submit_openmetric(self, metric_name, metric, scraper_config, hostname=None):
         """
         For each sample in the metric, report it as a gauge with all labels as tags
-        except if a labels dict is passed, in which case keys are label names we'll extract
+        except if a labels `dict` is passed, in which case keys are label names we'll extract
         and corresponding values are tag names we'll use (eg: {'node': 'node'}).
 
         Histograms generate a set of values instead of a unique metric.
-        send_histograms_buckets is used to specify if yes or no you want to
+        `send_histograms_buckets` is used to specify if you want to
             send the buckets as tagged values when dealing with histograms.
 
-        `custom_tags` is an array of 'tag:value' that will be added to the
+        `custom_tags` is an array of `tag:value` that will be added to the
         metric when sending the gauge to Datadog.
         """
         if metric.type in ["gauge", "counter", "rate"]:
