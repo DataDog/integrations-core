f0d3ba24042992cd4c586213ab2cb663317f7b0e
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-05-19 12:01:26 +0200

    Add metadata_from_profile

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp.py
@@ -111,6 +111,11 @@ def metadata_from_profile(ctx, profile_path):
     Generate metadata.csv from profile.
 
     You'll need to install pysnmp and pysnmp-mibs manually beforehand.
+
+    Build .py from MIB using:
+
+        mibdump.py --generate-mib-texts --mib-source <MIB_DIR> <MIB_NAME>
+        # --generate-mib-texts is needed to will include the descriptions
     """
     # Leave imports in function to not add the dependencies
     from pysnmp import hlapi
@@ -132,6 +137,19 @@ def metadata_from_profile(ctx, profile_path):
     with open(profile_path) as f:
         data = yaml.safe_load(f.read())
 
+    def _bulid_metric_row(symbol_name, forced_type=None):
+        node = mib_view_controller.mibBuilder.importSymbols(mib, symbol_name)[0]
+        if forced_type:
+            metric_type = forced_type
+        else:
+            metric_type = get_type(node.syntax)
+        row = {
+            'metric_name': "snmp.{}".format(symbol_name),
+            'metric_type': metric_type,
+            'description': node.getDescription(),
+        }
+        return row
+
     output = []
     for metric in data['metrics']:
         print("processing: ", metric)
@@ -141,50 +159,38 @@ def metadata_from_profile(ctx, profile_path):
         except MibNotFoundError:
             fetch_mib(mib)
         if 'table' in metric:
-            table = metric['table']
-            node = mib_view_controller.mibBuilder.importSymbols(mib, table)[0]
-            value = '.'.join([str(i) for i in node.getName()])
-            table = {'name': table, 'OID': value}
-            symbols = []
             for symbol in metric['symbols']:
-                node = mib_view_controller.mibBuilder.importSymbols(mib, symbol)[0]
-                value = '.'.join([str(i) for i in node.getName()])
-                symbols.append({'name': symbol, 'OID': value})
-            tags = []
-            for tag in metric['metric_tags']:
-                if 'column' in tag:
-                    tag_mib = tag.get('MIB', mib)
-                    key = tag['column']
-                    node = mib_view_controller.mibBuilder.importSymbols(tag_mib, key)[0]
-                    value = '.'.join([str(i) for i in node.getName()])
-                    tag = tag.copy()
-                    tag['column'] = {'name': key, 'OID': value}
-                    tags.append(tag)
-                else:
-                    tags.append(tag)
-            element = {'MIB': mib, 'table': table, 'symbols': symbols, 'metric_tags': tags}
-            if 'forced_type' in metric:
-                element['forced_type'] = metric['forced_type']
-            output.append(element)
-
+                output.append(_bulid_metric_row(symbol, metric.get('forced_type')))
         elif 'symbol' in metric:
-            key = metric['symbol']
-
-            node = mib_view_controller.mibBuilder.importSymbols(mib, key)[0]
-            value = '.'.join([str(i) for i in node.getName()])
-            element = {'MIB': mib, 'symbol': {'name': key, 'OID': value}}
-            if 'forced_type' in metric:
-                element['forced_type'] = metric['forced_type']
-            output.append(element)
+            output.append(_bulid_metric_row(metric['symbol'], metric.get('forced_type')))
         elif 'name' in metric:
-            key = metric['name']
-
-            node = mib_view_controller.mibBuilder.importSymbols(mib, key)[0]
-            print("node", node.getDescription())
-            value = '.'.join([str(i) for i in node.getName()])
-            element = {'MIB': mib, 'name': key, 'OID': value}
-            if 'forced_type' in metric:
-                element['forced_type'] = metric['forced_type']
-            print("out:", element)
-            output.append(element)
+            output.append(_bulid_metric_row(metric['name'], metric.get('forced_type')))
     print(yaml.dump({'metrics': output}))
+
+# metric_name,metric_type,interval,unit_name,per_unit_name,description,orientation,integration,short_name
+
+
+SNMP_COUNTER_CLASSES = {
+    'Counter32',
+    'Counter64',
+    # Additional types that are not part of the SNMP protocol (see RFC 2856).
+    'ZeroBasedCounter64',
+}
+
+SNMP_GAUGE_CLASSES = {
+    'Gauge32',
+    'Integer',
+    'Integer32',
+    'Unsigned32',
+    # Additional types that are not part of the SNMP protocol (see RFC 2856).
+    'CounterBasedGauge64',
+}
+
+
+def get_type(obj):
+    name = obj.__class__.__name__
+    if name in SNMP_COUNTER_CLASSES:
+        return 'count'
+    elif name in SNMP_GAUGE_CLASSES:
+        return 'gauge'
+    return 'unknown'
