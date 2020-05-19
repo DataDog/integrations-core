48907f8fd3b0dc06cf83bc23490588143c2a4246
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-05-19 11:18:55 +0200

    draft

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp.py
@@ -5,6 +5,7 @@ import os
 
 import click
 import yaml
+from pysnmp.smi import builder
 
 from ..console import CONTEXT_SETTINGS
 
@@ -98,5 +99,92 @@ def translate_profile(ctx, profile_path):
             element = {'MIB': mib, 'symbol': {'name': key, 'OID': value}}
             if 'forced_type' in metric:
                 element['forced_type'] = metric['forced_type']
+            output.append(element)
+    print(yaml.dump({'metrics': output}))
+
+
+@snmp.command(context_settings=CONTEXT_SETTINGS, short_help='Translate MIB name to OIDs in SNMP profiles')
+@click.argument('profile_path')
+@click.pass_context
+def metadata_from_profile(ctx, profile_path):
+    """
+    Generate metadata.csv from profile.
+
+    You'll need to install pysnmp and pysnmp-mibs manually beforehand.
+    """
+    # Leave imports in function to not add the dependencies
+    from pysnmp import hlapi
+    from pysnmp.smi import view
+    from pysnmp.smi.error import MibNotFoundError
+
+    snmp_engine = hlapi.SnmpEngine()
+    mib_builder = snmp_engine.getMibBuilder()
+
+    from os.path import expanduser
+    home = expanduser("~")
+    MIBDIR = os.path.join(home, '.pysnmp', 'mibs')
+    mibSources = mib_builder.getMibSources() + (builder.DirMibSource(MIBDIR),)
+    mib_builder.setMibSources(*mibSources)
+    mib_builder.loadTexts = True
+
+    mib_view_controller = view.MibViewController(mib_builder)
+
+    with open(profile_path) as f:
+        data = yaml.safe_load(f.read())
+
+    output = []
+    for metric in data['metrics']:
+        print("processing: ", metric)
+        mib = metric['MIB']
+        try:
+            mib_view_controller.mibBuilder.loadModule(mib)
+        except MibNotFoundError:
+            fetch_mib(mib)
+        if 'table' in metric:
+            table = metric['table']
+            node = mib_view_controller.mibBuilder.importSymbols(mib, table)[0]
+            value = '.'.join([str(i) for i in node.getName()])
+            table = {'name': table, 'OID': value}
+            symbols = []
+            for symbol in metric['symbols']:
+                node = mib_view_controller.mibBuilder.importSymbols(mib, symbol)[0]
+                value = '.'.join([str(i) for i in node.getName()])
+                symbols.append({'name': symbol, 'OID': value})
+            tags = []
+            for tag in metric['metric_tags']:
+                if 'column' in tag:
+                    tag_mib = tag.get('MIB', mib)
+                    key = tag['column']
+                    node = mib_view_controller.mibBuilder.importSymbols(tag_mib, key)[0]
+                    value = '.'.join([str(i) for i in node.getName()])
+                    tag = tag.copy()
+                    tag['column'] = {'name': key, 'OID': value}
+                    tags.append(tag)
+                else:
+                    tags.append(tag)
+            element = {'MIB': mib, 'table': table, 'symbols': symbols, 'metric_tags': tags}
+            if 'forced_type' in metric:
+                element['forced_type'] = metric['forced_type']
+            output.append(element)
+
+        elif 'symbol' in metric:
+            key = metric['symbol']
+
+            node = mib_view_controller.mibBuilder.importSymbols(mib, key)[0]
+            value = '.'.join([str(i) for i in node.getName()])
+            element = {'MIB': mib, 'symbol': {'name': key, 'OID': value}}
+            if 'forced_type' in metric:
+                element['forced_type'] = metric['forced_type']
+            output.append(element)
+        elif 'name' in metric:
+            key = metric['name']
+
+            node = mib_view_controller.mibBuilder.importSymbols(mib, key)[0]
+            print("node", node.getDescription())
+            value = '.'.join([str(i) for i in node.getName()])
+            element = {'MIB': mib, 'name': key, 'OID': value}
+            if 'forced_type' in metric:
+                element['forced_type'] = metric['forced_type']
+            print("out:", element)
             output.append(element)
     print(yaml.dump({'metrics': output}))
