b3b584cd60d9ffbb78ac462e5548461ceabe8d36
Julia <611228+hithwen@users.noreply.github.com>
2020-05-26 14:49:21 +0200

    wmi check to not use instance (#6325)
    * WinWMICheck to support new agent signature
    * Remove instance usage from base wmi check
    
    Co-Authored-By: Florimond Manca <florimond.manca@gmail.com>

--- wmi_check/setup.py
+++ wmi_check/setup.py
@@ -27,7 +27,7 @@ def get_dependencies():
         return f.readlines()
 
 
-CHECKS_BASE_REQ = 'datadog_checks_base>=11.0.0'
+CHECKS_BASE_REQ = 'datadog_checks_base>=11.6.0'
 
 setup(
     name='datadog-wmi_check',
