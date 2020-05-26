585b2ea0fcaf7f0489be2bf193f0911a880344df
Paul <paul.coignet@datadoghq.com>
2020-05-26 17:55:12 +0200

    Add ability to generate docs site as a PDF (#6719)
    * Add --pdf flag
    
    * Fix indent
    
    * Fix style
    
    * Update datadog_checks_dev/datadog_checks/dev/tooling/commands/docs/build.py
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    * Use env_vars for serve command
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/docs/build.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/docs/build.py
@@ -13,12 +13,15 @@ from .utils import insert_verbosity_flag
 
 @click.command(context_settings=CONTEXT_SETTINGS, short_help='Build documentation')
 @click.option('--verbose', '-v', count=True, help='Increase verbosity (can be used additively)')
-def build(verbose):
+@click.option('--pdf', is_flag=True, help='Also export the site as PDF')
+def build(verbose, pdf):
     """Build documentation."""
     command = ['tox', '-e', 'docs', '--', 'build', '--clean']
     insert_verbosity_flag(command, verbose)
 
-    with chdir(get_root()):
+    env_vars = {'ENABLE_PDF_SITE_EXPORT': '1' if pdf else '0'}
+
+    with chdir(get_root(), env_vars=env_vars):
         process = subprocess.run(command)
 
     abort(code=process.returncode)
