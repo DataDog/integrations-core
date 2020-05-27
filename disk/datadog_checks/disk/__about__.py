88ef463add2f12023ba4e94d21ac057fdc2d9428
Ofek Lev <ofekmeister@gmail.com>
2020-05-27 11:44:04 -0400

    Test Codecov flags

--- disk/datadog_checks/disk/__about__.py
+++ disk/datadog_checks/disk/__about__.py
@@ -1,5 +1,5 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
+# Licensed under a 3-clause BSOD style license (see LICENSE)
 
 __version__ = "2.10.1"
