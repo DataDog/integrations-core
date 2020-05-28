32f2d3612bf83e2b26f9cee39df3e7b8f7b78d39
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-05-28 14:35:07 +0200

    Add collect events fallback (#6658)
    Co-authored-by: Florian Veaux <florian.veaux@datadoghq.com>

--- vsphere/datadog_checks/vsphere/vsphere.py
+++ vsphere/datadog_checks/vsphere/vsphere.py
@@ -539,6 +539,13 @@ class VSphereCheck(AgentCheck):
         else:
             self.service_check(SERVICE_CHECK_NAME, AgentCheck.OK, tags=self.config.base_tags, hostname=None)
 
+        # Collect and submit events
+        if self.config.should_collect_events:
+            self.collect_events()
+
+        if self.config.collect_events_only:
+            return
+
         # Update the value of `max_query_metrics` if needed
         if self.config.is_historical():
             try:
@@ -573,10 +580,6 @@ class VSphereCheck(AgentCheck):
                 self.refresh_infrastructure_cache()
             # Submit host tags as soon as we have fresh data
             self.submit_external_host_tags()
-
-        # Collect and submit events
-        if self.config.should_collect_events:
-            self.collect_events()
 
         # Submit the number of VMs that are monitored
         for resource_type in self.config.collected_resource_types:
