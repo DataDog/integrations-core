799b3e634a794163dd86fb9b2bd37009c7e21e74
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-06-01 14:22:27 +0200

    fix style

--- process/datadog_checks/process/process.py
+++ process/datadog_checks/process/process.py
@@ -9,13 +9,13 @@ import subprocess
 import time
 from collections import defaultdict
 
+import datadog_agent
 import psutil
 from six import iteritems
 
 from datadog_checks.base import AgentCheck
 from datadog_checks.base.config import _is_affirmative
 from datadog_checks.base.utils.platform import Platform
-import datadog_agent
 
 from .cache import DEFAULT_SHARED_PROCESS_LIST_CACHE_DURATION, ProcessListCache
 
