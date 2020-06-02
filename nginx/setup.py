ca286bb554b4206db9ba381e2f8fcffa69b01a1a
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-06-02 13:10:05 +0200

    Add config specs

--- nginx/setup.py
+++ nginx/setup.py
@@ -27,7 +27,7 @@ def get_dependencies():
         return f.readlines()
 
 
-CHECKS_BASE_REQ = 'datadog-checks-base>=4.2.0'
+CHECKS_BASE_REQ = 'datadog-checks-base>=11.0.0'
 
 setup(
     name='datadog-nginx',
