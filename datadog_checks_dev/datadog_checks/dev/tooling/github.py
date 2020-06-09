494e2f15c3b1fd053cfba463bee3f6edf49f5f85
Karim Bogtob <karim.bogtob@datadoghq.com>
2020-06-09 10:07:02 +0200

    Improve github api client

--- datadog_checks_dev/datadog_checks/dev/tooling/github.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/github.py
@@ -28,6 +28,30 @@ def get_auth_info(config=None):
     if user and token:
         return user, token
 
+def get_commit(repo, commit_sha, config):
+    response = requests.get(
+        f'https://api.github.com/repos/DataDog/{repo}/git/commits/{commit_sha}', auth=get_auth_info(config),
+    )
+
+    response.raise_for_status()
+    return response.json()
+
+def get_tag(repo, ref, config):
+    response = requests.get(
+        f'https://api.github.com/repos/DataDog/{repo}/git/tags/{ref}', auth=get_auth_info(config),
+    )
+
+    response.raise_for_status()
+    return response.json()
+
+
+def get_tags(repo, config):
+    response = requests.get(
+        f'https://api.github.com/repos/DataDog/{repo}/git/refs/tags', auth=get_auth_info(config),
+    )
+
+    response.raise_for_status()
+    return response.json()
 
 def get_pr_labels(pr_payload):
     labels = []
@@ -38,10 +62,8 @@ def get_pr_labels(pr_payload):
 
     return labels
 
-
 def get_pr_milestone(pr_payload):
     return (pr_payload.get('milestone') or {}).get('title', '')
-
 
 def get_changelog_types(pr_payload):
     """
@@ -55,13 +77,16 @@ def get_changelog_types(pr_payload):
 
     return changelog_labels
 
+def get_compare(base_commit, head_commit, repo, config):
+    response = requests.get(
+        f'https://api.github.com/repos/DataDog/{repo}/compare/{base_commit}...{head_commit}', auth=get_auth_info(config),
+    )
 
-def get_pr(pr_num, config=None, raw=False, org='DataDog'):
-    """
-    Get the payload for the given PR number. Let exceptions bubble up.
-    """
-    repo = basepath(get_root())
+    response.raise_for_status()
+    return response.json()
 
+
+def get_pr_of_repo(pr_num, repo, config=None, raw=False, org='DataDog'):
     response = requests.get(PR_ENDPOINT.format(org, repo, pr_num), auth=get_auth_info(config))
 
     if raw:
@@ -69,6 +94,13 @@ def get_pr(pr_num, config=None, raw=False, org='DataDog'):
     else:
         response.raise_for_status()
         return response.json()
+
+def get_pr(pr_num, config=None, raw=False, org='DataDog'):
+    """
+    Get the payload for the given PR number. Let exceptions bubble up.
+    """
+    repo = basepath(get_root())
+    return get_pr_of_repo(pr_num, repo, config, raw, org)
 
 
 def get_pr_from_hash(commit_hash, repo, config=None, raw=False):
