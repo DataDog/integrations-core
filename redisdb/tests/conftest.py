e07073b6e0d7b8a6ca0d02f9396417e2e90d0c8b
ChristineTChen <christine.chen@datadoghq.com>
2020-06-11 18:17:20 -0400

    Revert "Add flag to enable CLIENT command metrics (#6877)"
    This reverts commit 4ad791c034bf764d8f90795a8156f5bc90648396.

--- redisdb/tests/conftest.py
+++ redisdb/tests/conftest.py
@@ -78,14 +78,7 @@ def dd_environment(master_instance):
 
 @pytest.fixture
 def redis_instance():
-    return {
-        'host': HOST,
-        'port': PORT,
-        'password': PASSWORD,
-        'keys': ['test_*'],
-        'tags': ["foo:bar"],
-        'collect_client_metrics': True,
-    }
+    return {'host': HOST, 'port': PORT, 'password': PASSWORD, 'keys': ['test_*'], 'tags': ["foo:bar"]}
 
 
 @pytest.fixture
@@ -95,7 +88,7 @@ def replica_instance():
 
 @pytest.fixture(scope='session')
 def master_instance():
-    return {'host': HOST, 'port': MASTER_PORT, 'keys': ['test_*'], 'collect_client_metrics': True}
+    return {'host': HOST, 'port': MASTER_PORT, 'keys': ['test_*']}
 
 
 @pytest.fixture
