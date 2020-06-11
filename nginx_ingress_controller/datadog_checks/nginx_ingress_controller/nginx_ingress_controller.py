bc8944fbd51b1b0b4e29c9f4d0e6565715eb6bf7
ChristineTChen <christine.chen@datadoghq.com>
2020-06-11 10:35:09 -0400

    Fix style

--- nginx_ingress_controller/datadog_checks/nginx_ingress_controller/nginx_ingress_controller.py
+++ nginx_ingress_controller/datadog_checks/nginx_ingress_controller/nginx_ingress_controller.py
@@ -1,9 +1,8 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from datadog_checks.base.checks.openmetrics import OpenMetricsBaseCheck
 from datadog_checks.base import is_affirmative
-
+from datadog_checks.base.checks.openmetrics import OpenMetricsBaseCheck
 
 DEFAULT_METRICS = [
     # nginx metrics
