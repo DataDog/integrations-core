5a33778300ddffb28a8613854c23facb562bc28e
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2020-06-15 11:50:30 -0400

    fix plan signature and send normalized plan

--- mysql/datadog_checks/mysql/sql.py
+++ mysql/datadog_checks/mysql/sql.py
@@ -13,11 +13,12 @@ def compute_sql_signature(query):
     return format(mmh3.hash64(normalized, signed=False)[0], 'x')
 
 
-def compute_exec_plan_signature(plan_dict):
+def compute_exec_plan_signature(json_plan):
     """
     Given a query execution plan, generate a 64-bit hex signature on the normalized execution plan
     """
-    json_plan = json.dumps(plan_dict)
+    if not json_plan:
+        return None, None
     normalized_json = datadog_agent.obfuscate_sql_exec_plan(json_plan, normalize=True)
     with_sorted_keys = json.dumps(json.loads(normalized_json), sort_keys=True)
-    return format(mmh3.hash64(with_sorted_keys, signed=False)[0], 'x')
+    return normalized_json, format(mmh3.hash64(with_sorted_keys, signed=False)[0], 'x')
