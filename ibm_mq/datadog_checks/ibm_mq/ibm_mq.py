7b39637e05e3d3dd3909c8a9f561b6a34f860f2a
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-06-22 17:53:15 +0200

    Refactor by grouping collectors (#6944)

--- ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
+++ ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
@@ -8,9 +8,8 @@ from datadog_checks.base import AgentCheck
 from datadog_checks.ibm_mq.metrics import COUNT, GAUGE
 
 from . import connection, errors
-from .channel_metric_collector import ChannelMetricCollector
+from .collectors import ChannelMetricCollector, QueueMetricCollector
 from .config import IBMMQConfig
-from .queue_metric_collector import QueueMetricCollector
 
 try:
     import pymqi
