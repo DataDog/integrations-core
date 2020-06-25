66dfdfab0f4116d48226696cbbc464f566787312
ChristineTChen <christine.chen@datadoghq.com>
2020-06-25 14:10:42 -0400

    Capitalize

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/create.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/create.py
@@ -94,7 +94,7 @@ def create(ctx, name, integration_type, location, non_interactive, quiet, dry_ru
     """
         Create scaffolding for a new integration.
         
-        NAME: use the display name as the integration would appear in documentation.
+        NAME: Use the display name as the integration would appear in documentation.
     """
 
     if name.islower():
