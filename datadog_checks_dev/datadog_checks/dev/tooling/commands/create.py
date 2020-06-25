bb5e63d25ff825aceb850fcd5f182d23967c0a1e
ChristineTChen <christine.chen@datadoghq.com>
2020-06-25 14:31:13 -0400

    fix style

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/create.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/create.py
@@ -93,7 +93,7 @@ def display_path_tree(path_tree):
 def create(ctx, name, integration_type, location, non_interactive, quiet, dry_run):
     """
         Create scaffolding for a new integration.
-        
+
         NAME: Use the display name as the integration would appear in documentation.
     """
 
