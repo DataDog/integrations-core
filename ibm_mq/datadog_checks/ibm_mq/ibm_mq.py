e1e7c002a1c48551a681801f238d07fbfaa8cc39
steveny91 <steven_yuen05@hotmail.com>
2020-06-25 12:53:18 -0400

    added logic for metadata collection from cmdline

--- ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
+++ ibm_mq/datadog_checks/ibm_mq/ibm_mq.py
@@ -1,10 +1,10 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-
 from typing import Any
 
 from datadog_checks.base import AgentCheck
+from datadog_checks.base.utils.subprocess_output import get_subprocess_output
 from datadog_checks.ibm_mq.metrics import COUNT, GAUGE
 
 from . import connection, errors
@@ -44,6 +44,8 @@ class IbmMqCheck(AgentCheck):
             self.service_check(self.SERVICE_CHECK, AgentCheck.CRITICAL, self.config.tags)
             return
 
+        self._collect_metadata()
+
         try:
             self.channel_metric_collector.get_pcf_channel_metrics(queue_manager)
             self.queue_metric_collector.collect_queue_metrics(queue_manager)
@@ -55,3 +57,31 @@ class IbmMqCheck(AgentCheck):
             getattr(self, metric_type)(metric_name, metric_value, tags=tags)
         else:
             self.log.warning("Unknown metric type `%s` for metric `%s`", metric_type, metric_name)
+
+    def _collect_metadata(self):
+        raw_version = self._get_version()
+        if not raw_version:
+            self.log.warning("Version not found in stdout")
+            return
+        self.log.debug('IBM MQ version: %s', raw_version)
+        self.set_metadata('version', raw_version)
+
+    def _get_version(self):
+
+        cmd = self.instance.get("dspmqver")
+        try:
+            pc_out, pc_err, _ = get_subprocess_output(cmd, self.log, False)
+        except OSError as e:
+            self.log.warning("Error collecting IBM MQ version: %s", e)
+            return None
+
+        if pc_out is None:
+            return None
+        return self._parse_version(pc_out)
+
+    @staticmethod
+    def _parse_version(output):
+        for line in output.splitlines():
+            line = line.strip()
+            if line.startswith("Version:"):
+                return line.split()[1]
