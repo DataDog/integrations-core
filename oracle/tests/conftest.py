197e5f9b34df81c4c17448ba429ae2b594774f20
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-06-25 13:10:17 +0200

    Style fix

--- oracle/tests/conftest.py
+++ oracle/tests/conftest.py
@@ -25,7 +25,7 @@ E2E_METADATA_ORACLE_CLIENT = {
     'start_commands': [
         'bash /tmp/install_instant_client.sh',
         'apt-get install libaio1',  # `apt-get update` already ran in install_instant_client.sh
-        'apt-get install gcc'
+        'apt-get install gcc',
     ],
     'env_vars': {'LD_LIBRARY_PATH': '/opt/oracle/instantclient_19_3'},
 }
@@ -35,9 +35,7 @@ E2E_METADATA_JDBC_CLIENT = {
     # the integration will fallback to JDBC client
     'use_jmx': True,  # Using jmx to have a ready to use java runtime
     'docker_volumes': ['{}/scripts/install_instant_client.sh:/tmp/install_instant_client.sh'.format(HERE)],
-    'start_commands': [
-        'bash /tmp/install_instant_client.sh'
-    ],
+    'start_commands': ['bash /tmp/install_instant_client.sh'],
 }
 
 
