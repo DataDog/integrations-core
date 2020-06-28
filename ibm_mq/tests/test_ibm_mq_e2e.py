10132ba3ac09699fcfe741986e58b837b69abcac
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-06-28 19:17:46 +0200

    Fix e2e

--- ibm_mq/tests/test_ibm_mq_e2e.py
+++ ibm_mq/tests/test_ibm_mq_e2e.py
@@ -6,12 +6,12 @@ import pytest
 
 from datadog_checks.dev.utils import get_metadata_metrics
 
-from .common import CHANNEL_STATS_METRICS, assert_all_metrics
+from .common import assert_all_metrics
 
 
 @pytest.mark.e2e
 def test_e2e_check_all(dd_agent_check, instance_collect_all):
     aggregator = dd_agent_check(instance_collect_all, rate=True)
 
-    assert_all_metrics(aggregator, extra_metrics=CHANNEL_STATS_METRICS)
+    assert_all_metrics(aggregator)
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
