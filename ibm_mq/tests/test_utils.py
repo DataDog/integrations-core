2bf92dbefae8e40732869e45bbe7b0b5c2ac9d09
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-06-29 10:26:16 +0200

    Add test for test_sanitize_strings

---
+++ ibm_mq/tests/test_utils.py
@@ -0,0 +1,21 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+import pytest
+
+from datadog_checks.ibm_mq.utils import sanitize_strings
+
+pytestmark = pytest.mark.unit
+
+
+@pytest.mark.parametrize(
+    'input,expected',
+    [
+        pytest.param('abc\x00\x00\x00\x00', 'abc', id='strip_null_chars'),
+        pytest.param('   123    ', '123', id='strip_whitespaces'),
+        pytest.param('   abc123    \x00\x00\x00\x00', 'abc123', id='strip_whitespaces_and_null_chars'),
+    ],
+)
+def test_sanitize_strings(input, expected):
+    assert expected == sanitize_strings(input)
