f232c7306fe84e7fad26febd8bbb4b9b874abb26
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-06-29 14:50:10 +0200

    Test bytes

--- ibm_mq/tests/test_utils.py
+++ ibm_mq/tests/test_utils.py
@@ -15,6 +15,8 @@ pytestmark = pytest.mark.unit
         pytest.param('abc\x00\x00\x00\x00', 'abc', id='strip_null_chars'),
         pytest.param('   123    ', '123', id='strip_whitespaces'),
         pytest.param('   abc123    \x00\x00\x00\x00', 'abc123', id='strip_whitespaces_and_null_chars'),
+        pytest.param(b'abc\x00\x00\x00\x00', 'abc', id='strip_null_chars_bytes'),
+        pytest.param(b'   123    ', '123', id='strip_whitespaces_bytes'),
     ],
 )
 def test_sanitize_strings(input_string, expected):
