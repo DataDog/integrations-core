f2b7c1631e4fc1a127ea8230dd9b6d34b3325c92
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-07-06 11:01:16 +0200

    Force datadog_test_libs as firstparty

--- datadog_checks_base/tests/test_pdhbasecheck.py
+++ datadog_checks_base/tests/test_pdhbasecheck.py
@@ -8,9 +8,8 @@ import pytest
 from .utils import requires_windows
 
 try:
-    from datadog_test_libs.win.pdh_mocks import initialize_pdh_tests, pdh_mocks_fixture  # noqa: F401
-
     from datadog_checks.checks.win.winpdh_base import PDHBaseCheck
+    from datadog_test_libs.win.pdh_mocks import initialize_pdh_tests, pdh_mocks_fixture  # noqa: F401
 except ImportError:
     pass
 
