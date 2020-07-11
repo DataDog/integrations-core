d23e572ba3e49b0a6c02f4e7e616fb39657a2c80
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-07-11 03:58:43 +0200

    More bench

--- snmp/tests/test_bench.py
+++ snmp/tests/test_bench.py
@@ -43,10 +43,40 @@ def test_tabular_no_bulk(benchmark):
 
 instance = generate_instance_config([])
 instance['community_string'] = 'f5'
-init = {'oid_batch_size': 128}
 
-check = SnmpCheck('snmp', init, [instance])
+check10 = SnmpCheck('snmp', {'oid_batch_size': 10}, [instance])
+check32 = SnmpCheck('snmp', {'oid_batch_size': 32}, [instance])
+check64 = SnmpCheck('snmp', {'oid_batch_size': 64}, [instance])
+check128 = SnmpCheck('snmp', {'oid_batch_size': 128}, [instance])
+check256 = SnmpCheck('snmp', {'oid_batch_size': 256}, [instance])
+# check512 = SnmpCheck('snmp', {'oid_batch_size': 512}, [instance])
+
+check10.check(None)
+check32.check(None)
+check64.check(None)
+check128.check(None)
+check256.check(None)
+# check512.check(None)
 
 
-def test_profile_f5(benchmark):
-    benchmark(check.check, instance)
+def test_profile_f5_10(benchmark):
+    benchmark(check10.check, instance)
+
+
+def test_profile_f5_32(benchmark):
+    benchmark(check32.check, instance)
+
+
+def test_profile_f5_64(benchmark):
+    benchmark(check64.check, instance)
+
+
+def test_profile_f5_128(benchmark):
+    benchmark(check128.check, instance)
+
+
+def test_profile_f5_256(benchmark):
+    benchmark(check256.check, instance)
+
+# def test_profile_f5_512(benchmark):
+#     benchmark(check512.check, instance)
