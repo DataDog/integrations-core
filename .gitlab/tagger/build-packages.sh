de32d324c9b597bd89c6170a6f0077bc952c31a7
florian.veaux <florian.veaux@datadoghq.com>
2020-07-14 12:46:17 +0200

    Fix #7120

---
+++ .gitlab/tagger/build-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euxo pipefail
+IFS=$'\n\t'
+
+curl --request POST --form "token=$CI_JOB_TOKEN" --form ref=master \
+  --form variables[ORIG_CI_BUILD_REF]=$CI_BUILD_REF \
+  --form variables[ORIG_CI_BUILD_REF_NAME]=$CI_BUILD_REF_NAME \
+  --form variables[ROOT_LAYOUT_TYPE]=core \
+  --form variables[REPO_NAME]=integrations-core \
+  https://gitlab.ddbuild.io/api/v4/projects/138/trigger/pipeline
+
