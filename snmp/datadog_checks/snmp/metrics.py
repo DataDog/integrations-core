14b24d328feeccc28ef8ebffa669caf9576f76c9
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-07-16 09:52:52 +0200

    Refactor sanitize 2

--- snmp/datadog_checks/snmp/metrics.py
+++ snmp/datadog_checks/snmp/metrics.py
@@ -47,23 +47,26 @@ def as_metric_with_inferred_type(value):
 def as_metric_with_forced_type(value, forced_type, options):
     # type: (Any, str, dict) -> Optional[MetricDefinition]
 
-    if forced_type == 'gauge':
-        return {'type': 'gauge', 'value': float(sanitize_varbind_value(value))}
-
-    if forced_type == 'percent':
-        return {'type': 'rate', 'value': total_time_to_temporal_percent(float(sanitize_varbind_value(value)), scale=1)}
-
-    if forced_type == 'counter':
-        return {'type': 'rate', 'value': float(sanitize_varbind_value(value))}
-
-    if forced_type == 'monotonic_count':
-        return {'type': 'monotonic_count', 'value': float(sanitize_varbind_value(value))}
-
-    if forced_type == 'monotonic_count_and_rate':
-        return {'type': 'monotonic_count_and_rate', 'value': float(sanitize_varbind_value(value))}
-
     if forced_type == 'flag_stream':
         index = int(options['placement']) - 1
         return {'type': 'gauge', 'value': int(str(value)[index])}
+
+    # Use float for following types
+    float_value = float(sanitize_varbind_value(value))
+
+    if forced_type == 'gauge':
+        return {'type': 'gauge', 'value': float_value}
+
+    if forced_type == 'percent':
+        return {'type': 'rate', 'value': total_time_to_temporal_percent(float_value, scale=1)}
+
+    if forced_type == 'counter':
+        return {'type': 'rate', 'value': float_value}
+
+    if forced_type == 'monotonic_count':
+        return {'type': 'monotonic_count', 'value': float_value}
+
+    if forced_type == 'monotonic_count_and_rate':
+        return {'type': 'monotonic_count_and_rate', 'value': float_value}
 
     return None
