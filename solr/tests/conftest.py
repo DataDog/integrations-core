1a8489c84aa98e127f35e1ef4f34a26042b17d03
Sarah Witt <sarah.witt@datadoghq.com>
2020-07-20 12:51:22 -0400

    Go back

--- solr/tests/conftest.py
+++ solr/tests/conftest.py
@@ -2,7 +2,6 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 
-import copy
 import os
 
 import pytest
@@ -19,10 +18,4 @@ def dd_environment():
         instance = load_jmx_config()
         instance['instances'][0]['host'] = HOST
         instance['instances'][0]['port'] = 18983
-        instance['instances'].append(copy.deepcopy(instance['instances'][0]))
-        instance['instances'][1]['host'] = HOST
-        instance['instances'][1]['port'] = 18982
-        instance['instances'].append(copy.deepcopy(instance['instances'][0]))
-        instance['instances'][2]['host'] = HOST
-        instance['instances'][2]['port'] = 18981
         yield instance, {'use_jmx': True}
