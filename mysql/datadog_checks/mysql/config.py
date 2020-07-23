3ede668b545cc6d55dec9620149ad6801ebf5bb4
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-07-23 13:34:07 +0200

    Split config out

---
+++ mysql/datadog_checks/mysql/config.py
@@ -0,0 +1,34 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+from datadog_checks.base import ConfigurationError
+from datadog_checks.base.log import get_check_logger
+
+DEFAULT_MAX_CUSTOM_QUERIES = 20
+
+
+class MySQLConfig(object):
+    def __init__(self, instance):
+        self.log = get_check_logger()
+        self.host = instance.get('server', '')
+        self.port = int(instance.get('port', 0))
+        self.tags = list(instance.get('tags', []))
+        self.mysql_sock = instance.get('sock', '')
+        self.defaults_file = instance.get('defaults_file', '')
+        self.user = instance.get('user', '')
+        self.password = str(instance.get('pass', ''))
+        self.tags = instance.get('tags', [])
+        self.options = instance.get('options', {}) or {}  # options could be None if empty in the YAML
+        self.queries = instance.get('queries', [])
+        self.ssl = instance.get('ssl', {})
+        self.connect_timeout = instance.get('connect_timeout', 10)
+        self.max_custom_queries = instance.get('max_custom_queries', DEFAULT_MAX_CUSTOM_QUERIES)
+
+        if self.queries or 'max_custom_queries' in instance:
+            self.log.warning(
+                'The options `queries` and `max_custom_queries` are deprecated and will be '
+                'removed in a future release. Use the `custom_queries` option instead.'
+            )
+
+        if not (self.host and self.user) and not self.defaults_file:
+            raise ConfigurationError("Mysql host and user are needed.")
