70d759a03b05614700600a1c6252720fce0326a3
Paul Coignet <paul.coignet@datadoghq.com>
2020-07-27 14:56:22 +0200

    Rename filters attribute

--- marklogic/datadog_checks/marklogic/config.py
+++ marklogic/datadog_checks/marklogic/config.py
@@ -5,6 +5,7 @@ import re
 from typing import Any, Dict, List, Optional, Pattern
 
 from datadog_checks.base import ConfigurationError
+from datadog_checks.base.config import _is_affirmative
 
 from .constants import RESOURCE_TYPES
 
@@ -19,14 +20,14 @@ class Config:
         # type: (List[Dict[str, Any]]) -> Dict[str, Dict[str, str]]
         created_filters = {'included': [], 'excluded': []}
         for f in raw_filters:
-            included = f.get('type', 'included') == 'included'
+            included = _is_affirmative(f.get('include', True))
             regex = re.compile(f['pattern'])
-            if f['resource'] not in ['database', 'forest', 'host', 'server']:
+            if f['resource_type'] not in ['database', 'forest', 'host', 'server']:
                 raise ConfigurationError('Unknown resource type: {}'.format(f['resource']))
             if included:
-                created_filters['included'].append(ResourceFilter(f['resource'], regex, True, f.get('group')))
+                created_filters['included'].append(ResourceFilter(f['resource_type'], regex, True, f.get('group')))
             else:
-                created_filters['excluded'].append(ResourceFilter(f['resource'], regex, False, f.get('group')))
+                created_filters['excluded'].append(ResourceFilter(f['resource_type'], regex, False, f.get('group')))
 
         return created_filters
 
@@ -34,16 +35,19 @@ class Config:
 class ResourceFilter:
     """Represents a given resource filter as defined in the conf.yaml file."""
 
-    def __init__(self, resource, regex, is_included=True, group=None):
+    def __init__(self, resource_type, regex, is_included=True, group=None):
         # type: (str, Pattern, bool, Optional[str]) -> None
-        self.resource = RESOURCE_TYPES[resource]['plural'] if RESOURCE_TYPES.get(resource) else resource
+        self.resource_type = (
+            RESOURCE_TYPES[resource_type]['plural'] if RESOURCE_TYPES.get(resource_type) else resource_type
+        )
         self.regex = regex
         self.is_included = is_included
         self.group = group
 
-    def match(self, resource, name, id, group=None):
+    def match(self, resource_type, name, id, group=None):
         # type: (str, str, str, Optional[str]) -> bool
-        return self.resource == resource and self.regex.match(name) and self.group == group
+        return self.resource_type == resource_type and self.regex.match(name) and self.group == group
 
     def __str__(self):
-        return "{} | {} | {} | {}".format(self.resource, self.regex, self.is_included, self.group)
+        # TODO: remove
+        return "{} | {} | {} | {}".format(self.resource_type, self.regex, self.is_included, self.group)
