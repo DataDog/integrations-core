59f2ba3f66648e267eac0a4ca88956e1c88c4ee8
Paul Coignet <paul.coignet@datadoghq.com>
2020-08-14 11:59:15 +0200

    Fix

--- .github/workflows/changelog-label-check.py
+++ .github/workflows/changelog-label-check.py
@@ -1,9 +1,10 @@
 from github import Github
+import os
 import re
 
 
-repo = Github().get_repo(get_env_var('GITHUB_REPOSITORY'))
-pr_number = int(re.search('refs/pull/([0-9]+)/merge', get_env_var('GITHUB_REF')).group(1))
+repo = Github().get_repo(os.environ.get('GITHUB_REPOSITORY'))
+pr_number = int(re.search('refs/pull/([0-9]+)/merge', os.environ.get('GITHUB_REF')).group(1))
 pr_labels = repo.get_pull(pr_number).get_labels()
 
 changelog_labels = list(filter(lambda label: label.name.startswith('changelog'), pr_labels))
