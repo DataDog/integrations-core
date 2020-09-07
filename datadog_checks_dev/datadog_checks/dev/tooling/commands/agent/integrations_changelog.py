8bcf21033764995e7f6545a54d808689603cfad6
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-09-07 13:28:01 +0200

    Update integrations_changelog

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/agent/integrations_changelog.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/agent/integrations_changelog.py
@@ -10,7 +10,7 @@ import click
 
 from ....utils import read_file, write_file, read_file_lines
 from ...constants import get_agent_changelog, get_agent_release_requirements, get_root
-from ...git import git_show_file
+from ...git import git_show_file, git_tag_list
 from ...release import DATADOG_PACKAGE_PREFIX, get_folder_name, get_package_name
 from ...utils import parse_agent_req_file, get_valid_checks
 from ..console import CONTEXT_SETTINGS, abort, echo_info
@@ -19,26 +19,19 @@ from .common import get_agent_tags
 
 @click.command(
     context_settings=CONTEXT_SETTINGS,
-    short_help="Provide a list of updated checks on a given Datadog Agent version, in changelog form",
+    short_help="Update integration change logs with first Agent version containing each integration release",
 )
-@click.option('--since', help="Initial Agent version", default='6.3.0')
-@click.option('--to', help="Final Agent version")
 @click.option(
     '--write', '-w', is_flag=True, help="Write to the changelog file, if omitted contents will be printed to stdout"
 )
-@click.option('--force', '-f', is_flag=True, default=False, help="Replace an existing file")
-def integrations_changelog(since, to, write, force):
+def integrations_changelog(write):
     """
-    Generates a markdown file containing the list of checks that changed for a
-    given Agent release. Agent version numbers are derived inspecting tags on
-    `integrations-core` so running this tool might provide unexpected results
-    if the repo is not up to date with the Agent release process.
-
-    If neither `--since` or `--to` are passed (the most common use case), the
-    tool will generate the whole changelog since Agent version 6.3.0
-    (before that point we don't have enough information to build the log).
+    Update integration change logs with first Agent version containing each integration release
     """
     checks = sorted(get_valid_checks())
+    # store the changelog in memory
+    changelog_contents = StringIO()
+
     for check in checks:
         changelog_file = get_integration_changelog(check)
         print("changelog_file:", changelog_file)
@@ -46,91 +39,26 @@ def integrations_changelog(since, to, write, force):
             match = re.search(r'## (\d\.\d\.\d) / \d{4}-\d{2}-\d{2}', line)
             if match:
                 version = match.groups()[0]
+                tag = "{}-{}".format(check, version)
                 print("version", version)
-        break
+                print("tag", tag)
 
-    return
-    agent_tags = get_agent_tags(since, to)
+                tag_list = sorted(git_tag_list(pattern=r"^\d+\.\d+\.\d+$", contains=tag))
+                if tag_list:
+                    first_agent_version = tag_list[0]
+                    print("tag_list", tag_list)
+                    print("first_agent_version", first_agent_version)
+                    line = "{} / Agent {}\n".format(line.strip(), first_agent_version)
+                    print("line:", line)
+            changelog_contents.write(line)
 
-    # store the changes in a mapping {agent_version --> {check_name --> current_version}}
-    changes_per_agent = {}
-
-    # to keep indexing easy, we run the loop off-by-one
-    for i in range(1, len(agent_tags)):
-        req_file_name = os.path.basename(get_agent_release_requirements())
-        current_tag = agent_tags[i - 1]
-        # Requirements for current tag
-        file_contents = git_show_file(req_file_name, current_tag)
-        catalog_now = parse_agent_req_file(file_contents)
-        # Requirements for previous tag
-        file_contents = git_show_file(req_file_name, agent_tags[i])
-        catalog_prev = parse_agent_req_file(file_contents)
-
-        changes_per_agent[current_tag] = {}
-
-        for name, ver in catalog_now.items():
-            # at some point in the git history, the requirements file erroneusly
-            # contained the folder name instead of the package name for each check,
-            # let's be resilient
-            old_ver = (
-                catalog_prev.get(name)
-                or catalog_prev.get(get_folder_name(name))
-                or catalog_prev.get(get_package_name(name))
-            )
-
-            # normalize the package name to the check_name
-            if name.startswith(DATADOG_PACKAGE_PREFIX):
-                name = get_folder_name(name)
-
-            if old_ver and old_ver != ver:
-                # determine whether major version changed
-                breaking = old_ver.split('.')[0] < ver.split('.')[0]
-                changes_per_agent[current_tag][name] = (ver, breaking)
-            elif not old_ver:
-                # New integration
-                changes_per_agent[current_tag][name] = (ver, False)
-
-    # store the changelog in memory
-    changelog_contents = StringIO()
-
-    # prepare the links
-    agent_changelog_url = 'https://github.com/DataDog/datadog-agent/blob/master/CHANGELOG.rst#{}'
-    check_changelog_url = 'https://github.com/DataDog/integrations-core/blob/master/{}/CHANGELOG.md'
-
-    # go through all the agent releases
-    for agent, version_changes in changes_per_agent.items():
-        url = agent_changelog_url.format(agent.replace('.', ''))  # Github removes dots from the anchor
-        changelog_contents.write(f'## Datadog Agent version [{agent}]({url})\n\n')
-
-        if not version_changes:
-            changelog_contents.write('* There were no integration updates for this version of the Agent.\n\n')
+        # save the changelog on disk if --write was passed
+        if write:
+            write_file(changelog_file, changelog_contents.getvalue())
         else:
-            for name, ver in version_changes.items():
-                # get the "display name" for the check
-                manifest_file = os.path.join(get_root(), name, 'manifest.json')
-                if os.path.exists(manifest_file):
-                    decoded = json.loads(read_file(manifest_file).strip())
-                    display_name = decoded.get('display_name')
-                else:
-                    display_name = name
+            echo_info(changelog_contents.getvalue())
 
-                breaking_notice = " **BREAKING CHANGE**" if ver[1] else ""
-                changelog_url = check_changelog_url.format(name)
-                changelog_contents.write(f'* {display_name} [{ver[0]}]({changelog_url}){breaking_notice}\n')
-            # add an extra line to separate the release block
-            changelog_contents.write('\n')
-
-    # save the changelog on disk if --write was passed
-    if write:
-        dest = get_agent_changelog()
-        # don't overwrite an existing file
-        if os.path.exists(dest) and not force:
-            msg = "Output file {} already exists, run the command again with --force to overwrite"
-            abort(msg.format(dest))
-
-        write_file(dest, changelog_contents.getvalue())
-    else:
-        echo_info(changelog_contents.getvalue())
+        break  # TODO: remove me
 
 
 def get_integration_changelog(check):
