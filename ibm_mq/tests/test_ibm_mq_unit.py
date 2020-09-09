6d2917b6c33bc116accf8215a82caf1fdc77857b
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-09-09 18:10:18 +0200

    Add test

--- ibm_mq/tests/test_ibm_mq_unit.py
+++ ibm_mq/tests/test_ibm_mq_unit.py
@@ -1,13 +1,14 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-
+import mock
 import pytest
 from six import iteritems
 
 from datadog_checks.base import AgentCheck, ConfigurationError
 from datadog_checks.ibm_mq import IbmMqCheck
 from datadog_checks.ibm_mq.config import IBMMQConfig
+from datadog_checks.ibm_mq.connection import get_queue_manager_connection
 
 pytestmark = pytest.mark.unit
 
@@ -195,3 +196,45 @@ def test_ssl_connection_creation(instance):
     assert 'bytes' not in warning
     assert 'unicode' not in warning
     assert 'MQRC_KEY_REPOSITORY_ERROR' in warning
+
+
+def test_ssl_check_normal_connection_before_ssl_connection(instance):
+    import pymqi
+
+    instance['ssl_auth'] = 'yes'
+    instance['ssl_cipher_spec'] = 'TLS_RSA_WITH_AES_256_CBC_SHA256'
+    instance['ssl_key_repository_location'] = '/dummy'
+    config = IBMMQConfig(instance)
+
+    # normal connection failed with with MQRC_HOST_NOT_AVAILABLE
+    error = pymqi.MQMIError(pymqi.CMQC.MQCC_FAILED, pymqi.CMQC.MQRC_HOST_NOT_AVAILABLE)
+    with mock.patch(
+        'datadog_checks.ibm_mq.connection.get_normal_connection', side_effect=error
+    ) as get_normal_connection, mock.patch('datadog_checks.ibm_mq.connection.get_ssl_connection') as get_ssl_connection:
+
+        with pytest.raises(pymqi.MQMIError):
+            get_queue_manager_connection(config)
+
+        get_normal_connection.assert_called_with(config)
+        assert not get_ssl_connection.called
+
+    # normal connection failed with with error other than MQRC_HOST_NOT_AVAILABLE
+    error = pymqi.MQMIError(pymqi.CMQC.MQCC_FAILED, pymqi.CMQC.MQRC_SSL_CONFIG_ERROR)
+    with mock.patch(
+        'datadog_checks.ibm_mq.connection.get_normal_connection', side_effect=error
+    ) as get_normal_connection, mock.patch('datadog_checks.ibm_mq.connection.get_ssl_connection') as get_ssl_connection:
+
+        get_queue_manager_connection(config)
+
+        get_normal_connection.assert_called_with(config)
+        get_ssl_connection.assert_called_with(config)
+
+    # no issue with normal connect
+    with mock.patch('datadog_checks.ibm_mq.connection.get_normal_connection') as get_normal_connection, mock.patch(
+        'datadog_checks.ibm_mq.connection.get_ssl_connection'
+    ) as get_ssl_connection:
+
+        get_queue_manager_connection(config)
+
+        get_normal_connection.assert_called_with(config)
+        get_ssl_connection.assert_called_with(config)
