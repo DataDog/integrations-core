6e1e3739713b82d6e1a5de7ac0117a285647e67f
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2020-09-09 16:43:53 -0400

    Use f string instead of .format

--- datadog_checks_dev/datadog_checks/dev/tooling/create.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/create.py
@@ -32,19 +32,22 @@ def construct_template_fields(integration_name, repo_choice, **kwargs):
     check_name_kebab = re.sub('_| ', '-', integration_name.lower())
 
     datadog_checks_base_req = 'datadog-checks-base[deps]>=6.6.0'
-    third_party_install_info = (
-        'To install the {integration_name} check on your host:\n\n'
-        '1. Install the [developer toolkit]'
-        '(https://docs.datadoghq.com/developers/integrations/new_check_howto/#developer-toolkit)'
-        ' on any machine.\n'
-        '2. Run `ddev release build {normalized_integration_name}` to build the package.\n'
-        '3. [Download the Datadog Agent](https://app.datadoghq.com/account/settings#agent).\n'
-        '4. Upload the build artifact to any host with an Agent and'
-        ' run `datadog-agent integration install -w'
-        ' path/to/{normalized_integration_name}/dist/<ARTIFACT_NAME>.whl`.'.format(
-            integration_name=integration_name, normalized_integration_name=normalized_integration_name
-        )
-    )
+    third_party_install_info = f"""\
+To install the {integration_name} check on your host:
+
+
+1. Install the [developer toolkit]
+(https://docs.datadoghq.com/developers/integrations/new_check_howto/#developer-toolkit)
+ on any machine.
+
+2. Run `ddev release build {normalized_integration_name}` to build the package.
+
+3. [Download the Datadog Agent](https://app.datadoghq.com/account/settings#agent).
+
+4. Upload the build artifact to any host with an Agent and
+ run `datadog-agent integration install -w
+ path/to/{normalized_integration_name}/dist/<ARTIFACT_NAME>.whl`.
+"""
 
     if repo_choice == 'core':
         check_name = normalized_integration_name
