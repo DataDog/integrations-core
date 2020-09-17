1141123070712a17b72e18592eb04152777312ee
Greg Marabout-Demazure <greg.maraboutdemazure@datadoghq.com>
2020-09-17 08:52:26 +0200

    Add option to disable datastore cluster name as vsphere folder tag

--- vsphere/tests/test_check.py
+++ vsphere/tests/test_check.py
@@ -53,6 +53,23 @@ def test_historical_metrics(aggregator, dd_run_check, historical_instance):
     aggregator.assert_all_metrics_covered()
 
 
+@pytest.mark.usefixtures("mock_type", "mock_threadpool", "mock_api")
+def test_historical_metrics_no_dsc_folder(aggregator, dd_run_check, historical_instance):
+    """This test does the same check than test_historical_events, but deactivate the option to get datastore cluster
+    folder in metrics tags"""
+    check = VSphereCheck('vsphere', {}, [historical_instance])
+    check.config.include_datastore_cluster_folder_tag = False
+    dd_run_check(check)
+
+    fixture_file = os.path.join(HERE, 'fixtures', 'metrics_historical_values_no_dsc_folder.json')
+    with open(fixture_file, 'r') as f:
+        data = json.load(f)
+        for metric in data:
+            aggregator.assert_metric(metric['name'], metric.get('value'), tags=metric.get('tags'))
+
+    aggregator.assert_all_metrics_covered()
+
+
 @pytest.mark.usefixtures('mock_type', 'mock_threadpool', 'mock_api', 'mock_rest_api')
 def test_events_only(aggregator, events_only_instance):
     check = VSphereCheck('vsphere', {}, [events_only_instance])
