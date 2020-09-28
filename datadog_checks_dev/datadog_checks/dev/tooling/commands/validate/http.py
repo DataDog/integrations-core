d2c053fe97eda1c51a7e75e9ede930805fdf4e52
Andrew Zhang <andrew.zhang@Andrews-MacBook-Pro.local>
2020-09-28 11:56:31 -0400

    cleaning up import statements and optimizing code

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/http.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/http.py
@@ -1,27 +1,47 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-import click
-from datadog_checks.dev.tooling.utils import get_valid_integrations, get_check_files
+import os
 
-from ..console import echo_success, abort, CONTEXT_SETTINGS, echo_info, echo_debug, echo_warning, echo_failure
+import click
+from datadog_checks.dev.tooling.utils import get_valid_integrations, get_check_files, get_config_spec
+
+from ..console import CONTEXT_SETTINGS, echo_info, echo_warning
 
 REQUEST_LIBRARY_FUNCTIONS = {
-    'request.get',
-    'request.post',
-    'request.head',
-    'request.put',
-    'request.patch',
-    'request.delete'
+    'requests.get',
+    'requests.post',
+    'requests.head',
+    'requests.put',
+    'requests.patch',
+    'requests.delete'
 }
 
 
-def validate_use_http_wrapper(file, check):
-    for http_func in REQUEST_LIBRARY_FUNCTIONS:
+def validate_config_http(file, check):
+    config_valid = True
+    if os.path.isfile(file):
         with open(file, "r") as f:
-            if http_func in f.read():
-                echo_warning(f'Detected {http_func} in {check}\' {file}, '
-                             f'please make sure to use http wrapper class instead of request library')
+            if 'instances/http' not in f.read():
+                echo_warning(f'{check}\'s spec.yaml file does not contain `instances/http` '
+                             f'but {check} uses http wrapper')
+
+    return config_valid
+
+
+def validate_use_http_wrapper(file, check):
+    uses_http_wrapper = False
+
+    with open(file, "r") as f:
+        for num, line in enumerate(f):
+            if 'self.http' in line:
+                uses_http_wrapper = True
+
+            for http_func in REQUEST_LIBRARY_FUNCTIONS:
+                if http_func in line:
+                    echo_warning(f'Detected `{http_func}` on line {num} in {check}\'s {file}, '
+                                 f'please make sure to use http wrapper class instead of request library')
+    return uses_http_wrapper
 
 
 @click.command('http', context_settings=CONTEXT_SETTINGS, short_help='Validate usage of http wrapper')
@@ -29,12 +49,14 @@ def http():
     """Validate all integrations for usage of http wrapper."""
     echo_info("Validating all integrations for usage of http wrapper...")
 
-    validate_use_http_wrapper('/Users/andrew.zhang/integrations-core/aerospike/datadog_checks/aerospike/aerospike.py',
-                              'aerospike')
+    for check in sorted(get_valid_integrations()):
+        uses_http_wrapper = False
+        # validate use of http wrapper (self.http.[...]) in check's .py files
+        for file in get_check_files(check, include_tests=False):
+            uses_http_wrapper = validate_use_http_wrapper(file, check)
 
-    # for check_name in sorted(get_valid_integrations()):
-        # for file in get_check_files(check_name, include_tests=False):
-        #     validate_use_http_wrapper(file, check_name)
-        # also validate if config has http
+        # validate use of `instances/http` in check's config files
+        if uses_http_wrapper:
+            validate_config_http(get_config_spec(check), check)
 
-    echo_success(f"ur smart")
+    echo_info('Completed http validation!')
