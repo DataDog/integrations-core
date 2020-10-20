b17839c58a9f8b7e954b405a47af73391a9403ee
Andrew Zhang <andrew.zhang@Andrews-MacBook-Pro.local>
2020-10-20 11:26:44 -0400

    Modify tox and conftest files to use multiple docker compose files

--- sqlserver/tests/conftest.py
+++ sqlserver/tests/conftest.py
@@ -68,11 +68,7 @@ def dd_environment():
         pyodbc.connect(conn, timeout=30)
 
     with docker_run(
-        # docker-compose for non-HA SQL instance
-        # compose_file=os.path.join(HERE, 'compose', 'docker-compose.yaml'),
-
-        # docker-compose for HA SQL instance
-        compose_file=os.path.join(HERE, 'compose-ha', 'docker-compose.yaml'),
+        compose_file=os.path.join(HERE, os.environ["COMPOSE_FOLDER"], 'docker-compose.yaml'),
         conditions=[WaitFor(sqlserver, wait=3, attempts=10)],
         mount_logs=True,
     ):
