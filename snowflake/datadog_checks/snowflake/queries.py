a63d12531b8c65bc2d0aa9f642bb006601332952
ChristineTChen <christine.chen@datadoghq.com>
2020-10-21 18:42:44 -0400

    Revert "Add bytes_spilled metrics (#7810)"
    This reverts commit 440809cc2a14b5b14fca0841331a7dd7187040a9.

--- snowflake/datadog_checks/snowflake/queries.py
+++ snowflake/datadog_checks/snowflake/queries.py
@@ -108,8 +108,7 @@ QueryHistory = {
     'name': 'warehouse_load.metrics',
     'query': (
         'select QUERY_TYPE, WAREHOUSE_NAME, DATABASE_NAME, SCHEMA_NAME, AVG(EXECUTION_TIME), '
-        'AVG(COMPILATION_TIME), AVG(BYTES_SCANNED), AVG(BYTES_WRITTEN), AVG(BYTES_DELETED), '
-        'AVG(BYTES_SPILLED_TO_LOCAL_STORAGE), AVG(BYTES_SPILLED_TO_REMOTE_STORAGE) '
+        'AVG(COMPILATION_TIME), AVG(BYTES_SCANNED), AVG(BYTES_WRITTEN), AVG(BYTES_DELETED) '
         'from QUERY_HISTORY where start_time >= date_trunc(day, current_date) '
         'group by 1, 2, 3, 4;'
     ),
@@ -123,8 +122,6 @@ QueryHistory = {
         {'name': 'query.bytes_scanned', 'type': 'gauge'},
         {'name': 'query.bytes_written', 'type': 'gauge'},
         {'name': 'query.bytes_deleted', 'type': 'gauge'},
-        {'name': 'query.bytes_spilled.local', 'type': 'gauge'},
-        {'name': 'query.bytes_spilled.remote', 'type': 'gauge'},
     ],
 }
 
