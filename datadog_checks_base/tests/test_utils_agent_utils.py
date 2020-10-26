9c9fb92a5adeeb1a33d51f3ca35eb01c2ef9871f
Albert Vaca Cintora <albert.vaca@datadoghq.com>
2020-10-26 19:34:37 +0100

    Tracemalloc: Rename white/blacklist to include/exclude (#7626)
    Rename, keeping compatibility with the old names.
    Log a warning if both options are set
    
    Needs corresponding changes in datadog-agent.

--- datadog_checks_base/tests/test_utils_agent_utils.py
+++ datadog_checks_base/tests/test_utils_agent_utils.py
@@ -12,6 +12,47 @@ class TestShouldProfileMemory:
         with mock.patch('datadog_checks.base.stubs.datadog_agent.get_config', return_value=''):
             assert should_profile_memory(stubs.datadog_agent, 'test') is True
 
+    def test_include_enable(self):
+        # Keep a reference for use during mock
+        get_config = stubs.datadog_agent.get_config
+
+        def mock_get_config(key):
+            if key == 'tracemalloc_include':
+                return 'test'
+
+            return get_config(key)
+
+        with mock.patch('datadog_checks.base.stubs.datadog_agent.get_config', side_effect=mock_get_config):
+            assert should_profile_memory(stubs.datadog_agent, 'test') is True
+
+    def test_include_disable(self):
+        # Keep a reference for use during mock
+        get_config = stubs.datadog_agent.get_config
+
+        def mock_get_config(key):
+            if key == 'tracemalloc_include':
+                return 'test1,test2'
+
+            return get_config(key)
+
+        with mock.patch('datadog_checks.base.stubs.datadog_agent.get_config', side_effect=mock_get_config):
+            assert should_profile_memory(stubs.datadog_agent, 'test') is False
+
+    def test_include_exclude(self):
+        # Keep a reference for use during mock
+        get_config = stubs.datadog_agent.get_config
+
+        def mock_get_config(key):
+            if key == 'tracemalloc_include':
+                return 'test'
+            elif key == 'tracemalloc_exclude':
+                return 'test'
+
+            return get_config(key)
+
+        with mock.patch('datadog_checks.base.stubs.datadog_agent.get_config', side_effect=mock_get_config):
+            assert should_profile_memory(stubs.datadog_agent, 'test') is False
+
     def test_whitelist_enable(self):
         # Keep a reference for use during mock
         get_config = stubs.datadog_agent.get_config
