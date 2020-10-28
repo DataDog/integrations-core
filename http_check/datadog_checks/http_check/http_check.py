fc99176822476dc5ba79b464d8a2da38df342aa1
Fanny Jiang <jiangf@bu.edu>
2020-10-28 16:22:04 -0400

    remove changes to http_check

--- http_check/datadog_checks/http_check/http_check.py
+++ http_check/datadog_checks/http_check/http_check.py
@@ -30,7 +30,7 @@ DEFAULT_EXPIRE_WARNING = DEFAULT_EXPIRE_DAYS_WARNING * 24 * 3600
 DEFAULT_EXPIRE_CRITICAL = DEFAULT_EXPIRE_DAYS_CRITICAL * 24 * 3600
 MESSAGE_LENGTH = 2500  # https://docs.datadoghq.com/api/v1/service-checks/
 
-DATA_METHODS = ['POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
+DATA_METHODS = ['POST', 'PUT', 'DELETE', 'PATCH']
 
 
 class HTTPCheck(AgentCheck):
@@ -117,8 +117,6 @@ class HTTPCheck(AgentCheck):
             if method.upper() in DATA_METHODS and not headers.get('Content-Type'):
                 self.http.options['headers']['Content-Type'] = 'application/x-www-form-urlencoded'
 
-            if method.upper() == 'OPTIONS':
-                method = 'options_method'
 
             r = getattr(self.http, method.lower())(
                 addr,
