e74a1534ec0036459b8a02abe99e5936a000ce5f
ChristineTChen <christine.chen@datadoghq.com>
2020-11-03 16:49:18 -0500

    Add tests

--- elastic/tests/test_metrics.py
+++ elastic/tests/test_metrics.py
@@ -28,6 +28,19 @@ from datadog_checks.elastic.metrics import (
         pytest.param([5, 0, 0], False, 180, id='v5'),
         pytest.param([6, 3, 0], False, 180, id='v6.3.0'),
         pytest.param([7, 2, 0], False, 177, id='v7.2.0'),
+        pytest.param([0, 90, 0], True, 133, id='v0.90'),
+        pytest.param([0, 90, 5], True, 134, id='v0.90.5'),
+        pytest.param([0, 90, 10], True, 136, id='v0.90.10'),
+        pytest.param([1, 0, 0], True, 144, id='jmx-rate-v1'),
+        pytest.param([1, 3, 0], True, 146, id='jmx-rate-v1.3.0'),
+        pytest.param([1, 4, 0], True, 166, id='jmx-rate-v1.4.0'),
+        pytest.param([1, 5, 0], True, 169, id='jmx-rate-v1.5.0'),
+        pytest.param([1, 6, 0], True, 177, id='jmx-rate-v1.6.0'),
+        pytest.param([2, 0, 0], True, 176, id='jmx-rate-v2.0.0'),
+        pytest.param([2, 1, 0], True, 181, id='jmx-rate-v2.1.0'),
+        pytest.param([5, 0, 0], True, 184, id='jmx-rate-v5'),
+        pytest.param([6, 3, 0], True, 184, id='jmx-rate-v6.3.0'),
+        pytest.param([7, 2, 0], True, 181, id='jmx-rate-v7.2.0'),
     ],
 )
 def test_stats_for_version(version, jvm_rate, expected_metric_count):
@@ -56,7 +69,7 @@ def test_stats_for_version(version, jvm_rate, expected_metric_count):
 )
 def test_pshard_stats_for_version(version, expected_metric_count):
     pshard_metrics = pshard_stats_for_version(version)
-    assert len(pshard_metrics) ==expected_metric_count
+    assert len(pshard_metrics) == expected_metric_count
 
 
 @pytest.mark.unit
