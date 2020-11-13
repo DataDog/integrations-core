54e4348e72b68456c41ece7e88fc34df04be7981
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-11-13 13:24:57 +0100

    Do not use instance

--- ibm_was/datadog_checks/ibm_was/ibm_was.py
+++ ibm_was/datadog_checks/ibm_was/ibm_was.py
@@ -7,7 +7,7 @@ import requests
 from lxml import etree
 from six import iteritems
 
-from datadog_checks.base import AgentCheck, ensure_unicode, is_affirmative
+from datadog_checks.base import AgentCheck, ensure_unicode, is_affirmative, ConfigurationError
 
 from . import metrics, validation
 
@@ -27,26 +27,28 @@ class IbmWasCheck(AgentCheck):
             'RangeStatistic': self.gauge,
             'TimeStatistic': self.gauge,
         }
-
-    def check(self, instance):
-        validation.validate_config(instance)
-        collect_stats = self.setup_configured_stats(instance)
-        url = instance.get('servlet_url')
-        self.custom_queries_units_gauge = set(instance.get('custom_queries_units_gauge', []))
-
-        nested_tags, metric_categories = self.append_custom_queries(instance, collect_stats)
+        self.url = self.instance.get('servlet_url')
+        self.custom_queries = set(self.instance.get('custom_queries', []))
+        self.custom_queries_units_gauge = set(self.instance.get('custom_queries_units_gauge', []))
+        self.custom_tags = set(self.instance.get('tags', []))
+        collect_stats = self.setup_configured_stats(self.instance)
+        nested_tags, metric_categories = self.append_custom_queries(collect_stats)
         self.custom_stats = list(nested_tags)
-        custom_tags = instance.get('tags', [])
+        self.service_check_tags = self.custom_tags.union(set('url:{}'.format(self.url)))
 
-        service_check_tags = list(custom_tags)
-        service_check_tags.append('url:{}'.format(url))
+        self.check_initializations.append(self._validate_config)
 
-        data = self.make_request(instance, url, service_check_tags)
+    def _validate_config(self):
+        if not self.instance.get('servlet_url'):
+            raise ConfigurationError("Please specify a servlet_url in the configuration file")
+
+    def check(self, _):
+        data = self.make_request()
 
         try:
             server_data_xml = etree.fromstring(data)
         except ParseError as e:
-            self.submit_service_checks(service_check_tags, AgentCheck.CRITICAL)
+            self.submit_service_checks(AgentCheck.CRITICAL)
             self.log.Error("Unable to parse the XML response: {}".format(e))
             return
 
@@ -54,7 +56,7 @@ class IbmWasCheck(AgentCheck):
 
         for node in node_list:
             server_list = self.get_node_from_root(node, 'Server')
-            node_tags = list(custom_tags)
+            node_tags = list(self.custom_tags)
 
             node_tags.append('node:{}'.format(node.get('name')))
             for server in server_list:
@@ -117,28 +119,27 @@ class IbmWasCheck(AgentCheck):
             jvm_metric_name = "{}_gauge".format(metric_name)
             self.gauge(jvm_metric_name, value, tags=tags)
 
-    def make_request(self, instance, url, tags):
+    def make_request(self):
         try:
-            resp = self.http.get(url)
+            resp = self.http.get(self.url)
             resp.raise_for_status()
-            self.submit_service_checks(tags, AgentCheck.OK)
+            self.submit_service_checks(AgentCheck.OK)
         except (requests.HTTPError, requests.ConnectionError) as e:
             self.warning(
                 "Couldn't connect to URL: %s with exception: %s. Please verify the address is reachable", url, e
             )
-            self.submit_service_checks(tags, AgentCheck.CRITICAL)
+            self.submit_service_checks(AgentCheck.CRITICAL)
             raise e
         return resp.content
 
-    def submit_service_checks(self, tags, value):
-        self.gauge(self.SERVICE_CHECK_CONNECT, 1 if value == AgentCheck.OK else 0, tags=tags)
-        self.service_check(self.SERVICE_CHECK_CONNECT, value, tags=tags)
+    def submit_service_checks(self, value):
+        self.gauge(self.SERVICE_CHECK_CONNECT, 1 if value == AgentCheck.OK else 0, tags=list(self.service_check_tags))
+        self.service_check(self.SERVICE_CHECK_CONNECT, value, tags=list(self.service_check_tags))
 
-    def append_custom_queries(self, instance, collect_stats):
+    def append_custom_queries(self, collect_stats):
         custom_recursion_tags = {}
         custom_metric_categories = {}
-        custom_queries = instance.get('custom_queries', [])
-        for query in custom_queries:
+        for query in self.custom_queries:
             validation.validate_query(query)
             custom_metric_categories[query['stat']] = query['metric_prefix']
             custom_recursion_tags[query['metric_prefix']] = [key for key in query.get('tag_keys', [])]
@@ -148,9 +149,9 @@ class IbmWasCheck(AgentCheck):
             dict(metrics.METRIC_CATEGORIES, **custom_metric_categories),
         )
 
-    def setup_configured_stats(self, instance):
+    def setup_configured_stats(self):
         collect_stats = {}
         for category, prefix in iteritems(metrics.METRIC_CATEGORIES):
-            if is_affirmative(instance.get('collect_{}_stats'.format(prefix), True)):
+            if is_affirmative(self.instance.get('collect_{}_stats'.format(prefix), True)):
                 collect_stats[category] = True
         return collect_stats
