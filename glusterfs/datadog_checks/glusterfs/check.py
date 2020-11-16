46d64ca78ea237307ccd39532dba638cce570422
ChristineTChen <christine.chen@datadoghq.com>
2020-11-16 18:46:46 -0500

    Add volume summary metrics

--- glusterfs/datadog_checks/glusterfs/check.py
+++ glusterfs/datadog_checks/glusterfs/check.py
@@ -8,15 +8,49 @@ from datadog_checks.base import AgentCheck
 from datadog_checks.base.utils.subprocess_output import get_subprocess_output
 
 CLUSTER_STATS = {
-    'node_count': 'cluster.node.count',
+    'node_count': 'cluster.nodes.count',
     'nodes_active': 'cluster.nodes.active',
     'volumes_started': 'volumes.started',
     'volume_count': 'volumes.count'
 }
 
-VOLUME_STATS = {
+
+GENERAL_STATS = {
+    'size_used': 'size.used',
+    'inodes_free': 'inodes.free',
+    'size_free': 'size.free',
+    'size_total': 'size.total',
+    'inodes_used': 'inodes.used',
+    'inodes_total': 'inodes.total',
+    'online': 'online',
 
 }
+
+VOLUME_STATS = {
+    'v_used_percent': 'used.percent',
+    'num_bricks': 'bricks.count',
+    'distribute': 'distribute',
+    'v_size_used': 'size.used', # strip GiB from value
+    'v_size': 'size.total', # strip GiB from value
+    'snapshot_count': 'snapshot.count',
+}
+
+VOL_SUBVOL_STATS = {
+    'disperse': 'disperse',
+    'disperse_redundancy': 'disperse_redundancy',
+    'replica': 'replica',
+}
+
+VOLUME_STATS.update(GENERAL_STATS)
+VOLUME_STATS.update(VOL_SUBVOL_STATS)
+
+
+BRICK_STATS = {
+    'block_size': 'block_size'
+}
+
+BRICK_STATS.update(GENERAL_STATS)
+
 
 GLUSTER_VERSION = 'glfs_version'
 CLUSTER_STATUS = 'cluster_status'
@@ -24,9 +58,12 @@ CLUSTER_STATUS = 'cluster_status'
 class GlusterfsCheck(AgentCheck):
     __NAMESPACE__ = 'glusterfs'
 
-    def check(self, _):
-        # type: (Any) -> None
+    def __init__(self, *args, **kwargs):
+        # type: (*Any, **Any) -> None
+        super(GlusterfsCheck, self).__init__(*args, **kwargs)
         self._tags = self.instance.get('tags', [])
+
+    def check(self, _):
         output, _, _ = get_subprocess_output(['gstatus', '-a', '-o', 'json'], self.log)
         gstatus = json.loads(output)
         if 'data' in gstatus:
@@ -34,11 +71,20 @@ class GlusterfsCheck(AgentCheck):
             for key, metric in CLUSTER_STATS.items():
                 if key in data:
                     self.gauge(metric, data[key], self._tags)
-            self.parse_volume_summary(data)
+
+            if 'volume_summary' in output:
+                self.parse_volume_summary(data['volume_summary'])
         else:
             self.log.warning("No data from gstatus")
 
     def parse_volume_summary(self, output):
-        if 'volume_summary' in output:
-            volume_summary = output['volume_summary']
+        for volume in output:
+            volume_tags = ["vol_name:{}".format(volume['name']), "vol_type:{}".format(volume['type'])]
+            volume_tags.extend(self._tags)
+            for key, metric in VOLUME_STATS.items():
+                if key in volume:
+                    self.gauge(metric, volume[key], volume_tags)
+                else:
+                    self.log.debug("Field not found in volume data: %s", key)
+
 
