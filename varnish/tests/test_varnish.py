4b1eabc4bd466ec9b56a3ef3f54e87735752d21f
Julia Simon <611228+hithwen@users.noreply.github.com>
2020-11-24 12:11:09 +0100

    Still exclude metrics not in metadata.csv

--- varnish/tests/test_varnish.py
+++ varnish/tests/test_varnish.py
@@ -31,7 +31,8 @@ def test_check(aggregator, check, instance):
         aggregator.assert_metric(mname, count=1, tags=['cluster:webs', 'varnish_name:default'])
 
     metadata_metrics = get_metadata_metrics()
-    aggregator.assert_metrics_using_metadata(metadata_metrics, check_metric_type=False)
+    exclude = [m for m in aggregator.metric_names if m not in metadata_metrics]
+    aggregator.assert_metrics_using_metadata(metadata_metrics, exclude=exclude, check_metric_type=False)
 
 
 def test_inclusion_filter(aggregator, check, instance):
