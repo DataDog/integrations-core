b1636982308279ebf8172044113ce1686f6908e3
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-11-27 22:45:43 +0100

    Add assert_python_vs_core

--- snmp/tests/test_core_e2e.py
+++ snmp/tests/test_core_e2e.py
@@ -18,68 +18,30 @@ SUPPORTED_METRIC_TYPES = [
 
 
 def test_e2e_metric_types(dd_agent_check):
-    python_instance = common.generate_container_instance_config(SUPPORTED_METRIC_TYPES)
-    python_instance['loader'] = 'python'
-    core_instance = python_instance.copy()
-    core_instance['loader'] = 'core'
+    instance = common.generate_container_instance_config(SUPPORTED_METRIC_TYPES)
+    assert_python_vs_core(dd_agent_check, instance)
 
+
+def assert_python_vs_core(dd_agent_check, instance):
+    python_instance = instance.copy()
+    python_instance['loader'] = 'python'
+    core_instance = instance.copy()
+    core_instance['loader'] = 'core'
     print(">>> python instance:", python_instance)
     print(">>> core instance  :", core_instance)
-
     aggregator = dd_agent_check(python_instance, rate=True)
-
     expected_metrics = defaultdict(int)
     print(">>> python metrics:")
     for metric_name, metrics in aggregator._metrics.items():
         print("{}: {}".format(metric_name, metrics))
         for metric in metrics:
             expected_metrics[(metric.name, metric.type, tuple(sorted(metric.tags)))] += 1
-
     aggregator.reset()
     aggregator = dd_agent_check(core_instance, rate=True)
-
     print(">>> core metrics:")
     for metric_name, metrics in aggregator._metrics.items():
         print("{}: {}".format(metric_name, metrics))
-
     print(">>> expected metrics:")
     for (name, mtype, tags), count in expected_metrics.items():
         print("metric:", name, mtype, tags, count)
         aggregator.assert_metric(name, metric_type=mtype, tags=tags, count=count)
-
-
-# def test_e2e_profile(dd_agent_check):
-#     python_instance = common.generate_container_profile_instance_config('f5-big-ip')
-#     python_instance['loader'] = 'python'
-#     core_instance = python_instance.copy()
-#     core_instance['loader'] = 'core'
-#
-#     print(">>> python instance:", python_instance)
-#     print(">>> core instance:", core_instance)
-#
-#     aggregator = dd_agent_check(python_instance, rate=True)
-#
-#     expected_metrics = defaultdict(int)
-#     print(">>> python metrics:")
-#     for metric_name, metrics in aggregator._metrics.items():
-#         print("metric_name", metric_name)
-#         for metric in metrics:
-#             expected_metrics[(metric.name, metric.type, tuple(sorted(metric.tags)))] += 1
-#
-#     aggregator.reset()
-#     aggregator = dd_agent_check(core_instance, rate=True)
-#
-#     print(">>> core metrics:")
-#     for metric_name, metrics in aggregator._metrics.items():
-#         print("metric_name", metric_name)
-#
-#     for (name, mtype, tags), count in expected_metrics.items():
-#         print("metric:", name, mtype, tags, count)
-#         aggregator.assert_metric(name, metric_type=mtype, tags=tags, count=count)
-#
-#     # Test service check
-#     # aggregator.assert_service_check("snmp.can_check", status=SnmpCheck.OK, tags=tags, at_least=1)  # TODO: implement me
-#
-#     # common.assert_common_metrics(aggregator)
-#     # aggregator.all_metrics_asserted()
-#
