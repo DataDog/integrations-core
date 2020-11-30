75f4b5331e08afff10422406440bd208cf213d80
ChristineTChen <christine.chen@datadoghq.com>
2020-11-30 15:00:51 -0500

    Fix message

--- snowflake/datadog_checks/snowflake/check.py
+++ snowflake/datadog_checks/snowflake/check.py
@@ -156,10 +156,11 @@ class SnowflakeCheck(AgentCheck):
             try:
                 return method(*args, **kwargs)
             except Exception as e:
+                msg = "Encountered error while attempting to connect to Snowflake "
                 if proxies:
-                    self.log.error(
-                        "Encountered error while attempting to connect to Snowflake via proxy settings: %s", str(e)
-                    )
+                    self.log.error("%s via proxy settings: %s", msg, str(e))
+                else:
+                    self.log.error("%s: %s", msg, str(e))
                 return
 
         return _request_exec
