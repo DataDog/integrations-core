d745b32b08b4bf2448c14af0b0728b3b1ed9f397
Ian Bucad <ian.bucad@datadoghq.com>
2020-11-30 18:54:06 +1100

    style fix

--- http_check/datadog_checks/http_check/http_check.py
+++ http_check/datadog_checks/http_check/http_check.py
@@ -243,9 +243,9 @@ class HTTPCheck(AgentCheck):
             tags_list = list(tags)
             tags_list.append('url:{}'.format(addr))
             tags_list.append("instance:{}".format(instance_name))
-            if days_left >=0:
+            if days_left >= 0:
                 self.gauge('http.ssl.days_left', days_left, tags=tags_list)
-            if seconds_left >=0:
+            if seconds_left >= 0:
                 self.gauge('http.ssl.seconds_left', seconds_left, tags=tags_list)
 
             service_checks.append((self.SC_SSL_CERT, status, msg))
