64b6dd52e03a861213548adcb361f6c84b72af1b
Florimond Manca <florimond.manca@datadoghq.com>
2020-12-01 16:02:43 +0100

    Update e2e test

--- voltdb/tests/test_e2e.py
+++ voltdb/tests/test_e2e.py
@@ -5,19 +5,13 @@ from typing import Callable
 
 import pytest
 
-from datadog_checks.base import AgentCheck
 from datadog_checks.base.stubs.aggregator import AggregatorStub
 
-from . import common
+from . import assertions
 
 
 @pytest.mark.e2e
 def test_check_ok(dd_agent_check):
     # type: (Callable) -> None
     aggregator = dd_agent_check(rate=True)  # type: AggregatorStub
-
-    for metric in common.METRICS:
-        aggregator.assert_metric(metric)
-
-    aggregator.assert_all_metrics_covered()
-    aggregator.assert_service_check('voltdb.can_connect', AgentCheck.OK)
+    assertions.assert_metrics(aggregator)
