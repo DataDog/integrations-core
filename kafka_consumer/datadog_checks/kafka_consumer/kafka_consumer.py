8ddb4715d1209f6d00d35288193efdb506bdbeae
Paul Coignet <paul.coignet@datadoghq.com>
2020-12-09 11:03:44 +0100

    Update deprecation notice

--- kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
+++ kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
@@ -335,7 +335,7 @@ class KafkaCheck(AgentCheck):
                 1. Issue a ListGroupsRequest to every broker
                 2. Attach a callback to each ListGroupsRequest that issues OffsetFetchRequests for every group.
                    Note: Because a broker only returns groups for which it is the coordinator, as an optimization we
-                  skip the FindCoordinatorRequest
+                   skip the FindCoordinatorRequest
             B: When fetching only listed groups:
                 1. Issue a FindCoordintorRequest for each group
                 2. Attach a callback to each FindCoordinatorResponse that issues OffsetFetchRequests for that group
