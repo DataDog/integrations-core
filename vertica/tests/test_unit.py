4f4200996958276332ae08910a28a08c7fc67cfc
Andrew Zhang <andrew.zhang@Andrews-MacBook-Pro.local>
2020-12-21 10:23:10 -0500

    Fix unit test

--- vertica/tests/test_unit.py
+++ vertica/tests/test_unit.py
@@ -16,24 +16,21 @@ cert = os.path.join(CERTIFICATE_DIR, 'cert.cert')
 private_key = os.path.join(CERTIFICATE_DIR, 'server.pem')
 
 
-# TODO: HOW TO GET DATADOG CHECKS BASE????
 def test_ssl_config_ok(aggregator, tls_instance):
-    check = VerticaCheck('vertica', {}, [tls_instance])
-
     with mock.patch('datadog_checks.vertica.vertica.vertica') as vertica:
-        with mock.patch('datadog_checks_base.base.utils.tls') as ssl:
+        with mock.patch('datadog_checks.base.utils.tls.ssl') as ssl:
             vertica.connect.return_value = mock.MagicMock()
             tls_context = mock.MagicMock()
-            ssl._create_tls_context.return_value = tls_context
-
+            ssl.SSLContext.return_value = tls_context
+            check = VerticaCheck('vertica', {}, [tls_instance])
             check.check(tls_instance)
 
             assert tls_context.verify_mode == ssl.CERT_REQUIRED
             assert tls_context.check_hostname is True
-            tls_context.load_verify_locations.assert_called_with(None, CERTIFICATE_DIR, None)
-            tls_context.load_cert_chain.assert_called_with(cert, keyfile=private_key)
+            tls_context.load_verify_locations.assert_called_with(cadata=None, cafile=None, capath=CERTIFICATE_DIR)
+            tls_context.load_cert_chain.assert_called_with(cert, keyfile=private_key, password=None)
 
-        assert check._connection is not None
+            assert check._connection is not None
 
     aggregator.assert_service_check("vertica.can_connect", status=AgentCheck.OK, tags=['db:abc', 'foo:bar'])
 
