dd00c4370d28ad61a644736eed120a2bbc354a14
Grégoire Marabout <greg.maraboutdemazure@datadoghq.com>
2020-12-21 09:50:35 +0100

    Update snmp/tests/test_profiles.py
    Co-authored-by: Paul <paul.coignet@datadoghq.com>

--- snmp/tests/test_profiles.py
+++ snmp/tests/test_profiles.py
@@ -2198,6 +2198,7 @@ def test_juniper_ex(aggregator):
     _check_juniper_firewall(aggregator, common_tags)
     aggregator.assert_metric('snmp.devices_monitored', count=1)
     aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
 
 
 @pytest.mark.usefixtures("dd_environment")
