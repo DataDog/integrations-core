051c3c4b5d461ca1d6ee1e00dab6667f927418f5
pducolin <paola.ducolin@datadoghq.com>
2020-12-22 17:55:57 +0100

    use normalize package name when printing directory name

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/create.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/create.py
@@ -106,14 +106,14 @@ def create(ctx, name, integration_type, location, non_interactive, quiet, dry_ru
 
     if integration_type == 'snmp_tile' and name.startswith('snmp_'):
         name_without_snmp_prefix = name[len('snmp_') :]
-        echo_warning(f'Creating snmp_tile for vendor ({name}) in directory (snmp_{name})')
+        echo_warning(f'Creating snmp_tile for vendor ({name}) in directory (snmp_{normalize_package_name(name)})')
         value = click.prompt(f'Did you mean vendor ({name_without_snmp_prefix})? (y/n)', default='y')
         if value.lower() == 'y':
             name = name_without_snmp_prefix
 
-    integration_dir_name = name
+    integration_dir_name = normalize_package_name(name)
     if integration_type == 'snmp_tile':
-        integration_dir_name = 'snmp_' + name
+        integration_dir_name = 'snmp_' + integration_dir_name
     integration_dir = os.path.join(root, normalize_package_name(integration_dir_name))
     if os.path.exists(integration_dir):
         abort(f'Path `{integration_dir}` already exists!')
