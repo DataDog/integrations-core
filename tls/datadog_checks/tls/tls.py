64f602dc6260530b763c2998a3b2d3832613e226
Andrew Zhang <andrew.zhang@Andrews-MacBook-Pro.local>
2020-12-22 11:35:38 -0500

    Remove unused variable

--- tls/datadog_checks/tls/tls.py
+++ tls/datadog_checks/tls/tls.py
@@ -116,7 +116,6 @@ class TLSCheck(AgentCheck):
             self.log.debug('TLS check able to connect')
             self.service_check(self.SERVICE_CHECK_CAN_CONNECT, self.OK, tags=self._tags)
 
-        protocol_version = None
         # Get the cert & TLS version from the connection
         with closing(sock):
             self.log.debug('Getting cert and TLS protocol version')
