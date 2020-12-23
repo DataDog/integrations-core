27ceda1df6a6db0467ddacc9b6e759c1dd200847
ChristineTChen <christine.chen@datadoghq.com>
2020-12-22 19:17:44 -0500

    Create test gluster cluster and volume

--- glusterfs/tests/conftest.py
+++ glusterfs/tests/conftest.py
@@ -5,16 +5,29 @@ import os
 
 import mock
 import pytest
+import time
 
 from datadog_checks.base.utils.common import get_docker_hostname
+from datadog_checks.dev import docker_run, run_command
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 HOST = get_docker_hostname()
 
+E2E_METADATA = {
+    'start_commands': [
+        'apt-get update',
+        'apt-get install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y docker.io',
+    ],
+    'docker_volumes': ['/var/run/docker.sock:/var/run/docker.sock'],
+}
+
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    yield
+    compose_file = os.path.join(HERE, 'docker', 'docker-compose.yaml')
+    with docker_run(compose_file=compose_file):
+        create_volume()
+        yield {}, E2E_METADATA
 
 
 @pytest.fixture
@@ -29,3 +42,16 @@ def mock_gstatus_data():
         data = f.read()
     with mock.patch('datadog_checks.glusterfs.check.get_subprocess_output', return_value=(data, '', 0)):
         yield
+
+
+def create_volume():
+    for command in (
+        'gluster peer probe gluster-node-2',
+        'mkdir /export',
+        'gluster volume create gv0 gluster-node-1:/export force',
+        'gluster volume start gv0',
+    ):
+        run_command("docker exec gluster-node-1 {}".format(command), capture=True, check=True)
+        time.sleep(10)
+
+
