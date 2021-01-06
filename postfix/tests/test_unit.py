1b08f65e6ec86b976cbb27216fd5ab96d1b9ddaf
Paul Coignet <paul.coignet@datadoghq.com>
2021-01-06 12:56:13 +0100

    Fix postqueue defer counting

--- postfix/tests/test_unit.py
+++ postfix/tests/test_unit.py
@@ -1,11 +1,31 @@
 # (C) Datadog, Inc. 2019-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+
 import mock
 
+from datadog_checks.dev import get_here
 from datadog_checks.postfix import PostfixCheck
 
 MOCK_VERSION = '1.3.1'
+
+
+def test__get_postqueue_stats(aggregator):
+    check = PostfixCheck('postfix', {}, [])
+    common_tags = ['instance:/etc/postfix', 'foo:bar']
+
+    filepath = os.path.join(get_here(), 'fixtures', 'postqueue_p.txt')
+    with open(filepath, 'r') as f:
+        mocked_output = f.read()
+
+    with mock.patch('datadog_checks.postfix.postfix.get_subprocess_output') as s:
+        s.side_effect = [(False, None, None), (mocked_output, None, None)]
+        check._get_postqueue_stats('/etc/postfix', ['foo:bar'])
+
+        aggregator.assert_metric('postfix.queue.size', 1, tags=common_tags + ['queue:active'])
+        aggregator.assert_metric('postfix.queue.size', 1, tags=common_tags + ['queue:hold'])
+        aggregator.assert_metric('postfix.queue.size', 2, tags=common_tags + ['queue:deferred'])
 
 
 @mock.patch(
