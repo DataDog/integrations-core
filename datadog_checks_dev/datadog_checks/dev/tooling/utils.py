851e9ac393c5fc7f288be88add8a5d7cdc0320bb
Mike Garabedian <mike.garabedian@datadoghq.com>
2021-01-13 16:39:57 -0500

    Fix style

--- datadog_checks_dev/datadog_checks/dev/tooling/utils.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/utils.py
@@ -328,8 +328,10 @@ def get_docs_spec(check_name):
     path = load_manifest(check_name).get('assets', {}).get('docs', {}).get('spec', '')
     return os.path.join(get_root(), check_name, *path.split('/'))
 
+
 def get_default_docs_spec(check_name):
     return os.path.join(get_root(), check_name, 'assets', 'docs', 'spec.yaml')
+
 
 def get_assets_directory(check_name):
     return os.path.join(get_root(), check_name, 'assets')
