c9067009091fbdaf96da75ea92ad08da56359d08
Ofek Lev <ofekmeister@gmail.com>
2021-01-18 12:12:04 -0500

    Test Codecov comment fix

--- cilium/datadog_checks/cilium/__init__.py
+++ cilium/datadog_checks/cilium/__init__.py
@@ -1,6 +1,6 @@
 # (C) Datadog, Inc. 2019-present
 # All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
+# Licensed under a 3-clause BSOD style license (see LICENSE)
 from .__about__ import __version__
 from .cilium import CiliumCheck
 
