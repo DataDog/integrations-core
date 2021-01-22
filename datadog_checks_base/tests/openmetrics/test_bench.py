613351d261fb7c88789d84620444fec2591fe963
Ofek Lev <ofekmeister@gmail.com>
2021-01-22 02:32:32 -0500

    add larger benchmark

--- datadog_checks_base/tests/openmetrics/test_bench.py
+++ datadog_checks_base/tests/openmetrics/test_bench.py
@@ -9,6 +9,7 @@ from datadog_checks.base import OpenMetricsBaseCheck
 from datadog_checks.dev import get_here
 
 from ..utils import requires_py3
+from .bench_utils import AMAZON_MSK_JMX_METRICS_MAP, AMAZON_MSK_JMX_METRICS_OVERRIDES
 
 pytestmark = [requires_py3, pytest.mark.openmetrics, pytest.mark.openmetrics_config]
 
@@ -18,6 +19,11 @@ HERE = get_here()
 @pytest.fixture
 def fixture_ksm():
     return os.path.join(os.path.dirname(HERE), 'fixtures', 'prometheus', 'ksm.txt')
+
+
+@pytest.fixture
+def fixture_amazon_msk_jmx_metrics():
+    return os.path.join(os.path.dirname(HERE), 'fixtures', 'prometheus', 'amazon_msk_jmx_metrics.txt')
 
 
 def test_ksm_new(benchmark, dd_run_check, mock_http_response, fixture_ksm):
@@ -33,6 +39,41 @@ def test_ksm_new(benchmark, dd_run_check, mock_http_response, fixture_ksm):
 def test_ksm_old(benchmark, dd_run_check, mock_http_response, fixture_ksm):
     mock_http_response(file_path=fixture_ksm)
     instance = {'prometheus_url': 'foo', 'namespace': 'bar', 'metrics': ['*']}
+    c = OpenMetricsBaseCheck('test', {}, [instance])
+
+    # Run once to get initialization steps out of the way.
+    dd_run_check(c)
+
+    benchmark(c.check, instance)
+
+
+def test_amazon_msk_jmx_metrics_new(benchmark, dd_run_check, mock_http_response, fixture_amazon_msk_jmx_metrics):
+    mock_http_response(file_path=fixture_amazon_msk_jmx_metrics)
+
+    metrics = []
+    for raw_metric_name, metric_name in AMAZON_MSK_JMX_METRICS_MAP.items():
+        config = {raw_metric_name: {'name': metric_name}}
+        if raw_metric_name in AMAZON_MSK_JMX_METRICS_OVERRIDES:
+            config[raw_metric_name]['type'] = AMAZON_MSK_JMX_METRICS_OVERRIDES[raw_metric_name]
+
+        metrics.append(config)
+
+    c = OpenMetricsBaseCheck('test', {}, [{'openmetrics_endpoint': 'foo', 'namespace': 'bar', 'metrics': metrics}])
+
+    # Run once to get initialization steps out of the way.
+    dd_run_check(c)
+
+    benchmark(c.check, None)
+
+
+def test_amazon_msk_jmx_metrics_old(benchmark, dd_run_check, mock_http_response, fixture_amazon_msk_jmx_metrics):
+    mock_http_response(file_path=fixture_amazon_msk_jmx_metrics)
+    instance = {
+        'prometheus_url': 'foo',
+        'namespace': 'bar',
+        'metrics': [AMAZON_MSK_JMX_METRICS_MAP],
+        'type_overrides': AMAZON_MSK_JMX_METRICS_OVERRIDES,
+    }
     c = OpenMetricsBaseCheck('test', {}, [instance])
 
     # Run once to get initialization steps out of the way.
