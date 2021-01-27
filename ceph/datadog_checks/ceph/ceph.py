477379ddec3614180817781a6db3a4de053bdb3e
Julia <611228+hithwen@users.noreply.github.com>
2021-01-27 13:44:36 +0100

    Update ceph/datadog_checks/ceph/ceph.py
    Co-authored-by: Christine Chen <ChristineTChen@users.noreply.github.com>

--- ceph/datadog_checks/ceph/ceph.py
+++ ceph/datadog_checks/ceph/ceph.py
@@ -72,7 +72,7 @@ class Ceph(AgentCheck):
 
         mon_map = raw.get('status', {}).get('monmap')
         if mon_map is None:
-            raise RuntimeError("Could not detect if ceph is an octopus release or not")
+            raise RuntimeError("Could not detect Ceph release series")
         if 'min_mon_release_name' in mon_map and mon_map['min_mon_release_name'] == 'octopus':
             self.log.debug("Detected octopus version of ceph...")
             self._octopus = True
