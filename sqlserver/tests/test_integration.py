6948c4a3fc1bcb31ceaa9522046833ab9cd99a52
Mike Garabedian <mike.garabedian@datadoghq.com>
2021-01-28 15:53:11 -0500

    Fix metrics

--- sqlserver/tests/test_integration.py
+++ sqlserver/tests/test_integration.py
@@ -251,7 +251,7 @@ def test_check_windows_defaults(aggregator, dd_run_check, init_config, instance_
 
     aggregator.assert_metric_has_tag('sqlserver.db.commit_table_entries', 'db:master')
 
-    for mname in EXPECTED_DEFAULT_METRICS:
+    for mname in EXPECTED_DEFAULT_METRICS + CUSTOM_METRICS:
         aggregator.assert_metric(mname)
 
     aggregator.assert_service_check('sqlserver.can_connect', status=SQLServer.OK)
