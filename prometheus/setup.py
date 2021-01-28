370c0b589253ed1224e5c925a9c7d5790910082e
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-01-28 13:37:26 +0100

    bump 11.0.0 to 11.0.2 due to pyaml incompatibility and 11.0.4 cause its not downloadable for some reason

--- prometheus/setup.py
+++ prometheus/setup.py
@@ -27,7 +27,7 @@ def get_dependencies():
         return f.readlines()
 
 
-CHECKS_BASE_REQ = 'datadog-checks-base>=11.0.0'
+CHECKS_BASE_REQ = 'datadog-checks-base>=11.0.2'
 
 setup(
     name='datadog-prometheus',
