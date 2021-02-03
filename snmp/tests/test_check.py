554dbaaf3db57cb17ece41cc6bb7734f7857274a
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-02-03 14:17:20 +0100

    Add test for test_submitted_metrics_count

--- snmp/tests/test_check.py
+++ snmp/tests/test_check.py
@@ -139,8 +139,26 @@ def test_scalar(aggregator):
 
     # Test service check
     aggregator.assert_service_check("snmp.can_check", status=SnmpCheck.OK, tags=common.CHECK_TAGS, at_least=1)
+    aggregator.assert_metric('datadog.snmp.submitted_metrics', value=99, metric_type=aggregator.GAUGE, count=1, tags=common.CHECK_TAGS)
 
-    common.assert_common_metrics(aggregator)
+    common.assert_common_metrics(aggregator, tags=common.CHECK_TAGS)
+    aggregator.all_metrics_asserted()
+
+
+def test_submitted_metrics_count(aggregator):
+    instance = common.generate_instance_config(common.SCALAR_OBJECTS)
+    check = common.create_check(instance)
+    check.check(instance)
+
+    for metric in common.SCALAR_OBJECTS:
+        metric_name = "snmp." + (metric.get('name') or metric.get('symbol'))
+        aggregator.assert_metric(metric_name, tags=common.CHECK_TAGS, count=1)
+
+    aggregator.assert_metric('snmp.sysUpTimeInstance', count=1)
+    aggregator.assert_metric('datadog.snmp.submitted_metrics', value=5.0, metric_type=aggregator.GAUGE, count=1, tags=common.CHECK_TAGS)
+    aggregator.assert_metric('datadog.snmp.check_duration', metric_type=aggregator.GAUGE, count=1, tags=common.CHECK_TAGS)
+    aggregator.assert_metric('datadog.snmp.check_interval', metric_type=aggregator.MONOTONIC_COUNT, count=1, tags=common.CHECK_TAGS)
+    common.assert_common_device_metrics(aggregator, tags=common.CHECK_TAGS)
     aggregator.all_metrics_asserted()
 
 
