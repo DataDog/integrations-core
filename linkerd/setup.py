2d327b6e71a25ed5b8ff10a1ad356b83058d1eb7
Paul Coignet <paul.coignet@datadoghq.com>
2021-02-09 15:29:40 +0100

    Bump base package

--- linkerd/setup.py
+++ linkerd/setup.py
@@ -27,7 +27,7 @@ def get_dependencies():
         return f.readlines()
 
 
-CHECKS_BASE_REQ = 'datadog-checks-base>=16.2.0'
+CHECKS_BASE_REQ = 'datadog-checks-base>=16.3.2'
 
 setup(
     name='datadog-linkerd',
