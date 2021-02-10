a22947deade440ce700b5fd36e194b74da29e593
Paul Coignet <paul.coignet@datadoghq.com>
2021-02-10 17:33:21 +0100

    Update host value with 127.0.0.1

--- aerospike/tests/common.py
+++ aerospike/tests/common.py
@@ -8,7 +8,7 @@ from datadog_checks.dev import get_docker_hostname
 HERE = os.path.dirname(os.path.abspath(__file__))
 COMPOSE_FILE = os.path.join(HERE, 'docker', 'docker-compose.yaml')
 
-HOST = get_docker_hostname()
+HOST = "127.0.0.1"
 PORT = 3000
 VERSION = os.environ.get('AEROSPIKE_VERSION')
 
