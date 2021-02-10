b35741e3c6134da6b27886c5d15d2b1a72b27d3f
Paul Coignet <paul.coignet@datadoghq.com>
2021-02-10 18:06:35 +0100

    Use localhost

--- aerospike/tests/common.py
+++ aerospike/tests/common.py
@@ -8,7 +8,7 @@ from datadog_checks.dev import get_docker_hostname
 HERE = os.path.dirname(os.path.abspath(__file__))
 COMPOSE_FILE = os.path.join(HERE, 'docker', 'docker-compose.yaml')
 
-HOST = "::1"
+HOST = "localhost"
 PORT = 3000
 VERSION = os.environ.get('AEROSPIKE_VERSION')
 
