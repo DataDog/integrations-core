457673a7aaedcf88ab319ed30653085d66ca3f00
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-02-15 15:14:10 +0100

    Add assert metrics using metadata

--- haproxy/tests/test_e2e.py
+++ haproxy/tests/test_e2e.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
+from datadog_checks.dev.utils import get_metadata_metrics
 
 from . import common
 
@@ -15,3 +16,4 @@ def test_check(dd_agent_check, prometheus_metrics):
         aggregator.assert_metric('haproxy.{}'.format(metric))
 
     aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
