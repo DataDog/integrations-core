22eb69418f572cdf271c1e2cb438d1cfa69ecbbc
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2021-02-15 09:01:49 -0700

    Collect mysql statement samples & execution plans
    **What does this PR do?**
    
    Adds a new feature to "Deep Database Monitoring", enabling collection of statement samples and execution plans. Follow-up to https://github.com/DataDog/integrations-core/pull/7851.
    
    **How it works**
    
    If enabled, a python thread is launched during a regular check run:
    * collects statement samples at the configured rate limit (default 1 collection per second)
    * maintains its own `pymysql` connection as `pymysql` is not thread safe so we can't share with the main check
    * collects execution plans through a mysql procedure that the user must install into each database being monitored (if we wanted the agent to collect execution plans directly by running `EXPLAIN` then it would need full write permission to all tables)
    * shuts down if it detects that the main check has stopped running
    
    During one "collection" we do the following:
    1. read out all new statements from `performance_schema.events_statement_{summary|history|history_long}`
    1. try to collect an execution plan for each statement
    1. submit events directly to the new database monitoring event intake
    
    **Rate Limiting**
    
    There are several different rate limits to keep load on the database to a minimum and to avoid reingesting duplicate events:
    * `collections_per_second`: limits how often collections are done (each collection is a query to an `events_statements_*` table)
    * `explained_statements_cache`: ttl limits how often we attempt to collect an execution plan for a given normalized query
    * `seen_samples_cache`: ttl limits how often we ingest statement samples for the same normalized query and execution plan
    
    **events statements tables**
    
    The check will collect samples from the best available `events_statements` table. It's up to the user to enable the required `events_statements` consumers. Some managed databases like RDS Aurora replicas don't support `events_statements_history_long` in which case we fall back to one of the other tables.
    1. `events_statements_history_long` - preferred as it has the longest retention giving us the highest chance of catching infrequent & fast queries
    1. `events_statements_current` - less likely to catch infrequent & fast queries
    1. `events_statements_history` - least preferred table because
    
    **Configuration**
    
    ```yaml
    statement_samples:
    	enabled: false
    	# default rate depends on which events_statements table is being used. user can override.
    	collections_per_second: -1
    	# the best table is chosen automatically by the check. user can override.
    	events_statements_table: ''
    	events_statements_row_limit: 5000
    	explain_procedure: 'explain_statement'
    	fully_qualified_explain_procedure: 'datadog.explain_statement'
    	events_statements_temp_table_name: 'datadog.temp_events'
    ```
    
    **Motivation**
    
    Collect statement samples & execution plans, enabling deeper insight into what's running on the database and how queries are being executed.

--- mysql/tests/test_mysql.py
+++ mysql/tests/test_mysql.py
@@ -2,22 +2,33 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import copy
+import json
 import subprocess
+import time
+from collections import Counter
 from contextlib import closing
 from os import environ
 
 import mock
 import psutil
 import pytest
-from pkg_resources import parse_version
-
+from datadog_checks.base.utils.db.statement_samples import statement_samples_client
 from datadog_checks.base.utils.platform import Platform
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.mysql import MySql, statements
 from datadog_checks.mysql.version_utils import get_version
+from pkg_resources import parse_version
 
 from . import common, tags, variables
 from .common import MYSQL_VERSION_PARSED
+
+
+@pytest.fixture
+def dbm_instance(instance_complex):
+    instance_complex['deep_database_monitoring'] = True
+    instance_complex['min_collection_interval'] = 1
+    instance_complex['statement_samples'] = {'enabled': True, 'run_sync': False, 'collections_per_second': 1}
+    return instance_complex
 
 
 @pytest.mark.integration
@@ -210,7 +221,7 @@ def test_complex_config_replica(aggregator, instance_complex):
 
 @pytest.mark.integration
 @pytest.mark.usefixtures('dd_environment')
-def test_statement_metrics(aggregator, instance_complex):
+def test_statement_metrics(aggregator, dbm_instance):
     QUERY = 'select * from information_schema.processlist'
     QUERY_DIGEST_TEXT = 'SELECT * FROM `information_schema` . `processlist`'
     # The query signature should match the query and consistency of this tag has product impact. Do not change
@@ -228,8 +239,7 @@ def test_statement_metrics(aggregator, instance_complex):
         QUERY_DIGEST = '6817a67871eb7edddad5b7836c93330aa3c98801ac759eed1bea6db1a34579c4'
         QUERY_SIGNATURE = '9d73cb71644af0a2'
 
-    config = copy.deepcopy(instance_complex)
-    mysql_check = MySql(common.CHECK_NAME, {}, instances=[config])
+    mysql_check = MySql(common.CHECK_NAME, {}, instances=[dbm_instance])
 
     def run_query(q):
         with mysql_check._connect() as db:
@@ -238,11 +248,11 @@ def test_statement_metrics(aggregator, instance_complex):
 
     # Run a query
     run_query(QUERY)
-    mysql_check.check(config)
+    mysql_check.check(dbm_instance)
 
     # Run the query and check a second time so statement metrics are computed from the previous run
     run_query(QUERY)
-    mysql_check.check(config)
+    mysql_check.check(dbm_instance)
     for name in statements.STATEMENT_METRICS.values():
         aggregator.assert_metric(
             name,
@@ -318,6 +328,150 @@ def test_generate_synthetic_rows():
             'rows_examined': 0,
         },
     ]
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+@pytest.mark.parametrize(
+    "events_statements_table",
+    ["events_statements_current", "events_statements_history", "events_statements_history_long", None],
+)
+@pytest.mark.parametrize("explain_strategy", ['PROCEDURE', 'FQ_PROCEDURE', 'STATEMENT', None])
+@pytest.mark.parametrize(
+    "schema,statement",
+    [
+        (None, 'select name as nam from testdb.users'),
+        ('information_schema', 'select name as nam from testdb.users'),
+        ('testdb', 'select name as nam from users'),
+    ],
+)
+def test_statement_samples_collect(dbm_instance, bob_conn, events_statements_table, explain_strategy, schema,
+                                   statement):
+    # try to collect a sample from all supported events_statements tables using all possible strategies
+    dbm_instance['statement_samples']['events_statements_table'] = events_statements_table
+    dbm_instance['statement_samples']['run_sync'] = True
+    mysql_check = MySql(common.CHECK_NAME, {}, instances=[dbm_instance])
+    if explain_strategy:
+        mysql_check._statement_samples._preferred_explain_strategies = [explain_strategy]
+
+    mysql_check.check(dbm_instance)
+    statement_samples_client._events = []
+    mysql_check._statement_samples._init_caches()
+
+    # we deliberately want to keep the connection open for the duration of the test to ensure
+    # the query remains in the events_statements_current and events_statements_history tables
+    # it would be cleared out upon connection close otherwise
+    with closing(bob_conn.cursor()) as cursor:
+        # run the check once, then clear out all saved events
+        # on the next check run it should only capture events since the last checkpoint
+        if schema:
+            cursor.execute("use {}".format(schema))
+        cursor.execute(statement)
+    mysql_check.check(dbm_instance)
+    matching = [e for e in statement_samples_client._events if e['db']['statement'] == statement]
+    assert len(matching) > 0, "should have collected an event"
+    with_plans = [e for e in matching if e['db']['plan']['definition'] is not None]
+    if schema == 'testdb' and explain_strategy == 'FQ_PROCEDURE':
+        # explain via the FQ_PROCEDURE will fail if a query contains non-fully-qualified tables because it will
+        # default to the schema of the FQ_PROCEDURE, so in case of "select * from testdb" it'll try to do
+        # "select start from datadog.testdb" which would be the wrong schema.
+        assert not with_plans, "cannot collect plan in this case"
+    elif schema == 'information_schema' and explain_strategy == 'PROCEDURE':
+        # we can't create an explain_statement procedure in performance_schema so this is not expected to work
+        assert not with_plans, "cannot collect plan in this case"
+    elif not schema and explain_strategy == 'PROCEDURE':
+        # if there is no default schema then we cannot use the non-fully-qualified procedure strategy
+        assert not with_plans, "cannot collect plan in this case"
+    else:
+        event = with_plans[0]
+        assert 'query_block' in json.loads(event['db']['plan']['definition']), "invalid json execution plan"
+
+    # we avoid closing these in a try/finally block in order to maintain the connections in case we want to
+    # debug the test with --pdb
+    mysql_check._statement_samples.close()
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_statement_samples_rate_limit(aggregator, bob_conn, dbm_instance):
+    dbm_instance['statement_samples']['collections_per_second'] = 0.5
+    query = "select name as nam from testdb.users where name = 'hello'"
+    mysql_check = MySql(common.CHECK_NAME, {}, instances=[dbm_instance])
+    with closing(bob_conn.cursor()) as cursor:
+        for _ in range(5):
+            cursor.execute(query)
+            mysql_check.check(dbm_instance)
+            time.sleep(1)
+    matching = [e for e in statement_samples_client._events if e['db']['statement'] == query]
+    assert len(matching) == 1, "should have collected exactly one event due to sample rate limit"
+    metrics = aggregator.metrics("dd.mysql.collect_statement_samples.time")
+    assert 2 < len(metrics) < 6
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_statement_samples_loop_inactive_stop(aggregator, dbm_instance):
+    # confirm that the collection loop stops on its own after the check has not been run for a while
+    mysql_check = MySql(common.CHECK_NAME, {}, instances=[dbm_instance])
+    mysql_check.check(dbm_instance)
+    while mysql_check._statement_samples._collection_loop_future.running():
+        time.sleep(0.1)
+    # make sure there were no unhandled exceptions
+    mysql_check._statement_samples._collection_loop_future.result()
+    aggregator.assert_metric("dd.mysql.statement_samples.collection_loop_inactive_stop")
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_statement_samples_max_per_digest(dbm_instance):
+    # clear out any events from previous test runs
+    statement_samples_client._events = []
+    dbm_instance['statement_samples']['run_sync'] = True
+    dbm_instance['statement_samples']['events_statements_table'] = 'events_statements_history_long'
+    mysql_check = MySql(common.CHECK_NAME, {}, instances=[dbm_instance])
+    for _ in range(3):
+        mysql_check.check(dbm_instance)
+    rows = mysql_check._statement_samples._get_new_events_statements('events_statements_history_long', 1000)
+    count_by_digest = Counter(r['digest'] for r in rows)
+    for digest, count in count_by_digest.items():
+        assert count == 1, "we should be reading exactly one row per digest out of the database"
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_statement_samples_invalid_explain_procedure(aggregator, dbm_instance):
+    dbm_instance['statement_samples']['run_sync'] = True
+    dbm_instance['statement_samples']['explain_procedure'] = 'hello'
+    mysql_check = MySql(common.CHECK_NAME, {}, instances=[dbm_instance])
+    mysql_check.check(dbm_instance)
+    aggregator.assert_metric_has_tag_prefix("dd.mysql.statement_samples.error", "error:explain-")
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+@pytest.mark.parametrize("events_statements_enable_procedure",
+                         ["datadog.enable_events_statements_consumers", "invalid_proc"])
+def test_statement_samples_enable_consumers(dbm_instance, root_conn, events_statements_enable_procedure):
+    dbm_instance['statement_samples']['run_sync'] = True
+    dbm_instance['statement_samples']['events_statements_enable_procedure'] = events_statements_enable_procedure
+    mysql_check = MySql(common.CHECK_NAME, {}, instances=[dbm_instance])
+
+    # deliberately disable one of the consumers
+    with closing(root_conn.cursor()) as cursor:
+        cursor.execute(
+            "UPDATE performance_schema.setup_consumers SET enabled='NO'  WHERE name = "
+            "'events_statements_history_long';")
+
+    original_enabled_consumers = mysql_check._statement_samples._get_enabled_performance_schema_consumers()
+    assert original_enabled_consumers == {'events_statements_current', 'events_statements_history'}
+
+    mysql_check.check(dbm_instance)
+
+    enabled_consumers = mysql_check._statement_samples._get_enabled_performance_schema_consumers()
+    if events_statements_enable_procedure == "datadog.enable_events_statements_consumers":
+        assert enabled_consumers == original_enabled_consumers.union({'events_statements_history_long'})
+    else:
+        assert enabled_consumers == original_enabled_consumers
 
 
 def _test_optional_metrics(aggregator, optional_metrics, at_least):
