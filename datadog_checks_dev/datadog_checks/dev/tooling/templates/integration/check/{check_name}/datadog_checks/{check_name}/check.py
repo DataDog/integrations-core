306cc248e27d5b51f163f503f250c4c1b0d0b77b
Luis Gonzalez <luisgpec1@gmail.com>
2021-02-18 22:01:19 -0600

    added code samples for various tasks -- requests, db queries, metric submission

--- datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/datadog_checks/{check_name}/check.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/datadog_checks/{check_name}/check.py
@@ -1,11 +1,31 @@
 {license_header}
 from typing import Any
 
-from datadog_checks.base import AgentCheck
+from datadog_checks.base import AgentCheck, ConfigurationError
 
 
 class {check_class}(AgentCheck):
     def check(self, _):
         # type: (Any) -> None
-        # Use self.instance to read the check configuration
-        pass
+        # The following are useful bits of code to help new users started.
+
+        # This is how you make HTTP Requests.
+        # More info at https://datadoghq.dev/integrations-core/base/http/
+        self.http.get("<url>")
+
+        # This is how you submit metrics
+        self.gauge("test", 1.23, tags=['foo:bar'])
+        # There are different types of metrics that you can submit (events, count).
+        # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
+        self.count("count", 1.0)
+
+        # This is how you can perform database queries.
+        # You would need to define your query manager in your __init__ method.
+        # More info at https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
+        self._query_manager.execute()
+
+        # If your check ran succesfully, you can send the status.
+        # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck.service_check
+        self.service_check(self.SERVICE_CHECK_NAME, AgentCheck.OK)
+
+
