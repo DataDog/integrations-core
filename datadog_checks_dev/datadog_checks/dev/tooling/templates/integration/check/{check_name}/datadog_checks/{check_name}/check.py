d75ffd9e0865333aff0ae90920d494878d95c3b8
Luis Gonzalez <luisgpec1@gmail.com>
2021-02-19 10:10:29 -0600

    added an __init__ method

--- datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/datadog_checks/{check_name}/check.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/datadog_checks/{check_name}/check.py
@@ -5,6 +5,16 @@ from datadog_checks.base import AgentCheck
 
 
 class {check_class}(AgentCheck):
+    def __init__(self, name, init_config, instances):
+        # super({check_name}, self).__init__(name, init_config, instances)
+
+        # Define a query manager.
+        # More info at
+        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
+        # self._query_manager = QueryManager(self, self.execute_query, queries=[queries.SomeQuery1])
+        # self.check_initializations.append(self._query_manager.compile_queries)
+        pass
+
     def check(self, _):
         # type: (Any) -> None
         # The following are useful bits of code to help new users started.
@@ -20,9 +30,7 @@ class {check_class}(AgentCheck):
         # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
         # self.count("count", 1.0)
 
-        # Define a query manager in the __init__ method and perform database queries.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
+        # Perform database queries using the Query Manager
         # self._query_manager.execute()
 
         # If your check ran succesfully, you can send the status.
