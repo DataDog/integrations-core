dcfd5ac9a66db2abd3635330d36c3b8f17b6423c
Ofek Lev <ofekmeister@gmail.com>
2021-03-22 13:45:20 -0400

    Sync config models

---
+++ statsd/datadog_checks/statsd/config_models/defaults.py
@@ -0,0 +1,28 @@
+# (C) Datadog, Inc. 2021-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.base.utils.models.fields import get_default_field_value
+
+
+def shared_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_empty_default_hostname(field, value):
+    return False
+
+
+def instance_min_collection_interval(field, value):
+    return 15
+
+
+def instance_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_tags(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_timeout(field, value):
+    return 10
