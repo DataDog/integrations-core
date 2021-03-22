54494a18fd6b49dbeb0e2eaf2ff7b18da763be32
Ofek Lev <ofekmeister@gmail.com>
2021-03-22 13:40:42 -0400

    Sync config models

---
+++ pdh_check/datadog_checks/pdh_check/config_models/defaults.py
@@ -0,0 +1,48 @@
+# (C) Datadog, Inc. 2021-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.base.utils.models.fields import get_default_field_value
+
+
+def shared_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_additional_metrics(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_admin_share(field, value):
+    return 'c$'
+
+
+def instance_counter_data_types(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_empty_default_hostname(field, value):
+    return False
+
+
+def instance_host(field, value):
+    return '.'
+
+
+def instance_min_collection_interval(field, value):
+    return 15
+
+
+def instance_password(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_tags(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_username(field, value):
+    return get_default_field_value(field, value)
