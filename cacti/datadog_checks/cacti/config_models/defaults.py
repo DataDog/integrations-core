9e19abbcf79bdc123ed80327933b5b3254c1c564
Ofek Lev <ofekmeister@gmail.com>
2021-03-22 13:13:17 -0400

    Sync config models

---
+++ cacti/datadog_checks/cacti/config_models/defaults.py
@@ -0,0 +1,36 @@
+# (C) Datadog, Inc. 2021-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.base.utils.models.fields import get_default_field_value
+
+
+def shared_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_empty_default_hostname(field, value):
+    return False
+
+
+def instance_field_names(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_min_collection_interval(field, value):
+    return 15
+
+
+def instance_mysql_port(field, value):
+    return 3306
+
+
+def instance_rrd_whitelist(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_tags(field, value):
+    return get_default_field_value(field, value)
