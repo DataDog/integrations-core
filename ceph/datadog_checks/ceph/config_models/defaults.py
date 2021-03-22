cba0127f829fa71606d5e2117038cf2b1be93b16
Ofek Lev <ofekmeister@gmail.com>
2021-03-22 13:13:45 -0400

    Sync config models

---
+++ ceph/datadog_checks/ceph/config_models/defaults.py
@@ -0,0 +1,40 @@
+# (C) Datadog, Inc. 2021-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.base.utils.models.fields import get_default_field_value
+
+
+def shared_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_ceph_cluster(field, value):
+    return 'ceph'
+
+
+def instance_ceph_cmd(field, value):
+    return '/usr/bin/ceph'
+
+
+def instance_collect_service_check_for(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_empty_default_hostname(field, value):
+    return False
+
+
+def instance_min_collection_interval(field, value):
+    return 15
+
+
+def instance_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_tags(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_use_sudo(field, value):
+    return False
