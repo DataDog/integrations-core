2d1c9efb0194640aabfce3b9ce9e29390ea64a7f
Ofek Lev <ofekmeister@gmail.com>
2021-03-22 13:42:43 -0400

    Sync config models

---
+++ riakcs/datadog_checks/riakcs/config_models/defaults.py
@@ -0,0 +1,44 @@
+# (C) Datadog, Inc. 2021-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.base.utils.models.fields import get_default_field_value
+
+
+def shared_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_empty_default_hostname(field, value):
+    return False
+
+
+def instance_host(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_is_secure(field, value):
+    return True
+
+
+def instance_metrics(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_min_collection_interval(field, value):
+    return 15
+
+
+def instance_port(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_s3_root(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_service(field, value):
+    return get_default_field_value(field, value)
+
+
+def instance_tags(field, value):
+    return get_default_field_value(field, value)
---
+++ riakcs/datadog_checks/riakcs/config_models/instance.py
@@ -0,0 +1,52 @@
+# (C) Datadog, Inc. 2021-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from __future__ import annotations
+
+from typing import Optional, Sequence
+
+from pydantic import BaseModel, root_validator, validator
+
+from datadog_checks.base.utils.functions import identity
+from datadog_checks.base.utils.models import validation
+
+from . import defaults, validators
+
+
+class InstanceConfig(BaseModel):
+    class Config:
+        allow_mutation = False
+
+    access_id: str
+    access_secret: str
+    empty_default_hostname: Optional[bool]
+    host: Optional[str]
+    is_secure: Optional[bool]
+    metrics: Optional[Sequence[str]]
+    min_collection_interval: Optional[float]
+    port: Optional[int]
+    s3_root: Optional[str]
+    service: Optional[str]
+    tags: Optional[Sequence[str]]
+
+    @root_validator(pre=True)
+    def _initial_validation(cls, values):
+        return validation.core.initialize_config(getattr(validators, 'initialize_instance', identity)(values))
+
+    @validator('*', pre=True, always=True)
+    def _ensure_defaults(cls, v, field):
+        if v is not None or field.required:
+            return v
+
+        return getattr(defaults, f'instance_{field.name}')(field, v)
+
+    @validator('*')
+    def _run_validations(cls, v, field):
+        if not v:
+            return v
+
+        return getattr(validators, f'instance_{field.name}', identity)(v, field=field)
+
+    @root_validator(pre=False)
+    def _final_validation(cls, values):
+        return validation.core.finalize_config(getattr(validators, 'finalize_instance', identity)(values))
