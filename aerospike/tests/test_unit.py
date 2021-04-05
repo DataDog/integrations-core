0a1a49f2eb35a333a398a8cbe971782582fdd218
ChristineTChen <christine.chen@datadoghq.com>
2021-04-05 19:21:20 -0400

    Use return value

--- aerospike/tests/test_unit.py
+++ aerospike/tests/test_unit.py
@@ -72,7 +72,7 @@ def test_multiple_xdr_metrics(aggregator):
 def test_collect_xdr_invalid_data(aggregator):
     check = AerospikeCheck('aerospike', {}, [common.INSTANCE])
     check.log = mock.MagicMock()
-    with mock.patch('datadog_checks.aerospike.AerospikeCheck.get_info', side_effect="ERROR::XDR-not-configured"):
+    with mock.patch('datadog_checks.aerospike.AerospikeCheck.get_info', return_value="ERROR::XDR-not-configured"):
         check.collect_xdr()
         check.log.debug.assert_called_with('Error collecting XDR metrics: ERROR::XDR-not-configured')
 
