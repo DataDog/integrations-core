fd2959bd1125075636495871854eaaa4d63d27ca
Ofek Lev <ofekmeister@gmail.com>
2021-04-05 13:51:14 -0400

    Upgrade virtualenv to 20.4.3 (#9086)

--- datadog_checks_dev/setup.py
+++ datadog_checks_dev/setup.py
@@ -86,7 +86,7 @@ setup(
             'toml>=0.9.4, <1.0.0',
             'tox>=3.12.1',
             'twine>=1.11.0',
-            'virtualenv>=20.0.8',
+            'virtualenv>=20.4.3',
             'wheel>=0.31.0',
         ]
     },
