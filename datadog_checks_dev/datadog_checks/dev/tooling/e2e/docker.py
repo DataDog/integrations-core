6ee7477e86857b43cbc2c43aa7431ad55ef6afd2
Paul Coignet <paul.coignet@datadoghq.com>
2021-04-07 17:28:11 +0200

    Use an external dd agent

--- datadog_checks_dev/datadog_checks/dev/tooling/e2e/docker.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/e2e/docker.py
@@ -307,7 +307,6 @@ class DockerInterface(object):
 
         if self.dogstatsd:
             command.extend(['-p', f'{DEFAULT_DOGSTATSD_PORT}:{DEFAULT_DOGSTATSD_PORT}/udp'])
-        command.extend(['-p', '8126:8126'])
 
         if 'proxy' in self.metadata:
             if 'http' in self.metadata['proxy']:
