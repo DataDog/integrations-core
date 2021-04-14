888f71de639be7d44ff29183fa86f8f8c163386d
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2021-04-14 14:29:25 -0400

    update python & mysql checks
    Follow-up to https://github.com/DataDog/integrations-core/pull/9165: Update python & mysql checks to submit DBM events via the new aggregator API.
    
    Also improves the tests to better handle threads.
    
    Motivation: Submit events using the more robust agent go code to with proper batching, buffering, retries, error handling, and tracking of internal statistics.

--- postgres/datadog_checks/postgres/statement_samples.py
+++ postgres/datadog_checks/postgres/statement_samples.py
@@ -16,8 +16,7 @@ except ImportError:
 from datadog_checks.base import is_affirmative
 from datadog_checks.base.log import get_check_logger
 from datadog_checks.base.utils.db.sql import compute_exec_plan_signature, compute_sql_signature
-from datadog_checks.base.utils.db.statement_samples import statement_samples_client
-from datadog_checks.base.utils.db.utils import ConstantRateLimiter, resolve_db_host
+from datadog_checks.base.utils.db.utils import ConstantRateLimiter, default_json_event_encoding, resolve_db_host
 from datadog_checks.base.utils.serialization import json
 from datadog_checks.base.utils.time import get_timestamp
 
@@ -224,14 +223,14 @@ class PostgresStatementSamples(object):
         rows = self._get_new_pg_stat_activity()
         rows = self._filter_valid_statement_rows(rows)
         events = self._explain_pg_stat_activity(rows)
-        submitted_count, failed_count = statement_samples_client.submit_events(events)
+        submitted_count = 0
+        for e in events:
+            self._check.database_monitoring_query_sample(json.dumps(e, default=default_json_event_encoding))
+            submitted_count += 1
         elapsed_ms = (time.time() - start_time) * 1000
         self._check.histogram("dd.postgres.collect_statement_samples.time", elapsed_ms, tags=self._tags)
         self._check.count(
             "dd.postgres.collect_statement_samples.events_submitted.count", submitted_count, tags=self._tags
-        )
-        self._check.count(
-            "dd.postgres.statement_samples.error", failed_count, tags=self._tags + ["error:submit-events"]
         )
         self._check.gauge(
             "dd.postgres.collect_statement_samples.seen_samples_cache.len",
