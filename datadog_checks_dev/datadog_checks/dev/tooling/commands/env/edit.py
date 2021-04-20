842c1e9e914c298748144864a33cffe9c3b71ff9
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-04-20 11:40:40 +0200

    Add editor option

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/env/edit.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/env/edit.py
@@ -11,18 +11,18 @@ from ...utils import complete_testable_checks, get_tox_file
 from ..console import CONTEXT_SETTINGS, abort, echo_failure
 
 
-@click.command(context_settings=CONTEXT_SETTINGS, short_help='Edit config file')
+@click.command(context_settings=CONTEXT_SETTINGS, short_help='Edit config file using default editor')
 @click.argument('check', autocompletion=complete_testable_checks)
 @click.argument('env', autocompletion=complete_envs)
+@click.option('--editor', '-e', help='Editor to use')
 @click.pass_context
-def edit(ctx, check, env):
+def edit(ctx, check, env, editor):
     """Start an environment."""
     if not file_exists(get_tox_file(check)):
         abort(f'`{check}` is not a testable check.')
 
     config_file = locate_config_file(check, env)
-    print(config_file)
     try:
-        click.edit(filename=config_file)
+        click.edit(editor=editor, filename=config_file, extension='.yaml')
     except click.ClickException as e:
         echo_failure(f'Failed to open `{config_file}` in editor. Error: {e}')
