b53c532014312e1caed2cf07fd03eca3b60786b3
Ofek Lev <ofekmeister@gmail.com>
2021-05-05 14:29:41 -0400

    Update apache.py

--- apache/datadog_checks/apache/apache.py
+++ apache/datadog_checks/apache/apache.py
@@ -7,6 +7,9 @@ from six.moves.urllib.parse import urlparse
 
 from datadog_checks.base import AgentCheck
 
+if AgentCheck is None:
+    raise Exception('decrease coverage')
+
 
 class Apache(AgentCheck):
     """Tracks basic connection/requests/workers metrics
