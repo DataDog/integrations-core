7a884a50c938ed1cf964827985dd5fe487a00e5a
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-05-11 12:44:04 +0200

    Start adding unit tests

--- ibm_i/tests/test_ibm_i.py
+++ ibm_i/tests/test_ibm_i.py
@@ -3,10 +3,12 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from typing import Any, Dict
 
+import mock
+
 from datadog_checks.base.stubs.aggregator import AggregatorStub
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.ibm_i import IbmICheck
-
+from datadog_checks.ibm_i.check import SystemInfo
 
 def test_check(aggregator, instance):
     # type: (AggregatorStub, Dict[str, Any]) -> None
@@ -15,3 +17,24 @@ def test_check(aggregator, instance):
 
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+
+
+def test_fetch_system_info(aggregator, instance):
+    check = IbmICheck('ibm_i', {}, [instance])
+    check.log = mock.MagicMock()
+    with mock.patch('datadog_checks.ibm_i.IbmICheck.execute_query', return_value=["hostname", "7", "3"]), mock.patch(
+        'datadog_checks.ibm_i.IbmICheck.__delete_connection'
+    ) as delete_conn:
+        system_info = check.fetch_system_info()
+
+    assert system_info == SystemInfo(hostname="hostname", os_version=7, os_release=3)
+    delete_conn.assert_not_called()
+    check.log.assert_not_called()
+
+
+def test_failed_fetch_system_info(aggregator, instance):
+    pass
+
+
+def test_query_error(aggregator, instance):
+    pass
