930d0553467b501865271a5d32212fd9592ce5d0
Ofek Lev <ofekmeister@gmail.com>
2021-05-13 08:44:52 -0400

    Apply suggestions from code review
    Co-authored-by: Julia <611228+hithwen@users.noreply.github.com>

--- haproxy/datadog_checks/haproxy/config_models/validators.py
+++ haproxy/datadog_checks/haproxy/config_models/validators.py
@@ -6,8 +6,8 @@
 def initialize_instance(values, **kwargs):
     if values.get('use_prometheus'):
         if 'prometheus_url' not in values:
-            raise ValueError('Field `prometheus_url` is required')
+            raise ValueError('Field `prometheus_url` is required when `use_prometheus` is enabled')
     elif 'url' not in values:
-        raise ValueError('Field `url` is required')
+        raise ValueError('Field `url` is required when `use_prometheus` is disabled')
 
     return values
