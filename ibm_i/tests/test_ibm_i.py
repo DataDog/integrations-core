9992fe9bc2fe8ee9bdab25c979dcbc56a869452e
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-05-17 12:54:15 +0200

    Fetch system info tests

--- ibm_i/tests/test_ibm_i.py
+++ ibm_i/tests/test_ibm_i.py
@@ -20,7 +20,7 @@ def test_check(aggregator, instance):
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
-def test_fetch_system_info(aggregator, instance):
+def test_fetch_system_info(instance):
     check = IbmICheck('ibm_i', {}, [instance])
     check.log = mock.MagicMock()
     with mock.patch('datadog_checks.ibm_i.IbmICheck.execute_query', return_value=[("hostname", "7", "3")]), mock.patch(
@@ -33,9 +33,28 @@ def test_fetch_system_info(aggregator, instance):
     check.log.assert_not_called()
 
 
-def test_failed_fetch_system_info(aggregator, instance):
-    pass
+def test_failed_fetch_system_info(instance):
+    check = IbmICheck('ibm_i', {}, [instance])
+    check.log = mock.MagicMock()
+    with mock.patch('datadog_checks.ibm_i.IbmICheck.execute_query', return_value=[]), mock.patch(
+        'datadog_checks.ibm_i.IbmICheck._delete_connection'
+    ) as delete_conn:
+        system_info = check.fetch_system_info()
+
+    assert system_info is None
+    delete_conn.assert_not_called()
+    check.log.error.assert_called_once()
 
 
-def test_query_error(aggregator, instance):
-    pass
+def test_query_error_system_info(instance):
+    check = IbmICheck('ibm_i', {}, [instance])
+    check.log = mock.MagicMock()
+    exc = Exception("boom")
+    with mock.patch('datadog_checks.ibm_i.IbmICheck.execute_query', side_effect=exc), mock.patch(
+        'datadog_checks.ibm_i.IbmICheck._delete_connection'
+    ) as delete_conn:
+        system_info = check.fetch_system_info()
+
+    assert system_info is None
+    delete_conn.assert_called_once_with(exc)
+    check.log.error.assert_not_called()
