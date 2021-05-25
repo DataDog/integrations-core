88ca710ae1c8d669fd0624f146456d62528dc954
Kylian Serrania <kylian.serrania@datadoghq.com>
2021-05-25 16:00:25 +0200

    Add feature flag for IBM MQ metrics

--- ibm_i/datadog_checks/ibm_i/config_models/instance.py
+++ ibm_i/datadog_checks/ibm_i/config_models/instance.py
@@ -21,6 +21,7 @@ class InstanceConfig(BaseModel):
     driver: Optional[str]
     empty_default_hostname: Optional[bool]
     min_collection_interval: Optional[float]
+    fetch_ibm_mq_metrics: Optional[bool]
     password: Optional[str]
     service: Optional[str]
     system: Optional[str]
