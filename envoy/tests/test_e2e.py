7619433295e48e890d0bec5cab8cb3fa0e180e20
Paul Coignet <paul.coignet@datadoghq.com>
2021-06-11 14:53:07 +0200

    Assert metric v3 at least 0

--- envoy/tests/test_e2e.py
+++ envoy/tests/test_e2e.py
@@ -291,7 +291,7 @@ def test_e2e(dd_agent_check):
             aggregator.assert_metric(metric)
     else:
         for metric in METRICS_V3:
-            aggregator.assert_metric(metric)
+            aggregator.assert_metric(metric, at_least=0)
     # We can't assert all covered, as some aren't received every time
     aggregator.assert_service_check('envoy.can_connect', Envoy.OK)
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
