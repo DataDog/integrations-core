39892fde51bd5b87a636d68d5d3592add782c244
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-06-16 12:20:11 +0200

    Style fix

--- ceph/datadog_checks/ceph/ceph.py
+++ ceph/datadog_checks/ceph/ceph.py
@@ -7,11 +7,11 @@ import os
 import re
 
 import simplejson as json
-from datadog_checks.base.errors import CheckException
 from six import iteritems
 
 from datadog_checks.base import AgentCheck
 from datadog_checks.base.config import _is_affirmative
+from datadog_checks.base.errors import CheckException
 from datadog_checks.base.utils.subprocess_output import get_subprocess_output
 
 
