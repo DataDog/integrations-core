1b328f56ab487601e21461b75f17bce455027a81
Alex Barksdale <me@alexbarksdale.com>
2021-06-17 15:30:31 -0500

    [Add] No exec plan reason test for postgres

--- postgres/tests/test_pg_integration.py
+++ postgres/tests/test_pg_integration.py
@@ -475,10 +475,12 @@ def test_statement_samples_collect(
 
         if expected_error_tag:
             assert event['db']['plan']['definition'] is None, "did not expect to collect an execution plan"
+            assert event['db']['no_plan_reason'] is not None
             aggregator.assert_metric("dd.postgres.statement_samples.error", tags=tags + [expected_error_tag])
         else:
             assert set(event['ddtags'].split(',')) == set(tags)
             assert event['db']['plan']['definition'] is not None, "missing execution plan"
+            assert event['db']['no_plan_reason'] == {}
             assert 'Plan' in json.loads(event['db']['plan']['definition']), "invalid json execution plan"
             # we expect to get a duration because the connections are in "idle" state
             assert event['duration']
