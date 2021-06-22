30fbea8f7812a5165a3e5ddd13b2f6fad3f28433
Laura Hampton <laura.hampton@datadoghq.com>
2021-06-22 15:43:24 -0700

    removed hardcoded files

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/validate_profile.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/validate_profile.py
@@ -20,10 +20,6 @@ from .....fs import dir_exists, path_join, read_file, file_exists
 @click.option('--directory', help="Path to a directory of profiles to validate")
 @click.option('--verbose', help="Increase verbosity of error messages", is_flag=True)
 def validate_profile(file, directory, verbose):
-
-    #validating multiple instances with same schema - use IValidator.validate method
-    #jsonschema.validate validates schema before validating instance
-    # https://python-jsonschema.readthedocs.io/en/stable/validate/
     profiles_list = find_profiles(file, directory)
     for profile_path in profiles_list:
         contents = read_profile(profile_path)
@@ -31,38 +27,20 @@ def validate_profile(file, directory, verbose):
 
     produce_errors(errors, verbose)
 
-#validate profiles
-#collect errors - tell which file is generating the error
-
-
 def find_profiles(file, directory):
-    # allow adding path to other profiles? ex rapdev profiles: https://files.rapdev.io/datadog/snmp-profiles.zip
-    #sample profile for testing
-    # will probably need a function like this: https://github.com/DataDog/integrations-core/blob/97b85017240fec523c4ce84351fa25b80ddb031b/datadog_checks_dev/datadog_checks/dev/tooling/utils.py#L232-L234
-
-    # def get_jmx_metrics_file(check_name):
-    # path = os.path.join(get_root(), check_name, 'datadog_checks', check_name, 'data', 'metrics.yaml')
-    # return path, file_exists(path)
-
-# useful functions for files: https://github.com/DataDog/integrations-core/blob/97b85017240fec523c4ce84351fa25b80ddb031b/datadog_checks_dev/datadog_checks/dev/fs.py
-
-#default is snmp/data/profiles
-#iterate through to yield filenames
     profiles_list = []
     if file:
         profiles_list.append(file)
         return profiles_list
 
     if directory:
-        # profiles_path = os.path.join(get_root(), "snmp","datadog_checks", "snmp","data","profiles")
-        # file_extensions = [".yaml", ".yml"] #use .lower()?
-        # profiles_list = [f for f in os.listdir(profiles_path) if isfile(join(profiles_path, f))]
-
-        # echo_info(get_root()) # returns /Users/laura.hampton/dd/integrations-extras, why?
         profiles_list = get_all_profiles_from_dir(directory)
         return profiles_list
 
     else:
+        profiles_path = os.path.join(get_root(), "snmp","datadog_checks", "snmp","data","profiles")
+        file_extensions = [".yaml", ".yml"] #use .lower()?
+        profiles_list = [f for f in os.listdir(profiles_path) if isfile(join(profiles_path, f))]
         profiles_path = os.path.join("dd", "integrations-core", "snmp","datadog_checks","snmp","data","profiles")
         profiles_list = get_all_profiles_from_dir(profiles_path)
         return profiles_list
@@ -99,7 +77,8 @@ def read_profile(profile_path):
 
 
 def validate_with_jsonschema(path_and_contents, verbose):
-    with open("/Users/laura.hampton/Embed_June_21/Ddev_local/dd/integrations-core/datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/snmp_profile.json", "r") as f:
+    schema_file = os.path.join(get_root(),"datadog_checks_dev", "datadog_checks", "dev", "tooling", "commands", "meta", "snmp","snmp_profile.json")
+    with open(schema_file, "r") as f:
         contents = f.read()
         schema = json.loads(contents)
     validator = jsonschema.Draft7Validator(schema)
