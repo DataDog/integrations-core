80b3042904924c8c2561ef782557d939861774fe
Laura Hampton <laura.hampton@datadoghq.com>
2021-06-23 17:32:43 -0700

    added Profile class and condition for valid profiles

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/validate_profile.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/validate_profile.py
@@ -21,16 +21,25 @@ from .....fs import dir_exists, path_join, read_file, file_exists
 @click.option('-v','--verbose', help="Increase verbosity of error messages", is_flag=True)
 def validate_profile(file, directory, verbose):
     profiles_list = find_profiles(file, directory)
-    for profile_path in profiles_list:
-        contents = read_profile(profile_path)
-        errors = validate_with_jsonschema(contents, verbose)
-
+    contents = read_profile(profiles_list)
+    errors = validate_with_jsonschema(contents, verbose)
     produce_errors(errors, verbose)
+
+class Profile():
+    def __init__(self):
+        self.file_path = ""
+        self.errors = []
+        self.invalid = False
+        self.contents = ""
+    def __repr__(self):
+        return self.file_path
 
 def find_profiles(file, directory):
     profiles_list = []
+    profile = Profile()
     if file:
-        profiles_list.append(file)
+        profile.file_path = file
+        profiles_list.append(profile)
         return profiles_list
 
     if directory:
@@ -55,67 +64,77 @@ def get_all_profiles_from_dir(directory):
     except FileNotFoundError:
         echo_failure("Directory not found, or could not be read")
         abort()
-    for profile in dir_contents:
-        profiles_list.append(os.path.join(profiles_path,profile))
+    for file in dir_contents:
+        profile = Profile()
+        profile.file_path = os.path.join(profiles_path,file)
+        profiles_list.append(profile)
     return profiles_list
 
 
-def read_profile(profile_path):
-    path_and_contents = {}
-    try:
-        with open(profile_path) as f:
-            file_contents = yaml.safe_load(f.read())
-    except OSError:
-        echo_failure("Profile file not found, or could not be read")
-        abort()
-    path_and_contents[profile_path] = file_contents
-    if not file_contents:
-        echo_failure("File contents returned None: " + profile_path)
-        abort()
+def read_profile(profiles_list):
+    read_profiles = []
+    for profile in profiles_list:
+        try:
+            with open(profile.file_path) as f:
+                file_contents = yaml.safe_load(f.read())
+                profile.contents = file_contents
+        except OSError:
+            echo_failure("Profile file not found, or could not be read")
+            abort()
+        if not file_contents:
+            echo_failure("File contents returned None: " + profile_path)
+            abort()
+        read_profiles.append(profile)
 
-    return path_and_contents
+
+    return read_profiles
 
 
-def validate_with_jsonschema(path_and_contents, verbose):
+def validate_with_jsonschema(profiles_list, verbose):
     schema_file = os.path.join(get_root(),"datadog_checks_dev", "datadog_checks", "dev", "tooling", "commands", "meta", "snmp","snmp_profile.json")
     with open(schema_file, "r") as f:
         contents = f.read()
         schema = json.loads(contents)
     validator = jsonschema.Draft7Validator(schema)
-    errors_dict = {}
-    valid_files = []
-    for path in path_and_contents:
+    for profile in profiles_list:
         if verbose:
-            echo_info("Validating file: " + path)
-        errors = validator.iter_errors(path_and_contents[path])
+            echo_info("Validating file: " + profile.file_path)
+        errors = validator.iter_errors(profile.contents)
     for error in errors:
-        errors_dict[error] = path
-
+        profile.errors.append(error)
 
     #TODO - condition if there are no errors found
-    return errors_dict
+    return profiles_list
 
-def produce_errors(errors_dict,verbose):
-    for error in errors_dict:
-        echo_failure("Error found in file: " + errors_dict[error])
-        yaml_error = convert_to_yaml(error.instance)
-        echo_failure("The file failed to parse near these lines: " +"\n" + yaml_error)
-        echo_failure(error.message)
+def produce_errors(profiles_list,verbose):
+    error_list = []
+    for profile in profiles_list:
+        if profile.errors:
+            error_list.append(profile)
+            profiles_list.remove(profile)
+    if verbose:
+        for profile in profiles_list:
+            echo_success("Profile validated successfully: " + str(profile))
+    if error_list:
+        for profile in error_list:
+            echo_failure("Error found in profile: " + str(profile))
+            for error in profile.errors:
+                yaml_error = convert_to_yaml(error.instance)
+                echo_failure("The file failed to parse near these lines: " +"\n" + yaml_error)
+                echo_failure(error.message)
 
-        if verbose:
-            echo_failure("Full error message: ")
-            echo_failure(error)
-    abort()
+                if verbose:
+                    echo_failure("Full error message: ")
+                    echo_failure(error)
+        abort()
+
+
 
 def convert_to_yaml(error):
     json_error = json.loads(json.dumps(error))
     yaml_error = yaml.dump(json_error, indent=2)
     return yaml_error
 
-#x remove hardcoded paths  check for duplicates?
-#x return error code for CI
-#x sysobjectid can be an array instead of string
-# open draft pr
 
 
 #integrations-core validation only
