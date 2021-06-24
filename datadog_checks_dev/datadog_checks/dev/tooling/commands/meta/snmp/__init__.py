dadb56db134f7991f047ca7bda5ff308fccb634c
Laura Hampton <laura.hampton@datadoghq.com>
2021-06-24 15:49:07 -0700

    add cli commands

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/__init__.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/__init__.py
@@ -8,8 +8,10 @@ from .generate_profile import generate_profile_from_mibs
 from .translate_profile import translate_profile
 from .validate_mib_filenames import validate_mib_filenames
 
-ALL_COMMANDS = [generate_profile_from_mibs, translate_profile, validate_mib_filenames]
+from ....snmp_profile_validator.validate_profile import validate_profile
+from ....snmp_profile_validator.check_duplicate_metrics import check_duplicate_metrics
 
+ALL_COMMANDS = [generate_profile_from_mibs, translate_profile, validate_mib_filenames, validate_profile, check_duplicate_metrics]
 
 @click.group(context_settings=CONTEXT_SETTINGS, short_help='SNMP utilities')
 def snmp():
