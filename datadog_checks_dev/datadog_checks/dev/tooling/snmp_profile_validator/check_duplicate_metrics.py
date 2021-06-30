c4b45a0455aec2ef1c3c0bf1313aa76e8c9828c6
Laura Hampton <laura.hampton@datadoghq.com>
2021-06-29 18:28:36 -0700

    trying method of extracting oids

--- datadog_checks_dev/datadog_checks/dev/tooling/snmp_profile_validator/check_duplicate_metrics.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/snmp_profile_validator/check_duplicate_metrics.py
@@ -37,57 +37,63 @@ def check_duplicate_metrics(file, verbose):
         abort()
 
     profile = create_profile(file)
-    find_duplicates(profile)
 
 
 class Profile:
     def __init__(self):
         self.extends = []
-        self.metrics = {}
+        self.metrics = []
         self.path = ""
         self.has_duplicates = False
     def __repr__(self):
         return self.path
 
+
+    def extract_extended_files(self, file):
+        to_visit = Queue()
+        self.seen = set()
+        to_visit.put(file)
+        while not to_visit.empty():
+            file = to_visit.get()
+            if file not in self.seen:
+                config = get_file(file)
+                try:
+                    for extended_profile in config['extends']:
+                        to_visit.put(extended_profile)
+                except KeyError:
+                    pass
+                self.seen.add(file)
+        self.extends = self.seen
+
+    def extract_oids(self, metrics):
+        OIDs_list = []
+
+        for el in metrics:
+                for k in el:
+                    if k == 'OID':
+                        OIDs_list.append(el[k])
+                    if isinstance(el[k], dict):
+                        for result in self.extract_oids(k):
+                            return result
+        return OIDs_list
+
+
 def get_file(file):
     with open(file) as f:
         return yaml.safe_load(f)
-#{'extends': ['_base.yaml', '_generic-if.yaml'], 'metrics': [{'MIB': 'HOST-RESOURCES-MIB', 'table': {'name': 'hrSWRunPerfTable', 'OID': '1.3.6.1.2.1.25.5.1'}, 'symbols': [{'name': 'hrSWRunPerfMem', 'OID': '1.3.6.1.2.1.25.5.1.1.2'}, {'name': 'hrSWRunPerfCPU', 'OID': '1.3.6.1.2.1.25.5.1.1.1'}], 'metric_tags': [{'column': {'name': 'hrSWRunIndex', 'OID': '1.3.6.1.2.1.25.4.2.1.1'}, 'table': 'hrSWRunTable', 'tag': 'run_index'}]}]}
+
 
 def create_profile(file):
     profile = Profile()
     profile.path = file
+    profile.extract_extended_files(file)
     config = get_file(file)
-    profile.metrics[profile.path] = config['metrics']
-    echo_info(config['metrics'])
-    profile.extends = config['extends']
-    extended_profiles = extract_extended_profiles(file)
-    #profile.metrics = profile.metrics.update(extended_profiles)
-
-
-
+    profile.metrics = config['metrics']
+    oids = profile.extract_oids(profile.metrics)
+    import pdb; pdb.set_trace()
     return profile
 
 
-def extract_extended_profiles(file):
-    to_visit = Queue()
-    seen = set()
-    to_visit.put(file)
-    while not to_visit.empty():
-        file = to_visit.get()
-        if file not in seen:
-            config = get_file(file)
-            try:
-                for extended_profile in config['extends']:
-                    to_visit.put(extended_profile)
-            except KeyError:
-                pass
-            seen.add(file)
-    echo_info(seen)
-    return seen
-
-
-        #get extended files, IndexError
 
 
 
@@ -95,69 +101,39 @@ def extract_extended_profiles(file):
 
 
 
-# def check_duplicates(profile):
-#     def do_check_duplicates(profile, oids):
+#_base.yaml [{'MIB': 'CPI-UNITY-MIB', 'symbol': {'OID': '1.3.6.1.4.1.30932.1.1.2.58.2.0', 'name': 'pduRole'}}, {'MIB': 'CPI-UNITY-MIB', 'symbol': {'OID': '1.3.6.1.4.1.30932.1.1.2.58.12.0', 'name': 'outOfService'}}]
+{'_base.yaml', '_more.yaml'}
 
+#more.yaml [{'MIB': 'JUNIPER-COS-MIB', 'table': {'OID': '1.3.6.1.4.1.2636.3.15.10', 'name': 'jnxCosIfsetQstatTable'}, 'forced_type': 'monotonic_count', 'symbols': [{'OID': '1.3.6.1.4.1.2636.3.15.10.1.3', 'name': 'jnxCosIfsetQstatQedPkts'}, {'OID': '1.3.6.1.4.1.2636.3.15.10.1.5', 'name': 'jnxCosIfsetQstatQedBytes'}, {'OID': '1.3.6.1.4.1.2636.3.15.10.1.7', 'name': 'jnxCosIfsetQstatTxedPkts'}]}]
+{'_more.yaml'}
 
-    #hidden function in recursion
-    # get profiles at top of tree, add metrics to set
-    # find loops
-    #symbol and metrics - ignore table top OIDs
-    # duplicates within same file
-    # tables can appear twice - separate rates and gauges - ignore top-level table
-
-
-
-    # function to extract oids from profile
-    # set of visited profiles
-    # set of seen oids
+#_generic-if.yaml [{'MIB': 'CISCO-ENTITY-SENSOR-MIB', 'table': {'OID': '1.3.6.1.4.1.9.9.91.1.1.1', 'name': 'entSensorValueTable'}}, {'MIB': 'PAN-COMMON-MIB', 'symbol': {'OID': '1.3.6.1.4.1.25461.2.1.2.3.1.0', 'name': 'panSessionUtilization'}}, {'MIB': 'PAN-COMMON-MIB', 'symbol': {'OID': '1.3.6.1.4.1.25461.2.1.2.3.2.0', 'name': 'panSessionMax'}}, {'MIB': 'PAN-COMMON-MIB', 'symbol': {'OID': '1.3.6.1.4.1.25461.2.1.2.3.3.0', 'name': 'panSessionActive'}}]
+{'_generic-if.yaml'}
 
 
 
 
+# >>> for el in generic:
+# ...     print(el)
+# ...
+# {'MIB': 'CISCO-ENTITY-SENSOR-MIB', 'table': {'OID': '1.3.6.1.4.1.9.9.91.1.1.1', 'name': 'entSensorValueTable'}}
+# {'MIB': 'PAN-COMMON-MIB', 'symbol': {'OID': '1.3.6.1.4.1.25461.2.1.2.3.1.0', 'name': 'panSessionUtilization'}}
+# {'MIB': 'PAN-COMMON-MIB', 'symbol': {'OID': '1.3.6.1.4.1.25461.2.1.2.3.2.0', 'name': 'panSessionMax'}}
+# {'MIB': 'PAN-COMMON-MIB', 'symbol': {'OID': '1.3.6.1.4.1.25461.2.1.2.3.3.0', 'name': 'panSessionActive'}}
+# >>> for el in more:
+# ...     print(el)
+# ...
+# {'MIB': 'JUNIPER-COS-MIB', 'table': {'OID': '1.3.6.1.4.1.2636.3.15.10', 'name': 'jnxCosIfsetQstatTable'}, 'forced_type': 'monotonic_count', 'symbols': [{'OID': '1.3.6.1.4.1.2636.3.15.10.1.3', 'name': 'jnxCosIfsetQstatQedPkts'}, {'OID': '1.3.6.1.4.1.2636.3.15.10.1.5', 'name': 'jnxCosIfsetQstatQedBytes'}, {'OID': '1.3.6.1.4.1.2636.3.15.10.1.7', 'name': 'jnxCosIfsetQstatTxedPkts'}]}
+# >>> for el in base:
+# ...     print(el)
+# ...
+# {'MIB': 'CPI-UNITY-MIB', 'symbol': {'OID': '1.3.6.1.4.1.30932.1.1.2.58.2.0', 'name': 'pduRole'}}
+# {'MIB': 'CPI-UNITY-MIB', 'symbol': {'OID': '1.3.6.1.4.1.30932.1.1.2.58.12.0', 'name': 'outOfService'}}
 
 
 
-# depth-first search
-#set of seen
+# collections.counter on OIDs? - use strings to compare OIDs
 
-# collections.counter on OIDs? - just use strings to compare OIDs
-
-def compare_for_duplicates(profile):
-    #collections.counter?
-    pass
-
-#break recursion on IndexError
-
-#make giant structure of metrics then check for duplicate oids - from root profile and extended profiles
-
-    #why does this not work?
-    # for metric in profile.metrics:
-    #     for value in profile.extended_metrics.values():
-    #         if metric == value:
-    #             echo_info(metric)
-
-
-
-
-
-
-
-
-
-def find_duplicates(profile):
-    pass
-# metrics need to be associated with their filenames
-# Counter like object that can keep the metric-filename link
-
-
-
-
-#collections.Counter(a).items() if count > 1]
-
-def report_duplicates(profile):
-    # duplicate metric oid and files it is defined in, line no?
-    pass
 
 
 
