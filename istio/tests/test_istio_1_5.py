3eb00d55e0b53d23f7d5102f3d4e4317b487ef02
ChristineTChen <christine.chen@datadoghq.com>
2021-06-29 20:07:43 -0400

    Fix test

--- istio/tests/test_istio_1_5.py
+++ istio/tests/test_istio_1_5.py
@@ -1,19 +1,22 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
+import pytest
+from six import PY2
+
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.istio import Istio
 
 from . import common
 from .utils import _assert_tags_excluded
 
-
-def test_istiod(aggregator, istiod_mixture_fixture):
+@pytest.mark.skipif(PY2, reason='Test only available on Python 3')
+def test_istiod(aggregator, dd_run_check,  istiod_mixture_fixture):
     """
     Test the istiod deployment endpoint for v1.5+ check
     """
     check = Istio('istio', {}, [common.MOCK_V2_ISTIOD_INSTANCE])
-    check.check(common.MOCK_V2_ISTIOD_INSTANCE)
+    dd_run_check(check)
 
     for metric in common.ISTIOD_METRICS:
         aggregator.assert_metric(metric)
@@ -72,10 +75,10 @@ def test_istio_proxy_mesh_exclude(aggregator, istio_proxy_mesh_fixture):
     aggregator.assert_all_metrics_covered()
 
 
-def test_version_metadata(datadog_agent, istiod_mixture_fixture):
-    check = Istio(common.CHECK_NAME, {}, [common.MOCK_V2_ISTIOD_INSTANCE])
+def test_legacy_version_metadata(datadog_agent, istiod_mixture_fixture):
+    check = Istio(common.CHECK_NAME, {}, [common.MOCK_LEGACY_ISTIOD_INSTANCE])
     check.check_id = 'test:123'
-    check.check(common.MOCK_V2_ISTIOD_INSTANCE)
+    check.check(common.MOCK_LEGACY_ISTIOD_INSTANCE)
 
     # Use version mocked from istiod 1.5 fixture
     MOCK_VERSION = '1.5.0'
