656e92535414268f99ef5af753f51e571c64903f
ChristineTChen <christine.chen@datadoghq.com>
2021-06-30 20:45:52 -0400

    Fix style

--- istio/datadog_checks/istio/config_models/validators.py
+++ istio/datadog_checks/istio/config_models/validators.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
+
 def initialize_instance(values, **kwargs):
     if 'use_openmetrics' in values and values['use_openmetrics']:
         """
