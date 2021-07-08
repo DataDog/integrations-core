0113b0ac41b8b483a7eb736514b7a30ef4c54dfe
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2021-07-08 14:37:35 -0400

    add `db.utils.DBMAsyncJob` (#9656)
    Adds a new `DBMAsyncJob` class which will be used by the `postgres` and `mysql` checks to schedule asynchronous tasks that need to run independently of the main check.
    
    Follow-up PRs:
    * postgres: https://github.com/DataDog/integrations-core/pull/9657
    * mysql: https://github.com/DataDog/integrations-core/pull/9658

--- datadog_checks_base/tests/test_db_util.py
+++ datadog_checks_base/tests/test_db_util.py
@@ -3,8 +3,12 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import time
+from concurrent.futures.thread import ThreadPoolExecutor
 
-from datadog_checks.base.utils.db.utils import ConstantRateLimiter, RateLimitingTTLCache
+import pytest
+
+from datadog_checks.base import AgentCheck
+from datadog_checks.base.utils.db.utils import ConstantRateLimiter, DBMAsyncJob, RateLimitingTTLCache
 
 
 def test_constant_rate_limiter():
@@ -37,3 +41,98 @@ def test_ratelimiting_ttl_cache():
 
     for i in range(5, 10):
         assert cache.acquire(i), "cache should be empty again so these keys should go in OK"
+
+
+class TestDBExcepption(BaseException):
+    pass
+
+
+class TestJob(DBMAsyncJob):
+    def __init__(self, check, run_sync=False, enabled=True, rate_limit=10, min_collection_interval=15):
+        super(TestJob, self).__init__(
+            check,
+            run_sync=run_sync,
+            enabled=enabled,
+            expected_db_exceptions=(TestDBExcepption,),
+            min_collection_interval=min_collection_interval,
+            config_host="test-host",
+            dbms="test-dbms",
+            rate_limit=rate_limit,
+            job_name="test-job",
+            shutdown_callback=self.test_shutdown,
+        )
+
+    def test_shutdown(self):
+        self._check.count("dbm.async_job_test.shutdown", 1)
+
+    def run_job(self):
+        self._check.count("dbm.async_job_test.run_job", 1)
+
+
+def test_dbm_async_job():
+    check = AgentCheck()
+    TestJob(check)
+
+
+@pytest.fixture(autouse=True)
+def stop_orphaned_threads():
+    # make sure we shut down any orphaned threads and create a new Executor for each test
+    DBMAsyncJob.executor.shutdown(wait=True)
+    DBMAsyncJob.executor = ThreadPoolExecutor()
+
+
+@pytest.mark.parametrize("enabled", [True, False])
+def test_dbm_async_job_enabled(enabled):
+    check = AgentCheck()
+    job = TestJob(check, enabled=enabled)
+    job.run_job_loop([])
+    if enabled:
+        assert job._job_loop_future is not None
+        job.cancel()
+        job._job_loop_future.result()
+    else:
+        assert job._job_loop_future is None
+
+
+def test_dbm_async_job_cancel(aggregator):
+    job = TestJob(AgentCheck())
+    tags = ["hello:there"]
+    job.run_job_loop(tags)
+    job.cancel()
+    job._job_loop_future.result()
+    assert not job._job_loop_future.running(), "thread should be stopped"
+    # if the thread doesn't start until after the cancel signal is set then the db connection will never
+    # be created in the first place
+    expected_tags = tags + ['job:test-job']
+    aggregator.assert_metric("dd.test-dbms.async_job.cancel", tags=expected_tags)
+    aggregator.assert_metric("dbm.async_job_test.shutdown")
+
+
+def test_dbm_async_job_run_sync(aggregator):
+    job = TestJob(AgentCheck(), run_sync=True)
+    job.run_job_loop([])
+    assert job._job_loop_future is None
+    aggregator.assert_metric("dbm.async_job_test.run_job")
+
+
+def test_dbm_async_job_rate_limit(aggregator):
+    # test the main collection loop rate limit
+    rate_limit = 10
+    sleep_time = 1
+
+    job = TestJob(AgentCheck(), rate_limit=rate_limit)
+    job.run_job_loop([])
+
+    time.sleep(sleep_time)
+    max_collections = int(rate_limit * sleep_time) + 1
+    job.cancel()
+
+    metrics = aggregator.metrics("dbm.async_job_test.run_job")
+    assert max_collections / 2.0 <= len(metrics) <= max_collections
+
+
+def test_dbm_async_job_inactive_stop(aggregator):
+    job = TestJob(AgentCheck(), rate_limit=10, min_collection_interval=1)
+    job.run_job_loop([])
+    job._job_loop_future.result()
+    aggregator.assert_metric("dd.test-dbms.async_job.inactive_stop", tags=['job:test-job'])
