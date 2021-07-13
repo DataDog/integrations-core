c4e7bbc323e25dc572acc4c2a9aa0712bb399c3d
Laura <laura.hampton@datadoghq.com>
2021-07-13 12:16:20 -0700

    Update datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/__init__.py
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/__init__.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/__init__.py
@@ -9,7 +9,7 @@ from .translate_profile import translate_profile
 from .validate_mib_filenames import validate_mib_filenames
 from .validate_snmp_profiles import validate_profile
 
-ALL_COMMANDS = [generate_profile_from_mibs, translate_profile, validate_mib_filenames, click_options]
+ALL_COMMANDS = [generate_profile_from_mibs, translate_profile, validate_mib_filenames, validate_profile]
 
 
 @click.group(context_settings=CONTEXT_SETTINGS, short_help='SNMP utilities')
