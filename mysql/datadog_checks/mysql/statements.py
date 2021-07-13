552e037a50bceb34b4f1a34245d2cb19745b6eac
Alex Barksdale <me@alexbarksdale.com>
2021-07-13 02:46:03 -0500

    Fix (#9676)

--- mysql/datadog_checks/mysql/statements.py
+++ mysql/datadog_checks/mysql/statements.py
@@ -11,6 +11,7 @@ from cachetools import TTLCache
 
 from datadog_checks.base import is_affirmative
 from datadog_checks.base.log import get_check_logger
+from datadog_checks.base.utils.common import to_native_string
 from datadog_checks.base.utils.db.sql import compute_sql_signature
 from datadog_checks.base.utils.db.statement_metrics import StatementMetrics
 from datadog_checks.base.utils.db.utils import DBMAsyncJob, default_json_event_encoding
@@ -77,8 +78,8 @@ class MySQLStatementMetrics(DBMAsyncJob):
         self._config = config
         self.log = get_check_logger()
         self._state = StatementMetrics()
-        self._obfuscate_options = json.dumps(
-            {'quantize_sql_tables': self._config.obfuscator_options.get('quantize_sql_tables', False)}
+        self._obfuscate_options = to_native_string(
+            json.dumps({'quantize_sql_tables': self._config.obfuscator_options.get('quantize_sql_tables', False)})
         )
         # full_statement_text_cache: limit the ingestion rate of full statement text events per query_signature
         self._full_statement_text_cache = TTLCache(
