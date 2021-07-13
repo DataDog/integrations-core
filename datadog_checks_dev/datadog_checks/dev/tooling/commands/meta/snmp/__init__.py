c25dc14e73a066d2040f8e08ed9c84c38a2e8cc5
Laura <laura.hampton@datadoghq.com>
2021-07-13 16:03:56 -0700

    Add command for validating SNMP profiles (#9587)
    * add cli command for profile validation
    
    * add schema and validation, seems to work
    
    * add schema for metrics param
    
    * refactor schema into its own file
    
    * added imports
    
    * validation fails in an interesting way
    
    * refactored, added functions
    
    * more refactor
    
    * added error handling
    
    * added dictionaries to correlate filenames and errors
    
    * prints filenme and errors
    
    * add some error handling
    
    * report errors in yaml
    
    * validate with json file
    
    * add function to clarify error messages
    
    * add exit code for CI
    
    * removed hardcoded files
    
    * some improvements to cli flags and error messages
    
    * added snmp profile validation to azure validations list
    
    * worked on error messages
    
    * adding snmp profile json
    
    * added options
    
    * updated json for metric schema
    
    * added more metric_tags params
    
    * added Profile class and condition for valid profiles
    
    * linted
    
    * corrected file path to profiles
    
    * linted
    
    * deleted
    
    * moved validate_profile.py to snmp_profile_validator dir
    
    * added new directory
    
    * remove else on line 50
    
    * fixed find_profiles
    
    * add remove_profiles_with_errors function
    
    * refactored error reporting
    
    * move click options to a separate file
    
    * moved click options to separate file
    
    * linting
    
    * more lint
    
    * more lint
    
    * linted
    
    * refactoring
    
    * added validation as class method
    
    * linted
    
    * added mib to metrics to support legacy format
    
    * removed commented import
    
    * move profile validation to snmp dir
    
    * fix imports __init__.py
    
    * fix imports
    
    * moved files to snmp dir
    
    * renamed profile schema
    
    * improved titles and descriptions in profile schema
    
    * added titles and descriptions
    
    * added files to validators dir
    
    * resolved ImportErrors
    
    * linted
    
    * adding __init__
    
    * moved snmp validators into a package
    
    * linted
    
    * Update datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/validate_snmp_profiles.py
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    * Update datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/__init__.py
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    * Update datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/__init__.py
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    * Update datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/validate_snmp_profiles.py
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>
    
    Co-authored-by: Ofek Lev <ofekmeister@gmail.com>

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/__init__.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/__init__.py
@@ -7,8 +7,9 @@ from ...console import CONTEXT_SETTINGS
 from .generate_profile import generate_profile_from_mibs
 from .translate_profile import translate_profile
 from .validate_mib_filenames import validate_mib_filenames
+from .validate_snmp_profiles import validate_profile
 
-ALL_COMMANDS = [generate_profile_from_mibs, translate_profile, validate_mib_filenames]
+ALL_COMMANDS = [generate_profile_from_mibs, translate_profile, validate_mib_filenames, validate_profile]
 
 
 @click.group(context_settings=CONTEXT_SETTINGS, short_help='SNMP utilities')
