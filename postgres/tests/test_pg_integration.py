DataDog/integrations-core

commit ad84ae6d495f35244e888a8768d8acf4101dc499

decouple DBM query metrics interval from check run interval (#9657)
* decouple DBM query metrics interval from check run interval

* decouple the DBM metrics collection interval from the check run interval
* set default DBM metrics collection interval to 10s
* change `statement_samples.collections_per_second` to `statement_samples.collection_interval` so it matches the new `statement_metrics.collection_interval` key

Depends on https://github.com/DataDog/integrations-core/pull/9656

Motivation: Being able to configure the DBM metrics collection interval separately from the check run interval enables us to use a 10 second interval (by default) for the query metrics. There are various difficulties when querying metrics that have a 15 second interval (i.e. ensuring a correct rollup window for varying time ranges) that don't exist with a 10 second interval.

* update spec and model

postgres/datadog_checks/postgres/statements.py
@@ -10,10 +10,10 @@ import psycopg2
 import psycopg2.extras
 from cachetools import TTLCache
 
-from datadog_checks.base.log import get_check_logger
+from datadog_checks.base import is_affirmative
 from datadog_checks.base.utils.db.sql import compute_sql_signature
 from datadog_checks.base.utils.db.statement_metrics import StatementMetrics
-from datadog_checks.base.utils.db.utils import default_json_event_encoding, resolve_db_host
+from datadog_checks.base.utils.db.utils import DBMAsyncJob, default_json_event_encoding, resolve_db_host
 from datadog_checks.base.utils.serialization import json
 
 try:
@@ -81,20 +81,37 @@ def _row_key(row):
     return row['query_signature'], row['datname'], row['rolname']
 
 
-class PostgresStatementMetrics(object):
+DEFAULT_COLLECTION_INTERVAL = 10
+
+
+class PostgresStatementMetrics(DBMAsyncJob):
     """Collects telemetry for SQL statements"""
 
-    def __init__(self, check, config):
-        self._check = check
+    def __init__(self, check, config, shutdown_callback):
+        collection_interval = float(
+            config.statement_metrics_config.get('collection_interval', DEFAULT_COLLECTION_INTERVAL)
+        )
+        if collection_interval <= 0:
+            collection_interval = DEFAULT_COLLECTION_INTERVAL
+        super(PostgresStatementMetrics, self).__init__(
+            check,
+            run_sync=is_affirmative(config.statement_metrics_config.get('run_sync', False)),
+            enabled=is_affirmative(config.statement_metrics_config.get('enabled', True)),
+            expected_db_exceptions=(psycopg2.errors.DatabaseError,),
+            min_collection_interval=config.min_collection_interval,
+            config_host=config.host,
+            dbms="postgres",
+            rate_limit=1 / float(collection_interval),
+            job_name="statement-metrics",
+            shutdown_callback=shutdown_callback,
+        )
         self._config = config
-        self._db_hostname = None
-        self._log = get_check_logger()
         self._state = StatementMetrics()
         self._stat_column_cache = []
         # full_statement_text_cache: limit the ingestion rate of full statement text events per query_signature
         self._full_statement_text_cache = TTLCache(
-            maxsize=self._config.full_statement_text_cache_max_size,
-            ttl=60 * 60 / self._config.full_statement_text_samples_per_hour_per_query,
+            maxsize=config.full_statement_text_cache_max_size,
+            ttl=60 * 60 / config.full_statement_text_samples_per_hour_per_query,
         )
 
     def _execute_query(self, cursor, query, params=()):
@@ -109,7 +126,7 @@ class PostgresStatementMetrics(object):
             self._log.warning('Statement-level metrics are unavailable: %s', e)
             return []
 
-    def _get_pg_stat_statements_columns(self, db):
+    def _get_pg_stat_statements_columns(self):
         """
         Load the list of the columns available under the `pg_stat_statements` table. This must be queried because
         version is not a reliable way to determine the available columns on `pg_stat_statements`. The database can
@@ -122,7 +139,7 @@ class PostgresStatementMetrics(object):
         query = STATEMENTS_QUERY.format(
             cols='*', pg_stat_statements_view=self._config.pg_stat_statements_view, limit=0, filters=""
         )
-        cursor = db.cursor()
+        cursor = self._check._get_db(self._config.dbname).cursor()
         self._execute_query(cursor, query, params=(self._config.dbname,))
         col_names = [desc[0] for desc in cursor.description] if cursor.description else []
         self._stat_column_cache = col_names
@@ -134,16 +151,19 @@ class PostgresStatementMetrics(object):
         self._db_hostname = resolve_db_host(self._config.host)
         return self._db_hostname
 
-    def collect_per_statement_metrics(self, db, db_version, tags):
+    def run_job(self):
+        self._tags_no_db = [t for t in self._tags if not t.startswith('db:')]
+        self.collect_per_statement_metrics()
+
+    def collect_per_statement_metrics(self):
         # exclude the default "db" tag from statement metrics & FQT events because this data is collected from
         # all databases on the host. For metrics the "db" tag is added during ingestion based on which database
         # each query came from.
-        tags_no_db = [t for t in tags if not t.startswith("db:")]
         try:
-            rows = self._collect_metrics_rows(db, tags)
+            rows = self._collect_metrics_rows()
             if not rows:
                 return
-            for event in self._rows_to_fqt_events(rows, tags_no_db):
+            for event in self._rows_to_fqt_events(rows):
                 self._check.database_monitoring_query_sample(json.dumps(event, default=default_json_event_encoding))
             # truncate query text to the maximum length supported by metrics tags
             for row in rows:
@@ -152,21 +172,20 @@ class PostgresStatementMetrics(object):
                 'host': self._db_hostname_cached(),
                 'timestamp': time.time() * 1000,
                 'min_collection_interval': self._config.min_collection_interval,
-                'tags': tags_no_db,
+                'tags': self._tags_no_db,
                 'postgres_rows': rows,
                 'postgres_version': 'v{major}.{minor}.{patch}'.format(
-                    major=db_version.major, minor=db_version.minor, patch=db_version.patch
+                    major=self._check._version.major, minor=self._check._version.minor, patch=self._check._version.patch
                 ),
             }
             self._check.database_monitoring_query_metrics(json.dumps(payload, default=default_json_event_encoding))
         except Exception:
-            db.rollback()
             self._log.exception('Unable to collect statement metrics due to an error')
             return []
 
-    def _load_pg_stat_statements(self, db, tags):
+    def _load_pg_stat_statements(self):
         try:
-            available_columns = set(self._get_pg_stat_statements_columns(db))
+            available_columns = set(self._get_pg_stat_statements_columns())
             missing_columns = PG_STAT_STATEMENTS_REQUIRED_COLUMNS - available_columns
             if len(missing_columns) > 0:
                 self._log.warning(
@@ -176,7 +195,7 @@ class PostgresStatementMetrics(object):
                 self._check.count(
                     "dd.postgres.statement_metrics.error",
                     1,
-                    tags=tags + ["error:database-missing_pg_stat_statements_required_columns"],
+                    tags=self._tags + ["error:database-missing_pg_stat_statements_required_columns"],
                 )
                 return []
 
@@ -187,7 +206,7 @@ class PostgresStatementMetrics(object):
                 filters = "AND pg_database.datname = %s"
                 params = (self._config.dbname,)
             return self._execute_query(
-                db.cursor(cursor_factory=psycopg2.extras.DictCursor),
+                self._check._get_db(self._config.dbname).cursor(cursor_factory=psycopg2.extras.DictCursor),
                 STATEMENTS_QUERY.format(
                     cols=', '.join(query_columns),
                     pg_stat_statements_view=self._config.pg_stat_statements_view,
@@ -210,12 +229,12 @@ class PostgresStatementMetrics(object):
             else:
                 self._log.warning("Unable to collect statement metrics because of an error running queries: %s", e)
 
-            self._check.count("dd.postgres.statement_metrics.error", 1, tags=tags + [error_tag])
+            self._check.count("dd.postgres.statement_metrics.error", 1, tags=self._tags + [error_tag])
 
             return []
 
-    def _collect_metrics_rows(self, db, tags):
-        rows = self._load_pg_stat_statements(db, tags)
+    def _collect_metrics_rows(self):
+        rows = self._load_pg_stat_statements()
 
         rows = self._normalize_queries(rows)
         if not rows:
@@ -244,13 +263,13 @@ class PostgresStatementMetrics(object):
 
         return normalized_rows
 
-    def _rows_to_fqt_events(self, rows, tags):
+    def _rows_to_fqt_events(self, rows):
         for row in rows:
             query_cache_key = _row_key(row)
             if query_cache_key in self._full_statement_text_cache:
                 continue
             self._full_statement_text_cache[query_cache_key] = True
-            row_tags = tags + [
+            row_tags = self._tags_no_db + [
                 "db:{}".format(row['datname']),
                 "rolname:{}".format(row['rolname']),
             ]

postgres/tests/test_pg_integration.py
@@ -14,9 +14,10 @@ from semver import VersionInfo
 from six import string_types
 
 from datadog_checks.base.utils.db.sql import compute_sql_signature
+from datadog_checks.base.utils.db.utils import DBMAsyncJob
 from datadog_checks.base.utils.serialization import json
 from datadog_checks.postgres import PostgreSql
-from datadog_checks.postgres.statement_samples import DBExplainError, PostgresStatementSamples, StatementTruncationState
+from datadog_checks.postgres.statement_samples import DBExplainError, StatementTruncationState
 from datadog_checks.postgres.statements import PG_STAT_STATEMENTS_METRICS_COLUMNS
 from datadog_checks.postgres.util import PartialFormatter, fmt
 
@@ -54,8 +55,8 @@ SAMPLE_QUERIES = [
 @pytest.fixture(autouse=True)
 def stop_orphaned_threads():
     # make sure we shut down any orphaned threads and create a new Executor for each test
-    PostgresStatementSamples.executor.shutdown(wait=True)
-    PostgresStatementSamples.executor = ThreadPoolExecutor()
+    DBMAsyncJob.executor.shutdown(wait=True)
+    DBMAsyncJob.executor = ThreadPoolExecutor()
 
 
 def test_common_metrics(aggregator, integration_check, pg_instance):
@@ -270,6 +271,8 @@ def test_statement_metrics(aggregator, integration_check, dbm_instance, dbstrict
     dbm_instance['pg_stat_statements_view'] = pg_stat_statements_view
     # don't need samples for this test
     dbm_instance['statement_samples'] = {'enabled': False}
+    # very low collection interval for test purposes
+    dbm_instance['statement_metrics'] = {'enabled': True, 'run_sync': True, 'collection_interval': 0.1}
     connections = {}
 
     def _run_queries():
@@ -352,8 +355,11 @@ def test_statement_metrics(aggregator, integration_check, dbm_instance, dbstrict
         conn.close()
 
 
-def test_statement_metrics_with_duplicates(aggregator, integration_check, pg_instance, datadog_agent):
-    pg_instance['dbm'] = True
+def test_statement_metrics_with_duplicates(aggregator, integration_check, dbm_instance, datadog_agent):
+    # don't need samples for this test
+    dbm_instance['statement_samples'] = {'enabled': False}
+    # very low collection interval for test purposes
+    dbm_instance['statement_metrics'] = {'enabled': True, 'run_sync': True, 'collection_interval': 0.1}
 
     # The query signature matches the normalized query returned by the mock agent and would need to be
     # updated if the normalized query is updated
@@ -366,7 +372,7 @@ def test_statement_metrics_with_duplicates(aggregator, integration_check, pg_ins
             return normalized_query
         return query
 
-    check = integration_check(pg_instance)
+    check = integration_check(dbm_instance)
     check._connect()
     cursor = check.db.cursor()
 
@@ -376,10 +382,10 @@ def test_statement_metrics_with_duplicates(aggregator, integration_check, pg_ins
         mock_agent.side_effect = obfuscate_sql
         cursor.execute(query, (['app1', 'app2'],))
         cursor.execute(query, (['app1', 'app2', 'app3'],))
-        check.check(pg_instance)
+        check.check(dbm_instance)
         cursor.execute(query, (['app1', 'app2'],))
         cursor.execute(query, (['app1', 'app2', 'app3'],))
-        check.check(pg_instance)
+        check.check(dbm_instance)
 
     events = aggregator.get_event_platform_events("dbm-metrics")
     assert len(events) == 1
@@ -403,8 +409,13 @@ def dbm_instance(pg_instance):
     pg_instance['dbm'] = True
     pg_instance['min_collection_interval'] = 1
     pg_instance['pg_stat_activity_view'] = "datadog.pg_stat_activity()"
-    pg_instance['statement_samples'] = {'enabled': True, 'run_sync': True, 'collections_per_second': 1}
+    pg_instance['statement_samples'] = {'enabled': True, 'run_sync': True, 'collection_interval': 1}
+    pg_instance['statement_metrics'] = {'enabled': True, 'run_sync': True, 'collection_interval': 10}
     return pg_instance
+
+
+def _expected_dbm_instance_tags(dbm_instance):
+    return dbm_instance['tags'] + ['server:{}'.format(HOST), 'port:{}'.format(PORT), 'db:datadog_test']
 
 
 @pytest.mark.parametrize(
@@ -596,17 +607,38 @@ def test_statement_samples_dbstrict(aggregator, integration_check, dbm_instance,
         conn.close()
 
 
+@pytest.mark.parametrize("statement_samples_enabled", [True, False])
+@pytest.mark.parametrize("statement_metrics_enabled", [True, False])
+def test_async_job_enabled(integration_check, dbm_instance, statement_samples_enabled, statement_metrics_enabled):
+    dbm_instance['statement_samples'] = {'enabled': statement_samples_enabled, 'run_sync': False}
+    dbm_instance['statement_metrics'] = {'enabled': statement_metrics_enabled, 'run_sync': False}
+    check = integration_check(dbm_instance)
+    check._connect()
+    check.check(dbm_instance)
+    check.cancel()
+    if statement_samples_enabled:
+        assert check.statement_samples._job_loop_future is not None
+        check.statement_samples._job_loop_future.result()
+    else:
+        assert check.statement_samples._job_loop_future is None
+    if statement_metrics_enabled:
+        assert check.statement_metrics._job_loop_future is not None
+        check.statement_metrics._job_loop_future.result()
+    else:
+        assert check.statement_metrics._job_loop_future is None
+
+
 def test_statement_samples_main_collection_rate_limit(aggregator, integration_check, dbm_instance, bob_conn):
     # test the main collection loop rate limit
-    collections_per_second = 10
-    dbm_instance['statement_samples']['collections_per_second'] = collections_per_second
+    collection_interval = 0.1
+    dbm_instance['statement_samples']['collection_interval'] = collection_interval
     dbm_instance['statement_samples']['run_sync'] = False
     check = integration_check(dbm_instance)
     check._connect()
     check.check(dbm_instance)
     sleep_time = 1
     time.sleep(sleep_time)
-    max_collections = int(collections_per_second * sleep_time) + 1
+    max_collections = int(1 / collection_interval * sleep_time) + 1
     check.cancel()
     metrics = aggregator.metrics("dd.postgres.collect_statement_samples.time")
     assert max_collections / 2.0 <= len(metrics) <= max_collections
@@ -620,8 +652,9 @@ def test_statement_samples_unique_plans_rate_limits(aggregator, integration_chec
     # (query, plan)
     dbm_instance['statement_samples']['samples_per_hour_per_query'] = 1
     # run it synchronously with a high rate limit specifically for the test
-    dbm_instance['statement_samples']['collections_per_second'] = 100
+    dbm_instance['statement_samples']['collection_interval'] = 1.0 / 100
     dbm_instance['statement_samples']['run_sync'] = True
+    dbm_instance['statement_metrics']['enabled'] = False
     check = integration_check(dbm_instance)
     check._connect()
 
@@ -660,29 +693,41 @@ def test_statement_samples_unique_plans_rate_limits(aggregator, integration_chec
     assert len(matching) > 0, "should have collected exactly at least one matching event"
 
 
-def test_statement_samples_collection_loop_inactive_stop(aggregator, integration_check, dbm_instance):
+def test_async_job_inactive_stop(aggregator, integration_check, dbm_instance):
     dbm_instance['statement_samples']['run_sync'] = False
+    dbm_instance['statement_metrics']['run_sync'] = False
     check = integration_check(dbm_instance)
     check._connect()
     check.check(dbm_instance)
     # make sure there were no unhandled exceptions
-    check.statement_samples._collection_loop_future.result()
-    aggregator.assert_metric("dd.postgres.statement_samples.collection_loop_inactive_stop")
+    check.statement_samples._job_loop_future.result()
+    check.statement_metrics._job_loop_future.result()
+    for job in ['statement-metrics', 'statement-samples']:
+        aggregator.assert_metric(
+            "dd.postgres.async_job.inactive_stop",
+            tags=_expected_dbm_instance_tags(dbm_instance) + ['job:' + job],
+        )
 
 
-def test_statement_samples_collection_loop_cancel(aggregator, integration_check, dbm_instance):
+def test_async_job_cancel_cancel(aggregator, integration_check, dbm_instance):
     dbm_instance['statement_samples']['run_sync'] = False
+    dbm_instance['statement_metrics']['run_sync'] = False
     check = integration_check(dbm_instance)
     check._connect()
     check.check(dbm_instance)
     check.cancel()
     # wait for it to stop and make sure it doesn't throw any exceptions
-    check.statement_samples._collection_loop_future.result()
-    assert not check.statement_samples._collection_loop_future.running(), "thread should be stopped"
+    check.statement_samples._job_loop_future.result()
+    check.statement_metrics._job_loop_future.result()
+    assert not check.statement_samples._job_loop_future.running(), "samples thread should be stopped"
+    assert not check.statement_metrics._job_loop_future.running(), "metrics thread should be stopped"
     # if the thread doesn't start until after the cancel signal is set then the db connection will never
     # be created in the first place
-    assert check.statement_samples._db_pool.get(dbm_instance['dbname']) is None, "db connection should be gone"
-    aggregator.assert_metric("dd.postgres.statement_samples.collection_loop_cancel")
+    assert check._db_pool.get(dbm_instance['dbname']) is None, "db connection should be gone"
+    for job in ['statement-metrics', 'statement-samples']:
+        aggregator.assert_metric(
+            "dd.postgres.async_job.cancel", tags=_expected_dbm_instance_tags(dbm_instance) + ['job:' + job]
+        )
 
 
 def test_statement_samples_invalid_activity_view(aggregator, integration_check, dbm_instance):
@@ -701,8 +746,12 @@ def test_statement_samples_invalid_activity_view(aggregator, integration_check, 
     check._connect()
     check.check(dbm_instance)
     # make sure there were no unhandled exceptions
-    check.statement_samples._collection_loop_future.result()
-    aggregator.assert_metric_has_tag_prefix("dd.postgres.statement_samples.error", "error:database-")
+    check.statement_samples._job_loop_future.result()
+    aggregator.assert_metric(
+        "dd.postgres.async_job.error",
+        tags=_expected_dbm_instance_tags(dbm_instance)
+        + ['job:statement-samples', "error:database-<class 'psycopg2.errors.UndefinedTable'>"],
+    )
 
 
 @pytest.mark.parametrize(
@@ -711,7 +760,7 @@ def test_statement_samples_invalid_activity_view(aggregator, integration_check, 
         "explained_statements_cache_maxsize",
         "explained_statements_per_hour_per_query",
         "seen_samples_cache_maxsize",
-        "collections_per_second",
+        "collection_interval",
     ],
 )
 def test_statement_samples_config_invalid_number(integration_check, pg_instance, number_key):

postgres/assets/configuration/spec.yaml
@@ -240,6 +240,23 @@ files:
       value:
         type: string
         example: show_pg_stat_statements()
+    - name: statement_metrics
+      description: Configure collection of statement metrics
+      options:
+        - name: enabled
+          description: |
+            Enable collection of statement metrics. Requires `dbm: true`.
+          value:
+            type: boolean
+            example: true
+        - name: collection_interval
+          description: |
+            Set the statement metric collection interval (in seconds). Each collection involves a single query to
+            `pg_stat_statements`. If a non-default value is chosen then that exact same value must be used for *every*
+            check instance. Running different instances with different collection intervals is not supported.
+          value:
+            type: number
+            example: 10
     - name: statement_samples
       description: Configure collection of statement samples
       options:
@@ -249,9 +266,9 @@ files:
           value:
             type: boolean
             example: true
-        - name: collections_per_second
+        - name: collection_interval
           description: |
-            Set the maximum statement sample collection rate. Each collection involves a single query to
+            Set the statement sample collection interval (in seconds). Each collection involves a single query to
             `pg_stat_activity` followed by at most one `EXPLAIN` query per unique statement seen.
           value:
             type: number

postgres/datadog_checks/postgres/config.py
@@ -81,6 +81,7 @@ class PostgresConfig:
         # statement samples & execution plans
         self.pg_stat_activity_view = instance.get('pg_stat_activity_view', 'pg_stat_activity')
         self.statement_samples_config = instance.get('statement_samples', {}) or {}
+        self.statement_metrics_config = instance.get('statement_metrics', {}) or {}
 
     def _build_tags(self, custom_tags):
         # Clean up tags in case there was a None entry in the instance

postgres/datadog_checks/postgres/config_models/defaults.py
@@ -92,6 +92,10 @@ def instance_ssl(field, value):
     return 'false'
 
 
+def instance_statement_metrics(field, value):
+    return get_default_field_value(field, value)
+
+
 def instance_statement_samples(field, value):
     return get_default_field_value(field, value)
 

postgres/datadog_checks/postgres/config_models/instance.py
@@ -24,11 +24,19 @@ class Relation(BaseModel):
     schemas: Optional[Sequence[str]]
 
 
+class StatementMetrics(BaseModel):
+    class Config:
+        allow_mutation = False
+
+    collection_interval: Optional[float]
+    enabled: Optional[bool]
+
+
 class StatementSamples(BaseModel):
     class Config:
         allow_mutation = False
 
-    collections_per_second: Optional[float]
+    collection_interval: Optional[float]
     enabled: Optional[bool]
     explain_function: Optional[str]
     explained_statements_cache_maxsize: Optional[int]
@@ -63,6 +71,7 @@ class InstanceConfig(BaseModel):
     relations: Optional[Sequence[Union[str, Relation]]]
     service: Optional[str]
     ssl: Optional[str]
+    statement_metrics: Optional[StatementMetrics]
     statement_samples: Optional[StatementSamples]
     table_count_limit: Optional[int]
     tag_replication_role: Optional[bool]

postgres/datadog_checks/postgres/data/conf.yaml.example
@@ -198,6 +198,22 @@ instances:
     #
     # pg_stat_statements_view: show_pg_stat_statements()
 
+    ## Configure collection of statement metrics
+    #
+    statement_metrics:
+
+        ## @param enabled - boolean - optional - default: true
+        ## Enable collection of statement metrics. Requires `dbm: true`.
+        #
+        # enabled: true
+
+        ## @param collection_interval - number - optional - default: 10
+        ## Set the statement metric collection interval (in seconds). Each collection involves a single query to
+        ## `pg_stat_statements`. If a non-default value is chosen then that exact same value must be used for *every*
+        ## check instance. Running different instances with different collection intervals is not supported.
+        #
+        # collection_interval: 10
+
     ## Configure collection of statement samples
     #
     statement_samples:
@@ -207,11 +223,11 @@ instances:
         #
         # enabled: true
 
-        ## @param collections_per_second - number - optional - default: 1
-        ## Set the maximum statement sample collection rate. Each collection involves a single query to
+        ## @param collection_interval - number - optional - default: 1
+        ## Set the statement sample collection interval (in seconds). Each collection involves a single query to
         ## `pg_stat_activity` followed by at most one `EXPLAIN` query per unique statement seen.
         #
-        # collections_per_second: 1
+        # collection_interval: 1
 
         ## @param explain_function - string - optional - default: datadog.explain_statement
         ## Override the default function used to collect execution plans for statements.

commit 05429350e7ea0670283e4dfd6adbb5f4c3104317

fix None version crash for DBM statement metrics
Fix crash caused by `None` version, which was introduced during a refactor in https://github.com/DataDog/integrations-core/pull/9657. Should have accessed `check.version` not `check._version`.

postgres/datadog_checks/postgres/statements.py
@@ -159,6 +159,12 @@ class PostgresStatementMetrics(DBMAsyncJob):
         self._tags_no_db = [t for t in self._tags if not t.startswith('db:')]
         self.collect_per_statement_metrics()
 
+    def _payload_pg_version(self):
+        version = self._check.version
+        if not version:
+            return ""
+        return 'v{major}.{minor}.{patch}'.format(major=version.major, minor=version.minor, patch=version.patch)
+
     def collect_per_statement_metrics(self):
         # exclude the default "db" tag from statement metrics & FQT events because this data is collected from
         # all databases on the host. For metrics the "db" tag is added during ingestion based on which database
@@ -178,9 +184,7 @@ class PostgresStatementMetrics(DBMAsyncJob):
                 'min_collection_interval': self._config.min_collection_interval,
                 'tags': self._tags_no_db,
                 'postgres_rows': rows,
-                'postgres_version': 'v{major}.{minor}.{patch}'.format(
-                    major=self._check._version.major, minor=self._check._version.minor, patch=self._check._version.patch
-                ),
+                'postgres_version': self._payload_pg_version(),
             }
             self._check.database_monitoring_query_metrics(json.dumps(payload, default=default_json_event_encoding))
         except Exception:

postgres/tests/test_pg_integration.py
@@ -6,6 +6,7 @@ import socket
 import time
 from collections import Counter
 from concurrent.futures.thread import ThreadPoolExecutor
+from unittest.mock import PropertyMock
 
 import mock
 import psycopg2
@@ -280,6 +281,29 @@ def test_statement_samples_enabled_config(
     assert check.statement_samples._enabled == statement_samples_enabled
 
 
+@pytest.mark.parametrize(
+    "version,expected_payload_version",
+    [
+        (VersionInfo(*[9, 6, 0]), "v9.6.0"),
+        (None, ""),
+    ],
+)
+def test_statement_metrics_version(integration_check, dbm_instance, version, expected_payload_version):
+    if version:
+        check = integration_check(dbm_instance)
+        check._version = version
+        check._connect()
+        assert check.statement_metrics._payload_pg_version() == expected_payload_version
+    else:
+        with mock.patch(
+            'datadog_checks.postgres.postgres.PostgreSql.version', new_callable=PropertyMock
+        ) as patched_version:
+            patched_version.return_value = None
+            check = integration_check(dbm_instance)
+            check._connect()
+            assert check.statement_metrics._payload_pg_version() == expected_payload_version
+
+
 @pytest.mark.parametrize("dbstrict", [True, False])
 @pytest.mark.parametrize("pg_stat_statements_view", ["pg_stat_statements", "datadog.pg_stat_statements()"])
 def test_statement_metrics(aggregator, integration_check, dbm_instance, dbstrict, pg_stat_statements_view):
@@ -323,6 +347,7 @@ def test_statement_metrics(aggregator, integration_check, dbm_instance, dbstrict
 
     assert event['host'] == 'stubbed.hostname'
     assert event['timestamp'] > 0
+    assert event['postgres_version'] == check.statement_metrics._payload_pg_version()
     assert event['min_collection_interval'] == dbm_instance['min_collection_interval']
     expected_dbm_metrics_tags = {'foo:bar', 'server:{}'.format(HOST), 'port:{}'.format(PORT)}
     assert set(event['tags']) == expected_dbm_metrics_tags