c7ca7c82fee0b71eccea3c20e515cfe85dcc0e8f
Julia <611228+hithwen@users.noreply.github.com>
2021-07-19 13:07:19 +0200

    Do not submit critical service check when there are no messages (#9703)

--- ibm_mq/datadog_checks/ibm_mq/collectors/channel_metric_collector.py
+++ ibm_mq/datadog_checks/ibm_mq/collectors/channel_metric_collector.py
@@ -93,12 +93,14 @@ class ChannelMetricCollector(object):
             response = pcf.MQCMD_INQUIRE_CHANNEL_STATUS(args)
             self.service_check(self.CHANNEL_SERVICE_CHECK, AgentCheck.OK, search_channel_tags)
         except pymqi.MQMIError as e:
-            self.service_check(self.CHANNEL_SERVICE_CHECK, AgentCheck.CRITICAL, search_channel_tags)
             if e.comp == pymqi.CMQC.MQCC_FAILED and e.reason == pymqi.CMQCFC.MQRCCF_CHL_STATUS_NOT_FOUND:
+                self.service_check(self.CHANNEL_SERVICE_CHECK, AgentCheck.CRITICAL, search_channel_tags)
                 self.log.debug("Channel status not found for channel %s: %s", search_channel_name, e)
             elif e.comp == pymqi.CMQC.MQCC_FAILED and e.reason == pymqi.CMQC.MQRC_NO_MSG_AVAILABLE:
+                self.service_check(self.CHANNEL_SERVICE_CHECK, AgentCheck.UNKNOWN, search_channel_tags)
                 self.log.debug("There are no messages available for channel %s", search_channel_name)
             else:
+                self.service_check(self.CHANNEL_SERVICE_CHECK, AgentCheck.CRITICAL, search_channel_tags)
                 self.log.warning("Error getting CHANNEL status for channel %s: %s", search_channel_name, e)
         else:
             for channel_info in response:
