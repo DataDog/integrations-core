f3a530f12c1b7815a83e2f78bbb48ca0151d703d
Ian Bucad <ian.bucad@datadoghq.com>
2021-07-25 13:26:50 +1000

    fix test

--- datadog_checks_dev/tests/tooling/configuration/consumers/model/test_defaults.py
+++ datadog_checks_dev/tests/tooling/configuration/consumers/model/test_defaults.py
@@ -32,7 +32,7 @@ def test():
               description: words
               value:
                 example: bar
-                default: baz
+                display_default: baz
                 type: string
             - name: example_ignored_array
               description: words
@@ -52,7 +52,7 @@ def test():
             - name: long_default_formatted
               description: words
               value:
-                default:
+                display_default:
                 - ["01", "02", "03", "04", "05"]
                 - ["06", "07", "08", "09", "10"]
                 - ["11", "12", "13", "14", "15"]
