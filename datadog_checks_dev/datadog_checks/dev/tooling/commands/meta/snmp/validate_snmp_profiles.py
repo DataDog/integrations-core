40d39b15ae9c3bb43f65fd02592f6896848a0af2
matheuskja <32466896+matheuskja@users.noreply.github.com>
2021-07-27 11:01:59 +0200

    Refactor profile validators (#9741)
    * Add command to cli to check duplicate metrics
    
    * Change to correct default relative_path
    
    * added path
    
    * Creation of ProfileValidator class that will be template for all the profile validators
    
    * Adding info and warning messages in validator
    
    * adding SchemaValidator as a subclass of ProfileValidator
    
    * Encapsulated better the validate_profile function by separating the responsibilities into other functions
    
    * Changed DuplicateMetricsValidator class to have all its needed functionalities
    
    * Making sure file is not None
    
    * Removing break line
    
    * removing path option
    
    * removed not used parameter
    
    * Make directory flag validate multiple profiles in the directory
    
    * transforming message methods into a constant
    
    * make used_metrics and duplicated instance variables
    
    * making it validate all profiles in dd_path if not file nor directory provided
    
    * using correct formatation style
    
    * Removing duplicated extension from profile cisco-csr1000v
    
    * taking into account flag_stream OIDs
    
    * created function to take the path to the profiles
    
    * Changing DuplicateMetrics to DuplicateOID
    
    * Changed hash with flagstream to OID.placement
    
    Co-authored-by: Matheus Kledeglau <matheus.kledeglau@datadoghq.com>

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/validate_snmp_profiles.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/snmp/validate_snmp_profiles.py
@@ -1,8 +1,13 @@
+import glob
+import os
+
 import click
 
-from .....fs import dir_exists, file_exists
-from ...console import CONTEXT_SETTINGS, abort, echo_failure
+from ...console import CONTEXT_SETTINGS, echo_failure, echo_info, echo_success, echo_warning
 from . import validators
+from .validators.utils import exist_profile_in_path, get_default_snmp_profiles_path, initialize_path
+
+MESSAGE_METHODS = {'success': echo_success, 'warning': echo_warning, 'failure': echo_failure, 'info': echo_info}
 
 
 @click.command("validate-profile", short_help="Validate SNMP profiles", context_settings=CONTEXT_SETTINGS)
@@ -10,12 +15,62 @@ from . import validators
 @click.option('-d', '--directory', help="Path to a directory of profiles to validate")
 @click.option('-v', '--verbose', help="Increase verbosity of error messages", is_flag=True)
 def validate_profile(file, directory, verbose):
+    path = initialize_path(directory)
+
     if file:
-        if not file_exists(file):
-            echo_failure("Profile file not found, or could not be read: " + file)
-            abort()
-    if directory:
-        if not dir_exists(directory):
-            echo_failure("Directory not found, or could not be read: " + directory)
-            abort()
-    validators.validate_profile(file, directory, verbose)
+        _validate_single_profile(file, path, verbose)
+
+    else:
+        if not directory:
+            directory = get_default_snmp_profiles_path()
+
+        all_profiles_directory = get_all_profiles_directory(directory)
+        for profile in all_profiles_directory:
+            echo_info("Start validation of profile {profile}:".format(profile=profile))
+            _validate_single_profile(profile, path, verbose)
+
+
+def _validate_single_profile(file, path, verbose):
+    if not exist_profile_in_path(file, path):
+        echo_failure("Profile file not found, or could not be read: " + str(file))
+        return
+
+    all_validators = validators.get_all_validators()
+
+    report = validate_profile_from_validators(all_validators, file, path)
+
+    show_report(report)
+
+
+def validate_profile_from_validators(all_validators, file, path):
+    display_queue = []
+    failure = False
+
+    for validator in all_validators:
+        validator.validate(file, path)
+        failure = validator.result.failed
+        for msg_type, messages in validator.result.messages.items():
+            for message in messages:
+                display_queue.append((MESSAGE_METHODS[msg_type], message))
+        if failure:
+            break
+
+    report_profile = {}
+    report_profile['messages'] = display_queue
+    report_profile['failed'] = failure
+
+    return report_profile
+
+
+def show_report(report):
+    if report['failed']:
+        echo_failure("FAILED")
+    else:
+        echo_success("Profile successfuly validated")
+
+    for display_func, message in report['messages']:
+        display_func(message)
+
+
+def get_all_profiles_directory(directory):
+    return glob.glob(os.path.join(directory, "*.yaml"))
