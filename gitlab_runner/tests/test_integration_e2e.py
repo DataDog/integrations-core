6c55d0ef22a555b97d66dd094e0c2fcb85b0a0b2
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-07-30 17:06:28 +0200

    Modernise test

--- gitlab_runner/tests/test_integration_e2e.py
+++ gitlab_runner/tests/test_integration_e2e.py
@@ -3,6 +3,7 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 import pytest
+from datadog_checks.dev.plugin.pytest import dd_run_check
 
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.gitlab_runner import GitlabRunnerCheck
@@ -24,18 +25,22 @@ def assert_check(aggregator):
         GitlabRunnerCheck.PROMETHEUS_SERVICE_CHECK_NAME, status=GitlabRunnerCheck.OK, tags=CUSTOM_TAGS, count=2
     )
     for metric in ALLOWED_METRICS:
+        metric_name = "gitlab_runner.{}".format(metric)
         if metric.startswith('ci_runner'):
-            aggregator.assert_metric("gitlab_runner.{}".format(metric))
+            aggregator.assert_metric(metric_name)
         else:
-            aggregator.assert_metric("gitlab_runner.{}".format(metric), tags=CUSTOM_TAGS, count=2)
+            aggregator.assert_metric(metric_name, tags=CUSTOM_TAGS, count=2)
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
-@pytest.mark.usefixtures("dd_environment")
-def test_check(aggregator):
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_check(aggregator, dd_run_check):
     gitlab_runner = GitlabRunnerCheck('gitlab_runner', CONFIG['init_config'], instances=CONFIG['instances'])
 
-    gitlab_runner.check(CONFIG['instances'][0])
-    gitlab_runner.check(CONFIG['instances'][0])
+    dd_run_check(gitlab_runner)
+    dd_run_check(gitlab_runner)
 
     assert_check(aggregator)
 
@@ -45,4 +50,3 @@ def test_e2e(dd_agent_check):
     aggregator = dd_agent_check(CONFIG, rate=True)
 
     assert_check(aggregator)
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
