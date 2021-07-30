fb07e255562025a42a9c2744d8c7b6c171baa46e
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-07-30 15:10:24 +0200

    change tested versions

--- gitlab_runner/tests/compose/support/wait_for_master.sh
+++ gitlab_runner/tests/compose/support/wait_for_master.sh
@@ -20,7 +20,7 @@ do
 		echo "Gitlab is up!"
 		break
 	fi
-	sleep 5
+	sleep 10
 done
 
 # Register the runner and start the Prometheus metrics server
@@ -28,4 +28,3 @@ set +x
 gitlab-ci-multi-runner register -u http://gitlab -r $GITLAB_SHARED_RUNNERS_REGISTRATION_TOKEN --executor shell -n
 gitlab-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner --config=/support/custom_config.toml --metrics-server 0.0.0.0:9292
 set -x
-
