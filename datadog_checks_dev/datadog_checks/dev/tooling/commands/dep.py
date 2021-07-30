4e86e7ca9d9f5cc7ec204e2696b59b0ed6f7c042
Sarah <sarah.witt@datadoghq.com>
2021-07-30 11:00:25 -0400

    Add dep sync command

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/dep.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/dep.py
@@ -10,7 +10,7 @@ from packaging.specifiers import SpecifierSet
 
 from ...fs import read_file_lines, write_file_lines
 from ..constants import get_agent_requirements, get_root
-from ..dependencies import read_agent_dependencies, read_check_dependencies
+from ..dependencies import normalize_dependency_marker, read_agent_dependencies, read_check_dependencies
 from ..utils import get_valid_checks
 from .console import CONTEXT_SETTINGS, abort, echo_failure, echo_info
 
@@ -123,6 +123,7 @@ def sync():
             echo_failure(error)
         abort()
 
+    files_updated = 0
     checks = sorted(get_valid_checks())
     for check_name in checks:
         check_dependencies, check_errors = read_check_dependencies(check=check_name)
@@ -131,29 +132,34 @@ def sync():
             for error in check_errors:
                 echo_failure(error)
             abort()
-
-        for check_name, check_dependency_defs in check_dependencies.items():
-            agent_deps = agent_dependencies[check_name]
-            for check_dep_name, dependency_definitions in check_dependency_defs.items():
+        deps_to_update = {}
+        agent_deps = agent_dependencies[check_name]
+        for check_dep_nam, check_dependency_defs in check_dependencies.items():
+            agent_deps = agent_dependencies[check_dep_nam]
+            for version, dependency_definitions in check_dependency_defs.items():
                 for check_dependency_def in dependency_definitions:
-                    for agent_dep_name, agent_dependency_definitions in agent_deps.items():
+                    for agent_dep_version, agent_dependency_definitions in agent_deps.items():
                         for agent_dependency_definition in agent_dependency_definitions:
-                            if agent_dependency_definition.requirement == check_dependency_def.requirement:
-                                continue
-                            else:
-                                check_dependency_def.requirement = agent_dependency_definition.requirement
-
-            data = sorted(
-                (
-                    (dependency_definition.name, str(dependency_definition.requirement).lower())
-                    for versions in check_dependencies.values()
-                    for dependency_definitions in versions.values()
-                    for dependency_definition in dependency_definitions
-                ),
-                key=lambda d: d[0],
-            )
+                            if agent_dependency_definition.name == check_dependency_def.name and normalize_dependency_marker(check_dependency_def.requirement.marker) == normalize_dependency_marker(agent_dependency_definition.requirement.marker):
+                                if version != agent_dep_version:
+                                    # the agent requirement was bumped
+                                    deps_to_update[agent_dep_version] = check_dependency_def
+                                    
             root = get_root()
             check_req_file = os.path.join(root, check_name, 'requirements.in')
-            lines = sorted(set(f'{d[1]}\n' for d in data))
-            print(lines)
+            old_lines = read_file_lines(check_req_file)
+            new_lines = old_lines.copy()
+            for new_version, dependency_definition in deps_to_update.items():
+                dependency_definition.requirement.specifier = new_version
+                new_lines[dependency_definition.line_number] = f'{dependency_definition.requirement}\n'
+
+                if new_lines != old_lines:
+                    files_updated += 1
+                    write_file_lines(check_req_file, new_lines)
+
+    if not files_updated:
+        abort('No dependency definitions to update')
+
+    echo_info(f'Files updated: {files_updated}')
+
 
