0eccb66f3030bf0f38512bb741e8cff86d2690a8
Alexandre Normand <alex.normand@datadoghq.com>
2021-08-13 09:09:59 -0700

    Update query to select from events_statements_by_digest to include rows with null values (#9892)

--- mysql/datadog_checks/mysql/statements.py
+++ mysql/datadog_checks/mysql/statements.py
@@ -121,7 +121,7 @@ class MySQLStatementMetrics(DBMAsyncJob):
 
             # truncate query text to the maximum length supported by metrics tags
             for row in rows:
-                row['digest_text'] = row['digest_text'][0:200]
+                row['digest_text'] = row['digest_text'][0:200] if row['digest_text'] is not None else None
 
             payload = {
                 'host': self._check.resolved_hostname,
@@ -166,7 +166,7 @@ class MySQLStatementMetrics(DBMAsyncJob):
                    `sum_no_index_used`,
                    `sum_no_good_index_used`
             FROM performance_schema.events_statements_summary_by_digest
-            WHERE `digest_text` NOT LIKE 'EXPLAIN %'
+            WHERE `digest_text` NOT LIKE 'EXPLAIN %' OR `digest_text` IS NULL
             ORDER BY `count_star` DESC
             LIMIT 10000"""
 
@@ -187,7 +187,11 @@ class MySQLStatementMetrics(DBMAsyncJob):
         for row in rows:
             normalized_row = dict(copy.copy(row))
             try:
-                obfuscated_statement = datadog_agent.obfuscate_sql(row['digest_text'], self._obfuscate_options)
+                obfuscated_statement = (
+                    datadog_agent.obfuscate_sql(row['digest_text'], self._obfuscate_options)
+                    if row['digest_text'] is not None
+                    else None
+                )
             except Exception as e:
                 self.log.warning("Failed to obfuscate query '%s': %s", row['digest_text'], e)
                 continue
