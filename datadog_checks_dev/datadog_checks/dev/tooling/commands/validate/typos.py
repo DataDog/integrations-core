bb48e9cf170fc73f8286e738530bb179547178b9
ChristineTChen <christine.chen@datadoghq.com>
2021-08-18 15:28:41 -0400

    Add annotations

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/typos.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/typos.py
@@ -2,43 +2,55 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import click
+import re
 import os
-import subprocess
 
+from ....subprocess import run_command
 from ...utils import complete_valid_checks
 from ..console import CONTEXT_SETTINGS, abort, echo_failure, echo_info, echo_success, echo_warning
 
 from datadog_checks.dev.tooling.constants import get_root
+from datadog_checks.dev.tooling.annotations import annotate_warning
 
+FILE_PATH_REGEX = r'(.+\/[^\/]+)\:(\d+)\:\s+(\w+\s+\=\=\>\s+\w+)'
 
 @click.command(context_settings=CONTEXT_SETTINGS, short_help='Validate spelling')
 @click.argument('check', autocompletion=complete_valid_checks, required=False)
 @click.option('--fix', is_flag=True, help='Apply suggested fix')
 @click.pass_context
 def typos(ctx, check, fix):
-    """Validate spelling in the source code
+    """Validate spelling in the source code.
 
-    If `check` is specified, only the directory is validated
+    If `check` is specified, only the directory is validated.
+    Use codespell command line tool to detect spelling errors.
     """
-    cmd = "codespell {} --config={}/.codespell/setup.cfg"
-    path = get_root()
+    root = get_root()
+
     if check:
-        path = os.path.join(path, check)
-        path += '/'
+        path = os.path.join(root, check)
+    else:
+        path = root
+
+    cmd = f"codespell {path} --config={root}/.codespell/setup.cfg"
 
     if fix:
         cmd += " -w"
 
     try:
-        output = subprocess.run(cmd.format(path, path), shell=True)
-
-        if output == 0:
+        output, err, code = run_command(cmd, capture='out')
+        if code == 0:
             echo_success("All files are valid!")
-        else:
-            annotate_typos(output)
+            abort()
     except Exception as e:
         echo_info(f"Encountered error validating spell check: {e}")
 
+    echo_warning("Detected typos...")
+    annotate_typos(output)
 
 def annotate_typos(output):
-    pass
+    parse_errors = output.split('\n')
+    for line in parse_errors:
+        m = re.match(FILE_PATH_REGEX, line)
+        file, num, suggestion = m.groups()
+        annotate_warning(file, f"Detected typo: {suggestion}", line=num)
+        echo_warning(line)
