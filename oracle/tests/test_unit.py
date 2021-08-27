e2034f03df896bd7fd942fb498403746c8cc6bb3
Andrew Zhang <yzhan289@jhu.edu>
2021-08-27 15:49:20 -0400

    Add new unit tests for service check changes

--- oracle/tests/test_unit.py
+++ oracle/tests/test_unit.py
@@ -10,7 +10,7 @@ except ImportError:
 
 from datadog_checks.oracle import Oracle
 
-from .common import CHECK_NAME
+from .common import CHECK_NAME, mock_bad_executor
 
 import mock
 
@@ -18,41 +18,56 @@ from datadog_checks.base.utils.db import Query, QueryManager
 from datadog_checks.oracle import queries
 
 
-def test__get_connection_instant_client(check, dd_run_check):
+def test__get_connection_instant_client(check, dd_run_check, aggregator):
     """
     Test the _get_connection method using the instant client
     """
     check.use_oracle_client = True
     con = mock.MagicMock()
-    service_check = mock.MagicMock()
-    check.service_check = service_check
     expected_tags = ['server:localhost:1521', 'optional:tag1']
     with mock.patch('datadog_checks.oracle.oracle.cx_Oracle') as cx:
         cx.connect.return_value = con
         dd_run_check(check)
         assert check._cached_connection == con
         cx.connect.assert_called_with(user='system', password='oracle', dsn=check._get_dsn())
-        service_check.assert_called_with(check.SERVICE_CHECK_NAME, check.OK, tags=expected_tags)
-        service_check.assert_called_with(check.SERVICE_CHECK_NAME, check.OK, tags=expected_tags)
+        aggregator.assert_service_check("oracle.can_connect", check.OK, count=1, tags=expected_tags)
+        aggregator.assert_service_check("oracle.can_query", check.OK, count=1, tags=expected_tags)
 
 
-# test that has successful connection to db via jdbc and instant client, but fails can_query
-def test__get_connection_jdbc_can_query_fail(check, dd_run_check):
+def test__get_connection_instant_client_query_fail(check, dd_run_check, aggregator):
     """
-    Test the _get_connection method using the JDBC client and successfully can query DB
+    Test the _get_connection method using the oracle client and unsuccessfully query DB
+    """
+    check.use_oracle_client = True
+    con = mock.MagicMock()
+    expected_tags = ['server:localhost:1521', 'optional:tag1']
+
+    check._query_manager.executor = mock_bad_executor()
+
+    with mock.patch('datadog_checks.oracle.oracle.cx_Oracle') as cx:
+        cx.connect.return_value = con
+        dd_run_check(check)
+        aggregator.assert_service_check("oracle.can_connect", check.OK, count=1, tags=expected_tags)
+        aggregator.assert_service_check("oracle.can_query", check.CRITICAL, count=1, tags=expected_tags)
+
+
+def test__get_connection_jdbc_query_fail(check, dd_run_check, aggregator):
+    """
+    Test the _get_connection method using the JDBC client and unsuccessfully query DB
     """
     check.use_oracle_client = False
     con = mock.MagicMock()
-    service_check = mock.MagicMock()
-    check.service_check = service_check
     expected_tags = ['server:localhost:1521', 'optional:tag1']
 
+    check._query_manager.executor = mock_bad_executor()
     cx = mock.MagicMock(DatabaseError=RuntimeError)
     cx.clientversion.side_effect = cx.DatabaseError()
 
     jdb = mock.MagicMock()
     jdb.connect.return_value = con
     jpype = mock.MagicMock(isJVMStarted=lambda: False)
+
+    check._query_errors = 1
 
     mocks = [
         ('datadog_checks.oracle.oracle.cx_Oracle', cx),
@@ -64,22 +79,17 @@ def test__get_connection_jdbc_can_query_fail(check, dd_run_check):
         for mock_call in mocks:
             stack.enter_context(mock.patch(*mock_call))
         dd_run_check(check)
-        assert check._cached_connection == con
 
-    jdb.connect.assert_called_with(
-        'oracle.jdbc.OracleDriver', 'jdbc:oracle:thin:@//localhost:1521/xe', ['system', 'oracle'], None
-    )
-    service_check.assert_called_with(check.SERVICE_CHECK_NAME, check.OK, tags=expected_tags)
+    aggregator.assert_service_check("oracle.can_connect", check.OK, count=1, tags=expected_tags)
+    aggregator.assert_service_check("oracle.can_query", check.CRITICAL, count=1, tags=expected_tags)
 
 
-def test__get_connection_jdbc(check, dd_run_check):
+def test__get_connection_jdbc(check, dd_run_check, aggregator):
     """
     Test the _get_connection method using the JDBC client
     """
     check.use_oracle_client = False
     con = mock.MagicMock()
-    service_check = mock.MagicMock()
-    check.service_check = service_check
     expected_tags = ['server:localhost:1521', 'optional:tag1']
 
     cx = mock.MagicMock(DatabaseError=RuntimeError)
@@ -104,10 +114,11 @@ def test__get_connection_jdbc(check, dd_run_check):
     jdb.connect.assert_called_with(
         'oracle.jdbc.OracleDriver', 'jdbc:oracle:thin:@//localhost:1521/xe', ['system', 'oracle'], None
     )
-    service_check.assert_called_with(check.SERVICE_CHECK_NAME, check.OK, tags=expected_tags)
+    aggregator.assert_service_check("oracle.can_connect", check.OK, count=1, tags=expected_tags)
+    aggregator.assert_service_check("oracle.can_query", check.OK, count=1, tags=expected_tags)
 
 
-def test__get_connection_failure(check, dd_run_check):
+def test__get_connection_failure(check, dd_run_check, aggregator):
     """
     Test the right service check is sent upon _get_connection failures
     """
@@ -115,7 +126,9 @@ def test__get_connection_failure(check, dd_run_check):
     service_check = mock.MagicMock()
     check.service_check = service_check
     dd_run_check(check)
-    service_check.assert_called_with(check.SERVICE_CHECK_NAME, check.CRITICAL, tags=expected_tags)
+
+    aggregator.assert_service_check("oracle.can_connect", check.CRITICAL, count=1, tags=expected_tags)
+    aggregator.assert_service_check("oracle.can_query", check.CRITICAL, count=1, tags=expected_tags)
     assert check._cached_connection is None
 
 
@@ -148,6 +161,7 @@ def __test__check_only_custom_queries_set_false(check, instance):
     Test the default metrics are called when only_custom queries is set to False
     """
     assert check._query_manager.queries != []
+
 
 def test_sys_metrics(aggregator, check):
     con = mock.MagicMock()
