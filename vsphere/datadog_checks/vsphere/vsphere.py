7b5225b8aec277317e23eb87c6d94d2db1a5b12a
FlorianVeaux <florian.veaux@datadoghq.com>
2021-08-30 11:30:51 +0200

    Add log line

--- vsphere/datadog_checks/vsphere/vsphere.py
+++ vsphere/datadog_checks/vsphere/vsphere.py
@@ -238,7 +238,12 @@ class VSphereCheck(AgentCheck):
                 # Hosts are not considered as parents of the VMs they run, we use the `runtime.host` property
                 # to get the name of the ESXi host
                 runtime_host = properties.get("runtime.host")
-                runtime_host_props = infrastructure_data.get(runtime_host, {}) if runtime_host else {}
+                runtime_host_props = {}
+                if runtime_host:
+                    if runtime_host in infrastructure_data:
+                        runtime_host_props = infrastructure_data.get(runtime_host, {}) if runtime_host else {}
+                    else:
+                        self.log.debug("Missing runtime.host details for VM %s", mor_name)
                 runtime_hostname = to_string(runtime_host_props.get("name", "unknown"))
                 tags.append('vsphere_host:{}'.format(runtime_hostname))
 
