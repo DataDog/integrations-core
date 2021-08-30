3bff151f4435d9e5e2f73866635dc95a68645825
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-08-30 13:23:28 +0200

    ensure fresh instance copies are generated for each test

--- kafka_consumer/tests/test_kafka_consumer.py
+++ kafka_consumer/tests/test_kafka_consumer.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
+import copy
 import os
 
 import pytest
@@ -23,8 +24,9 @@ CONSUMER_METRICS = ['kafka.consumer_offset', 'kafka.consumer_lag']
 
 @pytest.mark.unit
 def test_uses_legacy_implementation_when_legacy_version_specified(kafka_instance):
-    kafka_instance['kafka_client_api_version'] = '0.10.1'
-    kafka_consumer_check = KafkaCheck('kafka_consumer', {}, [kafka_instance])
+    instance = copy.deepcopy(kafka_instance)
+    instance['kafka_client_api_version'] = '0.10.1'
+    kafka_consumer_check = KafkaCheck('kafka_consumer', {}, [instance])
     kafka_consumer_check._init_check_based_on_kafka_version()
 
     assert isinstance(kafka_consumer_check.sub_check, LegacyKafkaCheck_0_10_2)
@@ -32,8 +34,9 @@ def test_uses_legacy_implementation_when_legacy_version_specified(kafka_instance
 
 @pytest.mark.unit
 def test_uses_new_implementation_when_new_version_specified(kafka_instance):
-    kafka_instance['kafka_client_api_version'] = '0.10.2'
-    kafka_consumer_check = KafkaCheck('kafka_consumer', {}, [kafka_instance])
+    instance = copy.deepcopy(kafka_instance)
+    instance['kafka_client_api_version'] = '0.10.2'
+    kafka_consumer_check = KafkaCheck('kafka_consumer', {}, [instance])
     kafka_consumer_check._init_check_based_on_kafka_version()
 
     assert isinstance(kafka_consumer_check.sub_check, NewKafkaConsumerCheck)
