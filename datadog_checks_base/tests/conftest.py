1913693a67016135b3ca83ae5f02454eb9f751f4
Andrew Zhang <yzhan289@jhu.edu>
2021-09-02 12:16:56 -0400

    Add docker requirement

--- datadog_checks_base/tests/conftest.py
+++ datadog_checks_base/tests/conftest.py
@@ -4,8 +4,7 @@ import pytest
 
 from datadog_checks.base.utils.platform import Platform
 from datadog_checks.dev import TempDir, docker_run, get_here
-from datadog_checks.dev.conditions import CheckDockerLogs
-
+from datadog_checks.dev.conditions import CheckDockerLogs, WaitFor
 HERE = get_here()
 
 
@@ -114,7 +113,8 @@ def uds_path():
                 "UDS_HOST_DIRECTORY": tmp_dir,
                 'UDS_FILENAME': uds_filename,
             },
-            sleep=60,
+            # sleep=30,
+            conditions=[WaitFor(lambda: os.path.exists(uds_path))]
         ):
             yield uds_path
 
