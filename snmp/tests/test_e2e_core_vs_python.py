74b2f81d7836f29f6a340fc0f5602671929552e6
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-09-08 17:04:59 +0200

    Fix tests

--- snmp/tests/test_e2e_core_vs_python.py
+++ snmp/tests/test_e2e_core_vs_python.py
@@ -340,7 +340,7 @@ def test_e2e_discovery(dd_agent_check):
     ]
     # execute 2 times with 300ms interval to be sure the autodiscovery has time to discover the devices
     # we might need to increase pause if the test is too flaky
-    assert_python_vs_core(dd_agent_check, config, rate=False, pause=500, times=3, metrics_to_skip=skip_metrics)
+    assert_python_vs_core(dd_agent_check, config, rate=False, pause=300, times=2, metrics_to_skip=skip_metrics)
 
 
 def assert_python_vs_core(
@@ -378,7 +378,7 @@ def assert_python_vs_core(
 
     # building core metrics (core)
     aggregator.reset()
-    aggregator = dd_agent_check(core_config, rate=True)
+    aggregator = dd_agent_check(core_config, rate=rate, pause=pause, times=times)
     aggregator_metrics = aggregator._metrics
     aggregator._metrics = defaultdict(list)
     for metric_name in aggregator_metrics:
