850c4a320871f9036d2cdd8fca7fd60bd9dba1a0
HantingZHANG <hanting.zhang@datadoghq.com>
2021-09-17 16:50:17 +0200

    Fixing tests

--- http_check/datadog_checks/http_check/config_models/defaults.py
+++ http_check/datadog_checks/http_check/config_models/defaults.py
@@ -19,14 +19,8 @@ def shared_skip_proxy(field, value):
 def shared_timeout(field, value):
     return 10
 
-
-def instance_allow_redirects(field, value):
-    return True
-
-
 def instance_auth_token(field, value):
     return get_default_field_value(field, value)
-
 
 def instance_auth_type(field, value):
     return 'basic'
