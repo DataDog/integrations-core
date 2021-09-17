a88b7519d553707cbf804537a14cf549cb0f3271
ChristineTChen <christine.chen@datadoghq.com>
2021-09-17 18:26:32 -0400

    Fix multiline format

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/imports.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/imports.py
@@ -87,9 +87,9 @@ def imports(ctx, check, autofix):
                         suggested_line.replace('datadog_checks', 'datadog_checks.{}'.format(check_name))
                     else:
                         suggested_line.replace('datadog_checks', 'datadog_checks.{base/dev}')
-                    message = (
-                        'Detected deprecated import: {}, run "ddev validate imports --autofix" to fix. %0A'
-                        'Import should look like: {}'.format(linetext, suggested_line)
+                    message = '''Detected deprecated import: {}, run "ddev validate imports --autofix" to fix. %0A
+                                Import should look like: {}'''.format(
+                        linetext, suggested_line
                     )
                     annotate_error(
                         f,
