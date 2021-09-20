f91d753b151fc2be03384256d75aaf9c308f2bb2
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-09-20 13:29:09 +0200

    Use valid tags in testing

--- mysql/tests/tags.py
+++ mysql/tests/tags.py
@@ -3,8 +3,8 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from . import common
 
-METRIC_TAGS = ['tag1', 'tag2']
-SC_TAGS = ['mysql_server:' + common.HOST, 'port:' + str(common.PORT), 'tag1', 'tag2']
+METRIC_TAGS = ['tag1:value1', 'tag2:value2']
+SC_TAGS = ['mysql_server:' + common.HOST, 'port:' + str(common.PORT), 'tag1:value1', 'tag2:value2']
 SC_TAGS_MIN = ['mysql_server:' + common.HOST, 'port:' + str(common.PORT)]
-SC_TAGS_REPLICA = ['mysql_server:' + common.HOST, 'port:' + str(common.SLAVE_PORT), 'tag1', 'tag2']
+SC_TAGS_REPLICA = ['mysql_server:' + common.HOST, 'port:' + str(common.SLAVE_PORT), 'tag1:value1', 'tag2:value2']
 SC_FAILURE_TAGS = ['mysql_server:' + common.HOST, 'port:unix_socket']
