91a9966b7237bbf1eb0ca1abebb5590d51c96089
HantingZHANG <hanting.zhang@datadoghq.com>
2021-09-23 19:24:27 +0200

    adds haproxy test version

--- haproxy/tests/common.py
+++ haproxy/tests/common.py
@@ -11,10 +11,17 @@ from datadog_checks.dev import get_docker_hostname, get_here
 HERE = get_here()
 HOST = get_docker_hostname()
 HAPROXY_LEGACY = os.getenv('HAPROXY_LEGACY')
-HAPROXY_VERSION_RAW = os.getenv('HAPROXY_VERSION')
-HAPROXY_VERSION = version.parse(HAPROXY_VERSION_RAW)
+HAPROXY_VERSION_RAW = os.getenv('HAPROXY_VERSION', 'latest')
+
+HAPROXY_VERSION_IS_LATEST = HAPROXY_VERSION_RAW.endswith('latest')
+if HAPROXY_VERSION_IS_LATEST:
+     #HAPROXY_VERSION = version.parse(str(float('inf')))
+     HAPROXY_VERSION = version.parse('latest')
+else:
+     HAPROXY_VERSION = version.parse(HAPROXY_VERSION_RAW)
 ENDPOINT_PROMETHEUS = 'http://{}:8404/metrics'.format(HOST)
 
 INSTANCE = {'use_prometheus': True, 'prometheus_url': ENDPOINT_PROMETHEUS}
 
 requires_new_environment = pytest.mark.skipif(HAPROXY_LEGACY != 'false', reason='Requires prometheus environment')
+requires_static_version = pytest.mark.skipif(HAPROXY_VERSION_IS_LATEST , reason='Version `latest` is ever-changing, skipping')
