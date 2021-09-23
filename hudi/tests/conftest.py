5091674eba43e55c5513d877293f852704343982
Sarah <sarah.witt@datadoghq.com>
2021-09-23 13:16:23 -0400

    Add CheckDockerLogs

--- hudi/tests/conftest.py
+++ hudi/tests/conftest.py
@@ -6,14 +6,16 @@ import os
 import pytest
 
 from datadog_checks.dev import docker_run
+from datadog_checks.dev.conditions import CheckDockerLogs
 
 from .common import CHECK_CONFIG, HERE
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
+    compose_file = os.path.join(HERE, 'docker', 'docker-compose.yaml')
     with docker_run(
-        compose_file=os.path.join(HERE, 'docker', 'docker-compose.yaml'),
-        sleep=100,
+        compose_file=compose_file,
+        conditions=[CheckDockerLogs('spark-app-hudi', 'finished: show at script.scala:163', attempts=50)],
     ):
         yield CHECK_CONFIG, {'use_jmx': True}
