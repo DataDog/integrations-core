d157cea34e3019d3e3f595846ae9be68d77f0529
Andrew Zhang <yzhan289@jhu.edu>
2021-09-24 17:11:00 -0400

    Fix style

--- aspdotnet/tests/test_aspdotnet.py
+++ aspdotnet/tests/test_aspdotnet.py
@@ -4,11 +4,12 @@
 import os
 
 import pytest
-from .conftest import windows_ci
-from datadog_checks.base import AgentCheck
 from datadog_test_libs.win.pdh_mocks import initialize_pdh_tests, pdh_mocks_fixture  # noqa: F401
 
 from datadog_checks.aspdotnet import AspdotnetCheck
+from datadog_checks.base import AgentCheck
+
+from .conftest import windows_ci
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 MINIMAL_INSTANCE = {'host': '.'}
