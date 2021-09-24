7381a34143aff04ef9a1d4898c57bbe3fdf3962a
HantingZHANG <hanting.zhang@datadoghq.com>
2021-09-24 16:24:10 +0200

    fix format

--- haproxy/tests/common.py
+++ haproxy/tests/common.py
@@ -2,10 +2,10 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import os
+from sys import maxsize
 
 import pytest
 from packaging import version
-from sys import maxsize
 
 from datadog_checks.dev import get_docker_hostname, get_here
 
