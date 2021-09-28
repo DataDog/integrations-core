a6eaa75ef3e903ff898a87463f84008b22da7a5d
Ofek Lev <ofekmeister@gmail.com>
2021-09-28 11:21:36 -0400

    re-sync

--- couch/datadog_checks/couch/config_models/instance.py
+++ couch/datadog_checks/couch/config_models/instance.py
@@ -64,6 +64,7 @@ class InstanceConfig(BaseModel):
     persist_connections: Optional[bool]
     proxy: Optional[Proxy]
     read_timeout: Optional[float]
+    request_size: Optional[float]
     server: str
     service: Optional[str]
     skip_proxy: Optional[bool]
