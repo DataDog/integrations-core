10e477e6f1ea46a65ab77f2b6986eed78f00a42d
Ofek Lev <ofekmeister@gmail.com>
2021-09-28 07:49:36 -0400

    re-sync

--- linux_proc_extras/datadog_checks/linux_proc_extras/config_models/instance.py
+++ linux_proc_extras/datadog_checks/linux_proc_extras/config_models/instance.py
@@ -17,6 +17,7 @@ class InstanceConfig(BaseModel):
     class Config:
         allow_mutation = False
 
+    disable_generic_tags: Optional[bool]
     empty_default_hostname: Optional[bool]
     include_interrupt_metrics: Optional[bool]
     min_collection_interval: Optional[float]
