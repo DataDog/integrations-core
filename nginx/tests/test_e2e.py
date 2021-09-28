fe93effd71681c298459d740fcc2c9efb010f2cc
HantingZHANG <hanting.zhang@datadoghq.com>
2021-09-28 14:28:49 +0200

    fix e2e test

--- nginx/tests/test_e2e.py
+++ nginx/tests/test_e2e.py
@@ -69,5 +69,9 @@ def test_e2e_vts(dd_agent_check, instance_vts):
 
     aggregator.assert_all_metrics_covered()
 
-    tags = common.TAGS + ['port:{}'.format(common.PORT), 'host:{}'.format(common.HOST)]
+    tags = common.TAGS + [
+        'nginx_host:{}'.format(common.HOST),
+        'port:{}'.format(common.PORT),
+        'host:{}'.format(common.HOST),
+    ]
     aggregator.assert_service_check('nginx.can_connect', status=Nginx.OK, tags=tags)
