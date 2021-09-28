4594a62928d87e82ef2df60f02c3500b6e70fb41
Ofek Lev <ofekmeister@gmail.com>
2021-09-28 11:20:11 -0400

    re-sync

--- activemq_xml/datadog_checks/activemq_xml/config_models/instance.py
+++ activemq_xml/datadog_checks/activemq_xml/config_models/instance.py
@@ -62,6 +62,7 @@ class InstanceConfig(BaseModel):
     persist_connections: Optional[bool]
     proxy: Optional[Proxy]
     read_timeout: Optional[float]
+    request_size: Optional[float]
     service: Optional[str]
     skip_proxy: Optional[bool]
     suppress_errors: Optional[bool]
