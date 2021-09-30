999792757a8a350a33db727400b8666e28c8e329
jmeunier28 <jojo.meunier@datadoghq.com>
2021-09-30 13:35:17 -0700

    Bump sqlserver dd base check version

--- sqlserver/setup.py
+++ sqlserver/setup.py
@@ -27,7 +27,7 @@ def get_dependencies():
         return f.readlines()
 
 
-CHECKS_BASE_REQ = 'datadog-checks-base>=20.2.0'
+CHECKS_BASE_REQ = 'datadog-checks-base>=23.0.0'
 
 setup(
     name='datadog-sqlserver',
