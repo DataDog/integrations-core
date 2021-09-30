a17ccbe865f9fd0f84ba870248f03b8bcb6e0544
jmeunier28 <jojo.meunier@datadoghq.com>
2021-09-30 15:04:32 -0700

    try to fix int tests by running whole setup script

--- .azure-pipelines/scripts/sqlserver/windows/55_setup_sqlserver.bat
+++ .azure-pipelines/scripts/sqlserver/windows/55_setup_sqlserver.bat
@@ -1,6 +1,9 @@
 :: Set password
 sqlcmd -Q "ALTER LOGIN sa with PASSWORD = 'Password12!';ALTER LOGIN sa ENABLE;"
 
+:: Set up tests
+sqlcmd -S localhost -U sa -P 'Password12!' -d master -i setup.sql -b
+
 :: Enable port
 powershell -Command "stop-service MSSQLSERVER"
 powershell -Command "set-itemproperty -path 'HKLM:\software\microsoft\microsoft sql server\mssql14.MSSQLSERVER\mssqlserver\supersocketnetlib\tcp\ipall' -name tcpdynamicports -value ''"
