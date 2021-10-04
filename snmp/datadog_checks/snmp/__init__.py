ebbe1831f790270e105fbe01d9cfa81d23f7277a
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-10-04 09:34:53 +0200

    snmp changed

--- snmp/datadog_checks/snmp/__init__.py
+++ snmp/datadog_checks/snmp/__init__.py
@@ -1,5 +1,6 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
+# CHANGED
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
 from .snmp import SnmpCheck
