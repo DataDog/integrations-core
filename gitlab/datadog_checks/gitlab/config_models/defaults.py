0b33f26d2253e0339dce401bde3021da21c24d00
Paul Coignet <paul.coignet@datadoghq.com>
2021-10-05 12:36:49 +0200

    Fix config example

--- gitlab/datadog_checks/gitlab/config_models/defaults.py
+++ gitlab/datadog_checks/gitlab/config_models/defaults.py
@@ -80,6 +80,10 @@ def instance_extra_headers(field, value):
     return get_default_field_value(field, value)
 
 
+def instance_gitlab_url(field, value):
+    return get_default_field_value(field, value)
+
+
 def instance_headers(field, value):
     return get_default_field_value(field, value)
 
