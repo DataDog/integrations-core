eed57c11b7ff7c6130b473b1a1d98f0fd3f07e1c
Pablo Baeyens <pablo.baeyens@datadoghq.com>
2021-10-14 12:50:42 +0200

    Lower warning to debug level

--- process/datadog_checks/process/process.py
+++ process/datadog_checks/process/process.py
@@ -260,7 +260,7 @@ class ProcessCheck(AgentCheck):
             except psutil.AccessDenied:
                 self.log.debug("psutil was denied access for method %s", method)
             except psutil.NoSuchProcess:
-                self.warning("Process %s disappeared while scanning", process.pid)
+                self.log.debug("Process %s disappeared while scanning", process.pid)
 
         return result
 
