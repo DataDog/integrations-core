be8eca2324b3c6c1fd0b2d9fb59f42670c39e443
Andrew Zhang <yzhan289@jhu.edu>
2021-10-15 13:41:03 -0400

    Remove unused import

--- ecs_fargate/tests/conftest.py
+++ ecs_fargate/tests/conftest.py
@@ -1,8 +1,6 @@
 # (C) Datadog, Inc. 2021-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from __future__ import unicode_literals
-
 import os
 
 import pytest
