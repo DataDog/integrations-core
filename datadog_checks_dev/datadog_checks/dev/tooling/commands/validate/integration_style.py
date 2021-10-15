4a5baee7d059c053773a28d9320b8f2f315d7370
ChristineTChen <christine.chen@datadoghq.com>
2021-10-15 18:03:23 -0400

    Add new validation to annotate style improvements

---
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/integration_style.py
@@ -0,0 +1,43 @@
+# (C) Datadog, Inc. 2020-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import re
+
+import click
+
+from ...annotations import annotate_warning
+from ...testing import process_checks_option
+from ...utils import complete_valid_checks, get_check_files
+from ..console import CONTEXT_SETTINGS, echo_info, echo_warning
+
+
+@click.command(context_settings=CONTEXT_SETTINGS, short_help='Validate check style for python files')
+@click.argument('check', autocompletion=complete_valid_checks, required=False)
+@click.option('--verbose', '-v', is_flag=True, help='Verbose mode')
+@click.pass_context
+def integration_style(ctx, check, verbose):
+    """Validate that check follows style guidelines.
+
+    If `check` is specified, only the check will be validated, if check value is 'changed' will only apply to changed
+    checks, an 'all' or empty `check` value will validate all README files.
+    """
+    checks = process_checks_option(check, source='integrations')
+    echo_info(f"Validating integration style for {len(checks)} integrations...")
+
+    for check_name in checks:
+        files = get_check_files(check_name, include_tests=False)
+        for file in files:
+            validate_check_instance(check_name, file, verbose)
+
+
+def validate_check_instance(check_name, file, verbose):
+    with open(file, 'r', encoding='utf-8') as f:
+        read_file = f.read()
+        found_match_arg = re.search(r"def check\(self, instance\):", read_file)
+        if found_match_arg and verbose:
+            message = (
+                f"{check_name}: Deprecation warning: the instance argument in the `check()` function is going to be "
+                f"deprecated in Agent 6. Please use `self.instance` instead."
+            )
+            echo_warning(message)
+            annotate_warning(file, message)
