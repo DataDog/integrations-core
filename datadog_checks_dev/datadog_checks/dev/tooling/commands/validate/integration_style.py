28292ead1e7ba91add8a219b683b918f10b884a5
ChristineTChen <christine.chen@datadoghq.com>
2021-10-15 21:53:55 -0400

    Validate hostname parameter

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/integration_style.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/integration_style.py
@@ -10,6 +10,15 @@ from ...testing import process_checks_option
 from ...utils import complete_valid_checks, get_check_files
 from ..console import CONTEXT_SETTINGS, echo_info, echo_warning
 
+HOSTNAME_REGEX = (
+    r"(self.rate|self.gauge|self.monotonic_count|self.service_check|self.count|self.histogram)\(((.|\n)*)hostname="
+)
+
+VALID_HOSTNAME_CHECKS = [
+    "sap_hana",
+    "vsphere",
+]
+
 
 @click.command(context_settings=CONTEXT_SETTINGS, short_help='Validate check style for python files')
 @click.argument('check', autocompletion=complete_valid_checks, required=False)
@@ -29,15 +38,37 @@ def integration_style(ctx, check, verbose):
         for file in files:
             validate_check_instance(check_name, file, verbose)
 
+            # Some integrations may submit hostname for valid reasons and should not warn on it
+            if check_name not in VALID_HOSTNAME_CHECKS:
+                validate_hostname_submission(check_name, file, verbose)
+
 
 def validate_check_instance(check_name, file, verbose):
+    """
+    Warns when the integration check function contains the `instance` parameter.
+    """
     with open(file, 'r', encoding='utf-8') as f:
         read_file = f.read()
         found_match_arg = re.search(r"def check\(self, instance\):", read_file)
         if found_match_arg and verbose:
             message = (
-                f"{check_name}: Deprecation warning: the instance argument in the `check()` function is going to be "
-                f"deprecated in Agent 6. Please use `self.instance` instead."
+                "The instance argument in the `check()` function is going to be "
+                "deprecated in Agent 6. Please use `self.instance` instead."
             )
-            echo_warning(message)
+            echo_warning(f"{check_name}: " + message)
+            annotate_warning(file, message)
+
+
+def validate_hostname_submission(check_name, file, verbose):
+    """
+    Warns when hostname parameter is submitted on metric or service check
+    """
+    with open(file, 'r', encoding='utf-8') as f:
+        read_file = f.read()
+        found_match_arg = re.search(HOSTNAME_REGEX, read_file)
+        skip_validation = re.search(r"SKIP_CHECK_VALIDATION", read_file)
+
+        if found_match_arg and verbose and not skip_validation:
+            message = f"Detected `hostname` passed with {found_match_arg.groups()[0]}"
+            echo_warning(f"{check_name}: " + message)
             annotate_warning(file, message)
