c32e9d72e081e90af90efc25ba09bba8b7635df3
Vikentiy Fesunov <vikentiy.fesunov@datadoghq.com>
2021-10-20 15:18:11 +0200

    Test use_process_start_time with histograms

--- datadog_checks_base/tests/base/checks/openmetrics/test_openmetrics.py
+++ datadog_checks_base/tests/base/checks/openmetrics/test_openmetrics.py
@@ -2816,7 +2816,7 @@ def test_ignore_tags_regex(aggregator, mocked_prometheus_check, ref_gauge):
     aggregator.assert_metric('prometheus.process.vm.bytes', tags=wanted_tags, count=1)
 
 
-test_process_start_time_data = """\
+test_use_process_start_time_data = """\
 # HELP go_memstats_alloc_bytes_total Total number of bytes allocated, even if freed.
 # TYPE go_memstats_alloc_bytes_total counter
 go_memstats_alloc_bytes_total 9.339544592e+09
@@ -2836,6 +2836,22 @@ go_gc_duration_seconds{quantile="1"} 0.0025860830000000003
 go_gc_duration_seconds_sum 1.154763349
 go_gc_duration_seconds_count 2351
 """
+
+
+def _make_test_use_process_start_time_data(process_start_time):
+    test_data = test_use_process_start_time_data
+    if process_start_time:
+        if not test_data.endswith('\n'):
+            test_data += '\n'
+        test_data += "# HELP process_start_time_seconds Start time of the process since unix epoch in seconds.\n"
+        test_data += "# TYPE process_start_time_seconds gauge\n"
+        for seq, pst in enumerate(process_start_time):
+            label = '{pid="%d"}' % (seq,) if len(process_start_time) > 1 else ""
+            test_data += "process_start_time_seconds%s %f\n" % (
+                label,
+                pst,
+            )
+    return test_data
 
 
 @pytest.mark.parametrize(
@@ -2878,22 +2894,10 @@ def test_use_process_start_time(
         "use_process_start_time": use_process_start_time,
     }
 
-    test_data = test_process_start_time_data
-    if process_start_time:
-        if not test_data.endswith('\n'):
-            test_data += '\n'
-        test_data += "# HELP process_start_time_seconds Start time of the process since unix epoch in seconds.\n"
-        test_data += "# TYPE process_start_time_seconds gauge\n"
-        for seq, pst in enumerate(process_start_time):
-            label = '{pid="%d"}' % (seq,) if len(process_start_time) > 1 else ""
-            test_data += "process_start_time_seconds%s %f\n" % (
-                label,
-                pst,
-            )
-
     datadog_agent.set_process_start_time(agent_start_time)
 
     check = mocked_openmetrics_check_factory(instance)
+    test_data = _make_test_use_process_start_time_data(process_start_time)
     check.poll = mock.MagicMock(return_value=MockResponse(test_data, headers={'Content-Type': text_content_type}))
     check.check(instance)
 
@@ -2931,4 +2935,89 @@ def test_use_process_start_time(
         )
         aggregator.assert_metric(
             'go_gc_duration_seconds.count', metric_type=aggregator.MONOTONIC_COUNT, count=1, flush_first_value=True
+        )
+
+
+@pytest.mark.parametrize(
+    'use_process_start_time, expect_first_flush, agent_start_time, process_start_time',
+    [
+        (False, False, None, None),
+        (True, True, 10, [20]),
+        (True, False, 20, [10]),
+        (True, False, 10, []),
+        (True, True, 10, [20, 30, 40]),
+        (True, False, 20, [10, 30, 40]),
+    ],
+    ids=[
+        "disabled",
+        "enabled, agent is older",
+        "enabled, agent is newer",
+        "enabled, metric n/a",
+        "enabled, many metrics, all newer",
+        "enabled, many metrics, some newer",
+    ],
+)
+def test_use_process_start_time_histograms(
+    aggregator,
+    datadog_agent,
+    mocked_openmetrics_check_factory,
+    expect_first_flush,
+    use_process_start_time,
+    process_start_time,
+    agent_start_time,
+):
+    """
+    Test that first sample is flushed or not depending on metric type, agent and server process start times.
+    """
+
+    instance = {
+        "prometheus_url": "xx",
+        "metrics": ["*"],
+        "namespace": "",
+        "send_distribution_buckets": True,
+        "use_process_start_time": use_process_start_time,
+    }
+
+    datadog_agent.set_process_start_time(agent_start_time)
+
+    check = mocked_openmetrics_check_factory(instance)
+    test_data = _make_test_use_process_start_time_data(process_start_time)
+    check.poll = mock.MagicMock(return_value=MockResponse(test_data, headers={'Content-Type': text_content_type}))
+    check.check(instance)
+
+    aggregator.assert_metric(
+        'go_memstats_alloc_bytes_total',
+        metric_type=aggregator.MONOTONIC_COUNT,
+        count=1,
+        flush_first_value=expect_first_flush,
+    )
+    aggregator.assert_histogram_bucket(
+        'skydns_skydns_dns_request_duration_seconds',
+        None,
+        None,
+        None,
+        True,
+        None,
+        None,
+        count=2,
+        flush_first_value=expect_first_flush,
+    )
+
+    for _ in range(0, 5):
+        aggregator.reset()
+        check.check(instance)
+
+        aggregator.assert_metric(
+            'go_memstats_alloc_bytes_total', metric_type=aggregator.MONOTONIC_COUNT, count=1, flush_first_value=True
+        )
+        aggregator.assert_histogram_bucket(
+            'skydns_skydns_dns_request_duration_seconds',
+            None,
+            None,
+            None,
+            True,
+            None,
+            None,
+            count=2,
+            flush_first_value=True,
         )
