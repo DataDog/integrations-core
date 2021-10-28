f2658b2415edcd52b568e6cda9a07134b7dbb929
abraham-leal <aleal@confluent.io>
2021-10-28 18:53:00 -0500

    Update log entry for connector publishing

--- confluent_platform/tests/conftest.py
+++ confluent_platform/tests/conftest.py
@@ -45,7 +45,7 @@ def dd_environment():
             CheckDockerLogs('connect', 'Kafka Connect started', attempts=120),
             # Create connectors
             WaitFor(create_connectors),
-            CheckDockerLogs('connect', 'Finished commitOffsets successfully'),
+            CheckDockerLogs('connect', 'Source task finished initialization and start'),
         ],
     ):
         yield CHECK_CONFIG, {'use_jmx': True}
