93255b0ad1aace547917e310dbe0660cc2d86cbf
Fanny Jiang <fanny.jiang@datadoghq.com>
2021-11-03 18:38:14 -0400

    add to E2E

--- weblogic/tests/conftest.py
+++ weblogic/tests/conftest.py
@@ -6,15 +6,27 @@ import os
 
 import pytest
 
-from datadog_checks.dev import docker_run, get_here
+from datadog_checks.dev import docker_run
+from datadog_checks.dev.subprocess import run_command
 from datadog_checks.dev.utils import load_jmx_config
+
+from .common import HERE
+
+HERE = os.path.join(HERE, 'compose')
+
+
+def setup_weblogic():
+    build_app_archive = os.path.join(HERE, 'weblogic', 'build-archive.sh')
+    setenv_script = os.path.join(HERE, 'weblogic', 'container-scripts', 'setEnv.sh')
+    domain_properties = os.path.join(HERE, 'weblogic', 'properties', 'docker-build', 'domain.properties')
+    run_command("/bin/bash -c '{}'".format(build_app_archive))
+    run_command("/bin/bash -c '{} {}'".format(setenv_script, domain_properties), check=True)
+
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    properties_dir = os.path.join(get_here(), 'compose', 'properties')
-    compose_file = os.path.join(get_here(), 'compose', 'docker-compose.yml')
-    with docker_run(
-        compose_file,
-        env_vars={'PROPERTIES_DIR': properties_dir}
-    ):
+    properties_dir = os.path.join(HERE, 'weblogic', 'properties')
+    compose_file = os.path.join(HERE, 'docker-compose.yml')
+    setup_weblogic()
+    with docker_run(compose_file, env_vars={'PROPERTIES_DIR': properties_dir}):
         yield load_jmx_config(), {'use_jmx': True}
--- weblogic/tests/test_check.py
+++ weblogic/tests/test_check.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.mark.e2e
 def test():
     pass
