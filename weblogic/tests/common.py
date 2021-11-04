7799b21e0780c213d92ef44cb4be281372ebdbff
Fanny Jiang <fanny.jiang@datadoghq.com>
2021-11-04 11:20:58 -0400

    update e2e

--- weblogic/tests/common.py
+++ weblogic/tests/common.py
@@ -10,3 +10,5 @@ INSTANCES = [{'host': 'localhost', 'port': '7002'}]
 
 CHECK_CONFIG = load_jmx_config()
 CHECK_CONFIG['instances'] = INSTANCES
+
+CHECK_NAME = "weblogic"
--- weblogic/tests/conftest.py
+++ weblogic/tests/conftest.py
@@ -3,6 +3,7 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 import os
+from copy import deepcopy
 
 import pytest
 
@@ -24,9 +25,22 @@ def setup_weblogic():
 
 
 @pytest.fixture(scope='session')
-def dd_environment():
+def dd_environment(instance):
     properties_dir = os.path.join(HERE, 'weblogic', 'properties')
     compose_file = os.path.join(HERE, 'docker-compose.yml')
     setup_weblogic()
     with docker_run(compose_file, env_vars={'PROPERTIES_DIR': properties_dir}):
-        yield load_jmx_config(), {'use_jmx': True}
+        yield instance, {'use_jmx': True}
+
+
+@pytest.fixture(scope='session', autouse=True)
+@pytest.mark.usefixtures('dd_environment')
+def instance():
+    inst = load_jmx_config()
+    # Add managed servers to the configuration
+    inst.get('instances').append(deepcopy(inst.get('instances')[0]))
+    inst['instances'][0]['port'] = 9091
+    inst.get('instances').append(deepcopy(inst.get('instances')[0]))
+    inst['instances'][0]['port'] = 9092
+
+    return inst
