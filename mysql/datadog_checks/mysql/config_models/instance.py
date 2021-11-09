0d5b1c6cd736a47f7a919b8bc0aac99528eb3446
Choonghyun Lee <notemusic0110@gmail.com>
2021-11-09 10:30:21 +0900

    check model sync

--- mysql/datadog_checks/mysql/config_models/instance.py
+++ mysql/datadog_checks/mysql/config_models/instance.py
@@ -85,6 +85,8 @@ class InstanceConfig(BaseModel):
     class Config:
         allow_mutation = False
 
+    additional_status: Optional[Sequence[Mapping[str, Any]]]
+    additional_variable: Optional[Sequence[Mapping[str, Any]]]
     charset: Optional[str]
     connect_timeout: Optional[float]
     custom_queries: Optional[Sequence[CustomQuery]]
