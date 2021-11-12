20033bdeb81fc2c54a685f5e80949ba63205c46b
Ofek Lev <ofekmeister@gmail.com>
2021-11-12 12:59:07 -0500

    remove redundant test

--- ibm_was/tests/test_unit.py
+++ ibm_was/tests/test_unit.py
@@ -81,12 +81,6 @@ def test_custom_query_unit_casing(aggregator, instance, check):
     aggregator.assert_metric('ibm_was.xdpm.total_memory', metric_type=aggregator.GAUGE)
 
 
-def test_config_validation(check, dd_run_check):
-    with pytest.raises(Exception, match='Please specify a servlet_url in the configuration file'):
-        check = IbmWasCheck('ibm_was', {}, [common.MISSING_REQ_FIELD_INSTANCE])
-        dd_run_check(check, extract_message=True)
-
-
 def test_critical_service_check(instance, check, aggregator):
     instance['servlet_url'] = 'http://localhost:5678/wasPerfTool/servlet/perfservlet'
     tags = ['url:{}'.format(instance['servlet_url']), 'key1:value1']
