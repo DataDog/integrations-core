cf455a9fce17b430a25fcb93b2fe46cd2a95202c
Ofek Lev <ofekmeister@gmail.com>
2021-11-12 20:12:47 -0500

    fix

--- windows_performance_counters/tests/conftest.py
+++ windows_performance_counters/tests/conftest.py
@@ -3,25 +3,9 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
 
+from .common import INSTANCE
+
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    instance = {
-        'namespace': 'test',
-        'metrics': {
-            'Processor': {
-                'name': 'cpu',
-                'tag_name': 'thread',
-                'instance_counts': {
-                    'total': 'num_cpu_threads.total',
-                    'monitored': 'num_cpu_threads.monitored',
-                    'unique': 'num_cpu_threads.unique',
-                },
-                'counters': [{'Interrupts/sec': 'interrupts.ps', '% User Time': {'metric_name': 'core.user_time'}}],
-            }
-        },
-        'server_tag': 'machine',
-        'tags': ['foo:bar', 'bar:baz'],
-    }
-    e2e_metadata = {'docker_platform': 'windows'}
-    yield instance, e2e_metadata
+    yield INSTANCE, {'docker_platform': 'windows'}
