0a433cb6d4caac49fe859e68119362a1fab83454
Fanny Jiang <fanny.jiang@datadoghq.com>
2021-11-12 19:44:24 -0500

    Document new include_labels option (#10617)
    * update configs
    
    * bump min base requirement
    
    * sync models
    
    * test
    
    * fix version
    
    * sync config
    
    * revert test

--- kube_scheduler/datadog_checks/kube_scheduler/config_models/defaults.py
+++ kube_scheduler/datadog_checks/kube_scheduler/config_models/defaults.py
@@ -96,6 +96,10 @@ def instance_ignore_tags(field, value):
     return get_default_field_value(field, value)
 
 
+def instance_include_labels(field, value):
+    return get_default_field_value(field, value)
+
+
 def instance_kerberos_auth(field, value):
     return 'disabled'
 
