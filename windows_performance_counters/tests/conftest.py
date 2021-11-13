5549764bc57261329141142c91838e3798fab59f
Ofek Lev <ofekmeister@gmail.com>
2021-11-12 19:17:16 -0500

    e2e necessary

---
+++ windows_performance_counters/tests/conftest.py
@@ -0,0 +1,25 @@
+# (C) Datadog, Inc. 2021-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
+
+
+@pytest.fixture(scope='session')
+def dd_environment():
+    yield {
+        'namespace': 'test',
+        'metrics': {
+            'Processor': {
+                'name': 'cpu',
+                'tag_name': 'thread',
+                'instance_counts': {
+                    'total': 'num_cpu_threads.total',
+                    'monitored': 'num_cpu_threads.monitored',
+                    'unique': 'num_cpu_threads.unique',
+                },
+                'counters': [{'Interrupts/sec': 'interrupts.ps', '% User Time': {'metric_name': 'core.user_time'}}],
+            }
+        },
+        'server_tag': 'machine',
+        'tags': ['foo:bar', 'bar:baz'],
+    }
