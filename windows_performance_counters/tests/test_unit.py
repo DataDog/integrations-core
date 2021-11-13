ad5c88924cf9fcd6cf778902521edfa72f34d63b
Ofek Lev <ofekmeister@gmail.com>
2021-11-12 21:41:57 -0500

    refactor tests

--- windows_performance_counters/tests/test_unit.py
+++ windows_performance_counters/tests/test_unit.py
@@ -7,7 +7,7 @@ from datadog_checks.base import PerfCountersBaseCheck
 from datadog_checks.base.constants import ServiceCheck
 from datadog_checks.windows_performance_counters import WindowsPerformanceCountersCheck
 
-from .common import INSTANCE
+pytestmark = pytest.mark.unit
 
 
 def test_subclass():
@@ -29,23 +29,3 @@ def test_basic(aggregator, dd_default_hostname, dd_run_check, mock_performance_o
 
     aggregator.assert_all_metrics_covered()
     aggregator.assert_service_check('test.windows.perf.health', ServiceCheck.OK, tags=static_tags)
-
-
-@pytest.mark.e2e
-def test_e2e(dd_agent_check):
-    aggregator = dd_agent_check(INSTANCE, rate=True)
-
-    num_threads = 1
-
-    aggregator.assert_service_check('test.windows.perf.health', ServiceCheck.OK)
-    aggregator.assert_metric('test.num_cpu_threads.total', num_threads + 1)
-    aggregator.assert_metric('test.num_cpu_threads.monitored', num_threads)
-    aggregator.assert_metric('test.num_cpu_threads.unique', num_threads)
-
-    for i in range(num_threads):
-        for metric in ('test.cpu.interrupts.ps', 'test.core.user_time'):
-            aggregator.assert_metric_has_tag(metric, f'thread:{i}')
-            aggregator.assert_metric_has_tag(metric, 'foo:bar')
-            aggregator.assert_metric_has_tag(metric, 'bar:baz')
-
-    aggregator.assert_all_metrics_covered()
