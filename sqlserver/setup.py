bee0621fe3cc9e228b1c8eca40978d718fd8679a
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2021-11-19 08:21:39 -0500

    bump checks base minimum version

--- sqlserver/setup.py
+++ sqlserver/setup.py
@@ -27,7 +27,7 @@ def get_dependencies():
         return f.readlines()
 
 
-CHECKS_BASE_REQ = 'datadog-checks-base>=23.1.5'
+CHECKS_BASE_REQ = 'datadog-checks-base>=23.3.1'
 
 setup(
     name='datadog-sqlserver',
