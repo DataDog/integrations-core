7dc7aead3cfe4dee99c5417d7d095b6b93f0d5f5
ChristineTChen <christine.chen@datadoghq.com>
2021-11-23 09:57:34 -0500

    Remove legacy test

--- istio/tests/test_istio_v1.py
+++ istio/tests/test_istio_v1.py
@@ -84,16 +84,3 @@ def test_legacy_version_metadata(datadog_agent, dd_run_check):
     }
 
     datadog_agent.assert_metadata('test:123', version_metadata)
-
-
-def test_legacy_istio_agent(aggregator, dd_run_check):
-    """
-    Test the istiod deployment endpoint for V2 implementation
-    """
-    check = Istio('istio', {}, [common.MOCK_LEGACY_MESH_INSTANCE])
-    with requests_mock.Mocker() as metric_request:
-        metric_request.get('http://localhost:15090/metrics', text=get_response('1.5', 'istio-merged.txt'))
-        dd_run_check(check)
-
-    for metric in common.ISTIO_AGENT_METRICS:
-        aggregator.assert_metric(metric)
