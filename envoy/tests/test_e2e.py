bf062bf7ff6732789a43c39808565a6de8869272
ChristineTChen <christine.chen@datadoghq.com>
2021-11-30 18:23:46 -0500

    Add e2e test

---
+++ envoy/tests/test_e2e.py
@@ -0,0 +1,13 @@
+from datadog_checks.envoy import Envoy
+
+from .common import DEFAULT_INSTANCE, PROMETHEUS_METRICS, requires_new_environment
+
+pytestmark = [requires_new_environment]
+
+
+@pytest.mark.e2e
+def test_e2e(dd_agent_check):
+    aggregator = dd_agent_check(DEFAULT_INSTANCE, rate=True)
+    for metric in PROMETHEUS_METRICS:
+        aggregator.assert_metric(metric)
+    aggregator.assert_service_check('envoy.can_connect', Envoy.OK)
