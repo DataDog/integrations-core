ea03b2a19766303e22401e4e6273d858ed22966e
ChristineTChen <christine.chen@datadoghq.com>
2021-12-02 13:47:40 -0500

    Fix docker e2e env

---
+++ silk/tests/common.py
@@ -0,0 +1,15 @@
+# (C) Datadog, Inc. 2021-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+
+from datadog_checks.dev import get_docker_hostname, get_here
+
+HERE = get_here()
+HOST = get_docker_hostname()
+PORT = 443
+INSTANCE_LEGACY = {
+    'host': '{}:{}'.format(HOST, PORT),
+    'tags': ['test:silk'],
+}
+COMPOSE_FILE = os.path.join(HERE, 'docker', 'docker-compose.yaml')
