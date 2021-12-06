e4c070a546420652aca8d79bbe85e95718164243
Alexandre Yang <alexandre.yang@datadoghq.com>
2021-12-06 16:09:35 +0100

    Add profile metadata for Cisco Catalyst and 3850 (#10767)

--- snmp/tests/test_e2e_core_metadata.py
+++ snmp/tests/test_e2e_core_metadata.py
@@ -1,6 +1,8 @@
 # (C) Datadog, Inc. 2021-present
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
+import pprint
+
 import pytest
 
 from . import common
@@ -130,3 +132,118 @@ def test_e2e_core_metadata_f5(dd_agent_check):
         },
     ]
     assert_network_devices_metadata(aggregator, events)
+
+
+def test_e2e_core_metadata_cisco_3850(dd_agent_check):
+    config = common.generate_container_instance_config([])
+    instance = config['instances'][0]
+    instance.update(
+        {
+            'community_string': 'cisco-3850',
+            'loader': 'core',
+        }
+    )
+
+    aggregator = dd_agent_check(config, rate=False)
+
+    device_ip = instance['ip_address']
+
+    events = aggregator.get_event_platform_events("network-devices-metadata", parse_json=True)
+
+    # since there are >100 resources (device+interfaces), the interfaces are split into 2 events
+    assert len(events) == 2
+    event1 = events[0]
+
+    # assert device (there is only one device)
+    pprint.pprint(event1['devices'])
+    assert len(event1['devices']) == 1
+    actual_device = event1['devices'][0]
+    device = {
+        u'description': u'Cisco IOS Software, IOS-XE Software, Catalyst L3 Switch '
+        u'Software (CAT3K_CAA-UNIVERSALK9-M), Version 03.06.06E RELEASE '
+        u'SOFTWARE (fc1) Technical Support: '
+        u'http://www.cisco.com/techsupport Copyright (c) 1986-2016 by '
+        u'Cisco Systems, Inc. Compiled Sat 17-Dec-',
+        u'id': u'default:' + device_ip,
+        u'id_tags': [u'device_namespace:default', u'snmp_device:' + device_ip],
+        u'ip_address': device_ip,
+        u'name': u'Cat-3850-4th-Floor.companyname.local',
+        u'os_name': u'IOS',
+        u'profile': u'cisco-3850',
+        u'status': 1,
+        u'sys_object_id': u'1.3.6.1.4.1.9.1.1745',
+        u'tags': [
+            u'device_namespace:default',
+            u'device_vendor:cisco',
+            u'snmp_device:' + device_ip,
+            u'snmp_host:Cat-3850-4th-Floor.companyname.local',
+            u'snmp_profile:cisco-3850',
+        ],
+        u'vendor': u'cisco',
+        u'version': u'03.06.06E',
+        u'serial_number': u'FOCXXXXXXXX',
+        u'model': u'CAT3K_CAA-UNIVERSALK9-M',
+    }
+    assert device == actual_device
+
+    # assert one interface
+    pprint.pprint(event1['interfaces'])
+    assert len(event1['interfaces']) > 1
+    actual_interface = event1['interfaces'][0]
+    interface = {
+        u'admin_status': 1,
+        u'description': u'GigabitEthernet0/0',
+        u'device_id': u'default:' + device_ip,
+        u'id_tags': [u'interface:Gi0/0'],
+        u'index': 1,
+        u'mac_address': u'0x000000000000',
+        u'name': u'Gi0/0',
+        u'oper_status': 2,
+    }
+    assert interface == actual_interface
+
+
+def test_e2e_core_metadata_cisco_catalyst(dd_agent_check):
+    config = common.generate_container_instance_config([])
+    instance = config['instances'][0]
+    instance.update(
+        {
+            'community_string': 'cisco-catalyst',
+            'loader': 'core',
+        }
+    )
+
+    aggregator = dd_agent_check(config, rate=False)
+
+    device_ip = instance['ip_address']
+
+    events = aggregator.get_event_platform_events("network-devices-metadata", parse_json=True)
+    assert len(events) == 1
+    event1 = events[0]
+
+    # assert device (there is only one device)
+    pprint.pprint(event1['devices'])
+    assert len(event1['devices']) == 1
+    actual_device = event1['devices'][0]
+    device = {
+        u'id': u'default:' + device_ip,
+        u'id_tags': [
+            u'device_namespace:default',
+            u'snmp_device:' + device_ip,
+        ],
+        u'ip_address': device_ip,
+        u'name': u'catalyst-6000.example',
+        u'profile': u'cisco-catalyst',
+        u'status': 1,
+        u'sys_object_id': u'1.3.6.1.4.1.9.1.241',
+        u'tags': [
+            u'device_namespace:default',
+            u'device_vendor:cisco',
+            u'snmp_device:' + device_ip,
+            u'snmp_host:catalyst-6000.example',
+            u'snmp_profile:cisco-catalyst',
+        ],
+        u'vendor': u'cisco',
+        u'serial_number': u'SCA044001J9',
+    }
+    assert device == actual_device
