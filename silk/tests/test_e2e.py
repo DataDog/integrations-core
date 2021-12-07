f7c5a92305060ff3aa0984e2a2b7c221ec1598c2
ChristineTChen <christine.chen@datadoghq.com>
2021-12-07 11:22:05 -0500

    FIx style

--- silk/tests/test_e2e.py
+++ silk/tests/test_e2e.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2021-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
 
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.silk import SilkCheck
@@ -9,11 +10,11 @@ from .common import METRICS
 
 
 @pytest.mark.e2e
-def test_e2e(dd_agent_check):
-    aggregator = dd_agent_check(DEFAULT_INSTANCE, rate=True)
+def test_e2e(dd_agent_check, instance):
+    aggregator = dd_agent_check(instance, rate=True)
     for metric in METRICS:
         aggregator.assert_metric(metric)
 
-    aggregator.assert_service_check('silk.can_connect', Envoy.OK)
-    aggregator.assert_service_check('silk.state', Envoy.OK)
+    aggregator.assert_service_check('silk.can_connect', SilkCheck.OK)
+    aggregator.assert_service_check('silk.state', SilkCheck.OK)
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
