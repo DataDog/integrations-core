41ddab19f68ca660fc911a9968e809e30116ecf2
Fanny Jiang <fanny.jiang@datadoghq.com>
2021-12-09 15:45:44 -0500

    move setup scripts to Dockerfile

--- weblogic/tests/conftest.py
+++ weblogic/tests/conftest.py
@@ -16,34 +16,15 @@ from .common import HERE
 HERE = os.path.join(HERE, 'compose')
 
 
-def setup_weblogic():
-    build_app_archive = os.path.join(HERE, 'weblogic', 'build-archive.sh')
-    setenv_script = os.path.join(HERE, 'weblogic', 'container-scripts', 'setEnv.sh')
-    domain_properties = os.path.join(HERE, 'weblogic', 'properties', 'docker-build', 'domain.properties')
-
-    run_command("/bin/bash -c '{}'".format(build_app_archive))
-    run_command("/bin/bash -c '{} {}'".format(setenv_script, domain_properties), check=True)
-
-
-def cleanup_weblogic():
-    run_command(
-        "/bin/bash -c 'rm -rf {} {}'".format(
-            os.path.join(HERE, 'weblogic', 'archive.zip'), os.path.join(HERE, 'weblogic', 'app-archive')
-        )
-    )
-
-
 @pytest.fixture(scope='session')
 def dd_environment(instance):
     properties_dir = os.path.join(HERE, 'weblogic', 'properties')
     compose_file = os.path.join(HERE, 'docker-compose.yml')
-    WaitFor(setup_weblogic())
     with docker_run(
         compose_file=compose_file,
         env_vars={'PROPERTIES_DIR': properties_dir},
         sleep=60,
         build=True,
-        down=cleanup_weblogic,
     ):
         yield instance, {'use_jmx': True}
 
