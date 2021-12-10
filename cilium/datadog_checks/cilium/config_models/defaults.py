48f61314647aad0c13535258c124d91da0ee2d32
ChristineTChen <christine.chen@datadoghq.com>
2021-12-10 17:47:39 -0500

    openmetrics_endpoint is not required

--- cilium/datadog_checks/cilium/config_models/defaults.py
+++ cilium/datadog_checks/cilium/config_models/defaults.py
@@ -208,6 +208,10 @@ def instance_ntlm_domain(field, value):
     return get_default_field_value(field, value)
 
 
+def instance_openmetrics_endpoint(field, value):
+    return get_default_field_value(field, value)
+
+
 def instance_operator_endpoint(field, value):
     return get_default_field_value(field, value)
 
