08b6e08c06d4e7202e2de60c23226a68f130db8e
ChristineTChen <christine.chen@datadoghq.com>
2021-12-10 21:51:31 -0500

    Add e2e

--- cilium/tests/test_e2e.py
+++ cilium/tests/test_e2e.py
@@ -1,7 +1,15 @@
 # (C) Datadog, Inc. 2021-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
 
-from .common import requires_new_environment
+from .common import AGENT_V2_METRICS, requires_new_environment
 
 pytestmark = [requires_new_environment]
+
+
+@pytest.mark.e2e
+def test_check_ok(dd_agent_check):
+    aggregator = dd_agent_check(rate=True)
+    for metric in AGENT_V2_METRICS:
+        aggregator.assert_metric(metric)
