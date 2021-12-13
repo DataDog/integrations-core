816d3fdeb491ef2e69c53cdf769035dea57cdd53
ChristineTChen <christine.chen@datadoghq.com>
2021-12-13 17:34:07 -0500

    Fix style

--- cilium/tests/legacy/test_cilium.py
+++ cilium/tests/legacy/test_cilium.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2019-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+
 from ..common import CILIUM_VERSION, requires_legacy_environment
 
 # Legacy common metrics
