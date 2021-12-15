d5e603e5c4f391e80283e087ba6ac0a36f1b1450
ChristineTChen <christine.chen@datadoghq.com>
2021-12-15 16:06:16 -0500

    refactor service check

--- silk/datadog_checks/silk/check.py
+++ silk/datadog_checks/silk/check.py
@@ -35,6 +35,30 @@ class SilkCheck(AgentCheck):
         self.tags = self.instance.get("tags", [])
 
     def check(self, _):
+        host_tags = self.get_host_tags()
+
+        # Get system state
+        self.submit_state()
+
+        # Get events
+        self.collect_events()
+
+        # Get metrics
+        get_method = getattr
+        for path, metrics_obj in METRICS.items():
+            # Need to submit an object of relevant tags
+            try:
+                response_json, _ = self.get_metrics(path)
+                self.parse_metrics(response_json, path, metrics_obj, get_method)
+            except Exception as e:
+                self.log.debug("Encountered error getting Silk metrics for path %s: %s", path, str(e))
+        self.service_check(self.CONNECT_SERVICE_CHECK, AgentCheck.OK, tags=self.tags)
+
+    def get_host_tags(self):
+
+        pass
+
+    def submit_state(self):
         # Get Silk State
         try:
             response_json, code = self.get_metrics(self.STATE_ENDPOINT)
@@ -58,20 +82,6 @@ class SilkCheck(AgentCheck):
                 self.service_check(self.STATE_SERVICE_CHECK, AgentCheck.UNKNOWN, message=msg, tags=self.tags)
                 self.service_check(self.CONNECT_SERVICE_CHECK, AgentCheck.CRITICAL, message=msg, tags=self.tags)
                 return
-
-        get_method = getattr
-
-        # Get events
-        self.collect_events()
-
-        for path, metrics_obj in METRICS.items():
-            # Need to submit an object of relevant tags
-            try:
-                response_json, _ = self.get_metrics(path)
-                self.parse_metrics(response_json, path, metrics_obj, get_method)
-            except Exception as e:
-                self.log.debug("Encountered error getting Silk metrics for path %s: %s", path, str(e))
-        self.service_check(self.CONNECT_SERVICE_CHECK, AgentCheck.OK, tags=self.tags)
 
     def parse_metrics(self, output, path, metrics_mapping=None, get_method=None, return_first=False):
         """
