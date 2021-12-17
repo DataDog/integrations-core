e2fe1f08b8e7c2fa99c0e4dff40c3db4d9fdd9c8
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-12-17 11:28:42 +0100

    Add MQ_FILE_PATH env var to tests

--- ibm_mq/tests/common.py
+++ ibm_mq/tests/common.py
@@ -140,7 +140,11 @@ INSTANCE_QUEUE_REGEX_TAG = {
 E2E_METADATA = {
     'docker_volumes': ['{}/agent_scripts/start_commands.sh:/tmp/start_commands.sh'.format(HERE)],
     'start_commands': ['bash /tmp/start_commands.sh'],
-    'env_vars': {'LD_LIBRARY_PATH': '/opt/mqm/lib64:/opt/mqm/lib', 'C_INCLUDE_PATH': '/opt/mqm/inc'},
+    'env_vars': {
+        'LD_LIBRARY_PATH': '/opt/mqm/lib64:/opt/mqm/lib',
+        'C_INCLUDE_PATH': '/opt/mqm/inc',
+        'MQ_FILE_PATH': 'c:\\ibm_mq',
+    },
 }
 
 QUEUE_METRICS = [
