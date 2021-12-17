34e57b0afba79bc66c4d32c73c58647ae58e4b29
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-12-17 13:24:31 +0100

    Fix bat script

--- .azure-pipelines/scripts/ibm_mq/windows/55_install_client.bat
+++ .azure-pipelines/scripts/ibm_mq/windows/55_install_client.bat
@@ -1,7 +1,5 @@
-#:: https://docs.microsoft.com/en-us/sql/connect/odbc/windows/system-requirements-installation-and-driver-files
-#:: Finding the actual URL not gated by a form was a nightmare
-# This script installs IBM MQ development version on the CI machines to be able to
-# * Compile pymqi image
-# * Run integration tests on the machine
+#:: This script installs IBM MQ development version on the CI machines to be able to
+#:: * Compile pymqi image
+#:: * Run integration tests on the machine
 
-RUN Powershell -C .\install_ibm_mq.ps1 "9.2.2.0"
+powershell -Command .\install_ibm_mq.ps1 "9.2.2.0"
