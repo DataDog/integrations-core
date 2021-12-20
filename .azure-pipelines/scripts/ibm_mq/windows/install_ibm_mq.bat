0c3800801f404005c4e3b0ed04dd3d898dde8401
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-12-20 15:14:42 +0100

    Create directory

--- .azure-pipelines/scripts/ibm_mq/windows/install_ibm_mq.bat
+++ .azure-pipelines/scripts/ibm_mq/windows/install_ibm_mq.bat
@@ -3,9 +3,9 @@ set version=9.2.2.0
 set file=%version%-IBM-MQC-Redist-Win64.zip
 set source=https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/%file%
 
+md %target%
 :: Downloading IBM MQ client
 powershell -command "Invoke-WebRequest -Uri %source% -OutFile %target%\%file%"
-dir $target
 :: Extracting IBM MQ client
 powershell -command "Expand-Archive %target%\%file% %target%"
 dir $target
