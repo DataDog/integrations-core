d19c6157712f48a841902b399a37d04fa8f71263
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-12-20 16:32:19 +0100

    fix url?

--- .azure-pipelines/scripts/ibm_mq/windows/install_ibm_mq.bat
+++ .azure-pipelines/scripts/ibm_mq/windows/install_ibm_mq.bat
@@ -2,7 +2,7 @@ set target=c:\ibm_mq
 set version=9.2.2.0
 set file=%version%-IBM-MQC-Redist-Win64.zip
 set ibm_source=https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/%file%
-set source=https://drive.google.com/uc?export=download&id=1ysTsHxAAxIwl0EwFnYzN5cNRBnvKwELw
+set "source=https://drive.google.com/uc?export=download&id=1ysTsHxAAxIwl0EwFnYzN5cNRBnvKwELw"
 
 md %target%
 :: Downloading IBM MQ client
