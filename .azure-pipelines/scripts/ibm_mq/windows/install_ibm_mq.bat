9d805ecfd5839fed9a110dc240149674d610249e
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-12-20 14:06:15 +0100

    Fix variables

--- .azure-pipelines/scripts/ibm_mq/windows/install_ibm_mq.bat
+++ .azure-pipelines/scripts/ibm_mq/windows/install_ibm_mq.bat
@@ -4,7 +4,7 @@ set file=%version%-IBM-MQC-Redist-Win64.zip
 set source=https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/%file%
 
 :: Downloading IBM MQ client
-powershell -command "Invoke-WebRequest -Uri $source -OutFile %target%\%file%"
+powershell -command "Invoke-WebRequest -Uri %source% -OutFile %target%\%file%"
 dir $target
 :: Extracting IBM MQ client
 powershell -command "Expand-Archive %target%\%file% %target%\"
