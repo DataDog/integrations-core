c22c9e078e16d9bdb9dfc6c919fa298e69f98549
Julia Simon <611228+hithwen@users.noreply.github.com>
2021-12-22 15:18:11 +0100

    Style fix

--- datadog_checks_base/tests/models/config_models/validators.py
+++ datadog_checks_base/tests/models/config_models/validators.py
@@ -3,7 +3,6 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 
-
 # Here you can include additional config validators or transformers
 #
 # def initialize_instance(values, **kwargs):
