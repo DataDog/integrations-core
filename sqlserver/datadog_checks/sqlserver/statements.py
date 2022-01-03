2d0f9ef5382047b0b36a836c02b3ec6184e4595a
Justin Iso <justin.james.iso@gmail.com>
2022-01-03 11:58:42 -0500

    code review feedback

--- sqlserver/datadog_checks/sqlserver/statements.py
+++ sqlserver/datadog_checks/sqlserver/statements.py
@@ -153,11 +153,11 @@ class SqlserverStatementMetrics(DBMAsyncJob):
             job_name="query-metrics",
             shutdown_callback=self._close_db_conn,
         )
-        self.disable_secondary_tags = bool(check.statement_metrics_config.get('disable_secondary_tags', False))
+        self.disable_secondary_tags = is_affirmative(check.statement_metrics_config.get('disable_secondary_tags', False))
         self.dm_exec_query_stats_row_limit = int(
             check.statement_metrics_config.get('dm_exec_query_stats_row_limit', 10000)
         )
-        self.enforce_collection_interval_deadline = bool(
+        self.enforce_collection_interval_deadline = is_affirmative(
             check.statement_metrics_config.get('enforce_collection_interval_deadline', True)
         )
         self._state = StatementMetrics()
@@ -202,8 +202,8 @@ class SqlserverStatementMetrics(DBMAsyncJob):
             return self._statement_metrics_query
         available_columns = self._get_available_query_metrics_columns(cursor, SQL_SERVER_QUERY_METRICS_COLUMNS)
 
-        sql = STATEMENT_METRICS_QUERY_NO_AGGREGATES if self.disable_secondary_tags else STATEMENT_METRICS_QUERY
-        self._statement_metrics_query = sql.format(
+        statements_query = STATEMENT_METRICS_QUERY_NO_AGGREGATES if self.disable_secondary_tags else STATEMENT_METRICS_QUERY
+        self._statement_metrics_query = statements_query.format(
             query_metrics_columns=', '.join(available_columns),
             query_metrics_column_sums=', '.join(['sum({}) as {}'.format(c, c) for c in available_columns]),
             collection_interval=int(math.ceil(self.collection_interval) * 2),
