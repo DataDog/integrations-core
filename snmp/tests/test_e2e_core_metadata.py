426c609b59db085dbe94a69091aba69defae5948
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-01-05 10:53:53 +0100

    Add profile metadata for netapp (#10968)

--- snmp/tests/test_e2e_core_metadata.py
+++ snmp/tests/test_e2e_core_metadata.py
@@ -690,3 +690,47 @@ def test_e2e_core_metadata_palo_alto(dd_agent_check):
         'version': '9.0.5',
     }
     assert_device_metadata(aggregator, device)
+
+
+def test_e2e_core_metadata_netapp(dd_agent_check):
+    config = common.generate_container_instance_config([])
+    instance = config['instances'][0]
+    instance.update(
+        {
+            'community_string': 'netapp',
+            'loader': 'core',
+        }
+    )
+
+    aggregator = dd_agent_check(config, rate=False)
+
+    device_ip = instance['ip_address']
+
+    device = {
+        'description': 'NetApp Release 9.3P7: Wed Jul 25 10:11:10 UTC 2018',
+        'id': 'default:' + device_ip,
+        'id_tags': [
+            'device_namespace:default',
+            'snmp_device:' + device_ip,
+        ],
+        'ip_address': device_ip,
+        'location': 'France',
+        'model': 'example-model',
+        'name': 'example-datacenter.company',
+        'os_name': 'ONTAP',
+        'os_version': '9.3',
+        'profile': 'netapp',
+        'serial_number': '1-23-456789',
+        'status': 1,
+        'sys_object_id': '1.3.6.1.4.1.789.2.5',
+        'tags': [
+            'device_namespace:default',
+            'device_vendor:netapp',
+            'snmp_device:' + device_ip,
+            'snmp_host:example-datacenter.company',
+            'snmp_profile:netapp',
+        ],
+        'vendor': 'netapp',
+        'version': '9.3P7:',
+    }
+    assert_device_metadata(aggregator, device)
