88b5471366033a90016be367397bc9a0e8eba0a5
Alex Barksdale <me@alexbarksdale.com>
2022-01-05 12:04:28 -0500

    Add option to enable metadata collection with default false

--- sqlserver/datadog_checks/sqlserver/sqlserver.py
+++ sqlserver/datadog_checks/sqlserver/sqlserver.py
@@ -118,10 +118,10 @@ class SQLServer(AgentCheck):
             json.dumps(
                 {
                     'replace_digits': obfuscator_options_config.get('replace_digits', False),
+                    'return_json_metadata': obfuscator_options_config.get('collect_metadata', False),
                     'table_names': obfuscator_options_config.get('collect_tables', True),
                     'collect_commands': obfuscator_options_config.get('collect_commands', True),
                     'collect_comments': obfuscator_options_config.get('collect_comments', True),
-                    'return_json_metadata': True,
                 }
             )
         )
