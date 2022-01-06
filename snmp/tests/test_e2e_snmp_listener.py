2154c4dea325e35032e200952771b442c62f5718
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-01-06 11:29:01 +0100

    use rate

--- snmp/tests/test_e2e_snmp_listener.py
+++ snmp/tests/test_e2e_snmp_listener.py
@@ -28,7 +28,7 @@ def test_e2e_snmp_listener(dd_agent_check, container_ip, autodiscovery_ready):
     """
     snmp_device = _build_device_ip(container_ip)
     subnet_prefix = ".".join(container_ip.split('.')[:3])
-    aggregator = dd_agent_check({'init_config': {}, 'instances': []})
+    aggregator = dd_agent_check({'init_config': {}, 'instances': []}, rate=True)
 
     # # === network profile ===
     # common_tags = [
@@ -131,7 +131,7 @@ def test_e2e_snmp_listener(dd_agent_check, container_ip, autodiscovery_ready):
     common.assert_common_metrics(aggregator, common_tags, is_e2e=True, loader='core')
     # aggregator.assert_metric('snmp.sysUpTimeInstance', tags=common_tags)
     for metric in IF_SCALAR_GAUGE:
-        aggregator.assert_metric('snmp.{}'.format(metric), metric_type=aggregator.GAUGE, tags=common_tags, count=1)
+        aggregator.assert_metric('snmp.{}'.format(metric), metric_type=aggregator.GAUGE, tags=common_tags, count=2)
 
     # # test ignored IPs
     # tags = [
