22e77fd3f8ffa53c5abfc30f0df641935bd36f6a
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-01-14 10:39:35 +0100

    Different errors for required options

--- ibm_mq/datadog_checks/ibm_mq/config_models/validators.py
+++ ibm_mq/datadog_checks/ibm_mq/config_models/validators.py
@@ -5,9 +5,13 @@
 # Here you can include additional config validators or transformers
 #
 
+
 def initialize_instance(values, **kwargs):
     for key, value in values.items():
         if value is None or value == '':
+            if key in ['channel', 'queue_manager']:  # These are required options
+                raise ValueError("'{}' cannot be empty.".format(key))
             raise ValueError(
-                "'{}' cannot be empty. If you don't want to provide a value you can comment this option".format(key))
+                "'{}' cannot be empty. If you don't want to provide a value you can comment this option".format(key)
+            )
     return values
