6d9be7dacaa7bba63581c5586b81d3c3174567ee
Ofek Lev <ofekmeister@gmail.com>
2022-01-19 16:40:18 -0500

    Add Pulsar integration

---
+++ pulsar/tests/test_integration.py
@@ -0,0 +1,25 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
+
+from datadog_checks.base.constants import ServiceCheck
+
+from .common import EXPECTED_METRICS, OPTIONAL_METRICS
+
+pytestmark = [pytest.mark.usefixtures('dd_environment'), pytest.mark.integration]
+
+
+def test_check(aggregator, dd_run_check, pulsar_check, instance):
+    check = pulsar_check(instance)
+    dd_run_check(check)
+
+    aggregator.assert_service_check('pulsar.openmetrics.health', ServiceCheck.OK)
+
+    for metric in EXPECTED_METRICS:
+        aggregator.assert_metric(metric)
+
+    for metric in OPTIONAL_METRICS:
+        aggregator.assert_metric(metric, count=0)
+
+    aggregator.assert_all_metrics_covered()
