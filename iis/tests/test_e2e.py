a6bd5107049feeddd171f46427637ff48f1fbaad
david-donovan-dd <david.donovan@datadoghq.com>
2022-01-21 16:32:48 -0500

    remove py2 check. IIS loading errors are not handled in v1, and will not permit an E2E test.

--- iis/tests/test_e2e.py
+++ iis/tests/test_e2e.py
@@ -4,15 +4,8 @@
 
 import pytest
 
-from datadog_checks.dev.testing import requires_py2, requires_py3
+from datadog_checks.dev.testing import requires_py3
 from datadog_checks.iis import IIS
-
-
-@pytest.mark.e2e
-@requires_py2
-def test_e2e_py2(dd_agent_check, aggregator, instance, check):
-    aggregator = dd_agent_check(instance)
-    aggregator.assert_service_check('iis.site_up', IIS.CRITICAL)
 
 
 @pytest.mark.e2e
