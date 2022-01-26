68bfbd72a197e56f1df46d739f89b810360abe51
Paul Coignet <paul.coignet@datadoghq.com>
2022-01-26 11:19:09 +0100

    Add tests

--- snowflake/datadog_checks/snowflake/config_models/validators.py
+++ snowflake/datadog_checks/snowflake/config_models/validators.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2021-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.base import ConfigurationError
 
 
 def initialize_instance(values, **kwargs):
@@ -11,14 +12,14 @@ def initialize_instance(values, **kwargs):
     _validate_authenticator_option(values)
 
     if 'private_key_password' in values and 'private_key_path' not in values:
-        raise Exception(
+        raise ConfigurationError(
             'Option `private_key_password` is set but not option `private_key_path`. '
             'Set `private_key_path` or remove `private_key_password` entry.'
         )
 
     if values.get('only_custom_queries', False) and len(values('metric_groups', [])) > 0:
-        raise Exception(
-            'Option `only_custom_queries` and `metric_groups` are not compatible.'
+        raise ConfigurationError(
+            'Option `only_custom_queries` and `metric_groups` are not compatible. '
             '`only_custom_queries` prevents `metric_groups` to be collected.'
         )
 
@@ -27,7 +28,7 @@ def initialize_instance(values, **kwargs):
 
 def _validate_authenticator_option(values):
     authenticator = values.get('authenticator', 'snowflake')
-    # `key` needs at list one item in `values` to be set
+    # `key` needs at least one item in `values` to be set
     authenticator_dependencies = {
         'snowflake': ['password', 'private_key_path'],
         'snowflake_jwt': ['private_key_path'],
@@ -35,14 +36,14 @@ def _validate_authenticator_option(values):
     }
 
     if authenticator not in authenticator_dependencies:
-        raise Exception(
+        raise ConfigurationError(
             'Unkwown authenticator option {}, supported options are {}.'.format(
                 authenticator, authenticator_dependencies.keys()
             )
         )
 
     if not any([opt in values for opt in authenticator_dependencies[authenticator]]):
-        raise Exception(
+        raise ConfigurationError(
             'Authenticator option `{}` needs `{}` option to be set.'.format(
                 authenticator, '` or `'.join(authenticator_dependencies[authenticator])
             )
