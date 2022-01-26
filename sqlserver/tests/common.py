7488aebf8cb1ebd0d723e95555213bf4fd172a78
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2022-01-26 15:23:38 -0500

    run full test suite for every supported Windows SQL Server driver (#10997)
    * run full test suite for every supported Windows SQL Server driver
    
    Remove the limited "can we connect to SQL Server using all supported drivers?" test in favor of running the full Windows test suite once per supported driver: SQLOLEDB, SQLNCLI11, MSOLEDBSQL, odbc.
    
    We've had bugs in the past that are specific to a single driver and this will ensure that all supported drivers are fully tested.
    
    * modify to reduce timeout:
    
    * typo
    
    * further simplify
    
    * not windows ADO
    
    * install sql server

--- sqlserver/tests/common.py
+++ sqlserver/tests/common.py
@@ -77,39 +77,6 @@ EXPECTED_AO_METRICS_PRIMARY = [m[0] for m in AO_METRICS_PRIMARY]
 EXPECTED_AO_METRICS_SECONDARY = [m[0] for m in AO_METRICS_SECONDARY]
 EXPECTED_AO_METRICS_COMMON = [m[0] for m in AO_METRICS]
 
-INSTANCE_DOCKER_DEFAULTS = {
-    'host': '{},1433'.format(HOST),
-    'connector': 'odbc',
-    'driver': get_local_driver(),
-    'username': 'datadog',
-    'password': 'Password12!',
-    'disable_generic_tags': True,
-    'tags': ['optional:tag1'],
-}
-
-INSTANCE_DOCKER = INSTANCE_DOCKER_DEFAULTS.copy()
-INSTANCE_DOCKER.update(
-    {
-        'include_task_scheduler_metrics': True,
-        'include_db_fragmentation_metrics': True,
-        'include_fci_metrics': True,
-        'include_ao_metrics': False,
-        'include_master_files_metrics': True,
-        'disable_generic_tags': True,
-    }
-)
-
-INSTANCE_AO_DOCKER_SECONDARY = {
-    'host': '{},1434'.format(HOST),
-    'connector': 'odbc',
-    'driver': 'FreeTDS',
-    'username': 'datadog',
-    'password': 'Password12!',
-    'tags': ['optional:tag1'],
-    'include_ao_metrics': True,
-    'disable_generic_tags': True,
-}
-
 CUSTOM_QUERY_A = {
     'query': "SELECT letter, num FROM (VALUES (97, 'a'), (98, 'b'), (99, 'c')) AS t (num,letter)",
     'columns': [{'name': 'customtag', 'type': 'tag'}, {'name': 'num', 'type': 'gauge'}],
@@ -121,10 +88,6 @@ CUSTOM_QUERY_B = {
     'columns': [{'name': 'customtag', 'type': 'tag'}, {'name': 'num', 'type': 'gauge'}],
     'tags': ['query:another_custom_one'],
 }
-
-INSTANCE_E2E = INSTANCE_DOCKER.copy()
-INSTANCE_E2E['driver'] = 'FreeTDS'
-INSTANCE_E2E['dbm'] = True
 
 INSTANCE_SQL_DEFAULTS = {
     'host': DOCKER_SERVER,
@@ -206,8 +169,6 @@ INIT_CONFIG_ALT_TABLES = {
         },
     ]
 }
-
-FULL_E2E_CONFIG = {"init_config": INIT_CONFIG, "instances": [INSTANCE_E2E]}
 
 
 def assert_metrics(aggregator, expected_tags, dbm_enabled=False):
