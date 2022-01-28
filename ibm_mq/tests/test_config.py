2a6bd81d2b8dcf41ee233dfcef3e426ff38152e0
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-01-28 10:38:15 +0100

    Add test about incompatible parameters

--- ibm_mq/tests/test_config.py
+++ ibm_mq/tests/test_config.py
@@ -4,6 +4,7 @@
 
 import pytest
 
+from datadog_checks.base import ConfigurationError
 from datadog_checks.ibm_mq.config import IBMMQConfig
 
 from .common import HOST
@@ -22,3 +23,20 @@ def test_mq_tag(instance, override_hostname, expected_hostname, expected_tag):
     assert config.hostname == expected_hostname
     if expected_tag:
         assert expected_tag in config.tags
+
+
+def test_cannot_set_host_and_connection_name(instance):
+    instance['connection_name'] = "localhost(8080)"
+    with pytest.raises(ConfigurationError, match="Specify only one host/port or connection_name configuration"):
+        IBMMQConfig(instance)
+
+
+def test_cannot_set_override_hostname_and_connection_name(instance):
+    instance['connection_name'] = "localhost(8080)"
+    del instance['host']
+    del instance['port']
+    instance['override_hostname'] = True
+    with pytest.raises(
+        ConfigurationError, match="You cannot override the hostname if you provide a connection_name instead of a host"
+    ):
+        IBMMQConfig(instance)
