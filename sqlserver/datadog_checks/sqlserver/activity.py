d15ab3c5e8a740bd5af7ed6ded101ed9373bedf9
jmeunier28 <jojo.meunier@datadoghq.com>
2022-01-30 20:00:58 -0800

    Fix activity query so we catch sessions that are not transactions

--- sqlserver/datadog_checks/sqlserver/activity.py
+++ sqlserver/datadog_checks/sqlserver/activity.py
@@ -15,6 +15,7 @@ except ImportError:
     from ..stubs import datadog_agent
 
 DEFAULT_COLLECTION_INTERVAL = 10
+DEFAULT_TX_COLLECTION_INTERVAL = 60
 MAX_PAYLOAD_BYTES = 19e6
 
 CONNECTIONS_QUERY = """\
@@ -28,6 +29,7 @@ FROM sys.dm_exec_sessions
     GROUP BY login_name, status, DB_NAME(database_id)
 """
 
+# collects active session load on db
 ACTIVITY_QUERY = re.sub(
     r'\s+',
     ' ',
@@ -37,14 +39,10 @@ SELECT
         NVARCHAR, TODATETIMEOFFSET(CURRENT_TIMESTAMP, DATEPART(TZOFFSET, SYSDATETIMEOFFSET())), 126
     ) as now,
     CONVERT(
-        NVARCHAR, TODATETIMEOFFSET(at.transaction_begin_time, DATEPART(TZOFFSET, SYSDATETIMEOFFSET())), 126
-    ) as transaction_begin_time,
-    CONVERT(
         NVARCHAR, TODATETIMEOFFSET(r.start_time, DATEPART(TZOFFSET, SYSDATETIMEOFFSET())), 126
     ) as query_start,
-    at.transaction_type,
-    at.transaction_state,
     sess.login_name as user_name,
+    sess.last_request_start_time,
     sess.session_id as id,
     DB_NAME(sess.database_id) as database_name,
     sess.status as session_status,
@@ -53,16 +51,59 @@ SELECT
     c.client_net_address as client_address,
     sess.host_name as host_name,
     r.*
-FROM sys.dm_tran_active_transactions at
-    INNER JOIN sys.dm_tran_session_transactions st ON st.transaction_id = at.transaction_id
-      LEFT OUTER JOIN sys.dm_exec_sessions sess ON st.session_id = sess.session_id
-    LEFT OUTER JOIN sys.dm_exec_connections c
+FROM sys.dm_exec_sessions sess
+    INNER JOIN sys.dm_exec_connections c
         ON sess.session_id = c.session_id
-    LEFT OUTER JOIN sys.dm_exec_requests r
+    INNER JOIN sys.dm_exec_requests r
         ON c.connection_id = r.connection_id
-        CROSS APPLY sys.dm_exec_sql_text(c.most_recent_sql_handle) text
-    WHERE sess.session_id != @@spid
-    ORDER BY at.transaction_begin_time ASC
+    OUTER APPLY sys.dm_exec_sql_text(c.most_recent_sql_handle) text
+WHERE sess.session_id != @@spid
+ORDER BY r.start_time ASC
+""",
+).strip()
+
+# collects activity about open transactions
+TX_ACTIVITY_QUERY = re.sub(
+    r'\s+',
+    ' ',
+    """\
+SELECT
+    CONVERT(
+        NVARCHAR, TODATETIMEOFFSET(CURRENT_TIMESTAMP, DATEPART(TZOFFSET, SYSDATETIMEOFFSET())), 126
+    ) as now,
+    CONVERT(
+        NVARCHAR, TODATETIMEOFFSET(tat.transaction_begin_time, DATEPART(TZOFFSET, SYSDATETIMEOFFSET())), 126
+    ) as transaction_begin_time,
+    tst.transaction_id as tx_id,
+    tst.session_id as id,
+    sess.status as session_status,
+    sess.last_request_start_time,
+    sess.original_login_name as user_name,
+    DB_NAME(sess.database_id) as database_name,
+    tat.transaction_state,
+    CASE
+        WHEN  sess.status  = 'sleeping' THEN TXT.text
+    END as text,
+    sess.host_name,
+    ec.client_tcp_port as client_port,
+    ec.client_net_address as client_address,
+    CASE tat.transaction_type
+        WHEN 1 THEN 'Read/Write Transaction'
+        WHEN 2 THEN 'Read-Only Transaction'
+        WHEN 3 THEN 'System Transaction'
+                WHEN 4 THEN 'Distributed Transaction'
+                ELSE 'Unknown'
+    END as tx_type
+FROM sys.dm_tran_session_transactions as tst
+       INNER JOIN sys.dm_tran_active_transactions as tat
+              ON tst.transaction_id = tat.transaction_id
+       INNER JOIN sys.dm_exec_sessions sess
+              ON tst.session_id = sess.session_id
+         INNER JOIN sys.dm_exec_connections ec
+               ON tst.session_id = ec.session_id
+       CROSS APPLY sys.dm_exec_sql_text(ec.most_recent_sql_handle) TXT
+WHERE sess.session_id != @@spid
+ORDER BY transaction_begin_time ASC
 """,
 ).strip()
 
@@ -98,9 +139,17 @@ class SqlserverActivity(DBMAsyncJob):
         self.check = check
         self.log = check.log
         collection_interval = float(check.activity_config.get('collection_interval', DEFAULT_COLLECTION_INTERVAL))
+        # tx collection interval should not be shorter than the main activity col interval
+        tx_collection_interval = max(
+            check.activity_config.get('tx_collection_interval', DEFAULT_TX_COLLECTION_INTERVAL),
+            collection_interval,
+        )
         if collection_interval <= 0:
             collection_interval = DEFAULT_COLLECTION_INTERVAL
+        if tx_collection_interval <= 0:
+            tx_collection_interval = DEFAULT_TX_COLLECTION_INTERVAL
         self.collection_interval = collection_interval
+        self.tx_collection_interval = tx_collection_interval
         super(SqlserverActivity, self).__init__(
             check,
             run_sync=is_affirmative(check.activity_config.get('run_sync', False)),
@@ -115,6 +164,8 @@ class SqlserverActivity(DBMAsyncJob):
         )
         self._conn_key_prefix = "dbm-activity-"
         self._activity_payload_max_bytes = MAX_PAYLOAD_BYTES
+        # keep track of last time we sent an tx_activity event
+        self._time_since_last_tx_activity_event = 0
 
     def _close_db_conn(self):
         pass
@@ -143,25 +194,77 @@ class SqlserverActivity(DBMAsyncJob):
         rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
         return rows
 
+    @tracked_method(agent_check_getter=agent_check_getter, track_result_length=True)
+    def _get_tx_activity(self, cursor):
+        self._time_since_last_tx_activity_event = time.time()
+        self.log.debug("collecting sql server transaction activity")
+        self.log.debug("Running query [%s]", TX_ACTIVITY_QUERY)
+        cursor.execute(TX_ACTIVITY_QUERY)
+        columns = [i[0] for i in cursor.description]
+        # construct row dicts manually as there's no DictCursor for pyodbc
+        rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        self.check.histogram(
+            "dd.sqlserver.activity.get_tx_activity.tx_rows", len(rows), **self.check.debug_stats_kwargs()
+        )
+        self.log.warning(len(rows))
+        return rows
+
     def _normalize_queries_and_filter_rows(self, rows, max_bytes_limit):
         normalized_rows = []
+        row_key_and_index = {}
         estimated_size = 0
-        for row in rows:
-            try:
-                obfuscated_statement = obfuscate_sql_with_metadata(row['text'], self.check.obfuscator_options)['query']
-                row['query_signature'] = compute_sql_signature(obfuscated_statement)
-            except Exception as e:
-                # obfuscation errors are relatively common so only log them during debugging
-                self.log.debug("Failed to obfuscate query: %s", e)
-                obfuscated_statement = "ERROR: failed to obfuscate"
-            row = self._sanitize_row(row, obfuscated_statement)
+        for index, row in enumerate(rows):
+            row = self._obfuscate_and_sanitize_row(row)
             estimated_size += self._get_estimated_row_size_bytes(row)
             if estimated_size > max_bytes_limit:
-                # query results are ORDER BY transaction_begin_time ASC
+                # query results are ORDER BY query_start ASC
                 # so once we hit the max bytes limit, return
                 return normalized_rows
             normalized_rows.append(row)
-        return normalized_rows
+            row_key = self._get_key_for_row(row)
+            if row_key not in row_key_and_index:
+                row_key_and_index.update({row_key: index})
+        return normalized_rows, row_key_and_index
+
+    def _get_key_for_row(self, row):
+        return "{}:{}".format(row['id'], row['last_request_start_time'])
+
+    def _normalize_tx_rows_and_join(self, tx_rows, active_rows, active_row_key_and_index):
+        updated_rows = active_rows
+        self.log.warning(updated_rows)
+        self.log.warning(active_row_key_and_index)
+        for cur_row in tx_rows:
+            cur_row = self._obfuscate_and_sanitize_row(cur_row)
+            row_key = self._get_key_for_row(cur_row)
+            self.log.warning(row_key)
+            if row_key in active_row_key_and_index:
+                # if the tx has a corresponding entry in exec_requests
+                # then we should merge it with its existing entry in active_rows
+                i = active_row_key_and_index[row_key]
+                old_row = updated_rows[i]
+                old_row.update(cur_row)
+            else:
+                # else just append the tx row to the end to the event
+                updated_rows.append(cur_row)
+        return updated_rows
+
+    def _obfuscate_and_sanitize_row(self, row):
+        row = self._remove_null_vals(row)
+        if 'text' not in row:
+            return row
+        try:
+            obfuscated_statement = obfuscate_sql_with_metadata(row['text'], self.check.obfuscator_options)['query']
+            row['query_signature'] = compute_sql_signature(obfuscated_statement)
+        except Exception as e:
+            # obfuscation errors are relatively common so only log them during debugging
+            self.log.debug("Failed to obfuscate query: %s", e)
+            obfuscated_statement = "ERROR: failed to obfuscate"
+        row = self._sanitize_row(row, obfuscated_statement)
+        return row
+
+    @staticmethod
+    def _remove_null_vals(row):
+        return {key: val for key, val in row.items() if key not in dm_exec_requests_exclude_keys and val is not None}
 
     @staticmethod
     def _sanitize_row(row, obfuscated_statement):
@@ -191,8 +294,13 @@ class SqlserverActivity(DBMAsyncJob):
         }
         return event
 
-    def _truncate_activity_rows(self, rows, max_bytes):
-        pass
+    def _report_tx_activity_event(self):
+        # Only send an event if we are configured to do so, and
+        # don't report more often than the configured collection interval
+        elapsed_s = time.time() - self._time_since_last_tx_activity_event
+        if elapsed_s >= self.tx_collection_interval:
+            return True
+        return False
 
     @tracked_method(agent_check_getter=agent_check_getter)
     def collect_activity(self):
@@ -207,7 +315,11 @@ class SqlserverActivity(DBMAsyncJob):
             with self.check.connection.get_managed_cursor(key_prefix=self._conn_key_prefix) as cursor:
                 connections = self._get_active_connections(cursor)
                 rows = self._get_activity(cursor)
-                normalized_rows = self._normalize_queries_and_filter_rows(rows, MAX_PAYLOAD_BYTES)
+                normalized_rows, row_key_and_index = self._normalize_queries_and_filter_rows(rows, MAX_PAYLOAD_BYTES)
+                if self._report_tx_activity_event():
+                    tx_rows = self._get_tx_activity(cursor)
+                    self.log.warning(tx_rows)
+                    normalized_rows = self._normalize_tx_rows_and_join(tx_rows, normalized_rows, row_key_and_index)
                 event = self._create_activity_event(normalized_rows, connections)
                 payload = json.dumps(event, default=default_json_event_encoding)
                 self._check.database_monitoring_query_activity(payload)
