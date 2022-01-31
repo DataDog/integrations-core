4c1d4c83371a35a69e2631d1e53de4278c658e81
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-01-31 17:00:49 -0500

    Update e2e tests

--- silk/tests/test_e2e.py
+++ silk/tests/test_e2e.py
@@ -15,6 +15,10 @@ def test_e2e(dd_agent_check, instance):
     for metric in METRICS:
         aggregator.assert_metric(metric)
 
+    # blocksize and read/write metrics don't appear in test env since caddy can't mock HTTP query strings
+    for metric in BLOCKSIZE_METRICS + READ_WRITE_METRICS:
+        aggregator.assert_metric(metric, at_least=0)
+
     aggregator.assert_service_check('silk.can_connect', SilkCheck.OK)
     aggregator.assert_service_check('silk.system.state', SilkCheck.OK)
     aggregator.assert_service_check('silk.server.state', SilkCheck.OK, count=2)
