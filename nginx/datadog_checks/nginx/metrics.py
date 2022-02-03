28157ba692a17fde03206bc1acc5ee24a8f9363d
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-02-03 15:27:28 +0100

    Add comment to histogram metrics

--- nginx/datadog_checks/nginx/metrics.py
+++ nginx/datadog_checks/nginx/metrics.py
@@ -112,6 +112,8 @@ METRICS_SEND_AS_COUNT = [
     'nginx.upstream.peers.unavail',
 ]
 
+# NGNINX Plus metrics that are sent as both a histogram and gauge for backwards compatibility
+# The histogram metrics will have _histogram appended to their names
 METRICS_SEND_AS_HISTOGRAM = {'nginx.upstream.peers.response_time', 'nginx.stream.upstream.peers.response_time'}
 
 # NGNINX Plus metrics that are sent as only a count.
