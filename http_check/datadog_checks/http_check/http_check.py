46f6d0385503c47c7050ce1640109fc4251b55f4
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-02-11 09:42:53 -0500

    Trigger CI

--- http_check/datadog_checks/http_check/http_check.py
+++ http_check/datadog_checks/http_check/http_check.py
@@ -92,7 +92,7 @@ class HTTPCheck(AgentCheck):
             self.log.info(loginfo)
             down_msg = self._include_content(include_content, down_msg, content)
             service_checks.append((self.SC_STATUS, AgentCheck.CRITICAL, down_msg))
-
+        # test
         # Store tags in a temporary list so that we don't modify the global tags data structure
         tags_list = list(tags)
         tags_list.append('url:{}'.format(addr))
