3bf881fddad4b94c981d36f56ee51cf652f76d2e
Sarah <sarah.witt@datadoghq.com>
2022-02-10 19:56:38 -0500

    Assert metrics in tests

--- traffic_server/tests/test_traffic_server.py
+++ traffic_server/tests/test_traffic_server.py
@@ -9,12 +9,20 @@ from datadog_checks.base import AgentCheck
 from datadog_checks.base.stubs.aggregator import AggregatorStub
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.traffic_server import TrafficServerCheck
+from datadog_checks.traffic_server.metrics import COUNT_METRICS, GAUGE_METRICS
 
 
-def test_check(aggregator, instance):
-    # type: (AggregatorStub, Dict[str, Any]) -> None
+def test_check(aggregator, instance, dd_run_check):
     check = TrafficServerCheck('traffic_server', {}, [instance])
-    check.check(instance)
+    dd_run_check(check)
+
+    for metric_name in COUNT_METRICS:
+        aggregator.assert_metric(
+            metric_name, count=1, tags=instance.get('tags'), metric_type=AggregatorStub.MONOTONIC_COUNT
+        )
+
+    for metric_name in GAUGE_METRICS:
+        aggregator.assert_metric(metric_name, count=1, tags=instance.get('tags'), metric_type=AggregatorStub.GAUGE)
 
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
