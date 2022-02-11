cce5e07659dd7c2658d11f8905b870202eef902b
Steven Yuen <steven.yuen@datadoghq.com>
2022-02-11 14:33:09 -0500

    logic refactor

--- haproxy/datadog_checks/haproxy/checkv2.py
+++ haproxy/datadog_checks/haproxy/checkv2.py
@@ -9,25 +9,9 @@ from .metrics import METRIC_MAP, construct_metrics_config
 
 
 class HaproxyCheckV2(OpenMetricsBaseCheckV2):
+    __NAMESPACE__ = 'haproxy'
+
     DEFAULT_METRIC_LIMIT = 0
 
-    def __init__(self, name, init_config, instances):
-        super().__init__(name, init_config, instances)
-        self.check_initializations.appendleft(self._parse_config)
-
-    def _parse_config(self):
-        self.scraper_configs = []
-        openmetrics_endpoint = self.instance.get("openmetrics_endpoint")
-        if not openmetrics_endpoint:
-            raise ConfigurationError("Must specify a openmetrics_endpoint")
-        self.scraper_configs.append(self._generate_config(openmetrics_endpoint, METRIC_MAP, "haproxy"))
-
-    def _generate_config(self, endpoint, metrics, namespace):
-        metrics = construct_metrics_config(metrics)
-        config = {
-            'openmetrics_endpoint': endpoint,
-            'metrics': metrics,
-            'namespace': namespace,
-        }
-        config.update(self.instance)
-        return config
+    def get_default_config(self):
+        return {'metrics': construct_metrics_config(METRIC_MAP)}
