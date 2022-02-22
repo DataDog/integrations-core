0e61ba73a76b5fea52dce4b3f88b3a323603c8ef
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-02-22 11:13:10 +0100

    Add missing assertion to e2e test

--- cilium/tests/test_e2e.py
+++ cilium/tests/test_e2e.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
+from datadog_checks.dev.utils import get_metadata_metrics
 
 from .common import AGENT_V2_METRICS, OPTIONAL_METRICS, requires_new_environment
 
@@ -16,3 +17,5 @@ def test_check_ok(dd_agent_check):
             aggregator.assert_metric(metric, at_least=0)
         else:
             aggregator.assert_metric(metric, at_least=1)
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
