cc07bd75fe56fe4b692f13bc533cfa3644645a1e
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-02-28 12:50:44 -0500

    add validations, tests

--- ibm_mq/datadog_checks/ibm_mq/config_models/validators.py
+++ ibm_mq/datadog_checks/ibm_mq/config_models/validators.py
@@ -4,10 +4,44 @@
 
 # Here you can include additional config validators or transformers
 #
-# def initialize_instance(values, **kwargs):
-#     if 'my_option' not in values and 'my_legacy_option' in values:
-#         values['my_option'] = values['my_legacy_option']
-#     if values.get('my_number') > 10:
-#         raise ValueError('my_number max value is 10, got %s' % str(values.get('my_number')))
-#
-#     return values
+def initialize_instance(values, **kwargs):
+    errors = []
+    queues = values.get('queues')
+    if queues is not None:
+        queues_set = set(queues)
+        if len(queues_set) < len(queues):
+            errors.append('`queues` must contain unique values.')
+
+    queue_patterns = values.get('queue_patterns')
+    if queue_patterns is not None:
+        queue_patterns_set = set(queue_patterns)
+        if len(queue_patterns_set) < len(queue_patterns):
+            errors.append('`queue_patterns` must contain unique values.')
+
+    queue_regex = values.get('queue_regex')
+    if queue_regex is not None:
+        queue_regex_set = set(queue_regex)
+        if len(queue_regex_set) < len(queue_regex):
+            errors.append('`queue_regex` must contain unique values.')
+
+    channels = values.get('channels')
+    if channels is not None:
+        channels_set = set(channels)
+        if len(channels_set) < len(channels):
+            errors.append('`channels` must contain unique values.')
+
+    channel_status_mapping = values.get('channel_status_mapping')
+    if channel_status_mapping is not None and len(channel_status_mapping) < 1:
+        errors.append('`channel_status_mapping` must contain at least 1 mapping.')
+
+    queue_tag_re = values.get('queue_tag_re')
+    if queue_tag_re is not None and len(queue_tag_re) < 1:
+        errors.append('`queue_tag_re` must contain at least 1 mapping.')
+
+    if len(errors) > 0:
+        error_msg = 'Found {} configuration errors: '.format(len(errors))
+        for error in errors:
+            error_msg += error + ' '
+        raise ValueError(error_msg)
+
+    return values
