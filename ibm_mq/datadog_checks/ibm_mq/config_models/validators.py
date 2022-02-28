61a49e5653f4dc8e5fff25a6e35f75829eb1ab06
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-02-28 18:15:53 -0500

    refactor validators

--- ibm_mq/datadog_checks/ibm_mq/config_models/validators.py
+++ ibm_mq/datadog_checks/ibm_mq/config_models/validators.py
@@ -2,41 +2,23 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-# Here you can include additional config validators or transformers
-#
+
 def initialize_instance(values, **kwargs):
     errors = []
-    queues = values.get('queues')
-    if queues is not None:
-        queues_set = set(queues)
-        if len(queues_set) < len(queues):
-            errors.append('`queues` must contain unique values.')
 
-    queue_patterns = values.get('queue_patterns')
-    if queue_patterns is not None:
-        queue_patterns_set = set(queue_patterns)
-        if len(queue_patterns_set) < len(queue_patterns):
-            errors.append('`queue_patterns` must contain unique values.')
+    unique_items_options = ['queues', 'queue_patterns', 'queue_regex', 'channels']
+    for unique_opt in unique_items_options:
+        value = values.get(unique_opt)
+        if value is not None:
+            value_set = set(value)
+            if len(value_set) < len(value):
+                errors.append('`{}` must contain unique values.'.format(unique_opt))
 
-    queue_regex = values.get('queue_regex')
-    if queue_regex is not None:
-        queue_regex_set = set(queue_regex)
-        if len(queue_regex_set) < len(queue_regex):
-            errors.append('`queue_regex` must contain unique values.')
-
-    channels = values.get('channels')
-    if channels is not None:
-        channels_set = set(channels)
-        if len(channels_set) < len(channels):
-            errors.append('`channels` must contain unique values.')
-
-    channel_status_mapping = values.get('channel_status_mapping')
-    if channel_status_mapping is not None and len(channel_status_mapping) < 1:
-        errors.append('`channel_status_mapping` must contain at least 1 mapping.')
-
-    queue_tag_re = values.get('queue_tag_re')
-    if queue_tag_re is not None and len(queue_tag_re) < 1:
-        errors.append('`queue_tag_re` must contain at least 1 mapping.')
+    min_props_options = ['channel_status_mapping', 'queue_tag_re']
+    for min_prop_opt in min_props_options:
+        value = values.get(min_prop_opt)
+        if value is not None and len(value) < 1:
+            errors.append('`{}` must contain at least 1 mapping.'.format(min_prop_opt))
 
     if len(errors) > 0:
         error_msg = 'Found {} configuration errors: '.format(len(errors))
