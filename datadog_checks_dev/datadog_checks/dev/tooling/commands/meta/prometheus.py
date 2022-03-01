1e8314196e77782f415cf882ebe7a29e385996e9
vatteh <victor.atteh@datadoghq.com>
2022-03-01 11:28:14 -0500

    update generators in prometheus.py and gen_clusterquota_metadata.py

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/prometheus.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/meta/prometheus.py
@@ -16,7 +16,7 @@ from ..console import CONTEXT_SETTINGS, abort, echo_info, echo_success, echo_wai
 METRIC_SEPARATORS = ('.', '_')
 TYPE_MAP = {'gauge': 'gauge', 'counter': 'count', 'rate': 'gauge', 'histogram': 'gauge', 'summary': 'gauge'}
 METADATA_CSV_HEADER = (
-    'metric_name,metric_type,interval,unit_name,per_unit_name,description,orientation,integration,short_name'
+    'metric_name,metric_type,interval,unit_name,per_unit_name,description,orientation,integration,short_name,curated_metric'
 )
 
 
