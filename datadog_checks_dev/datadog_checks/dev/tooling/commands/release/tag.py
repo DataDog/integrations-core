a76a164607e43eb4f1177aacc28db4686eaafbd8
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-03-02 17:47:15 -0500

    Add flag

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/release/tag.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/release/tag.py
@@ -8,13 +8,16 @@ from ...release import get_release_tag_string
 from ...utils import complete_valid_checks, get_valid_checks, get_version_string
 from ..console import CONTEXT_SETTINGS, abort, echo_info, echo_success, echo_waiting, echo_warning
 
+# This should be the initial version generated from the integration template
+PRERELEASE = '0.0.1'
 
 @click.command(context_settings=CONTEXT_SETTINGS, short_help='Tag the git repo with the current release of a check')
 @click.argument('check', autocompletion=complete_valid_checks)
 @click.argument('version', required=False)
 @click.option('--push/--no-push', default=True)
 @click.option('--dry-run', '-n', is_flag=True)
-def tag(check, version, push, dry_run):
+@click.option('--skip-prerelease', is_flag=True)
+def tag(check, version, push, dry_run, skip_prerelease):
     """Tag the HEAD of the git repo with the current release number for a
     specific check. The tag is pushed to origin by default.
 
@@ -48,8 +51,8 @@ def tag(check, version, push, dry_run):
         if not version:
             version = get_version_string(check)
 
-        if version == '0.0.1':
-            echo_warning('skipping since not released yet')
+        if skip_prerelease and version == PRERELEASE:
+            echo_warning('skipping prerelease version')
             continue
 
         # get the tag name
