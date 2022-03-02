9c8ffbd13950775c8c0e2a295e93f85fd9025201
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-03-02 17:49:09 -0500

    Update comment

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/release/tag.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/release/tag.py
@@ -8,7 +8,8 @@ from ...release import get_release_tag_string
 from ...utils import complete_valid_checks, get_valid_checks, get_version_string
 from ..console import CONTEXT_SETTINGS, abort, echo_info, echo_success, echo_waiting, echo_warning
 
-# This should be the initial version generated from the integration template
+# 0.0.1 is the initial pre-release version that is generated from the integrations template.
+# Updating to any version > 0.0.1 would require a tag.*.link file to be updated.
 PRERELEASE = '0.0.1'
 
 @click.command(context_settings=CONTEXT_SETTINGS, short_help='Tag the git repo with the current release of a check')
