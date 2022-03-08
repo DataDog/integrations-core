369df1bdb9368fc06dbf10db5aa6056543cbb3a3
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-03-08 18:20:50 -0500

    add support for non-odbc connection

--- teradata/datadog_checks/teradata/check.py
+++ teradata/datadog_checks/teradata/check.py
@@ -5,8 +5,10 @@ from contextlib import closing, contextmanager
 from typing import Any
 
 import pyodbc
+import teradatasql
+import json
 
-from datadog_checks.base import AgentCheck
+from datadog_checks.base import AgentCheck, ConfigurationError
 from datadog_checks.base.utils.db import QueryManager
 
 from .common import SERVICE_CHECK_NAME
@@ -27,6 +29,7 @@ class TeradataCheck(AgentCheck):
         self._connection = None
         self._query_manager = QueryManager(self, self.execute_query_raw, queries=DEFAULT_QUERIES)
         self.check_initializations.append(self._query_manager.compile_queries)
+        self._service_check_tags = ['teradata_host:{}'.format(self.config.host)] + self.config.tags
 
     def check(self, _):
         # type: (Any) -> None
@@ -51,51 +54,155 @@ class TeradataCheck(AgentCheck):
 
     @contextmanager
     def connect(self):
-        conn = None
-        connection_string = self._gen_conn_string()
+        try:
+            if self.config.use_odbc:
+                return self.connect_odbc()
+            else:
+                return self.connect_non_odbc()
+        except Exception as e:
+            self.service_check(SERVICE_CHECK_NAME, AgentCheck.CRITICAL, msg=e, tags=self._service_check_tags)
+            self.log.error('Failed to connect to Teradata database. %s', e)
+            raise
 
+    def connect_odbc(self):
+        conn = None
+        connection_string = self._gen_odbc_conn_string()
+        self.log.debug('Connecting to Teradata via ODBC with connection_string: %s.', connection_string)
         try:
             conn = pyodbc.connect(connection_string, timeout=self.config.timeout, autocommit=True)
-            self.service_check(SERVICE_CHECK_NAME, AgentCheck.OK)
-            self.log.debug('Connected to Teradata')
+            self.service_check(SERVICE_CHECK_NAME, AgentCheck.OK, tags=self._service_check_tags)
+            self.log.debug('Connected to Teradata.')
             yield conn
         except Exception as e:
             self.service_check(SERVICE_CHECK_NAME, AgentCheck.CRITICAL)
-            self.log.exception('Unable to connect. Error is %s', e)
+            self.log.exception('Unable to connect to Teradata database. %e', e)
             raise
         finally:
             if conn:
                 conn.close()
 
-    def _gen_conn_string(self):
+    def connect_non_odbc(self):
+        conn = None
+        self.log.debug('Connecting to Teradata via teradatasql driver...')
+        conn_params = {
+            'host': self.config.host,
+            'user': self.config.username,
+            'password': self.config.password,
+            'account': self.config.account,
+            'database': self.config.db,
+            'dbs_port': str(self.config.port),
+            'https_port': str(self.config.https_port),
+            'sslca': self.config.ssl_ca,
+            'sslcapath': self.config.ssl_ca_path,
+            'sslmode': self.config.ssl_mode,
+            'sslprotocol': self.config.ssl_protocol
+        }
+        if self.config.partition and self.config.partition is not None:
+            conn_params.update({'partition': self.config.partition})
+        try:
+            conn = teradatasql.connect(json.dumps(conn_params))
+            self.service_check(SERVICE_CHECK_NAME, AgentCheck.OK, tags=self._service_check_tags)
+            self.log.debug('Connected to Teradata.')
+            yield conn
+        except Exception as e:
+            self.log.exception('Unable to connect via teradatasql driver. %s.', e)
+            raise
+        finally:
+            if conn:
+                conn.close()
+
+    def _gen_odbc_conn_string(self):
         conn_str = 'RECONNECTCOUNT=2;CHARSET=UTF8;USEREGIONALSETTINGS=N;'
-        if self.config.connection_string:
-            conn_str += self.config.connection_string
+        try:
+            normalized_cs = self._validate_custom_connection_options()
+
+            if self.config.connection_string:
+                conn_str += normalized_cs
+            if self.config.dsn:
+                conn_str += 'DSN={};'.format(self.config.dsn)
+            if self.config.account:
+                conn_str += 'ACCOUNTSTR={};'.format(self.config.account)
+            if self.config.host:
+                conn_str += 'DBCNAME={};'.format(self.config.host)
+            if self.config.db:
+                conn_str += 'DATABASE={};'.format(self.config.db)
+            if self.config.driver:
+                conn_str += 'DRIVER={};'.format(self.config.driver)
+            if self.config.username:
+                conn_str += 'USERNAME={};'.format(self.config.username)
+            if self.config.password:
+                conn_str += 'PASSWORD={};'.format(self.config.password)
+            if self.config.use_tls and self.config.https_port:
+                conn_str += 'HTTPS_PORT={};'.format(self.config.https_port)
+            if self.config.ssl_mode:
+                conn_str += 'SSLMODE={}'.format(self.config.ssl_mode)
+            if self.config.ssl_ca:
+                conn_str += 'SSLCA={};'.format(self.config.ssl_ca)
+            if self.config.ssl_ca_path:
+                conn_str += 'SSLCAPATH={}'.format(self.config.ssl_ca_path)
+            if self.config.mechanism_key:
+                conn_str += 'MECHANISMKEY={};'.format(self.config.mechanism_key)
+            if self.config.mechanism_name:
+                conn_str += 'MECHANISMNAME={};'.format(self.config.mechanism_name)
+
             return conn_str
-        if self.config.dsn:
-            conn_str += 'DSN={};'.format(self.config.dsn)
-        if self.config.account:
-            conn_str += 'ACCOUNTSTR={};'.format(self.config.account)
-        if self.config.dbc_name:
-            conn_str += 'DBCNAME={};'.format(self.config.dbc_name)
-        if self.config.db:
-            conn_str += 'DATABASE={};'.format(self.config.db)
-        if self.config.driver:
-            conn_str += 'DRIVER={};'.format(self.config.driver)
-        if self.config.username:
-            conn_str += 'USERNAME={};'.format(self.config.username)
-        if self.config.password:
-            conn_str += 'PASSWORD={};'.format(self.config.password)
-        if self.config.use_tls and self.config.https_port:
-            conn_str += 'HTTPS_PORT={};'.format(self.config.https_port)
-        if self.config.ssl_mode:
-            conn_str += 'SSLMode={}'.format(self.config.ssl_mode)
-        if self.config.ssl_ca:
-            conn_str += 'SSLCA={};'.format(self.config.ssl_ca)
-        if self.config.ssl_ca_path:
-            conn_str += 'SSLCAPath={}'.format(self.config.ssl_ca_path)
-        if self.config.mechanism_key:
-            conn_str += 'MECHANISMKEY={};'.format(self.config.mechanism_key)
-        if self.config.mechanism_name:
-            conn_str += 'MECHANISMNAME={};'.format(self.config.mechanism_name)
-        return conn_str
+
+        except Exception:
+            self.log.error('Error occurred while constructing connection string. Check configuration options.')
+            raise
+
+    def _validate_custom_connection_options(self):
+        normalized_cs = self._normalize_conn_str(self.config.connection_string)
+
+        odbc_options = {
+            'dsn': ['DSN'],
+            'account': ['ACCOUNTSTR', 'ACCOUNT'],
+            'dbc_name': ['DBCNAME'],
+            'db': ['DEFAULTDATABASE', 'DATABASE'],
+            'driver': ['DRIVER'],
+            'username': ['USERNAME', 'UID'],
+            'password': ['PASSWORD', 'PWD'],
+            'https_port': ['HTTPS_PORT'],
+            'ssl_mode': ['SSLMODE'],
+            'ssl_protocol': ['SSLPROTOCOL'],
+            'ssl_ca': ['SSLCA'],
+            'ssl_ca_path': ['SSLCAPATH'],
+            'mechanism_key': ['MECHANISMKEY', 'AUTHENTICATIONPARAMETER'],
+            'mechanism_name': ['MECHANISMNAME', 'AUTHENTICATION'],
+        }
+
+        required_options = ['dbc_name', 'driver', 'username', 'password']
+
+        for option, values in odbc_options.items():
+            for value in values:
+                if option in required_options and self.config.get(option) is None:
+                    raise ConfigurationError("Configuration option %s is required.")
+                if value in normalized_cs and self.config.get(option) is not None:
+                    raise ConfigurationError(
+                        "%s has been provided both in the connection string and as a configuration"
+                        " option (%s), please specify it only once." % (value, option)
+                    )
+
+        return normalized_cs
+
+    def _normalize_conn_str(self, conn_str):
+        default_options = {
+            'RECONNECTCOUNT': '2',
+            'CHARSET': 'UTF8',
+            'USEREGIONALSETTINGS': 'N'
+        }
+
+        raw_cs = conn_str.split(';')
+
+        for index, config in enumerate(raw_cs):
+            raw_cs[index] = config.split('=')
+            option = raw_cs[index][0]
+            if option.upper() in default_options:
+                self.log.debug("Connection string option %s is ignored. Default setting is %s.", option, default_options[option])
+                raw_cs.remove(raw_cs[index])
+            else:
+                raw_cs[index][0] = option.upper()
+                raw_cs[index] = '='.join(raw_cs[index])
+
+        normalized_cs = ';'.join(raw_cs)
+        return normalized_cs
--- teradata/datadog_checks/teradata/config.py
+++ teradata/datadog_checks/teradata/config.py
@@ -5,26 +5,34 @@ from typing import Any, Dict
 
 from datadog_checks.base import is_affirmative
 
-from .common import DEFAULT_COMMAND_TIMEOUT
+from .common import DEFAULT_COMMAND_TIMEOUT, DEFAULT_CONNECT_FAILURE_TTL
 
 
 class TeradataConfig(object):
     def __init__(self, instance):
         # type: (Dict[str, Any]) -> None
+        self.use_odbc = is_affirmative(instance.get('use_odbc', True))
         self.dsn = str(instance.get('dsn', ''))
         self.account = str(instance.get('account', ''))
-        self.dbc_name = str(instance.get('dbc_name', ''))
+        self.host = str(instance.get('host', ''))
+        self.port = int(instance.get('port', 1025))
         self.db = str(instance.get('database', ''))
+        self.partition = str(instance.get('partition', ''))
         self.driver = str(instance.get('driver', ''))
         self.connection_string = str(instance.get('connection_string', ''))
         self.username = str(instance.get('username', ''))
         self.password = str(instance.get('password', ''))
         self.use_tls = is_affirmative(instance.get('use_tls', False))
         self.https_port = int(instance.get('https_port', 443))
-        self.ssl_mode = str(instance.get('ssl_mode', 'Prefer'))
+        self.ssl_mode = str(instance.get('ssl_mode', 'PREFER'))
+        self.ssl_protocol = str(instance.get('ssl_protocol', 'TLSv1.2'))
         self.ssl_ca = str(instance.get('ssl_ca', ''))
         self.ssl_ca_path = str(instance.get('ssl_ca_path', ''))
         self.mechanism_key = str(instance.get('mechanism_key', ''))
         self.mechanism_name = str(instance.get('mechanism_name', ''))
         self.tags = instance.get('tags', [])
         self.timeout = int(instance.get('command_timeout', DEFAULT_COMMAND_TIMEOUT))
+        self.connect_failure_ttl = int(instance.get('connect_failure_ttl', DEFAULT_CONNECT_FAILURE_TTL))
+
+    def get(self, option):
+        return option
