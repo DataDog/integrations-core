83063a27527a47827fb418ca9367f5cffbdffa9b
Sarah <sarah.witt@datadoghq.com>
2022-03-08 13:56:55 -0500

    Sync config, update tests

--- traffic_server/tests/test_unit.py
+++ traffic_server/tests/test_unit.py
@@ -109,7 +109,7 @@ def test_submit_metadata_invalid(caplog, instance, server_version, build_version
 
     check._submit_version_metadata(server_version, build_version)
     if logline is None:
-        assert caplog.text == ""
+        assert "traffic_server" not in caplog.text
     else:
         assert logline in caplog.text
 
