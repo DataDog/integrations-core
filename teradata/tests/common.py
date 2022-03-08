18168f3dada13ed45e59e5f812eb9cf2dc9cb5f8
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-03-07 21:34:58 -0500

    add e2e setup

---
+++ teradata/tests/common.py
@@ -0,0 +1,16 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+import os
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+E2E_METADATA = {
+    'docker_volumes': [
+        '{}/setup/odbc/odbc.ini:/opt/datadog-agent/embedded/etc/odbc.ini'.format(HERE),
+        '{}/setup/odbc/odbcinst.ini:/opt/datadog-agent/embedded/etc/odbcinst.ini'.format(HERE),
+        '{}/setup/start_commands.sh:/tmp/start_commands.sh'.format(HERE),
+    ],
+    'start_commands': ['bash /tmp/start_commands.sh'],
+}
--- teradata/tests/conftest.py
+++ teradata/tests/conftest.py
@@ -1,16 +1,26 @@
 # (C) Datadog, Inc. 2022-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import os
 from copy import deepcopy
 
 import pytest
 
-CONFIG = {'host': 'localhost', 'user': 'user', 'password': 'password'}
+from .common import E2E_METADATA
+
+DBCNAME = os.environ.get('DBCNAME')
+
+CONFIG = {
+    'host': 'localhost',
+    'connection_string': 'DRIVER=Teradata Database ODBC Driver 17.10;DBCName={};UID=dbc;PWD=datad0g123;'.format(
+        DBCNAME
+    ),
+}
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    yield CONFIG
+    yield CONFIG, E2E_METADATA
 
 
 @pytest.fixture
