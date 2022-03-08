3864f15b316f2e5b9ba47d6b4307492f6848c779
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-03-07 21:36:47 -0500

    add addl odbc configs

---
+++ teradata/datadog_checks/teradata/common.py
@@ -0,0 +1,5 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+DEFAULT_COMMAND_TIMEOUT = 5
+SERVICE_CHECK_NAME = "teradata.can_connect"
