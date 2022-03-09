7ad5ba049b7f0c123e206d5ac97f4eb5a16ff91a
Alex Barksdale <me@alexbarksdale.com>
2022-03-09 15:18:17 -0500

    Include metadata in FQT

--- postgres/tests/test_statements.py
+++ postgres/tests/test_statements.py
@@ -570,7 +570,7 @@ def test_statement_metadata(
 
     normalized_query = 'SELECT city FROM persons WHERE city = ?'
     # Metrics will match to the normalized query signature
-    normalized_query_signature = 'ca85e8d659051b3a'
+    normalized_query_signature = '92a069a551c280e7'
 
     def obfuscate_sql(query, options=None):
         if query.startswith('SELECT city FROM persons WHERE city'):
@@ -601,6 +601,14 @@ def test_statement_metadata(
     assert sample['db']['metadata']['tables'] == expected_metadata_payload['tables']
     assert sample['db']['metadata']['commands'] == expected_metadata_payload['commands']
     assert sample['db']['metadata']['comments'] == expected_metadata_payload['comments']
+
+    fqt_samples = [
+        s for s in samples if s.get('dbm_type') == 'fqt' and s['db']['query_signature'] == normalized_query_signature
+    ]
+    assert len(fqt_samples) == 1
+    fqt = fqt_samples[0]
+    assert fqt['db']['metadata']['tables'] == expected_metadata_payload['tables']
+    assert fqt['db']['metadata']['commands'] == expected_metadata_payload['commands']
 
     if POSTGRES_VERSION.split('.')[0] == "9" and pg_stat_statements_view == "pg_stat_statements":
         # cannot catch any queries from other users
