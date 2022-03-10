339928f7a2bb47d36b525c1b612d32e51c1ff7ba
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-03-10 15:13:32 -0500

    Convert private key password to string when registering secret

--- snowflake/datadog_checks/snowflake/check.py
+++ snowflake/datadog_checks/snowflake/check.py
@@ -53,7 +53,9 @@ class SnowflakeCheck(AgentCheck):
             self.register_secret(self._config.password)
 
         if self._config.private_key_password:
-            self.register_secret(self._config.private_key_password)
+            # private_key_password is of type bytes so it can be loaded for the pem private key
+            # but this needs to converted to string to be registered as a secret for filtering
+            self.register_secret(str(self._config.private_key_password))
 
         if self._config.role == 'ACCOUNTADMIN':
             self.log.info(
