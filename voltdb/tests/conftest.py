9d7c21f51b8ebd5736460281cc83aa8552bb8536
Ofek Lev <ofekmeister@gmail.com>
2022-03-15 15:30:17 -0400

    ok now, remove sleep

--- voltdb/tests/conftest.py
+++ voltdb/tests/conftest.py
@@ -46,7 +46,7 @@ def dd_environment(instance):
     else:
         e2e_metadata = {}
 
-    with docker_run(compose_file, conditions=conditions, env_vars=env_vars, mount_logs=True, sleep=60):
+    with docker_run(compose_file, conditions=conditions, env_vars=env_vars, mount_logs=True):
         yield instance, e2e_metadata
 
 
