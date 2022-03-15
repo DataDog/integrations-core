28c3c323f677fc37a47a7b9868da82477922048f
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2022-03-15 15:54:30 -0400

    allow whitespace

--- sqlserver/datadog_checks/sqlserver/connection.py
+++ sqlserver/datadog_checks/sqlserver/connection.py
@@ -28,7 +28,9 @@ class SQLConnectionError(Exception):
     pass
 
 
-CONNECTION_STRING_SPECIAL_CHARACTERS = set('=;[]{}"\' ')
+# we're not including space in the set of invalid characters because it's a valid key character for adodbapi
+# best be more permissive here so that the driver and the database have the final say as to whether it's valid or not
+CONNECTION_STRING_SPECIAL_CHARACTERS = set('=;[]{}"\'')
 
 
 def parse_connection_string_properties(cs):
