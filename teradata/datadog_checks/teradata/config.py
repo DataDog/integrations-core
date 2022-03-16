e2438a4b6c156bf1344ca2a2e21fb290971e832f
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-03-16 18:43:37 -0400

    update config spec

--- teradata/datadog_checks/teradata/config.py
+++ teradata/datadog_checks/teradata/config.py
@@ -7,14 +7,14 @@ from datadog_checks.base import is_affirmative
 class TeradataConfig(object):
     def __init__(self, instance):
         self.server = instance.get('server')
-        self.port = instance.get('port')
+        self.port = int(instance.get('port', 1025))
         self.account = instance.get('account')
         self.username = instance.get('username')
         self.password = instance.get('password')
         self.db = instance.get('database')
         self.collect_res_usage = is_affirmative(instance.get('collect_res_usage'))
         self.jdbc_driver_path = instance.get('jdbc_driver_path')
-        self.use_tls = is_affirmative(instance.get('use_tls', False))
+        self.use_tls = is_affirmative(instance.get('use_tls', True))
         self.https_port = int(instance.get('https_port', 443))
         self.ssl_mode = instance.get('ssl_mode', 'PREFER')
         self.ssl_protocol = instance.get('ssl_protocol', 'TLSv1.2')
