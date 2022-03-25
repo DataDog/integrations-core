47cdc07e2582b8539cf1b612c8199a8c21b2539b
Ofek Lev <ofekmeister@gmail.com>
2022-03-24 20:45:10 -0400

    all ok

--- datadog_checks_dev/datadog_checks/dev/tooling/e2e/docker.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/e2e/docker.py
@@ -270,13 +270,13 @@ class DockerInterface(object):
     def update_check(self):
         command = ['docker', 'exec', self.container_name]
         command.extend(get_pip_exe(self.python_version, platform=self.container_platform))
-        command.extend(('install', 'datadog_checks_base[deps]'))
+        command.extend(('install', '-e', f'{self.check_mount_dir}[deps]'))
         run_command(command, capture=True, check=True)
 
     def update_base_package(self):
         command = ['docker', 'exec', self.container_name]
         command.extend(get_pip_exe(self.python_version, platform=self.container_platform))
-        command.extend(('install', 'datadog_checks_base[db,deps,http,json,kube]'))
+        command.extend(('install', '-e', f'{self.base_mount_dir}[db,deps,http,json,kube]'))
         run_command(command, capture=True, check=True)
 
     def update_agent(self):
