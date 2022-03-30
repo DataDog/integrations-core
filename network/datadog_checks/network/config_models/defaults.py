bbe4a5dcdae31dc45b90accc5bb49fe66081e97f
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2022-03-30 10:18:00 +0200

    Update configuration and spec with new parameter

--- network/datadog_checks/network/config_models/defaults.py
+++ network/datadog_checks/network/config_models/defaults.py
@@ -34,6 +34,10 @@ def instance_collect_count_metrics(field, value):
     return False
 
 
+def instance_collect_ethtool_metrics(field, value):
+    return False
+
+
 def instance_collect_rate_metrics(field, value):
     return True
 
