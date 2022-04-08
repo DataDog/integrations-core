52dac3a7ecdafa49f4ca2f918596d0f277fac27c
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-04-08 12:17:15 +0200

    Make test a unit test

--- oracle/tests/test_e2e.py
+++ oracle/tests/test_e2e.py
@@ -55,9 +55,3 @@ def test_check(dd_agent_check):
     for service_check in SERVICE_CHECKS:
         aggregator.assert_service_check(service_check)
     aggregator.assert_all_metrics_covered()
-
-
-@pytest.mark.e2e
-def test_bad_service_check(dd_agent_check, bad_instance):
-    aggregator = dd_agent_check(bad_instance)
-    aggregator.assert_service_check("oracle.can_connect", Oracle.CRITICAL)
