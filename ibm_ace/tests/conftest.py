0b1af78d00a952e798b0f81c095014f2c3abbfa7
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-04-13 13:33:47 -0400

    add tests readme and revert e2e metric collection

--- ibm_ace/tests/conftest.py
+++ ibm_ace/tests/conftest.py
@@ -11,7 +11,7 @@ from . import common
 
 
 @pytest.fixture(scope='session')
-def dd_environment(instance):
+def dd_environment(instance_no_subscriptions):
     with docker_run(
         common.COMPOSE_FILE,
         build=True,
@@ -21,7 +21,7 @@ def dd_environment(instance):
             'ACE_SERVER_NAME': common.ACE_SERVER_NAME,
         },
     ):
-        yield instance, common.E2E_METADATA
+        yield instance_no_subscriptions, common.E2E_METADATA
 
 
 @pytest.fixture(scope='session')
