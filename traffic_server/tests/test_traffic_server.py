c0845a4abcf64c87abbed8b13c7b479843b659db
Sarah Witt <sarah.witt@datadoghq.com>
2022-04-20 15:44:33 -0400

    Add traffic server url tag (#11862)

--- traffic_server/tests/test_traffic_server.py
+++ traffic_server/tests/test_traffic_server.py
@@ -29,7 +29,7 @@ def test_check(aggregator, instance, dd_run_check):
         )
         for tag in traffic_server_tags:
             aggregator.assert_metric_has_tag(metric_name, tag)
-    aggregator.assert_service_check('traffic_server.can_connect', TrafficServerCheck.OK)
+    aggregator.assert_service_check('traffic_server.can_connect', TrafficServerCheck.OK, tags=traffic_server_tags)
     aggregator.assert_all_metrics_covered()
 
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
@@ -39,13 +39,14 @@ def test_check_cant_reach_url(aggregator, instance_bad_url, dd_run_check):
     # type: (AggregatorStub, Callable[[AgentCheck, bool], None], Dict[str, Any]) -> None
 
     check = TrafficServerCheck('traffic_server', {}, [instance_bad_url])
+    traffic_server_tags = instance_bad_url.get('tags')
 
     with pytest.raises(
         Exception, match='404 Client Error: Not Found on Accelerator for url: http://localhost:8080/_statss'
     ):
         dd_run_check(check)
 
-    aggregator.assert_service_check('traffic_server.can_connect', TrafficServerCheck.CRITICAL)
+    aggregator.assert_service_check('traffic_server.can_connect', TrafficServerCheck.CRITICAL, tags=traffic_server_tags)
     aggregator.assert_all_metrics_covered()
 
 
