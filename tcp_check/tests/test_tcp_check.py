1ab87920458573b18c714ba6c76f322e4a791361
Lénaïc Huard <lenaic.huard@datadoghq.com>
2022-04-27 16:46:27 +0200

    Fix test on Windows

--- tcp_check/tests/test_tcp_check.py
+++ tcp_check/tests/test_tcp_check.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 
+import platform
 import re
 import socket
 from copy import deepcopy
@@ -177,6 +178,7 @@ def test_ipv6(aggregator, check):
             aggregator.assert_service_check('tcp.can_connect', status=check.OK, tags=expected_tags)
             aggregator.assert_metric('network.tcp.can_connect', value=1, tags=expected_tags)
     assert nb_ipv4 == 8
-    assert nb_ipv6 == 8
+    # The Windows CI machine doesn't return IPv6
+    assert nb_ipv6 == 8 or platform.system() == 'Windows' and nb_ipv6 == 0
     aggregator.assert_all_metrics_covered()
-    assert len(aggregator.service_checks('tcp.can_connect')) == 16
+    assert len(aggregator.service_checks('tcp.can_connect')) == nb_ipv4 + nb_ipv6
