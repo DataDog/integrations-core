d1fedf800cc85faa4914b6120f766232f1f0ad35
npezzotti <nathan.pezzotti@datadoghq.com>
2022-05-03 21:46:02 -0400

    assert service check in e2e test, assert error msg in test_metrics_warning

--- aerospike/tests/test_aerospike.py
+++ aerospike/tests/test_aerospike.py
@@ -7,6 +7,7 @@ import mock
 import pytest
 
 from datadog_checks.aerospike import AerospikeCheck
+from datadog_checks.base import AgentCheck
 from datadog_checks.dev.testing import requires_py3
 from datadog_checks.dev.utils import get_metadata_metrics
 
@@ -76,12 +77,17 @@ def test_openmetrics_e2e(dd_agent_check, instance_openmetrics_v2):
 
     aggregator = dd_agent_check(instance_openmetrics_v2, rate=True)
 
+    tags = "endpoint:" + instance_openmetrics_v2.get('openmetrics_endpoint')
+    tags = instance_openmetrics_v2.get('tags').append(tags)
+
+    aggregator.assert_service_check('aerospike.openmetrics.health', AgentCheck.OK, tags=tags)
+
     for metric in EXPECTED_PROMETHEUS_METRICS:
-        aggregator.assert_metric(metric)
+        aggregator.assert_metric(metric, tags=tags)
 
     if version_parts >= [5, 6]:
         for metric in EXPECTED_PROMETHEUS_METRICS_5_6:
-            aggregator.assert_metric(metric)
+            aggregator.assert_metric(metric, tags=tags)
 
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
@@ -93,7 +99,7 @@ def test_metrics_warning(dd_run_check, instance_openmetrics_v2):
     instance_openmetrics_v2['metrics'] = ['migrate_rx_objs', 'migrate_tx_objs']
     check = AerospikeCheck('aerospike', {}, [instance_openmetrics_v2])
 
-    with pytest.raises(Exception):
+    with pytest.raises(Exception, match="Do not use 'metrics' parameter with 'openmetrics_endpoint'"):
         dd_run_check(check)
 
 
