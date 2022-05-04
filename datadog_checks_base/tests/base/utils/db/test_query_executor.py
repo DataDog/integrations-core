e51f3667e2e8f782dc7992b1b59ee041d7cb0b82
Justin Iso <justin.james.iso@gmail.com>
2022-05-03 21:09:24 -0400

    add test for running many queries

--- datadog_checks_base/tests/base/utils/db/test_query_executor.py
+++ datadog_checks_base/tests/base/utils/db/test_query_executor.py
@@ -11,71 +11,88 @@ from .common import mock_executor
 pytestmark = pytest.mark.db
 
 
-def create_query_executor(*args, **kwargs):
-    executor = kwargs.pop('executor', None)
-    if executor is None:
-        executor = mock_executor()
+class TestQueryExecutor:
+    def test_multiple_query_executor_simple(self, aggregator):
+        """Tests running multiple query executors with the same AgentCheck"""
+        check = AgentCheck('test', {}, [{}])
+        qe1 = QueryExecutor(
+            mock_executor([[1, 2, "hello"]]),
+            check,
+            [
+                {
+                    'name': 'query1',
+                    'query': 'select 1, 2, "hello"',
+                    'columns': [
+                        {'name': 'test.column.1', 'type': 'gauge'},
+                        {'name': 'test.column.2', 'type': 'gauge'},
+                        {'name': 'column_3', 'type': 'tag'},
+                    ],
+                }
+            ],
+            hostname='agent1',
+        )
+        qe2 = QueryExecutor(
+            mock_executor([["2015-04-01", 1500]]),
+            check,
+            [
+                {
+                    'name': 'query1',
+                    'query': 'select date(), column from table',
+                    'columns': [{'name': 'date', 'type': 'tag'}, {'name': 'test.table.column', 'type': 'count'}],
+                }
+            ],
+            hostname='agent2',
+        )
+        qe1.compile_queries()
+        qe2.compile_queries()
+        qe1.execute()
+        qe2.execute()
 
-    check = kwargs.pop("check")
+        aggregator.assert_metric(
+            'test.column.1',
+            1,
+            metric_type=aggregator.GAUGE,
+            tags=['column_3:hello'],
+            hostname='agent1',
+        )
 
-    return QueryExecutor(check, executor, args, **kwargs)
+        aggregator.assert_metric(
+            'test.column.2',
+            2,
+            metric_type=aggregator.GAUGE,
+            tags=['column_3:hello'],
+            hostname='agent1',
+        )
 
+        aggregator.assert_metric(
+            'test.table.column',
+            1500,
+            metric_type=aggregator.COUNT,
+            tags=['date:2015-04-01'],
+            hostname='agent2',
+        )
 
-def test_multiple_query_executor_simple(aggregator):
-    check = AgentCheck('test', {}, [{}])
-    qe1 = QueryExecutor(
-        mock_executor([[1, 2, "hello"]]),
-        check,
-        [
-            {
-                'name': 'query1',
-                'query': 'select 1, 2, "hello"',
-                'columns': [
-                    {'name': 'test.column.1', 'type': 'gauge'},
-                    {'name': 'test.column.2', 'type': 'gauge'},
-                    {'name': 'column_3', 'type': 'tag'},
-                ],
-            }
-        ],
-    )
-    qe2 = QueryExecutor(
-        mock_executor([["2015-04-01", 1500]]),
-        check,
-        [
-            {
-                'name': 'query1',
-                'query': 'select date(), column from table',
-                'columns': [{'name': 'date', 'type': 'tag'}, {'name': 'test.table.column', 'type': 'count'}],
-            }
-        ],
-    )
+    def test_many_queries(self, aggregator):
+        """Test running many unique queries"""
+        num_queries = 99
+        queries = []
+        rows = []
+        tags = ['duplicate_tag'] * 80
 
-    qe1.compile_queries()
-    qe2.compile_queries()
+        for i in range(num_queries):
+            queries.append(
+                {
+                    'name': 'query{}'.format(i),
+                    'query': 'select 1',
+                    'columns': [{'name': 'test.metric.{}'.format(i), 'type': 'gauge'}],
+                }
+            )
+            rows.append([i])
 
-    qe1.execute()
-    qe2.execute()
+        check = AgentCheck('test', {}, [{}])
+        qe = QueryExecutor(mock_executor(rows), check, queries, tags=tags)
+        qe.compile_queries()
+        qe.execute()
 
-    aggregator.assert_metric(
-        'test.column.1',
-        1,
-        metric_type=aggregator.GAUGE,
-        tags=['column_3:hello'],
-        hostname=None,
-    )
-
-    aggregator.assert_metric(
-        'test.column.2',
-        2,
-        metric_type=aggregator.GAUGE,
-        tags=['column_3:hello'],
-        hostname=None,
-    )
-
-    aggregator.assert_metric(
-        'test.table.column',
-        1500,
-        metric_type=aggregator.COUNT,
-        tags=['date:2015-04-01'],
-        hostname=None,
-    )
+        for i in range(num_queries):
+            aggregator.assert_metric('test.metric.{}'.format(i), i, metric_type=aggregator.GAUGE, tags=tags)
