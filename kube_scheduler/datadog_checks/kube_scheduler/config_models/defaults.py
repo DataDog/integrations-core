e396c469a551ff8a2796324bc0e2f20e665fecee
Ahmed Mezghani <38987709+ahmed-mez@users.noreply.github.com>
2022-05-10 12:53:58 +0200

    Support dynamic bearer tokens (Bound Service Account Token Volume) (#11915)
    * Support dynamic bearer tokens (Bound Service Account Token Volume)

--- kube_scheduler/datadog_checks/kube_scheduler/config_models/defaults.py
+++ kube_scheduler/datadog_checks/kube_scheduler/config_models/defaults.py
@@ -58,6 +58,10 @@ def instance_bearer_token_path(field, value):
     return get_default_field_value(field, value)
 
 
+def instance_bearer_token_refresh_interval(field, value):
+    return 60
+
+
 def instance_connect_timeout(field, value):
     return get_default_field_value(field, value)
 
