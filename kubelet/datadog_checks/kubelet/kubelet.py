09c88e055da1fc5278a27dd251d049a09d859f78
Ahmed Mezghani <ahmed.mezghani@outlook.com>
2022-05-10 12:44:01 +0200

    Apply container filter to kubernetes log_filesystem metric

--- kubelet/datadog_checks/kubelet/kubelet.py
+++ kubelet/datadog_checks/kubelet/kubelet.py
@@ -19,7 +19,13 @@ from datadog_checks.base.errors import CheckException
 from datadog_checks.base.utils.tagging import tagger
 
 from .cadvisor import CadvisorScraper
-from .common import CADVISOR_DEFAULT_PORT, PodListUtils, replace_container_rt_prefix
+from .common import (
+    CADVISOR_DEFAULT_PORT,
+    PodListUtils,
+    get_container_label,
+    replace_container_rt_prefix,
+    tags_for_docker,
+)
 from .probes import ProbesPrometheusScraperMixin
 from .prometheus import CadvisorPrometheusScraperMixin
 from .summary import SummaryScraperMixin
@@ -75,7 +81,6 @@ DEPRECATED_GAUGES = {
 NEW_1_14_GAUGES = {
     'kubelet_runtime_operations_total': 'kubelet.runtime.operations',
     'kubelet_runtime_operations_errors_total': 'kubelet.runtime.errors',
-    'kubelet_container_log_filesystem_used_bytes': 'kubelet.container.log_filesystem.used_bytes',
 }
 
 DEFAULT_HISTOGRAMS = {
@@ -159,6 +164,10 @@ class KubeletCheck(
     VOLUME_TAG_KEYS_TO_EXCLUDE = ['persistentvolumeclaim', 'pod_phase']
 
     def __init__(self, name, init_config, instances):
+        self.KUBELET_METRIC_TRANSFORMERS = {
+            'kubelet_container_log_filesystem_used_bytes': self.kubelet_container_log_filesystem_used_bytes,
+        }
+
         self.NAMESPACE = 'kubernetes'
         if instances is not None and len(instances) > 1:
             raise Exception('Kubelet check only supports one configured instance.')
@@ -213,6 +222,7 @@ class KubeletCheck(
         for d in [
             self.PROBES_METRIC_TRANSFORMERS,
             self.CADVISOR_METRIC_TRANSFORMERS,
+            self.KUBELET_METRIC_TRANSFORMERS,
             counter_transformers,
             histogram_transformers,
             volume_metric_transformers,
@@ -683,3 +693,28 @@ class KubeletCheck(
             pod_tags = self.pod_tags_by_pvc.get('{}/{}'.format(kube_ns, pvc_name), {})
             tags.extend(pod_tags)
             self.gauge(metric_name_with_namespace, val, tags=list(set(tags)), hostname=custom_hostname)
+
+    def kubelet_container_log_filesystem_used_bytes(self, metric, scraper_config):
+        metric_name = scraper_config['namespace'] + '.kubelet.container.log_filesystem.used_bytes'
+        for sample in metric.samples:
+            self._filter_and_send_gauge_sample(metric_name, sample)
+
+    def _filter_and_send_gauge_sample(self, metric_name, sample):
+        labels = sample[OpenMetricsBaseCheck.SAMPLE_LABELS]
+        container_id = self.pod_list_utils.get_cid_by_labels(labels)
+        tags = []
+        if container_id is not None:
+            if self.pod_list_utils.is_excluded(container_id):
+                return
+
+            tags = tags_for_docker(replace_container_rt_prefix(container_id), tagger.HIGH, True)
+            if not tags:
+                self.log.debug(
+                    "Tags not found for container: %s/%s/%s:%s",
+                    get_container_label(labels, 'namespace'),
+                    get_container_label(labels, 'pod'),
+                    get_container_label(labels, 'container'),
+                    container_id,
+                )
+
+        self.gauge(metric_name, sample[self.SAMPLE_VALUE], tags + self.instance_tags)
