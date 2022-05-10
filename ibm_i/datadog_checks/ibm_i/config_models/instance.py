3e0727b5506d81a4a6f26c52d3dc1b373de441f0
Julia <611228+hithwen@users.noreply.github.com>
2022-05-10 10:33:39 +0200

    Fix IBM ACE validation (#11964)
    * Update datamodel-code-generator
    * Add unit tests
    * Bump requirement in requirements-dev

--- ibm_i/datadog_checks/ibm_i/config_models/instance.py
+++ ibm_i/datadog_checks/ibm_i/config_models/instance.py
@@ -43,16 +43,16 @@ class InstanceConfig(BaseModel):
     driver: Optional[str]
     empty_default_hostname: Optional[bool]
     hostname: Optional[str] = Field(None, max_length=255, min_length=1)
-    job_query_timeout: Optional[int] = Field(None, gt=0.0)
+    job_query_timeout: Optional[int] = Field(None, gt=0)
     metric_patterns: Optional[MetricPatterns]
     min_collection_interval: Optional[float]
     password: Optional[str]
     queries: Optional[Sequence[Query]]
-    query_timeout: Optional[int] = Field(None, gt=0.0)
+    query_timeout: Optional[int] = Field(None, gt=0)
     service: Optional[str]
-    severity_threshold: Optional[int] = Field(None, ge=0.0, le=99.0)
+    severity_threshold: Optional[int] = Field(None, ge=0, le=99)
     system: Optional[str]
-    system_mq_query_timeout: Optional[int] = Field(None, gt=0.0)
+    system_mq_query_timeout: Optional[int] = Field(None, gt=0)
     tags: Optional[Sequence[str]]
     username: Optional[str]
 
