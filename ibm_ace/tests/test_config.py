9479525f783ff113327cce02aeebbff4e14bcb5d
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-05-11 17:17:23 -0400

    refactor test cases

--- ibm_ace/tests/test_config.py
+++ ibm_ace/tests/test_config.py
@@ -11,59 +11,25 @@ pytestmark = [skip_windows_ci, pytest.mark.usefixtures('dd_environment'), pytest
 
 
 @pytest.mark.parametrize(
-    "test_instance, expected_res_stats, expected_msg_flows",
+    "expected_res_stats, expected_msg_flows",
     [
-        pytest.param(
-            {
-                'mq_server': 'localhost',
-                'mq_port': 11414,
-                'channel': 'DEV.ADMIN.SVRCONN',
-                'queue_manager': 'QM1',
-                'mq_user': 'admin',
-                'mq_password': 'passw0rd',
-                'resource_statistics': False,
-                'message_flows': False,
-                'tags': ['foo:bar'],
-            },
-            False,
-            False,
-            id="Metric collection disabled",
-        ),
-        pytest.param(
-            {
-                'mq_server': 'localhost',
-                'mq_port': 11414,
-                'channel': 'DEV.ADMIN.SVRCONN',
-                'queue_manager': 'QM1',
-                'mq_user': 'admin',
-                'mq_password': 'passw0rd',
-                'resource_statistics': True,
-                'message_flows': False,
-                'tags': ['foo:bar'],
-            },
-            True,
-            False,
-            id="Resource stats collection enabled; message_flows disabled",
-        ),
-        pytest.param(
-            {
-                'mq_server': 'localhost',
-                'mq_port': 11414,
-                'channel': 'DEV.ADMIN.SVRCONN',
-                'queue_manager': 'QM1',
-                'mq_user': 'admin',
-                'mq_password': 'passw0rd',
-                'resource_statistics': False,
-                'message_flows': True,
-                'tags': ['foo:bar'],
-            },
-            False,
-            True,
-            id="Resource stats collection disabled; message_flows enabled",
-        ),
+        pytest.param(False, False, id="Metric collection disabled"),
+        pytest.param(True, False, id="Resource stats collection enabled; message_flows disabled"),
+        pytest.param(False, True, id="Resource stats collection disabled; message_flows enabled"),
     ],
 )
-def test_config(dd_run_check, aggregator, test_instance, expected_res_stats, expected_msg_flows):
+def test_config(dd_run_check, aggregator, expected_res_stats, expected_msg_flows):
+    test_instance = {
+        'mq_server': 'localhost',
+        'mq_port': 11414,
+        'channel': 'DEV.ADMIN.SVRCONN',
+        'queue_manager': 'QM1',
+        'mq_user': 'admin',
+        'mq_password': 'passw0rd',
+        'resource_statistics': expected_res_stats,
+        'message_flows': expected_msg_flows,
+        'tags': ['foo:bar'],
+    }
 
     check = IbmAceCheck('ibm_ace', {}, [test_instance])
     dd_run_check(check)
