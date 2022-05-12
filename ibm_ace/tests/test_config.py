a58bea7ab65ca5c63dd29303a104b809837e133d
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-05-12 16:47:33 -0400

    assert subscriptions

--- ibm_ace/tests/test_config.py
+++ ibm_ace/tests/test_config.py
@@ -4,6 +4,7 @@
 import pytest
 
 from datadog_checks.ibm_ace import IbmAceCheck
+from datadog_checks.ibm_ace.subscription import FlowMonitoringSubscription, ResourceStatisticsSubscription
 
 from .common import skip_windows_ci
 
@@ -16,6 +17,7 @@ pytestmark = [skip_windows_ci, pytest.mark.usefixtures('dd_environment'), pytest
         pytest.param(False, False, id="Metric collection disabled"),
         pytest.param(True, False, id="Resource stats collection enabled; message_flows disabled"),
         pytest.param(False, True, id="Resource stats collection disabled; message_flows enabled"),
+        pytest.param(True, True, id="Resource stats collection enabled; message_flows enabled"),
     ],
 )
 def test_config(dd_run_check, aggregator, expected_res_stats, expected_msg_flows):
@@ -33,6 +35,19 @@ def test_config(dd_run_check, aggregator, expected_res_stats, expected_msg_flows
 
     check = IbmAceCheck('ibm_ace', {}, [test_instance])
     dd_run_check(check)
+
+    if not expected_res_stats and not expected_msg_flows:
+        assert len(check._subscriptions) == 0
+    if expected_res_stats and expected_msg_flows:
+        assert len(check._subscriptions) == 0
+        assert isinstance(check._subscriptions[0], ResourceStatisticsSubscription)
+        assert isinstance(check._subscriptions[1], FlowMonitoringSubscription)
+    if expected_res_stats and not expected_msg_flows:
+        assert len(check._subscriptions) == 1
+        assert isinstance(check._subscriptions[0], ResourceStatisticsSubscription)
+    if expected_msg_flows and not expected_res_stats:
+        assert len(check._subscriptions) == 1
+        assert isinstance(check._subscriptions[0], FlowMonitoringSubscription)
 
     assert check.config.resource_statistics == expected_res_stats
     assert check.config.message_flows == expected_msg_flows
