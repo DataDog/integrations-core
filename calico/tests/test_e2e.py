98ee910e6e0b8a8cecfdcdaddda5717e6dd9d811
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-05-12 11:11:07 +0200

    fix calico flakey assertion

--- calico/tests/test_e2e.py
+++ calico/tests/test_e2e.py
@@ -12,9 +12,10 @@ from . import common
 @pytest.mark.e2e
 def test_check_ok(dd_agent_check):
     aggregator = dd_agent_check(rate=True)
-    metrics = common.FORMATTED_EXTRA_METRICS
 
-    for metric in metrics:
+    for metric in common.FORMATTED_EXTRA_METRICS:
         aggregator.assert_metric(metric)
+    for metric in common.OPTIONAL_METRICS:
+        aggregator.assert_metric(metric, at_least=0)
 
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
