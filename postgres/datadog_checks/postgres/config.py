c3ed973b8a6bf338a0d9a119208631f221ae850a
Zach Smith <zach.smith@datadoghq.com>
2022-05-12 23:56:27 -0400

    More descriptive name for public option

--- postgres/datadog_checks/postgres/config.py
+++ postgres/datadog_checks/postgres/config.py
@@ -100,7 +100,7 @@ class PostgresConfig:
                     'replace_digits', obfuscator_options_config.get('quantize_sql_tables', False)
                 )
             ),
-            'dollar_quoted_func': is_affirmative(obfuscator_options_config.get('dollar_quoted_func', False)),
+            'keep_dollar_quoted_func': is_affirmative(obfuscator_options_config.get('dollar_quoted_func', False)),
             'keep_sql_alias': is_affirmative(obfuscator_options_config.get('keep_sql_alias', False)),
             'return_json_metadata': is_affirmative(obfuscator_options_config.get('collect_metadata', True)),
             'table_names': is_affirmative(obfuscator_options_config.get('collect_tables', True)),
