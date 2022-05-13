fd66368d562018d9c6eaa5cc53c19b480867c20f
Zach Smith <zach.smith@datadoghq.com>
2022-05-13 14:43:41 -0400

    default to true

--- mysql/datadog_checks/mysql/config.py
+++ mysql/datadog_checks/mysql/config.py
@@ -62,8 +62,8 @@ class MySQLConfig(object):
                     'replace_digits', obfuscator_options_config.get('quantize_sql_tables', False)
                 )
             ),
-            'dollar_quoted_func': is_affirmative(obfuscator_options_config.get('keep_dollar_quoted_func', False)),
-            'keep_sql_alias': is_affirmative(obfuscator_options_config.get('keep_sql_alias', False)),
+            'dollar_quoted_func': is_affirmative(obfuscator_options_config.get('keep_dollar_quoted_func', True)),
+            'keep_sql_alias': is_affirmative(obfuscator_options_config.get('keep_sql_alias', True)),
             'return_json_metadata': is_affirmative(obfuscator_options_config.get('collect_metadata', True)),
             'table_names': is_affirmative(obfuscator_options_config.get('collect_tables', True)),
             'collect_commands': is_affirmative(obfuscator_options_config.get('collect_commands', True)),
