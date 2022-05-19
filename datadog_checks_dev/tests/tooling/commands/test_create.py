6f20767690e6faeb6ae35e2302fdb9a1588aac1d
Sarah <sarah.witt@datadoghq.com>
2022-05-19 18:47:02 -0400

    Fix style

--- datadog_checks_dev/tests/tooling/commands/test_create.py
+++ datadog_checks_dev/tests/tooling/commands/test_create.py
@@ -55,7 +55,7 @@ def test_new_check_test(integration_type, installable):
             # `ddev test` will not fail if the provided check name doesn't correspond to an existing integration.
             # Instead, it will log a message. So we test for that message to verify style checks ran at all.
             assert 'Nothing to test!' not in result.stdout
-            
+
             result = run_command(
                 [sys.executable, '-m', 'pip', 'uninstall', '-y', 'datadog-my-check'], capture=True, check=True
             )
