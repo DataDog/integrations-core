b1edbc5d948de4336619ffa2bf0fbd8cae9d3447
mawais54013 <muhammad.awais@datadoghq.com>
2022-05-25 15:49:35 -0700

    updated query

--- postgres/tests/test_statements.py
+++ postgres/tests/test_statements.py
@@ -425,8 +425,8 @@ def test_failed_explain_handling(
     check = integration_check(dbm_instance)
     check._connect()
 
-    if skip_on_versions is not None and check.version.major in skip_on_versions:
-        return True
+    if skip_on_versions is not None and POSTGRES_VERSION in skip_on_versions:
+        pytest.skip("DatatypeMisMatch error only appears on postgres v14")
 
     # run check so all internal state is correctly initialized
     check.check(dbm_instance)
