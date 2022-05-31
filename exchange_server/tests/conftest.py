0ffa66ef46665c4fbccf674f6a551a2d42e6fe0d
josemanuel.almaza <josemanuel.almaza@datadoghq.com>
2022-05-31 16:10:51 +0200

    Added needed fixtures

--- exchange_server/tests/conftest.py
+++ exchange_server/tests/conftest.py
@@ -1,8 +1,11 @@
 # (C) Datadog, Inc. 2022-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from copy import deepcopy
 
 import pytest
+
+from datadog_checks.exchange_server import ExchangeCheck
 
 from .common import INSTANCE
 
@@ -10,3 +13,13 @@ from .common import INSTANCE
 @pytest.fixture(scope="session")
 def dd_environment():
     yield INSTANCE, {'docker_platform': 'windows'}
+
+
+@pytest.fixture
+def check():
+    return lambda instance: ExchangeCheck('exchange_server', {}, [instance])
+
+
+@pytest.fixture
+def instance():
+    return deepcopy(INSTANCE)
