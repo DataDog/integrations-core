16f94ef9b1e4d7ed9329c69f7cd947022e9c5f67
josemanuel.almaza <josemanuel.almaza@datadoghq.com>
2022-05-31 13:48:38 +0200

    Solved format style error

--- exchange_server/tests/test_exchange_server.py
+++ exchange_server/tests/test_exchange_server.py
@@ -4,13 +4,12 @@
 import pytest
 from datadog_test_libs.win.pdh_mocks import initialize_pdh_tests, pdh_mocks_fixture  # noqa: F401
 
+from datadog_checks.base import AgentCheck
 from datadog_checks.dev.testing import requires_py2
 from datadog_checks.exchange_server import ExchangeCheck
 from datadog_checks.exchange_server.metrics import DEFAULT_COUNTERS
 
 from .common import CHECK_NAME, METRIC_INSTANCES, MINIMAL_INSTANCE
-
-from datadog_checks.base import AgentCheck
 
 pytestmark = [requires_py2, pytest.mark.usefixtures('pdh_mocks_fixture')]
 
