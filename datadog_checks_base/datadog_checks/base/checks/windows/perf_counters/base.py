a32460b8b5c1862698d9667fc261adc819a0fa5b
josemanuel.almaza <josemanuel.almaza@datadoghq.com>
2022-06-03 19:16:08 +0200

    Submit CRITICAL health check value if a perf object collect raises an exception

--- datadog_checks_base/datadog_checks/base/checks/windows/perf_counters/base.py
+++ datadog_checks_base/datadog_checks/base/checks/windows/perf_counters/base.py
@@ -94,7 +94,9 @@ class PerfCountersBaseCheck(AgentCheck):
             try:
                 perf_object.collect()
             except Exception as e:
-                self.log.error('Error collecting query data for performance object `%s`: %s', perf_object.name, e)
+                message = 'Error collecting query data for performance object `%s`: %s'.format(perf_object.name, e)
+                self.submit_health_check(self.CRITICAL, message=message)
+                self.log.error(message)
 
         self.submit_health_check(self.OK)
 
