1537aec1d53d684147b34922c46ff1cc5a611c9b
josemanuel.almaza <josemanuel.almaza@datadoghq.com>
2022-06-08 17:52:46 +0200

    Updated e2e test

--- exchange_server/tests/test_e2e.py
+++ exchange_server/tests/test_e2e.py
@@ -6,11 +6,20 @@ import pytest
 
 from datadog_checks.base import AgentCheck
 from datadog_checks.dev.testing import requires_py3
+from datadog_checks.exchange_server.metrics import METRICS_CONFIG
 
 
 @pytest.mark.e2e
 @requires_py3
 def test_e2e_py3(dd_agent_check, aggregator, instance):
+    metric_namespace = 'exchange'
     aggregator = dd_agent_check(instance)
     aggregator.assert_service_check('exchange.windows.perf.health', AgentCheck.OK)
-    assert aggregator.metric_names == []
+    for object_name, config in METRICS_CONFIG.items():
+        if object_name.startswith("MSExchange"):
+            metric_prefix = config.get('name', '')
+            counters_config = config.get('counters', [])
+            for _, entry in enumerate(counters_config, 1):
+                for _, counter_config in entry.items():
+                    metric_full_name = f"{metric_namespace}.{metric_prefix}.{counter_config}"
+                    aggregator.assert_metric(metric_full_name, count=0)
