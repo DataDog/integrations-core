6ebd073c1df1df5fb9246205ad1c7d401f230de1
josemanuel.almaza <josemanuel.almaza@datadoghq.com>
2022-06-08 18:10:44 +0200

    Corrected format in metric_full_name

--- exchange_server/tests/test_e2e.py
+++ exchange_server/tests/test_e2e.py
@@ -9,17 +9,24 @@ from datadog_checks.dev.testing import requires_py3
 from datadog_checks.exchange_server.metrics import METRICS_CONFIG
 
 
-@pytest.mark.e2e
-@requires_py3
-def test_e2e_py3(dd_agent_check, aggregator, instance):
+def get_exchange_server_metrics()
+    exchange_server_metrics = list([])
     metric_namespace = 'exchange'
-    aggregator = dd_agent_check(instance)
-    aggregator.assert_service_check('exchange.windows.perf.health', AgentCheck.OK)
     for object_name, config in METRICS_CONFIG.items():
         if object_name.startswith("MSExchange"):
             metric_prefix = config.get('name', '')
             counters_config = config.get('counters', [])
             for _, entry in enumerate(counters_config, 1):
                 for _, counter_config in entry.items():
-                    metric_full_name = f"{metric_namespace}.{metric_prefix}.{counter_config}"
-                    aggregator.assert_metric(metric_full_name, count=0)
+                    metric_full_name = "{}.{}.{}".format(metric_namespace, metric_prefix,counter_config)
+                    exchange_server_metrics.append(metric_full_name)
+    return exchange_server_metrics
+
+
+@pytest.mark.e2e
+@requires_py3
+def test_e2e_py3(dd_agent_check, aggregator, instance):
+    aggregator = dd_agent_check(instance)
+    aggregator.assert_service_check('exchange.windows.perf.health', AgentCheck.OK)
+    for metric in get_exchange_server_metrics():
+        aggregator.assert_metric(metric, count=0)
