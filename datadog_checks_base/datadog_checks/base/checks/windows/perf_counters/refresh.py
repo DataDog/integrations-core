38cebf0d2be2df2a054bcff4054a997202fc3006
Ofek Lev <ofekmeister@gmail.com>
2022-06-21 11:29:53 -0400

    dedent

--- datadog_checks_base/datadog_checks/base/checks/windows/perf_counters/refresh.py
+++ datadog_checks_base/datadog_checks/base/checks/windows/perf_counters/refresh.py
@@ -40,20 +40,20 @@ class WindowsPerformanceObjectRefresher(threading.Thread):
                 now = get_precise_time()
                 if server in self.last_refresh and now - self.last_refresh[server] < self.INTERVAL:
                     continue
-                else:
-                    self.logger.info('Refreshing performance objects for server: %s', server)
-                    try:
-                        # https://docs.microsoft.com/en-us/windows/win32/api/pdh/nf-pdh-pdhenumobjectsa
-                        # https://mhammond.github.io/pywin32/win32pdh__EnumObjects_meth.html
-                        win32pdh.EnumObjects(None, server, win32pdh.PERF_DETAIL_WIZARD, True)
-                    except pywintypes.error as error:
-                        self.logger.error(
-                            'Error refreshing performance objects for server `%s`: %s', server, error.strerror
-                        )
-                    else:
-                        self.logger.info('Successfully refreshed performance objects for server: %s', server)
 
-                    self.last_refresh[server] = now
+                self.logger.info('Refreshing performance objects for server: %s', server)
+                try:
+                    # https://docs.microsoft.com/en-us/windows/win32/api/pdh/nf-pdh-pdhenumobjectsa
+                    # https://mhammond.github.io/pywin32/win32pdh__EnumObjects_meth.html
+                    win32pdh.EnumObjects(None, server, win32pdh.PERF_DETAIL_WIZARD, True)
+                except pywintypes.error as error:
+                    self.logger.error(
+                        'Error refreshing performance objects for server `%s`: %s', server, error.strerror
+                    )
+                else:
+                    self.logger.info('Successfully refreshed performance objects for server: %s', server)
+
+                self.last_refresh[server] = now
 
             time.sleep(2)
 
