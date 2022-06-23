1ab87c6288807e8e6e5bce75b800401a0826e8e1
jmeunier28 <jojo.meunier@datadoghq.com>
2022-06-23 13:06:47 -0700

    fix query

--- sqlserver/datadog_checks/sqlserver/activity.py
+++ sqlserver/datadog_checks/sqlserver/activity.py
@@ -49,8 +49,8 @@ SELECT
     req.status as request_status,
     substring(text.text, 
         ((case req.statement_start_offset when '-1' 
-        then 0 else req.statement_start_offset end) / 2 + 1) ,
-        (req.statement_end_offset-r.statement_start_offset) / 2 )
+        then 0 else req.statement_start_offset end) / 2 + 1),
+        (req.statement_end_offset - req.statement_start_offset) / 2 ) as text,
     c.client_tcp_port as client_port,
     c.client_net_address as client_address,
     sess.host_name as host_name,
