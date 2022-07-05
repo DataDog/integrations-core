651ecfb54ee7cee736ef284d34a4a57fbfd53836
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-07-05 15:53:33 +0200

    Run ldconfig

--- .azure-pipelines/scripts/ibm_db2/linux/55_install_client.sh
+++ .azure-pipelines/scripts/ibm_db2/linux/55_install_client.sh
@@ -7,7 +7,8 @@
 set -ex
 
 IBM_DB_FOLDER=/opt/ibm_db
-DB2_URL=https://ddintegrations.blob.core.windows.net/ibm-db2/linuxx64_odbc_cli.tar.gz
+IBM_DB_HOME=$IBM_DB_FOLDER/clidriver
+DB2_CLI_URL=https://ddintegrations.blob.core.windows.net/ibm-db2/linuxx64_odbc_cli.tar.gz
 
 
 sudo apt-get update
@@ -21,7 +22,7 @@ pushd $IBM_DB_FOLDER
   # Retry necessary due to flaky download that might trigger:
   # curl: (56) OpenSSL SSL_read: SSL_ERROR_SYSCALL, errno 110
   for i in 2 4 8 16 32; do
-    curl --verbose -LO $DB2_URL && break
+    curl --verbose -LO $DB2_CLI_URL && break
     echo "[INFO] Wait $i seconds and retry curl download"
     sleep $i
   done
@@ -31,11 +32,11 @@ pushd $IBM_DB_FOLDER
 
 popd
 
-chmod -R a+xr /opt/ibm_db/clidriver
-ls /opt/ibm_db
-ls /opt/ibm_db/clidriver
-echo "Finding libdb2.so.1"
-find / -name libdb2.so.1
+chmod -R a+xr $IBM_DB_HOME
+ls $IBM_DB_HOME
+ls $IBM_DB_HOME/lib
 
+export LD_LIBRARY_PATH=$IBM_DB_HOME/lib:$LD_LIBRARY_PATH
+ldconfig
 
 set +ex
