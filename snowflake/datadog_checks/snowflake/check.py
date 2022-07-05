f5d722ef23d6e42471f191125947a919a3cfd4ab
Sarah <sarah.witt@datadoghq.com>
2022-07-05 15:07:09 -0400

    Add back utf-8

--- snowflake/datadog_checks/snowflake/check.py
+++ snowflake/datadog_checks/snowflake/check.py
@@ -108,7 +108,7 @@ class SnowflakeCheck(AgentCheck):
     def read_token(self):
         if self._config.token_path:
             self.log.debug("Renewing Snowflake client token")
-            with open(self._config.token_path, 'r') as f:
+            with open(self._config.token_path, 'r', encoding="UTF-8") as f:
                 self._config.token = f.read()
 
         return self._config.token
