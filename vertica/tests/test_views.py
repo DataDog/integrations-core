01e23b99045c97b503ab520c8151f9e580d74b61
Alex Lopez <alex.lopez@datadoghq.com>
2022-07-14 09:30:39 +0200

    Parametrize query construction tests

--- vertica/tests/test_views.py
+++ vertica/tests/test_views.py
@@ -1,84 +1,90 @@
 # (C) Datadog, Inc. 2022-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
 
 from datadog_checks.vertica import views
 
 
-def test_make_projection_storage_queries_old_versions():
-    queries = views.make_projection_storage_queries(9)
+@pytest.mark.parametrize(
+    'version, expected_per_projection_query',
+    [
+        (
+            9,
+            (
+                'SELECT '
+                'anchor_table_name, '
+                'node_name, '
+                'projection_name, '
+                'sum(ros_count) as ros_count, '
+                'sum(row_count) as row_count, '
+                'sum(used_bytes) as used_bytes, '
+                'sum(ros_row_count) as ros_row_count, '
+                'sum(wos_row_count) as wos_row_count, '
+                'sum(ros_used_bytes) as ros_used_bytes, '
+                'sum(wos_used_bytes) as wos_used_bytes '
+                'FROM v_monitor.projection_storage GROUP BY anchor_table_name, node_name, projection_name'
+            ),
+        ),
+        (
+            11,
+            (
+                'SELECT '
+                'anchor_table_name, '
+                'node_name, '
+                'projection_name, '
+                'sum(ros_count) as ros_count, '
+                'sum(row_count) as row_count, '
+                'sum(used_bytes) as used_bytes '
+                'FROM v_monitor.projection_storage GROUP BY anchor_table_name, node_name, projection_name'
+            ),
+        ),
+    ],
+)
+def test_make_projection_storage_queries(version, expected_per_projection_query):
+    queries = views.make_projection_storage_queries(version)
 
     assert 'per_projection' in queries
     assert 'per_table' in queries
     assert 'per_node' in queries
     assert 'total' in queries
 
-    assert queries['per_projection'] == (
-        'SELECT '
-        'anchor_table_name, '
-        'node_name, '
-        'projection_name, '
-        'sum(ros_count) as ros_count, '
-        'sum(row_count) as row_count, '
-        'sum(used_bytes) as used_bytes, '
-        'sum(ros_row_count) as ros_row_count, '
-        'sum(wos_row_count) as wos_row_count, '
-        'sum(ros_used_bytes) as ros_used_bytes, '
-        'sum(wos_used_bytes) as wos_used_bytes '
-        'FROM v_monitor.projection_storage GROUP BY anchor_table_name, node_name, projection_name'
-    )
+    assert queries['per_projection'] == expected_per_projection_query
 
 
-def test_make_projection_storage_queries():
-    queries = views.make_projection_storage_queries(11)
-
-    assert 'per_projection' in queries
-    assert 'per_table' in queries
-    assert 'per_node' in queries
-    assert 'total' in queries
-
-    assert queries['per_projection'] == (
-        'SELECT '
-        'anchor_table_name, '
-        'node_name, '
-        'projection_name, '
-        'sum(ros_count) as ros_count, '
-        'sum(row_count) as row_count, '
-        'sum(used_bytes) as used_bytes '
-        'FROM v_monitor.projection_storage GROUP BY anchor_table_name, node_name, projection_name'
-    )
-
-
-def test_make_storage_containers_queries_old():
-    queries = views.make_storage_containers_queries(9)
+@pytest.mark.parametrize(
+    'version, expected_per_projection_query',
+    [
+        (
+            9,
+            (
+                'SELECT '
+                'node_name, '
+                'projection_name, '
+                'storage_type, '
+                'sum(delete_vector_count) as delete_vector_count '
+                'FROM v_monitor.storage_containers '
+                'GROUP BY node_name, projection_name, storage_type'
+            ),
+        ),
+        (
+            11,
+            (
+                'SELECT '
+                'node_name, '
+                'projection_name, '
+                'sum(delete_vector_count) as delete_vector_count '
+                'FROM v_monitor.storage_containers '
+                'GROUP BY node_name, projection_name'
+            ),
+        ),
+    ],
+)
+def test_make_storage_containers_queries(version, expected_per_projection_query):
+    queries = views.make_storage_containers_queries(version)
 
     assert 'per_projection' in queries
     assert 'per_node' in queries
     assert 'total' in queries
 
-    assert queries['per_projection'] == (
-        'SELECT '
-        'node_name, '
-        'projection_name, '
-        'storage_type, '
-        'sum(delete_vector_count) as delete_vector_count '
-        'FROM v_monitor.storage_containers '
-        'GROUP BY node_name, projection_name, storage_type'
-    )
-
-
-def test_make_storage_containers_queries():
-    queries = views.make_storage_containers_queries(11)
-
-    assert 'per_projection' in queries
-    assert 'per_node' in queries
-    assert 'total' in queries
-
-    assert queries['per_projection'] == (
-        'SELECT '
-        'node_name, '
-        'projection_name, '
-        'sum(delete_vector_count) as delete_vector_count '
-        'FROM v_monitor.storage_containers '
-        'GROUP BY node_name, projection_name'
-    )
+    assert queries['per_projection'] == expected_per_projection_query
