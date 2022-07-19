d7d8704037b529879ad826b97aad59f3b4f20d57
Florent Clarret <florent.clarret@datadoghq.com>
2022-07-19 13:22:58 +0000

    Add an option to set the tenant databases schema (#12492)
    * Add an option to set the tenant databases schema
    
    * Move the new parameter to the top of the config file
    
    * Add an integration with an invalid schema
    
    * Rename tenant_databases_schema to schema
    
    * Regenerate models
    
    * Add the example to the schema properties in the config

--- sap_hana/tests/test_integration.py
+++ sap_hana/tests/test_integration.py
@@ -1,7 +1,9 @@
 # (C) Datadog, Inc. 2019-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import mock
 import pytest
+from mock.mock import ANY, call
 
 from datadog_checks.sap_hana import SapHanaCheck
 
@@ -20,6 +22,34 @@ def test_check(aggregator, instance, dd_run_check):
         aggregator.assert_metric_has_tag(metric, 'port:{}'.format(instance['port']))
 
     aggregator.assert_all_metrics_covered()
+
+
+@pytest.mark.usefixtures('dd_environment')
+def test_check_invalid_schema(aggregator, instance, dd_run_check):
+    instance["schema"] = "UNKNOWN_SCHEMA"
+    check = SapHanaCheck('sap_hana', {}, [instance])
+    check.log = mock.MagicMock()
+    dd_run_check(check)
+
+    check.log.error.assert_has_calls(
+        calls=[
+            call('Error querying %s: %s', 'UNKNOWN_SCHEMA.M_BACKUP_PROGRESS', ANY),
+            call('Error querying %s: %s', 'UNKNOWN_SCHEMA.M_LICENSES', ANY),
+            call('Error querying %s: %s', 'UNKNOWN_SCHEMA.M_CONNECTIONS', ANY),
+            call('Error querying %s: %s', 'UNKNOWN_SCHEMA.M_DISK_USAGE', ANY),
+            call('Error querying %s: %s', 'UNKNOWN_SCHEMA.M_SERVICE_MEMORY', ANY),
+            call('Error querying %s: %s', 'UNKNOWN_SCHEMA.M_SERVICE_COMPONENT_MEMORY', ANY),
+            call('Error querying %s: %s', 'UNKNOWN_SCHEMA.M_RS_MEMORY', ANY),
+            call('Error querying %s: %s', 'UNKNOWN_SCHEMA.M_SERVICE_STATISTICS', ANY),
+            call('Error querying %s: %s', 'UNKNOWN_SCHEMA.M_VOLUME_IO_TOTAL_STATISTICS', ANY),
+        ],
+        any_order=False,
+    )
+
+    assert check.log.error.call_count == 9
+
+    for call_args in check.log.error.call_args_list:
+        assert "invalid schema name: UNKNOWN_SCHEMA" in call_args[0][2]
 
 
 @pytest.mark.usefixtures('dd_environment')
