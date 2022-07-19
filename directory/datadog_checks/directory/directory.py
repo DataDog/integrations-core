e7ec17938eb6acf99c4f25d14c0888888122505a
Kyle Neale <kyle.neale@datadoghq.com>
2022-07-18 20:37:56 -0400

    added debug logging directory check

--- directory/datadog_checks/directory/directory.py
+++ directory/datadog_checks/directory/directory.py
@@ -92,7 +92,7 @@ class DirectoryCheck(AgentCheck):
                     dirs[:] = [d for d in dirs if not self._config.exclude_dirs_pattern.search(d.path)]
                 else:
                     dirs[:] = [d for d in dirs if not self._config.exclude_dirs_pattern.search(d.name)]
-
+                self.log.debug('Directories: %s', str(dirs))
             if self._config.pattern is not None:
                 # Check if the path of the file relative to the directory
                 # matches the pattern. Also check if the absolute path of the
@@ -116,14 +116,15 @@ class DirectoryCheck(AgentCheck):
 
             for file_entry in matched_files:
                 try:
+                    self.log.debug('File entries in matched files: %s', str(file_entry))
                     file_stat = file_entry.stat(follow_symlinks=self._config.stat_follow_symlinks)
-
                 except OSError as ose:
                     self.warning('DirectoryCheck: could not stat file %s - %s', join(root, file_entry.name), ose)
                 else:
                     # file specific metrics
                     directory_bytes += file_stat.st_size
                     if self._config.filegauges and matched_files_length <= max_filegauge_balance:
+                        self.log.debug('Matched files length: %s', matched_files_length)
                         filetags = ['{}:{}'.format(self._config.filetagname, join(root, file_entry.name))]
                         filetags.extend(dirtags)
                         self.gauge('system.disk.directory.file.bytes', file_stat.st_size, tags=filetags)
@@ -134,6 +135,12 @@ class DirectoryCheck(AgentCheck):
                             'system.disk.directory.file.created_sec_ago', time() - file_stat.st_ctime, tags=filetags
                         )
                         adjust_max_filegauge = True
+                        self.log.debug(
+                            'File stat output - size:%s mtime:%s ctime:%s',
+                            str(file_stat.st_size),
+                            str(file_stat.st_mtime),
+                            str(file_stat.st_ctime),
+                        )
                     elif submit_histograms:
                         self.histogram('system.disk.directory.file.bytes', file_stat.st_size, tags=dirtags)
                         self.histogram(
@@ -142,6 +149,13 @@ class DirectoryCheck(AgentCheck):
                         self.histogram(
                             'system.disk.directory.file.created_sec_ago', time() - file_stat.st_ctime, tags=dirtags
                         )
+                        self.log.debug(
+                            'File stat output histogram - size:%s mtime:%s ctime:%s',
+                            str(file_stat.st_size),
+                            str(file_stat.st_mtime),
+                            str(file_stat.st_ctime),
+                        )
+
             if adjust_max_filegauge:
                 max_filegauge_balance -= matched_files_length
 
@@ -151,3 +165,4 @@ class DirectoryCheck(AgentCheck):
         # total file size
         if not self._config.countonly:
             self.gauge('system.disk.directory.bytes', directory_bytes, tags=dirtags)
+            self.log.debug('`countonly` not enabled')
