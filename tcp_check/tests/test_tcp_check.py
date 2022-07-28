118ec870e17ef52c7b84ed635e8b6dbcad4a137f
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-07-28 14:50:16 -0400

    fix logic

--- tcp_check/tests/test_tcp_check.py
+++ tcp_check/tests/test_tcp_check.py
@@ -363,9 +363,9 @@ def test_ipv6(aggregator, check):
     assert nb_ipv4 == 4
     # The Windows CI machine doesn't return IPv6
     # Windows or MacOS might not have IPv6 connectivity when testing locally
-    if platform.system() not in ('Windows', 'Darwin'):
+    if has_ipv6 and platform.system() not in ('Windows', 'Darwin'):
         assert nb_ipv6 == 8
-    elif not has_ipv6:
+    else:
         assert nb_ipv6 == 0
 
     aggregator.assert_all_metrics_covered()
