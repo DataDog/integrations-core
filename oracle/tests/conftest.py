9aded67380a6f44b56861b1252dd98b80c52ff27
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-07-28 12:15:16 +0200

    Run apt commands in non interactive mode, do not install instant client when not needed

--- oracle/tests/conftest.py
+++ oracle/tests/conftest.py
@@ -34,8 +34,6 @@ E2E_METADATA_ORACLE_CLIENT = {
     ],
     'start_commands': [
         'bash /tmp/install_instant_client.sh',
-        'apt-get install libaio1',  # `apt-get update` already ran in install_instant_client.sh
-        'apt-get install gcc g++ -y',
     ],
     'env_vars': {'LD_LIBRARY_PATH': '/opt/oracle/instantclient_19_3', 'TNS_ADMIN': '/opt/oracle/instantclient_19_3'},
 }
@@ -45,7 +43,6 @@ E2E_METADATA_JDBC_CLIENT = {
     # the integration will fallback to JDBC client
     'use_jmx': True,  # Using jmx to have a ready to use java runtime
     'docker_volumes': [
-        '{}/scripts/install_instant_client.sh:/tmp/install_instant_client.sh'.format(HERE),
         '{}/docker/client/client_wallet:/opt/oracle/instantclient_19_3/client_wallet'.format(HERE),
         '{}/docker/client/sqlnet.ora:/opt/oracle/instantclient_19_3/sqlnet.ora'.format(HERE),
         '{}/docker/client/tnsnames.ora:/opt/oracle/instantclient_19_3/tnsnames.ora'.format(HERE),
@@ -55,8 +52,8 @@ E2E_METADATA_JDBC_CLIENT = {
         '{}/docker/client/osdt_core.jar:/opt/oracle/instantclient_19_3/osdt_core.jar'.format(HERE),
     ],
     'start_commands': [
-        'bash /tmp/install_instant_client.sh',
-        'apt-get install gcc g++ -y',  # `apt-get update` already ran in install_instant_client.sh
+        'apt-get update',
+        'DEBIAN_FRONTEND=noninteractive apt-get install -yq gcc g++',
     ],
     'env_vars': {'TNS_ADMIN': '/opt/oracle/instantclient_19_3'},
 }
