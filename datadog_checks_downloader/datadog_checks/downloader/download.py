5673b5d69ad14c2fd0d1680849f1fe5b4da57460
Florent Clarret <florent.clarret@datadoghq.com>
2022-08-01 13:18:24 +0000

    Revert #12559 to make it compatible with python2 (#12647)
    * Revert "Use context manager when creating temporary directory in datadog-checks-downloader (#12559)"
    
    This reverts commit 675fe0df01c38b6db79b53f78b4e7bdfb753cfb0.
    
    * Make sure the tempdir is removed in case an exception is raised

--- datadog_checks_downloader/datadog_checks/downloader/download.py
+++ datadog_checks_downloader/datadog_checks/downloader/download.py
@@ -215,7 +215,9 @@ class TUFDownloader:
 
     def __in_toto_verify(self, inspection_packet, target_relpath):
         # Make a temporary directory in a parent directory we control.
-        with tempfile.TemporaryDirectory(dir=REPOSITORIES_DIR) as tempdir:
+        tempdir = tempfile.mkdtemp(dir=REPOSITORIES_DIR)
+
+        try:
             # Copy files over into temp dir.
             for abs_path in inspection_packet:
                 shutil.copy(abs_path, tempdir)
@@ -226,16 +228,17 @@ class TUFDownloader:
             # Load the root layout and public keys in this temp dir.
             root_layout, root_layout_pubkeys, root_layout_params = self.__load_root_layout(target_relpath)
 
-            try:
-                verifylib.in_toto_verify(root_layout, root_layout_pubkeys, substitution_parameters=root_layout_params)
-            except Exception as e:
-                self.__handle_in_toto_verification_exception(target_relpath, e)
-            else:
-                logger.info('in-toto verified %s', target_relpath)
-            finally:
-                # Switch back to a parent directory we control, so that we can
-                # safely delete temp dir.
-                os.chdir(REPOSITORIES_DIR)
+            verifylib.in_toto_verify(root_layout, root_layout_pubkeys, substitution_parameters=root_layout_params)
+        except Exception as e:
+            self.__handle_in_toto_verification_exception(target_relpath, e)
+        else:
+            logger.info('in-toto verified %s', target_relpath)
+        finally:
+            # Switch back to a parent directory we control, so that we can
+            # safely delete temp dir.
+            os.chdir(REPOSITORIES_DIR)
+            # Delete temp dir.
+            shutil.rmtree(tempdir)
 
     def __download_and_verify_in_toto_metadata(self, target_relpath, target_abspath, target):
         # First, get our in-toto root layout.
