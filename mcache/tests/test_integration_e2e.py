c805acdb0733334b1099c1f4cb7ec653b0e41eb6
Florent Clarret <florent.clarret@datadoghq.com>
2022-08-05 14:54:55 +0200

    Revert "Assert metrics using metadata"
    This reverts commit 2c383380993525e98b8ba38c39ffe36bd8d71a77.

--- mcache/tests/test_integration_e2e.py
+++ mcache/tests/test_integration_e2e.py
@@ -5,7 +5,6 @@ import pytest
 
 from datadog_checks.mcache import Memcache
 
-from datadog_checks.dev.utils import get_metadata_metrics
 from .common import HOST, PORT, SERVICE_CHECK, requires_socket_support, requires_unix_utils
 from .metrics import GAUGES, ITEMS_GAUGES, ITEMS_RATES, RATES, SLABS_AGGREGATES, SLABS_GAUGES, SLABS_RATES
 from .utils import count_connections, get_host_socket_path
@@ -24,7 +23,6 @@ def assert_check_coverage(aggregator):
         aggregator.assert_metric(m, tags=expected_tags)
 
     aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
 @pytest.mark.e2e
