cbe51e5c63368f1d844fa3a1d25b1ff5ab7bec99
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2022-08-22 15:21:40 +0200

    Fixed tests errors

--- tdd/tests/conftest.py
+++ tdd/tests/conftest.py
@@ -4,11 +4,6 @@
 import pytest
 
 
-@pytest.fixture(scope='session')
-def dd_environment():
-    yield
-
-
 @pytest.fixture
 def instance():
     return {}
--- tdd/tests/test_tdd.py
+++ tdd/tests/test_tdd.py
@@ -2,32 +2,44 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-from typing import Any, Callable, Dict
+import mock
+import pytest
+from pymongo.errors import ConnectionFailure
 
-from datadog_checks.base import AgentCheck
-from datadog_checks.base.stubs.aggregator import AggregatorStub
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.tdd import TddCheck
 
 
-def test_check(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
+@pytest.mark.unit
+@mock.patch('datadog_checks.tdd.check.MongoClient')
+def test_check(mocked_mongo_client, dd_run_check, aggregator, instance):
+    # Given
     check = TddCheck('tdd', {}, [instance])
+    # When
     dd_run_check(check)
-
+    # Then
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
-def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
+@pytest.mark.unit
+@mock.patch('datadog_checks.tdd.check.MongoClient')
+def test_emits_critical_service_check_when_service_is_down(mocked_mongo_client, dd_run_check, aggregator, instance):
+    # Given
     check = TddCheck('tdd', {}, [instance])
+    mocked_mongo_client().admin.command.side_effect = ConnectionFailure('Service not available')
+    # When
     dd_run_check(check)
+    # Then
     aggregator.assert_service_check('tdd.can_connect', TddCheck.CRITICAL)
 
 
-def test_emits_ok_service_check_when_service_is_up(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
+@pytest.mark.unit
+@mock.patch('datadog_checks.tdd.check.MongoClient')
+def test_emits_ok_service_check_when_service_is_up(mocked_mongo_client, dd_run_check, aggregator, instance):
+    # Given
     check = TddCheck('tdd', {}, [instance])
+    # When
     dd_run_check(check)
+    # Then
     aggregator.assert_service_check('tdd.can_connect', TddCheck.OK)
