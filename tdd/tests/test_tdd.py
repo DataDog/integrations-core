b119d2bebcfeecaec443f0d01ec01e980bfeb78e
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2022-08-23 15:56:00 +0200

    Removed metadata.csv dependency in unit tests

--- tdd/tests/test_tdd.py
+++ tdd/tests/test_tdd.py
@@ -2,7 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-from datadog_checks.dev.utils import get_metadata_metrics
+# from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.tdd import TddCheck
 
 
@@ -11,7 +11,7 @@ def test_check(dd_run_check, aggregator, instance):
     dd_run_check(check)
 
     aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    # aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
 def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance):
