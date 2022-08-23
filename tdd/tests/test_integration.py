731679cc4ca6593ce89838cc748f0bfaba258a8e
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2022-08-23 16:45:05 +0200

    Fixed tests

--- tdd/tests/test_integration.py
+++ tdd/tests/test_integration.py
@@ -5,14 +5,24 @@ import pytest
 
 from datadog_checks.tdd import TddCheck
 
-from .common import HOST, PORT_ERROR, standalone
+from .common import HOST, PORT, PORT_ERROR, standalone
 
 
 @standalone
 @pytest.mark.usefixtures('dd_environment')
 @pytest.mark.integration
-def test_integration_mongo_standalone_critical(dd_run_check, aggregator):
+def test_integration_mongo_standalone_critical(dd_run_check, aggregator, instance):
     instance = {'hosts': ['{}:{}'.format(HOST, PORT_ERROR)], 'username': 'testUser', 'password': 'testPass'}
     check = TddCheck('tdd', {}, [instance])
     dd_run_check(check)
     aggregator.assert_service_check('tdd.can_connect', TddCheck.CRITICAL)
+
+
+@standalone
+@pytest.mark.usefixtures('dd_environment')
+@pytest.mark.integration
+def test_integration_mongo_standalone_ok(dd_run_check, aggregator, instance):
+    instance = {'hosts': ['{}:{}'.format(HOST, PORT)], 'username': 'testUser', 'password': 'testPass'}
+    check = TddCheck('tdd', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_service_check('tdd.can_connect', TddCheck.OK)
--- tdd/tests/test_tdd.py
+++ tdd/tests/test_tdd.py
@@ -2,19 +2,29 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-# from datadog_checks.dev.utils import get_metadata_metrics
+import mock
+import pytest
+from pymongo.errors import ConnectionFailure
+
 from datadog_checks.tdd import TddCheck
 
 
-def test_check(dd_run_check, aggregator, instance):
+@pytest.mark.unit
+@mock.patch('pymongo.database.Database.command', side_effect=ConnectionFailure('Service not available'))
+def test_emits_critical_service_check_when_service_is_down(mock_command, dd_run_check, aggregator, instance):
+    # Given
     check = TddCheck('tdd', {}, [instance])
+    # When
     dd_run_check(check)
-
-    aggregator.assert_all_metrics_covered()
-    # aggregator.assert_metrics_using_metadata(get_metadata_metrics())
-
-
-def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance):
-    check = TddCheck('tdd', {}, [instance])
-    dd_run_check(check)
+    # Then
     aggregator.assert_service_check('tdd.can_connect', TddCheck.CRITICAL)
+    mock_command.assert_has_calls([mock.call('ping')])
+
+
+@pytest.mark.unit
+@mock.patch('pymongo.database.Database.command')
+def test_emits_ok_service_check_when_service_is_up(mock_command, dd_run_check, aggregator, instance):
+    check = TddCheck('tdd', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_service_check('tdd.can_connect', TddCheck.OK)
+    mock_command.assert_has_calls([mock.call('ping')])
