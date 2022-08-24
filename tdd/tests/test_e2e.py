73105a18b7c263899e1a60c5cc301001b35b8585
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2022-08-24 10:57:38 +0200

    Added new e2e test

--- tdd/tests/test_e2e.py
+++ tdd/tests/test_e2e.py
@@ -5,7 +5,7 @@ import pytest
 
 from datadog_checks.tdd import TddCheck
 
-from .common import HOST, PORT_ERROR, standalone
+from .common import HOST, PORT, PORT_ERROR, standalone
 
 
 @standalone
@@ -14,3 +14,11 @@ def test_e2e_mongo_standalone_critical(dd_agent_check):
     instance = {'hosts': ['{}:{}'.format(HOST, PORT_ERROR)], 'username': 'testUser', 'password': 'testPass'}
     aggregator = dd_agent_check(instance, rate=True)
     aggregator.assert_service_check('tdd.can_connect', status=TddCheck.CRITICAL)
+
+
+@standalone
+@pytest.mark.e2e
+def test_e2e_mongo_standalone_ok(dd_agent_check):
+    instance = {'hosts': ['{}:{}'.format(HOST, PORT)], 'username': 'testUser', 'password': 'testPass'}
+    aggregator = dd_agent_check(instance, rate=True)
+    aggregator.assert_service_check('tdd.can_connect', status=TddCheck.OK)
