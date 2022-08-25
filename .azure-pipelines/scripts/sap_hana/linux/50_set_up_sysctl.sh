e1558d9dd06e8f18175a414e3b3c159fd4570521
Julia <611228+hithwen@users.noreply.github.com>
2022-08-25 14:19:32 +0200

    Change docker image / Do not emit message on OK service check (#12826)
    * Change tested image
    * Do not submit service check message if status is OK
    * Remove redundant test
    * Apply recommended settings on sysctl
    * Rename azure script
    * Reattempt connection
    * assert a % of metrics

--- .azure-pipelines/scripts/sap_hana/linux/50_set_up_sysctl.sh
+++ .azure-pipelines/scripts/sap_hana/linux/50_set_up_sysctl.sh
@@ -7,4 +7,9 @@ set -ex
 # => To get 6GB, we multiply that amount by 12.
 sudo sysctl -w vm.max_map_count=$(expr 16 \* 65536)
 
+# Recommended settings on https://developers.sap.com/tutorials/hxe-ua-install-using-docker.html
+sudo sysctl -w fs.file-max=20000000
+sudo sysctl -w fs.aio-max-nr=262144
+sudo sysctl -w vm.memory_failure_early_kill=1
+
 set +ex
