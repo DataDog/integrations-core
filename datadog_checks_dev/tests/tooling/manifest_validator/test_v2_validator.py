71032ead77db4f9cd2c4e84d6782aaf71dcae14f
Florent Clarret <florent.clarret@datadoghq.com>
2022-08-25 14:39:53 +0200

    Validate the changelog in the manifest file

--- datadog_checks_dev/tests/tooling/manifest_validator/test_v2_validator.py
+++ datadog_checks_dev/tests/tooling/manifest_validator/test_v2_validator.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import mock
 import pytest
+from click.testing import CliRunner
 import tests.tooling.manifest_validator.input_constants as input_constants
 
 import datadog_checks.dev.tooling.manifest_validator.common.validator as common
@@ -476,3 +477,33 @@ def test_manifest_v2_tile_description_validator_invalid(setup_route):
     # Assert test case
     assert validator.result.failed, validator.result
     assert not validator.result.fixed
+
+
+def test_manifest_v2_changelog_found(setup_route):
+    manifest = JSONDict(
+        {
+            "tile": {
+                "changelog": "CHANGELOG.md",
+            },
+        }
+    )
+
+    validator = v2_validators.ChangelogValidator(version=V2)
+    validator.validate('datadog_checks_dev', manifest, False)
+
+    assert not validator.result.failed
+
+
+def test_manifest_v2_changelog_not_found(setup_route):
+    manifest = JSONDict(
+        {
+            "tile": {
+                "changelog": "CHANGELOG_NOT_FOUND.md",
+            },
+        }
+    )
+
+    validator = v2_validators.ChangelogValidator(version=V2)
+    validator.validate('datadog_checks_dev', manifest, False)
+
+    assert validator.result.failed
