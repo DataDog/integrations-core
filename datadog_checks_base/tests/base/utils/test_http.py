4827d5f36c05c799ae2990c6d54e0e34e804c79a
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-08-29 11:47:21 +0200

    Add missing import

--- datadog_checks_base/tests/base/utils/test_http.py
+++ datadog_checks_base/tests/base/utils/test_http.py
@@ -24,7 +24,7 @@ from datadog_checks.base.utils.headers import headers as agent_headers
 from datadog_checks.base.utils.http import STANDARD_FIELDS, RequestsWrapper, is_uds_url, quote_uds_url
 from datadog_checks.base.utils.time import get_timestamp
 from datadog_checks.dev import EnvVars, TempDir
-from datadog_checks.dev.ci import running_on_windows_ci
+from datadog_checks.dev.ci import running_on_ci, running_on_windows_ci
 from datadog_checks.dev.fs import read_file, write_file
 from datadog_checks.dev.http import MockResponse
 from datadog_checks.dev.utils import ON_WINDOWS
