5b321dd816bbe57745acf5c5eb8e121a983b3d09
Fanny Jiang <fanny.jiang@datadoghq.com>
2022-09-02 14:53:16 -0400

    test

--- kong/datadog_checks/kong/check.py
+++ kong/datadog_checks/kong/check.py
@@ -1,6 +1,6 @@
 # (C) Datadog, Inc. 2021-present
 # All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
+# Licensed under a 3-clause BSD style license (see LICENSE) 
 from datadog_checks.base import OpenMetricsBaseCheckV2
 
 from .metrics import METRIC_MAP
