c49d0440e278fe602c94d0087a3970e3013a08ac
Julia <611228+hithwen@users.noreply.github.com>
2022-09-02 12:40:54 +0200

    Skip all sock5 tests on ci (#12857)
    * Skip all sock5 tests on ci

--- datadog_checks_base/tests/base/utils/http/test_socks5_proxy.py
+++ datadog_checks_base/tests/base/utils/http/test_socks5_proxy.py
@@ -10,10 +10,10 @@ from datadog_checks.dev.ci import running_on_ci, running_on_windows_ci
 pytestmark = [
     pytest.mark.integration,
     pytest.mark.skipif(running_on_windows_ci(), reason='Test cannot be run on Windows CI'),
+    pytest.mark.skipif(running_on_ci(), reason='Test is failing on CI'),
 ]
 
 
-@pytest.mark.skipif(running_on_ci(), reason='Test is failing on CI')
 def test_socks5_proxy(socks5_proxy):
     instance = {'proxy': {'http': 'socks5h://{}'.format(socks5_proxy)}}
     init_config = {}
