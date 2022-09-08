88ff6c75d853261ab9ba946f93f9ea1050ffaa17
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-09-08 13:04:01 +0200

    skip sock5 tests on py2

--- datadog_checks_base/tests/base/utils/http/test_socks5_proxy.py
+++ datadog_checks_base/tests/base/utils/http/test_socks5_proxy.py
@@ -7,9 +7,11 @@ from requests.exceptions import ConnectTimeout, ProxyError
 from datadog_checks.base.utils.http import RequestsWrapper
 from datadog_checks.base.utils.platform import Platform
 from datadog_checks.dev.ci import running_on_windows_ci
+from six import PY2
 
 pytestmark = [
     pytest.mark.integration,
+    pytest.mark.skipif(PY2, reason='Test flakes on py2'),
     pytest.mark.skipif(running_on_windows_ci(), reason='Test cannot be run on Windows CI'),
 ]
 
