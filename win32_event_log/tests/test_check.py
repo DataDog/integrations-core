cebd8ab783aba67a5417a5e12b90728abf9ce5ae
Branden Clark <branden.clark@datadoghq.com>
2022-09-14 12:14:36 -0400

    fix style

--- win32_event_log/tests/test_check.py
+++ win32_event_log/tests/test_check.py
@@ -1,9 +1,10 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import logging
+
 import pytest
 import win32evtlog
-import logging
 
 from . import common
 
