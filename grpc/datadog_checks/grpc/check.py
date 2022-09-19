75b7bba8b97a87c62673a27cf41cce52437020d8
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2022-09-19 10:30:27 +0200

    Black file

--- grpc/datadog_checks/grpc/check.py
+++ grpc/datadog_checks/grpc/check.py
@@ -18,13 +18,13 @@ logger = logging.getLogger(__name__)
 class GrpcCheck(AgentCheck):
 
     # This will be the prefix of every metric and service check the integration sends
-    __NAMESPACE__ = 'grpc'
+    __NAMESPACE__ = "grpc"
 
     def __init__(self, name, init_config, instances):
         super(GrpcCheck, self).__init__(name, init_config, instances)
         host = self.instance.get("host")
         port = self.instance.get("port")
-        self.addr = '{}:{}'.format(host, port)
+        self.addr = "{}:{}".format(host, port)
         logger.debug("addr: %s", self.addr)
 
     def _get_server_metrics(self, channelz_stub, server):
@@ -33,31 +33,42 @@ class GrpcCheck(AgentCheck):
         last_socket_id = 0
 
         while not end:
-            logger.debug('Queryring sockets, server_id: %d, start_socket_id: %d', server.ref.server_id, last_socket_id)
-            sockets_stub = channelz_stub.GetServerSockets(channelz_pb2.GetServerSocketsRequest(
-                server_id=server.ref.server_id,
-                max_results=100,
-                start_socket_id=last_socket_id))
+            logger.debug(
+                "Queryring sockets, server_id: %d, start_socket_id: %d",
+                server.ref.server_id,
+                last_socket_id,
+            )
+            sockets_stub = channelz_stub.GetServerSockets(
+                channelz_pb2.GetServerSocketsRequest(
+                    server_id=server.ref.server_id,
+                    max_results=100,
+                    start_socket_id=last_socket_id,
+                )
+            )
             # [{socket_id: 17, name: "127.0.0.1:40438 -> 127.0.0.1:8888"}]
             sockets = sockets_stub.socket_ref
             num_sockets += len(sockets)
             last_socket_id = sockets[-1].socket_id
             end = sockets_stub.end
 
-        logger.debug('Processing server %r, %d connected sockets', server, num_sockets)
+        logger.debug("Processing server %r, %d connected sockets", server, num_sockets)
         server_data = server.data
 
         tags = []
         # Very likely to be empty
         # https://github.com/grpc/grpc-go/blob/aee9f0ed1722db9c45e66eab9e530e72fa2b067c/server.go#L620
-        if hasattr(server.ref, 'server_name'):
-            tags = [{'name': server.ref.server_name}]
+        if hasattr(server.ref, "server_name"):
+            tags = [{"name": server.ref.server_name}]
 
         for listening_socket in server.listen_socket:
-            tags += [f'listening_socket:{listening_socket.name}']
+            tags += [f"listening_socket:{listening_socket.name}"]
 
-        self.monotonic_count("server.calls_started", server_data.calls_started, tags=tags)
-        self.monotonic_count("server.calls_succeeded", server_data.calls_succeeded, tags=tags)
+        self.monotonic_count(
+            "server.calls_started", server_data.calls_started, tags=tags
+        )
+        self.monotonic_count(
+            "server.calls_succeeded", server_data.calls_succeeded, tags=tags
+        )
         self.monotonic_count("server.calls_failed", server_data.calls_failed, tags=tags)
         self.count("server.connected_clients", num_sockets, tags=tags)
 
@@ -77,10 +88,10 @@ class GrpcCheck(AgentCheck):
         return m.group(1)
 
     def _get_lb_policy(self, events):
-        logger.debug('Processing %d events', len(events))
+        logger.debug("Processing %d events", len(events))
         for event in events:
             # logger.debug('Checking event %s', event)
-            if 'LB policy' in event.description:
+            if "LB policy" in event.description:
                 # Checking event description: "Channel switches to new LB policy "round_robin"
                 regex = re.compile('.*LB policy "(.*)"', re.IGNORECASE)
                 m = regex.match(event.description)
@@ -94,14 +105,14 @@ class GrpcCheck(AgentCheck):
         state_value = channel_data.state.state
         state_str = channel_data.state.State.Name(state_value)
 
-        state_tags = [f'state:{state_str}'] + additional_tags
+        state_tags = [f"state:{state_str}"] + additional_tags
         if channel_data.target:
-            state_tags += [f'target:{channel_data.target}']
-        if channel_type == 'channel':
+            state_tags += [f"target:{channel_data.target}"]
+        if channel_type == "channel":
             lb_policy = self._get_lb_policy(channel_data.trace.events)
             logger.debug("lb_policy: %s", lb_policy)
             if lb_policy:
-                state_tags += [f'lb_policy:{lb_policy}']
+                state_tags += [f"lb_policy:{lb_policy}"]
 
         self.count(f"{channel_type}.state", 1, tags=state_tags)
 
@@ -109,27 +120,40 @@ class GrpcCheck(AgentCheck):
         uptime = datetime.now() - creation_time
         self.gauge(f"{channel_type}.uptime", uptime.seconds, tags=state_tags)
 
-        self.monotonic_count(f"{channel_type}.calls_started", channel_data.calls_started, tags=state_tags)
-        self.monotonic_count(f"{channel_type}.calls_succeeded", channel_data.calls_succeeded, tags=state_tags)
-        self.monotonic_count(f"{channel_type}.calls_failed", channel_data.calls_failed, tags=state_tags)
+        self.monotonic_count(
+            f"{channel_type}.calls_started", channel_data.calls_started, tags=state_tags
+        )
+        self.monotonic_count(
+            f"{channel_type}.calls_succeeded",
+            channel_data.calls_succeeded,
+            tags=state_tags,
+        )
+        self.monotonic_count(
+            f"{channel_type}.calls_failed", channel_data.calls_failed, tags=state_tags
+        )
 
     def _get_subchannel_metrics(self, channelz_stub, subchannel_id, additional_tags):
-        subchannel_response = channelz_stub.GetSubchannel(channelz_pb2.GetSubchannelRequest(
-            subchannel_id=subchannel_id))
+        subchannel_response = channelz_stub.GetSubchannel(
+            channelz_pb2.GetSubchannelRequest(subchannel_id=subchannel_id)
+        )
         subchannel = subchannel_response.subchannel
-        self._channel_data_metrics('subchannel', subchannel.data, additional_tags)
+        self._channel_data_metrics("subchannel", subchannel.data, additional_tags)
 
     def _get_channels_metrics(self, channelz_stub):
-        top_channels = channelz_stub.GetTopChannels(channelz_pb2.GetTopChannelsRequest())
+        top_channels = channelz_stub.GetTopChannels(
+            channelz_pb2.GetTopChannelsRequest()
+        )
         channels = top_channels.channel
         logger.debug("Found %d channels", len(channels))
         for channel in channels:
-            self._channel_data_metrics('channel', channel.data, [])
+            self._channel_data_metrics("channel", channel.data, [])
             channel_target = channel.data.target
-            additional_tags = [f'channel_target:{channel_target}']
+            additional_tags = [f"channel_target:{channel_target}"]
             logger.debug("Processing %d subchannel", len(channel.subchannel_ref))
             for subchannel_ref in channel.subchannel_ref:
-                self._get_subchannel_metrics(channelz_stub, subchannel_ref.subchannel_id, additional_tags)
+                self._get_subchannel_metrics(
+                    channelz_stub, subchannel_ref.subchannel_id, additional_tags
+                )
 
     def check(self, _):
         # type: (Any) -> None
