843c3b06fd8ae422d69f0a3f5a37649fc86432f7
wenzstev <steve.wenzel@datadoghq.com>
2022-10-05 17:59:37 -0400

    added metrics checks for 3.x of Kong check

--- kong/datadog_checks/kong/metrics.py
+++ kong/datadog_checks/kong/metrics.py
@@ -5,17 +5,25 @@
 # https://docs.konghq.com/hub/kong-inc/prometheus/#available-metrics
 METRIC_MAP = {
     'kong_bandwidth': 'bandwidth',
+    'kong_bandwidth_bytes': 'bandwidth.bytes',
     'kong_datastore_reachable': {
         'name': 'datastore.reachable',
         'type': 'service_check',
         'status_map': {'0': 'CRITICAL', '1': 'OK'},
     },
     'kong_http_consumer_status': 'http.consumer.status',
+    'kong_http_requests_total': 'http.requests.total',
     'kong_http_status': 'http.status',
+    'kong_kong_latency_ms': 'kong.latency.ms',
     'kong_latency': 'latency',
     'kong_memory_lua_shared_dict_bytes': 'memory.lua.shared_dict.bytes',
     'kong_memory_lua_shared_dict_total_bytes': 'memory.lua.shared_dict.total_bytes',
+    'kong_nginx_connections_total': 'nginx.connections.total',
     'kong_nginx_http_current_connections': 'nginx.http.current_connections',
     'kong_nginx_stream_current_connections': 'nginx.stream.current_connections',
+    'kong_node_info': 'node.info',
+    'kong_request_latency_ms': 'request.latency.ms',
+    'kong_session_duration_ms': 'session.duration.ms',
     'kong_stream_status': 'stream.status',
+    'kong_upstream_latency_ms': 'upstream.latency.ms',
 }
