3e20c3115fd0b4a3a93bca1c36742f4800a71ec1
Alex Lopez <alex.lopez@datadoghq.com>
2022-10-10 10:37:35 +0200

    Dummy change

--- oracle/datadog_checks/oracle/oracle.py
+++ oracle/datadog_checks/oracle/oracle.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+# Trigger PR Changes
 import itertools
 import threading
 from contextlib import closing
