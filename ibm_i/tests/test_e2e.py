cc29aed22cc9d4e8d202417f8c05d94542666851
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2022-10-13 10:38:53 +0200

    Implement feedback

--- ibm_i/tests/test_e2e.py
+++ ibm_i/tests/test_e2e.py
@@ -1,11 +1,9 @@
 import pytest
 
-from datadog_checks.dev.testing import requires_py3
 from datadog_checks.ibm_i import IbmICheck
 
 
 @pytest.mark.e2e
-@requires_py3
 def test_e2e(dd_agent_check, aggregator, instance):
     aggregator = dd_agent_check(instance)
-    aggregator.assert_service_check('ibm_i', IbmCheck.CRITICAL)
+    aggregator.assert_service_check('ibm_i', IbmICheck.CRITICAL)
