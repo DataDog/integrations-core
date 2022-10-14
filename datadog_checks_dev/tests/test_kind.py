112f96ddfb8cf03906ffc2c25500f862ae7ee7f8
Florent Clarret <florent.clarret@datadoghq.com>
2022-10-14 10:30:30 +0200

    fix import

--- datadog_checks_dev/tests/test_kind.py
+++ datadog_checks_dev/tests/test_kind.py
@@ -5,11 +5,12 @@
 import mock
 import pytest
 import tenacity
-from common import not_windows_ci
 
 from datadog_checks.dev import RetryError
 from datadog_checks.dev.ci import running_on_ci
 from datadog_checks.dev.kind import kind_run
+
+from .common import not_windows_ci
 
 
 class TestKindRun:
