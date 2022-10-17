707021958e271d1bbed52daea35f98e5e645e150
Julia <611228+hithwen@users.noreply.github.com>
2022-10-17 12:10:26 +0200

    Update network/tests/test_windows.py

--- network/tests/test_windows.py
+++ network/tests/test_windows.py
@@ -14,6 +14,6 @@ if PY3:
 @mock.patch('datadog_checks.network.network.Platform.is_bsd', return_value=False)
 @mock.patch('datadog_checks.network.network.Platform.is_solaris', return_value=False)
 @mock.patch('datadog_checks.network.network.Platform.is_windows', return_value=True)
-def test_win_uses_psutil(is_linux, is_bsd, is_solaris, is_windows, check):
+def test_creates_windows_instance(is_linux, is_bsd, is_solaris, is_windows, check):
     check_instance = check({})
     isinstance(check_instance, WindowsNetwork)
