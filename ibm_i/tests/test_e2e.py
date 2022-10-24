ac25691d2de415b44f1fcde4f2839c86d8b7fe43
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2022-10-24 16:53:57 +0200

    get the test to pass

--- ibm_i/tests/test_e2e.py
+++ ibm_i/tests/test_e2e.py
@@ -6,4 +6,4 @@ from datadog_checks.ibm_i import IbmICheck
 @pytest.mark.e2e
 def test_e2e(dd_agent_check, aggregator, instance):
     aggregator = dd_agent_check(instance)
-    aggregator.assert_service_check('ibm_i', IbmICheck.CRITICAL)
+    aggregator.assert_service_check("ibm_i.can_connect", IbmICheck.CRITICAL)
