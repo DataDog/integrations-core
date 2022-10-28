ad275de56ee3bb0ff892450e9e513da0566ec76a
steveny91 <steven.yuen@datadoghq.com>
2022-10-28 14:33:41 -0400

    clean up function names

--- argocd/datadog_checks/argocd/check.py
+++ argocd/datadog_checks/argocd/check.py
@@ -27,10 +27,10 @@ class ArgocdCheck(OpenMetricsBaseCheckV2, ConfigMixin):
                 "for more information."
             )
         super(ArgocdCheck, self).__init__(name, init_config, instances)
-        self.check_initializations.appendleft(self._parse_config)
+        self.check_initializations.appendleft(self.parse_config)
         self.check_initializations.append(self.configure_additional_transformers)
 
-    def _parse_config(self):
+    def parse_config(self):
         self.scraper_configs = []
         app_controller_endpoint = self.instance.get("app_controller_endpoint")
         api_server_endpoint = self.instance.get("api_server_endpoint")
@@ -43,18 +43,18 @@ class ArgocdCheck(OpenMetricsBaseCheckV2, ConfigMixin):
 
         if app_controller_endpoint:
             self.scraper_configs.append(
-                self._generate_config(app_controller_endpoint, APP_CONTROLLER_NAMESPACE, APPLICATION_CONTROLLER_METRICS)
+                self.generate_config(app_controller_endpoint, APP_CONTROLLER_NAMESPACE, APPLICATION_CONTROLLER_METRICS)
             )
         if api_server_endpoint:
             self.scraper_configs.append(
-                self._generate_config(api_server_endpoint, API_SERVER_NAMESPACE, API_SERVER_METRICS)
+                self.generate_config(api_server_endpoint, API_SERVER_NAMESPACE, API_SERVER_METRICS)
             )
         if repo_server_endpoint:
             self.scraper_configs.append(
-                self._generate_config(repo_server_endpoint, REPO_SERVER_NAMESPACE, REPO_SERVER_METRICS)
+                self.generate_config(repo_server_endpoint, REPO_SERVER_NAMESPACE, REPO_SERVER_METRICS)
             )
 
-    def _generate_config(self, endpoint, namespace, metrics):
+    def generate_config(self, endpoint, namespace, metrics):
         config = {
             'openmetrics_endpoint': endpoint,
             'metrics': metrics,
@@ -68,7 +68,7 @@ class ArgocdCheck(OpenMetricsBaseCheckV2, ConfigMixin):
         # ArgoCD exposes a similar metric `go_memstats_alloc_bytes_total` as a counter
         # which would also be collected under the same name and could cause inaccuracies
         # in the metric value
-        def go_memstats_alloc_bytes_transformer(metric, sample_data, runtime_data):
+        def go_memstats_alloc_bytes_transformer(_metric, sample_data, _runtime_data):
             for sample, tags, hostname in sample_data:
                 self.gauge(metric_name, sample.value, tags=tags, hostname=hostname)
 
@@ -82,7 +82,7 @@ class ArgocdCheck(OpenMetricsBaseCheckV2, ConfigMixin):
         status_map = {1: ServiceCheck.OK, 0: ServiceCheck.CRITICAL}
         service_check_method = self.service_check
 
-        def argocd_cluster_connection_status_transformer(metric, sample_data, runtime_data):
+        def argocd_cluster_connection_status_transformer(_metric, sample_data, _runtime_data):
             for sample, tags, hostname in sample_data:
                 service_check_method(
                     metric_name, status_map.get(int(sample.value), ServiceCheck.UNKNOWN), hostname=hostname, tags=tags
