f18ab006052fcb61219410a1eb1ef04644fca42f
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-11-02 13:29:24 -0400

    Outline plan for integration

---
+++ cloudera/datadog_checks/cloudera/client.py
@@ -0,0 +1,19 @@
+import cm_client
+
+
+class ClouderaClient:
+    def __init__(self, username, password, api_url):
+        cm_client.configuration.username = username
+        cm_client.configuration.password = password
+        self.api_client = cm_client.ApiClient(api_url)
+
+    def query(self, cloudera_api):
+        # api_instance = cm_client.cloudera_api(self.api_client)
+
+        raise NotImplemented
+
+    def query_time_series(self, query, from_time, to_time):
+        api_instance = cm_client.TimeSeriesResourceApi(self.api_client)
+        result = api_instance.query_time_series(_from=from_time, query=query, to=to_time)
+
+        raise NotImplemented
---
+++ cloudera/datadog_checks/cloudera/client_factory.py
@@ -0,0 +1,12 @@
+from .cloudera.client import ClouderaClient
+
+
+def make_api_client(check, instance):
+    workload_username = instance.get("workload_username")
+    workload_password = instance.get("workload_password")
+    api_url = instance.get("api_url")
+
+    try:
+        return ClouderaClient(username=workload_username, password=workload_password, api_url=api_url), None
+    except Exception as e:
+        return None, str(e)
