c2115ca89c95ee0e5f1ef798a7629857841dfe43
JoshPatel13 <joshptl313@gmail.com>
2022-11-04 16:12:39 -0400

    Testing new uninstallation check and removed overview from Active Directory readme to make sure there is consistency between missing overview and uninstallation in check

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/readmes.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/readmes.py
@@ -110,6 +110,11 @@ def validate_readme(integration, repo, display_queue, files_failed, readme_count
         files_failed[readme_path] = True
         display_queue.append((echo_failure, "     readme is missing a Support H2 (##) section"))
 
+    if "Uninstallation" not in h2s:
+        files_failed[readme_path] = True
+        display_queue.append((echo_failure, "     readme is missing a Uninstallation H2 (##) section"))
+
+
     # Check all referenced images are in the `images` folder and that
     # they use the `raw.githubusercontent` format or relative paths to the `images` folder
     allow_relative = False
