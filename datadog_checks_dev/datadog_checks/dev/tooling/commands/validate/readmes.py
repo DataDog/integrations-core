0ea7b6cf2a680b829f53ae72f93e2725c5ba0f8b
JoshPatel13 <joshptl313@gmail.com>
2022-11-07 09:29:58 -0500

    Testing validation

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/readmes.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/readmes.py
@@ -110,7 +110,7 @@ def validate_readme(integration, repo, display_queue, files_failed, readme_count
         files_failed[readme_path] = True
         display_queue.append((echo_failure, "     readme is missing a Support H2 (##) section"))
     
-    if "Uninstallation" not in h2s and repo == 'marketplace':
+    if "Uninstallation" not in h2s:
         files_failed[readme_path] = True
         display_queue.append((echo_failure, "     readme is missing a Uninstallation H2 (##) section"))
 
