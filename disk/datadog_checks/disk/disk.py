a2ff32014f2a2de7ef4b8b2ca4466371bb3f5ffb
Julia <611228+hithwen@users.noreply.github.com>
2022-11-07 10:58:35 +0100

    Extrat tag calculation to a separate method (#13245)

--- disk/datadog_checks/disk/disk.py
+++ disk/datadog_checks/disk/disk.py
@@ -148,27 +148,7 @@ class Disk(AgentCheck):
 
             self.log.debug('Passed: %s', part.device)
 
-            device_name = part.mountpoint if self._use_mount else part.device
-
-            tags = [part.fstype, 'filesystem:{}'.format(part.fstype)] if self._tag_by_filesystem else []
-            tags.extend(self._custom_tags)
-
-            # apply device/mountpoint specific tags
-            for regex, device_tags in self._device_tag_re:
-                if regex.match(device_name):
-                    tags.extend(device_tags)
-
-            # apply device labels as tags (from blkid or lsblk).
-            # we want to use the real device name and not the device_name (which can be the mountpoint)
-            if self.devices_label.get(part.device):
-                tags.extend(self.devices_label.get(part.device))
-
-            # legacy check names c: vs psutil name C:\\
-            if Platform.is_win32():
-                device_name = device_name.strip('\\').lower()
-
-            tags.append('device:{}'.format(device_name))
-            tags.append('device_name:{}'.format(_base_device_name(part.device)))
+            tags = self._get_tags(part)
             for metric_name, metric_value in iteritems(self._collect_part_metrics(part, disk_usage)):
                 self.gauge(metric_name, metric_value, tags=tags)
 
@@ -183,6 +163,29 @@ class Disk(AgentCheck):
                     self.service_check('disk.read_write', AgentCheck.UNKNOWN, tags=tags)
 
         self.collect_latency_metrics()
+
+    def _get_tags(self, part):
+        device_name = part.mountpoint if self._use_mount else part.device
+        tags = [part.fstype, 'filesystem:{}'.format(part.fstype)] if self._tag_by_filesystem else []
+        tags.extend(self._custom_tags)
+
+        # apply device/mountpoint specific tags
+        for regex, device_tags in self._device_tag_re:
+            if regex.match(device_name):
+                tags.extend(device_tags)
+
+        # apply device labels as tags (from blkid or lsblk).
+        # we want to use the real device name and not the device_name (which can be the mountpoint)
+        if self.devices_label.get(part.device):
+            tags.extend(self.devices_label.get(part.device))
+
+        # legacy check names c: vs psutil name C:\\
+        if Platform.is_win32():
+            device_name = device_name.strip('\\').lower()
+
+        tags.append('device:{}'.format(device_name))
+        tags.append('device_name:{}'.format(_base_device_name(part.device)))
+        return tags
 
     def exclude_disk(self, part):
         # skip cd-rom drives with no disk in it; they may raise
