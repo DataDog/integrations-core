43f97010dabe9c3f2a07b621fc3314b78eef9e44
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2022-11-09 15:28:22 +0100

    Added new unit tests

--- sonarqube/tests/test_unit_projects.py
+++ sonarqube/tests/test_unit_projects.py
@@ -412,6 +412,83 @@ def test_projects_discovery(mock_api, aggregator, dd_run_check, sonarqube_check)
     )
     for metric, value in metrics_with_values:
         aggregator.assert_metric(
-            f'sonarqube.{metric}', value=value, tags=['endpoint:http://localhost:9000', 'component:project1']
+            f'sonarqube.{metric}',
+            value=value,
+            tags=['endpoint:http://localhost:9000', 'component:project1'],
+        )
+        aggregator.assert_metric(
+            f'sonarqube.{metric}',
+            value=value,
+            tags=['endpoint:http://localhost:9000', 'component:project2'],
+        )
+    aggregator.assert_service_check('sonarqube.api_access', status=check.OK, tags=['endpoint:http://localhost:9000'])
+
+
+@mock.patch("datadog_checks.sonarqube.check.Api")
+def test_projects_discovery_with_include(mock_api, aggregator, dd_run_check, sonarqube_check):
+    # Given
+    config = {
+        'web_endpoint': 'http://{}:{}'.format(HOST, PORT),
+        'projects': {'discovery': {'include': ['^project.*']}},
+    }
+    mock_api.return_value.get_projects.return_value = ['project1', 'project2', 'tmp_project1']
+    mock_api.return_value.get_metrics.return_value = [metric for metric in METRICS]
+    excluded_metrics = [metric for metric in METRICS if re.search('^.*\\.new_.*', metric)]
+    included_metrics = [metric for metric in METRICS if metric not in excluded_metrics]
+    metrics_with_values = [(metric, randrange(0, 100)) for metric in included_metrics]
+    measures = [(f"{metric.split('.')[1]}", value) for metric, value in metrics_with_values]
+    mock_api.return_value.get_measures.return_value = measures
+    check = sonarqube_check(config)
+    # When
+    dd_run_check(check)
+    # Then
+    mock_api.return_value.get_measures.assert_has_calls(
+        [
+            mock.call('project1', [f"{metric.split('.')[1]}" for metric, _ in metrics_with_values]),
+            mock.call('project2', [f"{metric.split('.')[1]}" for metric, _ in metrics_with_values]),
+        ]
+    )
+    for metric, value in metrics_with_values:
+        aggregator.assert_metric(
+            f'sonarqube.{metric}',
+            value=value,
+            tags=['endpoint:http://localhost:9000', 'component:project1'],
+        )
+        aggregator.assert_metric(
+            f'sonarqube.{metric}',
+            value=value,
+            tags=['endpoint:http://localhost:9000', 'component:project2'],
+        )
+    aggregator.assert_service_check('sonarqube.api_access', status=check.OK, tags=['endpoint:http://localhost:9000'])
+
+
+@mock.patch("datadog_checks.sonarqube.check.Api")
+def test_projects_discovery_with_include_and_exclude(mock_api, aggregator, dd_run_check, sonarqube_check):
+    # Given
+    config = {
+        'web_endpoint': 'http://{}:{}'.format(HOST, PORT),
+        'projects': {'discovery': {'include': ['^project1.*'], 'exclude': ['^tmp_.*']}},
+    }
+    mock_api.return_value.get_projects.return_value = ['project1', 'project2', 'tmp_project1']
+    mock_api.return_value.get_metrics.return_value = [metric for metric in METRICS]
+    excluded_metrics = [metric for metric in METRICS if re.search('^.*\\.new_.*', metric)]
+    included_metrics = [metric for metric in METRICS if metric not in excluded_metrics]
+    metrics_with_values = [(metric, randrange(0, 100)) for metric in included_metrics]
+    measures = [(f"{metric.split('.')[1]}", value) for metric, value in metrics_with_values]
+    mock_api.return_value.get_measures.return_value = measures
+    check = sonarqube_check(config)
+    # When
+    dd_run_check(check)
+    # Then
+    mock_api.return_value.get_measures.assert_has_calls(
+        [
+            mock.call('project1', [f"{metric.split('.')[1]}" for metric, _ in metrics_with_values]),
+        ]
+    )
+    for metric, value in metrics_with_values:
+        aggregator.assert_metric(
+            f'sonarqube.{metric}',
+            value=value,
+            tags=['endpoint:http://localhost:9000', 'component:project1'],
         )
     aggregator.assert_service_check('sonarqube.api_access', status=check.OK, tags=['endpoint:http://localhost:9000'])
