2a145ff57793e9eb09e9f5cebfff5510153f7546
Julia Simon <611228+hithwen@users.noreply.github.com>
2022-11-15 13:29:08 +0100

    Add BSD integration test

--- network/tests/test_integration.py
+++ network/tests/test_integration.py
@@ -5,6 +5,7 @@ import platform
 
 import pytest
 
+from datadog_checks.network.check_bsd import BSDNetwork
 from . import common
 
 pytestmark = pytest.mark.integration
@@ -19,11 +20,20 @@ def test_check(aggregator, check, instance):
         aggregator.assert_metric(metric)
 
 
-@pytest.mark.skipif(platform.system() != 'Linux', reason="Only runs on Unix systems")
+@pytest.mark.skipif(platform.system() != 'Linux', reason="Only runs on Linux systems")
 @pytest.mark.usefixtures("dd_environment")
 def test_check_linux(aggregator, check, instance_blacklist):
     check_instance = check(instance_blacklist)
     check_instance.check({})
 
     for metric in common.CONNTRACK_METRICS:
+        aggregator.assert_metric(metric)
+
+
+@pytest.mark.skipif(platform.system() == 'Windows', reason="Only runs on Unix systems")
+@pytest.mark.usefixtures("dd_environment")
+def test_check_bsd(instance, aggregator):
+    check = BSDNetwork('network', {}, [instance])
+    check.check({})
+    for metric in common.EXPECTED_METRICS:
         aggregator.assert_metric(metric)
