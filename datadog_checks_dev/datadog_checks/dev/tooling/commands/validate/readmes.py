1e28fea1bf6de0353ff12e325d7471db4e9cc524
JoshPatel13 <joshptl313@gmail.com>
2022-11-16 15:57:01 -0500

    testing

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/readmes.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/readmes.py
@@ -116,7 +116,9 @@ def validate_readme(integration, repo, display_queue, files_failed, readme_count
     else:
         current = soup.find('h2', text='Uninstallation')
         check = current.find_next("p")
+        print(check)
         if not check:
+            files_failed[readme_path] = True
             display_queue.append((echo_failure, "     readme has an empty Uninstallation H2 (##) section"))
 
     # Check all referenced images are in the `images` folder and that
