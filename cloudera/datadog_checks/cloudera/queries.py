e25bcd52515118398e750c9316326ace8b60398c
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-11-21 16:05:18 -0500

    Add more metrics

--- cloudera/datadog_checks/cloudera/queries.py
+++ cloudera/datadog_checks/cloudera/queries.py
@@ -19,13 +19,91 @@ TIMESERIES_QUERIES = [
     },
     {
         'query_string': 'select last(cpu_system_rate)',
-        'tags': [  # TODO: Add more tags
+        'tags': [
             ('cloudera_hostname', 'hostname'),
             ('entity_name', 'entityName'),
             ('host_id', 'hostId'),
-            ('rack_id', 'rackId')
+            ('rack_id', 'rackId'),
+            ('cluster_display_name', 'clusterDisplayName'),
         ],
         'metric_name': 'cpu_system_rate'
     },
+    {
+        'query_string': 'select last(load_1)',
+        'tags': [
+            ('cloudera_hostname', 'hostname'),
+            ('entity_name', 'entityName'),
+            ('host_id', 'hostId'),
+            ('rack_id', 'rackId'),
+            ('cluster_display_name', 'clusterDisplayName'),
+        ],
+        'metric_name': 'load_1'
+    },
+    {
+        'query_string': 'select last(physical_memory_cached)',
+        'tags': [
+            ('cloudera_hostname', 'hostname'),
+            ('entity_name', 'entityName'),
+            ('host_id', 'hostId'),
+            ('rack_id', 'rackId'),
+            ('cluster_display_name', 'clusterDisplayName'),
+        ],
+        'metric_name': 'physical_memory_cached'
+    },
+    {
+        'query_string': 'select last(mem_rss)',
+        'tags': [
+            ('cloudera_hostname', 'hostname'),
+            ('entity_name', 'entityName'),
+            ('host_id', 'hostId'),
+            ('rack_id', 'rackId'),
+            ('cluster_display_name', 'clusterDisplayName'),
+            ('role_type', 'roleType'),
+            ('service_display_name', 'serviceDisplayName'),
+        ],
+        'metric_name': 'mem_rss'
+    },
+    {
+        'query_string': 'select last(swap_out_rate)',
+        'tags': [
+            ('cloudera_hostname', 'hostname'),
+            ('entity_name', 'entityName'),
+            ('host_id', 'hostId'),
+            ('rack_id', 'rackId'),
+            ('cluster_display_name', 'clusterDisplayName'),
+            ('role_type', 'roleType'),
+            ('service_display_name', 'serviceDisplayName'),
+        ],
+        'metric_name': 'swap_out_rate'
+    },
+    {
+        'query_string': 'select last(total_bytes_receive_rate_across_network_interfaces)',
+        'tags': [
+            ('cloudera_hostname', 'hostname'),
+            ('entity_name', 'entityName'),
+            ('host_id', 'hostId'),
+            ('rack_id', 'rackId'),
+            ('cluster_display_name', 'clusterDisplayName'),
+            ('role_type', 'roleType'),
+            ('service_display_name', 'serviceDisplayName'),
+            ('role_config_group', 'roleConfigGroup'),
+        ],
+        'metric_name': 'total_bytes_receive_rate_across_network_interfaces'
+    },
+    {
+        'query_string': 'select last(await_time)',
+        'tags': [
+            ('cloudera_hostname', 'hostname'),
+            ('entity_name', 'entityName'),
+            ('host_id', 'hostId'),
+            ('rack_id', 'rackId'),
+            ('cluster_display_name', 'clusterDisplayName'),
+            ('role_type', 'roleType'),
+            ('service_display_name', 'serviceDisplayName'),
+        ],
+        'metric_name': 'await_time'
+    }
+
+
 ]
 
