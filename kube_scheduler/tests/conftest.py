dfb4b98d49357f085440aab3790dac5ea808fda1
Kyle Neale <kyle.neale@datadoghq.com>
2022-11-29 17:41:08 -0500

    minimal e2e test kube_scheduler

---
+++ kube_scheduler/tests/conftest.py
@@ -0,0 +1,22 @@
+# (C) Datadog, Inc. 2019-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from copy import deepcopy
+
+import pytest
+
+INSTANCE = {
+    'prometheus_url': 'http://localhost:10251/metrics',
+    'send_histograms_buckets': True,
+    'tags': ['custom:tag'],
+}
+
+
+@pytest.fixture(scope='session')
+def dd_environment():
+    yield deepcopy(INSTANCE)
+
+
+@pytest.fixture
+def instance():
+    return deepcopy(INSTANCE)
