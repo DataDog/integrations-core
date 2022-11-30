301848a5d078e5f06ace7b8f2ce3d1825f23578e
alexbarksdale <alx.barksdale@gmail.com>
2022-11-30 23:13:00 +0000

    Docstring on how the workaround works

--- postgres/datadog_checks/postgres/explain_parameterized_queries.py
+++ postgres/datadog_checks/postgres/explain_parameterized_queries.py
@@ -19,12 +19,37 @@ SELECT parameter_types FROM pg_prepared_statements WHERE name = 'dd_{query_signa
 
 EXECUTE_PREPARED_STATEMENT_QUERY = 'EXECUTE dd_{prepared_statement}({null_parameter})'
 
-# TODO: Get from statement_samples file
 EXPLAIN_QUERY = 'SELECT {explain_function}($stmt${statement}$stmt$)'
 
 
 class ExplainParameterizedQueries:
-    # TODO: Docstring on how this works
+    '''
+    ExplainParameterizedQueries will attempt to use a workaround to explain a parameterized query.
+
+    High-level explanation:
+        Given the query: SELECT * FROM products WHERE id = $1;
+
+        We're unable to explain this because we do not know the value of the parameter ($1). Attempting to explain
+        this will result in an error. We could provide `null` as a value because it works with any datatype, but
+        that also does not work because Postgres knows that no rows will be returned.
+            e.g. EXPLAIN SELECT * FROM products WHERE id = null;
+
+        However, with Postgres versions 12 and above, you can control how the query planner behaves with the `plan_cache_mode`.
+        The mode `force_generic_plan` will force Postgres to produce a generic plan.
+
+        We're still faced with the problem of not knowing how many parameters there are in a query. So is there a clever way
+        we can go about finding this information? Yes, if we create a prepared statement, the `pg_prepared_statements`
+        table provides information about a query's parameters. More specifically, the type and how many parameters there are.
+
+        The idea is to create a prepared statement for a query and explain the prepared statement with generic values.
+
+        Walkthrough:
+        1. Set the plan cache mode: SET plan_cache_mode = force_generic_plan;
+        2. Create a prepared statement: PREPARE dd_products AS SELECT * FROM products WHERE id = $1;
+        3. Execute and explain: EXPLAIN EXECUTE dd_products(null);
+            Returns: (plan)
+    '''
+
     def __init__(self, check, config):
         self._check = check
         self._config = config
@@ -73,7 +98,6 @@ class ExplainParameterizedQueries:
             return False
 
     def _create_prepared_statement(self, dbname, statement, obfuscated_statement, query_signature):
-        # TODO: Is there an 'ON CONFLICT DO NOTHING' equivalent for prepared statements so we can avoid this?
         if self._prepared_statement_exists(dbname, statement, obfuscated_statement, query_signature):
             return True
         try:
