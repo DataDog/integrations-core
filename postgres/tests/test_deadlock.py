0a9aa983608b1fb7e66873d7facd7dbb77f565ff
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2022-11-30 13:39:38 +0000

    DBM-790 styling corrections

--- postgres/tests/test_deadlock.py
+++ postgres/tests/test_deadlock.py
@@ -8,22 +8,22 @@ import pytest
 import select
 import time
 
-import sys
-
 from .common import DB_NAME, HOST, PORT, POSTGRES_VERSION
 
-def wait_on_result(cursor = None, sql = None, binds = None, expected_value = None):
-    for i in range (100):
+
+def wait_on_result(cursor=None, sql=None, binds=None, expected_value=None):
+    for _i in range (100):
         cursor.execute(sql, binds)
         result = cursor.fetchone()[0]
-        if ( result == expected_value ):
+        if (result == expected_value):
             break
 
         time.sleep(.1)
     else:
         return False
-    
+
     return True
+
 
 @pytest.mark.e2e
 @pytest.mark.skipif(
@@ -48,9 +48,9 @@ def test_deadlock(aggregator, dd_run_check, integration_check, pg_instance):
                 raise psycopg2.OperationalError("poll() returned %s" % state)
             time.sleep(0.1)
 
-    conn_args = { 'host': HOST, 'dbname': DB_NAME, 'user': "bob", 'password': "bob"}
+    conn_args = {'host': HOST, 'dbname': DB_NAME, 'user': "bob", 'password': "bob"}
     conn_args_async = copy.copy(conn_args)
-    conn_args_async["async_"]=1
+    conn_args_async["async_"] = 1
     conn1 = psycopg2.connect(**conn_args)
     conn1.autocommit = False
 
@@ -61,7 +61,7 @@ def test_deadlock(aggregator, dd_run_check, integration_check, pg_instance):
     appname1 = appname + '1'
     appname2 = appname + '2'
     appname_sql = "SET application_name=%s"
-    update_sql =  "update personsdup1 set address = 'changed' where personid = %s"
+    update_sql = "update personsdup1 set address = 'changed' where personid = %s"
 
     deadlock_count_sql = "select deadlocks from pg_stat_database where datname = %s"
     cursor.execute(deadlock_count_sql, (DB_NAME, ))
@@ -69,7 +69,7 @@ def test_deadlock(aggregator, dd_run_check, integration_check, pg_instance):
 
     cur1 = conn1.cursor()
     cur1.execute(appname_sql, (appname1,))
-    cur1.execute(update_sql, (1,) )
+    cur1.execute(update_sql, (1,))
 
     cur2 = conn2.cursor()
     cur2.execute("""SET application_name=%s;
@@ -82,7 +82,7 @@ commit;
     lock_count_sql = """SELECT COUNT(1)
    FROM  pg_catalog.pg_locks         blocked_locks
     JOIN pg_catalog.pg_stat_activity blocked_activity  ON blocked_activity.pid = blocked_locks.pid
-    JOIN pg_catalog.pg_locks         blocking_locks 
+    JOIN pg_catalog.pg_locks         blocking_locks
         ON blocking_locks.locktype = blocked_locks.locktype
         AND blocking_locks.DATABASE IS NOT DISTINCT FROM blocked_locks.DATABASE
         AND blocking_locks.relation IS NOT DISTINCT FROM blocked_locks.relation
@@ -95,12 +95,12 @@ commit;
         AND blocking_locks.objsubid IS NOT DISTINCT FROM blocked_locks.objsubid
         AND blocking_locks.pid != blocked_locks.pid
     JOIN pg_catalog.pg_stat_activity blocking_activity ON blocking_activity.pid = blocking_locks.pid
-   WHERE NOT blocked_locks.GRANTED 
-   	AND blocking_activity.application_name = %s 
-   	AND blocked_activity.application_name = %s """
+   WHERE NOT blocked_locks.GRANTED
+   	AND blocking_activity.application_name = %s
+    AND blocked_activity.application_name = %s """
 
     is_locked = wait_on_result(
-        cursor = cursor, sql = lock_count_sql, binds = (appname1, appname2), 
+        cursor=cursor, sql=lock_count_sql, binds=(appname1, appname2),
         expected_value=1)
 
     if not is_locked:
@@ -115,7 +115,9 @@ commit;
     dd_run_check(check)
 
     wait_on_result(
-        cursor = cursor,sql=deadlock_count_sql,binds=(DB_NAME,),
-        expected_value = deadlocks_before + 1)
+        cursor=cursor, sql=deadlock_count_sql, binds=(DB_NAME,),
+        expected_value=deadlocks_before + 1)
 
-    aggregator.assert_metric('postgresql.deadlocks.count',value=deadlocks_before+1,tags = pg_instance["tags"]+["db:{}".format(DB_NAME),"port:{}".format(PORT)],hostname='stubbed.hostname')
+    aggregator.assert_metric(
+        'postgresql.deadlocks.count', value=deadlocks_before + 1,
+        tags=pg_instance["tags"]+["db:{}".format(DB_NAME), "port:{}".format(PORT)])
