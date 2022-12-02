a0e5f5ffdc94b7b2e76183001e31a61c8d32d8ff
alexbarksdale <alx.barksdale@gmail.com>
2022-12-02 21:27:31 +0000

    Add test for generic params

--- postgres/tests/test_explain_parameterized_queries.py
+++ postgres/tests/test_explain_parameterized_queries.py
@@ -24,20 +24,17 @@ def dbm_instance(pg_instance):
 
 
 @pytest.mark.parametrize(
-    "query,expected_explain_err_code,expected_generic_values",
+    "query,expected_explain_err_code",
     [
-        ("SELECT * FROM pg_settings WHERE name = $1", DBExplainError.explained_with_prepared_statement, 1),
+        ("SELECT * FROM pg_settings WHERE name = $1", DBExplainError.explained_with_prepared_statement),
         (
             "SELECT * FROM pg_settings WHERE name = $1 AND "
             "context = (SELECT context FROM pg_settings WHERE vartype = $2) AND source = $3",
             DBExplainError.explained_with_prepared_statement,
-            3,
         ),
     ],
 )
-def test_explain_parameterized_queries(
-    integration_check, dbm_instance, aggregator, query, expected_explain_err_code, expected_generic_values
-):
+def test_explain_parameterized_queries(integration_check, dbm_instance, aggregator, query, expected_explain_err_code):
     check = integration_check(dbm_instance)
     check._connect()
 
@@ -59,3 +56,33 @@ def test_explain_parameterized_queries(
         ),
     )
     assert len(rows) == 0
+
+
+@pytest.mark.parametrize(
+    "query,expected_generic_values",
+    [
+        ("SELECT * FROM pg_settings WHERE name = $1", 1),
+        (
+            "SELECT * FROM pg_settings WHERE name = $1 AND "
+            "context = (SELECT context FROM pg_settings WHERE vartype = $2) AND source = $3",
+            3,
+        ),
+    ],
+)
+def test_explain_parameterized_queries_generic_params(
+    integration_check, dbm_instance, aggregator, query, expected_generic_values
+):
+    check = integration_check(dbm_instance)
+    check._connect()
+
+    check.check(dbm_instance)
+    if check.version < V12:
+        return
+
+    query_signature = compute_sql_signature(query)
+
+    explain_param_queries = check.statement_samples._explain_parameterized_queries
+    assert explain_param_queries._create_prepared_statement(DB_NAME, query, query, query_signature) is True
+    assert expected_generic_values == explain_param_queries._get_number_of_parameters_for_prepared_statement(
+        DB_NAME, query_signature
+    )
