8de24cc4f51a8d72e82eeda6715ab71eda6e441f
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-12-02 14:49:38 -0500

    Remove unused constants

--- cloudera/datadog_checks/cloudera/common.py
+++ cloudera/datadog_checks/cloudera/common.py
@@ -1,6 +1,3 @@
 CAN_CONNECT = "can_connect"
 CLUSTER_HEALTH = "cluster.health"
-ROLE_HEALTH = "role.health"
 HOST_HEALTH = "host.health"
-
-# mock all of the methods and classes of the cloudera client
