b3ca60060298165c74ab8d7ad3c987262e6ec4f8
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-12-07 15:27:52 -0500

    Fix e2e test

--- cloudera/tests/test_e2e.py
+++ cloudera/tests/test_e2e.py
@@ -2,6 +2,7 @@ import pytest
 
 from datadog_checks.cloudera import ClouderaCheck
 from datadog_checks.cloudera.metrics import METRICS
+from datadog_checks.dev.utils import get_metadata_metrics
 
 
 @pytest.mark.e2e
@@ -17,3 +18,5 @@ def test_e2e(dd_agent_check, instance):
     # caddy test env is supposed to be in BAD_HEALTH
     aggregator.assert_service_check('cloudera.cluster.health', ClouderaCheck.CRITICAL, message="BAD_HEALTH")
     aggregator.assert_service_check('cloudera.host.health', ClouderaCheck.OK)
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
