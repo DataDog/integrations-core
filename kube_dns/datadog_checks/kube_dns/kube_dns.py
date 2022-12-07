af9ccbc81cb0dca1d0388164c8510396a7c985c5
khewonc <39867936+khewonc@users.noreply.github.com>
2022-12-07 15:44:53 -0500

    Add health check to kube_* integrations (#10668)
    * Add health checks for kube_* integrations
    
    * Add health_url to config specs
    
    * Sync config models using ddev
    
    * Fix example conf files
    
    * Fix example confs again
    
    * Run ddev validate models -s again
    
    * ddev validate config/models
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Steven Yuen <steven.yuen@datadoghq.com>
    
    Co-authored-by: Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>
    Co-authored-by: Steven Yuen <steven.yuen@datadoghq.com>

--- kube_dns/datadog_checks/kube_dns/kube_dns.py
+++ kube_dns/datadog_checks/kube_dns/kube_dns.py
@@ -2,11 +2,15 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
+import re
 from copy import deepcopy
 
+import requests
 from six import iteritems
 
+from datadog_checks.base import AgentCheck
 from datadog_checks.base.checks.openmetrics import OpenMetricsBaseCheck
+from datadog_checks.base.utils.http import RequestsWrapper
 
 
 class KubeDNSCheck(OpenMetricsBaseCheck):
@@ -37,10 +41,22 @@ class KubeDNSCheck(OpenMetricsBaseCheck):
 
         super(KubeDNSCheck, self).__init__(name, init_config, instances=generic_instances)
 
+        if instances is not None:
+            for instance in instances:
+                url = instance.get('health_url')
+                prometheus_endpoint = instance.get('prometheus_endpoint')
+
+                if url is None and re.search(r':[0-9]+/metrics$', prometheus_endpoint):
+                    url = re.sub(r':[0-9]+/metrics$', ':8081/readiness', prometheus_endpoint)
+
+                instance['health_url'] = url
+
     def check(self, instance):
         endpoint = instance.get('prometheus_endpoint')
         scraper_config = self.config_map[endpoint]
         self.process(scraper_config, metric_transformers=self.METRIC_TRANSFORMERS)
+
+        self._perform_service_check(instance)
 
     def create_generic_instances(self, instances):
         """
@@ -120,3 +136,41 @@ class KubeDNSCheck(OpenMetricsBaseCheck):
 
     def skydns_skydns_dns_cachemiss_count_total(self, metric, scraper_config):
         self.kubedns_kubedns_dns_cachemiss_count_total(metric, scraper_config)
+
+    def _perform_service_check(self, instance):
+        url = instance.get('health_url')
+        if url is None:
+            return
+
+        tags = instance.get("tags", [])
+        service_check_name = 'kubedns.up'
+        http_handler = self._healthcheck_http_handler(instance, url)
+
+        try:
+            response = http_handler.get(url)
+            response.raise_for_status()
+            self.service_check(service_check_name, AgentCheck.OK, tags=tags)
+        except requests.exceptions.RequestException as e:
+            message = str(e)
+            self.service_check(service_check_name, AgentCheck.CRITICAL, message=message, tags=tags)
+
+    def _healthcheck_http_handler(self, instance, endpoint):
+        if endpoint in self._http_handlers:
+            return self._http_handlers[endpoint]
+
+        config = {}
+        config['tls_cert'] = instance.get('ssl_cert', None)
+        config['tls_private_key'] = instance.get('ssl_private_key', None)
+        config['tls_verify'] = instance.get('ssl_verify', True)
+        config['tls_ignore_warning'] = instance.get('ssl_ignore_warning', False)
+        config['tls_ca_cert'] = instance.get('ssl_ca_cert', None)
+
+        if config['tls_ca_cert'] is None:
+            config['tls_ignore_warning'] = True
+            config['tls_verify'] = False
+
+        http_handler = self._http_handlers[endpoint] = RequestsWrapper(
+            config, self.init_config, self.HTTP_CONFIG_REMAPPER, self.log
+        )
+
+        return http_handler
