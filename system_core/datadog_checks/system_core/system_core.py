d087f87b752835d2020941b782e3a82b3d19f25f
Ian Bucad <ian.bucad@datadoghq.com>
2022-12-09 05:40:49 +1100

    Adds `system.core.frequency` metric (#13349)
    * Adds CPU Core Frequency
    
    * Adjust tests
    
    * add debug log lines
    
    * remove cpu count change
    
    * fix variable
    
    Co-authored-by: steveny91 <steven.yuen@datadoghq.com>

--- system_core/datadog_checks/system_core/system_core.py
+++ system_core/datadog_checks/system_core/system_core.py
@@ -5,14 +5,17 @@ import psutil
 from six import iteritems
 
 from datadog_checks.base import AgentCheck
+from datadog_checks.base.utils.platform import Platform
 
 
 class SystemCore(AgentCheck):
     def check(self, instance):
         instance_tags = instance.get('tags', [])
 
+        # https://psutil.readthedocs.io/en/latest/#psutil.cpu_times
         cpu_times = psutil.cpu_times(percpu=True)
         self.gauge('system.core.count', len(cpu_times), tags=instance_tags)
+        self.log.debug('CPU times: %s', str(cpu_times))
 
         for i, cpu in enumerate(cpu_times):
             tags = instance_tags + ['core:{0}'.format(i)]
@@ -20,6 +23,16 @@ class SystemCore(AgentCheck):
                 self.rate('system.core.{0}'.format(key), 100.0 * value, tags=tags)
 
         total_cpu_times = psutil.cpu_times()
-        n_cpus = psutil.cpu_count()
         for key, value in iteritems(total_cpu_times._asdict()):
-            self.rate('system.core.{0}.total'.format(key), 100.0 * value / n_cpus, tags=instance_tags)
+            self.rate('system.core.{0}.total'.format(key), 100.0 * value / len(cpu_times), tags=instance_tags)
+
+        # https://psutil.readthedocs.io/en/latest/#psutil.cpu_freq
+        # scpufreq(current=2236.812, min=800.0, max=3500.0)
+        # Ignore min/max as they are often reported as 0.0 if undetermined.
+        cpu_freq = psutil.cpu_freq(percpu=True)
+        self.log.debug('CPU frequency: %s', str(cpu_freq))
+        for i, cpu in enumerate(cpu_freq):
+            if Platform.is_unix():
+                # Per-cpu frequency retrieval (Linux only)
+                tags = instance_tags + ['core:{0}'.format(i)]
+            self.gauge('system.core.frequency', cpu._asdict().get('current'), tags=tags)
