f8e4c7eacef88c104801df37822283cd4a50f8a0
Andrew Zhang <andrew.zhang@datadoghq.com>
2022-12-12 14:22:44 -0500

    Fix test for e2e test

--- cloudera/tests/test_e2e.py
+++ cloudera/tests/test_e2e.py
@@ -1,7 +1,9 @@
 import pytest
 
 from datadog_checks.cloudera import ClouderaCheck
-from datadog_checks.cloudera.metrics import METRICS
+from datadog_checks.cloudera.metrics import METRICS, TIMESERIES_METRICS
+
+from .common import CAN_CONNECT_TAGS, CLUSTER_HEALTH_TAGS
 
 
 @pytest.mark.e2e
@@ -12,9 +14,30 @@ def test_e2e(dd_agent_check, instance):
     # Then
     for category, metrics in METRICS.items():
         for metric in metrics:
-            aggregator.assert_metric(f'cloudera.{category}.{metric}')
-    aggregator.assert_service_check('cloudera.can_connect', ClouderaCheck.OK)
+            aggregator.assert_metric(f'cloudera.{category}.{metric}', at_least=1)
+            aggregator.assert_metric_has_tag_prefix(f'cloudera.{category}.{metric}', "cloudera_cluster")
+            aggregator.assert_metric_has_tag(f'cloudera.{category}.{metric}', "test1")
+
+            # Only non-cluster metrics have rack_id tags
+            if category != 'cluster':
+                aggregator.assert_metric_has_tag_prefix(f'cloudera.{category}.{metric}', "cloudera_rack_id")
+
+    # All timeseries metrics should have cloudera_{category} tag
+    for category, metrics in TIMESERIES_METRICS.items():
+        for metric in metrics:
+            aggregator.assert_metric_has_tag_prefix(f'cloudera.{category}.{metric}', f"cloudera_{category}")
+
+    aggregator.assert_service_check(
+        'cloudera.can_connect',
+        ClouderaCheck.OK,
+        tags=CAN_CONNECT_TAGS,
+    )
     # caddy test env is supposed to be in BAD_HEALTH
-    aggregator.assert_service_check('cloudera.cluster.health', ClouderaCheck.CRITICAL, message="BAD_HEALTH")
+    aggregator.assert_service_check(
+        'cloudera.cluster.health',
+        ClouderaCheck.CRITICAL,
+        message="BAD_HEALTH",
+        tags=CLUSTER_HEALTH_TAGS,
+    )
     aggregator.assert_service_check('cloudera.host.health', ClouderaCheck.OK)
     aggregator.assert_all_metrics_covered()
