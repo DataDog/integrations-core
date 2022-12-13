7652c57998349f9ad2f10e68289168e7c4f65272
Florent Clarret <florent.clarret@datadoghq.com>
2022-12-13 10:54:33 +0100

    Add new versions to the test suite

--- kafka/tests/common.py
+++ kafka/tests/common.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2020-present
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
+import os
 import socket
 
 from datadog_checks.dev import get_docker_hostname, get_here
@@ -10,6 +11,7 @@ CHECK_NAME = "kafka"
 HERE = get_here()
 HOST = get_docker_hostname()
 HOST_IP = socket.gethostbyname(HOST)
+COMPOSE_FILE = os.environ["DOCKER_COMPOSE_FILE"]
 
 
 """
--- kafka/tests/conftest.py
+++ kafka/tests/conftest.py
@@ -9,12 +9,12 @@ from datadog_checks.dev import docker_run
 from datadog_checks.dev.conditions import CheckDockerLogs
 from datadog_checks.dev.utils import load_jmx_config
 
-from .common import HERE
+from .common import COMPOSE_FILE, HERE
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    compose_file = os.path.join(HERE, 'compose', 'docker-compose.yml')
+    compose_file = os.path.join(HERE, 'compose', COMPOSE_FILE)
 
     with docker_run(
         compose_file, conditions=[CheckDockerLogs(compose_file, [r'\[KafkaServer id=\d+\] started'], matches="all")]
