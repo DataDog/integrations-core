c3438250b4332697e8691fea2dd38ea732790df2
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2022-12-15 10:13:14 +0000

    generate unique query signatures

---
+++ mysql/tests/generate_unique_query_signatures.py
@@ -0,0 +1,10 @@
+import pymysql
+import common
+
+conn = pymysql.connect(
+    host=common.HOST, port=common.PORT, user='root', password=None
+)
+cursor = conn.cursor()
+for i in range(10000):
+    cursor.execute("SELECT 1 qxxx{} from performance_schema.threads limit 1".format(str(i)))
+conn.close()
