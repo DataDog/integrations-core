14a824ee0a5970e7e0fb1f0b95c9d42908020463
Dusan Jovanovic <dusan.jovanovic@datadoghq.com>
2022-12-27 09:09:23 -0800

    openstack remove redundant tracing
    Remove the `@traced` decorator as it was redundant and used only by the openstack integration. All integrations already have optional tracing by default via the `@traced_class` decorator on the common base class `AgentCheck`.

--- datadog_checks_base/tests/base/utils/test_tracing.py
+++ datadog_checks_base/tests/base/utils/test_tracing.py
@@ -8,7 +8,7 @@ import mock
 import pytest
 
 from datadog_checks.base.stubs import aggregator
-from datadog_checks.base.utils.tracing import traced, traced_class
+from datadog_checks.base.utils.tracing import traced_class
 
 
 class MockAgentCheck(object):
@@ -27,54 +27,8 @@ class DummyCheck(MockAgentCheck):
         super(DummyCheck, self).__init__(*args, **kwargs)
         self.checked = False
 
-    @traced
     def check(self, instance):
         self.gauge('dummy.metric', 10)
-
-
-@pytest.mark.parametrize(
-    'agent_config, init_config, called',
-    [
-        pytest.param({}, {}, False, id='agent_notset_init_notset_notcalled'),
-        pytest.param({'integration_tracing': True}, {}, False, id='agent_true_init_notset_notcalled'),
-        pytest.param(
-            {'integration_tracing': True}, {'trace_check': False}, False, id='agent_true_init_false_notcalled'
-        ),
-        pytest.param(
-            {'integration_tracing': False}, {'trace_check': False}, False, id='agent_false_init_false_notcalled'
-        ),
-        pytest.param({}, {'trace_check': True}, False, id='agent_notset_init_true_notcalled'),
-        pytest.param(
-            {'integration_tracing': False}, {'trace_check': True}, False, id='agent_false_init_true_notcalled'
-        ),
-        pytest.param(
-            {'integration_tracing': True, 'integration_tracing_futures': False},
-            {'trace_check': True},
-            True,
-            id='agent_true_init_true_called',
-        ),
-        pytest.param(
-            {'integration_tracing': True, 'integration_tracing_futures': True},
-            {'trace_check': True},
-            True,
-            id='agent_true_futures_true_init_true_called',
-        ),
-    ],
-)
-def test_traced(aggregator, agent_config, init_config, called):
-    check = DummyCheck('dummy', init_config, [{}])
-
-    with mock.patch('datadog_checks.base.utils.tracing.datadog_agent') as datadog_agent, mock.patch(
-        'ddtrace.tracer'
-    ) as tracer:
-        datadog_agent.get_config = lambda k: agent_config.get(k)
-        check.check({})
-
-        if called:
-            tracer.trace.assert_called_once_with('check', service='dummy-integration', resource='check')
-        else:
-            tracer.trace.assert_not_called()
-        aggregator.assert_metric('dummy.metric', 10, count=1)
 
 
 @pytest.mark.parametrize('traces_enabled', [pytest.param('false'), (pytest.param('true'))])
