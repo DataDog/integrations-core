e5dd30b656cee2295c79ccce03ac0d9164c5f64c
Ofek Lev <ofekmeister@gmail.com>
2022-12-27 13:21:38 -0500

    Properly account for other integration repos

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/config.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/config.py
@@ -104,7 +104,7 @@ def set_value(ctx, key, value):
         scrubbing = any(fnmatch(key, pattern) for pattern in SECRET_KEYS)
         value = click.prompt(f'Value for `{key}`', hide_input=scrubbing)
 
-    if key in ('repos.core', 'repos.extras', 'repos.agent', 'repos.marketplace') and not value.startswith('~'):
+    if key.startswith('repos.') and not value.startswith('~'):
         value = os.path.abspath(value)
 
     user_config = new_config = ctx.obj.copy()
