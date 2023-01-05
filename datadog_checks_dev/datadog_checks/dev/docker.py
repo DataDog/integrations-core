2bfad3b530f73e8d7eb35ab2b7e7fc0c96b1734e
Florent Clarret <florent.clarret@datadoghq.com>
2023-01-05 15:44:27 +0100

    Remove the docker compatibility mode

--- datadog_checks_dev/datadog_checks/dev/docker.py
+++ datadog_checks_dev/datadog_checks/dev/docker.py
@@ -48,7 +48,7 @@ def compose_file_active(compose_file):
     """
     Returns a `bool` indicating whether or not a compose file has any active services.
     """
-    command = ['docker', 'compose', '--compatibility', '-f', compose_file, 'ps']
+    command = ['docker', 'compose', '-f', compose_file, 'ps']
     lines = run_command(command, capture='out', check=True).stdout.strip().splitlines()
 
     return len(lines) > 1
@@ -220,7 +220,7 @@ class ComposeFileUp(LazyFunction):
         self.compose_file = compose_file
         self.build = build
         self.service_name = service_name
-        self.command = ['docker', 'compose', '--compatibility', '-f', self.compose_file, 'up', '-d']
+        self.command = ['docker', 'compose', '-f', self.compose_file, 'up', '-d']
 
         if self.build:
             self.command.append('--build')
@@ -236,7 +236,7 @@ class ComposeFileLogs(LazyFunction):
     def __init__(self, compose_file, check=True):
         self.compose_file = compose_file
         self.check = check
-        self.command = ['docker', 'compose', '--compatibility', '-f', self.compose_file, 'logs']
+        self.command = ['docker', 'compose', '-f', self.compose_file, 'logs']
 
     def __call__(self, exception):
         return run_command(self.command, capture=False, check=self.check)
@@ -249,7 +249,6 @@ class ComposeFileDown(LazyFunction):
         self.command = [
             'docker',
             'compose',
-            '--compatibility',
             '-f',
             self.compose_file,
             'down',
@@ -277,8 +276,8 @@ def _read_example_logs_config(check_root):
 @contextmanager
 def temporarily_stop_service(service, compose_file, check=True):
     # type: (str, str, bool) -> Iterator[None]
-    stop_command = ['docker', 'compose', '--compatibility', '-f', compose_file, 'stop', service]
-    start_command = ['docker', 'compose', '--compatibility', '-f', compose_file, 'start', service]
+    stop_command = ['docker', 'compose', '-f', compose_file, 'stop', service]
+    start_command = ['docker', 'compose', '-f', compose_file, 'start', service]
 
     run_command(stop_command, capture=False, check=check)
     yield
