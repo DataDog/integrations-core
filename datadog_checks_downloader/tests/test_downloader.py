1d8ec2e659a8de80af906132b58116dfa0d966e2
Alex Lopez <alex.lopez@datadoghq.com>
2023-01-05 14:42:00 +0100

    Ensure posix-style path in test assertion

--- datadog_checks_downloader/tests/test_downloader.py
+++ datadog_checks_downloader/tests/test_downloader.py
@@ -5,6 +5,7 @@
 import json
 import logging
 import os
+import pathlib
 import random
 import re
 import shutil
@@ -83,7 +84,10 @@ def test_download(capfd, distribution_name, distribution_version, temporary_loca
     assert len(output) == 1, "Only one output line expected, got {}:\n\t{}".format(len(output), stdout)
 
     expected_output = r"{}/repo/targets/simple/{}/{}-{}-.*?\.whl".format(
-        temporary_local_repo, distribution_name, distribution_name.replace("-", "_"), distribution_version
+        pathlib.PurePath(temporary_local_repo).as_posix(),
+        distribution_name,
+        distribution_name.replace("-", "_"),
+        distribution_version,
     )
     assert re.match(expected_output, output[0]), "Expected '{}' to match '{}'".format(output[0], expected_output)
 
