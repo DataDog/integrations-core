f7f04c0c977e5244d6ad93a9dc90ef768a1804cf
Florent Clarret <florent.clarret@datadoghq.com>
2023-01-09 10:00:56 +0100

    Update nagios/tests/common.py

--- nagios/tests/common.py
+++ nagios/tests/common.py
@@ -22,7 +22,6 @@ INSTANCE_INTEGRATION = {
 CHECK_NAME = 'nagios'
 CUSTOM_TAGS = ['optional:tag1']
 
-NAGIOS_VERSION = os.environ['NAGIOS_VERSION']
 
 NAGIOS_TEST_LOG = os.path.join(HERE, 'fixtures', 'nagios')
 NAGIOS_TEST_HOST = os.path.join(HERE, 'fixtures', 'host-perfdata')
