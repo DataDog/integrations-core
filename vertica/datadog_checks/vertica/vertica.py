a93e3414c5fb3b9a8dc611b1de87a7ff1b68fbe8
Alex Lopez <alex.lopez@datadoghq.com>
2023-01-11 14:25:44 +0100

    Revert "Test failure"
    This reverts commit d5717a076533e3a4308f10f6bbacba549ac7f10b.

--- vertica/datadog_checks/vertica/vertica.py
+++ vertica/datadog_checks/vertica/vertica.py
@@ -1,4 +1,4 @@
-# (C) Datadog, Inc. 2018-present
+# (C) Datadog, Inc. 2019-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from __future__ import division
