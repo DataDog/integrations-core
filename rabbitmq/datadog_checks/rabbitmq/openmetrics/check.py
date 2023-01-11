daec3943d4456b253ef23ac84e58f08115313b32
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2023-01-11 10:10:25 +0100

    only aggregated endpoint

--- rabbitmq/datadog_checks/rabbitmq/openmetrics/check.py
+++ rabbitmq/datadog_checks/rabbitmq/openmetrics/check.py
@@ -1,5 +1,13 @@
 from datadog_checks.base import OpenMetricsBaseCheckV2
 
+from .metrics import METRICS_RENAMES
+
 
 class RabbitMQOpenMetrics(OpenMetricsBaseCheckV2):
     __NAMESPACE__ = "rabbitmq"
+
+    def configure_scrapers(self):
+        self.instance['openmetrics_endpoint'] = self.instance['prometheus_plugin']['url'] + "/metrics"
+        self.scraper_configs.clear()
+        self.scraper_configs.append({**self.instance, 'metrics': [METRICS_RENAMES]})
+        super().configure_scrapers()
