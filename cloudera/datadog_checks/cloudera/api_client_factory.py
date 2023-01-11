0bd581981e72826e16ac7048ff457515f9cd68cc
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2023-01-11 17:10:25 +0100

    Added license header

--- cloudera/datadog_checks/cloudera/api_client_factory.py
+++ cloudera/datadog_checks/cloudera/api_client_factory.py
@@ -1,3 +1,7 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
 import cm_client
 from cm_client.rest import RESTClientObject
 from packaging.version import parse
--- cloudera/datadog_checks/cloudera/metrics.py
+++ cloudera/datadog_checks/cloudera/metrics.py
@@ -1,3 +1,7 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
 TIMESERIES_METRICS = {
     'cluster': [
         'cpu_percent_across_hosts',
