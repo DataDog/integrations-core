310f2e006a49a58ca6f703bffb9194e4d3e43781
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2023-01-11 17:05:32 +0100

    Added license header

--- cloudera/tests/conftest.py
+++ cloudera/tests/conftest.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2022-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+
 import json
 import os
 from copy import deepcopy
--- cloudera/tests/test_integration.py
+++ cloudera/tests/test_integration.py
@@ -1,3 +1,7 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
 import pytest
 
 from datadog_checks.cloudera import ClouderaCheck
