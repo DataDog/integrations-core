f1cd6b4772496302233d75da57bf4226f9ad5de3
Alex Lopez <alex.lopez.zorzano@gmail.com>
2023-01-12 16:59:39 +0100

    Add license header files where missing (#13677)

--- cloudera/datadog_checks/cloudera/common.py
+++ cloudera/datadog_checks/cloudera/common.py
@@ -1,3 +1,7 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
 CAN_CONNECT = "can_connect"
 CLUSTER_HEALTH = "cluster.health"
 HOST_HEALTH = "host.health"
