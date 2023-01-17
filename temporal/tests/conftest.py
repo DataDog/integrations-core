1d5c478c23bc04b8421932d44cc7d9ac82a852f4
Florent Clarret <florent.clarret@datadoghq.com>
2023-01-17 11:08:23 +0100

    Add the temporal integration

---
+++ temporal/tests/conftest.py
@@ -0,0 +1,25 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+
+import pytest
+
+from datadog_checks.dev import get_here, docker_run
+
+
+@pytest.fixture(scope='session')
+def dd_environment():
+    compose_file = os.path.join(get_here(), "compose", "docker-compose.yaml")
+    conditions = []
+
+    with docker_run(
+            compose_file=compose_file,
+            conditions=conditions,
+    ):
+        yield {}
+
+
+@pytest.fixture
+def instance():
+    return {}
