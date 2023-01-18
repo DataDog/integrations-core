9226746cf7d3177fc2488e3736ed0e37bc6226f5
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2023-01-18 03:35:42 +0100

    specify both detailed and aggregated endpoints in one instance

--- rabbitmq/datadog_checks/rabbitmq/openmetrics/check.py
+++ rabbitmq/datadog_checks/rabbitmq/openmetrics/check.py
@@ -1,5 +1,3 @@
-import re
-
 from datadog_checks.base import OpenMetricsBaseCheckV2
 
 from . import metrics
@@ -12,42 +10,13 @@ class RabbitMQOpenMetrics(OpenMetricsBaseCheckV2):
         base_url = self.instance['prometheus_plugin']['url']
         self.scraper_configs.clear()
         endpoints = []
-        if self.instance['prometheus_plugin'].get('include_aggregated_endpoint', False):
-            endpoints.append(
-                (
-                    "/metrics",
-                    {
-                        'metrics': [metrics.RENAME_RABBITMQ_TO_DATADOG],
-                    },
-                )
-            )
         unagg_ep = self.instance['prometheus_plugin'].get('unaggregated_endpoint', '')
-        if 'detailed' in unagg_ep:
-            endpoints.append(
-                (
-                    f"/metrics/{unagg_ep}",
-                    {
-                        "metrics": [
-                            {
-                                (
-                                    re.sub("^rabbitmq_", "rabbitmq_detailed_", k)
-                                    if k not in ['rabbitmq_build_info', 'rabbitmq_identity_info']
-                                    else k
-                                ): v
-                                for k, v in metrics.RENAME_RABBITMQ_TO_DATADOG.items()
-                            }
-                        ],
-                    },
-                )
-            )
-        if 'per-object' in unagg_ep:
-            endpoints.append(("/metrics/per-object", {'metrics': [metrics.RENAME_RABBITMQ_TO_DATADOG]}))
+        exclude_from_agg = set()
+        if unagg_ep:
+            renames, exclude_from_agg = metrics.unaggregated_renames_and_exclusions(unagg_ep)
+            endpoints.append((f"/metrics/{unagg_ep}", {"metrics": [renames]}))
+        if self.instance['prometheus_plugin'].get('include_aggregated_endpoint', False):
+            endpoints.append(("/metrics", {'metrics': [metrics.aggregated_renames(exclude_from_agg)]}))
         for ep, ep_config in endpoints:
-            self.scraper_configs.append(
-                {
-                    **self.instance,
-                    'openmetrics_endpoint': base_url + ep,
-                    **ep_config,
-                }
-            )
+            self.scraper_configs.append({**self.instance, 'openmetrics_endpoint': base_url + ep, **ep_config})
         super().configure_scrapers()
