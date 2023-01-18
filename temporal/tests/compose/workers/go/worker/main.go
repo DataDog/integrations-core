cd3f62414b4ba8f901a82e048906bb0993d3f230
Florent Clarret <florent.clarret@datadoghq.com>
2023-01-18 15:42:56 +0100

    expose go sdk metrics

--- temporal/tests/compose/workers/go/worker/main.go
+++ temporal/tests/compose/workers/go/worker/main.go
@@ -4,14 +4,26 @@ package main
 import (
 	"hello-world-temporal/app"
 	"log"
+	"time"
 
+	prom "github.com/prometheus/client_golang/prometheus"
+
+    "github.com/uber-go/tally/v4"
+	"github.com/uber-go/tally/v4/prometheus"
+	sdktally "go.temporal.io/sdk/contrib/tally"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 )
 
 func main() {
 	// Create the client object just once per process
-	c, err := client.Dial(client.Options{HostPort: "temporal:7233"})
+    c, err := client.Dial(client.Options{
+        HostPort: "temporal:7233",
+		MetricsHandler: sdktally.NewMetricsHandler(newPrometheusScope(prometheus.Configuration{
+			ListenAddress: "0.0.0.0:9090",
+			TimerType:     "histogram",
+		})),
+	})
 	if err != nil {
 		log.Fatalln("unable to create Temporal client", err)
 	}
@@ -27,6 +39,31 @@ func main() {
 	if err != nil {
 		log.Fatalln("unable to start Worker", err)
 	}
+}
+
+func newPrometheusScope(c prometheus.Configuration) tally.Scope {
+	reporter, err := c.NewReporter(
+		prometheus.ConfigurationOptions{
+			Registry: prom.NewRegistry(),
+			OnError: func(err error) {
+				log.Println("error in prometheus reporter", err)
+			},
+		},
+	)
+	if err != nil {
+		log.Fatalln("error creating prometheus reporter", err)
+	}
+	scopeOpts := tally.ScopeOptions{
+		CachedReporter:  reporter,
+		Separator:       prometheus.DefaultSeparator,
+		SanitizeOptions: &sdktally.PrometheusSanitizeOptions,
+		Prefix:          "temporal_samples",
+	}
+	scope, _ := tally.NewRootScope(scopeOpts, time.Second)
+	scope = sdktally.NewPrometheusNamingScope(scope)
+
+	log.Println("prometheus metrics scope created")
+	return scope
 }
 
 // @@@SNIPEND
