8d9d4f1a6304ed98100ce93b729adfeb529502cd
Alex Lopez <alex.lopez.zorzano@gmail.com>
2023-01-18 11:17:35 +0100

    Revert increased waiting time (#13712)
    * Revert increased waiting time and reorder conditions
    
    * Revert back to original order

--- clickhouse/tests/conftest.py
+++ clickhouse/tests/conftest.py
@@ -15,7 +15,7 @@ from . import common
 def dd_environment():
     conditions = []
     for i in range(6):
-        conditions.append(CheckEndpoints(['http://{}:{}'.format(common.HOST, common.HTTP_START_PORT + i)], wait=5))
+        conditions.append(CheckEndpoints(['http://{}:{}'.format(common.HOST, common.HTTP_START_PORT + i)]))
         conditions.append(
             CheckDockerLogs(
                 'clickhouse-0{}'.format(i + 1), 'Logging errors to /var/log/clickhouse-server/clickhouse-server.err.log'
