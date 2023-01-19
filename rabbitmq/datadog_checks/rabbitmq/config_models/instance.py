821a8ddc75a3288154e5149f2ffd7fde5923011b
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2023-01-19 18:20:03 +0100

    Only select aggregated endpoint (#13673)
    * only aggregated endpoint
    
    * fix erlang.vm.allocators assertion
    
    * copy suggestions
    
    Co-authored-by: Austin Lai <76412946+alai97@users.noreply.github.com>
    
    * Update rabbitmq/assets/configuration/spec.yaml
    
    Co-authored-by: Austin Lai <76412946+alai97@users.noreply.github.com>
    
    * Update models and example config to reflect current spec
    
    * Don't test rabbitmq 3.8 (openmetrics) with python2
    
    * fix PY2 failure
    
    * consistent python env names; better setup to skip openmetrics tests
    
    * Add openmetrics to E2E environment (#13695)
    
    * add openmetrics e2e
    
    * re-add newline to compose file
    
    * revert hatch.toml changes
    
    Co-authored-by: Austin Lai <76412946+alai97@users.noreply.github.com>
    Co-authored-by: Fanny Jiang <fanny.jiang@datadoghq.com>

--- rabbitmq/datadog_checks/rabbitmq/config_models/instance.py
+++ rabbitmq/datadog_checks/rabbitmq/config_models/instance.py
@@ -9,9 +9,9 @@
 
 from __future__ import annotations
 
-from typing import Any, Mapping, Optional, Sequence
+from typing import Any, Mapping, Optional, Sequence, Union
 
-from pydantic import BaseModel, root_validator, validator
+from pydantic import BaseModel, Extra, Field, root_validator, validator
 
 from datadog_checks.base.utils.functions import identity
 from datadog_checks.base.utils.models import validation
@@ -27,12 +27,38 @@ class AuthToken(BaseModel):
     writer: Optional[Mapping[str, Any]]
 
 
+class ExtraMetric(BaseModel):
+    class Config:
+        extra = Extra.allow
+        allow_mutation = False
+
+    name: Optional[str]
+    type: Optional[str]
+
+
 class MetricPatterns(BaseModel):
     class Config:
         allow_mutation = False
 
     exclude: Optional[Sequence[str]]
     include: Optional[Sequence[str]]
+
+
+class Metric(BaseModel):
+    class Config:
+        extra = Extra.allow
+        allow_mutation = False
+
+    name: Optional[str]
+    type: Optional[str]
+
+
+class PrometheusPlugin(BaseModel):
+    class Config:
+        allow_mutation = False
+
+    include_aggregated_endpoint: Optional[bool]
+    url: Optional[str]
 
 
 class Proxy(BaseModel):
@@ -42,6 +68,14 @@ class Proxy(BaseModel):
     http: Optional[str]
     https: Optional[str]
     no_proxy: Optional[Sequence[str]]
+
+
+class ShareLabel(BaseModel):
+    class Config:
+        allow_mutation = False
+
+    labels: Optional[Sequence[str]]
+    match: Optional[Sequence[str]]
 
 
 class InstanceConfig(BaseModel):
@@ -54,14 +88,28 @@ class InstanceConfig(BaseModel):
     aws_host: Optional[str]
     aws_region: Optional[str]
     aws_service: Optional[str]
+    cache_metric_wildcards: Optional[bool]
+    cache_shared_labels: Optional[bool]
+    collect_counters_with_distributions: Optional[bool]
+    collect_histogram_buckets: Optional[bool]
     collect_node_metrics: Optional[bool]
     connect_timeout: Optional[float]
     disable_generic_tags: Optional[bool]
     empty_default_hostname: Optional[bool]
+    enable_health_service_check: Optional[bool]
     exchanges: Optional[Sequence[str]]
     exchanges_regexes: Optional[Sequence[str]]
+    exclude_labels: Optional[Sequence[str]]
+    exclude_metrics: Optional[Sequence[str]]
+    exclude_metrics_by_labels: Optional[Mapping[str, Union[bool, Sequence[str]]]]
     extra_headers: Optional[Mapping[str, Any]]
+    extra_metrics: Optional[Sequence[Union[str, Mapping[str, Union[str, ExtraMetric]]]]]
     headers: Optional[Mapping[str, Any]]
+    histogram_buckets_as_distributions: Optional[bool]
+    hostname_format: Optional[str]
+    hostname_label: Optional[str]
+    ignore_tags: Optional[Sequence[str]]
+    include_labels: Optional[Sequence[str]]
     kerberos_auth: Optional[str]
     kerberos_cache: Optional[str]
     kerberos_delegate: Optional[bool]
@@ -71,22 +119,33 @@ class InstanceConfig(BaseModel):
     kerberos_principal: Optional[str]
     log_requests: Optional[bool]
     metric_patterns: Optional[MetricPatterns]
+    metrics: Optional[Sequence[Union[str, Mapping[str, Union[str, Metric]]]]]
     min_collection_interval: Optional[float]
+    namespace: Optional[str] = Field(None, regex='\\w*')
     nodes: Optional[Sequence[str]]
     nodes_regexes: Optional[Sequence[str]]
+    non_cumulative_histogram_buckets: Optional[bool]
     ntlm_domain: Optional[str]
+    openmetrics_endpoint: Optional[str]
     password: Optional[str]
     persist_connections: Optional[bool]
+    prometheus_plugin: Optional[PrometheusPlugin]
     proxy: Optional[Proxy]
     queues: Optional[Sequence[str]]
     queues_regexes: Optional[Sequence[str]]
     rabbitmq_api_url: Optional[str]
+    raw_line_filters: Optional[Sequence[str]]
+    raw_metric_prefix: Optional[str]
     read_timeout: Optional[float]
+    rename_labels: Optional[Mapping[str, Any]]
     request_size: Optional[float]
     service: Optional[str]
+    share_labels: Optional[Mapping[str, Union[bool, ShareLabel]]]
     skip_proxy: Optional[bool]
+    tag_by_endpoint: Optional[bool]
     tag_families: Optional[bool]
     tags: Optional[Sequence[str]]
+    telemetry: Optional[bool]
     timeout: Optional[float]
     tls_ca_cert: Optional[str]
     tls_cert: Optional[str]
@@ -95,7 +154,9 @@ class InstanceConfig(BaseModel):
     tls_protocols_allowed: Optional[Sequence[str]]
     tls_use_host_header: Optional[bool]
     tls_verify: Optional[bool]
+    use_latest_spec: Optional[bool]
     use_legacy_auth_encoding: Optional[bool]
+    use_process_start_time: Optional[bool]
     username: Optional[str]
     vhosts: Optional[Sequence[str]]
 
