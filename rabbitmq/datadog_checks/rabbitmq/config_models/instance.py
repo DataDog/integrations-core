ed928e1770a29ca6c79759efbe58e223e2ce003f
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2023-01-19 18:27:48 +0100

    support for specifying just detailed endpoint (#13691)
    * support for specifying just detailed endpoint
    
    * Update rabbitmq/datadog_checks/rabbitmq/data/conf.yaml.example
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    
    * Update rabbitmq/datadog_checks/rabbitmq/data/conf.yaml.example
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    
    * sync config
    
    * apply and sync all the suggestions
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>

--- rabbitmq/datadog_checks/rabbitmq/config_models/instance.py
+++ rabbitmq/datadog_checks/rabbitmq/config_models/instance.py
@@ -58,6 +58,7 @@ class PrometheusPlugin(BaseModel):
         allow_mutation = False
 
     include_aggregated_endpoint: Optional[bool]
+    unaggregated_endpoint: Optional[str]
     url: Optional[str]
 
 
