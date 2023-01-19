b08f8cc435b421d322eeb6f5d67a236073332204
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2023-01-19 18:27:48 +0100

    support for specifying just detailed endpoint (#13691)
    * support for specifying just detailed endpoint
    
    * Update rabbitmq/datadog_checks/rabbitmq/data/conf.yaml.example
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    
    * Update rabbitmq/datadog_checks/rabbitmq/data/conf.yaml.example
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>
    
    * sync config
    
    * apply and sync all the suggestions
    
    Co-authored-by: Bryce Eadie <bryce.eadie@datadoghq.com>

--- rabbitmq/tests/test_openmetrics.py
+++ rabbitmq/tests/test_openmetrics.py
@@ -1,3 +1,4 @@
+from itertools import product
 from pathlib import Path
 
 from datadog_checks.dev.utils import get_metadata_metrics
@@ -44,3 +45,132 @@ def test_aggregated_endpoint(aggregator, dd_run_check, mock_http_response):
     aggregator.assert_metric('rabbitmq.identity_info', tags=DEFAULT_OM_TAGS + identity_info_tags)
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
     aggregator.assert_all_metrics_covered()
+
+
+def test_bare_detailed_endpoint(aggregator, dd_run_check, mock_http_response):
+    """User enables only the /metrics/detailed endpoint and without any query to it.
+
+    We expect at least metrics like build_info and identity_info to still be present.
+    """
+    mock_http_response(file_path=OPENMETRICS_RESPONSE_FIXTURES / "detailed.txt")
+    check = RabbitMQ(
+        "rabbitmq",
+        {},
+        [
+            {
+                'prometheus_plugin': {
+                    'url': "localhost:15692",
+                    'unaggregated_endpoint': 'detailed',
+                },
+                "metrics": [".+"],
+            }
+        ],
+    )
+    dd_run_check(check)
+    expected_metrics = [
+        dict(
+            name='rabbitmq.build_info',
+            value=1,
+            metric_type=aggregator.GAUGE,
+            tags=[
+                'erlang_version:25.1.2',
+                'prometheus_client_version:4.9.1',
+                'prometheus_plugin_version:3.11.3',
+                'rabbitmq_version:3.11.3',
+            ],
+        ),
+        dict(
+            name='rabbitmq.identity_info',
+            value=1,
+            metric_type=aggregator.GAUGE,
+            tags=[
+                'rabbitmq_node:rabbit@54cfac2199f1',
+                "rabbitmq_cluster:rabbit@54cfac2199f1",
+                "rabbitmq_cluster_permanent_id:rabbitmq-cluster-id-cyw_z6c4UMIBoK51iVq9rw",
+            ],
+        ),
+    ]
+    for m in expected_metrics:
+        kwargs = {**m, "tags": ["endpoint:localhost:15692/metrics/detailed"] + m.get('tags', [])}
+        aggregator.assert_metric(**kwargs)
+
+
+def test_detailed_endpoint_queue_coarse_metrics(aggregator, dd_run_check, mock_http_response):
+    """We scrape the /metrics/detailed endpoint with the 'queue_coarse_metrics' family query."""
+    mock_http_response(file_path=OPENMETRICS_RESPONSE_FIXTURES / "detailed-queue_coarse_metrics.txt")
+    check = RabbitMQ(
+        "rabbitmq",
+        {},
+        [
+            {
+                'prometheus_plugin': {
+                    'url': "localhost:15692",
+                    'unaggregated_endpoint': 'detailed?family=queue_coarse_metrics',
+                },
+                "metrics": [".+"],
+            }
+        ],
+    )
+    dd_run_check(check)
+
+    expected_metrics = (
+        [
+            dict(
+                name='rabbitmq.build_info',
+                value=1,
+                metric_type=aggregator.GAUGE,
+                tags=[
+                    'erlang_version:25.1.2',
+                    'prometheus_client_version:4.9.1',
+                    'prometheus_plugin_version:3.11.3',
+                    'rabbitmq_version:3.11.3',
+                ],
+            ),
+            dict(
+                name='rabbitmq.identity_info',
+                value=1,
+                metric_type=aggregator.GAUGE,
+                tags=[
+                    'rabbitmq_node:rabbit@54cfac2199f1',
+                    "rabbitmq_cluster:rabbit@54cfac2199f1",
+                    "rabbitmq_cluster_permanent_id:rabbitmq-cluster-id-cyw_z6c4UMIBoK51iVq9rw",
+                ],
+            ),
+        ]
+        + [
+            dict(
+                name=name,
+                value=value,
+                metric_type=aggregator.GAUGE,
+                tags=['vhost:/', f'queue:{qname}'],
+            )
+            for name, (value, qname) in product(
+                ('rabbitmq.queue.messages', 'rabbitmq.queue.messages.ready'),
+                ((0, 'queue1'), (1, 'queue2'), (0, 'queue3')),
+            )
+        ]
+        + [
+            dict(
+                name='rabbitmq.queue.messages.unacked',
+                value=0,
+                metric_type=aggregator.GAUGE,
+                tags=['vhost:/', f'queue:{qname}'],
+            )
+            for qname in ('queue1', 'queue2', 'queue3')
+        ]
+        + [
+            dict(
+                name='rabbitmq.queue.process_reductions.count',
+                value=value,
+                metric_type=aggregator.MONOTONIC_COUNT,
+                tags=['vhost:/', f'queue:{qname}'],
+            )
+            for qname, value in (('queue1', 30773), ('queue2', 30466), ('queue3', 35146))
+        ]
+    )
+    for m in expected_metrics:
+        kwargs = {
+            **m,
+            "tags": ["endpoint:localhost:15692/metrics/detailed?family=queue_coarse_metrics"] + m.get('tags', []),
+        }
+        aggregator.assert_metric(**kwargs)
