7533e19d903447d4fe1571f3b107f3c8b5869952
Branden Clark <branden.clark@datadoghq.com>
2023-01-19 01:25:25 -0500

    license

--- iis/datadog_checks/iis/service_check.py
+++ iis/datadog_checks/iis/service_check.py
@@ -1,4 +1,4 @@
-# (C) Datadog, Inc. 2021-present
+# (C) Datadog, Inc. 2023-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from datadog_checks.base.constants import ServiceCheck
