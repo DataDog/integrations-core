e2670fb315abc27885fad010137f9488462df632
Florent Clarret <florent.clarret@datadoghq.com>
2023-01-26 11:23:01 +0100

    Add a basic e2e environment

--- temporal/tests/conftest.py
+++ temporal/tests/conftest.py
@@ -2,15 +2,25 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import copy
+import os
 
 import pytest
+
+from datadog_checks.dev import docker_run, get_docker_hostname, get_here
+from datadog_checks.dev.conditions import CheckEndpoints
 
 INSTANCE = {"service": "server"}
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    yield copy.deepcopy(INSTANCE)
+    compose_file = os.path.join(get_here(), "compose", "docker-compose.yaml")
+
+    with docker_run(
+        compose_file=compose_file,
+        conditions=[CheckEndpoints(f"http://{get_docker_hostname()}:8000/metrics")],
+    ):
+        yield copy.deepcopy(INSTANCE)
 
 
 @pytest.fixture
