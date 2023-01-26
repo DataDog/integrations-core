b76fbcf6469d95b453acbe0e3058475f657266ef
Florent Clarret <florent.clarret@datadoghq.com>
2023-01-26 10:45:30 +0100

    Collect the temporal logs

--- temporal/tests/conftest.py
+++ temporal/tests/conftest.py
@@ -4,10 +4,12 @@
 import copy
 import os
 import time
+from contextlib import ExitStack, contextmanager
 
 import pytest
 
-from datadog_checks.dev import docker_run, get_docker_hostname, get_here, run_command
+from datadog_checks.dev import EnvVars, TempDir, docker_run, get_docker_hostname, get_here, run_command
+from datadog_checks.dev._env import get_state, save_state
 from datadog_checks.dev.conditions import CheckEndpoints
 
 INSTANCE = {"service": "server"}
@@ -23,6 +25,7 @@ def dd_environment():
             CheckEndpoints(f"http://{get_docker_hostname()}:8000/metrics"),
             CheckEndpoints(f"http://{get_docker_hostname()}:8001/metrics"),
         ),
+        wrappers=[create_log_volumes()],
     ):
         # Run the workflow a couple of times.
         for param in ("World", "Datadog", "Agent Integrations"):
@@ -36,6 +39,33 @@ def dd_environment():
         time.sleep(2)
 
         yield copy.deepcopy(INSTANCE)
+
+
+@contextmanager
+def create_log_volumes():
+    env_vars = {}
+    docker_volumes = get_state("docker_volumes", [])
+
+    with ExitStack() as stack:
+        d = stack.enter_context(TempDir("temporal"))
+        os.chmod(d, 0o777)
+        docker_volumes.append(f"{d}:/var/log/temporal")
+        env_vars["TEMPORAL_LOG_FOLDER"] = d
+
+        config = [
+            {
+                "type": "file",
+                "path": "/var/log/temporal/temporal-server.log",
+                "source": "temporal",
+                "service": "temporal",
+            },
+        ]
+
+        save_state("logs_config", config)
+        save_state("docker_volumes", docker_volumes)
+
+        with EnvVars(env_vars):
+            yield
 
 
 @pytest.fixture
