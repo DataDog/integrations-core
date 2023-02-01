884fc3c4ef58fe8da047039bf592818be5314dfa
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2023-02-01 14:42:43 +0100

    ddev validate license-headers --fix cloudera

--- cloudera/tests/test_unit_hosts.py
+++ cloudera/tests/test_unit_hosts.py
@@ -1,3 +1,7 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
 import mock
 import pytest
 from tests.common import query_time_series
