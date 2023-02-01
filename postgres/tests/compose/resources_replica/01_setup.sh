15b38691eed413d6d2d381928cae8dbfcc9e35b6
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2023-02-01 08:46:33 +0100

    Add replication container

---
+++ postgres/tests/compose/resources_replica/01_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "Testing primary"
+while ! nc -z postgres 5432 ; do
+    echo "Primary not running, waiting"
+    sleep 1
+done
+
+pg_ctl -D /var/lib/postgresql/data -l /tmp/logfile stop
+rm -rf /var/lib/postgresql/data/*
+
+echo "Running pg basebackup"
+export PGPASSWORD='replicator'
+pg_basebackup -h postgres -U replicator -X stream -C -S replica_1 -v -R -D /var/lib/postgresql/data/
+echo "pg basebackup ran"
+
+pg_ctl -D /var/lib/postgresql/data -l /tmp/logfile start
