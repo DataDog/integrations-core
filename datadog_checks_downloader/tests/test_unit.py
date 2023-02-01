db821a6e9843d0dc92799078fe7910a193b6fb99
steveny91 <steven.yuen@datadoghq.com>
2023-01-31 20:04:38 -0500

    test

--- datadog_checks_downloader/tests/test_unit.py
+++ datadog_checks_downloader/tests/test_unit.py
@@ -3,11 +3,21 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
 from unittest.mock import patch
-from datadog_checks.downloader.download import get_wheel_relpath
+from datadog_checks.downloader.download import TUFDownloader
 
-mocked_wheels = {'3.6.1': {'py2.py3': 'datadog_vsphere-3.6.1-py2.py3-none-any.whl'}, '5.4.0rc2': {'py2.py3': 'datadog_vsphere-5.4.0rc2-py2.py3-none-any.whl'}, '6.2.2a1': {'py2.py3': 'datadog_vsphere-6.2.2b1-py2.py3-none-any.whl'}, '6.3.0b1': {'py2.py3': 'datadog_vsphere-6.3.0b1-py2.py3-none-any.whl'}}
-@patch('datadog_checks.downloader.download.wheels', mocked_wheels)
-def test_non_official_wheel_filter():
+def test_non_official_wheel_filter(mocker):
+    mocked_wheels = {
+        '3.6.1': {'py2.py3': 'datadog_vsphere-3.6.1-py2.py3-none-any.whl'},
+        '5.4.0rc2': {'py2.py3': 'datadog_vsphere-5.4.0rc2-py2.py3-none-any.whl'},
+        '6.2.2a1': {'py2.py3': 'datadog_vsphere-6.2.2b1-py2.py3-none-any.whl'},
+        '6.3.0b1': {'py2.py3': 'datadog_vsphere-6.3.0b1-py2.py3-none-any.whl'}
+    }
+    
+    downloader = TUFDownloader()
+    mock_wheels_call = mocker.patch.object(TUFDownloader, '_TUFDownloader__get_versions', return_value=mocked_wheels)
+
     integration = "datadog-vsphere"
-    result = get_wheel_relpath(integration)
+    result = downloader.get_wheel_relpath(integration)
+
+    mock_wheels_call.assert_called_once()
     assert result == 'simple/datadog-vsphere/datadog_vsphere-3.6.1-py2.py3-none-any.whl'
