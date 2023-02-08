f373a56b46c5e01ff34278fed5d0de2ab1b62dfe
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2023-02-08 16:59:25 +0100

    Fixed some debug logs

--- cloudera/datadog_checks/cloudera/config.py
+++ cloudera/datadog_checks/cloudera/config.py
@@ -5,9 +5,9 @@
 # Discovery class requires 'include' to be a dict, so this function is needed to normalize the config
 def normalize_discover_config_include(log, config):
     normalized_config = {}
-    log.debug("normalize_discover_config_include: %s", type(config))
+    log.debug("normalize_discover_config_include config: %s", config)
     include_list = config.get('include') if isinstance(config, dict) else config.include if config else []
-    log.debug("normalize_discover_config_include: %s", include_list)
+    log.debug("normalize_discover_config_include include_list: %s", include_list)
     if not isinstance(include_list, list):
         raise TypeError('Setting `include` must be an array')
     for entry in include_list:
