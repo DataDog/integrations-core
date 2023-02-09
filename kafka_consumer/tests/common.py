b58be21cf57943f2951fe25e83fef3a8451d51ff
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-02-09 14:22:31 -0500

    Remove deprecated implementation of kafka_consumer (#13915)
    * Remove deprecated implementation of kafka_consumer
    
    * Apply suggestions

--- kafka_consumer/tests/common.py
+++ kafka_consumer/tests/common.py
@@ -4,7 +4,6 @@
 import os
 import socket
 
-from datadog_checks.base import is_affirmative
 from datadog_checks.dev import get_docker_hostname
 
 HERE = os.path.dirname(os.path.abspath(__file__))
@@ -14,28 +13,5 @@ KAFKA_CONNECT_STR = '{}:9092'.format(HOST_IP)
 ZK_CONNECT_STR = '{}:2181'.format(HOST)
 TOPICS = ['marvel', 'dc']
 PARTITIONS = [0, 1]
-USE_MULTIPLE_BROKERS = is_affirmative(os.environ['USE_MULTIPLE_BROKERS'])
-if USE_MULTIPLE_BROKERS:
-    DOCKER_IMAGE_PATH = os.path.join(HERE, 'docker', 'multiple-brokers.yaml')
-else:
-    DOCKER_IMAGE_PATH = os.path.join(HERE, 'docker', 'single-broker.yaml')
+DOCKER_IMAGE_PATH = os.path.join(HERE, 'docker', 'multiple-brokers.yaml')
 KAFKA_VERSION = os.environ.get('KAFKA_VERSION')
-
-
-def is_supported(flavor):
-    """
-    Returns whether the current CI configuration is supported
-    """
-    if not KAFKA_VERSION:
-        return False
-
-    if flavor != os.environ.get('KAFKA_OFFSETS_STORAGE'):
-        return False
-
-    return True
-
-
-def is_legacy_check():
-    return os.environ.get('KAFKA_OFFSETS_STORAGE') == 'zookeeper' or os.environ.get('KAFKA_VERSION', '').startswith(
-        '0.9'
-    )
