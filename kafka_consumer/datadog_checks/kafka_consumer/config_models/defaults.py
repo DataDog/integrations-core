4174b80a3fcc0e56be6f3c8f9912fb4bfcdcdab0
Fanny Jiang <fanny.jiang@datadoghq.com>
2023-02-09 15:34:34 -0500

    Remove DSM (#13914)
    * remove dsm
    
    * remove dsm from metadata.csv

--- kafka_consumer/datadog_checks/kafka_consumer/config_models/defaults.py
+++ kafka_consumer/datadog_checks/kafka_consumer/config_models/defaults.py
@@ -26,10 +26,6 @@ def instance_consumer_groups(field, value):
     return get_default_field_value(field, value)
 
 
-def instance_data_streams_enabled(field, value):
-    return False
-
-
 def instance_disable_generic_tags(field, value):
     return False
 
