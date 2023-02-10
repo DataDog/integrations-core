3ceb5adfd65456eda2eb090007214dbe0c68ee50
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-02-10 16:03:57 -0500

    Undo

--- kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
+++ kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 from time import time
 
 import six
-from kafka import KafkaAdminClient
+from kafka import KafkaAdminClient, KafkaClient
 from kafka import errors as kafka_errors
 from kafka.oauth.abstract import AbstractTokenProvider
 from kafka.protocol.admin import ListGroupsRequest
@@ -88,8 +88,11 @@ class KafkaCheck(AgentCheck):
         }
         self.event(event_dict)
 
+    def create_kafka_client(self):
+        return self._create_kafka_client(clazz=KafkaClient)
+
     def create_kafka_admin_client(self):
-        return self._create_kafka_client()
+        return self._create_kafka_client(clazz=KafkaAdminClient)
 
     def validate_consumer_groups(self):
         """Validate any explicitly specified consumer groups.
@@ -108,7 +111,7 @@ class KafkaCheck(AgentCheck):
                         for partition in partitions:
                             assert isinstance(partition, int)
 
-    def _create_kafka_client(self):
+    def _create_kafka_client(self, clazz):
         kafka_connect_str = self.instance.get('kafka_connect_str')
         if not isinstance(kafka_connect_str, (string_types, list)):
             raise ConfigurationError('kafka_connect_str should be string or list of strings')
@@ -122,7 +125,7 @@ class KafkaCheck(AgentCheck):
             tls_context.load_verify_locations(crlfile)
             tls_context.verify_flags |= ssl.VERIFY_CRL_CHECK_LEAF
 
-        return KafkaAdminClient(
+        return clazz(
             bootstrap_servers=kafka_connect_str,
             client_id='dd-agent',
             request_timeout_ms=self.init_config.get('kafka_timeout', DEFAULT_KAFKA_TIMEOUT) * 1000,
