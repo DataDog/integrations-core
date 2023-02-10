2d1e363fa7f23aa9e5b16f326c633d0c6a6ef9de
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-02-10 16:31:21 -0500

    Fix style

--- kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client.py
@@ -2,14 +2,14 @@ class KafkaClient:
     def __init__(self) -> None:
         pass
 
-    def get_consumer_offset_and_lag():
+    def get_consumer_offset_and_lag(self):
         pass
 
-    def get_broker_offset():
+    def get_broker_offset(self):
         pass
 
-    def report_consumer_offset_and_lag():
+    def report_consumer_offset_and_lag(self):
         pass
 
-    def report_broker_offset():
+    def report_broker_offset(self):
         pass
