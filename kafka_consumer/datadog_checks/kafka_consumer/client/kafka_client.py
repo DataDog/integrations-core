8341e3092deafad56e89733298df54c9dda6b976
Fanny Jiang <fanny.jiang@datadoghq.com>
2023-02-11 12:56:00 -0500

    Add consumer offset and log collection

--- kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client.py
@@ -1,15 +1,18 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
 class KafkaClient:
     def __init__(self) -> None:
         pass
 
-    def get_consumer_offset_and_lag():
+    def get_consumer_offset_and_lag(self):
         pass
 
-    def get_broker_offset():
+    def get_broker_offset(self):
         pass
 
-    def report_consumer_offset_and_lag():
+    def report_consumer_offset_and_lag(self):
         pass
 
-    def report_broker_offset():
+    def report_broker_offset(self):
         pass
