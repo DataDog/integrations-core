317bf94f500de1dc9eeebd9b508d80d452593860
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-02-14 18:25:23 -0500

    Revert confluent kafka change for separate PR

--- kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
@@ -11,5 +11,11 @@ class ConfluentKafkaClient:
     def get_broker_offset(self):
         pass
 
-    def get_version(self):
+    def report_consumer_offset_and_lag(self):
+        pass
+
+    def report_broker_offset(self):
+        pass
+
+    def collect_broker_metadata(self):
         pass
