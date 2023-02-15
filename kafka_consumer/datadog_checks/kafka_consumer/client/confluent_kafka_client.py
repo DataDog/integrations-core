423bb346e6485f4133c1fdad8d83ab4bf36b4e54
Fanny Jiang <fanny.jiang@datadoghq.com>
2023-02-15 17:39:09 -0500

    Move metric reporting back into main check (#13973)
    * Refactor metric submissions back into check
    
    * fix spaces
    
    * remove todo note
    
    * fix style
    
    * move get broker metadata
    
    * remove broker metadata method from classes
    
    * reset client offsets

--- kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
@@ -10,12 +10,3 @@ class ConfluentKafkaClient:
 
     def get_broker_offset(self):
         pass
-
-    def report_consumer_offset_and_lag(self):
-        pass
-
-    def report_broker_offset(self):
-        pass
-
-    def collect_broker_metadata(self):
-        pass
