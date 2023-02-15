89b1ca8dcd804d802389e625f672fc16ef2cfabd
Florent Clarret <florent.clarret@datadoghq.com>
2023-02-15 14:31:54 +0100

    Drop Python 2 support

--- kafka_consumer/tests/common.py
+++ kafka_consumer/tests/common.py
@@ -9,7 +9,7 @@ from datadog_checks.dev import get_docker_hostname
 HERE = os.path.dirname(os.path.abspath(__file__))
 HOST = get_docker_hostname()
 HOST_IP = socket.gethostbyname(HOST)
-KAFKA_CONNECT_STR = '{}:9092'.format(HOST_IP)
+KAFKA_CONNECT_STR = f'{HOST_IP}:9092'
 TOPICS = ['marvel', 'dc']
 PARTITIONS = [0, 1]
 DOCKER_IMAGE_PATH = os.path.join(HERE, 'docker', 'docker-compose.yaml')
