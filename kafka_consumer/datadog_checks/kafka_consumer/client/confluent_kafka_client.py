bdd2bdc2f7f792e873c7b87e900509cf820221eb
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-02-15 12:16:35 -0500

    Refactor Kafka Consumer (#13931)
    * Map out structure
    
    * Combine classes
    
    * Remove deprecated call
    
    * Remove clazz
    
    * Create structure for kafka client classes
    
    * Undo
    
    * Fix style
    
    * Add consumer offset and log collection (#13944)
    
    * Refactor broker offset metric collection (#13934)
    
    * Add broker offset metric collection
    
    * Change import
    
    * Clean up broker offset functions and change names
    
    * Fix style
    
    * Use updated values for check
    
    * Clean up functions
    
    * Refactor client creation (#13946)
    
    * Refactor client creation
    
    * Add back e2e test
    
    * Remove commented out line
    
    * Remove KafkaClient and refactor tests (#13954)
    
    * Revert "Remove KafkaClient and refactor tests (#13954)"
    
    This reverts commit e327d71fb21180731720c0772dff3168af57bc6e.
    
    ---------
    
    Co-authored-by: Fanny Jiang <fanny.jiang@datadoghq.com>

---
+++ kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
@@ -0,0 +1,21 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+class ConfluentKafkaClient:
+    def __init__(self) -> None:
+        pass
+
+    def get_consumer_offsets(self):
+        pass
+
+    def get_broker_offset(self):
+        pass
+
+    def report_consumer_offset_and_lag(self):
+        pass
+
+    def report_broker_offset(self):
+        pass
+
+    def collect_broker_metadata(self):
+        pass
