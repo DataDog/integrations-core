20214c5f427f90ad81cb95ac8f13381575ec6c2e
Fanny Jiang <fanny.jiang@datadoghq.com>
2023-02-15 15:56:54 -0500

    fix spaces

--- kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
+++ kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
@@ -157,8 +157,8 @@ class KafkaCheck(AgentCheck, ConfigMixin):
     def collect_broker_metadata(self):
         self.client.collect_broker_metadata()
 
-    # TODO: Remove me once the tests are refactored
 
+    # TODO: Remove me once the tests are refactored
     def send_event(self, title, text, tags, event_type, aggregation_key, severity='info'):
         """Emit an event to the Datadog Event Stream."""
         event_dict = {
