1831009dda9f2a2e40666e49dc534193e871f8f5
Florent Clarret <florent.clarret@datadoghq.com>
2023-02-16 16:46:25 +0100

    Add missing license headers

--- kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client_factory.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client_factory.py
@@ -1,3 +1,7 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
 from datadog_checks.kafka_consumer.client.kafka_client import KafkaClient
 from datadog_checks.kafka_consumer.client.kafka_python_client import KafkaPythonClient
 
