efcb6a3ee35c8a60b11c37915fc0a368f9965ed0
Fanny Jiang <fanny.jiang@datadoghq.com>
2023-02-16 11:36:45 -0500

    move tls_context back into check

--- kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client_factory.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client_factory.py
@@ -6,5 +6,5 @@ from datadog_checks.kafka_consumer.client.kafka_client import KafkaClient
 from datadog_checks.kafka_consumer.client.kafka_python_client import KafkaPythonClient
 
 
-def make_client(check, config) -> KafkaClient:
-    return KafkaPythonClient(check, config)
+def make_client(check, config, tls_context) -> KafkaClient:
+    return KafkaPythonClient(check, config, tls_context)
