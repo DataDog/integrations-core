a50b84092ceab70ffe51065b6de8722d88f2b24e
steveny91 <steven.yuen@datadoghq.com>
2023-02-15 21:14:59 -0500

    request_nltm got renamed

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/licenses.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/licenses.py
@@ -58,7 +58,7 @@ EXPLICIT_LICENSES = {
     # https://github.com/requests/requests-kerberos/pull/123
     'requests-kerberos': ['ISC'],
     # https://github.com/requests/requests-ntlm/blob/master/LICENSE
-    'requests_ntlm': ['ISC'],
+    'requests-ntlm': ['ISC'],
     # https://github.com/rethinkdb/rethinkdb-python/blob/master/LICENSE
     'rethinkdb': ['Apache-2.0'],
     # https://github.com/simplejson/simplejson/blob/master/LICENSE.txt
