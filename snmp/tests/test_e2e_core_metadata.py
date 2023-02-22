2d22cec646f5b4cd6fcbd74456775063063c1241
NouhaManai <nouha.manai@yahoo.fr>
2023-02-22 17:36:12 +0100

    add e2e_core_metadata test for 9901

--- snmp/tests/test_e2e_core_metadata.py
+++ snmp/tests/test_e2e_core_metadata.py
@@ -1045,3 +1045,43 @@ def test_e2e_core_metadata_cisco_asr_9001(dd_agent_check):
         u'version': u'6.4.2',
     }
     assert_device_metadata(aggregator, device)
+
+
+def test_e2e_core_metadata_cisco_asr_9901(dd_agent_check):
+    config = common.generate_container_instance_config([])
+    instance = config['instances'][0]
+    instance.update(
+        {
+            'community_string': 'cisco-asr-9901',
+            'loader': 'core',
+        }
+    )
+
+    aggregator = dd_agent_check(config, rate=False)
+
+    device_ip = instance['ip_address']
+
+    device = {
+        u'description': u'Cisco IOS XR Software (ASR9K), Version 7.1.3  Copyright (c) '
+                 '2013-2020 by Cisco Systems, Inc.',
+        u'id': u'default:' + device_ip,
+        u'id_tags': [
+            u'device_namespace:default',
+            u'snmp_device:' + device_ip,
+        ],
+        u'ip_address': device_ip,
+        u'model': u'ASR9K',
+        u'os_name': u'IOSXR',
+        u'profile': u'cisco-asr',
+        u'status': 1,
+        u'sys_object_id': u'1.3.6.1.4.1.9.1.2658',
+        u'tags': [
+            u'device_namespace:default',
+            u'device_vendor:cisco',
+            u'snmp_device:' + device_ip,
+            u'snmp_profile:cisco-asr',
+        ],
+        u'vendor': u'cisco',
+        u'version': u'7.1.3',
+    }
+    assert_device_metadata(aggregator, device)
