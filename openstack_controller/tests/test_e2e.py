3d0b31ff1ad3b9433d11e8a386d2efe115b14913
Sarah Witt <sarah.witt@datadoghq.com>
2023-02-23 15:49:03 -0500

    Assert service checks in e2e test

--- openstack_controller/tests/test_e2e.py
+++ openstack_controller/tests/test_e2e.py
@@ -4,6 +4,8 @@
 
 import pytest
 
+from datadog_checks.base import AgentCheck
+
 from . import common
 
 
@@ -13,3 +15,6 @@ def test_check(dd_agent_check):
     for metric in common.DEFAULT_METRICS:
         aggregator.assert_metric(metric)
     aggregator.assert_all_metrics_covered()
+    aggregator.assert_service_check('openstack.neutron.api.up', AgentCheck.OK, count=1)
+    aggregator.assert_service_check('openstack.nova.api.up', AgentCheck.OK, count=1)
+    aggregator.assert_service_check('openstack.keystone.api.up', AgentCheck.OK, count=1)
