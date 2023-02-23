0a9c2cdc69e07c2e3ee4a4598093e137de075c63
Sarah Witt <sarah.witt@datadoghq.com>
2023-02-23 15:52:54 -0500

    Add comments and more service checks

--- openstack_controller/tests/test_e2e.py
+++ openstack_controller/tests/test_e2e.py
@@ -12,9 +12,16 @@ from . import common
 @pytest.mark.e2e
 def test_check(dd_agent_check):
     aggregator = dd_agent_check()
+
+    # assert default metrics
     for metric in common.DEFAULT_METRICS:
         aggregator.assert_metric(metric)
     aggregator.assert_all_metrics_covered()
+
+    # assert service checks
     aggregator.assert_service_check('openstack.neutron.api.up', AgentCheck.OK, count=1)
     aggregator.assert_service_check('openstack.nova.api.up', AgentCheck.OK, count=1)
+    aggregator.assert_service_check('openstack.keystone.api.up', AgentCheck.OK, count=1)
+    aggregator.assert_service_check('openstack.nova.hypervisor.up', AgentCheck.OK, count=10)
+    aggregator.assert_service_check('openstack.neutron.network.up', AgentCheck.OK, count=2)
     aggregator.assert_service_check('openstack.keystone.api.up', AgentCheck.OK, count=1)
