2e35ad4a2cdd302d1548030a601fe92cff292320
root <root@ip-172-21-28-134.ec2.internal>
2023-02-24 21:32:28 +0000

    add the new option to tests

--- sqlserver/tests/conftest.py
+++ sqlserver/tests/conftest.py
@@ -91,6 +91,7 @@ def instance_docker(instance_docker_defaults):
             'include_fci_metrics': True,
             'include_ao_metrics': False,
             'include_master_files_metrics': True,
+            'include_query_plan_cache_metrics': True,
             'disable_generic_tags': True,
         }
     )
