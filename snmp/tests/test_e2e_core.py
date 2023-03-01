6aeb5eaa3aee10bf4cd6d93efcdb7b54b379e914
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-03-01 18:28:54 +0100

    test custom speed

--- snmp/tests/test_e2e_core.py
+++ snmp/tests/test_e2e_core.py
@@ -300,6 +300,10 @@ def test_e2e_meraki_cloud_controller(dd_agent_check):
     for metric in metrics.IF_BANDWIDTH_USAGE:
         aggregator.assert_metric('snmp.{}'.format(metric), metric_type=aggregator.GAUGE, tags=if_tags, count=1)
 
+    custom_speed_tags = if_tags + ['speed_source:snmp']
+    for metric in metrics.IF_CUSTOM_SPEED_GAUGES:
+        aggregator.assert_metric('snmp.{}'.format(metric), metric_type=aggregator.GAUGE, tags=custom_speed_tags, count=2)
+
     aggregator.assert_metric('snmp.sysUpTimeInstance', count=2, tags=common_tags)
     aggregator.assert_metric(
         'snmp.interface.status',
