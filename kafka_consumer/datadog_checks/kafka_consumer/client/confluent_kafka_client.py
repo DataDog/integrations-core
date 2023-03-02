d37b39bae3f5dfb3a01c17558754ca5958041ae5
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-03-02 15:22:49 -0500

    Fix style

--- kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
@@ -1,8 +1,10 @@
 # (C) Datadog, Inc. 2023-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from datadog_checks.kafka_consumer.client.kafka_client import KafkaClient
 from confluent_kafka.admin import AdminClient
+
+from datadog_checks.kafka_consumer.client.kafka_client import KafkaClient
+
 
 class ConfluentKafkaClient(KafkaClient):
     @property
