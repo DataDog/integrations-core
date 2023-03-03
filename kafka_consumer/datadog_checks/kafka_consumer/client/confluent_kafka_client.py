74e3b19b00138c0cae639e97b6a71c310817e9f2
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-03-03 09:59:05 -0500

    Create Kafka client for confluent lib (#14078)
    * Create Kafka client for confluent lib
    
    * Fix style
    
    * Validate kafka_connect_str

--- kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
@@ -1,10 +1,24 @@
 # (C) Datadog, Inc. 2023-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from confluent_kafka.admin import AdminClient
+
 from datadog_checks.kafka_consumer.client.kafka_client import KafkaClient
 
 
 class ConfluentKafkaClient(KafkaClient):
+    @property
+    def kafka_client(self):
+        if self._kafka_client is None:
+            self._kafka_client = AdminClient(
+                {
+                    "bootstrap.servers": self.config._kafka_connect_str,
+                    "socket.timeout.ms": self.config._request_timeout_ms,
+                    "client.id": "dd-agent",
+                }
+            )
+        return self._kafka_client
+
     def create_kafka_admin_client(self):
         raise NotImplementedError
 
