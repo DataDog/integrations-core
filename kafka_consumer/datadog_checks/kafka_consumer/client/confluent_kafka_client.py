579fe1e587d007979184d142059cc1784898d47f
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-03-03 17:32:14 -0500

    Add exception handling

--- kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/confluent_kafka_client.py
@@ -2,7 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from confluent_kafka.admin import AdminClient
-
+from confluent_kafka import KafkaException
 from datadog_checks.kafka_consumer.client.kafka_client import KafkaClient
 
 
@@ -35,10 +35,15 @@ class ConfluentKafkaClient(KafkaClient):
         raise NotImplementedError
 
     def get_partitions_for_topic(self, topic):
-        cluster_metadata = self.kafka_client.list_topics(topic)
-        topics = cluster_metadata.topics
-        partitions = list(topics[topic].partitions.keys())
-        return partitions or []
+        try:
+            cluster_metadata = self.kafka_client.list_topics(topic)
+            topic_metadata = cluster_metadata.topics[topic]
+            partitions = list(topic_metadata.partitions.keys())
+            return partitions
+        except KafkaException as e:
+            self.log.error("Received exception when getting partitions for topic %s: %s" % topic, e)
+            return None
+
 
     def request_metadata_update(self):
         raise NotImplementedError
