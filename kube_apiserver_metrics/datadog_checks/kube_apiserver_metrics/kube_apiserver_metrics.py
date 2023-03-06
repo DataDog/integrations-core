6584ce8e129362021c2d3111dbeca289fc76727f
hipatel <v-hipatel@expediagroup.com>
2023-03-06 10:58:03 -0500

    Adding api server additional metrics

--- kube_apiserver_metrics/datadog_checks/kube_apiserver_metrics/kube_apiserver_metrics.py
+++ kube_apiserver_metrics/datadog_checks/kube_apiserver_metrics/kube_apiserver_metrics.py
@@ -67,6 +67,12 @@ METRICS = {
     'apiserver_storage_list_fetched_objects_total': 'storage_list_fetched_objects_total',
     'apiserver_storage_list_evaluated_objects_total': 'storage_list_evaluated_objects_total',
     'apiserver_storage_list_returned_objects_total': 'storage_list_returned_objects_total',
+
+    'apiserver_envelope_encryption_dek_cache_fill_percent': 'envelope_encryption_dek_cache_fill_percent',
+    'aggregator_unavailable_apiservice': 'aggregator_unavailable_apiservice',
+    'apiserver_flowcontrol_request_concurrency_limit': 'flowcontrol_request_concurrency_limit',
+    'apiserver_flowcontrol_current_executing_requests': 'flowcontrol_current_executing_requests',
+    'apiserver_flowcontrol_rejected_requests_total':'flowcontrol_rejected_requests_total',
 }
 
 
