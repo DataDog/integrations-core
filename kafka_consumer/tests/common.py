2ab41b2f1ff87aee3f17ac03b0af6ce1048100c7
Florent Clarret <florent.clarret@datadoghq.com>
2023-03-07 10:23:42 +0100

    kerberos docker compose

--- kafka_consumer/tests/common.py
+++ kafka_consumer/tests/common.py
@@ -14,11 +14,14 @@ HOST_IP = socket.gethostbyname(HOST)
 KAFKA_CONNECT_STR = f'{HOST_IP}:9092'
 TOPICS = ['marvel', 'dc']
 PARTITIONS = [0, 1]
-DOCKER_IMAGE_PATH = os.path.join(HERE, 'docker', 'docker-compose.yaml')
 KAFKA_VERSION = os.environ.get('KAFKA_VERSION')
 BROKER_METRICS = ['kafka.broker_offset']
 CONSUMER_METRICS = ['kafka.consumer_offset', 'kafka.consumer_lag']
 LEGACY_CLIENT = is_affirmative(os.environ.get('LEGACY_CLIENT', 'false'))
+AUTHENTICATION = os.environ.get('AUTHENTICATION', 'noauth')
+DOCKER_FILE = os.environ.get('DOCKERFILE', 'docker-compose.yaml')
+DOCKER_IMAGE_PATH = os.path.join(HERE, 'docker', DOCKER_FILE)
+
 
 metrics = BROKER_METRICS + CONSUMER_METRICS
 
