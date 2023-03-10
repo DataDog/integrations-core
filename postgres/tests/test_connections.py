acc5a478691048617a7ed317be221308fe157aea
root <root@ip-172-21-28-134.ec2.internal>
2023-03-10 18:58:38 +0000

    add assertions on stats

--- postgres/tests/test_connections.py
+++ postgres/tests/test_connections.py
@@ -19,8 +19,10 @@ def test_conn_pool(pg_instance):
 
     pool = MultiDatabaseConnectionPool(check._new_connection)
     db = pool.get_connection('postgres', 1)
+    assert pool._stats.connection_opened == 1
     pool.prune_connections()
     assert len(pool._conns) == 1
+    assert pool._stats.connection_closed == 0
 
     with db.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
         cursor.execute("select 1")
@@ -30,12 +32,17 @@ def test_conn_pool(pg_instance):
     time.sleep(0.001)
     pool.prune_connections()
     assert len(pool._conns) == 0
+    assert pool._stats.connection_closed == 1
+    assert pool._stats.connection_closed_failed == 0
 
     db = pool.get_connection('postgres', 999 * 1000)
     assert len(pool._conns) == 1
+    assert pool._stats.connection_opened == 2
     success = pool.close_all_connections()
     assert success
     assert len(pool._conns) == 0
+    assert pool._stats.connection_closed == 2
+    assert pool._stats.connection_closed_failed == 0
 
 
 def test_conn_pool_no_leaks(pg_instance):
@@ -46,16 +53,17 @@ def test_conn_pool_no_leaks(pg_instance):
     check = PostgreSql('postgres', {}, [pg_instance])
 
     pool = MultiDatabaseConnectionPool(check._new_connection)
+    pool2 = MultiDatabaseConnectionPool(check._new_connection)  # Used to make verification queries
     ttl_long = 90 * 1000
     ttl_short = 1
 
     def get_time():
-        with pool.get_connection('postgres', 1).cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        with pool2.get_connection('postgres', 1).cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
             cursor.execute("SELECT now()")
             return cursor.fetchone()
 
     def get_activity():
-        with pool.get_connection('postgres', 1).cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+        with pool2.get_connection('postgres', 1).cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
             cursor.execute(
                 "SELECT pid, datname, usename, state, query_start, state_change FROM pg_stat_activity "
                 "WHERE usename = 'datadog' AND datname LIKE 'dogs%%' AND query_start > %s",
@@ -70,15 +78,19 @@ def test_conn_pool_no_leaks(pg_instance):
             with db.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
                 cursor.execute("select current_database()")
                 rows = cursor.fetchall()
-                assert len(rows) == 1 and rows[0][0] == dbname
+                assert len(rows) == 1
+                assert rows[0][0] == dbname
 
     try:
         start_time = get_time()
         pool.close_all_connections()
 
+        pool._stats.reset()
+
         # Create many connections with long-lived TTLs
         get_many_connections(50, ttl_long)
         assert len(pool._conns) == 50
+        assert pool._stats.connection_opened == 50
         # Ensure those connections have the correct deadline and connection status
         for i in range(0, 50):
             dbname = 'dogs_{}'.format(i)
@@ -97,11 +109,14 @@ def test_conn_pool_no_leaks(pg_instance):
         assert len(set(row['datname'] for row in rows)) == 50
         assert all(row['state'] == 'idle' for row in rows)
 
+        pool._stats.reset()
+
         # Repeat this process many times and expect that only one connection is created per database
         for _ in range(500):
             get_many_connections(51, ttl_long)
+            assert pool._stats.connection_opened == 1
 
-            attempts_to_verify = 5
+            attempts_to_verify = 10
             # Loop here to prevent flakiness. Sometimes postgres doesn't immediately terminate backends.
             # The test can be considered successful as long as the backend is eventually terminated.
             for attempt in range(attempts_to_verify):
@@ -119,10 +134,20 @@ def test_conn_pool_no_leaks(pg_instance):
             assert len(set(row['datname'] for row in rows)) == 51
             assert len(rows) == 51, 'Possible leaked connections'
             assert all(row['state'] == 'idle' for row in rows)
+        assert pool._stats.connection_opened == 1
+        assert pool._stats.connection_closed == 0
+
+        pool._stats.reset()
 
         # Now update db connections with short-lived TTLs and expect them to self-prune
         get_many_connections(55, ttl_short)
+        time.sleep(0.001)
         pool.prune_connections()
+        
+        assert pool._stats.connection_opened == 55-51
+        assert pool._stats.connection_closed == 55
+        assert pool._stats.connection_pruned == 55
+        assert pool._stats.connection_closed_failed == 0
         attempts_to_verify = 10
         for attempt in range(attempts_to_verify):
             leaked_rows = get_activity()
@@ -134,7 +159,11 @@ def test_conn_pool_no_leaks(pg_instance):
         # Final check that the server contains no leaked connections still open
         rows = get_activity()
         assert len(rows) == 0
+    # except:
+    #     import pdb
 
+    #     pdb.set_trace()
+    #     raise
     finally:
         success = pool.close_all_connections()
         print('Successfully closed all connections? {}'.format(success))
