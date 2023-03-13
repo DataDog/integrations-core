bf04ff0656c8fda0efb87317e14c9a2619ce2cf4
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2023-03-13 13:16:21 +0100

    Added unit tests

---
+++ openstack_controller/tests/test_unit.py
@@ -0,0 +1,123 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+from contextlib import nullcontext as does_not_raise
+
+import mock
+import pytest
+
+from datadog_checks.dev.http import MockResponse
+from datadog_checks.openstack_controller import OpenStackControllerCheck
+
+pytestmark = [pytest.mark.unit]
+
+
+@pytest.mark.parametrize(
+    'instance, expected_exception',
+    [
+        pytest.param(
+            {},
+            pytest.raises(
+                Exception, match='Either `keystone_server_url` or `openstack_config_file_path` need to be configured'
+            ),
+            id='keystone_server_url_not_configured',
+        ),
+        pytest.param(
+            {'keystone_server_url': 'http://127.0.0.1/identity'},
+            pytest.raises(Exception, match='`user_name` and `user_password` need to be configured'),
+            id='user_name_not_configured',
+        ),
+        pytest.param(
+            {'keystone_server_url': 'http://127.0.0.1/identity', 'user_name': 'admin'},
+            pytest.raises(Exception, match='`user_name` and `user_password` need to be configured'),
+            id='user_password_not_configured',
+        ),
+        pytest.param(
+            {
+                'keystone_server_url': 'http://127.0.0.1:8080/identity',
+                'user_name': 'admin',
+                'user_password': 'password',
+            },
+            does_not_raise(),
+            id='ok',
+        ),
+    ],
+)
+@mock.patch(
+    'requests.get',
+    side_effect=[
+        MockResponse(status_code=200, json_data={}),
+        MockResponse(status_code=200, json_data={'projects': []}),
+    ],
+)
+@mock.patch(
+    'requests.post', side_effect=[MockResponse(status_code=200, json_data={}, headers={'X-Subject-Token': 'abcd'})]
+)
+def test_config_validation(
+    mocked_post,
+    mocked_get,
+    aggregator,
+    dd_run_check,
+    instance,
+    expected_exception,
+):
+    check = OpenStackControllerCheck('test', {}, [instance])
+    with expected_exception:
+        dd_run_check(check)
+
+
+@mock.patch('requests.get', side_effect=[MockResponse(status_code=500)])
+def test_keystone_server_down(
+    mocked_get,
+    aggregator,
+    dd_run_check,
+):
+    instance = {'keystone_server_url': 'http://127.0.0.1/identity', 'user_name': 'admin', 'user_password': 'password'}
+    check = OpenStackControllerCheck('test', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_service_check('openstack.keystone.api.up', status=check.CRITICAL)
+
+
+@mock.patch('requests.get', side_effect=[MockResponse(status_code=200, json_data={})])
+@mock.patch('requests.post', side_effect=[MockResponse(status_code=401)])
+def test_keystone_server_up_and_credentials_fail(
+    mocked_post,
+    mocked_get,
+    aggregator,
+    dd_run_check,
+):
+    instance = {
+        'keystone_server_url': 'http://127.0.0.1/identity',
+        'user_name': 'admin',
+        'user_password': 'password',
+    }
+    check = OpenStackControllerCheck('test', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_service_check('openstack.keystone.api.up', status=check.CRITICAL)
+
+
+@mock.patch(
+    'requests.get',
+    side_effect=[
+        MockResponse(status_code=200, json_data={}),
+        MockResponse(status_code=200, json_data={'projects': []}),
+    ],
+)
+@mock.patch(
+    'requests.post', side_effect=[MockResponse(status_code=200, json_data={}, headers={'X-Subject-Token': 'abcd'})]
+)
+def test_keystone_server_up_and_credentials_ok(
+    mocked_post,
+    mocked_get,
+    aggregator,
+    dd_run_check,
+):
+    instance = {
+        'keystone_server_url': 'http://127.0.0.1/identity',
+        'user_name': 'admin',
+        'user_password': 'password',
+    }
+    check = OpenStackControllerCheck('test', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_service_check('openstack.keystone.api.up', status=check.OK)
