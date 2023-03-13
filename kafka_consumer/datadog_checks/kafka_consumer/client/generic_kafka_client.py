075c8fa4c9a9cad2f05d90e5beab3ddf9501686b
Florent Clarret <florent.clarret@datadoghq.com>
2023-03-13 16:13:57 +0100

    Remove some unused functions

--- kafka_consumer/datadog_checks/kafka_consumer/client/generic_kafka_client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/generic_kafka_client.py
@@ -53,10 +53,3 @@ class GenericKafkaClient(KafkaClient):
             return self.python_kafka_client.get_consumer_offsets_dict()
         return self.confluent_kafka_client.get_consumer_offsets_dict()
 
-    def create_kafka_admin_client(self):
-        # TODO when this method is implemented in ConfluentKafkaClient, replace this with:
-        # if self.use_legacy_client:
-        #     return self.python_kafka_client.get_consumer_offsets()
-        # return self.confluent_kafka_client.get_consumer_offsets()
-
-        return self.python_kafka_client.create_kafka_admin_client()
--- kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client/kafka_client.py
@@ -13,9 +13,6 @@ class KafkaClient(ABC):
         self._consumer_offsets = {}
         self._tls_context = tls_context
 
-    @abstractmethod
-    def create_kafka_admin_client(self):
-        pass
 
     @abstractmethod
     def get_consumer_offsets(self):
