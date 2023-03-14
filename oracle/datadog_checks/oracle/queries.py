e35e96f95ac007a11036619d48430b46113930db
jcondello <jake.condello@ellucian.com>
2023-03-14 12:42:14 -0400

    Reverted ProcessMetrics query back to original with pid added and updated appropriate tests

--- oracle/datadog_checks/oracle/queries.py
+++ oracle/datadog_checks/oracle/queries.py
@@ -5,9 +5,9 @@
 
 ProcessMetrics = {
     'name': 'process',
-    'query': 'SELECT PROGRAM, SUM(PGA_USED_MEM), SUM(PGA_ALLOC_MEM), SUM(PGA_FREEABLE_MEM), SUM(PGA_MAX_MEM)\
-         FROM GV$PROCESS GROUP BY PROGRAM',
+    'query': 'SELECT PID, PROGRAM, PGA_USED_MEM, PGA_ALLOC_MEM, PGA_FREEABLE_MEM, PGA_MAX_MEM FROM GV$PROCESS',
     'columns': [
+        {'name': 'pid', 'type': 'tag'},
         {'name': 'program', 'type': 'tag'},
         {'name': 'process.pga_used_memory', 'type': 'gauge'},
         {'name': 'process.pga_allocated_memory', 'type': 'gauge'},
