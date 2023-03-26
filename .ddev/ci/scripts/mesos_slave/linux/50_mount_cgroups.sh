907d1b91f9214b20d092b3b229c0ff3368d06893
Ofek Lev <ofekmeister@gmail.com>
2023-03-26 17:56:37 -0400

    .

--- .ddev/ci/scripts/mesos_slave/linux/50_mount_cgroups.sh
+++ .ddev/ci/scripts/mesos_slave/linux/50_mount_cgroups.sh
@@ -4,10 +4,4 @@ set -ex
 
 docker info
 
-sudo apt update
-sudo apt -y install cgroup-tools
-
-sudo mkdir /sys/fs/cgroup/systemd
-sudo mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
-
 set +ex
