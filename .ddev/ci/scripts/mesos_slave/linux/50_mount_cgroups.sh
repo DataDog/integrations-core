bb089b5757c94ac58c9d1d81077af70d7e6e8b97
Ofek Lev <ofekmeister@gmail.com>
2023-03-26 19:25:53 -0400

    .

--- .ddev/ci/scripts/mesos_slave/linux/50_mount_cgroups.sh
+++ .ddev/ci/scripts/mesos_slave/linux/50_mount_cgroups.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-mkdir -p $HOME/.docker/cli-plugins
-curl -sLo $HOME/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64
-sudo chmod +x $HOME/.docker/cli-plugins/docker-compose
+# mkdir -p $HOME/.docker/cli-plugins
+# curl -sLo $HOME/.docker/cli-plugins/docker-compose https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64
+# sudo chmod +x $HOME/.docker/cli-plugins/docker-compose
 
 set +ex
