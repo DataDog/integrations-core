c2751152f325db99823e41fcf055b7b5e94d2897
Ofek Lev <ofekmeister@gmail.com>
2023-03-26 18:04:12 -0400

    .

--- .ddev/ci/scripts/mesos_slave/linux/50_mount_cgroups.sh
+++ .ddev/ci/scripts/mesos_slave/linux/50_mount_cgroups.sh
@@ -4,4 +4,10 @@ set -ex
 
 docker info
 
+echo 'GRUB_CMDLINE_LINUX=systemd.unified_cgroup_hierarchy=false' > /etc/default/grub.d/cgroup.cfg
+update-grub
+
+# sudo mkdir /sys/fs/cgroup/systemd
+# sudo mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
+
 set +ex
