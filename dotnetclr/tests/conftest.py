a92bac31cafdb6b19f85ce688cc7ae79c1def773
david-donovan-dd <75643014+david-donovan-dd@users.noreply.github.com>
2023-04-03 10:50:22 -0400

    Create minimal e2e test for dotnetclr
    https://datadoghq.atlassian.net/browse/AI-2990

---
+++ dotnetclr/tests/conftest.py
@@ -0,0 +1,31 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from copy import deepcopy
+
+import pytest
+
+from datadog_checks.dotnetclr import dotnetclr
+
+from .common import INSTANCE
+
+
+@pytest.fixture(scope="session")
+def dd_environment():
+    yield INSTANCE, {
+        'docker_platform': 'windows',
+        #'start_commands': [
+            # Install IIS
+        #    'powershell.exe -Command Add-WindowsFeature Web-Server'
+        #],
+    }
+
+
+@pytest.fixture
+def check():
+    return lambda instance: dotnetclr('dotnetclr', {}, [instance])
+
+
+@pytest.fixture
+def instance():
+    return deepcopy(INSTANCE)
