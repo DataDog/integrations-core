b41e10fa544f0e53bd6a6109668c87f39de52055
Ofek Lev <ofekmeister@gmail.com>
2023-04-03 15:14:01 -0400

    Fix SingleStore CI script for Azure Pipelines (#14306)

--- .ddev/ci/scripts/singlestore/linux/55_set_license_secret.sh
+++ .ddev/ci/scripts/singlestore/linux/55_set_license_secret.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 set +x
 
-echo "SINGLESTORE_LICENSE=$SINGLESTORE_LICENSE" >> "$GITHUB_ENV"
+echo "SINGLESTORE_LICENSE=$SINGLESTORE_LICENSE" >> "${GITHUB_ENV:-/tmp/gh-output}"
 
 set -x
