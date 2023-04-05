717ead06271a0c9d7bd6cc58d6d0f3c91d4d048f
david-donovan-dd <david.donovan@datadoghq.com>
2023-04-05 11:32:22 -0400

    Adjusting aggregator assertion

--- dotnetclr/tests/e2e_test.py
+++ dotnetclr/tests/e2e_test.py
@@ -3,15 +3,15 @@
 # Licensed under Simplified BSD License (see LICENSE)
 import pytest
 
-from datadog_checks.base.constants import ServiceCheck
 from datadog_checks.dev.testing import requires_py3
+from datadog_checks.dotnetclr import DotnetclrCheckV2
 
-from .common import INSTANCE
+from .common import MINIMAL_INSTANCE
 
 pytestmark = pytest.mark.e2e
 
 
 @requires_py3
 def test(dd_agent_check):
-    aggregator = dd_agent_check(INSTANCE, rate=True)
-    aggregator.assert_service_check('test.windows.perf.health', ServiceCheck.OK)
+    aggregator = dd_agent_check(MINIMAL_INSTANCE, rate=True)
+    aggregator.assert_service_check('dotnetclr.windows.perf.health', DotnetclrCheckV2.OK)
