563e2e5fbdcaf35c4ef18f70a49840e4c75592e4
Florent Clarret <florent.clarret@datadoghq.com>
2023-04-11 13:57:31 +0000

    Implement Temporal integration (#13833)
    * Add the test structure
    
    * Remove setup.py
    
    * Populate METRICS_MAP
    
    * Populate metadata.csv
    
    * Refine metrics tests
    
    * Set version metadata from metric
    
    * Update documentation
    
    * Update manifest.json
    
    * Adjust metric names
    
    * Remove deprecated error metrics
    
    Starting with
    v1.17 (https://github.com/temporalio/temporal/releases/tag/v1.17.0),
    service_errors_* and persistence_errors_* metrics were replaced by a
    single metric that includes the error as a label. Better to remove
    them already from the first version of the integration.
    
    * Add descriptions and units to metadata.csv
    
    * Mention separate deployment of Temporal services in the README
    
    * Add license header to script
    
    * Improve instructions on how to run the metadata generation script
    
    * Update README.md with suggestions
    
    Co-authored-by: Rosa Trieu <107086888+rtrieu@users.noreply.github.com>
    
    * Apply title casing to "Temporal" more consistently
    
    Co-authored-by: Andrew Zhang <andrew.zhang@datadoghq.com>
    
    * Bump agent_version in temporal/assets/service_checks.json
    
    Co-authored-by: Andrew Zhang <andrew.zhang@datadoghq.com>
    
    * Add tests to GitHub actions CI
    
    * Sync config
    
    ---------
    
    Co-authored-by: Alex Lopez <alex.lopez@datadoghq.com>
    Co-authored-by: Alex Lopez <alex.lopez.zorzano@gmail.com>
    Co-authored-by: Rosa Trieu <107086888+rtrieu@users.noreply.github.com>
    Co-authored-by: Andrew Zhang <andrew.zhang@datadoghq.com>

--- temporal/datadog_checks/temporal/check.py
+++ temporal/datadog_checks/temporal/check.py
@@ -1,98 +1,29 @@
 # (C) Datadog, Inc. 2023-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from typing import Any
 
-from datadog_checks.base import AgentCheck
-
-# from datadog_checks.base.utils.db import QueryManager
-# from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
-# from json import JSONDecodeError
+from datadog_checks.base import OpenMetricsBaseCheckV2
+from datadog_checks.temporal.config_models import ConfigMixin
+from datadog_checks.temporal.metrics import METRIC_MAP
 
 
-class TemporalCheck(AgentCheck):
+class TemporalCheck(OpenMetricsBaseCheckV2, ConfigMixin):
+    __NAMESPACE__ = 'temporal.server'
+    DEFAULT_METRIC_LIMIT = 0
 
-    # This will be the prefix of every metric and service check the integration sends
-    __NAMESPACE__ = 'temporal'
+    def get_default_config(self):
+        return {
+            "metrics": [METRIC_MAP],
+        }
 
-    def __init__(self, name, init_config, instances):
-        super(TemporalCheck, self).__init__(name, init_config, instances)
+    def configure_scrapers(self):
+        super().configure_scrapers()
 
-        # Use self.instance to read the check configuration
-        # self.url = self.instance.get("url")
+        self.scrapers[self.instance['openmetrics_endpoint']].metric_transformer.add_custom_transformer(
+            "build_information",
+            self._transform_build_information,
+        )
 
-        # If the check is going to perform SQL queries you should define a query manager here.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
-        # sample_query = {
-        #     "name": "sample",
-        #     "query": "SELECT * FROM sample_table",
-        #     "columns": [
-        #         {"name": "metric", "type": "gauge"}
-        #     ],
-        # }
-        # self._query_manager = QueryManager(self, self.execute_query, queries=[sample_query])
-        # self.check_initializations.append(self._query_manager.compile_queries)
-
-    def check(self, _):
-        # type: (Any) -> None
-        # The following are useful bits of code to help new users get started.
-
-        # Perform HTTP Requests with our HTTP wrapper.
-        # More info at https://datadoghq.dev/integrations-core/base/http/
-        # try:
-        #     response = self.http.get(self.url)
-        #     response.raise_for_status()
-        #     response_json = response.json()
-
-        # except Timeout as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request timeout: {}, {}".format(self.url, e),
-        #     )
-        #     raise
-
-        # except (HTTPError, InvalidURL, ConnectionError) as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
-
-        # except JSONDecodeError as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="JSON Parse failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
-
-        # except ValueError as e:
-        #     self.service_check(
-        #         "can_connect", AgentCheck.CRITICAL, message=str(e)
-        #     )
-        #     raise
-
-        # This is how you submit metrics
-        # There are different types of metrics that you can submit (gauge, event).
-        # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
-        # self.gauge("test", 1.23, tags=['foo:bar'])
-
-        # Perform database queries using the Query Manager
-        # self._query_manager.execute()
-
-        # This is how you use the persistent cache. This cache file based and persists across agent restarts.
-        # If you need an in-memory cache that is persisted across runs
-        # You can define a dictionary in the __init__ method.
-        # self.write_persistent_cache("key", "value")
-        # value = self.read_persistent_cache("key")
-
-        # If your check ran successfully, you can send the status.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck.service_check
-        # self.service_check("can_connect", AgentCheck.OK)
-
-        # If it didn't then it should send a critical service check
-        self.service_check("can_connect", AgentCheck.CRITICAL)
+    def _transform_build_information(self, metric, sample_data, runtime_data):
+        for sample, *_ in sample_data:
+            self.set_metadata('version', sample.labels['build_version'].replace('_', '.'))
