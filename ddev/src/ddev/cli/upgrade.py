6bd61df8df4e3071e1868422b12ebff13e691dad
steveny91 <steven.yuen@datadoghq.com>
2023-04-11 11:09:52 -0400

    leave comments

--- ddev/src/ddev/cli/upgrade.py
+++ ddev/src/ddev/cli/upgrade.py
@@ -8,6 +8,8 @@ from semver import VersionInfo
 PACKAGE_NAME = 'ddev'
 
 def read_last_run(file_path):
+    # Read the last run the file registry.json and returns a version and a date. 
+    # Format: {"version": "1.6.0", "date": "2023-04-11T10:56:39.786412"}
     try:
         with open(file_path, 'r') as f:
             data = json.load(f)
@@ -20,6 +22,7 @@ def read_last_run(file_path):
     return "0.0.1", datetime.now() - timedelta(days=7)
 
 def write_last_run(file_path, version_value, date):
+    # Records/overwrites the run in the registry.json. If the file isn't there, it will be created
     data = {'version': version_value, 'date': date.isoformat()}
     with open(file_path, 'w') as f:
         json.dump(data, f)
@@ -28,14 +31,17 @@ def exit_handler(app, msg):
     return app.display_info(msg, highlight=False)   
 
 def check_upgrade(app, version):
+    # Finds the current location of the config file to put the registry.json in the same directory
     file_path = app.config_file.path
     dir_path, _file_name = os.path.split(file_path)
     registry_name = 'registry.json'
     registry_file_path = os.path.join(dir_path, registry_name)
+
     current_version = VersionInfo.parse(version)
     last_checked_version, last_run = read_last_run(registry_file_path)
     date_now = datetime.now()
     
+    # Check from last run to see if the data inside the registry.json is older than 7 days and if the last checked version is newer than current.
     if date_now - last_run < timedelta(days=7) and last_checked_version < current_version:
         msg = f'\n!!An upgrade to version {last_checked_version} is available for {PACKAGE_NAME}. Your current version is {current_version}!!'
         atexit.register(exit_handler, app, msg)
@@ -43,6 +49,7 @@ def check_upgrade(app, version):
     
     url = 'https://pypi.org/pypi/ddev/json'
 
+    # Get latest version from PyPI and check with current version. Record the latest version and date.
     try:
         response = requests.get(url, timeout=2)
         response.raise_for_status()
