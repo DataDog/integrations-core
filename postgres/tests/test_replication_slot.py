b8b8b085e5693211f17bf588afc54920a2cdf91a
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2023-04-12 09:30:59 +0200

    Adjust sleep time to match wal receiver status interval

--- postgres/tests/test_replication_slot.py
+++ postgres/tests/test_replication_slot.py
@@ -25,8 +25,8 @@ def test_physical_replication_slots(aggregator, integration_check, pg_instance):
             cur.execute("select * from pg_create_physical_replication_slot('phys_2', true);")
             cur.execute("select * from pg_create_physical_replication_slot('phys_3', true, true);")
 
-    # Wait for replication slot to be visible and up to date in pg_replication_slots
-    time.sleep(0.5)
+    # Wait at least wal_receiver_status_interval time for replication slot to be up to date in pg_replication_slots
+    time.sleep(1.1)
     check.check(pg_instance)
 
     #     slot_name     | slot_type | temporary | active | active_pid | xmin | restart_lsn
