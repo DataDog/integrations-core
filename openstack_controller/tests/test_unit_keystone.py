9c182ea55fc8dc43ce53a004dae74e3b33e4e7e4
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2023-04-24 08:01:11 +0200

    Added domains_metrics to keystone unit tests

--- openstack_controller/tests/test_unit_keystone.py
+++ openstack_controller/tests/test_unit_keystone.py
@@ -44,7 +44,7 @@ def test_endpoint_down(aggregator, dd_run_check, instance, monkeypatch):
 
 
 def test_endpoint_up(aggregator, dd_run_check, instance, monkeypatch):
-    http = MockHttp("agent-integrations-openstack-ironic")
+    http = MockHttp("agent-integrations-openstack-default")
     monkeypatch.setattr('requests.get', mock.MagicMock(side_effect=http.get))
     monkeypatch.setattr('requests.post', mock.MagicMock(side_effect=http.post))
 
@@ -68,5 +68,37 @@ def test_endpoint_up(aggregator, dd_run_check, instance, monkeypatch):
         'openstack.keystone.response_time',
         tags=[
             'keystone_server:{}'.format(instance["keystone_server_url"]),
+        ],
+    )
+
+
+def test_domains_metrics(aggregator, dd_run_check, instance, monkeypatch):
+    http = MockHttp("agent-integrations-openstack-default")
+    monkeypatch.setattr('requests.get', mock.MagicMock(side_effect=http.get))
+    monkeypatch.setattr('requests.post', mock.MagicMock(side_effect=http.post))
+
+    check = OpenStackControllerCheck('test', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_metric(
+        'openstack.keystone.domains.count',
+        value=2,
+        tags=[
+            'keystone_server:{}'.format(instance["keystone_server_url"]),
+        ],
+    )
+    aggregator.assert_metric(
+        'openstack.keystone.domains.enabled',
+        value=1,
+        tags=[
+            'keystone_server:{}'.format(instance["keystone_server_url"]),
+            'domain_id:default',
+        ],
+    )
+    aggregator.assert_metric(
+        'openstack.keystone.domains.enabled',
+        value=1,
+        tags=[
+            'keystone_server:{}'.format(instance["keystone_server_url"]),
+            'domain_id:03e40b01788d403e98e4b9a20210492e',
         ],
     )
