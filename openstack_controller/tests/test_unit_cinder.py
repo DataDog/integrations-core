b8bca1f04a006ded9db93aa42a95ef57fa27c2e2
Sarah Witt <sarah.witt@datadoghq.com>
2023-04-27 18:07:13 -0400

    Add test for cinder

---
+++ openstack_controller/tests/test_unit_cinder.py
@@ -0,0 +1,79 @@
+import mock
+import pytest
+
+from datadog_checks.base import AgentCheck
+from datadog_checks.dev.http import MockResponse
+from datadog_checks.openstack_controller import OpenStackControllerCheck
+
+from .common import MockHttp
+
+pytestmark = [pytest.mark.unit]
+
+
+def test_exception(aggregator, dd_run_check, instance, caplog, monkeypatch):
+    http = MockHttp("agent-integrations-openstack-default", exceptions={'volume/v3/': Exception()})
+    monkeypatch.setattr('requests.get', mock.MagicMock(side_effect=http.get))
+    monkeypatch.setattr('requests.post', mock.MagicMock(side_effect=http.post))
+
+    check = OpenStackControllerCheck('test', {}, [instance])
+    dd_run_check(check)
+    assert 'Exception while reporting block storage metrics' in caplog.text
+
+
+def test_endpoint_down(aggregator, dd_run_check, instance, monkeypatch):
+    http = MockHttp("agent-integrations-openstack-default", defaults={'volume/v3/': MockResponse(status_code=500)})
+    monkeypatch.setattr('requests.get', mock.MagicMock(side_effect=http.get))
+    monkeypatch.setattr('requests.post', mock.MagicMock(side_effect=http.post))
+
+    check = OpenStackControllerCheck('test', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_service_check(
+        'openstack.cinder.api.up',
+        status=AgentCheck.CRITICAL,
+        tags=[
+            'keystone_server:{}'.format(instance["keystone_server_url"]),
+            'project_id:6e39099cccde4f809b003d9e0dd09304',
+            'project_name:admin',
+            'domain_id:default',
+        ],
+    )
+    aggregator.assert_service_check(
+        'openstack.cinder.api.up',
+        status=AgentCheck.CRITICAL,
+        tags=[
+            'keystone_server:{}'.format(instance["keystone_server_url"]),
+            'project_id:1e6e233e637d4d55a50a62b63398ad15',
+            'project_name:demo',
+            'domain_id:default',
+        ],
+    )
+
+
+def test_endpoint_up(aggregator, dd_run_check, instance, monkeypatch):
+    http = MockHttp("agent-integrations-openstack-default")
+    monkeypatch.setattr('requests.get', mock.MagicMock(side_effect=http.get))
+    monkeypatch.setattr('requests.post', mock.MagicMock(side_effect=http.post))
+
+    check = OpenStackControllerCheck('test', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_service_check(
+        'openstack.cinder.api.up',
+        status=AgentCheck.OK,
+        tags=[
+            'keystone_server:{}'.format(instance["keystone_server_url"]),
+            'project_id:1e6e233e637d4d55a50a62b63398ad15',
+            'project_name:demo',
+            'domain_id:default',
+        ],
+    )
+    aggregator.assert_service_check(
+        'openstack.cinder.api.up',
+        status=AgentCheck.OK,
+        tags=[
+            'keystone_server:{}'.format(instance["keystone_server_url"]),
+            'project_id:6e39099cccde4f809b003d9e0dd09304',
+            'project_name:admin',
+            'domain_id:default']
+    )
+
+
