d56d6621bdc6a7d525f9168fa86a339b1120eaa4
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2023-04-28 09:59:56 +0200

    uncomment lines from create_all_mocks.sh

--- openstack_controller/create_all_mocks.sh
+++ openstack_controller/create_all_mocks.sh
@@ -1,14 +1,14 @@
 #!/opt/homebrew/bin/bash
 
-# ./create_mocks.sh "agent-integrations-openstack-default" "" ""
-# ./create_mocks.sh "agent-integrations-openstack-default" "" 1.80
-# ./create_mocks.sh "agent-integrations-openstack-default" 2.93 ""
-# ./create_mocks.sh "agent-integrations-openstack-default" 2.93 1.80
+./create_mocks.sh "agent-integrations-openstack-default" "" ""
+./create_mocks.sh "agent-integrations-openstack-default" "" 1.80
+./create_mocks.sh "agent-integrations-openstack-default" 2.93 ""
+./create_mocks.sh "agent-integrations-openstack-default" 2.93 1.80
 
-# ./create_mocks.sh "agent-integrations-openstack-ironic" "" ""
-# ./create_mocks.sh "agent-integrations-openstack-ironic" "" 1.80
-# ./create_mocks.sh "agent-integrations-openstack-ironic" 2.93 ""
-# ./create_mocks.sh "agent-integrations-openstack-ironic" 2.93 1.80
+./create_mocks.sh "agent-integrations-openstack-ironic" "" ""
+./create_mocks.sh "agent-integrations-openstack-ironic" "" 1.80
+./create_mocks.sh "agent-integrations-openstack-ironic" 2.93 ""
+./create_mocks.sh "agent-integrations-openstack-ironic" 2.93 1.80
 
 ./create_mocks.sh "agent-integrations-openstack-octavia" "" ""
 ./create_mocks.sh "agent-integrations-openstack-octavia" "" 1.80
