b491bab61a163bdecb82ad152f0d61f578d69221
boluwaji-deriv <boluwaji@deriv.com>
2023-05-07 03:01:29 +0800

    Fixed a bug that caused error message spams to a postgres replica DB has replication slot

--- postgres/datadog_checks/postgres/util.py
+++ postgres/datadog_checks/postgres/util.py
@@ -293,12 +293,12 @@ QUERY_PG_REPLICATION_SLOTS = {
     'name': 'pg_replication_slots',
     'query': """
     SELECT
-        a.slot_name, 
-        a.slot_type, 
-        CASE WHEN a.temporary THEN 'temporary' ELSE 'permanent' END, 
+        a.slot_name,
+        a.slot_type,
+        CASE WHEN a.temporary THEN 'temporary' ELSE 'permanent' END,
         CASE WHEN a.active THEN 'active' ELSE 'inactive' END,
         CASE WHEN xmin IS NULL THEN NULL ELSE age(a.xmin) END,
-        pg_wal_lsn_diff(cur.lsn, a.restart_lsn), 
+        pg_wal_lsn_diff(cur.lsn, a.restart_lsn),
         pg_wal_lsn_diff(cur.lsn, a.confirmed_flush_lsn)
     FROM pg_replication_slots as a
     CROSS JOIN (select case when pg_is_in_recovery() then pg_last_wal_receive_lsn() else pg_current_wal_lsn() end) as cur(lsn);
