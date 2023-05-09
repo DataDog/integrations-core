7cdb0dab54b795a677bb3793ba6db83575a003d8
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2023-05-09 17:08:42 +0200

    Fix version check

--- postgres/tests/compose/resources/03_load_data.sh
+++ postgres/tests/compose/resources/03_load_data.sh
@@ -19,7 +19,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" datadog_test <<-EOSQL
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO blocking_bob;
 EOSQL
 
-if [[ "$PG_MAJOR" != "9" && "$PG_MAJOR" != "10" ]]; then
+if [[ !("$PG_MAJOR" == 9.*) && !("$PG_MAJOR" == 10) ]]; then
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" datadog_test <<-EOSQL
     CREATE TABLE test_part (id SERIAL PRIMARY KEY, filler text) PARTITION BY RANGE (id);
     CREATE TABLE test_part1 PARTITION OF test_part FOR VALUES FROM (MINVALUE) TO (500);
