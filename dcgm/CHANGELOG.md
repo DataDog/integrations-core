e00570c0090df0019038ea72c6723f523cc3b31a
ollieharDD <oliver.harrison@datadoghq.com>
2023-05-22 15:55:04 +0200

    Works on metric.py and metadata along with some corrections

--- dcgm/CHANGELOG.md
+++ dcgm/CHANGELOG.md
@@ -1,5 +1,1 @@
 # CHANGELOG - DCGM
-
-## 1.0.0 / 2023-05-17
-
-* [Added] Add the DCGM integration. See [#](https://github.com/DataDog/integrations-core/pull/[****]).
--- dcgm/datadog_checks/dcgm/metrics.py
+++ dcgm/datadog_checks/dcgm/metrics.py
@@ -1,5 +1,19 @@
 
 METRIC_MAP = {
     # Exposed OM Metrics to map:
-    'DCGM_FI_DEV_GPU_TEMP': 'dcgm.temperature',
+    'DCGM_FI_DEV_GPU_TEMP': 'temperature',
+    'DCGM_FI_DEV_DEC_UTIL': 'dec_utilization',
+    'DCGM_FI_DEV_ENC_UTIL': 'enc_utilization',
+    'DCGM_FI_DEV_FB_FREE': 'fb_free',
+    'DCGM_FI_DEV_FB_USED': 'fb_used',
+    'DCGM_FI_DEV_GPU_UTIL': 'gpu_utilization',
+    'DCGM_FI_DEV_MEM_CLOCK': 'mem_clock',
+    'DCGM_FI_DEV_MEM_COPY_UTIL': 'mem_copy_utilization',
+    'DCGM_FI_DEV_NVLINK_BANDWIDTH_TOTAL': 'nvlink_bandwidth_total',
+    'DCGM_FI_DEV_PCIE_REPLAY_COUNTER': 'pcie_replay_counter',
+    'DCGM_FI_DEV_POWER_USAGE': 'power_usage',
+    'DCGM_FI_DEV_SM_CLOCK': 'sm_clock',
+    'DCGM_FI_DEV_TOTAL_ENERGY_CONSUMPTION': 'total_energy_consumption',
+    'DCGM_FI_DEV_VGPU_LICENSE_STATUS': 'vgpu_licence_status',
+    'DCGM_FI_DEV_XID_ERRORS': 'xid_errors',
 }
