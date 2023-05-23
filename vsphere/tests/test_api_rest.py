848ab5a90cd05aef83dacc774318e42cbf6dcf97
Sarah Witt <sarah.witt@datadoghq.com>
2023-05-23 10:01:36 -0400

    Remove hostname

--- vsphere/tests/test_api_rest.py
+++ vsphere/tests/test_api_rest.py
@@ -55,7 +55,7 @@ def test_tagging_tags_get_exception(realtime_instance, dd_run_check, caplog, agg
         "defaultdict(<class 'list'>, {}), <class 'pyVmomi.VmomiSupport.vim.ClusterComputeResource'>: "
         "defaultdict(<class 'list'>, {})}" in caplog.text
 
-        aggregator.assert_metric('vsphere.net.bytesTx.avg', tags=['vcenter_server:FAKE'], hostname='VM4-4')
+        aggregator.assert_metric('vsphere.net.bytesTx.avg', tags=['vcenter_server:FAKE'])
 
 
 @pytest.mark.usefixtures("mock_rest_api", "mock_type", "mock_api")
@@ -80,7 +80,7 @@ def test_tagging_category_get_exception(realtime_instance, dd_run_check, caplog,
         "defaultdict(<class 'list'>, {}), <class 'pyVmomi.VmomiSupport.vim.ClusterComputeResource'>: "
         "defaultdict(<class 'list'>, {})}" in caplog.text
 
-        aggregator.assert_metric('vsphere.net.bytesTx.avg', tags=['vcenter_server:FAKE'], hostname='VM4-4')
+        aggregator.assert_metric('vsphere.net.bytesTx.avg', tags=['vcenter_server:FAKE'])
 
 
 @pytest.mark.usefixtures("mock_rest_api", "mock_type", "mock_api")
@@ -100,7 +100,8 @@ def test_get_tags_log(realtime_instance, dd_run_check, caplog, aggregator):
         "'my_cat_name_2:my_tag_name_2']})" in caplog.text
 
         aggregator.assert_metric(
-            'vsphere.net.bytesTx.avg', tags=['my_cat_name_1:my_tag_name_1', 'vcenter_server:FAKE'], hostname='VM4-4'
+            'vsphere.net.bytesTx.avg',
+            tags=['my_cat_name_1:my_tag_name_1', 'vcenter_server:FAKE'],
         )
 
 
