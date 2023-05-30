b0e256b15dd6edf24e0f70d9bf4eecca9cbf3f77
Florent Clarret <florent.clarret@datadoghq.com>
2023-05-30 12:02:13 +0200

    Test the metadata collection for OMv2

--- envoy/tests/test_unit.py
+++ envoy/tests/test_unit.py
@@ -1,7 +1,7 @@
 # (C) Datadog, Inc. 2023-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-
+import mock
 import pytest
 
 from datadog_checks.base import AgentCheck, ConfigurationError
@@ -48,3 +48,39 @@ def test_check(aggregator, dd_run_check, check, mock_http_response):
     aggregator.assert_all_metrics_covered()
     aggregator.assert_no_duplicate_metrics()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+
+
+@requires_py3
+def test_collect_metadata(datadog_agent, fixture_path, mock_http_response, check, default_instance):
+    c = check(default_instance)
+    c.check_id = 'test:123'
+    c.log = mock.MagicMock()
+
+    mock_http_response(file_path=fixture_path('server_info_api_v3'))
+
+    c._collect_metadata()
+
+    version_metadata = {
+        'version.scheme': 'semver',
+        'version.major': "1",
+        'version.minor': "18",
+        'version.patch': "3",
+        'version.raw': '1.18.3',
+    }
+
+    datadog_agent.assert_metadata('test:123', version_metadata)
+    datadog_agent.assert_metadata_count(len(version_metadata))
+
+
+@requires_py3
+def test_collect_metadata_with_invalid_base_url(
+    datadog_agent, fixture_path, mock_http_response, check, default_instance
+):
+    default_instance["openmetrics_endpoint"] = None
+    c = check(default_instance)
+    c.check_id = 'test:123'
+    c.log = mock.MagicMock()
+
+    c._collect_metadata()
+    datadog_agent.assert_metadata_count(0)
+    c.log.debug.assert_called_with('Skipping server info collection due to malformed url: %s', b'')
