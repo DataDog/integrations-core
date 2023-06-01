8aed20a92560542c4ba9f48587310d3e1ef93564
ollieharDD <oliver.harrison@datadoghq.com>
2023-06-01 10:51:44 +0200

    Improves tests and PR checks

--- dcgm/datadog_checks/dcgm/metrics.py
+++ dcgm/datadog_checks/dcgm/metrics.py
@@ -1,4 +1,3 @@
-
 METRIC_MAP = {
     # Exposed OM Metrics to map:
     'DCGM_FI_DEV_GPU_TEMP': 'temperature',
@@ -9,19 +8,19 @@ METRIC_MAP = {
     'DCGM_FI_DEV_GPU_UTIL': 'gpu_utilization',
     'DCGM_FI_DEV_MEM_CLOCK': 'mem_clock',
     'DCGM_FI_DEV_MEM_COPY_UTIL': 'mem_copy_utilization',
-    'DCGM_FI_DEV_NVLINK_BANDWIDTH_TOTAL': 'nvlink_bandwidth', #removed the total for processing
+    'DCGM_FI_DEV_NVLINK_BANDWIDTH_TOTAL': 'nvlink_bandwidth',  # removed the total for processing
     'DCGM_FI_DEV_PCIE_REPLAY_COUNTER': 'pcie_replay_counter',
     'DCGM_FI_DEV_POWER_USAGE': 'power_usage',
     'DCGM_FI_DEV_SM_CLOCK': 'sm_clock',
     'DCGM_FI_DEV_TOTAL_ENERGY_CONSUMPTION': 'total_energy_consumption',
     'DCGM_FI_DEV_VGPU_LICENSE_STATUS': 'vgpu_license_status',
     'DCGM_FI_DEV_XID_ERRORS': 'xid_errors',
-    #NVML Specific Missing Metrics (5)
+    # NVML Specific Missing Metrics (5)
     'DCGM_FI_DEV_COUNT': 'device_count',
     'DCGM_FI_DEV_FAN_SPEED': 'fan_speed',
     'DCGM_FI_PROF_PCIE_TX_BYTES': 'pcie_tx_throughput',
     'DCGM_FI_PROF_PCIE_RX_BYTES': 'pcie_rx_throughput',
-    #Others from default-counters.csv
+    # Others from default-counters.csv
     'DCGM_FI_DEV_MEMORY_TEMP': 'memory_temperature',
     'DCGM_FI_DEV_UNCORRECTABLE_REMAPPED_ROWS': 'uncorrectable_remapped_rows',
     'DCGM_FI_DEV_CORRECTABLE_REMAPPED_ROWS': 'correctable_remapped_rows',
--- dcgm/tests/test_dcgm.py
+++ dcgm/tests/test_dcgm.py
@@ -3,22 +3,25 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 import os
+from unittest import mock
+
 import pytest
 
-from datadog_checks import * # Might as well try all since I was having problems
-# from datadog_checks.dev.utils import get_metadata_metrics
 # from datadog_checks.base.stubs.aggregator import AggregatorStub #Needed for MERTIS=[]
 from datadog_checks.dcgm import DcgmCheck
-from unittest import mock
+
+# from datadog_checks.dev.utils import get_metadata_metrics
 
 
 @pytest.fixture
 def instance():
     return {"openmetrics_endpoint": "http://host:9400/metrics"}
 
+
 @pytest.fixture
 def check(instance):
     return DcgmCheck('dcgm.', {}, [instance])
+
 
 @pytest.fixture()
 def mock_metrics():
@@ -33,6 +36,7 @@ def mock_metrics():
     ):
         yield
 
+
 def test_check(dd_run_check, aggregator, check, mock_metrics):
     dd_run_check(check)
 
@@ -41,59 +45,35 @@ def test_check(dd_run_check, aggregator, check, mock_metrics):
             name=f"dcgm.{expected_metric['name']}",
         )
 
-#TODO: To collect counter metrics with names ending in `_total`, specify the metric name without the `_total`
-# It seems that those metrics that are not exposed in the curl are causing some issues are they have nothing to correspond to.
+        # TODO Add service check form openmetrics
+
+
+# TODO  add the following
+# def _common_assertions(aggregator):
+#     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+#     aggregator.assert_all_metrics_covered()
+#     aggregator.assert_service_check('rabbitmq.openmetrics.health', status=ServiceCheck.OK)
+
+# TODO: To collect counter metrics with names ending in `_total`, specify the metric name without the `_total`
 
 # A representative sampling of metrics from the fixture used for unit tests
 METRICS = [
-    {
-        "name": "dec_utilization"
-    },
-    {
-        "name": "enc_utilization"
-    },
-    {
-        "name": "fb_free"
-    },
-    {
-        "name": "fb_used"
-    },
-    {
-        "name": "temperature"
-    },
-    {
-        "name": "gpu_utilization"
-    },
-    {
-        "name": "mem_clock"
-    },
-    {
-        "name": "mem_copy_utilization"
-    },
-    # {
-    #     "name": "nvlink_bandwidth_total"  #removed the total for processing
-    # },
-    {
-        "name": "pcie_replay_counter.count" # added .count to pass
-    },
-    {
-        "name": "power_usage"
-    },
-    {
-        "name": "sm_clock"
-    },
-    {
-        "name": "total_energy_consumption.count" # added .count to pass
-    },
-    {
-        "name": "vgpu_license_status"
-    },
-    {
-        "name": "xid_errors"
-    },
-    {
-        "name": "device_count.count"  # added .count to pass
-    },
+    {"name": "dec_utilization"},
+    {"name": "enc_utilization"},
+    {"name": "fb_free"},
+    {"name": "fb_used"},
+    {"name": "temperature"},
+    {"name": "gpu_utilization"},
+    {"name": "mem_clock"},
+    {"name": "mem_copy_utilization"},
+    {"name": "nvlink_bandwidth.count"},  # removed the total for processing
+    {"name": "pcie_replay_counter.count"},  # added .count to pass
+    {"name": "power_usage"},
+    {"name": "sm_clock"},
+    {"name": "total_energy_consumption.count"},  # added .count to pass
+    {"name": "vgpu_license_status"},
+    {"name": "xid_errors"},
+    {"name": "device_count.count"},  # added .count to pass
     # {
     #     "name": "fan_speed"
     # },
@@ -116,3 +96,5 @@ METRICS = [
     #     "name": "row_remap_failure"
     # }
 ]
+
+# TODO Drop the non-exposed metrics.
