222575fd1a83636fe2013cc6589b2bd69bb802d5
Ofek Lev <ofekmeister@gmail.com>
2023-06-07 11:10:22 -0400

    Migrate command to upgrade Python

--- ddev/src/ddev/repo/constants.py
+++ ddev/src/ddev/repo/constants.py
@@ -3,3 +3,6 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 CONFIG_DIRECTORY = '.ddev'
 NOT_SHIPPABLE = frozenset(['datadog_checks_dev', 'datadog_checks_tests_helper', 'ddev'])
+
+# This is automatically maintained
+PYTHON_VERSION = '3.8'
