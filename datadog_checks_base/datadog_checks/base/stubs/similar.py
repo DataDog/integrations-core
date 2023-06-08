837d08605c2ccaf9493cd000cc4f373b05a9a290
Eden <eden.gorevoy@datadoghq.com>
2023-06-08 13:41:34 +0000

    reverting similar.py change for now, to see how tests run

--- datadog_checks_base/datadog_checks/base/stubs/similar.py
+++ datadog_checks_base/datadog_checks/base/stubs/similar.py
@@ -47,7 +47,7 @@ def _build_similar_elements(expected_element, submitted_elements):
         raise NotImplementedError("Invalid type: {}".format(expected_element))
 
     similar_elements = []
-    for _, metric_stubs in iteritems(list(submitted_elements)):
+    for _, metric_stubs in iteritems(submitted_elements):
         for candidate_metric in metric_stubs:
             score = scoring_fn(expected_element, candidate_metric)
             similar_elements.append((score, candidate_metric))
