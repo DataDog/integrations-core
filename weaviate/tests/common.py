d51602e1c0725befaead7d2989b873d8e8d761b4
steveny91 <steven.yuen@datadoghq.com>
2023-06-08 13:42:06 -0400

    add tests

---
+++ weaviate/tests/common.py
@@ -0,0 +1,72 @@
+# (C) Datadog, Inc. 2022-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+MOCKED_INSTANCE = {'openmetrics_endpoint': 'http://weaviate:2112/metrics'}
+
+E2E_METRICS = [
+    "weaviate.go.gc.duration.seconds.count",
+    "weaviate.go.gc.duration.seconds.quantile",
+    "weaviate.go.gc.duration.seconds.sum",
+    "weaviate.go.goroutines",
+    "weaviate.go.memstats.buck_hash.sys_bytes",
+    "weaviate.go.memstats.frees.count",
+    "weaviate.go.memstats.lookups.count",
+    "weaviate.go.memstats.gc.cpu_fraction",
+    "weaviate.go.memstats.gc.sys_bytes",
+    "weaviate.go.memstats.heap.alloc_bytes",
+    "weaviate.go.memstats.heap.idle_bytes",
+    "weaviate.go.memstats.heap.inuse_bytes",
+    "weaviate.go.memstats.heap.objects",
+    "weaviate.go.memstats.heap.released_bytes",
+    "weaviate.go.memstats.heap.sys_bytes",
+    "weaviate.go.memstats.last_gc_time.seconds",
+    "weaviate.go.memstats.mallocs.count",
+    "weaviate.go.memstats.mcache.inuse_bytes",
+    "weaviate.go.memstats.mcache.sys_bytes",
+    "weaviate.go.memstats.mspan.inuse_use",
+    "weaviate.go.memstats.mspan.sys_bytes",
+    "weaviate.go.memstats.next.gc_bytes",
+    "weaviate.go.memstats.other.sys_bytes",
+    "weaviate.go.memstats.stack.inuse_bytes",
+    "weaviate.go.memstats.stack.sys_bytes",
+    "weaviate.go.memstats.sys_bytes",
+    "weaviate.go.threads",
+    "weaviate.process.cpu.seconds.count",
+    "weaviate.process.max_fds",
+    "weaviate.process.open_fds",
+    "weaviate.process.resident_memory.bytes",
+    "weaviate.process.start_time.seconds",
+    "weaviate.process.virtual_memory.bytes",
+    "weaviate.process.virtual_memory.max_bytes",
+    "weaviate.promhttp.metric_handler.requests.count",
+    "weaviate.promhttp.metric_handler.requests_in_flight"
+]
+
+fixture_metrics = [
+    "weaviate.batch.durations_ms.bucket",
+    "weaviate.batch.durations_ms.count",
+    "weaviate.batch.durations_ms.sum",
+    "weaviate.lsm.active.segments",
+    "weaviate.lsm.segment.count",
+    "weaviate.lsm.segment.size",
+    "weaviate.object_count",
+    "weaviate.objects.durations_ms.count",
+    "weaviate.objects.durations_ms.sum",
+    "weaviate.queries.filtered_vector.durations_ms.count",
+    "weaviate.queries.filtered_vector.durations_ms.sum",
+    "weaviate.startup.durations_ms.count",
+    "weaviate.startup.durations_ms.sum",
+    "weaviate.startup.progress",
+    "weaviate.vector.index.durations_ms.count",
+    "weaviate.vector.index.durations_ms.sum",
+    "weaviate.vector.index.maintenance.durations_ms.count",
+    "weaviate.vector.index.maintenance.durations_ms.sum",
+    "weaviate.vector.index.operations",
+    "weaviate.vector.index.size",
+    "weaviate.vector.index.tombstone.cleaned.count",
+    "weaviate.vector.index.tombstone.cleanup.threads",
+    "weaviate.vector.index.tombstones"
+]
+
+TEST_METRICS = E2E_METRICS + fixture_metrics
--- weaviate/tests/test_weaviate.py
+++ weaviate/tests/test_weaviate.py
@@ -1,26 +1,28 @@
 # (C) Datadog, Inc. 2023-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
 
 from typing import Any, Callable, Dict  # noqa: F401
 
 from datadog_checks.base import AgentCheck  # noqa: F401
 from datadog_checks.base.stubs.aggregator import AggregatorStub  # noqa: F401
+from datadog_checks.base.constants import ServiceCheck
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.weaviate import WeaviateCheck
+from .common import TEST_METRICS, MOCKED_INSTANCE
+from .utils import get_fixture_path
 
 
-# def test_check(dd_run_check, aggregator, instance):
-#     # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
-#     check = WeaviateCheck('weaviate', {}, [instance])
-#     dd_run_check(check)
-
-#     aggregator.assert_all_metrics_covered()
-#     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
-
-
-def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
-    check = WeaviateCheck('weaviate', {}, [instance])
+@pytest.mark.unit
+def test_check_mock_weaviate(dd_run_check, aggregator, mock_http_response):
+    mock_http_response(file_path=get_fixture_path('weaviate_metrics.txt'))
+    check = WeaviateCheck('weaviate', {}, [MOCKED_INSTANCE])
     dd_run_check(check)
-    aggregator.assert_service_check('weaviate.can_connect', WeaviateCheck.CRITICAL)
+
+    for metric in TEST_METRICS:
+        aggregator.assert_metric(metric, at_least=1)
+    
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_service_check('weaviate.openmetrics.health', ServiceCheck.OK)
+
