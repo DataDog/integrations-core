7755d3acf7f3d0acb013405c0cb06a74ccb797a2
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-06-08 17:30:13 +0200

    rename to 3com

--- snmp/tests/test_e2e_core_profiles/test_profile_3com_generic.py
+++ snmp/tests/test_e2e_core_profiles/test_profile_3com_generic.py
@@ -17,12 +17,12 @@ pytestmark = [pytest.mark.e2e, common.py3_plus_only, common.snmp_integration_onl
 
 
 def test_e2e_profile_3com_generic(dd_agent_check):
-    config = create_profile_test_config('3com-generic')
+    config = create_profile_test_config('3com')
     aggregator = common.dd_agent_check_wrapper(dd_agent_check, config, rate=True)
 
     ip_address = get_device_ip_from_config(config)
     common_tags = [
-        'snmp_profile:3com-generic',
+        'snmp_profile:3com',
         'snmp_host:3com.device.name',
         'device_namespace:default',
         'snmp_device:' + ip_address,
@@ -40,14 +40,14 @@ def test_e2e_profile_3com_generic(dd_agent_check):
         'id_tags': ['device_namespace:default', 'snmp_device:' + ip_address],
         'ip_address': '' + ip_address,
         'name': '3com.device.name',
-        'profile': '3com-generic',
+        'profile': '3com',
         'status': 1,
         'sys_object_id': '1.3.6.1.4.1.43.1.999',
         'tags': [
             'device_namespace:default',
             'snmp_device:' + ip_address,
             'snmp_host:3com.device.name',
-            'snmp_profile:3com-generic',
+            'snmp_profile:3com',
         ],
         'vendor': '3com',
     }
