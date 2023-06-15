530b9cd814e747b9f586ceafb39e708a2b1b5cf6
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-06-15 11:06:07 +0200

    remove special char

--- snmp/tests/utils.py
+++ snmp/tests/utils.py
@@ -29,7 +29,7 @@ def mock_profiles_confd_user_root(root):
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc:  # Python ≥ 2.5
+    except OSError as exc:  # Python >= 2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         # possibly handle other errno cases here, otherwise finally:
