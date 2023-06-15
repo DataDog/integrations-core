21d5881d795afc8826bbaf0e514b003f54be40fd
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-06-15 10:58:14 +0200

    add mkdir_p

--- snmp/tests/utils.py
+++ snmp/tests/utils.py
@@ -3,6 +3,8 @@
 # Licensed under Simplified BSD License (see LICENSE)
 
 import contextlib
+import errno
+import os
 from typing import Iterator  # noqa: F401
 
 import mock
@@ -22,3 +24,14 @@ def mock_profiles_confd_user_root(root):
     # type: (str) -> Iterator[None]
     with mock.patch.object(utils, '_get_profiles_confd_user_root', return_value=root):
         yield
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python ≥ 2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        # possibly handle other errno cases here, otherwise finally:
+        else:
+            raise
