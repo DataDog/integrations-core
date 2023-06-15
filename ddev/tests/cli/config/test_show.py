9e46f89b46f6b6500f9d2428abdbde0bec52ec33
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-06-15 13:59:25 -0400

    Try commenting out model.github override

--- ddev/tests/cli/config/test_show.py
+++ ddev/tests/cli/config/test_show.py
@@ -7,7 +7,7 @@ import os
 def test_default_scrubbed(ddev, config_file, helpers):
     config_file.model.orgs['default']['api_key'] = 'foo'
     config_file.model.orgs['default']['app_key'] = 'bar'
-    config_file.model.github = {'user': '', 'token': ''}
+    #config_file.model.github = {'user': '', 'token': ''}
     config_file.save()
 
     result = ddev('config', 'show')
@@ -69,7 +69,7 @@ def test_default_scrubbed(ddev, config_file, helpers):
 def test_reveal(ddev, config_file, helpers):
     config_file.model.orgs['default']['api_key'] = 'foo'
     config_file.model.orgs['default']['app_key'] = 'bar'
-    config_file.model.github = {'user': '', 'token': ''}
+    #config_file.model.github = {'user': '', 'token': ''}
     config_file.save()
 
     result = ddev('config', 'show', '-a')
