fe3837bcae6056c7514b65a0f0a030acae2a337c
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2023-06-15 10:01:36 -0700

    remove unnecessary comments

--- dcgm/tests/common.py
+++ dcgm/tests/common.py
@@ -14,29 +14,6 @@ INSTANCE = {
 HERE = get_here()
 COMPOSE_FILE = os.path.join(HERE, 'docker', 'docker-compose.yaml')
 
-# TODO will need to use this structure when comparing tags, etc.
-# A representative sampling of metrics from the fixture used for unit tests
-# for expected_metric in METRICS:
-#     aggregator.assert_metric(name=f"dcgm.{expected_metric['name']}", at_least=0)
-# METRICS = [
-#     {"name": "dec_utilization"},
-#     {"name": "enc_utilization"},
-#     {"name": "fb_free"},
-#     {"name": "fb_used"},
-#     {"name": "temperature"},
-#     {"name": "gpu_utilization"},
-#     {"name": "mem_clock"},
-#     {"name": "mem_copy_utilization"},
-#     {"name": "nvlink_bandwidth.count"},
-#     {"name": "pcie_replay_counter.count"},
-#     {"name": "power_usage"},
-#     {"name": "sm_clock"},
-#     {"name": "total_energy_consumption.count"},
-#     {"name": "vgpu_license_status"},
-#     {"name": "xid_errors"},
-#     {"name": "device_count.count"},
-# ]
-
 EXPECTED_METRICS = [
     'dec_utilization',
     'enc_utilization',
--- dcgm/tests/test_e2e.py
+++ dcgm/tests/test_e2e.py
@@ -8,17 +8,9 @@ from .common import EXPECTED_METRICS
 
 pytestmark = [pytest.mark.e2e, pytest.mark.usefixtures("dd_environment")]
 
-# TODO metrics are filing because we need to add count to those predicted ones (nvml.nband, pcie_replay, etc)
-# instead of what I'm doing, I should just add them to the common.py file?
-
 
 def test_e2e(dd_agent_check, instance):
     aggregator = dd_agent_check(instance, rate=True)
 
     for metric in EXPECTED_METRICS:
         aggregator.assert_metric(name=f"dcgm.{metric}", at_least=0)
-
-
-# def test_e2e_service_checks(dd_agent_check, instance):
-#     aggregator = dd_agent_check(instance, rate=True),
-#     aggregator.assert_service_check('dcgm.openmetrics.health', DcgmCheck.OK)
