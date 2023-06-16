fe2b0511fb3a188716fe5ff3e3f6c494450674ac
Eden <eden.gorevoy@datadoghq.com>
2023-06-16 19:34:47 +0000

    accidentally added an extraneous file

--- postgres/tests/test_bash.sh
+++
@@ -1,6 +0,0 @@
-x=0
-while [ $x -le 100 ]; 
-do 
-    ddev test postgres:py3.9-14.0 -k test_statement_samples_collect[dd_admin-dd_admin-dogs-SELECT * FROM breed WHERE name = %s-Labrador-None-None-not_truncated-expected_warnings1-pg_stat_activity] --skip-env
-    ((x++))
-done
