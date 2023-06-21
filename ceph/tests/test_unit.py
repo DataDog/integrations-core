2ae4029856aeffa2f27e300d965c6f46462afea5
Max Meinhold <mxmeinhold@gmail.com>
2023-06-21 16:24:31 -0400

    [ceph] Add OSD metadata tags (#14201)
    * Add objectstore tags to osds
    
    at time of writing, objectstore is useful for tracking the status of the
    filestore and bluestore migration
    
    * Add device metadata tags to osds
    
    associating OSD metrics with the device ID allows more individual
    tracking of disks
    
    device class is useful for tracking a migration between hard disks and
    solid state disks
    
    * Add version tags to osd metrics
    
    * Clean up ceph osd tags
    
    We only need to iterate through the metadata once
    
    * add device specific tags to tests
    
    * fixup! add device specific tags to tests
    
    formatting

--- ceph/tests/test_unit.py
+++ ceph/tests/test_unit.py
@@ -31,6 +31,33 @@ EXPECTED_METRICS_POOL_TAGS = [
 
 EXPECTED_TAGS = ['ceph_fsid:e0efcf84-e8ed-4916-8ce1-9c70242d390a', 'ceph_mon_state:peon', 'optional:tag1']
 
+OSD_SPECIFIC_TAGS = {
+    'osd0': [
+        'ceph_osd_device:/dev/sda',
+        'ceph_osd_device_class:hdd',
+        'ceph_osd_device_id:disk0',
+        'ceph_osd_objectstore:bluestore',
+        'ceph_release:octopus',
+        'ceph_version:15.2.8',
+    ],
+    'osd1': [
+        'ceph_osd_device:/dev/sdb',
+        'ceph_osd_device_class:hdd',
+        'ceph_osd_device_id:disk1',
+        'ceph_osd_objectstore:bluestore',
+        'ceph_release:octopus',
+        'ceph_version:15.2.8',
+    ],
+    'osd2': [
+        'ceph_osd_device:/dev/sdc',
+        'ceph_osd_device_class:hdd',
+        'ceph_osd_device_id:disk2',
+        'ceph_osd_objectstore:bluestore',
+        'ceph_release:octopus',
+        'ceph_version:15.2.8',
+    ],
+}
+
 pytestmark = pytest.mark.unit
 
 
@@ -96,7 +123,7 @@ def test_tagged_metrics(_, aggregator):
     ceph_check.check({})
 
     for osd in ['osd0', 'osd1', 'osd2']:
-        expected_tags = EXPECTED_TAGS + ['ceph_osd:%s' % osd]
+        expected_tags = EXPECTED_TAGS + ['ceph_osd:%s' % osd] + OSD_SPECIFIC_TAGS[osd]
 
         for metric in ['ceph.commit_latency_ms', 'ceph.apply_latency_ms']:
             aggregator.assert_metric(metric, count=1, tags=expected_tags)
@@ -115,7 +142,7 @@ def test_osd_perf_with_osdstats(_, aggregator):
     ceph_check.check({})
 
     for osd in ['osd0', 'osd1', 'osd2']:
-        expected_tags = EXPECTED_TAGS + ['ceph_osd:%s' % osd]
+        expected_tags = EXPECTED_TAGS + ['ceph_osd:%s' % osd] + OSD_SPECIFIC_TAGS[osd]
 
         for metric in ['ceph.commit_latency_ms', 'ceph.apply_latency_ms']:
             aggregator.assert_metric(metric, count=1, tags=expected_tags)
