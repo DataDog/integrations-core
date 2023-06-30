d5d39319e159d611285eddff4979eace9e881802
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-06-30 10:45:54 -0400

    Revert "Skip list_consumer_groups if only specifying consumer_groups"
    This reverts commit bdd8e8e6f875a08f4ba81f3d1444b40c8d90cd53.

--- kafka_consumer/datadog_checks/kafka_consumer/client.py
+++ kafka_consumer/datadog_checks/kafka_consumer/client.py
@@ -156,22 +156,20 @@ class KafkaClient:
         return consumer_offsets
 
     def _get_consumer_groups(self):
-        # Get all consumer groups to monitor
+        # Get all consumer groups
         consumer_groups = []
-        if self.config._monitor_unlisted_consumer_groups or self.config._consumer_groups_compiled_regex:
-            consumer_groups_future = self.kafka_client.list_consumer_groups()
-            try:
-                list_consumer_groups_result = consumer_groups_future.result()
+        consumer_groups_future = self.kafka_client.list_consumer_groups()
+        self.log.debug('MONITOR UNLISTED CG FUTURES: %s', consumer_groups_future)
+        try:
+            list_consumer_groups_result = consumer_groups_future.result()
+            self.log.debug('MONITOR UNLISTED FUTURES RESULT: %s', list_consumer_groups_result)
 
-                consumer_groups.extend(
-                    valid_consumer_group.group_id for valid_consumer_group in list_consumer_groups_result.valid
-                )
-            except Exception as e:
-                self.log.error("Failed to collect consumer groups: %s", e)
-            # add debug log here to list the consumer groups result
-            return consumer_groups
-        else:
-            return self.config._consumer_groups
+            consumer_groups.extend(
+                valid_consumer_group.group_id for valid_consumer_group in list_consumer_groups_result.valid
+            )
+        except Exception as e:
+            self.log.error("Failed to collect consumer groups: %s", e)
+        return consumer_groups
 
     def _get_consumer_offset_futures(self, consumer_groups):
         topic_metadata = self.kafka_client.list_topics(timeout=self.config._request_timeout).topics
