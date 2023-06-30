5fc3939bf2560da9f1446a778c5b274080421832
steveny91 <steven.yuen@datadoghq.com>
2023-06-30 03:17:46 -0400

    revert

--- weaviate/tests/conftest.py
+++ weaviate/tests/conftest.py
@@ -58,8 +58,6 @@ def dd_environment():
                 instance["headers"] = {"Authorization": "Bearer test123"}
 
             make_weaviate_request(instance)
-            make_weaviate_request(instance)
-            make_weaviate_request(instance)
             yield instance
 
 
