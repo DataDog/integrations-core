bff8c1335c6062895d01592a8480baf9bc3a399a
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-07-03 17:20:29 +0200

    Update assert_extend_*

--- snmp/tests/test_e2e_core_profiles/utils.py
+++ snmp/tests/test_e2e_core_profiles/utils.py
@@ -27,11 +27,15 @@ def assert_common_metrics(aggregator, common_tags):
 
 def assert_extend_generic_if(aggregator, common_tags):
     """Add the following to the snmprec
-    1.3.6.1.2.1.2.1.0|2|28"""
+1.3.6.1.2.1.2.1.0|2|28
+    """
     aggregator.assert_metric("snmp.ifNumber", metric_type=aggregator.GAUGE, tags=common_tags)
 
 
 def assert_extend_generic_ip(aggregator, common_tags):
+    """Add the following to the snmprec
+1.3.6.1.2.1.4.31.1.1.4.1 = Counter64: 310637142
+    """
     aggregator.assert_metric(
         "snmp.ipSystemStatsHCInReceives",
         metric_type=aggregator.COUNT,
@@ -40,14 +44,23 @@ def assert_extend_generic_ip(aggregator, common_tags):
 
 
 def assert_extend_generic_tcp(aggregator, common_tags):
+    """Add the following to the snmprec
+1.3.6.1.2.1.6.5.0|65|4698
+    """
     aggregator.assert_metric("snmp.tcpActiveOpens", metric_type=aggregator.COUNT, tags=common_tags)
 
 
 def assert_extend_generic_udp(aggregator, common_tags):
+    """Add the following to the snmprec
+1.3.6.1.2.1.7.8.0|70|6116206687099577350
+    """
     aggregator.assert_metric("snmp.udpHCInDatagrams", metric_type=aggregator.COUNT, tags=common_tags)
 
 
 def assert_extend_generic_ospf(aggregator, common_tags):
+    """Add the following to the snmprec
+1.3.6.1.2.1.14.10.1.6.192.29.116.26.0|2|8
+    """
     aggregator.assert_metric(
         "snmp.ospfNbr", metric_type=aggregator.GAUGE, tags=common_tags + ["neighbor_state:full"], value=1
     )
@@ -57,6 +70,9 @@ def assert_extend_generic_ospf(aggregator, common_tags):
 
 
 def assert_extend_generic_bgp4(aggregator, common_tags):
+    """Add the following to the snmprec
+1.3.6.1.2.1.15.3.1.3.244.12.239.177|2|2
+    """
     aggregator.assert_metric(
         "snmp.bgpPeerAdminStatus",
         metric_type=aggregator.GAUGE,
@@ -83,17 +99,18 @@ def assert_extend_cisco_cpu_memory(aggregator, common_tags):
 
 def assert_extend_generic_host_resources(aggregator, common_tags):
     """Add the following to the snmprec
-    1.3.6.1.2.1.25.1.1.0|67|201526890
-    1.3.6.1.2.1.25.2.3.1.1.4|2|4
-    1.3.6.1.2.1.25.2.3.1.1.31|2|31
-    1.3.6.1.2.1.25.2.3.1.2.4|6|1.3.6.1.3.167.36
-    1.3.6.1.2.1.25.2.3.1.2.31|6|1.3.6.1.3
-    1.3.6.1.2.1.25.2.3.1.3.4|4x|6b65707420627574207468656972204a61646564206275742064726976696e67
-    1.3.6.1.2.1.25.2.3.1.3.31|4x|7a6f6d62696573206f78656e206b657074204a6164656420717561696e746c79207a6f6d62696573
-    1.3.6.1.2.1.25.2.3.1.5.4|2|17
-    1.3.6.1.2.1.25.2.3.1.5.31|2|21
-    1.3.6.1.2.1.25.2.3.1.6.4|2|30
-    1.3.6.1.2.1.25.2.3.1.6.31|2|4"""
+1.3.6.1.2.1.25.1.1.0|67|201526890
+1.3.6.1.2.1.25.2.3.1.1.4|2|4
+1.3.6.1.2.1.25.2.3.1.1.31|2|31
+1.3.6.1.2.1.25.2.3.1.2.4|6|1.3.6.1.3.167.36
+1.3.6.1.2.1.25.2.3.1.2.31|6|1.3.6.1.3
+1.3.6.1.2.1.25.2.3.1.3.4|4x|6b65707420627574207468656972204a61646564206275742064726976696e67
+1.3.6.1.2.1.25.2.3.1.3.31|4x|7a6f6d62696573206f78656e206b657074204a6164656420717561696e746c79207a6f6d62696573
+1.3.6.1.2.1.25.2.3.1.5.4|2|17
+1.3.6.1.2.1.25.2.3.1.5.31|2|21
+1.3.6.1.2.1.25.2.3.1.6.4|2|30
+1.3.6.1.2.1.25.2.3.1.6.31|2|4
+    """
     aggregator.assert_metric("snmp.hrSystemUptime", metric_type=aggregator.GAUGE, tags=common_tags)
 
     cpu_rows = ['10', '21']
@@ -126,25 +143,26 @@ def assert_extend_generic_host_resources(aggregator, common_tags):
 
 def assert_extend_generic_host_resources_base(aggregator, common_tags):
     """Add the following to the snmprec
-    1.3.6.1.2.1.25.1.1.0|67|201526890
+1.3.6.1.2.1.25.1.1.0|67|201526890
     """
     aggregator.assert_metric("snmp.hrSystemUptime", metric_type=aggregator.GAUGE, tags=common_tags)
 
 
 def assert_extend_entity_sensor(aggregator, common_tags):
     """Add the following to the snmprec
-    1.3.6.1.2.1.99.1.1.1.1.8|2|9
-    1.3.6.1.2.1.99.1.1.1.2.8|2|7
-    1.3.6.1.2.1.99.1.1.1.3.8|2|7
-    1.3.6.1.2.1.99.1.1.1.4.8|2|2
-    1.3.6.1.2.1.99.1.1.1.5.8|2|3
-    1.3.6.1.2.1.99.1.1.1.6.8|4x|64726976696e672064726976696e6720666f727761726420616374656420746865697220627574
-    1.3.6.1.2.1.99.1.1.1.7.8|67|2113891456
-    1.3.6.1.2.1.99.1.1.1.8.8|66|6698
-    1.3.6.1.6.3.10.2.1.1.0|4x|4a616465642062757420666f7277617264
-    1.3.6.1.6.3.10.2.1.2.0|2|31
-    1.3.6.1.6.3.10.2.1.3.0|2|31
-    1.3.6.1.6.3.10.2.1.4.0|2|1234"""
+1.3.6.1.2.1.99.1.1.1.1.8|2|9
+1.3.6.1.2.1.99.1.1.1.2.8|2|7
+1.3.6.1.2.1.99.1.1.1.3.8|2|7
+1.3.6.1.2.1.99.1.1.1.4.8|2|2
+1.3.6.1.2.1.99.1.1.1.5.8|2|3
+1.3.6.1.2.1.99.1.1.1.6.8|4x|64726976696e672064726976696e6720666f727761726420616374656420746865697220627574
+1.3.6.1.2.1.99.1.1.1.7.8|67|2113891456
+1.3.6.1.2.1.99.1.1.1.8.8|66|6698
+1.3.6.1.6.3.10.2.1.1.0|4x|4a616465642062757420666f7277617264
+1.3.6.1.6.3.10.2.1.2.0|2|31
+1.3.6.1.6.3.10.2.1.3.0|2|31
+1.3.6.1.6.3.10.2.1.4.0|2|1234
+    """
     additional_tags = [
         'ent_phy_sensor_oper_status:nonoperational',
         'ent_phy_sensor_precision:7',
