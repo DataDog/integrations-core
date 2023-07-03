061909faadf8af6cab684bdffd86d7149bdcf6f1
lizaerem <elizaveta.eremina@datadoghq.com>
2023-07-03 14:19:22 +0200

    delete check_submission_type from e2e core tests

--- snmp/tests/test_e2e_core.py
+++ snmp/tests/test_e2e_core.py
@@ -206,7 +206,7 @@ def assert_apc_ups_metrics(dd_agent_check, config):
         aggregator.assert_metric(metric, value=value, metric_type=aggregator.GAUGE, count=2, tags=tags)
 
     aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
 def test_e2e_memory_cpu_f5_big_ip(dd_agent_check):
@@ -425,7 +425,7 @@ def test_e2e_meraki_cloud_controller(dd_agent_check):
         value=1,
     )
     aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
 def test_e2e_core_detect_metrics_using_apc_ups_metrics(dd_agent_check):
@@ -485,7 +485,7 @@ def test_e2e_core_detect_metrics_using_apc_ups_metrics(dd_agent_check):
     assert if_in_error_metrics[0].value > 0
 
     aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
 def test_e2e_core_cisco_csr(dd_agent_check):
@@ -516,7 +516,7 @@ def test_e2e_core_cisco_csr(dd_agent_check):
     aggregator.assert_metric('snmp.peerConnectionByState', metric_type=aggregator.GAUGE, tags=metric_tags, value=1)
 
     aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
 def test_e2e_cisco_nexus(dd_agent_check):
@@ -630,7 +630,7 @@ def test_e2e_cisco_nexus(dd_agent_check):
 
     aggregator.assert_metric('snmp.sysUpTimeInstance', count=2)
     aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
 def test_e2e_cisco_legacy_wlc(dd_agent_check):
@@ -745,4 +745,4 @@ def test_e2e_cisco_legacy_wlc(dd_agent_check):
     aggregator.assert_metric('snmp.bsnDot11EssNumberOfMobileStations', metric_type=aggregator.GAUGE, tags=wlan_tags)
 
     aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
