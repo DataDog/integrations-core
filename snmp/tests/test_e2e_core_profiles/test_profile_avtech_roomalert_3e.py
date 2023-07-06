a55636217d984e54f6a7abb26e810cae85dc070f
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-07-06 12:42:05 +0200

    update test

--- snmp/tests/test_e2e_core_profiles/test_profile_avtech_roomalert_3e.py
+++ snmp/tests/test_e2e_core_profiles/test_profile_avtech_roomalert_3e.py
@@ -32,17 +32,16 @@ def test_e2e_profile_avtech_roomalert_3e(dd_agent_check):
     # --- TEST METRICS ---
     assert_common_metrics(aggregator, common_tags)
 
-    aggregator.assert_metric('snmp.digital_sen1_1', metric_type=aggregator.GAUGE, tags=common_tags)
-    aggregator.assert_metric('snmp.digital_sen1_2', metric_type=aggregator.GAUGE, tags=common_tags)
-    aggregator.assert_metric('snmp.digital_sen2_1', metric_type=aggregator.GAUGE, tags=common_tags)
-    aggregator.assert_metric('snmp.digital_sen2_2', metric_type=aggregator.GAUGE, tags=common_tags)
-    aggregator.assert_metric('snmp.digital_sen2_3', metric_type=aggregator.GAUGE, tags=common_tags)
-    aggregator.assert_metric('snmp.digital_sen2_4', metric_type=aggregator.GAUGE, tags=common_tags)
-    aggregator.assert_metric('snmp.digital_sen2_5', metric_type=aggregator.GAUGE, tags=common_tags)
-    aggregator.assert_metric('snmp.digital_sen2_7', metric_type=aggregator.GAUGE, tags=common_tags)
-    aggregator.assert_metric('snmp.switch_sen1', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric('snmp.roomalert.3e.digital_sen1_1', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric('snmp.roomalert.3e.digital_sen1_2', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric('snmp.roomalert.3e.digital_sen2_1', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric('snmp.roomalert.3e.digital_sen2_2', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric('snmp.roomalert.3e.digital_sen2_3', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric('snmp.roomalert.3e.digital_sen2_4', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric('snmp.roomalert.3e.digital_sen2_5', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric('snmp.roomalert.3e.digital_sen2_7', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric('snmp.roomalert.3e.switch_sen1', metric_type=aggregator.GAUGE, tags=common_tags)
 
-    aggregator.assert_all_metrics_covered()
 
     # --- TEST METADATA ---
     device = {
