9a30a422d5209988fd77278ba6d6cb3285236d4f
NouhaManai <nouha.manai@yahoo.fr>
2023-07-07 16:27:29 +0200

    address review

--- snmp/tests/test_e2e_core_profiles/test_profile_hp_h3c_switch.py
+++ snmp/tests/test_e2e_core_profiles/test_profile_hp_h3c_switch.py
@@ -31,29 +31,24 @@ def test_e2e_profile_hp_h3c_switch(dd_agent_check):
     ]
 
     # --- TEST EXTENDED METRICS ---
-    assert_extend_generic_if(aggregator, common_tags)\
-
+    assert_extend_generic_if(aggregator, common_tags)
     # --- TEST METRICS ---
     assert_common_metrics(aggregator, common_tags)
 
     tag_rows = [
-         ['hh3c_process_id:17191', 'hh3c_process_name:kept but'],
-         ['hh3c_process_id:27865', 'hh3c_process_name:kept their'],
-
+        ['hh3c_process_id:17191', 'hh3c_process_name:kept but'],
+        ['hh3c_process_id:27865', 'hh3c_process_name:kept their'],
     ]
     for tag_row in tag_rows:
         aggregator.assert_metric('snmp.hh3cProcessUtil5Min', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
 
     tag_rows = [
-         ['hh3c_entity_ext_physical_index:11'],
-         ['hh3c_entity_ext_physical_index:2'],
-
+        ['hh3c_entity_ext_physical_index:11'],
+        ['hh3c_entity_ext_physical_index:2'],
     ]
     for tag_row in tag_rows:
         aggregator.assert_metric('snmp.cpu.usage', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
         aggregator.assert_metric('snmp.memory.usage', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
-
-
 
     # --- TEST METADATA ---
     device = {
