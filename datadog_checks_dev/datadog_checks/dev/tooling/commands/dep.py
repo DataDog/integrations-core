188677688a9ff201f454c6e98ef9f82da494a38b
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2023-07-11 09:34:41 +0200

    fix style

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/dep.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/dep.py
@@ -47,9 +47,9 @@ IGNORED_DEPS = {
     'pyodbc',  # Breaking sqlserver tests
     'psutil',  # Breaking disk tests
     'aerospike',  # v8+ breaks agent build.
-    'protobuf', # 3.20.2->4.23.3 breaks kubernetes_state, kube_dns, gitlab and gitlab_runner tests.
-    'service-identity', # 21.1->23.1 breaks tls tests.
-    'pyvmomi', # 7->8 breaks vsphere tests.
+    'protobuf',  # 3.20.2->4.23.3 breaks kubernetes_state, kube_dns, gitlab and gitlab_runner tests.
+    'service-identity',  # 21.1->23.1 breaks tls tests.
+    'pyvmomi',  # 7->8 breaks vsphere tests.
     # 4.3->4.4 changes the license field in the package metadata to something our validations cannot handle.
     'pymongo',
 }
