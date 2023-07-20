d3620f209bdd975f928529703c256964fd2e368c
root <root@ip-172-21-28-134.ec2.internal>
2023-07-20 21:49:28 +0000

    sync postgres models

--- postgres/datadog_checks/postgres/config_models/shared.py
+++ postgres/datadog_checks/postgres/config_models/shared.py
@@ -11,38 +11,38 @@ from __future__ import annotations
 
 from typing import Optional
 
-from pydantic import BaseModel, ConfigDict, field_validator, model_validator
+from pydantic import BaseModel, root_validator, validator
 
 from datadog_checks.base.utils.functions import identity
 from datadog_checks.base.utils.models import validation
 
-from . import validators
+from . import defaults, validators
 
 
 class SharedConfig(BaseModel):
-    model_config = ConfigDict(
-        validate_default=True,
-        frozen=True,
-    )
-    service: Optional[str] = None
+    class Config:
+        allow_mutation = False
 
-    @model_validator(mode='before')
+    service: Optional[str]
+
+    @root_validator(pre=True)
     def _initial_validation(cls, values):
         return validation.core.initialize_config(getattr(validators, 'initialize_shared', identity)(values))
 
-    @field_validator('*')
-    def _run_validations(cls, value, info):
-        field = cls.model_fields[info.field_name]
-        field_name = field.alias or info.field_name
-        if field_name not in info.context['configured_fields']:
-            return value
+    @validator('*', pre=True, always=True)
+    def _ensure_defaults(cls, v, field):
+        if v is not None or field.required:
+            return v
 
-        return getattr(validators, f'shared_{info.field_name}', identity)(value, field=field)
+        return getattr(defaults, f'shared_{field.name}')(field, v)
 
-    @field_validator('*', mode='after')
-    def _make_immutable(cls, value):
-        return validation.utils.make_immutable(value)
+    @validator('*')
+    def _run_validations(cls, v, field):
+        if not v:
+            return v
 
-    @model_validator(mode='after')
-    def _final_validation(cls, model):
-        return validation.core.check_model(getattr(validators, 'check_shared', identity)(model))
+        return getattr(validators, f'shared_{field.name}', identity)(v, field=field)
+
+    @root_validator(pre=False)
+    def _final_validation(cls, values):
+        return validation.core.finalize_config(getattr(validators, 'finalize_shared', identity)(values))
