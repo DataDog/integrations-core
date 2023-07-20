3f8d29e1f90708d2a2b9ed4665572c01e69667a1
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2023-07-20 13:02:16 +0200

    Fixed license-headers validation errors

--- openstack_controller/tests/test_metrics.py
+++ openstack_controller/tests/test_metrics.py
@@ -1,6 +1,6 @@
-# # (C) Datadog, Inc. 2018-present
-# # All rights reserved
-# # Licensed under Simplified BSD License (see LICENSE)
+# (C) Datadog, Inc. 2018-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
 # import json
 # import os
 #
