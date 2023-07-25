4132430ae31c910aa2ac695668023b4f703c1d90
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-07-25 19:14:30 +0200

    fix style

--- snmp/tests/test_e2e_core_profiles/test_profile_infinera_coriant_groove.py
+++ snmp/tests/test_e2e_core_profiles/test_profile_infinera_coriant_groove.py
@@ -36,42 +36,88 @@ def test_e2e_profile_infinera_coriant_groove(dd_agent_check):
     # --- TEST METRICS ---
     assert_common_metrics(aggregator, common_tags)
 
-    aggregator.assert_metric('snmp.coriant.groove.systemPowerConsumptionCurrent', metric_type=aggregator.GAUGE, tags=common_tags)
+    aggregator.assert_metric(
+        'snmp.coriant.groove.systemPowerConsumptionCurrent', metric_type=aggregator.GAUGE, tags=common_tags
+    )
     tag_rows = [
-         ['coriant_groove_shelf_admin_status:up', 'coriant_groove_shelf_alias_name:driving their acted their', 'coriant_groove_shelf_location:oxen but zombies kept acted oxen kept', 'coriant_groove_shelf_oper_status:down'],
-         ['coriant_groove_shelf_alias_name:Jaded kept oxen driving', 'coriant_groove_shelf_location:forward but zombies forward', 'coriant_groove_shelf_oper_status:down'],
-
+        [
+            'coriant_groove_shelf_admin_status:up',
+            'coriant_groove_shelf_alias_name:driving their acted their',
+            'coriant_groove_shelf_location:oxen but zombies kept acted oxen kept',
+            'coriant_groove_shelf_oper_status:down',
+        ],
+        [
+            'coriant_groove_shelf_alias_name:Jaded kept oxen driving',
+            'coriant_groove_shelf_location:forward but zombies forward',
+            'coriant_groove_shelf_oper_status:down',
+        ],
     ]
     for tag_row in tag_rows:
-        aggregator.assert_metric('snmp.coriant.groove.shelfInletTemperature', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
-        aggregator.assert_metric('snmp.coriant.groove.shelfOutletTemperature', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
+        aggregator.assert_metric(
+            'snmp.coriant.groove.shelfInletTemperature', metric_type=aggregator.GAUGE, tags=common_tags + tag_row
+        )
+        aggregator.assert_metric(
+            'snmp.coriant.groove.shelfOutletTemperature', metric_type=aggregator.GAUGE, tags=common_tags + tag_row
+        )
 
     tag_rows = [
-         ['coriant_groove_card_admin_status:up', 'coriant_groove_card_alias_name:driving zombies forward oxen driving their', 'coriant_groove_card_equipment_name:driving forward acted forward forward quaintly', 'coriant_groove_card_mode:regen', 'coriant_groove_card_oper_status:down', 'coriant_groove_card_required_type:chm1lh'],
-         ['coriant_groove_card_alias_name:Jaded kept quaintly forward Jaded', 'coriant_groove_card_equipment_name:their', 'coriant_groove_card_mode:not_applicable', 'coriant_groove_card_oper_status:up', 'coriant_groove_card_required_type:chm1'],
-
+        [
+            'coriant_groove_card_admin_status:up',
+            'coriant_groove_card_alias_name:driving zombies forward oxen driving their',
+            'coriant_groove_card_equipment_name:driving forward acted forward forward quaintly',
+            'coriant_groove_card_mode:regen',
+            'coriant_groove_card_oper_status:down',
+            'coriant_groove_card_required_type:chm1lh',
+        ],
+        [
+            'coriant_groove_card_alias_name:Jaded kept quaintly forward Jaded',
+            'coriant_groove_card_equipment_name:their',
+            'coriant_groove_card_mode:not_applicable',
+            'coriant_groove_card_oper_status:up',
+            'coriant_groove_card_required_type:chm1',
+        ],
     ]
     for tag_row in tag_rows:
-        aggregator.assert_metric('snmp.coriant.groove.cardFanSpeedRate', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
-        aggregator.assert_metric('snmp.coriant.groove.cardTemperature', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
+        aggregator.assert_metric(
+            'snmp.coriant.groove.cardFanSpeedRate', metric_type=aggregator.GAUGE, tags=common_tags + tag_row
+        )
+        aggregator.assert_metric(
+            'snmp.coriant.groove.cardTemperature', metric_type=aggregator.GAUGE, tags=common_tags + tag_row
+        )
 
     tag_rows = [
-         ['coriant_groove_port_alias_name:Jaded kept their', 'coriant_groove_port_connected_to:forward zombies forward Jaded forward forward quaintly oxen', 'coriant_groove_port_name:Jaded', 'coriant_groove_port_service_label:zombies driving but zombies'],
-         ['coriant_groove_port_alias_name:oxen their', 'coriant_groove_port_connected_to:driving kept but', 'coriant_groove_port_name:driving kept oxen', 'coriant_groove_port_service_label:driving acted acted quaintly'],
-
+        [
+            'coriant_groove_port_alias_name:Jaded kept their',
+            'coriant_groove_port_connected_to:forward zombies forward Jaded forward forward quaintly oxen',
+            'coriant_groove_port_name:Jaded',
+            'coriant_groove_port_service_label:zombies driving but zombies',
+        ],
+        [
+            'coriant_groove_port_alias_name:oxen their',
+            'coriant_groove_port_connected_to:driving kept but',
+            'coriant_groove_port_name:driving kept oxen',
+            'coriant_groove_port_service_label:driving acted acted quaintly',
+        ],
     ]
     for tag_row in tag_rows:
-        aggregator.assert_metric('snmp.coriant.groove.portRxOpticalPower', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
-        aggregator.assert_metric('snmp.coriant.groove.portTxOpticalPower', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
+        aggregator.assert_metric(
+            'snmp.coriant.groove.portRxOpticalPower', metric_type=aggregator.GAUGE, tags=common_tags + tag_row
+        )
+        aggregator.assert_metric(
+            'snmp.coriant.groove.portTxOpticalPower', metric_type=aggregator.GAUGE, tags=common_tags + tag_row
+        )
 
     tag_rows = [
-         ['coriant_groove_och_os_service_label:oxen but quaintly forward but'],
-         ['coriant_groove_och_os_service_label:their quaintly driving their kept their forward zombies'],
-
+        ['coriant_groove_och_os_service_label:oxen but quaintly forward but'],
+        ['coriant_groove_och_os_service_label:their quaintly driving their kept their forward zombies'],
     ]
     for tag_row in tag_rows:
-        aggregator.assert_metric('snmp.coriant.groove.ochOsCD', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
-        aggregator.assert_metric('snmp.coriant.groove.ochOsOSNR', metric_type=aggregator.GAUGE, tags=common_tags + tag_row)
+        aggregator.assert_metric(
+            'snmp.coriant.groove.ochOsCD', metric_type=aggregator.GAUGE, tags=common_tags + tag_row
+        )
+        aggregator.assert_metric(
+            'snmp.coriant.groove.ochOsOSNR', metric_type=aggregator.GAUGE, tags=common_tags + tag_row
+        )
 
     # --- TEST METADATA ---
     device = {
