7708b97903af09a47a31689ff8bcc669dbb091d1
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-07-25 11:52:28 +0200

    comment

--- snmp/tests/test_e2e_core_profiles/test_profile_juniper_qfx.py
+++ snmp/tests/test_e2e_core_profiles/test_profile_juniper_qfx.py
@@ -14,10 +14,10 @@ from .utils import (
     assert_extend_generic_host_resources,
     assert_extend_generic_if,
     assert_extend_generic_ospf,
-    assert_extend_juniper_cos,
-    assert_extend_juniper_dcu,
-    assert_extend_juniper_firewall,
-    assert_extend_juniper_virtualchassis,
+    # assert_extend_juniper_cos,
+    # assert_extend_juniper_dcu,
+    # assert_extend_juniper_firewall,
+    # assert_extend_juniper_virtualchassis,
     create_e2e_core_test_config,
     get_device_ip_from_config,
 )
@@ -42,10 +42,10 @@ def test_e2e_profile_juniper_qfx(dd_agent_check):
     assert_extend_generic_host_resources(aggregator, common_tags)
     assert_extend_generic_if(aggregator, common_tags)
     assert_extend_generic_ospf(aggregator, common_tags)
-    assert_extend_juniper_cos(aggregator, common_tags)
-    assert_extend_juniper_dcu(aggregator, common_tags)
-    assert_extend_juniper_firewall(aggregator, common_tags)
-    assert_extend_juniper_virtualchassis(aggregator, common_tags)
+    # assert_extend_juniper_cos(aggregator, common_tags)
+    # assert_extend_juniper_dcu(aggregator, common_tags)
+    # assert_extend_juniper_firewall(aggregator, common_tags)
+    # assert_extend_juniper_virtualchassis(aggregator, common_tags)
 
     # --- TEST METRICS ---
     assert_common_metrics(aggregator, common_tags)
