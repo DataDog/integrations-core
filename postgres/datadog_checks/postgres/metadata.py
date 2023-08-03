584b2690d8d554bfd65076b8816bb635565f93f5
edengorevoy <eden.gorevoy@datadoghq.com>
2023-08-03 21:20:42 +0000

    Formatting, updating to psycopg3

--- postgres/datadog_checks/postgres/metadata.py
+++ postgres/datadog_checks/postgres/metadata.py
@@ -259,9 +259,7 @@ class PostgresMetadata(DBMAsyncJob):
         self._time_since_last_schemas_query = time.time()
         return metadata
 
-    def _query_database_information(
-        self, cursor: psycopg2.extensions.cursor, dbname: str
-    ) -> Dict[str, Union[str, int]]:
+    def _query_database_information(self, cursor: psycopg.cursor, dbname: str) -> Dict[str, Union[str, int]]:
         """
         Collect database info. Returns
             description: str
@@ -274,7 +272,7 @@ class PostgresMetadata(DBMAsyncJob):
         row = cursor.fetchone()
         return row
 
-    def _query_schema_information(self, cursor: psycopg2.extensions.cursor, dbname: str) -> Dict[str, str]:
+    def _query_schema_information(self, cursor: psycopg.cursor, dbname: str) -> Dict[str, str]:
         """
         Collect user schemas. Returns
             id: str
@@ -329,7 +327,7 @@ class PostgresMetadata(DBMAsyncJob):
         return table_info[:limit]
 
     def _query_table_information_for_schema(
-        self, cursor: psycopg2.extensions.cursor, schemaname: str, dbname: str
+        self, cursor: psycopg.cursor, schemaname: str, dbname: str
     ) -> List[Dict[str, Union[str, Dict]]]:
         """
         Collect table information per schema. Returns a list of dictionaries
@@ -402,7 +400,7 @@ class PostgresMetadata(DBMAsyncJob):
     def _collect_metadata_for_database(self, dbname):
         metadata = {}
         with self.db_pool.get_connection(dbname, self._config.idle_connection_timeout) as conn:
-            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
+            with conn.cursor(row_factory=dict_row) as cursor:
                 database_info = self._query_database_information(cursor, dbname)
                 metadata.update(
                     {
