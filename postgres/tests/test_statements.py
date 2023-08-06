ed29666dc1d06df4689ed5fd9e5dc70834ea4954
Jo-Ann Meunier <jojo.meunier@datadoghq.com>
2023-08-06 14:46:16 -0400

    cleanup

--- postgres/tests/test_statements.py
+++ postgres/tests/test_statements.py
@@ -1360,10 +1360,10 @@ def test_pg_settings_caching(aggregator, integration_check, dbm_instance):
     check = integration_check(dbm_instance)
     assert not check.pg_settings, "pg_settings should not have been initialized yet"
     check._connect()
-    check.db_pool.get_main_db()
+    check.db_pool._get_main_db()
     assert "track_activity_query_size" in check.pg_settings
     check.pg_settings["test_key"] = True
-    check.db_pool.get_main_db()
+    check.db_pool._get_main_db()
     assert (
         "test_key" in check.pg_settings
     ), "key should not have been blown away. If it was then pg_settings was not cached correctly"
