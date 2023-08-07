f22c8732628ce32cc955b4e62a0274200d3e46a6
lizaerem <elizaveta.eremina@datadoghq.com>
2023-08-07 11:47:48 +0200

    Separate constant_value_one & regenerate snmprec & fix tests

--- snmp/tests/test_profiles.py
+++ snmp/tests/test_profiles.py
@@ -1829,16 +1829,9 @@ def test_proliant(aggregator):
         'cpqHeFltTolPowerSupplyCapacityUsed',
         'cpqHeFltTolPowerSupplyCapacityMaximum',
     ]
-    tag_mappings = [
-        ('30', '3'),
-    ]
-    for number, status in tag_mappings:
-        tags = [
-            'power_supply_status:{}'.format(status),
-            'chassis_num:{}'.format(number),
-        ] + common_tags
-        for gauge in power_metrics:
-            aggregator.assert_metric('snmp.{}'.format(gauge), metric_type=aggregator.GAUGE, tags=tags, count=1)
+    for gauge in power_metrics:
+        tags = ['chassis_num:30'] + common_tags
+        aggregator.assert_metric('snmp.{}'.format(gauge), metric_type=aggregator.GAUGE, tags=tags, count=1)
 
     controller_index = ['controller_index:3'] + common_tags
     aggregator.assert_metric(
