356edccdaac64c262d76f06c8708a16718c2a33d
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2023-08-08 08:03:38 +0200

    sync models

--- supervisord/datadog_checks/supervisord/config_models/instance.py
+++ supervisord/datadog_checks/supervisord/config_models/instance.py
@@ -9,7 +9,8 @@
 
 from __future__ import annotations
 
-from typing import Any, Mapping, Optional, Sequence
+from types import MappingProxyType
+from typing import Any, Optional
 
 from pydantic import BaseModel, ConfigDict, field_validator, model_validator
 
@@ -21,15 +22,17 @@ from . import defaults, validators
 
 class MetricPatterns(BaseModel):
     model_config = ConfigDict(
+        arbitrary_types_allowed=True,
         frozen=True,
     )
-    exclude: Optional[Sequence[str]] = None
-    include: Optional[Sequence[str]] = None
+    exclude: Optional[tuple[str, ...]] = None
+    include: Optional[tuple[str, ...]] = None
 
 
 class InstanceConfig(BaseModel):
     model_config = ConfigDict(
         validate_default=True,
+        arbitrary_types_allowed=True,
         frozen=True,
     )
     disable_generic_tags: Optional[bool] = None
@@ -40,14 +43,14 @@ class InstanceConfig(BaseModel):
     name: str
     password: Optional[str] = None
     port: Optional[int] = None
-    proc_names: Optional[Sequence[str]] = None
-    proc_names_exclude: Optional[Sequence[str]] = None
-    proc_regex: Optional[Sequence[str]] = None
-    proc_regex_exclude: Optional[Sequence[str]] = None
+    proc_names: Optional[tuple[str, ...]] = None
+    proc_names_exclude: Optional[tuple[str, ...]] = None
+    proc_regex: Optional[tuple[str, ...]] = None
+    proc_regex_exclude: Optional[tuple[str, ...]] = None
     service: Optional[str] = None
     socket: Optional[str] = None
-    status_mapping_override: Optional[Mapping[str, Any]] = None
-    tags: Optional[Sequence[str]] = None
+    status_mapping_override: Optional[MappingProxyType[str, Any]] = None
+    tags: Optional[tuple[str, ...]] = None
     username: Optional[str] = None
 
     @model_validator(mode='before')
@@ -55,25 +58,14 @@ class InstanceConfig(BaseModel):
         return validation.core.initialize_config(getattr(validators, 'initialize_instance', identity)(values))
 
     @field_validator('*', mode='before')
-    def _ensure_defaults(cls, value, info):
+    def _validate(cls, value, info):
         field = cls.model_fields[info.field_name]
         field_name = field.alias or info.field_name
         if field_name in info.context['configured_fields']:
-            return value
+            value = getattr(validators, f'instance_{info.field_name}', identity)(value, field=field)
+        else:
+            value = getattr(defaults, f'instance_{info.field_name}', lambda: value)()
 
-        return getattr(defaults, f'instance_{info.field_name}', lambda: value)()
-
-    @field_validator('*')
-    def _run_validations(cls, value, info):
-        field = cls.model_fields[info.field_name]
-        field_name = field.alias or info.field_name
-        if field_name not in info.context['configured_fields']:
-            return value
-
-        return getattr(validators, f'instance_{info.field_name}', identity)(value, field=field)
-
-    @field_validator('*', mode='after')
-    def _make_immutable(cls, value):
         return validation.utils.make_immutable(value)
 
     @model_validator(mode='after')
