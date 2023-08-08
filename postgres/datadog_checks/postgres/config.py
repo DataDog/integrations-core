627113d742befca108faf2072e9fb2a4f8f261ef
edengorevoy <eden.gorevoy@datadoghq.com>
2023-08-08 21:22:35 +0000

    AGGHHHH

--- postgres/datadog_checks/postgres/config.py
+++ postgres/datadog_checks/postgres/config.py
@@ -60,7 +60,7 @@ class PostgresConfig:
         self.max_connections = instance.get('max_connections', 30)
         self.tags = self._build_tags(instance.get('tags', []))
 
-        ssl = instance.get('ssl', False)
+        ssl = instance.get('ssl', "false")
         if ssl in SSL_MODES:
             self.ssl_mode = ssl
         else:
