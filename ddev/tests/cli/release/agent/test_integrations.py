86d6e0ba39b5d0dd1b8d09a248ee4d9439e0c49c
Alex Lopez <alex.lopez@datadoghq.com>
2023-08-14 10:51:12 +0200

    Complete test suite for command to be migrated

--- ddev/tests/cli/release/agent/test_integrations.py
+++ ddev/tests/cli/release/agent/test_integrations.py
@@ -1,8 +1,10 @@
 # (C) Datadog, Inc. 2023-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import re
 
 import pytest
+from datadog_checks.dev.tooling.constants import get_root, set_root
 
 
 def test_integrations_without_arguments(fake_integrations, ddev):
@@ -12,12 +14,53 @@ def test_integrations_without_arguments(fake_integrations, ddev):
     assert result.output.rstrip('\n') == fake_integrations
 
 
+def test_integrations_write_without_force_aborts_when_changelog_already_exists(repo_with_fake_integrations, ddev):
+    repo, fake_changelog = repo_with_fake_integrations
+
+    # Create the integrations file to trigger the condition
+    open(repo.path / 'AGENT_INTEGRATIONS.md', 'w').close()
+
+    result = ddev('release', 'agent', 'integrations', '--write')
+    assert result.exit_code == 1
+    assert re.match(
+        'Output file (.*?)AGENT_INTEGRATIONS.md already exists, run the command again with --force to overwrite',
+        result.output,
+    )
+
+
+def test_integrations_write_force(repo_with_fake_integrations, ddev):
+    repo, fake_changelog = repo_with_fake_integrations
+
+    result = ddev('release', 'agent', 'integrations', '--write', '--force')
+    assert result.exit_code == 0
+    with open(repo.path / 'AGENT_INTEGRATIONS.md') as f:
+        assert f.read().rstrip('\n') == fake_changelog
+
+
+def test_integrations_since_to(fake_integrations, ddev):
+    result = ddev('release', 'agent', 'integrations', '--since', '7.38.0', '--to', '7.39.0')
+    assert result.exit_code == 0
+
+    expected_output = """## Datadog Agent version 7.39.0
+
+* datadog-bar: 2.0.0
+* datadog-checks-base: 3.0.0
+
+## Datadog Agent version 7.38.0
+
+* foo: 1.5.0
+* bar: 1.0.0
+* datadog_checks_base: 2.1.3"""
+    assert result.output.rstrip('\n') == expected_output.strip('\n')
+
+
 @pytest.fixture
 def repo_with_fake_integrations(repo_with_history, config_file):
+    old_root = get_root()
+    set_root(str(repo_with_history.path))
     config_file.model.repos['core'] = str(repo_with_history.path)
     config_file.save()
-    expected_output = (
-        """
+    expected_output = """
 ## Datadog Agent version 7.40.0
 
 * datadog-onlywin: 1.0.0
@@ -37,11 +80,11 @@ def repo_with_fake_integrations(repo_with_history, config_file):
 
 * datadog-foo: 1.0.0
 """
-    )
-    return (
+    yield (
         repo_with_history,
         expected_output.strip('\n'),
     )
+    set_root(old_root)
 
 
 @pytest.fixture
