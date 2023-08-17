8c4a5d9c2aaed6682b36f4c1f84d540839b5878f
Alex Lopez <alex.lopez@datadoghq.com>
2023-08-17 17:59:09 +0200

    Remove autocompletion and rename checks -> integrations

--- ddev/src/ddev/cli/release/agent/integrations_changelog.py
+++ ddev/src/ddev/cli/release/agent/integrations_changelog.py
@@ -9,7 +9,6 @@ from io import StringIO
 from typing import TYPE_CHECKING
 
 import click
-from datadog_checks.dev.tooling.testing import complete_active_checks
 
 if TYPE_CHECKING:
     from ddev.cli.application import Application
@@ -20,14 +19,14 @@ INTEGRATION_CHANGELOG_PATTERN = r'^## (\d+\.\d+\.\d+) / \d{4}-\d{2}-\d{2}$'
 @click.command(
     short_help="Update integration CHANGELOG.md by adding the Agent version",
 )
-@click.argument('checks', shell_complete=complete_active_checks, nargs=-1)
+@click.argument('integrations', nargs=-1)
 @click.option('--since', help="Initial Agent version", default='6.3.0')
 @click.option('--to', help="Final Agent version")
 @click.option(
     '--write', '-w', is_flag=True, help="Write to the changelog file, if omitted contents will be printed to stdout"
 )
 @click.pass_obj
-def integrations_changelog(app: Application, checks: list[str], since: str, to: str, write: bool):
+def integrations_changelog(app: Application, integrations: tuple[str], since: str, to: str, write: bool):
     """
     Update integration CHANGELOG.md by adding the Agent version.
 
@@ -37,21 +36,21 @@ def integrations_changelog(app: Application, checks: list[str], since: str, to: 
     from ddev.cli.release.agent.common import get_changes_per_agent
 
     # Process all checks if no check is passed
-    if not checks:
-        checks = [integration.name for integration in app.repo.integrations.iter('all')]
+    if not integrations:
+        integrations = [integration.name for integration in app.repo.integrations.iter('all')]
 
     changes_per_agent = get_changes_per_agent(app.repo, since, to)
 
     integrations_versions: dict[str, dict[str, str]] = defaultdict(dict)
     for agent_version, version_changes in changes_per_agent.items():
         for name, (ver, _) in version_changes.items():
-            if name not in checks:
+            if name not in integrations:
                 continue
             integrations_versions[name][ver] = agent_version
 
-    for check, versions in integrations_versions.items():
+    for integration, versions in integrations_versions.items():
         changelog_contents = StringIO()
-        changelog_file = app.repo.integrations.get(check).path / 'CHANGELOG.md'
+        changelog_file = app.repo.integrations.get(integration).path / 'CHANGELOG.md'
 
         if not changelog_file.exists():
             continue
@@ -64,12 +63,14 @@ def integrations_changelog(app: Application, checks: list[str], since: str, to: 
                     agent_version = versions[version]
                     line = "{} / Agent {}\n".format(line.strip(), agent_version)
                 else:
-                    app.display_debug("Agent version not found for integration {} line {}".format(check, line.strip()))
+                    app.display_debug(
+                        "Agent version not found for integration {} line {}".format(integration, line.strip())
+                    )
             changelog_contents.write(line)
 
         # Save the changelog on disk if --write was passed
         if write:
             app.display_info("Writing to {}".format(changelog_file))
-            (app.repo.integrations.get(check).path / changelog_file).write_text(changelog_contents.getvalue())
+            changelog_file.write_text(changelog_contents.getvalue())
         else:
             print(changelog_contents.getvalue())
