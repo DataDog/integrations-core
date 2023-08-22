ba0d46fa71e5fffb82d2f2d1599d42770a8b9db1
Florent Clarret <florent.clarret@datadoghq.com>
2023-08-22 13:19:19 +0200

    Update the test environments

--- solr/tests/conftest.py
+++ solr/tests/conftest.py
@@ -7,14 +7,22 @@ import os
 import pytest
 
 from datadog_checks.dev import docker_run, get_here
+from datadog_checks.dev.conditions import CheckEndpoints
 from datadog_checks.dev.utils import load_jmx_config
 
-from .common import HOST
+from .common import HOST, UI_PORT
 
 
 @pytest.fixture(scope="session")
 def dd_environment():
-    with docker_run(os.path.join(get_here(), 'docker', 'docker-compose.yml'), mount_logs=True):
+    with docker_run(
+        os.path.join(get_here(), 'docker', 'docker-compose.yml'),
+        conditions=[
+            CheckEndpoints(f"http://{HOST}:{UI_PORT}"),
+        ],
+        mount_logs=True,
+        build=True,
+    ):
         instance = load_jmx_config()
         instance['instances'][0]['host'] = HOST
         instance['instances'][0]['port'] = 18983
