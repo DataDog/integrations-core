2773d90699b6fae591cbf5e951beaa0f76540d68
Andrew Zhang <andrew.zhang@datadoghq.com>
2023-08-28 15:54:11 -0400

    Fix style

--- kafka_consumer/datadog_checks/kafka_consumer/config.py
+++ kafka_consumer/datadog_checks/kafka_consumer/config.py
@@ -49,7 +49,11 @@ class KafkaConfig:
         self._tls_private_key = instance.get("tls_private_key") or instance.get("ssl_keyfile")
         self._tls_private_key_password = instance.get("tls_private_key_password") or instance.get("ssl_password")
         # Note: Remapped field is ignored if standard field is already used
-        self._tls_validate_hostname = is_affirmative(instance.get("tls_validate_hostname", True)) if "tls_validate_hostname" in instance else is_affirmative(instance.get("ssl_check_hostname", True))
+        self._tls_validate_hostname = (
+            is_affirmative(instance.get("tls_validate_hostname", True))
+            if "tls_validate_hostname" in instance
+            else is_affirmative(instance.get("ssl_check_hostname", True))
+        )
 
         if self._tls_cert or self._tls_ca_cert or self._tls_private_key or self._tls_private_key_password:
             self._tls_verify = True
