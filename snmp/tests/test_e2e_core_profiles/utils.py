d01de55e1aaa9b6edacf13bb566927d3fa968e1d
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-08-30 15:56:24 +0200

    format

--- snmp/tests/test_e2e_core_profiles/utils.py
+++ snmp/tests/test_e2e_core_profiles/utils.py
@@ -357,8 +357,9 @@ def assert_all_profile_metrics_and_tags_covered(profile, aggregator):
     for metric, metric_info in metric_and_tags["table_metrics"].items():
         collected_metric_tag_keys = get_collected_metric_tag_keys(aggregator, metric)
         expected_metric_tag_keys = global_tags | set(metric_info.get('tags'))
-        assert collected_metric_tag_keys == expected_metric_tag_keys, \
-            f"collected and profile metric tags differ for metric `{metric}`"
+        assert (
+            collected_metric_tag_keys == expected_metric_tag_keys
+        ), f"collected and profile metric tags differ for metric `{metric}`"
     for metric in metric_and_tags["scalar_metrics"]:
         assert len(aggregator.metrics('snmp.' + metric)) > 0
         for collected_metric in aggregator.metrics('snmp.' + metric):
