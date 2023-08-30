2d75568d9efd6e181a346319859ee726538c4d7b
NouhaManai <nouha.manai@yahoo.fr>
2023-08-30 13:37:06 +0200

    remove unnecessary ip_address formatting

--- snmp/tests/test_e2e_core_profiles/test_profile_citrix_netscaler_sdx.py
+++ snmp/tests/test_e2e_core_profiles/test_profile_citrix_netscaler_sdx.py
@@ -29,7 +29,6 @@ def test_e2e_profile_citrix_netscaler_sdx(dd_agent_check):
         'snmp_device:' + ip_address,
     ] + [
         'netscaler_sdx_system_bios_version:zombies',
-        'netscaler_sdx_system_dns:driving driving Jaded their oxen',
         'netscaler_sdx_system_gateway:acted quaintly oxen',
         'netscaler_sdx_system_gateway_type:ipv4',
         'netscaler_sdx_system_netmask:driving acted',
