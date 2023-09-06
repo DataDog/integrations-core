922c44353c31860206cfd424be14ffae94169109
JoJo <jojo.meunier@datadoghq.com>
2023-09-06 12:08:02 -0400

    Set lower connection timeout on connection pool, to avoid long running checks (#15768)
    * Set lower connection timeout on connection pool, to avoid long running checks
    
    * except PoolTimeout on autodiscovery & emit warning
    
    * changelog entry

--- postgres/datadog_checks/postgres/connections.py
+++ postgres/datadog_checks/postgres/connections.py
@@ -144,7 +144,7 @@ class MultiDatabaseConnectionPool(object):
         """
         with self._mu:
             pool = self._get_connection_pool(dbname=dbname, ttl_ms=ttl_ms, timeout=timeout, persistent=persistent)
-            db = pool.getconn(timeout=timeout)
+            db = pool.getconn()
         try:
             yield db
         finally:
