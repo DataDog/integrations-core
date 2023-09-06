8c61ac6d2f95746095cf33fce51aca32bcaa07f5
Sarah Witt <sarah.witt@datadoghq.com>
2023-09-06 15:38:27 -0400

    update models and style

--- cloud_foundry_api/datadog_checks/cloud_foundry_api/config_models/defaults.py
+++ cloud_foundry_api/datadog_checks/cloud_foundry_api/config_models/defaults.py
@@ -106,6 +106,10 @@ def instance_log_requests(field, value):
     return False
 
 
+def instance_max_lookback(field, value):
+    return 60
+
+
 def instance_metric_patterns(field, value):
     return get_default_field_value(field, value)
 
--- cloud_foundry_api/datadog_checks/cloud_foundry_api/config_models/instance.py
+++ cloud_foundry_api/datadog_checks/cloud_foundry_api/config_models/instance.py
@@ -71,6 +71,7 @@ class InstanceConfig(BaseModel):
     kerberos_keytab: Optional[str]
     kerberos_principal: Optional[str]
     log_requests: Optional[bool]
+    max_lookback: Optional[int]
     metric_patterns: Optional[MetricPatterns]
     min_collection_interval: Optional[float]
     ntlm_domain: Optional[str]
