00ec8658da7908923b01bf7260ca8847c277a4a2
Jennifer Chen <jennifer.chen@datadoghq.com>
2023-09-08 09:24:43 -0400

    Use fixtures and common helper function for tests

--- kube_scheduler/tests/test_kube_scheduler_1_26.py
+++ kube_scheduler/tests/test_kube_scheduler_1_26.py
@@ -10,16 +10,15 @@ import pytest
 from datadog_checks.base.checks.kube_leader import ElectionRecordAnnotation
 from datadog_checks.kube_scheduler import KubeSchedulerCheck
 
-instance = {'prometheus_url': 'http://localhost:10251/metrics', 'send_histograms_buckets': True}
+from .common import HERE
 
 # Constants
 CHECK_NAME = 'kube_scheduler'
-NAMESPACE = 'kube_scheduler'
 
 
 @pytest.fixture()
 def mock_metrics():
-    f_name = os.path.join(os.path.dirname(__file__), 'fixtures', 'metrics_1.26.0.txt')
+    f_name = os.path.join(HERE, 'fixtures', 'metrics_1.26.0.txt')
     with open(f_name, 'r') as f:
         text_data = f.read()
     with mock.patch(
@@ -46,87 +45,87 @@ def mock_leader():
         yield
 
 
-def test_check_metrics_1_26(aggregator, mock_metrics, mock_leader):
+def test_check_metrics_1_26(aggregator, mock_metrics, mock_leader, instance):
     c = KubeSchedulerCheck(CHECK_NAME, {}, [instance])
     c.check(instance)
 
     def assert_metric(name, **kwargs):
         # Wrapper to keep assertions < 120 chars
-        aggregator.assert_metric(NAMESPACE + name, **kwargs)
+        aggregator.assert_metric("{}.{}".format(CHECK_NAME, name), **kwargs)
 
-    assert_metric('.pod_preemption.attempts', value=3.0, tags=[])
+    assert_metric('pod_preemption.attempts', value=3.0, tags=[])
 
-    assert_metric('.scheduling.algorithm_duration.count', value=7, tags=['upper_bound:0.001'])
-    assert_metric('.scheduling.algorithm_duration.sum', value=0.002138012)
+    assert_metric('scheduling.algorithm_duration.count', value=7, tags=['upper_bound:0.001'])
+    assert_metric('scheduling.algorithm_duration.sum', value=0.002138012)
 
-    assert_metric('.schedule_attempts', value=7.0, tags=['profile:default-scheduler', 'result:scheduled'])
-    assert_metric('.schedule_attempts', value=3.0, tags=['profile:default-scheduler', 'result:unschedulable'])
+    assert_metric('schedule_attempts', value=7.0, tags=['profile:default-scheduler', 'result:scheduled'])
+    assert_metric('schedule_attempts', value=3.0, tags=['profile:default-scheduler', 'result:unschedulable'])
 
-    assert_metric('.scheduling.attempt_duration.sum', value=0.000195006, tags=[])
+    assert_metric('scheduling.attempt_duration.sum', value=0.000195006, tags=[])
     assert_metric(
-        '.scheduling.attempt_duration.count',
+        'scheduling.attempt_duration.count',
         value=7.0,
         tags=['profile:default-scheduler', 'result:scheduled', 'upper_bound:none'],
     )
     assert_metric(
-        '.scheduling.attempt_duration.count',
+        'scheduling.attempt_duration.count',
         value=3.0,
         tags=['profile:default-scheduler', 'result:unschedulable', 'upper_bound:none'],
     )
 
-    assert_metric('.scheduling.pod.scheduling_duration.sum', value=0.056623216, tags=['attempts:1'])
-    assert_metric('.scheduling.pod.scheduling_duration.sum', value=20.781201637, tags=['attempts:2'])
-    assert_metric('.scheduling.pod.scheduling_duration.count', value=4.0, tags=['attempts:1', 'upper_bound:none'])
-    assert_metric('.scheduling.pod.scheduling_duration.count', value=3.0, tags=['attempts:2', 'upper_bound:none'])
+    assert_metric('scheduling.pod.scheduling_duration.sum', value=0.056623216, tags=['attempts:1'])
+    assert_metric('scheduling.pod.scheduling_duration.sum', value=20.781201637, tags=['attempts:2'])
+    assert_metric('scheduling.pod.scheduling_duration.count', value=4.0, tags=['attempts:1', 'upper_bound:none'])
+    assert_metric('scheduling.pod.scheduling_duration.count', value=3.0, tags=['attempts:2', 'upper_bound:none'])
 
-    assert_metric('.scheduling.pod.scheduling_attempts.sum', value=10.0)
-    assert_metric('.scheduling.pod.scheduling_attempts.count', value=7.0)
+    assert_metric('scheduling.pod.scheduling_attempts.sum', value=10.0)
+    assert_metric('scheduling.pod.scheduling_attempts.count', value=7.0)
 
     assert_metric(
-        '.client.http.requests_duration.count',
+        'client.http.requests_duration.count',
         tags=['host:192.168.58.2:8443', 'upper_bound:8.0', 'verb:GET'],
     )
     assert_metric(
-        '.client.http.requests_duration.sum',
+        'client.http.requests_duration.sum',
         value=0.7267887909999999,
         tags=['host:192.168.58.2:8443', 'verb:GET'],
     )
 
     assert_metric(
-        '.pod_preemption.victims.sum',
+        'pod_preemption.victims.sum',
         value=2.0,
         tags=[],
     )
 
     assert_metric(
-        '.pod_preemption.victims.count',
+        'pod_preemption.victims.count',
         value=2.0,
         tags=[],
     )
 
-    assert_metric('.pending_pods', value=1.0, tags=['queue:active'])
-    assert_metric('.pending_pods', value=2.0, tags=['queue:backoff'])
-    assert_metric('.pending_pods', value=3.0, tags=['queue:unschedulable'])
+    assert_metric('pending_pods', value=1.0, tags=['queue:active'])
+    assert_metric('pending_pods', value=2.0, tags=['queue:backoff'])
+    assert_metric('pending_pods', value=3.0, tags=['queue:unschedulable'])
 
-    assert_metric('.queue.incoming_pods', value=7.0, tags=['event:PodAdd', 'queue:active'])
-    assert_metric('.queue.incoming_pods', value=3.0, tags=['event:NodeTaintChange', 'queue:active'])
-    assert_metric('.queue.incoming_pods', value=3.0, tags=['event:ScheduleAttemptFailure', 'queue:unschedulable'])
+    assert_metric('queue.incoming_pods', value=7.0, tags=['event:PodAdd', 'queue:active'])
+    assert_metric('queue.incoming_pods', value=3.0, tags=['event:NodeTaintChange', 'queue:active'])
+    assert_metric('queue.incoming_pods', value=3.0, tags=['event:ScheduleAttemptFailure', 'queue:unschedulable'])
 
-    assert_metric('.goroutines')
-    assert_metric('.gc_duration_seconds.sum')
-    assert_metric('.gc_duration_seconds.count')
-    assert_metric('.gc_duration_seconds.quantile')
-    assert_metric('.threads')
-    assert_metric('.open_fds')
-    assert_metric('.max_fds')
-    assert_metric('.client.http.requests')
+    assert_metric('goroutines')
+    assert_metric('gc_duration_seconds.sum')
+    assert_metric('gc_duration_seconds.count')
+    assert_metric('gc_duration_seconds.quantile')
+    assert_metric('threads')
+    assert_metric('open_fds')
+    assert_metric('max_fds')
+    assert_metric('client.http.requests')
 
     expected_le_tags = ["record_kind:endpoints", "record_name:kube-scheduler", "record_namespace:kube-system"]
-    assert_metric('.leader_election.transitions', value=3, tags=expected_le_tags)
-    assert_metric('.leader_election.lease_duration', value=15, tags=expected_le_tags)
-    aggregator.assert_service_check(NAMESPACE + ".leader_election.status", tags=expected_le_tags)
+    assert_metric('leader_election.transitions', value=3, tags=expected_le_tags)
+    assert_metric('leader_election.lease_duration', value=15, tags=expected_le_tags)
+    aggregator.assert_service_check(CHECK_NAME + ".leader_election.status", tags=expected_le_tags)
 
-    assert_metric('.goroutine_by_scheduling_operation', value=0, tags=['operation:Filter'])
-    assert_metric('.goroutine_by_scheduling_operation', value=0, tags=['operation:binding'])
+    assert_metric('goroutine_by_scheduling_operation', value=0, tags=['operation:Filter'])
+    assert_metric('goroutine_by_scheduling_operation', value=0, tags=['operation:binding'])
 
     aggregator.assert_all_metrics_covered()
