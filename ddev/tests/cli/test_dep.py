5e41ecbc84423b2e70ce4b0225631b6f9b158703
Alex Lopez <alex.lopez@datadoghq.com>
2023-09-13 15:21:54 +0200

    Clean up tests

--- ddev/tests/cli/test_dep.py
+++ ddev/tests/cli/test_dep.py
@@ -7,7 +7,6 @@ from unittest import mock
 import pytest
 import tomli
 import tomli_w
-from datadog_checks.dev.tooling.utils import get_root, set_root
 
 
 def test_pin(ddev, fake_repo):
@@ -37,8 +36,8 @@ def test_pin_non_canonical_name(ddev, fake_repo):
 
 
 def test_freeze(ddev, fake_repo):
-    create_integration(fake_repo, 'foo1', ['dep-a==1.0.0', 'dep-b==3.1.4'])
-    create_integration(fake_repo, 'bar1', ['dep-a==1.0.0', 'dep-c==5.1.0'])
+    create_integration(fake_repo, 'foo', ['dep-a==1.0.0', 'dep-b==3.1.4'])
+    create_integration(fake_repo, 'bar', ['dep-a==1.0.0', 'dep-c==5.1.0'])
     # datadog_checks_dev deps are not shipped with the agent
     create_integration(fake_repo, 'datadog_checks_dev', ['dep-d==4.4.4'])
 
@@ -62,8 +61,8 @@ dep-c==5.1.0
 
 
 def test_sync(ddev, fake_repo):
-    create_integration(fake_repo, 'foo2', ['dep-a==1.0.0', 'dep-b==3.1.4'])
-    create_integration(fake_repo, 'bar2', ['dep-a==1.0.0'])
+    create_integration(fake_repo, 'foo', ['dep-a==1.0.0', 'dep-b==3.1.4'])
+    create_integration(fake_repo, 'bar', ['dep-a==1.0.0'])
 
     requirements = """
 dep-a==1.1.1
@@ -78,8 +77,8 @@ dep-b==3.1.4
     assert result.exit_code == 0
     assert result.output == 'Files updated: 2\n'
 
-    assert_dependencies(fake_repo, 'foo2', ['dep-a==1.1.1', 'dep-b==3.1.4'])
-    assert_dependencies(fake_repo, 'bar2', ['dep-a==1.1.1'])
+    assert_dependencies(fake_repo, 'foo', ['dep-a==1.1.1', 'dep-b==3.1.4'])
+    assert_dependencies(fake_repo, 'bar', ['dep-a==1.1.1'])
 
 
 class TestUpdates:
@@ -287,11 +286,7 @@ def fake_repo(tmp_path, config_file):
     config_file.model.repos['core'] = str(tmp_path)
     config_file.save()
 
-    # XXX: Remove the root manipulation once the commands are ported over
-    old_root = get_root()
-    set_root(str(tmp_path))
     yield tmp_path
-    set_root(old_root)
 
 
 def assert_dependencies(root, name, dependencies):
