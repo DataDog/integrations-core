aca665d3bd6edf7466a88f9a6ee4d59969496468
Zhengda Lu <zhengda.lu@datadoghq.com>
2023-09-14 19:02:36 +0000

    update changelog

--- postgres/tests/test_connections.py
+++ postgres/tests/test_connections.py
@@ -323,7 +323,6 @@ def test_conn_attempt_to_connect(pg_instance):
     # Change the password to something that is not the correct password
     pg_instance['password'] = 1234
     check = PostgreSql('postgres', {}, [pg_instance])
-    check._attempt_to_connect()
     with pytest.raises(psycopg.OperationalError):
         check._attempt_to_connect()
 
