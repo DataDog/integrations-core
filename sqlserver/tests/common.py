1230326ba62659ad4b19519d7a7811bdebd4c5cb
Zhengda Lu <zhengda.lu@datadoghq.com>
2023-09-20 03:23:58 +0000

    Emit metrics for TempDB page counts

--- sqlserver/tests/common.py
+++ sqlserver/tests/common.py
@@ -144,6 +144,7 @@ INSTANCE_SQL.update(
         'include_ao_metrics': False,
         'include_master_files_metrics': True,
         'disable_generic_tags': True,
+        'include_db_file_space_usage_metrics': True,
     }
 )
 
