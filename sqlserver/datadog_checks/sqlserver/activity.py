9c55e18343dedcb169265e3169c935bf0eafec3d
Joel Marcotte <joel.marcotte@datadoghq.com>
2023-09-27 10:52:33 -0400

    tabs

--- sqlserver/datadog_checks/sqlserver/activity.py
+++ sqlserver/datadog_checks/sqlserver/activity.py
@@ -103,12 +103,12 @@ SELECT
     c.client_net_address as client_address,
     sess.host_name as host_name,
     sess.program_name as program_name,
-	dbspu.user_objects_alloc_page_count as user_objects_alloc_page_count,
+    dbspu.user_objects_alloc_page_count as user_objects_alloc_page_count,
     dbspu.user_objects_dealloc_page_count as user_objects_dealloc_page_count,
     dbspu.internal_objects_alloc_page_count as internal_objects_alloc_page_count,
     dbspu.internal_objects_dealloc_page_count as internal_objects_dealloc_page_count
 FROM sys.dm_exec_sessions sess
-	INNER JOIN sys.dm_db_session_space_usage dbspu
+    INNER JOIN sys.dm_db_session_space_usage dbspu
            ON sess.session_id = dbspu.session_id
     INNER JOIN sys.dm_exec_connections c
         ON sess.session_id = c.session_id
