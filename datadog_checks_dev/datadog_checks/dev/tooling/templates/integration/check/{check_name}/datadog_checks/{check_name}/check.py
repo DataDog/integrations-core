97073697e83b0d92f249e82432afa30f8559190b
steveny91 <steven.yuen@datadoghq.com>
2023-09-27 17:14:31 -0400

    fix template

--- datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/datadog_checks/{check_name}/check.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/templates/integration/check/{check_name}/datadog_checks/{check_name}/check.py
@@ -9,7 +9,6 @@ from datadog_checks.base import AgentCheck  # noqa: F401
 
 
 class {check_class}(AgentCheck):
-
     # This will be the prefix of every metric and service check the integration sends
     __NAMESPACE__ = '{check_name}'
 
