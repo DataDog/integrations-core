61c6640b8697ada0b70cb1e85df0ad27f4b0496b
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2023-09-28 11:27:07 +0200

    Adjust changelog generation to use towncrier

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/release/changelog.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/release/changelog.py
@@ -1,18 +1,13 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-import os
 from collections import namedtuple
-from datetime import date, datetime
-from io import StringIO
 
 import click
 from semver import VersionInfo
 
-from ....fs import stream_file_lines, write_file
-from ...constants import get_root
 from ...utils import complete_testable_checks, get_valid_checks, get_version_string
-from ..console import CONTEXT_SETTINGS, abort, echo_info, echo_success, validate_check_arg
+from ..console import CONTEXT_SETTINGS, abort, echo_info, run_or_abort, validate_check_arg
 
 ChangelogEntry = namedtuple('ChangelogEntry', 'number, title, url, author, author_url, from_contributor')
 
@@ -71,69 +66,4 @@ def changelog(
     if not quiet:
         echo_info(f'Current version of check {check}: {cur_version}, bumping to: {version}')
 
-    # read the old contents
-    if check:
-        changelog_path = os.path.join(get_root(), check, output_file)
-    else:
-        changelog_path = os.path.join(get_root(), output_file)
-    old = list(stream_file_lines(changelog_path))
-
-    if initial:
-        # For initial releases, just keep the ddev generated CHANGELOG but update the date to today
-        for idx, line in enumerate(old):
-            if line.startswith("## 1.0.0"):
-                old[idx] = f"## 1.0.0 / {date.today()}\n"
-                break
-        write_result(dry_run, changelog_path, ''.join(old), num_changes=1)
-        return
-
-    # find the first header below the Unreleased section
-    header_index = 2
-    for index in range(2, len(old)):
-        if old[index].startswith("##") and "## Unreleased" not in old[index]:
-            header_index = index
-            break
-
-    # get text from the unreleased section
-    if header_index == 4:
-        abort('There are no changes for this integration')
-
-    changelogs = old[4:header_index]
-    num_changelogs = 0
-    for line in changelogs:
-        if line.startswith('* '):
-            num_changelogs += 1
-
-    # the header contains version and date
-    header = f"## {version} / {datetime.utcnow().strftime('%Y-%m-%d')}\n"
-
-    # store the new changelog in memory
-    new_entry = StringIO()
-    new_entry.write(header)
-    new_entry.write('\n')
-
-    # write the new changelog in memory
-    changelog_buffer = StringIO()
-
-    # preserve the title and unreleased section
-    changelog_buffer.write(''.join(old[:4]))
-
-    # prepend the new changelog to the old contents
-    # make the command idempotent
-    if header not in old:
-        changelog_buffer.write(new_entry.getvalue())
-
-    # append the rest of the old changelog
-    changelog_buffer.write(''.join(old[4:]))
-
-    write_result(dry_run, changelog_path, changelog_buffer.getvalue(), num_changelogs)
-
-
-def write_result(dry_run, changelog_path, final_output, num_changes):
-    # print on the standard out in case of a dry run
-    if dry_run:
-        echo_info(final_output)
-    else:
-        # overwrite the old changelog
-        write_file(changelog_path, final_output)
-        echo_success(f"Successfully generated {num_changes} change{'s' if num_changes > 1 else ''}")
+    run_or_abort(["towncrier", "build", "--dir", check, "--config", "towncrier.toml", "--version", version])
