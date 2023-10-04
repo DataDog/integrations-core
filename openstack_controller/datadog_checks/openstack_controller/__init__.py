95d6336ada471a5e9e6188015826a08ef7f2f59b
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2023-10-04 17:20:00 +0200

    Renamed check.py to openstack_controller.py

--- openstack_controller/datadog_checks/openstack_controller/__init__.py
+++ openstack_controller/datadog_checks/openstack_controller/__init__.py
@@ -2,6 +2,6 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
-from .check import OpenStackControllerCheck
+from .openstack_controller import OpenStackControllerCheck
 
 __all__ = ['__version__', 'OpenStackControllerCheck']
