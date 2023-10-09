93586314b3e60d33ab2b888436699688c4a0a783
Joe Rouvier <joe@rouvier.org>
2023-10-09 14:19:08 -0700

    add `yarn.node.last_update_time` and make `yarn.node.last_health_update` optional for unit tests

--- yarn/datadog_checks/yarn/yarn.py
+++ yarn/datadog_checks/yarn/yarn.py
@@ -111,7 +111,8 @@ DEPRECATED_YARN_APP_METRICS = {
 
 # Node metrics for YARN
 YARN_NODE_METRICS = {
-    'lastHealthUpdate': ('yarn.node.last_health_update', GAUGE),
+    'lastHealthUpdate': ('yarn.node.last_health_update', GAUGE),  # Replaced by lastNodeUpdateTime in yarn 3+
+    'lastNodeUpdateTime': ('yarn.node.last_update_time', GAUGE),  # Only in yarn 3+
     'usedMemoryMB': ('yarn.node.used_memory_mb', GAUGE),
     'availMemoryMB': ('yarn.node.avail_memory_mb', GAUGE),
     'usedVirtualCores': ('yarn.node.used_virtual_cores', GAUGE),
