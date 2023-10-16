cccc74b8fb7e2b9a29d117c7afc935b57f705055
HadhemiDD <hadhemi.samaali@datadoghq.com>
2023-10-16 15:26:54 +0200

    metadata and check.py

--- nvidia_triton/datadog_checks/nvidia_triton/check.py
+++ nvidia_triton/datadog_checks/nvidia_triton/check.py
@@ -1,98 +1,88 @@
 # (C) Datadog, Inc. 2023-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from typing import Any  # noqa: F401
+from typing import Any
+from urllib.parse import urljoin  # noqa: F401
 
 from datadog_checks.base import AgentCheck  # noqa: F401
+from datadog_checks.base import AgentCheck, OpenMetricsBaseCheckV2
 
-# from datadog_checks.base.utils.db import QueryManager
-# from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
-# from json import JSONDecodeError
+from .metrics import METRICS_MAP
 
 
-class NvidiaTritonCheck(AgentCheck):
+DEFAULT_METADATA_ENDPOINT = '/v2'
+DEFAULT_SERVER_STATS_ENDPOINT = '/v2/models/stats'
+DEFAULT_HEALTH_ENDPOINT = 'v2/health/ready'
+
+class NvidiaTritonCheck(AgentCheck , OpenMetricsBaseCheckV2):
 
     # This will be the prefix of every metric and service check the integration sends
+    DEFAULT_METRIC_LIMIT = 0
     __NAMESPACE__ = 'nvidia_triton'
 
-    def __init__(self, name, init_config, instances):
+    def __init__(self, name, init_config, instances=None):
         super(NvidiaTritonCheck, self).__init__(name, init_config, instances)
+        self.tags = self.instance.get('tags', [])
+        self.api_url = self.instance.get('nvidia_triton_api_endpoint')
 
-        # Use self.instance to read the check configuration
-        # self.url = self.instance.get("url")
+    def get_default_config(self):
+        return {'metrics': [METRICS_MAP]}
+        
+    @AgentCheck.metadata_entrypoint
+    def _submit_version_metadata(self):
+        endpoint = urljoin(self.api_url, DEFAULT_METADATA_ENDPOINT)
+        response = self.http.get(endpoint)
 
-        # If the check is going to perform SQL queries you should define a query manager here.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
-        # sample_query = {
-        #     "name": "sample",
-        #     "query": "SELECT * FROM sample_table",
-        #     "columns": [
-        #         {"name": "metric", "type": "gauge"}
-        #     ],
-        # }
-        # self._query_manager = QueryManager(self, self.execute_query, queries=[sample_query])
-        # self.check_initializations.append(self._query_manager.compile_queries)
+        if response.ok:
+            data = response.json()
+            version = data.get("version", "")
+            version_split = version.split(".")
+            if len(version_split) >= 3:
+                major = version_split[0]
+                minor = version_split[1]
+                patch = version_split[2]
 
-    def check(self, _):
-        # type: (Any) -> None
-        # The following are useful bits of code to help new users get started.
+                version_raw = f'{major}.{minor}.{patch}'
 
-        # Perform HTTP Requests with our HTTP wrapper.
-        # More info at https://datadoghq.dev/integrations-core/base/http/
-        # try:
-        #     response = self.http.get(self.url)
-        #     response.raise_for_status()
-        #     response_json = response.json()
+                version_parts = {
+                    'major': major,
+                    'minor': minor,
+                    'patch': patch,
+                }
+                self.set_metadata('version', version_raw, scheme='semver', part_map=version_parts)
+            else:
+                self.log.debug("Malformed Triton Server version format: %s", version)
+        else:
+            self.log.debug("Could not retrieve version metadata.")
 
-        # except Timeout as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request timeout: {}, {}".format(self.url, e),
-        #     )
-        #     raise
+    
+    def _submit_server_metrics(self):
+        endpoint = self.openmetrics_endpoint
+        response = self.http.get(endpoint)
+        
+        if not response.ok:
+            self.log.error("Unable to get a valid response from url: %s with status code: %s", str(endpoint), str(response.status_code))
+            return
+        
+        #TO DO : add the code to parse the endpoint metrics
+        
+    def _check_server_health(self, check_type, extra_params=None, response_handler=None):
 
-        # except (HTTPError, InvalidURL, ConnectionError) as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
+        endpoint = urljoin(self.api_url, DEFAULT_HEALTH_ENDPOINT)
+        response = self.http.get(endpoint)
+        #The helath endpoint only exposes the status code in verbose mode, so we need to check we can properly retrieve it from the response
 
-        # except JSONDecodeError as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="JSON Parse failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
+        if response.status_code != 200:
+            self.service_check('health.status', AgentCheck.CRITICAL, self._tags)
+        if response.status_code == 200:
+            self.service_check('health.status', AgentCheck.OK, self._tags)
+        else:
+            self.service_check('health.status', AgentCheck.UNKNOWN, self._tags)
 
-        # except ValueError as e:
-        #     self.service_check(
-        #         "can_connect", AgentCheck.CRITICAL, message=str(e)
-        #     )
-        #     raise
-
-        # This is how you submit metrics
-        # There are different types of metrics that you can submit (gauge, event).
-        # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
-        # self.gauge("test", 1.23, tags=['foo:bar'])
-
-        # Perform database queries using the Query Manager
-        # self._query_manager.execute()
-
-        # This is how you use the persistent cache. This cache file based and persists across agent restarts.
-        # If you need an in-memory cache that is persisted across runs
-        # You can define a dictionary in the __init__ method.
-        # self.write_persistent_cache("key", "value")
-        # value = self.read_persistent_cache("key")
-
-        # If your check ran successfully, you can send the status.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck.service_check
-        # self.service_check("can_connect", AgentCheck.OK)
-
-        # If it didn't then it should send a critical service check
-        self.service_check("can_connect", AgentCheck.CRITICAL)
+    def check(self, instance):
+        if self.instance.get("openmetrics_endpoint"):
+            super().check(instance)
+        if self.api_url:
+            self._check_server_health()
+            self._submit_version_metadata()
+            self._submit_server_metrics()
