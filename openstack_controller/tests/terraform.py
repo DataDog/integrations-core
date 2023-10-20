ec418240c1628573f05856f1a0857311122808ff
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2023-10-20 17:39:30 +0200

    Fixed error starting and stopping e2e environment

--- openstack_controller/tests/terraform.py
+++ openstack_controller/tests/terraform.py
@@ -112,7 +112,7 @@ class TerraformDown(LazyFunction):
         with TempDir('terraform') as temp_dir:
             terraform_dir = os.path.join(temp_dir, 'terraform')
             with chdir(terraform_dir):
-                pass
+                return ""
                 # env = construct_env_vars()
                 # env['TF_VAR_desired_status'] = "TERMINATED"
                 # run_command(['terraform', 'apply', '-auto-approve', '-input=false', '-no-color'], check=True, env=env)
