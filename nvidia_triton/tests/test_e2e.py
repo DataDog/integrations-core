ed89f75be5e78780ef6e0ea660567388075311c9
HadhemiDD <hadhemi.samaali@datadoghq.com>
2023-10-20 17:04:35 +0200

    e2e test

---
+++ nvidia_triton/tests/test_e2e.py
@@ -0,0 +1,19 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+import pytest
+
+from datadog_checks.dev.utils import get_metadata_metrics
+
+from . import common
+
+def test_check_nvidia_triton(dd_agent_check, instance):
+    aggregator = dd_agent_check(instance, rate=True)
+    metrics = common.METRICS_MOCK
+
+    for metric in metrics:
+        aggregator.assert_metric(name=metric)
+
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    aggregator.assert_all_metrics_covered()
