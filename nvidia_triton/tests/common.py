43de4ad1213be59c0d3efcac11b57cdbb7e356a1
HadhemiDD <hadhemi.samaali@datadoghq.com>
2023-10-25 12:21:09 +0200

    change metrics name

--- nvidia_triton/tests/common.py
+++ nvidia_triton/tests/common.py
@@ -7,6 +7,7 @@ from datadog_checks.dev import get_docker_hostname, get_here
 
 HERE = get_here()
 HOST = get_docker_hostname()
+PORT = "8002"
 
 
 def get_fixture_path(filename):
@@ -14,17 +15,12 @@ def get_fixture_path(filename):
 
 
 INSTANCE = {
-    "openmetrics_endpoint": f"http://{HOST}:8002/metrics",
-    'tags': ['test:test'],
-}
-
-INSTANCE_MOCK = {
-    'openmetrics_endpoint': 'http://localhost:8002/metrics',
+    "openmetrics_endpoint": f"http://{HOST}:{PORT}/metrics",
     'tags': ['test:test'],
 }
 
 INSTANCE_DISABLED_SERVER_INFO = {
-    'openmetrics_endpoint': f'http://{HOST}:8002/metrics',
+    'openmetrics_endpoint': f'http://{HOST}:{PORT}/metrics',
     'collect_server_info': False,
     'tags': ['test:test'],
 }
@@ -32,48 +28,48 @@ INSTANCE_DISABLED_SERVER_INFO = {
 COMPOSE_FILE = os.path.join(HERE, 'docker', 'docker-compose.yaml')
 
 METRICS_MOCK = {
-    'nv.cache.insertion.duration',
-    'nv.cache.lookup.duration',
-    'nv.cache.num.entries',
-    'nv.cache.num.evictions',
-    'nv.cache.num.hits',
-    'nv.cache.num.lookups',
-    'nv.cache.num.misses',
-    'nv.cache.util',
-    'nv.cpu.memory.total_bytes',
-    'nv.cpu.memory.used_bytes',
-    'nv.cpu.utilization',
-    'nv.energy.consumption.count',
-    'nv.gpu.memory.total_bytes',
-    'nv.gpu.memory.used_bytes',
-    'nv.gpu.power.limit',
-    'nv.gpu.power.usage',
-    'nv.gpu.utilization',
-    'nv.inference.compute.infer.duration_us.count',
-    'nv.inference.compute.infer.summary_us.count',
-    'nv.inference.compute.infer.summary_us.sum',
-    'nv.inference.compute.infer.summary_us.quantile',
-    'nv.inference.compute.input.duration_us.count',
-    'nv.inference.compute.input.summary_us.count',
-    'nv.inference.compute.input.summary_us.sum',
-    'nv.inference.compute.input.summary_us.quantile',
-    'nv.inference.compute.output.duration_us.count',
-    'nv.inference.compute.output.summary_us.count',
-    'nv.inference.compute.output.summary_us.sum',
-    'nv.inference.compute.output.summary_us.quantile',
-    'nv.inference.count.count',
-    'nv.inference.exec.count.count',
-    'nv.inference.pending.request.count',
-    'nv.inference.queue.duration_us.count',
-    'nv.inference.queue.summary_us.count',
-    'nv.inference.queue.summary_us.sum',
-    'nv.inference.queue.summary_us.quantile',
-    'nv.inference.request.duration_us.count',
-    'nv.inference.request.summary_us.count',
-    'nv.inference.request.summary_us.sum',
-    'nv.inference.request.summary_us.quantile',
-    'nv.inference.request_failure.count',
-    'nv.inference.request_success.count',
+    'cache.insertion.duration',
+    'cache.lookup.duration',
+    'cache.num.entries',
+    'cache.num.evictions',
+    'cache.num.hits',
+    'cache.num.lookups',
+    'cache.num.misses',
+    'cache.util',
+    'cpu.memory.total_bytes',
+    'cpu.memory.used_bytes',
+    'cpu.utilization',
+    'energy.consumption.count',
+    'gpu.memory.total_bytes',
+    'gpu.memory.used_bytes',
+    'gpu.power.limit',
+    'gpu.power.usage',
+    'gpu.utilization',
+    'inference.compute.infer.duration_us.count',
+    'inference.compute.infer.summary_us.count',
+    'inference.compute.infer.summary_us.sum',
+    'inference.compute.infer.summary_us.quantile',
+    'inference.compute.input.duration_us.count',
+    'inference.compute.input.summary_us.count',
+    'inference.compute.input.summary_us.sum',
+    'inference.compute.input.summary_us.quantile',
+    'inference.compute.output.duration_us.count',
+    'inference.compute.output.summary_us.count',
+    'inference.compute.output.summary_us.sum',
+    'inference.compute.output.summary_us.quantile',
+    'inference.count.count',
+    'inference.exec.count.count',
+    'inference.pending.request.count',
+    'inference.queue.duration_us.count',
+    'inference.queue.summary_us.count',
+    'inference.queue.summary_us.sum',
+    'inference.queue.summary_us.quantile',
+    'inference.request.duration_us.count',
+    'inference.request.summary_us.count',
+    'inference.request.summary_us.sum',
+    'inference.request.summary_us.quantile',
+    'inference.request_failure.count',
+    'inference.request_success.count',
 }
 
 METRICS_MOCK = [f'nvidia_triton.{m}' for m in METRICS_MOCK]
