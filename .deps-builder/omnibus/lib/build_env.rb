e69c65dbb8e8cf9986ea9cd2a2321a01d95b16c0
Alex Lopez <alex.lopez@datadoghq.com>
2023-10-30 11:17:01 +0100

    Make handling of the constraints file more consistent with the execution model

--- .deps-builder/omnibus/lib/build_env.rb
+++ .deps-builder/omnibus/lib/build_env.rb
@@ -10,10 +10,12 @@ end
 class PythonBuildEnvironment
   # Class to interact with the Python build environment in a way that is convenient
   # for implementing the Python dependency resolution and building model that we
-  # use for Agent integration dependencies.
+  # use for Agent integration dependencies. It's designed to add build commands in `build` blocks
+  # inside software definitions.
   # Note that the Python build environment itself is assumed to be a global (per-project) entity,
   # and instances of the class are expected to use the same data for the most part,
   # they exist as instances mostly to hold a reference to the current builder.
+  @@constraints_file = nil
 
   def initialize(builder)
     @builder = builder
@@ -31,23 +33,31 @@ class PythonBuildEnvironment
 
   # Set a constraint file (for all instances)
   def constrain(val)
-    @@constraint_file = val
+    # Actually set the value at build time, not load time
+    @builder.block "Set constraint file to #{val}" do
+      python_build_env.constraints_file = val
+    end
   end
 
-  def constraint_file
-    @@constraint_file
+  def constraints_file
+    @@constraints_file
   end
 
-  # Run pip wheel with PYTHONPATH set to this build environment and store the produced wheels
-  # inside the project's `installdir`. Apply constraint file if set.
+  def constraints_file=(val)
+    @@constraints_file = val
+  end
+
+  # Run `pip wheel` with this build environment store the produced wheels
+  # inside the project's `installdir`. Apply constraints file if set.
   # `options` are passed to the `shellout!` function used to run commands
   def wheel(arg, **options)
+    # We need to use a block here to ensure an order of execution that is consistent wrt how we set the `constraints_file`.
     # Builder#block uses `instance_eval`, therefore the scope inside the block is as if it were running
     # within the builder instance; that's why we need to access methods via `python_build_env`.
     @builder.block "Build wheels for #{arg}" do
-      constraint_arg = python_build_env.constraint_file && "-c #{python_build_env.constraint_file}" || ""
+      constraints_arg = python_build_env.constraints_file && "-c #{python_build_env.constraints_file}" || ""
 
-      shellout! "#{python_build_env.python} -m pip wheel --no-build-isolation #{constraint_arg} -w #{python_build_env.wheels_dir} #{arg}", **options
+      shellout! "#{python_build_env.python} -m pip wheel --no-build-isolation #{constraints_arg} -w #{python_build_env.wheels_dir} #{arg}", **options
     end
   end
 
