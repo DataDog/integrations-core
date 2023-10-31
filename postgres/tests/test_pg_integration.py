2a46665b3dc180940776e23c27d15c97aa4e33d7
Alex Normand <alex.normand@datadoghq.com>
2023-10-31 17:53:16 +0000

    Revert "Bring back force wal file writing"
    This reverts commit e7e64a4d5ed714c8011a66e7f37bf76fb2ac01ed.

--- postgres/tests/test_pg_integration.py
+++ postgres/tests/test_pg_integration.py
@@ -549,23 +549,6 @@ def test_wal_metrics(aggregator, integration_check, pg_instance, is_aurora):
     aggregator.assert_metric('postgresql.wal_count', count=1, value=expected_num_wals, tags=dd_agent_tags)
     aggregator.assert_metric('postgresql.wal_size', count=1, value=expected_wal_size, tags=dd_agent_tags)
 
-    with postgres_conn.cursor() as cur:
-        # Make sure we're in a stable wal state
-        cur.execute("CHECKPOINT;")
-        # Force a wal switch
-        cur.execute("select pg_switch_wal();")
-        cur.fetchall()
-        # Checkpoint to accelerate new wal file
-        cur.execute("CHECKPOINT;")
-
-    aggregator.reset()
-    check.check(pg_instance)
-
-    expected_num_wals += 1
-    expected_wal_size = expected_num_wals * wal_size
-    aggregator.assert_metric('postgresql.wal_count', count=1, value=expected_num_wals, tags=dd_agent_tags)
-    aggregator.assert_metric('postgresql.wal_size', count=1, value=expected_wal_size, tags=dd_agent_tags)
-
 
 def test_pg_control(aggregator, integration_check, pg_instance):
     check = integration_check(pg_instance)
