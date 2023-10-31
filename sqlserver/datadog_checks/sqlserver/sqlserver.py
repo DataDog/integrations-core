db66c36e88d826fd3a37ca822ee16a412501cd4f
Zhengda Lu <zhengda.lu@datadoghq.com>
2023-10-31 15:59:40 +0000

    Add obfuscation_mode config option to allow enabling obfuscation with go-sqllexer

--- sqlserver/datadog_checks/sqlserver/sqlserver.py
+++ sqlserver/datadog_checks/sqlserver/sqlserver.py
@@ -165,6 +165,9 @@ class SQLServer(AgentCheck):
                     'table_names': is_affirmative(obfuscator_options_config.get('collect_tables', True)),
                     'collect_commands': is_affirmative(obfuscator_options_config.get('collect_commands', True)),
                     'collect_comments': is_affirmative(obfuscator_options_config.get('collect_comments', True)),
+                    # Config to enable/disable obfuscation of sql statements with go-sqllexer pkg
+                    # Valid values for this can be found at https://github.com/DataDog/datadog-agent/blob/main/pkg/obfuscate/obfuscate.go#L108
+                    'obfuscation_mode': obfuscator_options_config.get('obfuscation_mode', ''),
                 }
             )
         )
