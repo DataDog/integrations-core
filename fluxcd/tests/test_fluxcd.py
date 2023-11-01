1bad5af9e851a9754cc3259de8627b10a0b8df31
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2023-11-01 17:57:40 +0100

    fluxcd basic fixes (#16131)
    * Basic fixes:
    
    - add CI test steps
    - adjust readme links
    - adjust/update pyproject.toml
    
    * Add license headers
    
    * Update model files after adding license header
    
    * fix style

--- fluxcd/tests/test_fluxcd.py
+++ fluxcd/tests/test_fluxcd.py
@@ -1,3 +1,6 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
 
 from datadog_checks.dev.utils import get_metadata_metrics
