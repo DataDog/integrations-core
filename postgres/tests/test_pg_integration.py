118b0269b7f9f97bc02e0916e63b289bb71dbf38
Zhengda Lu <zhengda.lu@datadoghq.com>
2023-11-01 14:21:27 +0000

    resolve conflicts

--- postgres/tests/test_pg_integration.py
+++ postgres/tests/test_pg_integration.py
@@ -335,11 +335,13 @@ def test_activity_metrics_no_application_aggregation(aggregator, integration_che
 
 def test_activity_metrics_no_aggregations(aggregator, integration_check, pg_instance):
     pg_instance['collect_activity_metrics'] = True
+    # datname is a required aggregation because our activity metric query is always grouping by database id.
+    # Setting it should issue a warning, be ignored and still produce an aggregation by db
     pg_instance['activity_metrics_excluded_aggregations'] = ['datname', 'application_name', 'usename']
     check = integration_check(pg_instance)
     check.check(pg_instance)
 
-    expected_tags = _get_expected_tags(check, pg_instance)
+    expected_tags = _get_expected_tags(check, pg_instance, db=DB_NAME)
     check_activity_metrics(aggregator, expected_tags)
 
 
@@ -544,21 +546,6 @@ def test_wal_metrics(aggregator, integration_check, pg_instance, is_aurora):
 
     expected_wal_size = expected_num_wals * wal_size
     dd_agent_tags = _get_expected_tags(check, pg_instance)
-    aggregator.assert_metric('postgresql.wal_count', count=1, value=expected_num_wals, tags=dd_agent_tags)
-    aggregator.assert_metric('postgresql.wal_size', count=1, value=expected_wal_size, tags=dd_agent_tags)
-
-    with postgres_conn.cursor() as cur:
-        # Force a wal switch
-        cur.execute("select pg_switch_wal();")
-        cur.fetchall()
-        # Checkpoint to accelerate new wal file
-        cur.execute("CHECKPOINT;")
-
-    aggregator.reset()
-    check.check(pg_instance)
-
-    expected_num_wals += 1
-    expected_wal_size = expected_num_wals * wal_size
     aggregator.assert_metric('postgresql.wal_count', count=1, value=expected_num_wals, tags=dd_agent_tags)
     aggregator.assert_metric('postgresql.wal_size', count=1, value=expected_wal_size, tags=dd_agent_tags)
 
