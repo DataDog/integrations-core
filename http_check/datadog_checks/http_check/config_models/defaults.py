b93efd89c5297646696750ce633731e3087a98d3
Alex Lopez <alex.lopez@datadoghq.com>
2023-11-07 11:33:15 +0100

    Keep the default
    Due to switching to a union type, the example is not picked up as
    default, so to keep this as before, I added it to the default field as well.

--- http_check/datadog_checks/http_check/config_models/defaults.py
+++ http_check/datadog_checks/http_check/config_models/defaults.py
@@ -40,6 +40,10 @@ def instance_empty_default_hostname():
     return False
 
 
+def instance_http_response_status_code():
+    return '(1|2|3)\\d\\d'
+
+
 def instance_include_content():
     return False
 
