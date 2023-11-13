d02fe92f3e6fdd17315bba22c9c058419c2aadb7
Cedric Lamoriniere <cedric.lamoriniere@datadoghq.com>
2023-11-13 23:13:34 +0100

    [CONTINT-3443] fix(kubelet): centralize `get_prometheus_url` call (#16192)
    * fix(kubelet): centralize get_prometheus_url call
    
    * update code after review

--- kubelet/datadog_checks/kubelet/common.py
+++ kubelet/datadog_checks/kubelet/common.py
@@ -2,20 +2,19 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 
-import logging
 import re
 
 from kubeutil import get_connection_info
 
 from datadog_checks.base.utils.tagging import tagger
 
-log = logging.getLogger(__name__)
-
 try:
     from containers import is_excluded as c_is_excluded
 except ImportError:
     # Don't fail on < 6.2
-    log.info('Agent does not provide filtering logic, disabling container filtering')
+    import logging
+
+    logging.getLogger(__name__).info('Agent does not provide filtering logic, disabling container filtering')
 
     def c_is_excluded(name, image, namespace=""):
         return False
@@ -107,12 +106,15 @@ def get_container_label(labels, l_name):
 
 
 def get_prometheus_url(default_url):
+    """
+    Use to retrieve the prometheus URL configuration from the get_connection_info()
+    :param default_url: the default prometheus URL
+    :rtype: (string, error)
+    :return: a tuple (the prometheus url, possible get_connection_info() call error )
+    """
     kubelet_conn_info = get_connection_info()
     kubelet_conn_info = {} if kubelet_conn_info is None else kubelet_conn_info
-    error = kubelet_conn_info.get("err")
-    if error:
-        log.warning(error)
-    return kubelet_conn_info.get("url", default_url)
+    return kubelet_conn_info.get("url", default_url), kubelet_conn_info.get("err")
 
 
 class PodListUtils(object):
