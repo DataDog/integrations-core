4b8c0a7f305a9032ad238a149d384f18ff51d418
Sarah Witt <sarah.witt@datadoghq.com>
2023-11-14 12:20:52 -0500

    Test project_id param

--- openstack_controller/tests/test_unit_neutron.py
+++ openstack_controller/tests/test_unit_neutron.py
@@ -509,9 +509,19 @@ def test_networks_exception(aggregator, check, dd_run_check, mock_http_get, conn
     if api_type == ApiType.REST:
         args_list = []
         for call in mock_http_get.call_args_list:
-            args, _ = call
-            args_list += list(args)
-        assert args_list.count('http://127.0.0.1:9696/networking/v2.0/networks') == 2
+            args, kwargs = call
+            project_id = kwargs.get('params', {}).get('project_id', None)
+            args_list += [(list(args), project_id)]
+
+        assert (
+            args_list.count((['http://127.0.0.1:9696/networking/v2.0/networks'], '6e39099cccde4f809b003d9e0dd09304'))
+            == 1
+        )
+        assert (
+            args_list.count((['http://127.0.0.1:9696/networking/v2.0/networks'], '1e6e233e637d4d55a50a62b63398ad15'))
+            == 1
+        )
+
     if api_type == ApiType.SDK:
         assert connection_network.networks.call_count == 2
         assert (
