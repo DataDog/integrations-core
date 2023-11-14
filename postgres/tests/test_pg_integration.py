abe7df4426803e9a3da92d230deefb9baafd937b
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2023-11-14 17:51:57 +0100

    PostgreSQL: Exclude manually launched vacuum from pg_stat_activity
    Manually launched vacuum behaves the more or less the same way as
    autovacuum and won't keep the transaction and snapshot open during the
    vacuum. We want to be able to monitor xact age and xmin without vacuums.

--- postgres/tests/test_pg_integration.py
+++ postgres/tests/test_pg_integration.py
@@ -17,7 +17,9 @@ from .common import (
     DB_NAME,
     DBM_MIGRATED_METRICS,
     HOST,
+    PASSWORD_ADMIN,
     POSTGRES_VERSION,
+    USER_ADMIN,
     _get_expected_tags,
     assert_metric_at_least,
     check_activity_metrics,
@@ -343,6 +345,19 @@ def test_activity_metrics_no_aggregations(aggregator, integration_check, pg_inst
 
     expected_tags = _get_expected_tags(check, pg_instance, db=DB_NAME)
     check_activity_metrics(aggregator, expected_tags)
+
+
+def test_activity_vacuum_excluded(aggregator, integration_check, pg_instance):
+    pg_instance['collect_activity_metrics'] = True
+    check = integration_check(pg_instance)
+
+    conn = _get_conn(pg_instance, user=USER_ADMIN, password=PASSWORD_ADMIN)
+    cur = conn.cursor()
+    cur.execute('VACUUM analyze persons')
+
+    check.check(pg_instance)
+    dd_agent_tags = _get_expected_tags(check, pg_instance, db=DB_NAME, app='test', user=USER_ADMIN)
+    aggregator.assert_metric('postgresql.waiting_queries', value=0, count=1, tags=dd_agent_tags)
 
 
 def test_backend_transaction_age(aggregator, integration_check, pg_instance):
