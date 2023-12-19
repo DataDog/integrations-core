e9e5bb447eed9ad60942819a7f1be0f7f4321e92
Zhengda Lu <zhengda.lu@datadoghq.com>
2023-12-19 10:14:05 -0500

    [DBMON-3197] ensure accurate collection of query activity with statement when stored procedure is failed to obfuscate (#16455)
    * ensure complete collection of query activity with query statement when stored procedure is failed to obfuscate
    
    * add changelog

--- sqlserver/datadog_checks/sqlserver/activity.py
+++ sqlserver/datadog_checks/sqlserver/activity.py
@@ -276,27 +276,32 @@ class SqlserverActivity(DBMAsyncJob):
             return self._sanitize_row(row)
         try:
             statement = obfuscate_sql_with_metadata(row['statement_text'], self._config.obfuscator_options)
-            procedure_statement = None
             # sqlserver doesn't have a boolean data type so convert integer to boolean
             comments, row['is_proc'], procedure_name = extract_sql_comments_and_procedure_name(row['text'])
             if row['is_proc'] and 'text' in row:
-                procedure_statement = obfuscate_sql_with_metadata(row['text'], self._config.obfuscator_options)
+                try:
+                    procedure_statement = obfuscate_sql_with_metadata(row['text'], self._config.obfuscator_options)
+                    # procedure_signature is used to link this activity event with
+                    # its related plan events
+                    row['procedure_signature'] = compute_sql_signature(procedure_statement['query'])
+                except Exception as e:
+                    # if we fail to obfuscate the procedure text,
+                    # we should not mark query statement as failed to obfuscate
+                    if self._config.log_unobfuscated_queries:
+                        self.log.warning("Failed to obfuscate stored procedure=[%s] | err=[%s]", row['text'], e)
+                    else:
+                        self.log.debug("Failed to obfuscate stored procedure | err=[%s]", e)
             obfuscated_statement = statement['query']
             metadata = statement['metadata']
             row['dd_commands'] = metadata.get('commands', None)
             row['dd_tables'] = metadata.get('tables', None)
             row['dd_comments'] = comments
             row['query_signature'] = compute_sql_signature(obfuscated_statement)
-            # procedure_signature is used to link this activity event with
-            # its related plan events
-            if procedure_statement:
-                row['procedure_signature'] = compute_sql_signature(procedure_statement['query'])
             if procedure_name:
                 row['procedure_name'] = procedure_name
         except Exception as e:
             if self._config.log_unobfuscated_queries:
-                raw_query_text = row['text'] if row.get('is_proc', False) else row['statement_text']
-                self.log.warning("Failed to obfuscate query=[%s] | err=[%s]", raw_query_text, e)
+                self.log.warning("Failed to obfuscate query=[%s] | err=[%s]", row['statement_text'], e)
             else:
                 self.log.debug("Failed to obfuscate query | err=[%s]", e)
             obfuscated_statement = "ERROR: failed to obfuscate"
