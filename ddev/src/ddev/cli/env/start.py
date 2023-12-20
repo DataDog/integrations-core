a8d4ec33bcd6f2fefd3be32ead1317882ea6eeb1
Florent Clarret <florent.clarret@datadoghq.com>
2023-12-20 16:10:59 +0100

    Override the default configuration when environment are provided in the `env start` command

--- ddev/src/ddev/cli/env/start.py
+++ ddev/src/ddev/cli/env/start.py
@@ -174,7 +174,6 @@ def _get_agent_env_vars(org_config, metadata, extra_env_vars, dogstatsd):
 
     # Use the environment variables defined by tests as defaults so tooling can override them
     env_vars: dict[str, str] = metadata.get('env_vars', {}).copy()
-    env_vars.update(ev.split('=', maxsplit=1) for ev in extra_env_vars)
 
     if api_key := org_config.get('api_key'):
         env_vars['DD_API_KEY'] = api_key
@@ -199,5 +198,7 @@ def _get_agent_env_vars(org_config, metadata, extra_env_vars, dogstatsd):
     # Enable logs Agent by default if the environment is mounting logs
     if any(ev.startswith(E2EEnvVars.LOGS_DIR_PREFIX) for ev in metadata.get(E2EMetadata.ENV_VARS, {})):
         env_vars.setdefault('DD_LOGS_ENABLED', 'true')
+
+    env_vars.update(ev.split('=', maxsplit=1) for ev in extra_env_vars)
 
     return env_vars
