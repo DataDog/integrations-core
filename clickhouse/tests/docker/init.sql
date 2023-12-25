d5f6d68591bb095b5c38676652c94dd4c6236cf6
Pradeep Chhetri <pradeepchhetri4444@gmail.com>
2023-12-25 23:52:25 +0800

    Feedback

--- clickhouse/tests/docker/init.sql
+++ clickhouse/tests/docker/init.sql
@@ -1,3 +1,5 @@
 CREATE TABLE IF NOT EXISTS tableau (id Int64) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{layer}-{shard}/tableau', '{replica}') PARTITION BY id ORDER BY id;
 CREATE TABLE IF NOT EXISTS tableau_distributed as tableau ENGINE = Distributed(cluster_1, default, tableau, rand());
 INSERT INTO tableau VALUES (123),(456),(789);
+ALTER USER default SETTINGS async_insert=1;
+INSERT INTO tableau SETTINGS async_insert=1, wait_for_async_insert=2 VALUES (111),(222),(333);
