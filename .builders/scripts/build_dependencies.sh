deab84c7215a981448a6db4d705aa0c39b564adf
Alex Lopez <alex.lopez@datadoghq.com>
2023-12-28 13:13:29 +0100

    Move installation of virtualenv package to dockerfile

--- .builders/scripts/build_dependencies.sh
+++ .builders/scripts/build_dependencies.sh
@@ -10,12 +10,8 @@ patches_folder="/home/patches"
 touch "${constraints_file}"
 echo "bcrypt < 4.1.0" >> "${constraints_file}"
 
-# Note: virtualenv v20.22.0 dropped support for creating py2-based virtual environments,
-# that's why we stick to an older version.
-python3 -m pip install virtualenv==20.21.1
+# Create and activate the build virtualenv and install build dependencies on it
 python3 -m virtualenv --python python3 build_env_py3
-
-# Activate the build virtualenv and install build dependencies on it
 source build_env_py3/bin/activate
 python -m pip install -r ${build_dependencies_file}
 
