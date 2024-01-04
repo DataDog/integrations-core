525dce4b987e03034eb17e1b12154383136ba617
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-01-05 00:33:06 +0100

    add missing license header

--- fluxcd/tests/common.py
+++ fluxcd/tests/common.py
@@ -1,3 +1,7 @@
+# (C) Datadog, Inc. 2024-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
 EXPECTED_METRICS = {
     "v1": {
         "fluxcd.controller.runtime.active.workers",
