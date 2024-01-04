d4de0c7a5636832d750edbe51ba7e2bc13977fd8
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-01-04 12:26:07 +0100

    Add more metrics to fluxcd (#16476)
    * Add more metrics
    
    * add units
    
    * fix metric names
    
    * Unit tests for v2 metrics
    
    * first part of feedback
    
    * remove unit from histogram buckets and counts
    
    * fix test function name
    
    * Copy improvements
    
    Co-authored-by: Austin Lai <76412946+alai97@users.noreply.github.com>
    
    * Remove "bytes" from metric names
    
    ---------
    
    Co-authored-by: Austin Lai <76412946+alai97@users.noreply.github.com>

--- fluxcd/tests/conftest.py
+++ fluxcd/tests/conftest.py
@@ -27,8 +27,26 @@ def check(instance):
 
 
 @pytest.fixture()
-def mock_metrics():
-    fixture_file = os.path.join(os.path.dirname(__file__), "fixtures", "metrics.txt")
+def mock_metrics_v1():
+    fixture_file = os.path.join(os.path.dirname(__file__), "fixtures", "metrics-v1.txt")
+
+    with open(fixture_file, "r") as f:
+        content = f.read()
+
+    with mock.patch(
+        "requests.get",
+        return_value=mock.MagicMock(
+            status_code=200,
+            iter_lines=lambda **kwargs: content.split("\n"),
+            headers={"Content-Type": "text/plain"},
+        ),
+    ):
+        yield
+
+
+@pytest.fixture()
+def mock_metrics_v2():
+    fixture_file = os.path.join(os.path.dirname(__file__), "fixtures", "metrics-v2.txt")
 
     with open(fixture_file, "r") as f:
         content = f.read()
--- fluxcd/tests/test_unit.py
+++ fluxcd/tests/test_unit.py
@@ -1,28 +1,60 @@
 # (C) Datadog, Inc. 2023-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
 
 from datadog_checks.dev.utils import get_metadata_metrics
 
 EXPECTED_METRICS = {
-    "fluxcd.gotk.reconcile.condition",
-    "fluxcd.gotk.suspend.status",
-    "fluxcd.gotk.reconcile.duration.seconds.count",
-    "fluxcd.gotk.reconcile.duration.seconds.sum",
-    "fluxcd.gotk.reconcile.duration.seconds.bucket",
-    "fluxcd.controller.runtime.active.workers",
-    "fluxcd.controller.runtime.reconcile.count",
-    "fluxcd.controller.runtime.reconcile.time.seconds.count",
-    "fluxcd.controller.runtime.reconcile.time.seconds.sum",
-    "fluxcd.controller.runtime.reconcile.time.seconds.bucket",
-    "fluxcd.controller.runtime.max.concurrent.reconciles",
-    "fluxcd.controller.runtime.reconcile.errors.count",
+    "v1": {
+        "fluxcd.controller.runtime.active.workers",
+        "fluxcd.controller.runtime.max.concurrent.reconciles",
+        "fluxcd.controller.runtime.reconcile.count",
+        "fluxcd.controller.runtime.reconcile.errors.count",
+        "fluxcd.controller.runtime.reconcile.time.seconds.bucket",
+        "fluxcd.controller.runtime.reconcile.time.seconds.count",
+        "fluxcd.controller.runtime.reconcile.time.seconds.sum",
+        "fluxcd.gotk.reconcile.condition",
+        "fluxcd.gotk.reconcile.duration.seconds.bucket",
+        "fluxcd.gotk.reconcile.duration.seconds.count",
+        "fluxcd.gotk.reconcile.duration.seconds.sum",
+        "fluxcd.gotk.suspend.status",
+    },
+    "v2": {
+        "fluxcd.controller.runtime.active.workers",
+        "fluxcd.controller.runtime.max.concurrent.reconciles",
+        "fluxcd.controller.runtime.reconcile.count",
+        "fluxcd.controller.runtime.reconcile.errors.count",
+        "fluxcd.controller.runtime.reconcile.time.seconds.bucket",
+        "fluxcd.controller.runtime.reconcile.time.seconds.count",
+        "fluxcd.controller.runtime.reconcile.time.seconds.sum",
+        "fluxcd.gotk.reconcile.condition",
+        "fluxcd.gotk.reconcile.duration.seconds.bucket",
+        "fluxcd.gotk.reconcile.duration.seconds.count",
+        "fluxcd.gotk.reconcile.duration.seconds.sum",
+        "fluxcd.gotk.suspend.status",
+        "fluxcd.leader_election_master_status",
+        "fluxcd.process.cpu_seconds.count",
+        "fluxcd.process.max_fds",
+        "fluxcd.process.open_fds",
+        "fluxcd.process.resident_memory",
+        "fluxcd.process.start_time",
+        "fluxcd.process.virtual_memory",
+        "fluxcd.process.virtual_memory.max",
+        "fluxcd.workqueue.adds.count",
+        "fluxcd.workqueue.depth",
+        "fluxcd.workqueue.longest_running_processor",
+        "fluxcd.workqueue.retries.count",
+        "fluxcd.workqueue.unfinished_work",
+    },
 }
 
 
-def test_mock_assert_metrics(dd_run_check, aggregator, check, mock_metrics):
+@pytest.mark.parametrize("fluxcd_version", ["v1", "v2"])
+def test_assert_metrics(dd_run_check, aggregator, check, request, fluxcd_version):
+    _mock_response = request.getfixturevalue(f"mock_metrics_{fluxcd_version}")
     dd_run_check(check)
-    for metric_name in EXPECTED_METRICS:
+    for metric_name in EXPECTED_METRICS[fluxcd_version]:
         aggregator.assert_metric(metric_name)
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
