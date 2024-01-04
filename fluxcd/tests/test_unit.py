a2539ae869937377185a753e7b6cffde4cde5a0c
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-01-04 13:02:31 +0100

    Correct config models for OpenMetrics check (#16519)

--- fluxcd/tests/test_unit.py
+++ fluxcd/tests/test_unit.py
@@ -3,7 +3,9 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
 
+from datadog_checks.base.errors import ConfigurationError
 from datadog_checks.dev.utils import get_metadata_metrics
+from datadog_checks.fluxcd import FluxcdCheck
 
 EXPECTED_METRICS = {
     "v1": {
@@ -58,3 +60,8 @@ def test_assert_metrics(dd_run_check, aggregator, check, request, fluxcd_version
         aggregator.assert_metric(metric_name)
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+
+
+def test_config():
+    with pytest.raises(ConfigurationError, match="Input should be a valid string"):
+        FluxcdCheck('fluxcd', {}, [{'openmetrics_endpoint': 2}]).load_configuration_models()
