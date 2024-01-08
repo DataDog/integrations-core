c4d38ec0ae72360a160c1150ae031dab3148496a
Florent Clarret <florent.clarret@datadoghq.com>
2024-01-08 09:09:56 +0100

    Bump `foundationdb` version to 7.3.27

--- .ddev/ci/scripts/foundationdb/linux/55_install_client.sh
+++ .ddev/ci/scripts/foundationdb/linux/55_install_client.sh
@@ -6,14 +6,14 @@
 set -ex
 
 TMP_DIR=/tmp/fdb
-FDB_URL=https://github.com/apple/foundationdb/releases/download/6.3.15/foundationdb-clients_6.3.15-1_amd64.deb
+FDB_URL=https://github.com/apple/foundationdb/releases/download/7.3.27/foundationdb-clients_7.3.27-1_amd64.deb
 
 mkdir -p $TMP_DIR
 pushd $TMP_DIR
 
 curl --verbose -LO $FDB_URL
 
-sudo dpkg -i foundationdb-clients_6.3.15-1_amd64.deb
+sudo dpkg -i foundationdb-clients_7.3.27-1_amd64.deb
 sudo apt-get install -f
 
 popd
