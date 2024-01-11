465bd03da94e3076b2b28fe2b60300a576ae0dea
Florent Clarret <florent.clarret@datadoghq.com>
2024-01-11 22:10:49 +0100

    Collect logs in the e2e environment

--- tomcat/tests/conftest.py
+++ tomcat/tests/conftest.py
@@ -2,23 +2,58 @@
 # All rights reserved
 # Licensed under Simplified BSD License (see LICENSE)
 import os
+from contextlib import contextmanager
 
 import pytest
 
+from datadog_checks.dev import EnvVars, TempDir
 from datadog_checks.dev.docker import docker_run
+from datadog_checks.dev.env import get_state, save_state
 from datadog_checks.dev.utils import load_jmx_config
 
-from .common import HELLO_URL, HERE
+from .common import E2E_METADATA, HELLO_URL, HERE
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
     compose_file = os.path.join(HERE, 'compose', 'docker-compose.yml')
 
-    with docker_run(compose_file, build=True, endpoints=HELLO_URL, log_patterns=['Server startup']):
-        yield load_jmx_config(), {'use_jmx': True}
+    with docker_run(
+        compose_file,
+        build=True,
+        endpoints=HELLO_URL,
+        log_patterns=['Server startup'],
+        wrappers=[create_log_volumes()],
+    ):
+        yield load_jmx_config(), E2E_METADATA
 
 
 @pytest.fixture
 def instance():
     return {}
+
+
+@contextmanager
+def create_log_volumes():
+    env_vars = {}
+    docker_volumes = get_state("docker_volumes", [])
+
+    with TempDir("tomcat") as d:
+        os.chmod(d, 0o777)
+        docker_volumes.append(f"{d}:/var/log/tomcat")
+        env_vars["TOMCAT_LOG_FOLDER"] = d
+
+        config = [
+            {
+                "type": "file",
+                "path": "/var/log/tomcat/*",
+                "source": "tomcat",
+                "service": "tomcat",
+            },
+        ]
+
+        save_state("logs_config", config)
+        save_state("docker_volumes", docker_volumes)
+
+        with EnvVars(env_vars):
+            yield
