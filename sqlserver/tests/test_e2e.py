87da62852bc5fec8468e166100c34d6e0e972233
Joel Marcotte <joel.marcotte@users.noreply.github.com>
2024-01-18 15:55:44 +0000

    fix import

--- sqlserver/tests/test_e2e.py
+++ sqlserver/tests/test_e2e.py
@@ -2,7 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
-from sqlserver.datadog_checks.sqlserver.const import DATABASE_INDEX_METRICS
+from datadog_checks.sqlserver.const import DATABASE_INDEX_METRICS
 
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.sqlserver import SQLServer
