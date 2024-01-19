b177046cc0fb327a2ffe595cf98593232d29e264
Florent Clarret <florent.clarret@datadoghq.com>
2024-01-19 14:35:25 +0100

    Implement the check and add tests

---
+++ tekton/datadog_checks/tekton/metrics.py
@@ -0,0 +1,7 @@
+# (C) Datadog, Inc. 2024-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+METRIC_MAP = {
+    'tekton_pipelines_controller_go_alloc': 'pipelines_controller.go_alloc',
+}
