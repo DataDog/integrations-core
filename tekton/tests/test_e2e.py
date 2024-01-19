7e30cf616a1f9165c0881bcd6c5c5281a5c11642
Florent Clarret <florent.clarret@datadoghq.com>
2024-01-19 14:35:25 +0100

    Implement the check and add tests

--- tekton/tests/test_e2e.py
+++ tekton/tests/test_e2e.py
@@ -1,7 +1,18 @@
 # (C) Datadog, Inc. 2024-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.base import AgentCheck
+from datadog_checks.dev.utils import get_metadata_metrics
+
+from .common import METRICS
 
 
 def test_check(dd_agent_check):
-    dd_agent_check(rate=True)
+    aggregator = dd_agent_check(rate=True)
+
+    for expected_metric in METRICS:
+        aggregator.assert_metric(f"tekton.{expected_metric}")
+
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    aggregator.assert_service_check("tekton.openmetrics.health", status=AgentCheck.OK)
