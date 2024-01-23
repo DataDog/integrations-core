1925c59c3fede2bc186c44df593af32834ac9f10
Florent Clarret <florent.clarret@datadoghq.com>
2024-01-23 10:08:19 +0100

    Prevent `ddev dep` to bump `kubernetes`

--- ddev/src/ddev/cli/dep.py
+++ ddev/src/ddev/cli/dep.py
@@ -46,6 +46,9 @@ IGNORED_DEPS = {
     # cryptography>=42 requires rust>=1.63.0. We have rust 1.62 on centos 6
     # https://github.com/DataDog/datadog-agent/pull/22268
     'cryptography',
+    # Brings urllib 2.0.7 which breaks `test_uds_request` in the base check
+    # https://github.com/kubernetes-client/python/pull/2131
+    'kubernetes',
 }
 
 # Dependencies for the downloader that are security-related and should be updated separately from the others
