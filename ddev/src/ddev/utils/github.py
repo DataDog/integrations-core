d2933fe4356e80a6c4d5a8ca9f8a90baabd732c2
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-02-06 15:40:08 +0100

    now test should pass

--- ddev/src/ddev/utils/github.py
+++ ddev/src/ddev/utils/github.py
@@ -20,8 +20,9 @@ class PullRequest:
         self.__title = data['title']
         self.__html_url = data['pull_request']['html_url']
         self.__diff_url = data['pull_request']['diff_url']
-        # Normalize to remove carriage returns on Windows
-        self.__body = '\n'.join(data['body'].splitlines())
+        # Github API returns `None` for empty bodies, but we use empty string as default.
+        # Normalize to remove carriage returns on Windows.
+        self.__body = data.get('body', '').replace('\r', '')
         self.__author = data['user']['login']
         self.__labels = sorted(label['name'] for label in data['labels'])
 
