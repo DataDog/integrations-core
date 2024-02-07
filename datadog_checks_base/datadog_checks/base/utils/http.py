3a194c7964a8b8f10ce89aaf3bc981823c961c3c
Guangning E <guangning@streamnative.io>
2024-02-07 12:18:34 +0800

    Add space line

--- datadog_checks_base/datadog_checks/base/utils/http.py
+++ datadog_checks_base/datadog_checks/base/utils/http.py
@@ -826,6 +826,7 @@ class AuthTokenOAuthReader(object):
         else:
             self._fetch_options['client_id'] = self._client_id
             self._fetch_options['client_secret'] = self._client_secret
+
         self.auth_params = config.get('auth_params', {})
         if isinstance(self.auth_params, dict):
             for key, value in self.auth_params.items():
