d9d3952225e3ece4f9e0f1f08eb36515d9c50599
Florent Clarret <florent.clarret@datadoghq.com>
2024-02-09 13:41:14 +0100

    add tasks

--- tekton/tests/conftest.py
+++ tekton/tests/conftest.py
@@ -8,7 +8,6 @@ from contextlib import ExitStack
 import pytest
 
 from datadog_checks.dev import get_here, run_command
-from datadog_checks.dev.conditions import CheckEndpoints
 from datadog_checks.dev.kind import kind_run
 from datadog_checks.dev.kube_port_forward import port_forward
 
@@ -18,6 +17,7 @@ HERE = get_here()
 def setup_tekton():
     run_command(["kubectl", "create", "namespace", "tekton-operator"])
     run_command(["kubectl", "apply", "-f", os.path.join(HERE, "kind", "tekton-operator.yaml"), "-n", "tekton-operator"])
+    time.sleep(30)
     run_command(
         ["kubectl", "wait", "pods", "--all", "--for=condition=Ready", "--timeout=300s", "-n", "tekton-operator"]
     )
@@ -28,7 +28,20 @@ def setup_tekton():
             "wait",
             "pods",
             "-l",
-            "run=tekton-dashboard",
+            "app=tekton-pipelines-controller",
+            "--for=condition=Ready",
+            "--timeout=300s",
+            "-n",
+            "tekton-pipelines",
+        ]
+    )
+    run_command(
+        [
+            "kubectl",
+            "wait",
+            "pods",
+            "-l",
+            "app=tekton-triggers-controller",
             "--for=condition=Ready",
             "--timeout=300s",
             "-n",
@@ -36,27 +49,33 @@ def setup_tekton():
         ]
     )
 
+    for task in ("hello", "sleep"):
+        run_command(
+            ["kubectl", "apply", "-f", os.path.join(HERE, "kind", f"tekton-task-{task}.yaml"), "-n", "tekton-pipelines"]
+        )
+        run_command(
+            [
+                "kubectl",
+                "apply",
+                "-f",
+                os.path.join(HERE, "kind", f"tekton-taskrun-{task}.yaml"),
+                "-n",
+                "tekton-pipelines",
+            ]
+        )
+
 
 @pytest.fixture(scope='session')
 def dd_environment(dd_save_state):
-    # https://github.com/tektoncd/dashboard/blob/main/docs/walkthrough/walkthrough-kind.md
-    with kind_run(conditions=[setup_tekton], sleep=30) as kubeconfig, ExitStack() as stack:
-        instances = []
-
+    with (kind_run(conditions=[setup_tekton], sleep=30) as kubeconfig, ExitStack() as stack):
         pipeline_host, pipeline_port = stack.enter_context(
             port_forward(kubeconfig, 'tekton-pipelines', 9090, 'service', 'tekton-pipelines-controller')
         )
-        instances.append({'openmetrics_endpoint': f'http://{pipeline_host}:{pipeline_port}/metrics'})
-
+        instances = [{'openmetrics_endpoint': f'http://{pipeline_host}:{pipeline_port}/metrics'}]
         trigger_host, trigger_port = stack.enter_context(
             port_forward(kubeconfig, 'tekton-pipelines', 9000, 'service', 'tekton-triggers-controller')
         )
         instances.append({'openmetrics_endpoint': f'http://{trigger_host}:{trigger_port}/metrics'})
-
-        # We can't add this to `kind_run` because we don't know the URL at this moment
-        for instance in instances:
-            condition = CheckEndpoints(instance['openmetrics_endpoint'])
-            condition()
 
         yield {'instances': instances}
 
