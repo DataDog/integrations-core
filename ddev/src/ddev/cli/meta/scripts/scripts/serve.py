1cc879e72cf2118fd66791583b5bfbd7d2c40543
Florent Clarret <florent.clarret@datadoghq.com>
2024-02-12 13:47:58 +0100

    address

--- ddev/src/ddev/cli/meta/scripts/scripts/serve.py
+++ ddev/src/ddev/cli/meta/scripts/scripts/serve.py
@@ -1,6 +1,8 @@
 # (C) Datadog, Inc. 2024-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import functools
+import socketserver
 import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
@@ -13,28 +15,32 @@ else:
 
 PORT = 8080
 
+current_payload = 0
+
 
 class OpenMetricsHandler(BaseHTTPRequestHandler):
+    def __init__(
+        self, payloads, request: bytes, client_address: tuple[str, int], server: socketserver.BaseServer
+    ) -> None:
+        self.payloads = payloads
+        super().__init__(request, client_address, server)
+
     def do_GET(self):
         global current_payload
-        global payloads
-
-        if len(payloads) > 1:
-            print(f"Serving file {payloads[current_payload]}")
+        if len(self.payloads) > 1:
+            print(f"Serving file {self.payloads[current_payload]}")
 
         self.send_response(200)
         self.send_header('Content-Type', CONTENT_TYPE)
         self.end_headers()
-        with open(f"/tmp/{payloads[current_payload]}", 'rb') as f:
+        with open(f"/tmp/{self.payloads[current_payload]}", 'rb') as f:
             self.wfile.write(f.read())
 
         # Otherwise we keep using the last one
-        if current_payload < len(payloads) - 1:
+        if current_payload < len(self.payloads) - 1:
             current_payload += 1
 
 
 if __name__ == '__main__':
-    current_payload = 0
-    payloads = sys.argv[1:]
-    with HTTPServer(("", PORT), OpenMetricsHandler) as httpd:
+    with HTTPServer(("", PORT), functools.partial(OpenMetricsHandler, sys.argv[1:])) as httpd:
         httpd.serve_forever()
