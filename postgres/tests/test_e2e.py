c9e7e080434b050906b1fdd24be8d0c3b3ab1e62
Anthonin Bonnefoy <anthonin.bonnefoy@datadoghq.com>
2024-02-15 14:37:40 +0100

    Update tests

--- postgres/tests/test_e2e.py
+++ postgres/tests/test_e2e.py
@@ -4,15 +4,13 @@
 
 import pytest
 
-from .common import PORT, check_bgw_metrics, check_common_metrics
+from .common import _get_expected_tags, check_bgw_metrics, check_common_metrics
 
 
 @pytest.mark.e2e
 def test_e2e(dd_agent_check, pg_instance):
     aggregator = dd_agent_check(pg_instance, rate=True)
 
-    expected_tags = pg_instance['tags'] + [
-        'port:{}'.format(PORT),
-    ]
+    expected_tags = _get_expected_tags(None, pg_instance)
     check_bgw_metrics(aggregator, expected_tags)
     check_common_metrics(aggregator, expected_tags=expected_tags, count=None)
