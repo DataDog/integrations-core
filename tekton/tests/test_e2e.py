b497b165ca324e51dd1efd1bff365004b8bfa642
Florent Clarret <florent.clarret@datadoghq.com>
2024-02-19 16:54:54 +0100

    drop optional metrics

--- tekton/tests/test_e2e.py
+++ tekton/tests/test_e2e.py
@@ -4,17 +4,14 @@
 from datadog_checks.base import AgentCheck
 from datadog_checks.dev.utils import get_metadata_metrics
 
-from .common import E2E_PIPELINES_OPTIONAL_METRICS, PIPELINES_METRICS, TRIGGERS_METRICS
+from .common import PIPELINES_METRICS, TRIGGERS_METRICS
 
 
 def test_check(dd_agent_check):
     aggregator = dd_agent_check(rate=True)
 
     for expected_metric in PIPELINES_METRICS:
-        metric_name = f"tekton.pipelines_controller.{expected_metric}"
-        at_least = 0 if metric_name in E2E_PIPELINES_OPTIONAL_METRICS else 1
-
-        aggregator.assert_metric(metric_name, at_least=at_least)
+        aggregator.assert_metric(f"tekton.pipelines_controller.{expected_metric}")
 
     for expected_metric in TRIGGERS_METRICS:
         aggregator.assert_metric(f"tekton.triggers_controller.{expected_metric}")
