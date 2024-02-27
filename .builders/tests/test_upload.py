7ea7a0621de62b8c0a7c9d7e99b4bf1d9581486f
Alex Lopez <alex.lopez@datadoghq.com>
2024-02-27 16:34:47 +0100

    Test upload of built wheels

--- .builders/tests/test_upload.py
+++ .builders/tests/test_upload.py
@@ -11,39 +11,35 @@ import upload
 def setup_fake_bucket(monkeypatch):
     """Patch google storage functions to simulate a bucket."""
 
-    files = {
-        'external/updated-version/updated_version-3.13.0-cp311-cp311-manylinux1_x86_64.whl':
-        {'requires-python': '', 'sha256': ''},
-        'external/existing-version/existing_version-5.14.2-py3-none-any.whl':
-        {'requires-python': '', 'sha256': ''},
-    }
+    def _setup_bucket(files):
+        uploads = []
 
-    uploads = []
+        def make_blob(name):
+            def fake_upload(wheel_name):
+                files[name] = {'requires-python': '', 'sha256': ''}
+                uploads.append(wheel_name)
 
-    def make_blob(name):
-        def fake_upload(wheel_name):
-            files[name] = {'requires-python': '', 'sha256': ''}
-            uploads.append(wheel_name)
+            blob = mock.Mock()
+            blob.exists.side_effect = lambda: name in files
+            blob.upload_from_filename.side_effect = fake_upload
+            blob.name = name
+            blob.metadata = files.get(name, None)
+            return blob
 
-        blob = mock.Mock()
-        blob.exists.side_effect = lambda: name in files
-        blob.upload_from_filename.side_effect = fake_upload
-        blob.name = name
-        blob.metadata = files.get(name, None)
-        return blob
+        def list_blobs(prefix=''):
+            return [make_blob(f) for f in files if f.startswith(prefix)]
 
-    def list_blobs(prefix=''):
-        return [make_blob(f) for f in files if f.startswith(prefix)]
+        bucket = mock.Mock()
+        bucket.list_blobs.side_effect = list_blobs
+        bucket.blob.side_effect = make_blob
 
-    bucket = mock.Mock()
-    bucket.list_blobs.side_effect = list_blobs
-    bucket.blob.side_effect = make_blob
+        client = mock.Mock()
+        client.bucket.return_value = bucket
 
-    client = mock.Mock()
-    client.bucket.return_value = bucket
+        monkeypatch.setattr(upload.storage, 'Client', mock.Mock(return_value=client))
+        return bucket, uploads
 
-    monkeypatch.setattr(upload.storage, 'Client', mock.Mock(return_value=client))
-    return bucket, uploads
+    return _setup_bucket
 
 
 def write_dummy_wheel(path, project_name, version, requires_python):
@@ -60,16 +56,16 @@ def write_dummy_wheel(path, project_name, version, requires_python):
 
 @pytest.fixture
 def setup_targets_dir(tmp_path_factory):
-    def _setup(wheels):
+    def _setup(wheels, platform='linux-x86_64', python_major_version='3'):
         targets_dir = tmp_path_factory.mktemp('output')
 
-        platform = 'linux-x86_64'
-        external_dir = targets_dir / platform / 'py3' / 'wheels' / 'external'
-        external_dir.mkdir(parents=True)
+        for artifact_type, wheel_entries in wheels.items():
+            external_dir = targets_dir / platform / f'py{python_major_version}' / 'wheels' / artifact_type
+            external_dir.mkdir(parents=True)
 
-        for path, *pkginfo in wheels:
-            full_path = external_dir / path
-            write_dummy_wheel(full_path, *pkginfo)
+            for path, *pkginfo in wheel_entries:
+                full_path = external_dir / path
+                write_dummy_wheel(full_path, *pkginfo)
 
         return targets_dir
 
@@ -77,14 +73,23 @@ def setup_targets_dir(tmp_path_factory):
 
 
 def test_upload_external(setup_targets_dir, setup_fake_bucket):
-    wheels = [
-        ('all_new-2.31.0-py3-none-any.whl', 'all-new', '2.31.0', '>=3.6'),
-        ('updated_version-3.14.1-cp311-cp311-manylinux1_x86_64.whl', 'updated_version', '3.14.1', '>=3.7'),
-        ('existing_version-5.14.2-py3-none-any.whl', 'existing-version', '5.14.2', '>=3.8'),
-    ]
+    wheels = {
+        'external': [
+            ('all_new-2.31.0-py3-none-any.whl', 'all-new', '2.31.0', '>=3.6'),
+            ('updated_version-3.14.1-cp311-cp311-manylinux1_x86_64.whl', 'updated_version', '3.14.1', '>=3.7'),
+            ('existing_version-5.14.2-py3-none-any.whl', 'existing-version', '5.14.2', '>=3.8'),
+        ]
+    }
     targets_dir = setup_targets_dir(wheels)
 
-    bucket, uploads = setup_fake_bucket
+    bucket_files = {
+        'external/updated-version/updated_version-3.13.0-cp311-cp311-manylinux1_x86_64.whl':
+        {'requires-python': '', 'sha256': ''},
+        'external/existing-version/existing_version-5.14.2-py3-none-any.whl':
+        {'requires-python': '', 'sha256': ''},
+    }
+    bucket, uploads = setup_fake_bucket(bucket_files)
+
     upload.upload(targets_dir)
 
     bucket_files = [f.name for f in bucket.list_blobs()]
@@ -93,3 +98,25 @@ def test_upload_external(setup_targets_dir, setup_fake_bucket):
 
     uploads = {str(Path(f).relative_to(targets_dir / 'linux-x86_64' / 'py3' / 'wheels' / 'external')) for f in uploads}
     assert {'all_new-2.31.0-py3-none-any.whl', 'updated_version-3.14.1-cp311-cp311-manylinux1_x86_64.whl'} <= uploads
+
+
+def test_upload_built(setup_targets_dir, setup_fake_bucket):
+    wheels = {
+        'built': [
+            ('without_collision-3.14.1-cp311-cp311-manylinux2010_x86_64.whl', 'without-collision', '3.14.1', '>=3.7'),
+            ('collision-1.1.1-cp311-cp311-manylinux2010_x86_64.whl', 'collision', '1.1.1', '>=3.7'),
+        ]
+    }
+    targets_dir = setup_targets_dir(wheels)
+
+    bucket_files = {
+        'built/collision/collision-1.1.1-0-cp311-cp311-manylinux2010_x86_64.whl':
+        {'requires-python': '', 'sha256': ''},
+    }
+    bucket, uploads = setup_fake_bucket(bucket_files)
+
+    upload.upload(targets_dir)
+
+    bucket_files = [f.name for f in bucket.list_blobs()]
+    assert 'built/without-collision/without_collision-3.14.1-0-cp311-cp311-manylinux2010_x86_64.whl' in bucket_files
+    assert 'built/collision/collision-1.1.1-1-cp311-cp311-manylinux2010_x86_64.whl' in bucket_files
