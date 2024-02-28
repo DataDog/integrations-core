ec6ddf03260c0aef140b27e83439c15591854f04
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2024-02-28 11:44:11 +0100

    add unit tests for proxy metrics

--- teleport/tests/test_unit.py
+++ teleport/tests/test_unit.py
@@ -41,3 +41,51 @@ def test_common_teleport_metrics(dd_run_check, aggregator, mock_http_response):
 
     for metric in COMMON_METRICS:
         aggregator.assert_metric(f"teleport.{metric}")
+
+
+PROXY_METRICS = [
+    "failed_connect_to_node_attempts.count",
+    "failed_login_attempts.count",
+    "grpc_client_started.count",
+    "grpc_client_handled.count",
+    "grpc_client_msg_received.count",
+    "grpc_client_msg_sent.count",
+    "proxy_connection_limit_exceeded.count",
+    "proxy_peer_client_dial_error.count",
+    "proxy_peer_server_connections",
+    "proxy_peer_client_rpc",
+    "proxy_peer_client_rpc.count",
+    "proxy_peer_client_rpc_duration_seconds",
+    "proxy_peer_client_message_sent_size",
+    "proxy_peer_client_message_received_size",
+    "proxy_peer_server_connections",
+    "proxy_peer_server_rpc",
+    "proxy_peer_server_rpc.count",
+    "proxy_peer_server_rpc_duration_seconds",
+    "proxy_peer_server_message_sent_size",
+    "proxy_peer_server_message_received_size",
+    "proxy_ssh_sessions",
+    "proxy_missing_ssh_tunnels",
+    "remote_clusters",
+    "teleport_connect_to_node_attempts.count",
+    "teleport_reverse_tunnels_connected",
+    "trusted_clusters",
+    "teleport_proxy_db_connection_setup_time_seconds",
+    "teleport_proxy_db_connection_dial_attempts.count",
+    "teleport_proxy_db_connection_dial_failures.count",
+    "teleport_proxy_db_attempted_servers_total",
+    "teleport_proxy_db_connection_tls_config_time_seconds",
+    "teleport_proxy_db_active_connections_total",
+]
+
+
+def test_proxy_teleport_metrics(dd_run_check, aggregator, mock_http_response):
+    fixtures_path = os.path.join(get_here(), 'fixtures', 'metrics.txt')
+    mock_http_response(file_path=fixtures_path)
+
+    instance = {"diagnostic_url": "http://hostname:3000"}
+    check = TeleportCheck('teleport', {}, [instance])
+    dd_run_check(check)
+
+    for metric in PROXY_METRICS:
+        aggregator.assert_metric(f"teleport.{metric}")
