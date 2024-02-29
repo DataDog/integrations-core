f383e8527338bbce4c02ac3956f1b9dd42be3e7f
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-02-29 17:19:56 +0100

    remove exception

--- ddev/tests/conftest.py
+++ ddev/tests/conftest.py
@@ -177,7 +177,6 @@ def local_clone(isolation, local_repo) -> Generator[ClonedRepo, None, None]:
 
     This is being deprecated and removed, use empty_git_repo instead!
     '''
-    raise RuntimeError("this is a test")
     cloned_repo_path = isolation / local_repo.name
 
     PLATFORM.check_command_output(
