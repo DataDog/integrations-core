ce997469cb707ba2b353323e97a4d2b4eda4bcfb
Florent Clarret <florent.clarret@datadoghq.com>
2024-03-01 15:59:25 +0100

    Update tekton/datadog_checks/tekton/check.py
    Co-authored-by: Ilia Kurenkov <ilia.kurenkov@datadoghq.com>

--- tekton/datadog_checks/tekton/check.py
+++ tekton/datadog_checks/tekton/check.py
@@ -8,13 +8,8 @@ from datadog_checks.tekton.config_models import ConfigMixin
 
 from .metrics import ENDPOINTS_METRICS_MAP
 
-(
-    PIPELINES_CONTROLLER_NAMESPACE,
-    TRIGGERS_CONTROLLER_NAMESPACE,
-) = [
-    'tekton.pipelines_controller',
-    'tekton.trigger_controller',
-]
+PIPELINES_CONTROLLER_NAMESPACE = 'tekton.pipelines_controller'
+TRIGGERS_CONTROLLER_NAMESPACE =  'tekton.trigger_controller'
 
 
 class TektonCheck(OpenMetricsBaseCheckV2, ConfigMixin):
