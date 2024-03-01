d1256cd1d5ebfafd8d0090ebaedeb2d8342d9d30
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2024-03-01 16:44:00 +0100

    fix linting

--- teleport/tests/test_e2e.py
+++ teleport/tests/test_e2e.py
@@ -2,24 +2,17 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-import logging
-
 import pytest
 
-
 from datadog_checks.teleport import TeleportCheck
-
 
 pytestmark = pytest.mark.e2e
 
 CONFIG = {
     'init_config': {},
-    'instances': [
-        {
-            'diagnostic_url': "http://127.0.0.1:3000"
-        }
-    ],
+    'instances': [{'diagnostic_url': "http://127.0.0.1:3000"}],
 }
+
 
 def test_teleport_e2e(dd_agent_check):
     aggregator = dd_agent_check(CONFIG)
