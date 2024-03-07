bb1945107ef9a0b50d0d719a765901b895503f97
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-03-07 14:07:21 +0100

    Implementation and metadata.csv

--- argo_workflows/datadog_checks/argo_workflows/check.py
+++ argo_workflows/datadog_checks/argo_workflows/check.py
@@ -1,98 +1,56 @@
 # (C) Datadog, Inc. 2024-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from typing import Any  # noqa: F401
 
-from datadog_checks.base import AgentCheck  # noqa: F401
+from datadog_checks.base import OpenMetricsBaseCheckV2
 
-# from datadog_checks.base.utils.db import QueryManager
-# from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
-# from json import JSONDecodeError
+METRIC_MAP = {
+    'argo_workflows_count': 'current_workflows',
+    'argo_workflows_error_count': 'error',
+    'argo_workflows_k8s_request': 'k8s_request',
+    'argo_workflows_operation_duration_seconds': 'operation_duration_seconds',
+    'argo_workflows_pods_count': 'pods',
+    'argo_workflows_queue_adds_count': 'queue_adds',
+    'argo_workflows_queue_depth_count': 'queue_depth',
+    'argo_workflows_queue_latency': 'queue_latency',
+    'argo_workflows_workers_busy_count': 'workers_busy',
+    'argo_workflows_workflow_condition': 'workflow_condition',
+    'argo_workflows_workflows_processed_count': 'workflows_processed',
+    'log_messages': 'log_messages',
+    'go_gc_duration_seconds': 'go.gc.duration.seconds',
+    'go_goroutines': 'go.goroutines',
+    'go_info': "go.info",
+    'go_memstats_alloc_bytes': {'name': 'go.memstats.alloc_bytes', 'type': 'native_dynamic'},
+    'go_memstats_buck_hash_sys_bytes': 'go.memstats.buck_hash.sys_bytes',
+    'go_memstats_frees': 'go.memstats.frees',
+    'go_memstats_gc_sys_bytes': 'go.memstats.gc.sys_bytes',
+    'go_memstats_heap_alloc_bytes': 'go.memstats.heap.alloc_bytes',
+    'go_memstats_heap_idle_bytes': 'go.memstats.heap.idle_bytes',
+    'go_memstats_heap_inuse_bytes': 'go.memstats.heap.inuse_bytes',
+    'go_memstats_heap_objects': 'go.memstats.heap.objects',
+    'go_memstats_heap_released_bytes': 'go.memstats.heap.released_bytes',
+    'go_memstats_heap_sys_bytes': 'go.memstats.heap.sys_bytes',
+    'go_memstats_lookups': 'go.memstats.lookups',
+    'go_memstats_mallocs': 'go.memstats.mallocs',
+    'go_memstats_mcache_inuse_bytes': 'go.memstats.mcache.inuse_bytes',
+    'go_memstats_mcache_sys_bytes': 'go.memstats.mcache.sys_bytes',
+    'go_memstats_mspan_inuse_bytes': 'go.memstats.mspan.inuse_bytes',
+    'go_memstats_mspan_sys_bytes': 'go.memstats.mspan.sys_bytes',
+    'go_memstats_next_gc_bytes': 'go.memstats.next.gc_bytes',
+    'go_memstats_other_sys_bytes': 'go.memstats.other.sys_bytes',
+    'go_memstats_stack_inuse_bytes': 'go.memstats.stack.inuse_bytes',
+    'go_memstats_stack_sys_bytes': 'go.memstats.stack.sys_bytes',
+    'go_memstats_sys_bytes': 'go.memstats.sys_bytes',
+    'go_threads': 'go.threads',
+}
 
 
-class ArgoWorkflowsCheck(AgentCheck):
+class ArgoWorkflowsCheck(OpenMetricsBaseCheckV2):
+    __NAMESPACE__ = "argo_workflows"
+    DEFAULT_METRIC_LIMIT = 0
 
-    # This will be the prefix of every metric and service check the integration sends
-    __NAMESPACE__ = 'argo_workflows'
-
-    def __init__(self, name, init_config, instances):
-        super(ArgoWorkflowsCheck, self).__init__(name, init_config, instances)
-
-        # Use self.instance to read the check configuration
-        # self.url = self.instance.get("url")
-
-        # If the check is going to perform SQL queries you should define a query manager here.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
-        # sample_query = {
-        #     "name": "sample",
-        #     "query": "SELECT * FROM sample_table",
-        #     "columns": [
-        #         {"name": "metric", "type": "gauge"}
-        #     ],
-        # }
-        # self._query_manager = QueryManager(self, self.execute_query, queries=[sample_query])
-        # self.check_initializations.append(self._query_manager.compile_queries)
-
-    def check(self, _):
-        # type: (Any) -> None
-        # The following are useful bits of code to help new users get started.
-
-        # Perform HTTP Requests with our HTTP wrapper.
-        # More info at https://datadoghq.dev/integrations-core/base/http/
-        # try:
-        #     response = self.http.get(self.url)
-        #     response.raise_for_status()
-        #     response_json = response.json()
-
-        # except Timeout as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request timeout: {}, {}".format(self.url, e),
-        #     )
-        #     raise
-
-        # except (HTTPError, InvalidURL, ConnectionError) as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="Request failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
-
-        # except JSONDecodeError as e:
-        #     self.service_check(
-        #         "can_connect",
-        #         AgentCheck.CRITICAL,
-        #         message="JSON Parse failed: {}, {}".format(self.url, e),
-        #     )
-        #     raise
-
-        # except ValueError as e:
-        #     self.service_check(
-        #         "can_connect", AgentCheck.CRITICAL, message=str(e)
-        #     )
-        #     raise
-
-        # This is how you submit metrics
-        # There are different types of metrics that you can submit (gauge, event).
-        # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
-        # self.gauge("test", 1.23, tags=['foo:bar'])
-
-        # Perform database queries using the Query Manager
-        # self._query_manager.execute()
-
-        # This is how you use the persistent cache. This cache file based and persists across agent restarts.
-        # If you need an in-memory cache that is persisted across runs
-        # You can define a dictionary in the __init__ method.
-        # self.write_persistent_cache("key", "value")
-        # value = self.read_persistent_cache("key")
-
-        # If your check ran successfully, you can send the status.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck.service_check
-        # self.service_check("can_connect", AgentCheck.OK)
-
-        # If it didn't then it should send a critical service check
-        self.service_check("can_connect", AgentCheck.CRITICAL)
+    def get_default_config(self):
+        return {
+            "metrics": [METRIC_MAP],
+            "rename_labels": {"version": "go_version"},
+        }
--- argo_workflows/tests/test_unit.py
+++ argo_workflows/tests/test_unit.py
@@ -2,25 +2,93 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-from typing import Any, Callable, Dict  # noqa: F401
+import pytest
 
-from datadog_checks.base import AgentCheck  # noqa: F401
-from datadog_checks.base.stubs.aggregator import AggregatorStub  # noqa: F401
-from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.argo_workflows import ArgoWorkflowsCheck
+from datadog_checks.base.stubs import aggregator as agg
+from datadog_checks.dev.utils import get_metadata_metrics
 
 
-def test_check(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
+@pytest.fixture
+def instance():
+    return {'openmetrics_endpoint': 'http://localhost:9090/metrics'}
+
+
+GAUGES = {
+    (n, agg.GAUGE)
+    for n in {
+        'current_workflows',
+        'pods',
+        'queue_depth',
+        'workers_busy',
+        'workflow_condition',
+        'go.goroutines',
+        'go.info',
+        'go.memstats.alloc_bytes',
+        'go.memstats.buck_hash.sys_bytes',
+        'go.memstats.gc.sys_bytes',
+        'go.memstats.heap.alloc_bytes',
+        'go.memstats.heap.idle_bytes',
+        'go.memstats.heap.inuse_bytes',
+        'go.memstats.heap.objects',
+        'go.memstats.heap.released_bytes',
+        'go.memstats.heap.sys_bytes',
+        'go.memstats.mcache.inuse_bytes',
+        'go.memstats.mcache.sys_bytes',
+        'go.memstats.mspan.inuse_bytes',
+        'go.memstats.mspan.sys_bytes',
+        'go.memstats.next.gc_bytes',
+        'go.memstats.other.sys_bytes',
+        'go.memstats.stack.inuse_bytes',
+        'go.memstats.stack.sys_bytes',
+        'go.memstats.sys_bytes',
+        'go.threads',
+    }
+}
+COUNTS = {
+    (n, agg.MONOTONIC_COUNT)
+    for n in {
+        'error.count',
+        'k8s_request.count',
+        'log_messages.count',
+        'queue_adds.count',
+        'workflows_processed.count',
+        'go.memstats.alloc_bytes.count',
+        'go.memstats.frees.count',
+        'go.memstats.lookups.count',
+        'go.memstats.mallocs.count',
+    }
+}
+EXPECTED_METRICS = sorted(GAUGES | COUNTS)
+
+
+def test_check(dd_run_check, aggregator, instance, mock_http_response):
+    mock_http_response(file_path='tests/fixtures/metrics.txt')
     check = ArgoWorkflowsCheck('argo_workflows', {}, [instance])
     dd_run_check(check)
+
+    for m_name, m_type in EXPECTED_METRICS:
+        aggregator.assert_metric('argo_workflows.' + m_name, metric_type=m_type, at_least=1)
+
+    histograms = {
+        'operation_duration_seconds',
+        'queue_latency',
+    }
+    for m_name in histograms:
+        aggregator.assert_metric('argo_workflows.' + m_name + '.sum', metric_type=agg.MONOTONIC_COUNT, at_least=1)
+        aggregator.assert_metric("argo_workflows." + m_name + '.count', metric_type=agg.MONOTONIC_COUNT, at_least=1)
+        aggregator.assert_metric('argo_workflows.' + m_name + '.bucket', metric_type=agg.MONOTONIC_COUNT, at_least=1)
+
+    for suff in ('.count', '.quantile', '.sum'):
+        aggregator.assert_metric('argo_workflows.go.gc.duration.seconds' + suff, at_least=1)
 
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
-def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
+def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance, mock_http_response):
+    mock_http_response(status_code=404)
     check = ArgoWorkflowsCheck('argo_workflows', {}, [instance])
-    dd_run_check(check)
-    aggregator.assert_service_check('argo_workflows.can_connect', ArgoWorkflowsCheck.CRITICAL)
+    with pytest.raises(Exception, match='requests.exceptions.HTTPError'):
+        dd_run_check(check)
+    aggregator.assert_service_check('argo_workflows.openmetrics.health', ArgoWorkflowsCheck.CRITICAL)
