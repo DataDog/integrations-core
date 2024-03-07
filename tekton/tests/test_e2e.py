41441240de7f865444a613310ac33b6c33fd1d6b
Florent Clarret <florent.clarret@datadoghq.com>
2024-03-07 07:43:47 +0000

    Add the Tekton integration (#16665)
    * Add the structure of the Tekton integration
    
    * update manifest
    
    * auto install
    
    * Add the e2e environment
    
    * add triggers
    
    * lint
    
    * sleep
    
    * switch to the operator
    
    * lint
    
    * add tasks
    
    * echo sleep
    
    * sleep
    
    * sh
    
    * lint
    
    * add pipeline run
    
    * lint
    
    * add pipelinerun
    
    * Implement the check and add tests
    
    * triggers tests
    
    * update the config
    
    * metrics in manifest
    
    * update the config
    
    * nit
    
    * update tests
    
    * nit
    
    * lint
    
    * fix
    
    * test
    
    * add metrics
    
    * update manifest
    
    * simplify
    
    * config
    
    * lint
    
    * add metrics
    
    * update tests
    
    * add metrics
    
    * drop optional metrics
    
    * metadata
    
    * add service checks
    
    * fix
    
    * lint
    
    * metadata
    
    * revert gitlab stuff
    
    * Update tekton/datadog_checks/tekton/check.py
    
    Co-authored-by: Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
    
    * address
    
    * lint
    
    * fix
    
    * Update tekton/tests/conftest.py
    
    Co-authored-by: Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
    
    * Update tekton/datadog_checks/tekton/check.py
    
    * Update tekton/datadog_checks/tekton/check.py
    
    * changelog
    
    ---------
    
    Co-authored-by: Ilia Kurenkov <ilia.kurenkov@datadoghq.com>

---
+++ tekton/tests/test_e2e.py
@@ -0,0 +1,22 @@
+# (C) Datadog, Inc. 2024-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from datadog_checks.base import AgentCheck
+from datadog_checks.dev.utils import get_metadata_metrics
+
+from .common import PIPELINES_METRICS, TRIGGERS_METRICS
+
+
+def test_check(dd_agent_check):
+    aggregator = dd_agent_check(rate=True)
+
+    for expected_metric in PIPELINES_METRICS:
+        aggregator.assert_metric(f"tekton.pipelines_controller.{expected_metric}")
+
+    for expected_metric in TRIGGERS_METRICS:
+        aggregator.assert_metric(f"tekton.triggers_controller.{expected_metric}")
+
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    aggregator.assert_service_check("tekton.pipelines_controller.openmetrics.health", status=AgentCheck.OK)
+    aggregator.assert_service_check("tekton.triggers_controller.openmetrics.health", status=AgentCheck.OK)
