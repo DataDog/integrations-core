506c601e1773d73f3f2110f2aa7622b2c2e8f94f
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2024-03-08 11:34:16 +0100

    update integration tests

--- teleport/tests/test_integration.py
+++ teleport/tests/test_integration.py
@@ -9,17 +9,15 @@ from datadog_checks.teleport import TeleportCheck
 pytestmark = [pytest.mark.integration, pytest.mark.usefixtures("dd_environment")]
 
 
-def test_connect_ok(aggregator, dd_run_check):
-    instance = {"diagnostic_url": "http://127.0.0.1:3000"}
+def test_connect_ok(aggregator, instance, dd_run_check):
     check = TeleportCheck("teleport", {}, [instance])
     dd_run_check(check)
     aggregator.assert_service_check("teleport.health.up", status=TeleportCheck.OK, count=1)
     aggregator.assert_service_check("teleport.health.up", status=TeleportCheck.CRITICAL, count=0)
 
 
-def test_check_collects_teleport_common_metrics(aggregator, dd_run_check):
-    instance = {"diagnostic_url": "http://127.0.0.1:3000"}
+def test_check_collects_teleport_common_metrics(aggregator, instance, dd_run_check):
     check = TeleportCheck("teleport", {}, [instance])
     dd_run_check(check)
 
-    aggregator.assert_metric("teleport.process.state")
+    aggregator.assert_metric("teleport.common.process.state")
