23ca8b8946785545c2391aca0e03e6e5c7fb92f0
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-03-11 18:38:06 +0000

    use quote from six

--- datadog_checks_base/datadog_checks/base/utils/db/sql_commenter.py
+++ datadog_checks_base/datadog_checks/base/utils/db/sql_commenter.py
@@ -1,12 +1,7 @@
 # (C) Datadog, Inc. 2024-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from six import PY2
-
-if PY2:
-    from urllib import quote
-else:
-    from urllib.parse import quote
+from six.moves.urllib.parse import quote
 
 
 def generate_sql_comment(**data):
