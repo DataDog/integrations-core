8fe0de35b6c1808d59432af94c982d1efc6b199c
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-03-11 09:26:46 +0100

    fix style

--- gitlab_runner/tests/test_e2e.py
+++ gitlab_runner/tests/test_e2e.py
@@ -10,9 +10,7 @@ from .common import CONFIG, CUSTOM_TAGS, GITLAB_RUNNER_TAGS
 def test_e2e(dd_agent_check):
     aggregator = dd_agent_check(CONFIG)
     aggregator.assert_service_check(
-        GitlabRunnerCheck.MASTER_SERVICE_CHECK_NAME,
-        status=GitlabRunnerCheck.OK,
-        tags=GITLAB_RUNNER_TAGS + CUSTOM_TAGS
+        GitlabRunnerCheck.MASTER_SERVICE_CHECK_NAME, status=GitlabRunnerCheck.OK, tags=GITLAB_RUNNER_TAGS + CUSTOM_TAGS
     )
     aggregator.assert_service_check(
         GitlabRunnerCheck.PROMETHEUS_SERVICE_CHECK_NAME, status=GitlabRunnerCheck.OK, tags=CUSTOM_TAGS
