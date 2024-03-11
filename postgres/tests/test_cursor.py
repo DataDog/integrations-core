720f07e5d3b5e9ba04e788621b87434a0a344aa5
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-03-11 18:19:54 +0000

    tag agent queries

---
+++ postgres/tests/test_cursor.py
@@ -0,0 +1,43 @@
+# (C) Datadog, Inc. 2024-present
+# All rights reserved
+# Licensed under Simplified BSD License (see LICENSE)
+
+import pytest
+
+from datadog_checks.postgres.cursor import CommenterCursor, CommenterDictCursor
+
+from .utils import _get_superconn
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_integration_connection_with_commenter_cursor(integration_check, pg_instance):
+    check = integration_check(pg_instance)
+
+    with check.db() as conn:
+        # verify CommenterCursor and CommenterDictCursor prepend the query with /* source='datadog-agent' */
+        with conn.cursor(cursor_factory=CommenterCursor) as cursor:
+            cursor.execute('SELECT name, setting FROM pg_settings where name = %s', ('pg_stat_statements.max',))
+            result = cursor.fetchone()
+            assert result[0] == 'pg_stat_statements.max'
+        __check_prepand_sql_comment(pg_instance)
+
+        with conn.cursor(cursor_factory=CommenterDictCursor) as cursor:
+            cursor.execute('SELECT name, setting FROM pg_settings where name = %s', ('max_connections',))
+            result = cursor.fetchone()
+            assert result['name'] == 'max_connections'
+        __check_prepand_sql_comment(pg_instance)
+
+    check.cancel()
+
+
+def __check_prepand_sql_comment(pg_instance):
+    # collect query_text from pg_stat_activity
+    # assert /* source='datadog-agent' */ is present in the query
+    super_conn = _get_superconn(pg_instance)
+    with super_conn.cursor() as cursor:
+        cursor.execute("SELECT query FROM pg_stat_activity where query like '%pg_settings%'")
+        result = cursor.fetchall()
+        assert len(result) > 0
+        assert result[0][0].startswith('/* source=\'datadog-agent\' */')
+    super_conn.close()
