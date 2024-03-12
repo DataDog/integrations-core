ad6e3a37b2d13b5b0cd2f2462e189dd3aa88b011
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-03-12 12:28:22 +0000

    change to kwargs

--- datadog_checks_base/datadog_checks/base/utils/db/sql_commenter.py
+++ datadog_checks_base/datadog_checks/base/utils/db/sql_commenter.py
@@ -1,7 +1,7 @@
 # (C) Datadog, Inc. 2024-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-def generate_sql_comment(**data):
+def generate_sql_comment(**kwargs):
     '''
     Generate a SQL comment from a dictionary of key-value pairs.
     Returns an empty string if the input dictionary is empty or if all values are empty.
@@ -9,15 +9,17 @@ def generate_sql_comment(**data):
     >>> generate_sql_comment(foo='bar', baz='qux')
     "/* baz='qux',foo='bar' */"
     '''
-    if not data:
+    if not kwargs:
         return ''
 
-    comment = ','.join("{}='{}'".format(key, value) for key, value in data.items() if value is not None and value != '')
+    comment = ','.join(
+        "{}='{}'".format(key, value) for key, value in kwargs.items() if value is not None and value != ''
+    )
 
     return '/* {} */'.format(comment)
 
 
-def add_sql_comment(sql, prepand=True, **data):
+def add_sql_comment(sql, prepand=True, **kwargs):
     '''
     Prepand or append a SQL comment to a SQL statement from a dictionary of key-value pairs.
     Returns the original SQL statement if the input dictionary is empty or if all values are empty.
@@ -27,7 +29,7 @@ def add_sql_comment(sql, prepand=True, **data):
     >>> add_sql_comment('SELECT * FROM table', False, foo='bar', baz='qux')
     "SELECT * FROM table /* baz='qux',foo='bar' */"
     '''
-    comment = generate_sql_comment(**data)
+    comment = generate_sql_comment(**kwargs)
     if not comment:
         return sql
 
