71aaaec1bf18482420aa3dc2bbcfa6401dc6b111
Florent Clarret <florent.clarret@datadoghq.com>
2024-03-14 14:23:01 +0100

    Collect more metrics

--- cockroachdb/tests/test_unit.py
+++ cockroachdb/tests/test_unit.py
@@ -17,6 +17,7 @@ from .common import (
     KV_METRICS,
     PHYSICAL_METRICS,
     QUEUE_METRICS,
+    RAFT_METRICS,
     assert_metrics,
     get_fixture_path,
 )
@@ -61,6 +62,7 @@ def test_fixture_metrics(aggregator, instance, dd_run_check, mock_http_response,
         pytest.param('kv_metrics.txt', KV_METRICS, id='kv'),
         pytest.param('physical_metrics.txt', PHYSICAL_METRICS, id='physical'),
         pytest.param('queue_metrics.txt', QUEUE_METRICS, id='queue'),
+        pytest.param('raft_metrics.txt', RAFT_METRICS, id='raft'),
     ],
 )
 def test_metrics(aggregator, instance, dd_run_check, mock_http_response, file, metrics):
