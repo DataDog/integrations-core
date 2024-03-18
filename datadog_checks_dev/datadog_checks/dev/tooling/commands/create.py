a6a979ec8f704fe55a91c02adb1b7c8cdb6f5859
Inigo Mediavilla Saiz <inigo.mediavilla@datadoghq.com>
2024-03-18 17:30:12 +0100

    Lint python files

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/create.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/create.py
@@ -153,9 +153,9 @@ def create(ctx, name, integration_type, location, non_interactive, quiet, dry_ru
 
             eula = 'assets/eula.pdf'
             template_fields['terms'] = f'\n  "terms": {{\n    "eula": "{eula}"\n  }},'
-            template_fields['author_info'] = (
-                f'\n  "author": {{\n    "name": "{author_name}",\n    "homepage": "{homepage}",\n    "vendor_id": "{TODO_FILL_IN}",\n    "sales_email": "{sales_email}",\n    "support_email": "{support_email}"\n  }},'  # noqa
-            )
+            template_fields[
+                'author_info'
+            ] = f'\n  "author": {{\n    "name": "{author_name}",\n    "homepage": "{homepage}",\n    "vendor_id": "{TODO_FILL_IN}",\n    "sales_email": "{sales_email}",\n    "support_email": "{support_email}"\n  }},'  # noqa
 
             template_fields['pricing_plan'] = '\n  "pricing": [],'
 
