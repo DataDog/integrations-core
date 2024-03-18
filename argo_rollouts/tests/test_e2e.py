ae8f5d190771089b70c865c10dc606249cf6e127
steveny91 <steven.yuen@datadoghq.com>
2024-03-18 10:18:01 -0400

    simplify e2e

--- argo_rollouts/tests/test_e2e.py
+++ argo_rollouts/tests/test_e2e.py
@@ -4,18 +4,11 @@
 import pytest
 
 from datadog_checks.base.constants import ServiceCheck
-from datadog_checks.dev.utils import assert_service_checks, get_metadata_metrics
-
-from .common import E2E_METRICS
+from datadog_checks.dev.utils import assert_service_checks
 
 
 def test_e2e_openmetrics_v2(dd_agent_check):
-    aggregator = dd_agent_check(rate=True)
+    aggregator = dd_agent_check()
 
-    aggregator.assert_service_check('argo_rollouts.openmetrics.health', ServiceCheck.OK, count=2)
-    for metric in E2E_METRICS:
-        aggregator.assert_metric(metric)
-
-    aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    aggregator.assert_service_check('argo_rollouts.openmetrics.health', ServiceCheck.OK, count=1)
     assert_service_checks(aggregator)
