ab36d15d5d2ae5b7afc80a26e5c84859bf39b478
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2024-03-19 13:59:41 +0100

    add custom metric transformer to add 'teleport_service' tag

--- teleport/datadog_checks/teleport/check.py
+++ teleport/datadog_checks/teleport/check.py
@@ -3,8 +3,9 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 from datadog_checks.base import OpenMetricsBaseCheckV2
+from datadog_checks.base.checks.openmetrics.v2.transform import get_native_dynamic_transformer
 
-from .metrics import METRIC_MAP
+from .metrics import METRIC_MAP, METRIC_MAP_BY_SERVICE
 
 
 class TeleportCheck(OpenMetricsBaseCheckV2):
@@ -14,6 +15,7 @@ class TeleportCheck(OpenMetricsBaseCheckV2):
     def __init__(self, name, init_config, instances):
         super().__init__(name, init_config, instances)
         self.check_initializations.appendleft(self._parse_config)
+        self.check_initializations.append(self._configure_additional_transformers)
 
     def check(self, _):
         try:
@@ -31,5 +33,52 @@ class TeleportCheck(OpenMetricsBaseCheckV2):
         if self.teleport_url:
             self.diag_addr = "{}:{}".format(self.teleport_url, self.diag_port)
             self.instance.setdefault("openmetrics_endpoint", "{}/metrics".format(self.diag_addr))
-            self.instance.setdefault("metrics", [METRIC_MAP])
             self.instance.setdefault("rename_labels", {'version': "teleport_version"})
+
+    def _configure_additional_transformers(self):
+        if not self.scrapers:
+            return
+        metric_transformer = self.scrapers[self.instance['openmetrics_endpoint']].metric_transformer
+        metric_transformer.add_custom_transformer(r'.*', self.configure_transformer_teleport_metrics(), pattern=True)
+
+    def configure_transformer_teleport_metrics(self):
+        def transform(_metric, sample_data, _runtime_data):
+            for sample, tags, hostname in sample_data:
+                metric_name = _metric.name
+                metric_type = _metric.type
+
+                # ignore metrics we don't collect
+                if metric_name not in METRIC_MAP:
+                    continue
+
+                # extract `teleport_service` tag
+                service = METRIC_MAP_BY_SERVICE.get(metric_name, "teleport")
+                tags = tags + [f"teleport_service:{service}"]
+
+                # get mapped metric name
+                new_metric_name = METRIC_MAP[metric_name]
+                if isinstance(new_metric_name, dict) and "name" in new_metric_name:
+                    new_metric_name = new_metric_name["name"]
+
+                # send metric
+                metric_transformer = self.scrapers[self.instance['openmetrics_endpoint']].metric_transformer
+
+                if metric_type == "counter":
+                    self.count(new_metric_name + ".count", sample.value, tags=tags, hostname=hostname)
+                elif metric_type == "gauge":
+                    self.gauge(new_metric_name, sample.value, tags=tags, hostname=hostname)
+                elif metric_type == "rate":
+                    self.rate(new_metric_name, sample.value, tags=tags, hostname=hostname)
+                else:
+                    native_transformer = get_native_dynamic_transformer(
+                        self, new_metric_name, None, metric_transformer.global_options
+                    )
+
+                    def add_tag_to_sample(sample, service):
+                        [sample, tags, hostname] = sample
+                        return [sample, tags + [f"teleport_service:{service}"], hostname]
+
+                    modified_sample_data = (add_tag_to_sample(x, service) for x in sample_data)
+                    native_transformer(_metric, modified_sample_data, _runtime_data)
+
+        return transform
