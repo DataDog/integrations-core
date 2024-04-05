dc6adf09e93cd7ac024d659cd0f73547952261d9
Boris Kozlov <boris.kozlov@datadoghq.com>
2024-04-05 08:54:04 +0000

    snapshot collect data

--- sqlserver/datadog_checks/sqlserver/sqlserver.py
+++ sqlserver/datadog_checks/sqlserver/sqlserver.py
@@ -748,6 +748,7 @@ class SQLServer(AgentCheck):
 
         # Todo put in consts
         # there is also principal_id not sure if need it.
+        # TODO exclude schemas like INFORMATION_SCHEMA
         SCHEMA_QUERY = "SELECT name,schema_id,principal_id FROM sys.schemas;"
         self.log.debug("collecting db schemas")
         self.log.debug("Running query [%s]", SCHEMA_QUERY)
@@ -755,28 +756,103 @@ class SQLServer(AgentCheck):
         schemas = []
         columns = [i[0] for i in cursor.description]
         schemas = [dict(zip(columns, row)) for row in cursor.fetchall()]
-        schemas_by_name = {}
-
-        schemas_by_name = {}
-
-        for schema in schemas:
-            name = schema['name'].lower()
-            #add tables
-            schema['tables'] = {}
-            schemas_by_name[name] = schema
-
+        #add tables
+        
+        for s in schemas:
+            s['tables'] = {}
+ 
         self.log.debug("fetched schemas len(rows)=%s", len(schemas))
-        return schemas_by_name
+        return schemas
     
 #in tables we have modified date !
     # can be a separate query 
     
+    # plan lets do per db per schema , get all tables , then (sort or pick first batch), then query columns per batch or table ?
+    def _get_table_infos_sys_tables_per_schema(self, schemas, cursor):
 
+        for schema in schemas:
+            self._get_table_infos_sys_tables(schema, cursor)
 
-    def _get_table_infos_sys_tables(self, schemas, cursor):
-        print("Hello")
-        TABLE_QUERY = ""
+    #TODOTODO do we need this map/list format if we are not dumping in json ??? May be we need to send query results as they are ? 
+    def _get_table_infos_sys_tables(self, schema, cursor):
+        tables_dict_for_schema = schema['tables']
         
+        # we could get data from sys tables too ...  
+        # can be done by table as well , might be usefull in case if we get too many rows i.e. we could split this query in several 
+        # patches. As for updates we could have a separate mechanism
+        # .
+        # TODO modify_date - there is a modify date !!! 
+        # TODO what is principal_id
+        # TODO is_replicated - might be interesting ? 
+        TABLES_IN_SCHEMA_QUERY = "SELECT name, object_id FROM sys.tables WHERE schema_id={}".format(schema["schema_id"])
+
+        cursor.execute(TABLES_IN_SCHEMA_QUERY)
+        columns = [str(i[0]).lower() for i in cursor.description]
+        rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        for row in rows:
+            if len(row) != 2:
+            #TODO some warning ? 
+                print("warning") 
+            
+            tables_dict_for_schema[row['object_id']] = {"name" : row['name'], "columns" : [], "indexes" : [], "foreign_keys" : []}
+        #TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE, COLUMN_DEFAULT 
+        # in sys.columns I cannot see a data type but there are other things   
+        #object_id   name       
+        #column_id   system_type_id user_type_id max_length precision scale collation_name   
+        # is_nullable is_ansi_padded is_rowguidcol is_identity is_computed is_filestream is_replicated 
+        # is_non_sql_subscribed is_merge_published is_dts_replicated is_xml_document xml_collection_id 
+        # default_object_id rule_object_id is_sparse is_column_set generated_always_type generated_always_type_desc                                   encryption_type encryption_type_desc                                             encryption_algorithm_name                                                                                                        column_encryption_key_id column_encryption_key_database_name                                                                                              is_hidden is_masked graph_type  graph_type_desc                                              is_data_deletion_filter_column ledger_view_column_type ledger_view_column_type_desc                                 is_dropped_ledger_column
+        # might be slower then in sys.columns as we cant get data by object_id .... but we get data_type
+        #COLUMN_QUERY = "SELECT COLUMN_NAME, DATA_TYPE, COLUMN_DEFAULT FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA={} and TABLE_NAME={};".format(schema_name, )
+        
+        #if using query 2 we need to figure out user_type_id - its like a user defined type
+        # TODO AL in all query 2 will be query 2 faster ? or its just less convinient at the end ... ? object_id is nice
+
+        COLUMN_QUERY2 = "SELECT c.name AS name, t.name AS data_type, dc.definition AS default_value FROM sys.columns c JOIN sys.types t ON c.system_type_id = t.system_type_id LEFT JOIN sys.default_constraints dc ON c.default_object_id = dc.object_id WHERE c.object_id = {}"
+       
+        # TODO can be a function query and unwrap in dict
+        for table_object_id, table_value in tables_dict_for_schema.items():
+            cursor.execute(COLUMN_QUERY2.format(table_object_id))
+            columns = [str(i[0]).lower() for i in cursor.description]
+            rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            for row in rows:
+                table_value["columns"].append(row)
+        
+        # object_id   name  index_id    type type_desc is_unique data_space_id ignore_dup_key is_primary_key is_unique_constraint 
+        # fill_factor is_padded is_disabled is_hypothetical is_ignored_in_optimization allow_row_locks allow_page_locks has_filter 
+        # filter_definition    
+        
+        #May be better to query sys.index_columns ?                                                                                                                                                                                                                                            compression_delay suppress_dup_key_messages auto_created optimize_for_sequential_key
+        INDEX_QUERY = "SELECT name, type, is_unique, is_primary_key, is_unique_constraint, is_disabled FROM sys.indexes WHERE object_id={}"
+
+        # index query:
+        for table_object_id, table_value in tables_dict_for_schema.items():
+            cursor.execute(INDEX_QUERY.format(table_object_id))
+            columns = [str(i[0]).lower() for i in cursor.description]
+            rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            for row in rows:
+                table_value["indexes"].append(row)
+        
+        # foreign keys
+        # name object_id principal_id schema_id parent_object_id type type_desc create_date modify_date is_ms_shipped 
+        # is_published is_schema_published referenced_object_id key_index_id is_disabled is_not_for_replication 
+        # is_not_trusted delete_referential_action delete_referential_action_desc update_referential_action 
+        # update_referential_action_desc is_system_named compression_delay suppress_dup_key_messages auto_created optimize_for_sequential_key
+        # SELECT name , OBJECT_NAME(parent_object_id) FROM sys.foreign_keys;
+        # fk.name AS foreign_key_name, OBJECT_NAME(fk.parent_object_id) AS parent_table, COL_NAME(fkc.parent_object_id, fkc.parent_column_id) AS parent_column, OBJECT_NAME(fk.referenced_object_id) AS referenced_table, COL_NAME(fkc.referenced_object_id, fkc.referenced_column_id) AS referenced_column FROM  sys.foreign_keys fk JOIN  sys.foreign_key_columns fkc ON fk.object_id = fkc.constraint_object_id WHERE  fk.parent_object_id = 'YourTableObjectID' -- Replace 'YourTableObjectID' with the object_id of your table
+                                                                                                                                                                            
+        FOREIGN_KEY_QUERY = "SELECT name , OBJECT_NAME(parent_object_id) FROM sys.foreign_keys WHERE object_id={};"
+        
+        # index query:
+        for table_object_id, table_value in tables_dict_for_schema.items():
+            cursor.execute(FOREIGN_KEY_QUERY.format(table_object_id))
+            columns = [str(i[0]).lower() for i in cursor.description]
+            rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            for row in rows:
+                table_value["foreign_keys"].append(row)
+
+        print("the end")
+
     # TODO how often ?
     # TODO put in a class
     # for big DBs somehow first determine tables we are intereted in and query only for them ?
@@ -840,17 +916,20 @@ class SQLServer(AgentCheck):
         schemas_per_db = {}
         #TODO its copy paste make a function
         db_names = [d.name for d in self.databases] or [self.instance.get('database', self.connection.DEFAULT_DATABASE)]
-        pdb.set_trace()
         with self.connection.open_managed_default_connection():
             with self.connection.get_managed_cursor() as cursor:
                 for db in db_names:                    
                     try:
-                        pdb.set_trace()
                         cursor.execute(SWITCH_DB_STATEMENT.format(db))
                         schemas = self._query_schema_information(cursor)
-                        self._get_table_infos(schemas, cursor)
+                        #self._get_table_infos(schemas, cursor)
+
+                        self._get_table_infos_sys_tables_per_schema(schemas, cursor)
+                        
                         schemas_per_db[db] = schemas
+                        pdb.set_trace()
                     except Exception as e:
+                        pdb.set_trace()
                         print("TODO")
                 # Switch DB back to MASTER
                 cursor.execute(SWITCH_DB_STATEMENT.format(self.connection.DEFAULT_DATABASE))
