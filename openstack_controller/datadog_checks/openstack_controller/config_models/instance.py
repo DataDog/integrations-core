386b40971b0b032df3a9e7d4e3b2821967bdc204
rahulkaukuntla <rahul.kaukuntla@datadoghq.com>
2024-04-18 10:27:27 -0400

    updating instance.py
    Signed-off-by: rahulkaukuntla <rahul.kaukuntla@datadoghq.com>

--- openstack_controller/datadog_checks/openstack_controller/config_models/instance.py
+++ openstack_controller/datadog_checks/openstack_controller/config_models/instance.py
@@ -63,8 +63,8 @@ class BlockStorageItem(BaseModel):
         arbitrary_types_allowed=True,
         frozen=True,
     )
-    snapshots: Optional[MappingProxyType[str, Any]] = None
     pools: Optional[MappingProxyType[str, Any]] = None
+    snapshots: Optional[MappingProxyType[str, Any]] = None
     transfers: Optional[MappingProxyType[str, Any]] = None
     volumes: Optional[MappingProxyType[str, Any]] = None
 
