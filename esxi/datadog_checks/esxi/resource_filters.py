20708d23c9a932192684d9d833795e1bdca06abf
Sarah Witt <sarah.witt@datadoghq.com>
2024-04-22 12:19:35 -0400

    Update header

--- esxi/datadog_checks/esxi/resource_filters.py
+++ esxi/datadog_checks/esxi/resource_filters.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2024-present
 # All rights reserved
-# Licensed under Simplified BSD License (see LICENSE)
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
 from typing import List, Pattern, Tuple  # noqa: F401
 
 
