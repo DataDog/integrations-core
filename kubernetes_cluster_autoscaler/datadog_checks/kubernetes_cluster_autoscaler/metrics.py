33e17cd39ca570583ec8c0dfc9c19d38e976f777
HadhemiDD <hadhemi.samaali@datadoghq.com>
2024-04-30 15:15:14 +0200

    implement check

---
+++ kubernetes_cluster_autoscaler/datadog_checks/kubernetes_cluster_autoscaler/metrics.py
@@ -0,0 +1,36 @@
+# (C) Datadog, Inc. 2023-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+# Some metrics mapping are too long. This turns off the 120 line limit for this file:
+# ruff: noqa: E501
+
+# metrics are documented here 
+# https://github.com/kubernetes/autoscaler/blob/master/cluster-autoscaler/proposals/metrics.md
+
+METRIC_MAP = {
+    'cluster_autoscaler.cluster_cpu_current_cores': 'cluster_autoscaler.cluster.cpu.current.cores',
+    'cluster_autoscaler.cluster_memory_current_bytes': 'cluster_autoscaler.cluster.memory.current.bytes',
+    'cluster_autoscaler.cluster_safe_to_autoscale': 'cluster_autoscaler.cluster.safe.to.autoscale',
+    'cluster_autoscaler.cpu_limits_cores': 'cluster_autoscaler.cpu.limits.cores',
+    'cluster_autoscaler.created_node_groups_total': 'cluster_autoscaler.created.node.groups.total',
+    'cluster_autoscaler.deleted_node_groups_total': 'cluster_autoscaler.deleted.node.groups.total',
+    'cluster_autoscaler.errors_total': 'cluster_autoscaler.errors.total',
+    'cluster_autoscaler.evicted_pods_total': 'cluster_autoscaler.evicted.pods.total',
+    'cluster_autoscaler.failed_scale_ups_total': 'cluster_autoscaler.failed.scale.ups.total',
+    'cluster_autoscaler.function_duration_seconds': 'cluster_autoscaler.function.duration.seconds',
+    'cluster_autoscaler.last_activity': 'cluster_autoscaler.last.activity',
+    'cluster_autoscaler.max_nodes_count': 'cluster_autoscaler.max.nodes.count',
+    'cluster_autoscaler.memory_limits_bytes': 'cluster_autoscaler.memory.limits.bytes',
+    'cluster_autoscaler.nap_enabled': 'cluster_autoscaler.nap.enabled',
+    'cluster_autoscaler.node_count': 'cluster_autoscaler.node.count',
+    'cluster_autoscaler.node_groups_count': 'cluster_autoscaler.node.groups.count',
+    'cluster_autoscaler.old_unregistered_nodes_removed_count': 'cluster_autoscaler.old.unregistered.nodes.removed.count',
+    'cluster_autoscaler.scaled_dow_gpu_nodes_total': 'cluster_autoscaler.scaled.down.gpu.nodes.total',
+    'cluster_autoscaler.scaled_down_nodes_total': 'cluster_autoscaler.scaled.down.nodes.total',
+    'cluster_autoscaler.scaled_up_gpu_nodes_total': 'cluster_autoscaler.scaled.up.gpu.nodes.total',
+    'cluster_autoscaler.scaled_up_nodes_total': 'cluster_autoscaler.scaled.up.nodes.total',
+    'cluster_autoscaler.skipped_scale_events_count': 'cluster_autoscaler.skipped.scale.events.count',
+    'cluster_autoscaler.unneeded_nodes_count': 'cluster_autoscaler.unneeded.nodes.count',
+    'cluster_autoscaler.unschedulable_pods_count': 'cluster_autoscaler.unschedulable.pods.count',
+}
