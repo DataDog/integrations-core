6550777b12a948b3e997aa4f015db993086e1753
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2024-05-09 15:06:59 +0200

    Updated default script

--- openstack_controller/tests/e2e-gcp/default/script.sh
+++ openstack_controller/tests/e2e-gcp/default/script.sh
@@ -3,21 +3,23 @@ sudo apt update
 sudo apt upgrade -y
 sudo useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+sudo -u stack bash << EOF
 cd /opt/stack
-sudo git clone https://opendev.org/openstack/devstack
+git clone https://opendev.org/openstack/devstack --branch stable/2023.1
 cd devstack
-sudo git checkout origin/stable/zed
-cat <<EOF | sudo tee -a local.conf
-[[local|localrc]]
-DATABASE_PASSWORD=password
-ADMIN_PASSWORD=password
-SERVICE_PASSWORD=password
-SERVICE_TOKEN=password
-RABBIT_PASSWORD=password
-# Enable Logging
-LOGFILE=/opt/stack/logs/stack.sh.log
-VERBOSE=True
-LOG_COLOR=True
+echo "[[local|localrc]]" > local.conf
+echo "DATABASE_PASSWORD=password" >> local.conf
+echo "ADMIN_PASSWORD=password" >> local.conf
+echo "SERVICE_PASSWORD=password" >> local.conf
+echo "SERVICE_TOKEN=password" >> local.conf
+echo "RABBIT_PASSWORD=password" >> local.conf
+echo "# Enable Logging" >> local.conf
+echo "LOGFILE=/opt/stack/logs/stack.sh.log" >> local.conf
+echo "VERBOSE=True" >> local.conf
+echo "LOG_COLOR=True" >> local.conf
+echo "#Enable heat services" >> local.conf
+echo "enable_service h-eng h-api h-api-cfn h-api-cw" >> local.conf
+echo "#Enable heat plugin" >> local.conf
+echo "enable_plugin heat https://opendev.org/openstack/heat stable/2023.1" >> local.conf
+./stack.sh
 EOF
-sudo chown stack:stack /opt/stack/devstack -R
-sudo -u stack -H ./stack.sh
