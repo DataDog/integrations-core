df7e0ae67bd2ef8cab1218665dce0bf22b934300
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-05-10 20:54:57 +0000

    add example

--- datadog_checks_base/datadog_checks/base/utils/db/timed_cache.py
+++ datadog_checks_base/datadog_checks/base/utils/db/timed_cache.py
@@ -11,6 +11,19 @@ class TimedCache:
     Note: The cache DOES NOT automatically clear itself.
     It is up to the user to check if the cache is expired and clear it if needed.
     This is to avoid clearing the cache while it is being used in the middle of a check run.
+
+    Example:
+    ```
+    cache = TimedCache(600)  # Cache expires after 10 minutes
+    if cache.is_expired():  # Check if the cache is expired
+        cache.clear()  # Clear the cache and reset the last refresh time
+        # Do something to rebuild the cache
+
+    cache['key'] = 'value'  # Set an item in the cache
+    print(cache['key'])  # Get an item from the cache
+    print(cache.get('mykey', 1))  # Get an item from the cache with a default value
+    del cache['key']
+    ```
     """
 
     def __init__(self, ttl):
