970bbb0693424aa129f4e601eabf2513e4023ef9
Sarah Witt <sarah.witt@datadoghq.com>
2024-05-10 14:06:00 -0400

    Remove invalid host tags (#17549)
    * remove invalid host tags
    
    * Improve validation
    
    * Add changelog
    
    * Update test after merge

--- esxi/datadog_checks/esxi/check.py
+++ esxi/datadog_checks/esxi/check.py
@@ -63,8 +63,7 @@ class EsxiCheck(AgentCheck):
             if excluded_host_tag not in AVAILABLE_HOST_TAGS:
                 self.log.warning(
                     "Unknown host tag `%s` cannot be excluded. Available host tags are: "
-                    "`esxi_url`, `esxi_type`, `esxi_host`, `esxi_folder`, `esxi_cluster` "
-                    "`esxi_compute`, `esxi_datacenter`, and `esxi_datastore`",
+                    "`esxi_url`, `esxi_type`, `esxi_host`, `esxi_compute`, and `esxi_datastore`",
                     excluded_host_tag,
                 )
             else:
@@ -423,17 +422,12 @@ class EsxiCheck(AgentCheck):
                 runtime_hostname = to_string(runtime_host_props.get("name", "unknown"))
                 tags.append('esxi_host:{}'.format(runtime_hostname))
 
-                if runtime_host is not None:
-                    tags.extend(
-                        get_tags_recursively(
-                            runtime_host,
-                            resource_map,
-                            include_only=['esxi_cluster'],
-                        )
-                    )
-
             if parent is not None:
-                tags.extend(get_tags_recursively(parent, resource_map))
+                tags.extend(
+                    get_tags_recursively(
+                        parent, resource_map, include_only=["esxi_compute", "esxi_host", "esxi_datastore"]
+                    )
+                )
 
             tags.append('esxi_type:{}'.format(resource_type_name))
 
