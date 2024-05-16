c59fd2f97460c4a21d0d5c04d937933a409d3cf0
Gustavo Caso <gustavo.caso@datadoghq.com>
2024-05-16 23:08:18 +0200

    Ensure passing Class and instance to super

--- datadog_checks_base/datadog_checks/base/utils/profiling.py
+++ datadog_checks_base/datadog_checks/base/utils/profiling.py
@@ -10,7 +10,7 @@ class Profiling(object):
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super().__new__(cls)
+            cls._instance = super(Profiling, cls).__new__(cls)
             cls._instance._running = False
             cls._instance._profiler = None
 
