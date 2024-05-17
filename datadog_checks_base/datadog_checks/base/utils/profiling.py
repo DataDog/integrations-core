f3490ac2b750d461619d33af41a7e7b2b11352fd
Gustavo Caso <gustavo.caso@datadoghq.com>
2024-05-17 17:43:03 +0200

    use Mutex to ensure multiple processes operate correctly

--- datadog_checks_base/datadog_checks/base/utils/profiling.py
+++ datadog_checks_base/datadog_checks/base/utils/profiling.py
@@ -2,6 +2,8 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
+from threading import Lock
+
 
 class Profiling(object):
     _instance = None
@@ -11,29 +13,30 @@ class Profiling(object):
             cls._instance = super(Profiling, cls).__new__(cls)
             cls._instance._running = False
             cls._instance._profiler = None
+            cls._instance._mutex = Lock()
             cls._instance._profiling_imported = False
 
         return cls._instance
 
-    # TODO: Double check if we need to use any concurrency control mechanism
     def start(self):
-        if not self._running and self._profiler is None:
-            if not self._profiling_imported:
-                from ddtrace.profiling import Profiler
+        with self._mutex:
+            if not self._running and self._profiler is None:
+                if not self._profiling_imported:
+                    from ddtrace.profiling import Profiler
 
-                self._profiling_imported = True
+                    self._profiling_imported = True
 
-            self._profiler = Profiler(service="datadog-agent-integrations")
-            self._profiler.start()
-            self._running = True
-        if not self._running and self._profiler is not None:
-            # This branch is exercise if we have previously stopped the profiler
-            if self._profiler.status != "running":
+                self._profiler = Profiler(service="datadog-agent-integrations")
                 self._profiler.start()
                 self._running = True
+            if not self._running and self._profiler is not None:
+                # This branch is exercise if we have previously stopped the profiler
+                if self._profiler.status != "running":
+                    self._profiler.start()
+                    self._running = True
 
-    # TODO: Double check if we need to use any concurrency control mechanism
     def stop(self):
-        if self._running:
-            self._profiler.stop()
-            self._running = False
+        with self._mutex:
+            if self._running:
+                self._profiler.stop()
+                self._running = False
