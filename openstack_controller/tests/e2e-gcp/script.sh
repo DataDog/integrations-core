98021b2003fe5617e3b99d351cdd99c1b9a5cbca
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2024-05-21 11:47:42 +0200

    Added steps to install podman

--- openstack_controller/tests/e2e-gcp/script.sh
+++ openstack_controller/tests/e2e-gcp/script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
+sudo apt install -y software-properties-common build-essential git curl wget
+
+source /etc/os-release
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L "http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add -
+sudo apt update
+sudo apt -y install podman
+
 sudo useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 sudo -u stack bash << EOF
