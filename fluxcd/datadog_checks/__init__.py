e61de963293847459d970b55299f908a7980cd3d
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-05-22 15:13:51 +0200

    swith to smaller trigger

--- fluxcd/datadog_checks/__init__.py
+++ fluxcd/datadog_checks/__init__.py
@@ -2,3 +2,4 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
+# TODO: remove this after done testing CI step
