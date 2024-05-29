9534c99f1276d2641f4a118e5dd4f5e7ba36e376
root <root@ip-172-29-198-131.ec2.internal>
2024-05-29 08:43:40 +0000

    Add context_info to activity samples

--- sqlserver/datadog_checks/sqlserver/activity.py
+++ sqlserver/datadog_checks/sqlserver/activity.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
+import base64
 import binascii
 import datetime
 import re
@@ -136,6 +137,7 @@ DM_EXEC_REQUESTS_COLS = [
     "row_count",
     "query_hash",
     "query_plan_hash",
+    "context_info",
 ]
 
 
@@ -333,6 +335,10 @@ class SqlserverActivity(DBMAsyncJob):
         # remove deobfuscated sql text from event
         if 'statement_text' in row:
             del row['statement_text']
+        # convert `context_info` to string, since the default decoding format 
+        # for json is utf-8, and `context_info` need not comply to utf-8
+        if 'context_info' in row:
+            row['context_info'] = row['context_info'].hex()
         return row
 
     @staticmethod
