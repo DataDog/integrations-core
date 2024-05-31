d6efe98bf17a96fc8a51d1b2344e35d19fd1f313
sguillen18 <seanguillen18@gmail.com>
2024-05-31 15:09:41 -0400

    added support for TeamCity 2023.05.4

--- teamcity/tests/test_openmetrics.py
+++ teamcity/tests/test_openmetrics.py
@@ -6,7 +6,7 @@ from six import PY2
 
 from datadog_checks.dev.utils import get_metadata_metrics
 
-from .common import PROMETHEUS_METRICS, get_fixture_path
+from .common import PROMETHEUS_METRICS, PROMETHEUS_METRICS_2023_05_04, get_fixture_path
 
 
 @pytest.mark.integration
@@ -19,6 +19,20 @@ def test_omv2_check(aggregator, openmetrics_instance, mock_http_response, dd_run
         aggregator.assert_metric(m)
         aggregator.assert_metric_has_tag(m, 'endpoint:http://localhost:8111/guestAuth/app/metrics')
         aggregator.assert_metric_has_tag(m, 'nodeId:tc-instance-1')
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    aggregator.assert_service_check(
+        'teamcity.openmetrics.health', check.OK, tags=['endpoint:http://localhost:8111/guestAuth/app/metrics']
+    )
+
+def test_omv2_check_2023_05_04(aggregator, openmetrics_instance, mock_http_response, dd_run_check, teamcity_om_check):
+    mock_http_response(file_path=get_fixture_path('metrics2023.05.4.txt'))
+    check = teamcity_om_check(openmetrics_instance)
+    dd_run_check(check)
+    for m in PROMETHEUS_METRICS_2023_05_04:
+        aggregator.assert_metric(m)
+        aggregator.assert_metric_has_tag(m, 'endpoint:http://localhost:8111/guestAuth/app/metrics')
+        aggregator.assert_metric_has_tag(m, 'nodeId:MAIN_SERVER')
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
     aggregator.assert_service_check(
