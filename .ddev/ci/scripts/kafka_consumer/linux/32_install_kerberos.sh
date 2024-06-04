981568aca93179fc9579b64b531478470f452dbd
steveny91 <steven.yuen@datadoghq.com>
2024-06-04 11:53:51 -0400

    test adding ssl

--- .ddev/ci/scripts/kafka_consumer/linux/32_install_kerberos.sh
+++ .ddev/ci/scripts/kafka_consumer/linux/32_install_kerberos.sh
@@ -3,13 +3,13 @@
 set -ex
 
 sudo apt update
-sudo apt install -y --no-install-recommends build-essential libkrb5-dev wget software-properties-common lsb-release gcc make python3 python3-pip python3-dev libsasl2-modules-gssapi-mit krb5-user
+sudo apt install -y --no-install-recommends build-essential libkrb5-dev wget software-properties-common lsb-release gcc make python3 python3-pip python3-dev libsasl2-modules-gssapi-mit krb5-user libsasl2-dev
 
 # Install librdkafka from source since no binaries are available for the distribution we use on the CI:
 git clone https://github.com/confluentinc/librdkafka
 cd librdkafka
 git checkout v2.4.0
-sudo ./configure --install-deps --prefix=/usr
+sudo ./configure --install-deps --prefix=/usr --enable-ssl
 make
 sudo make install
 
