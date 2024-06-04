ecc70b559797dfc9d680523e8dbe3787f1d91988
steveny91 <steven.yuen@datadoghq.com>
2024-06-04 15:12:54 -0400

    enable sasl

--- .ddev/ci/scripts/kafka_consumer/linux/32_install_kerberos.sh
+++ .ddev/ci/scripts/kafka_consumer/linux/32_install_kerberos.sh
@@ -3,7 +3,7 @@
 set -ex
 
 sudo apt update
-sudo apt install -y --no-install-recommends build-essential libkrb5-dev wget software-properties-common lsb-release gcc make python3 python3-pip python3-dev libsasl2-modules-gssapi-mit krb5-user
+sudo apt install -y --no-install-recommends build-essential libkrb5-dev wget software-properties-common lsb-release gcc make python3 python3-pip python3-dev libsasl2-modules-gssapi-mit krb5-user libsasl2-dev
 
 # Install librdkafka from source since no binaries are available for the distribution we use on the CI:
 git clone https://github.com/confluentinc/librdkafka
