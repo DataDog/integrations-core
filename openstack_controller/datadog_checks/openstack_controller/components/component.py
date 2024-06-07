5074574f9ee5573f263df52c90cc746e96a59a04
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2024-06-07 17:20:02 +0200

    Add new handled exceptions

--- openstack_controller/datadog_checks/openstack_controller/components/component.py
+++ openstack_controller/datadog_checks/openstack_controller/components/component.py
@@ -6,9 +6,10 @@ import inspect
 from enum import Enum, unique
 from functools import wraps
 
-from requests.exceptions import HTTPError
+import requests
 
 from datadog_checks.base import AgentCheck
+from datadog_checks.openstack_controller.api.catalog import CatalogEndPointFailure
 
 
 def argument_value(arg_name, func, *args, **kwargs):
@@ -75,10 +76,12 @@ class Component:
                         tags = argument_value('tags', func, *args, **kwargs)
                         self.check.service_check(self.SERVICE_CHECK, AgentCheck.OK, tags=tags)
                     return result if result is not None else True
-                except HTTPError as e:
-                    self.check.log.error("HTTPError: %s", e.response)
+                except (requests.HTTPError, requests.ConnectionError, requests.Timeout, requests.RequestException) as e:
+                    self.check.log.debug("HTTPError: %s", e.response)
                     if report_service_check:
                         self.check.service_check(self.SERVICE_CHECK, AgentCheck.CRITICAL, tags=tags)
+                except CatalogEndPointFailure as e:
+                    self.check.log.debug("CatalogEndPointFailure: %s", e)
                 except Exception as e:
                     self.check.log.error("Exception: %s", e)
                 return None
