9430b3e9bb6802b394550df618b5d70a8d2f8069
Seth Samuel <seth.samuel@datadoghq.com>
2024-06-11 15:01:10 -0400

    Derp

--- datadog_checks_base/datadog_checks/base/utils/db/telemetry.py
+++ datadog_checks_base/datadog_checks/base/utils/db/telemetry.py
@@ -55,7 +55,7 @@ class Telemetry:
         :param force (_bool_): Send events even if less than FLUSH_INTERVAL has elapsed. Only used for testing.
         """
         elapsed_s = time() - self._last_flush 
-        self._log.warn("aq: telemetry flush after $d", elapsed_s)
+        self._log.warn("aq: telemetry flush after %d", elapsed_s)
         if not force and elapsed_s < FLUSH_INTERVAL:
             return
         for op in self._buffer.values():
@@ -72,3 +72,4 @@ class Telemetry:
             json_event = json.dumps(event, default=default_json_event_encoding)
             self._log.warn("aq: Reporting the following payload for telemetry collection: {}".format(json_event))
             submit(json_event)
+        self._last_flush = time()
