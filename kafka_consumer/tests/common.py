1d3e3bfb29213b72f4fc31458cc9bcdae10d2604
HadhemiDD <hadhemi.samaali@datadoghq.com>
2024-06-18 10:58:44 +0100

    commit 2

--- kafka_consumer/tests/common.py
+++ kafka_consumer/tests/common.py
@@ -103,9 +103,9 @@ def assert_check_kafka(aggregator, consumer_groups):
 
                 for mname in CONSUMER_METRICS:
                     aggregator.assert_metric(mname)
-                    tags = tags + [f"consumer_group:{name}"]
-                    if aggregator.assert_metric_has_tags(mname, tags):
-                        # Check for the prefix tag consumer_group_state
+                    t = tags + [f"consumer_group:{name}"]
+                    if aggregator.assert_metric_has_tags(mname, t):
+                        # Check for the tag consumer_group_state
                         aggregator.assert_metric_has_tag_prefix(mname, tag_prefix='consumer_group_state')
 
     aggregator.assert_all_metrics_covered()
