911e476bdfb0c8993d6f243f3a2fde2b26f72d53
Sarah Witt <sarah.witt@datadoghq.com>
2024-06-20 16:50:11 -0400

    Add service check assertion

--- fly_io/tests/test_unit.py
+++ fly_io/tests/test_unit.py
@@ -4,6 +4,7 @@
 
 import os
 
+from datadog_checks.base.constants import ServiceCheck
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.fly_io import FlyIoCheck
 
@@ -25,6 +26,8 @@ def test_check(dd_run_check, aggregator, instance, mock_http_response, caplog):
         aggregator.assert_metric(metric, at_least=1, hostname="708725eaa12297")
         aggregator.assert_metric(metric, at_least=1, hostname="20976671ha2292")
         aggregator.assert_metric(metric, at_least=1, hostname="119dc024cbf534")
+
+    aggregator.assert_service_check('fly_io.openmetrics.health', ServiceCheck.OK, count=1)
 
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
