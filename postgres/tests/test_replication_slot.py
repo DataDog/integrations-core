4791981279e05f69eb2f5febbc74a268684178fc
Seth Samuel <seth.samuel@datadoghq.com>
2024-06-20 12:08:51 -0400

    None check

--- postgres/tests/test_replication_slot.py
+++ postgres/tests/test_replication_slot.py
@@ -22,8 +22,8 @@ def test_physical_replication_slots(aggregator, integration_check, pg_instance):
             cur.execute("select pg_wal_lsn_diff(pg_current_wal_lsn(), redo_lsn) from pg_control_checkpoint();")
             redo_lsn_age = int(cur.fetchall()[0][0])
             cur.execute('select age(xmin) FROM pg_replication_slots;')
-            rows = cur.fetchall()
-            xmin_age_higher_bound += int(rows[0][0]) if rows is not None else 0
+            bound = cur.fetchall()[0][0]
+            xmin_age_higher_bound += int(bound) if bound is not None else 0
 
             cur.execute("select * from pg_create_physical_replication_slot('phys_1');")
             cur.execute("select * from pg_create_physical_replication_slot('phys_2', true);")
