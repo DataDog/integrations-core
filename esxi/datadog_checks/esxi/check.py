39c21bcfe2955635cea42ec1533f6083d249d2ac
Sarah Witt <sarah.witt@datadoghq.com>
2024-06-24 14:47:38 -0400

    long lived connection

--- esxi/datadog_checks/esxi/check.py
+++ esxi/datadog_checks/esxi/check.py
@@ -15,6 +15,7 @@ from six import iteritems
 
 from datadog_checks.base import AgentCheck, is_affirmative
 from datadog_checks.base.utils.common import to_string
+from datadog_checks.base.utils.time import get_timestamp
 
 from .constants import (
     ALL_RESOURCES,
@@ -74,6 +75,49 @@ class EsxiCheck(AgentCheck):
             else:
                 self.proxy_host = parsed_proxy.hostname
                 self.proxy_port = parsed_proxy.port
+        self.conn = None
+        self.content = None
+        self.last_connection_time = get_timestamp()
+
+        self.check_initializations.append(self.initiate_api_connection)
+
+    def initiate_api_connection(self):
+        try:
+            context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
+            context.check_hostname = True if self.ssl_verify else False
+            context.verify_mode = ssl.CERT_REQUIRED if self.ssl_verify else ssl.CERT_NONE
+
+            if self.ssl_capath:
+                context.load_verify_locations(cafile=None, capath=self.ssl_capath, cadata=None)
+            elif self.ssl_cafile:
+                context.load_verify_locations(cafile=self.ssl_cafile, capath=None, cadata=None)
+            else:
+                context.load_default_certs(ssl.Purpose.SERVER_AUTH)
+
+            create_connection_method = socket.create_connection
+            if self.proxy_host:
+                socket.create_connection = lambda address, timeout, source_address, **kwargs: create_connection(
+                    address, timeout, source_address, self.proxy_host, self.proxy_port
+                )
+
+            connection = connect.SmartConnect(host=self.host, user=self.username, pwd=self.password, sslContext=context)
+            socket.create_connection = create_connection_method
+
+            self.conn = connection
+            self.content = connection.content
+
+            if self.content.about.apiType != "HostAgent":
+                raise Exception(
+                    f"{self.host} is not an ESXi host; please set the `host` config option to an ESXi host "
+                    "or use the vSphere integration to collect data from the vCenter",
+                )
+
+            self.log.info("Connected to ESXi host %s: %s", self.host, self.content.about.fullName)
+            self.count("host.can_connect", 1, tags=self.tags)
+        except Exception as e:
+            self.log.exception("Cannot connect to ESXi host %s: %s", self.host, str(e))
+            self.count("host.can_connect", 0, tags=self.tags)
+            raise
 
     def _validate_excluded_host_tags(self, excluded_host_tags):
         valid_excluded_host_tags = []
@@ -353,43 +397,11 @@ class EsxiCheck(AgentCheck):
         self.set_metadata('version', f'{esxi_version}+{build_version}')
 
     def check(self, _):
-        try:
-            context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
-            context.check_hostname = True if self.ssl_verify else False
-            context.verify_mode = ssl.CERT_REQUIRED if self.ssl_verify else ssl.CERT_NONE
 
-            if self.ssl_capath:
-                context.load_verify_locations(cafile=None, capath=self.ssl_capath, cadata=None)
-            elif self.ssl_cafile:
-                context.load_verify_locations(cafile=self.ssl_cafile, capath=None, cadata=None)
-            else:
-                context.load_default_certs(ssl.Purpose.SERVER_AUTH)
-
-            create_connection_method = socket.create_connection
-            if self.proxy_host:
-                socket.create_connection = lambda address, timeout, source_address, **kwargs: create_connection(
-                    address, timeout, source_address, self.proxy_host, self.proxy_port
-                )
-
-            connection = connect.SmartConnect(host=self.host, user=self.username, pwd=self.password, sslContext=context)
-            socket.create_connection = create_connection_method
-
-            self.conn = connection
-            self.content = connection.content
-
-            if self.content.about.apiType != "HostAgent":
-                raise Exception(
-                    f"{self.host} is not an ESXi host; please set the `host` config option to an ESXi host "
-                    "or use the vSphere integration to collect data from the vCenter",
-                )
-
-            self.log.info("Connected to ESXi host %s: %s", self.host, self.content.about.fullName)
-            self.count("host.can_connect", 1, tags=self.tags)
-
-        except Exception as e:
-            self.log.exception("Cannot connect to ESXi host %s: %s", self.host, str(e))
-            self.count("host.can_connect", 0, tags=self.tags)
-            raise
+        if self.conn is None or self.content is None:
+            self.conn = None
+            self.content = None
+            self.initiate_api_connection()
 
         self.set_version_metadata()
         resources = self.get_resources()
