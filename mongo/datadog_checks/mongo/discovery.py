f351db78370844c73af6f05f07b0d25337d464aa
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-07-01 17:14:21 +0000

    fix license header

--- mongo/datadog_checks/mongo/discovery.py
+++ mongo/datadog_checks/mongo/discovery.py
@@ -1,3 +1,8 @@
+# (C) Datadog, Inc. 2024-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+
 from datadog_checks.base.utils.discovery import Discovery
 from datadog_checks.mongo.common import ReplicaSetDeployment
 
