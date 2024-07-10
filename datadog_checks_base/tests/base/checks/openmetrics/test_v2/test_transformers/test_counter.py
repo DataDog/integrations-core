888baed6b87b5d070dede6de67246ee22ab4e545
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-07-10 16:07:43 +0200

    Fix tests for openmetrics counter transformer

--- datadog_checks_base/tests/base/checks/openmetrics/test_v2/test_transformers/test_counter.py
+++ datadog_checks_base/tests/base/checks/openmetrics/test_v2/test_transformers/test_counter.py
@@ -11,52 +11,38 @@ pytestmark = [
 
 
 def test_basic(aggregator, dd_run_check, mock_http_response):
+    """
+    Metrics with type 'counter' in the raw payload get collected.
+
+    If they end in '_total' that suffix is dropped.
+    """
+
     mock_http_response(
         """
-        # HELP go_memstats_alloc_bytes_total Total number of bytes allocated, even if freed.
-        # TYPE go_memstats_alloc_bytes_total counter
-        go_memstats_alloc_bytes_total 9.339544592e+09
-        # HELP go_memstats_frees_total Total number of frees.
-        # TYPE go_memstats_frees_total counter
-        go_memstats_frees_total 1.28219257e+08
+        # HELP foo_total Example of '_total' getting dropped
+        # TYPE foo_total counter
+        foo_total 9.339544592e+09
+        # HELP bar_count Example of '_count' not getting dropped
+        # TYPE bar_count counter
+        bar_count 1.28219257e+08
+        # HELP baz Example that doesn't end in '_total' nor '_count'
+        # TYPE baz counter
+        baz 1.28219257e+08
         """
     )
     check = get_check({'metrics': ['.+']})
     dd_run_check(check)
 
     aggregator.assert_metric(
-        'test.go_memstats_alloc_bytes.count', 9339544592, metric_type=aggregator.MONOTONIC_COUNT, tags=['endpoint:test']
+        'test.foo.count', 9339544592, metric_type=aggregator.MONOTONIC_COUNT, tags=['endpoint:test']
     )
     aggregator.assert_metric(
-        'test.go_memstats_frees.count', 128219257, metric_type=aggregator.MONOTONIC_COUNT, tags=['endpoint:test']
+        'test.bar_count.count', 128219257, metric_type=aggregator.MONOTONIC_COUNT, tags=['endpoint:test']
+    )
+    aggregator.assert_metric(
+        'test.baz.count', 128219257, metric_type=aggregator.MONOTONIC_COUNT, tags=['endpoint:test']
     )
 
-    aggregator.assert_all_metrics_covered()
-
-
-def test_basic_incorrect_suffix(aggregator, dd_run_check, mock_http_response):
-    """
-    We won't collect counter metrics unless they end in '_total'.
-
-    We commonly see metrics that end in '_count' or ones that have no meaningful suffix at all.
-    Here we make sure that by default these won't be collected.
-    """
-
-    mock_http_response(
-        """
-        # HELP go_memstats_alloc_bytes_count Total number of bytes allocated, even if freed.
-        # TYPE go_memstats_alloc_bytes_count counter
-        go_memstats_alloc_bytes_total 9.339544592e+09
-        # HELP go_memstats_frees Total number of frees.
-        # TYPE go_memstats_frees counter
-        go_memstats_frees_total 1.28219257e+08
-        """
-    )
-    check = get_check({'metrics': ['.+']})
-    dd_run_check(check)
-
-    assert not aggregator.metrics('test.go_memstats_alloc_bytes.count')
-    assert not aggregator.metrics('test.go_memstats_frees.count')
     aggregator.assert_all_metrics_covered()
 
 
@@ -91,11 +77,11 @@ def test_untyped_incorrect_suffix(aggregator, dd_run_check, mock_http_response):
     mock_http_response(
         """
         # HELP go_memstats_alloc_bytes_count Total number of bytes allocated, even if freed.
-        # TYPE go_memstats_alloc_bytes_count counter
-        go_memstats_alloc_bytes_total 9.339544592e+09
+        # TYPE go_memstats_alloc_bytes_count untyped
+        go_memstats_alloc_bytes_count 9.339544592e+09
         # HELP go_memstats_frees Total number of frees.
-        # TYPE go_memstats_frees counter
-        go_memstats_frees_total 1.28219257e+08
+        # TYPE go_memstats_frees untyped
+        go_memstats_frees 1.28219257e+08
         """
     )
     check = get_check(
