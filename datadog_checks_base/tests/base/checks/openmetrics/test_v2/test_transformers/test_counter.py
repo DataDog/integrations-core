21f9ede1270f26bc8759c759f9637db0b3a50a8c
steveny91 <steven.yuen@datadoghq.com>
2024-07-16 17:04:11 -0400

    add type override examples

--- datadog_checks_base/tests/base/checks/openmetrics/test_v2/test_transformers/test_counter.py
+++ datadog_checks_base/tests/base/checks/openmetrics/test_v2/test_transformers/test_counter.py
@@ -46,61 +46,6 @@ def test_basic(aggregator, dd_run_check, mock_http_response):
     aggregator.assert_all_metrics_covered()
 
 
-def test_untyped_correct_suffix(aggregator, dd_run_check, mock_http_response):
-    """
-    We can force a metric that is 'untyped' in the raw payload to a counter as long as it ends in '_total'.
-    """
-
-    mock_http_response(
-        """
-        # HELP go_memstats_alloc_bytes_total Total number of bytes allocated, even if freed.
-        # TYPE go_memstats_alloc_bytes_total untyped
-        go_memstats_alloc_bytes_total 9.339544592e+09
-        """
-    )
-    check = get_check(
-        {'metrics': [{'go_memstats_alloc_bytes_total': {'name': 'go_memstats_alloc_bytes', 'type': 'counter'}}]}
-    )
-    dd_run_check(check)
-
-    aggregator.assert_metric(
-        'test.go_memstats_alloc_bytes.count', 9339544592, metric_type=aggregator.MONOTONIC_COUNT, tags=['endpoint:test']
-    )
-    aggregator.assert_all_metrics_covered()
-
-
-def test_untyped_incorrect_suffix(aggregator, dd_run_check, mock_http_response):
-    """
-    Forcing an untyped metric as a counter won't work without it ending in '_total'.
-    """
-
-    mock_http_response(
-        """
-        # HELP go_memstats_alloc_bytes_count Total number of bytes allocated, even if freed.
-        # TYPE go_memstats_alloc_bytes_count untyped
-        go_memstats_alloc_bytes_count 9.339544592e+09
-        # HELP go_memstats_frees Total number of frees.
-        # TYPE go_memstats_frees untyped
-        go_memstats_frees 1.28219257e+08
-        """
-    )
-    check = get_check(
-        {
-            'metrics': [
-                {
-                    'go_memstats_alloc_bytes': {'name': 'go_memstats_alloc_bytes', 'type': 'counter'},
-                    'go_memstats_frees': {'name': 'go_memstats_frees', 'type': 'counter'},
-                }
-            ]
-        }
-    )
-    dd_run_check(check)
-
-    assert not aggregator.metrics('test.go_memstats_alloc_bytes.count')
-    assert not aggregator.metrics('test.go_memstats_frees.count')
-    aggregator.assert_all_metrics_covered()
-
-
 def test_tags(aggregator, dd_run_check, mock_http_response):
     mock_http_response(
         """
---
+++ datadog_checks_base/tests/base/checks/openmetrics/test_v2/test_transformers/test_type_override.py
@@ -0,0 +1,83 @@
+# (C) Datadog, Inc. 2024-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import pytest
+
+from datadog_checks.dev.testing import requires_py3
+
+from ..utils import get_check
+
+pytestmark = [
+    requires_py3,
+]
+
+
+@pytest.mark.parametrize(
+    'metric_type',
+    [
+        ('counter'),
+        ('gauge'),
+    ],
+)
+def test_untyped_counter(aggregator, dd_run_check, mock_http_response, metric_type):
+    """
+    When we encounter an untyped metric, we can force the agent to collect it as a specific type.
+    In this scenario, the agent will ignore the metadata lines # HELP and # TYPE and will instead
+    only look at the metric sample name. Below are some of the scenarios that we might encounter:
+    1. The metricset and metric sample does not end in '_total' and is untyped.
+    2. The metricset and metric sample does end in '_total' and is untyped.
+    3. The metricset  does not end in '_total' and metric sample end in '_total' and is untyped.
+    4. The metricset and metric samples don't match
+    5. The metricset and metric samples are a non counter/gauge type
+
+    The test will show how these can be configured to be collected as gauge or counters. The metric sample just
+    has to match what the agent is looking for. The agent will then collect the metric as a we config
+    provided type. Only testing for counter and gauge here because these are the only 2 types I see we can accurately
+    collect with overrides.
+    """
+
+    mock_http_response(
+        """
+        # HELP foo The metricset and metric sample does not end in '_total' and is untyped.
+        # TYPE foo untyped
+        foo 0
+        # HELP bar_total The metricset and metric sample does end in '_total' and is untyped.
+        # TYPE bar_total untyped
+        bar_total 1
+        # HELP baz The metricset  does not end in '_total' and metric sample end in '_total' and is untyped.
+        # TYPE baz untyped
+        baz_total 2
+        # HELP qux The metricset and metric samples don't match
+        # TYPE qux untyped
+        fiz 3
+        # HELP bux The metricset and metric samples are a non counter type
+        # TYPE bux histogram
+        bux 4
+        """
+    )
+    check = get_check(
+        {
+            'metrics': [
+                {
+                    'foo': {'name': 'foo', 'type': f'{metric_type}'},
+                    'bar_total': {'name': 'bar', 'type': f'{metric_type}'},
+                    'baz_total': {'name': 'baz', 'type': f'{metric_type}'},
+                    'fiz': {'name': 'fiz', 'type': f'{metric_type}'},
+                    'bux': {'name': 'bux', 'type': f'{metric_type}'},
+                }
+            ]
+        }
+    )
+    dd_run_check(check)
+
+    metrics = ["test.foo", "test.bar", "test.baz", "test.fiz", "test.bux"]
+
+    if metric_type == 'counter':
+        for metric in metrics:
+            # add the .count suffix to the metric name
+            aggregator.assert_metric(f'{metric}.count', metric_type=aggregator.MONOTONIC_COUNT, tags=['endpoint:test'])
+    else:
+        for metric in metrics:
+            aggregator.assert_metric(metric, metric_type=aggregator.GAUGE, tags=['endpoint:test'])
+
+    aggregator.assert_all_metrics_covered()
