5e14d0d598aa01a06abf717334d465177af04464
David Kirov <david.kirov@datadoghq.com>
2024-07-17 11:41:35 +0200

    Add comment

--- teamcity/datadog_checks/teamcity/teamcity_rest.py
+++ teamcity/datadog_checks/teamcity/teamcity_rest.py
@@ -240,13 +240,16 @@ class TeamCityRest(AgentCheck):
     def _collect_new_builds(self, project_id):
         last_build_id = self.bc_store.get_last_build_id(project_id, self.current_build_config)
         if not last_build_id:
-            self.log.debug('No builds for project {} and build config {}, checking again'.format(project_id, self.current_build_config))
+            # We want to handle the case of an unbuilt build config by checking for any last builds
+            self.log.debug(
+                'No builds for project %d and build config %d checking again', project_id, self.current_build_config
+            )
             ressource = "last_build"
-            options = {"project_id":project_id}
+            options = {"project_id": project_id}
         else:
             self.log.debug('Checking for new builds...')
             ressource = "new_builds"
-            options = {"since_build":project_id}
+            options = {"since_build": project_id}
         return get_response(self, ressource, build_conf=self.current_build_config, **options)
 
     def _get_build_config_type(self, build_config):
