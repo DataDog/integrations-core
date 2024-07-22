ddce70245cb9d5023c5f62bcba8394811bcb9952
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-07-22 22:45:48 +0000

    filter on plan events for operation samples

--- mongo/tests/test_dbm_operation_samples.py
+++ mongo/tests/test_dbm_operation_samples.py
@@ -28,14 +28,15 @@ def test_mongo_operation_samples_standalone(aggregator, instance_integration_clu
 
     # we will not assert the metrics, as they are already tested in test_integration.py
     # we will only assert the operation sample and activity events
-    dbm_samples = aggregator.get_event_platform_events("dbm-samples")
     dbm_activities = aggregator.get_event_platform_events("dbm-activity")
+    dbm_samples = aggregator.get_event_platform_events("dbm-samples")
+    plan_samples = [event for event in dbm_samples if event['dbm_type'] == 'plan']
 
     # assert samples
     with open(os.path.join(HERE, "results", "operation-samples-standalone.json"), 'r') as f:
         expected_samples = json.load(f)
-        assert len(dbm_samples) == len(expected_samples)
-        for i, sample in enumerate(dbm_samples):
+        assert len(plan_samples) == len(expected_samples)
+        for i, sample in enumerate(plan_samples):
             assert sample == expected_samples[i]
 
     # assert activities
@@ -58,13 +59,14 @@ def test_mongo_operation_samples_mongos(aggregator, instance_integration_cluster
     with mock_pymongo("mongos"):
         run_check_once(mongo_check, dd_run_check)
 
-    dbm_samples = aggregator.get_event_platform_events("dbm-samples")
     dbm_activities = aggregator.get_event_platform_events("dbm-activity")
+    dbm_samples = aggregator.get_event_platform_events("dbm-samples")
+    plan_samples = [event for event in dbm_samples if event['dbm_type'] == 'plan']
 
     with open(os.path.join(HERE, "results", "operation-samples-mongos.json"), 'r') as f:
         expected_samples = json.load(f)
-        assert len(dbm_samples) == len(expected_samples)
-        for i, sample in enumerate(dbm_samples):
+        assert len(plan_samples) == len(expected_samples)
+        for i, sample in enumerate(plan_samples):
             assert sample == expected_samples[i]
 
     # assert activities
