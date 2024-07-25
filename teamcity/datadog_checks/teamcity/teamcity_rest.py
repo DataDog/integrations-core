21ea58129abb0a62bf7da7aa50e9465bc9449c53
David Kirov <david.kirov@datadoghq.com>
2024-07-25 14:03:47 +0200

    Run lint

--- teamcity/datadog_checks/teamcity/teamcity_rest.py
+++ teamcity/datadog_checks/teamcity/teamcity_rest.py
@@ -2,8 +2,8 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from copy import deepcopy
-from requests.exceptions import HTTPError
 
+from requests.exceptions import HTTPError
 from six import PY2
 
 from datadog_checks.base import AgentCheck, ConfigurationError, is_affirmative
@@ -245,20 +245,18 @@ class TeamCityRest(AgentCheck):
             self.log.debug(
                 'No builds for project %d and build config %d, checking again', project_id, self.current_build_config
             )
-            ressource = "last_build"
+            resource = "last_build"
             options = {"project_id": project_id}
         else:
             self.log.debug('Checking for new builds...')
-            ressource = "new_builds"
+            resource = "new_builds"
             options = {"since_build": last_build_id}
         try:
-            new_builds = get_response(self, ressource, build_conf=self.current_build_config, **options)
+            new_builds = get_response(self, resource, build_conf=self.current_build_config, **options)
         except HTTPError:
             # In the case where a build config has been deleted, no new builds should be returned and it will be removed
             # from the list of all build configs in the next re-initialization
-            self.log.debug(
-                'Failed to retrieve new builds for build config %s', self.current_build_config
-            )
+            self.log.debug('Failed to retrieve new builds for build config %s', self.current_build_config)
             new_builds = {}
         return new_builds
 
