1b50f7365d78d9f5f3762dccb72479c63126e593
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2024-07-28 10:42:30 +0200

    add conditions to docker_run to fix flakiness

--- teleport/tests/conftest.py
+++ teleport/tests/conftest.py
@@ -5,10 +5,14 @@ import os
 
 import pytest
 
-from datadog_checks.dev import docker_run, get_here
-from datadog_checks.dev.conditions import CheckDockerLogs
+from datadog_checks.dev import docker_run, get_docker_hostname, get_here
+from datadog_checks.dev.conditions import CheckDockerLogs, CheckEndpoints
 
 from .common import INSTANCE, USE_TELEPORT_CADDY
+
+HOST = get_docker_hostname()
+
+URL = "http://{}".format(HOST)
 
 
 @pytest.fixture(scope="session")
@@ -16,14 +20,21 @@ def dd_environment():
     if USE_TELEPORT_CADDY:
         compose_file = os.path.join(get_here(), "docker", "caddy", "docker-compose.yaml")
         conditions = [
-            CheckDockerLogs(identifier="teleport-service", patterns=["server running"]),
+            CheckEndpoints(URL + ":3001/healthz", attempts=120),
         ]
         with docker_run(compose_file, conditions=conditions):
             instance = {"teleport_url": "http://127.0.0.1", "diag_port": "3001"}
             yield instance
     else:
         compose_file = os.path.join(get_here(), "docker", "teleport", "docker-compose.yaml")
-        with docker_run(compose_file, sleep=5):
+        with docker_run(
+            compose_file,
+            sleep=5,
+            conditions=[
+                CheckDockerLogs(identifier="teleport-service", patterns=["server running"]),
+                CheckEndpoints(URL + ":3000/healthz", attempts=120),
+            ],
+        ):
             yield INSTANCE
 
 
