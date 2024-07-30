e9fe49f628ef998a5dae62262ad2a1d49d889708
Sarah Witt <sarah.witt@datadoghq.com>
2024-07-30 09:38:48 -0700

    Update metric names

--- fly_io/datadog_checks/fly_io/metrics.py
+++ fly_io/datadog_checks/fly_io/metrics.py
@@ -3,10 +3,10 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
 FLY_EDGE_METRICS = {
-    'fly_edge_http_responses_count': {'name': 'edge.http_responses.count', 'type': 'counter'},
+    'fly_edge_http_responses_count': {'name': 'edge.http_responses', 'type': 'counter'},
     'fly_edge_http_response_time_seconds': {'name': 'edge.http_response_time', 'type': 'histogram'},
-    'fly_edge_tcp_connects_count': {'name': 'edge.tcp_connects.count', 'type': 'counter'},
-    'fly_edge_tcp_disconnects_count': {'name': 'edge.tcp_disconnects.count', 'type': 'counter'},
+    'fly_edge_tcp_connects_count': {'name': 'edge.tcp_connects', 'type': 'counter'},
+    'fly_edge_tcp_disconnects_count': {'name': 'edge.tcp_disconnects', 'type': 'counter'},
     'fly_edge_data_out': {'name': 'edge.data_out', 'type': 'counter'},
     'fly_edge_data_in': {'name': 'edge.data_in', 'type': 'counter'},
     'fly_edge_tls_handshake_errors': {'name': 'edge.tls_handshake_errors', 'type': 'counter'},
@@ -15,11 +15,11 @@ FLY_EDGE_METRICS = {
 
 FLY_APP_METRICS = {
     'fly_app_concurrency': {'name': 'app.concurrency', 'type': 'gauge'},
-    'fly_app_http_responses_count': {'name': 'app.http_responses.count', 'type': 'counter'},
+    'fly_app_http_responses_count': {'name': 'app.http_responses', 'type': 'counter'},
     'fly_app_http_response_time_seconds': {'name': 'app.http_response_time', 'type': 'histogram'},
     'fly_app_connect_time_seconds': {'name': 'app.connect_time', 'type': 'histogram'},
-    'fly_app_tcp_connects_count': {'name': 'app.tcp_connects.count', 'type': 'counter'},
-    'fly_app_tcp_disconnects_count': {'name': 'app.tcp_disconnects.count', 'type': 'counter'},
+    'fly_app_tcp_connects_count': {'name': 'app.tcp_connects', 'type': 'counter'},
+    'fly_app_tcp_disconnects_count': {'name': 'app.tcp_disconnects', 'type': 'counter'},
 }
 
 FLY_INSTANCE_METRICS = {
