53fa50c88549de1a369b704c4bbf60644e887e24
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-08-02 18:39:35 +0000

    fix test

--- postgres/tests/test_logical_replication.py
+++ postgres/tests/test_logical_replication.py
@@ -127,7 +127,7 @@ def test_stat_subscription(aggregator, integration_check, pg_replica_logical):
     for metric in _iterate_metric_name(STAT_SUBSCRIPTION_METRICS):
         assert_metric_at_least(
             aggregator,
-            metric,
+            'postgresql.{0}'.format(metric),
             lower_bound=0.001,
             tags=expected_subscription_tags,
             min_count=1,
