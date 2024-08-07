92215d9900971d6566096859e0e5fb896fadc42f
Jo-Ann Meunier <jojo.meunier@datadoghq.com>
2024-08-07 13:30:55 -0400

    always reference resolved_hostname property

--- sqlserver/datadog_checks/sqlserver/sqlserver.py
+++ sqlserver/datadog_checks/sqlserver/sqlserver.py
@@ -217,7 +217,7 @@ class SQLServer(AgentCheck):
         elif AWS_RDS_HOSTNAME_SUFFIX in self._resolved_hostname:
             # allow for detecting if the host is an RDS host, and emit
             # the resource properly even if the `aws` config is unset
-            self.tags.append("dd.internal.resource:aws_rds_instance:{}".format(self._resolved_hostname))
+            self.tags.append("dd.internal.resource:aws_rds_instance:{}".format(self.resolved_hostname))
         if self._config.cloud_metadata.get("azure") is not None:
             deployment_type = self._config.cloud_metadata.get("azure")["deployment_type"]
             name = self._config.cloud_metadata.get("azure")["name"]
@@ -226,7 +226,7 @@ class SQLServer(AgentCheck):
                 # azure sql databases have a special format, which is set for DBM
                 # customers in the resolved_hostname.
                 # If user is not DBM customer, the resource_name should just be set to the `name`
-                db_instance = self._resolved_hostname
+                db_instance = self.resolved_hostname
             # some `deployment_type`s map to multiple `resource_type`s
             resource_types = AZURE_DEPLOYMENT_TYPE_TO_RESOURCE_TYPES.get(deployment_type).split(",")
             for r_type in resource_types:
@@ -237,7 +237,7 @@ class SQLServer(AgentCheck):
         # finally, emit a `database_instance` resource for this instance
         self.tags.append(
             "dd.internal.resource:database_instance:{}".format(
-                self._resolved_hostname,
+                self.resolved_hostname,
             )
         )
 
@@ -278,7 +278,7 @@ class SQLServer(AgentCheck):
         # ensure _resolved_hostname is never None when referenced by other parts
         # of this check. If it falls back to None, this will result in gaps in data
         if self._resolved_hostname is None:
-            return self.set_resolved_hostname()
+            self.set_resolved_hostname()
         return self._resolved_hostname
 
     def load_static_information(self):
