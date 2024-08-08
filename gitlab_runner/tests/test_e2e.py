6ad788b9997843dcd33c687f29459a14fdf06493
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-08-08 14:30:25 +0200

    Retry gitlab runner test if we get transient 502 response

--- gitlab_runner/tests/test_e2e.py
+++ gitlab_runner/tests/test_e2e.py
@@ -2,12 +2,31 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
+from flaky import flaky
+
 from datadog_checks.gitlab_runner import GitlabRunnerCheck
 
 from .common import CONFIG, CUSTOM_TAGS, GITLAB_RUNNER_TAGS
 
 
+def _rerun_on_502_response(err, name, test, plugin):
+    """
+    Gitlab randomly returns 502 sometimes even if it's up and running fine.
+
+    In this case we just want to retry the request without waiting.
+    """
+
+    return 'Http status code 502 on url' in str(err)
+
+
+@flaky(max_runs=5, rerun_filter=_rerun_on_502_response)
 def test_e2e(dd_agent_check):
+    """
+    Because this test's flakiness is very specific we still run it in master CI.
+
+    We retry it to get past the transient 502 response.
+    """
+
     aggregator = dd_agent_check(CONFIG)
     aggregator.assert_service_check(
         GitlabRunnerCheck.MASTER_SERVICE_CHECK_NAME, status=GitlabRunnerCheck.OK, tags=GITLAB_RUNNER_TAGS + CUSTOM_TAGS
