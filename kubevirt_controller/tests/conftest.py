f395e09d2d3c730292947a4d419154dbcd6673b4
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2024-08-09 15:10:29 +0200

    add e2e tests

--- kubevirt_controller/tests/conftest.py
+++ kubevirt_controller/tests/conftest.py
@@ -2,18 +2,86 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import os
+import time
+from contextlib import ExitStack
 
 import pytest
+import yaml
 
-from datadog_checks.dev import get_here
+from datadog_checks.dev import get_here, run_command
 from datadog_checks.dev.http import MockResponse
+from datadog_checks.dev.kind import kind_run
+from datadog_checks.dev.kube_port_forward import port_forward
 
 HERE = get_here()
 
 
+KUBEVIRT_VERSION = "v1.2.2"
+
+
+def setup_kubevirt():
+    # deploy the KubeVirt operator
+    run_command(["kubectl", "create", "-f", os.path.join(HERE, "kind", "kubevirt-operator.yaml")])
+
+    # deploy the KubeVirt Custom Resource Definitions
+    run_command(["kubectl", "create", "-f", os.path.join(HERE, "kind", "kubevirt-cr.yaml")])
+
+    # enable nested virtualization
+    run_command(
+        [
+            "kubectl",
+            "-n",
+            "kubevirt",
+            "patch",
+            "kubevirt",
+            "kubevirt",
+            "--type=merge",
+            "--patch",
+            '{"spec":{"configuration":{"developerConfiguration":{"useEmulation":true}}}}',
+        ]
+    )
+    time.sleep(30)
+
+    # wait for kubevirt deployment
+    run_command(
+        [
+            "kubectl",
+            "wait",
+            "kubevirt.kubevirt.io/kubevirt",
+            "-n",
+            "kubevirt",
+            "--for=jsonpath={.status.phase}=Deployed",
+            "--timeout=2m",
+        ]
+    )
+
+    time.sleep(30)
+
+    # deploy a VirtualMachine instance
+    run_command(["kubectl", "apply", "-f", os.path.join(HERE, "kind", "vm.yaml")])
+    run_command(["kubectl", "patch", "virtualmachine", "testvm", "--type", "merge", "-p", '{"spec":{"running":true}}'])
+
+
 @pytest.fixture(scope="session")
 def dd_environment():
-    yield
+    with kind_run(conditions=[setup_kubevirt], sleep=10) as kubeconfig, ExitStack() as stack:
+        kubeconfig_content = None
+
+        with open(kubeconfig, "r") as f:
+            kubeconfig_content = yaml.safe_load(f)
+
+        instance = {}
+
+        host, port = stack.enter_context(port_forward(kubeconfig, "kubevirt", 443, "service", "virt-api"))
+
+        instance["kubevirt_controller_metrics_endpoint"] = f"https://{host}:{port}/metrics"
+        instance["kubevirt_controller_healthz_endpoint"] = f"https://{host}:{port}/healthz"
+        instance["kube_cluster_name"] = "test-cluster-e2e"
+        instance["kube_namespace"] = "kubevirt"
+        instance["kube_pod_name"] = "virt-controller-some-id"
+        instance["kube_config_dict"] = kubeconfig_content
+
+        yield {"instances": [instance]}
 
 
 @pytest.fixture
