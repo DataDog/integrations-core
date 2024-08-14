f80891d047b406154deeeb78a62b792699def35f
Natasha Dada <natasha.dada@datadoghq.com>
2024-08-14 11:34:25 -0400

    syntax

--- datadog_checks_base/datadog_checks/base/utils/db/utils.py
+++ datadog_checks_base/datadog_checks/base/utils/db/utils.py
@@ -195,7 +195,7 @@ def get_agent_host_tags():
     try:
         tags_dict = json.loads(host_tags) or {}
     except Exception as e:
-        raise ValueError(f"Failed to parse tags from the agent host: {host_tags}. Error: {str(e)}")
+        raise ValueError(f"Failed to parse tags from the agent host: {host_tags}. Error: {e}.")
     for key, value in tags_dict.items():
         if isinstance(value, list):
             result.extend(value)
