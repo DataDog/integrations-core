f7218899e96531116bd4828d1e5837fd1ede2729
Laura Hampton <laura.hampton@datadoghq.com>
2024-08-27 15:53:39 -0700

    added metrics.py

---
+++ appgate_sdp/datadog_checks/appgate_sdp/metrics.py
@@ -0,0 +1,14 @@
+'''
+apn = appliance
+
+ctr = Controller
+
+gw = Gateway
+
+ptl = Portal
+'''
+METRICS_MAP={
+    'apn_cpu_usage_percent': 'appliance.cpu.usage.percent',
+    'apn_disk': 'appliance.disk',
+    'ctr_client_authentication': 'controller.client.authentication'
+}
