83cd1a70868855648c6a1cdcec76c973c4772f18
Kyle-Neale <kyle.neale@datadoghq.com>
2024-08-30 10:09:26 -0400

    change asyncore references to asyncio

--- snmp/datadog_checks/snmp/commands.py
+++ snmp/datadog_checks/snmp/commands.py
@@ -6,7 +6,7 @@ from typing import Any, Dict, Generator  # noqa: F401
 from pyasn1.type.univ import Null
 from pysnmp import hlapi  # noqa: F401
 from pysnmp.entity.rfc3413 import cmdgen
-from pysnmp.hlapi.asyncore.cmdgen import vbProcessor
+from pysnmp.hlapi.asyncio.cmdgen import vbProcessor
 from pysnmp.proto import errind
 from pysnmp.proto.rfc1905 import endOfMibView
 from pysnmp.smi.rfc1902 import ObjectIdentity, ObjectType
