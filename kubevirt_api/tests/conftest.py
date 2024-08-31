40d0733c9e47be2b5c757ad524783cc641c69046
Noueman Khalikine <noueman.khalikine@datadoghq.com>
2024-08-31 14:15:12 +0200

    remove time.sleep from conftest dev env setup

--- kubevirt_api/tests/conftest.py
+++ kubevirt_api/tests/conftest.py
@@ -2,7 +2,6 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import os
-import time
 from contextlib import ExitStack
 
 import pytest
@@ -24,6 +23,19 @@ def setup_kubevirt():
     # deploy the KubeVirt Custom Resource Definitions
     run_command(["kubectl", "create", "-f", os.path.join(HERE, "kind", "kubevirt-cr.yaml")])
 
+    # wait for kubevirt deployment
+    run_command(
+        [
+            "kubectl",
+            "wait",
+            "kubevirt.kubevirt.io/kubevirt",
+            "-n",
+            "kubevirt",
+            "--for=jsonpath={.status.phase}=Deployed",
+            "--timeout=2m",
+        ]
+    )
+
     # enable nested virtualization
     run_command(
         [
@@ -38,22 +50,6 @@ def setup_kubevirt():
             '{"spec":{"configuration":{"developerConfiguration":{"useEmulation":true}}}}',
         ]
     )
-    time.sleep(30)
-
-    # wait for kubevirt deployment
-    run_command(
-        [
-            "kubectl",
-            "wait",
-            "kubevirt.kubevirt.io/kubevirt",
-            "-n",
-            "kubevirt",
-            "--for=jsonpath={.status.phase}=Deployed",
-            "--timeout=2m",
-        ]
-    )
-
-    time.sleep(30)
 
     # deploy a VirtualMachine instance
     run_command(["kubectl", "apply", "-f", os.path.join(HERE, "kind", "vm.yaml")])
