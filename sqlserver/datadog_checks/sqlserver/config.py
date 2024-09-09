08b14306f44b9d8c3fa800d4aed6859522075c9e
Natasha Dada <natasha.dada@datadoghq.com>
2024-09-09 15:29:09 -0400

    lint

--- sqlserver/datadog_checks/sqlserver/config.py
+++ sqlserver/datadog_checks/sqlserver/config.py
@@ -138,7 +138,7 @@ class SQLServerConfig:
         if custom_tags is None:
             tags = []
         else:
-            tags = custom_tags # list(set(custom_tags))
+            tags = custom_tags  # list(set(custom_tags))
 
         if propagate_agent_tags:
             try:
