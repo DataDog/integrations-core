b93b5545d526259f20e6163d530e910f03291044
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-09-20 21:26:28 +0200

    Remove more esoteric uses of 'six'

--- directory/datadog_checks/directory/traverse.py
+++ directory/datadog_checks/directory/traverse.py
@@ -1,18 +1,13 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-import platform
-import sys
-
-import six
-
 try:
     from os import scandir
 except ImportError:
     from scandir import scandir
 
 
-def _walk(top, onerror=None, followlinks=False):
+def walk(top, onerror=None, followlinks=False):
     """A simplified and modified version of stdlib's `os.walk` that yields the
     `os.DirEntry` objects that `scandir` produces during traversal instead of paths as
     strings.
@@ -72,16 +67,3 @@ def _walk(top, onerror=None, followlinks=False):
     for dir_entry in dirs:
         for entry in walk(dir_entry.path, onerror, followlinks):
             yield entry
-
-
-if six.PY3 or platform.system() != 'Windows':
-    walk = _walk
-else:
-    # Fix for broken unicode handling on Windows on Python 2.x, see:
-    # https://github.com/benhoyt/scandir/issues/54
-    file_system_encoding = sys.getfilesystemencoding()
-
-    def walk(top, onerror, followlinks):
-        if isinstance(top, bytes):
-            top = top.decode(file_system_encoding)
-        return _walk(top, onerror, followlinks)
