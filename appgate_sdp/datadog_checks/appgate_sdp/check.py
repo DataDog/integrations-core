8b6e9222fd577ed643336d6ee50d71c99893ec2d
Kyle-Neale <kyle.neale@datadoghq.com>
2024-09-23 13:16:27 +0200

    ddev validate ci and some fixes

--- appgate_sdp/datadog_checks/appgate_sdp/check.py
+++ appgate_sdp/datadog_checks/appgate_sdp/check.py
@@ -8,12 +8,12 @@ from datadog_checks.base import OpenMetricsBaseCheckV2
 from .metrics import METRIC_MAP
 
 
-class AppgateSdpCheck(OpenMetricsBaseCheckV2):
+class AppgateSDPCheck(OpenMetricsBaseCheckV2):
 
-    __NAMESPACE__ = 'appgate'
+    __NAMESPACE__ = 'appgate_sdp'
 
     def __init__(self, name, init_config, instances):
-        super(AppgateSdpCheck, self).__init__(name, init_config, instances)
+        super(AppgateSDPCheck, self).__init__(name, init_config, instances)
 
     def get_default_config(self):
         return {
--- appgate_sdp/tests/test_unit.py
+++ appgate_sdp/tests/test_unit.py
@@ -4,7 +4,7 @@
 
 from typing import Any, Callable, Dict  # noqa: F401
 
-from datadog_checks.appgate_sdp import AppgateSdpCheck
+from datadog_checks.appgate_sdp import AppgateSDPCheck
 from datadog_checks.base import AgentCheck  # noqa: F401
 from datadog_checks.base.stubs.aggregator import AggregatorStub  # noqa: F401
 from datadog_checks.dev.utils import get_metadata_metrics
@@ -12,7 +12,7 @@ from datadog_checks.dev.utils import get_metadata_metrics
 
 def test_check(dd_run_check, aggregator, instance):
     # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
-    check = AppgateSdpCheck('appgate_sdp', {}, [instance])
+    check = AppgateSDPCheck('appgate_sdp', {}, [instance])
     dd_run_check(check)
 
     aggregator.assert_all_metrics_covered()
@@ -21,6 +21,6 @@ def test_check(dd_run_check, aggregator, instance):
 
 def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance):
     # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
-    check = AppgateSdpCheck('appgate_sdp', {}, [instance])
+    check = AppgateSDPCheck('appgate_sdp', {}, [instance])
     dd_run_check(check)
-    aggregator.assert_service_check('appgate_sdp.can_connect', AppgateSdpCheck.CRITICAL)
+    aggregator.assert_service_check('appgate_sdp.can_connect', AppgateSDPCheck.CRITICAL)
