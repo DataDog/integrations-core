bff0b1aa08575ff725a7c780923e7b109a36a0a7
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-09-26 18:46:54 +0000

    update test image

--- sqlserver/tests/compose-ha/sql/db-init.sh
+++ sqlserver/tests/compose-ha/sql/db-init.sh
@@ -6,6 +6,20 @@ sleep ${SLEEP_TIME}
 
 echo "#######    running set up script ${SQL_SCRIPT}   #######"
 
+# Define both potential sqlcmd paths
+SQLCMD_PATH_18="/opt/mssql-tools18/bin/sqlcmd"
+SQLCMD_PATH="/opt/mssql-tools/bin/sqlcmd"
+
+# Check if sqlcmd exists in the first path
+if [ -x "$SQLCMD_PATH_18" ]; then
+    SQLCMD_EXEC="$SQLCMD_PATH_18"
+elif [ -x "$SQLCMD_PATH" ]; then
+    SQLCMD_EXEC="$SQLCMD_PATH"
+else
+    echo "sqlcmd not found in either path."
+    exit 1
+fi
+
 #run the setup script to create the DB and the schema in the DB
 #if this is the primary node, remove the certificate files.
 #if docker containers are stopped, but volumes are not removed, this certificate will be persisted
@@ -16,6 +30,6 @@ then
 fi
 
 #use the SA password from the environment variable
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i $SQL_SCRIPT
+$SQLCMD_EXEC -C -N -S localhost -U sa -P $SA_PASSWORD -d master -i $SQL_SCRIPT
 
 echo "#######      AOAG script execution completed     #######"
