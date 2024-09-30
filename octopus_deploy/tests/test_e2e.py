21e8ff80f5f187ef88c4ea9607e217cfcf52b5ad
Sarah Witt <sarah.witt@datadoghq.com>
2024-09-30 15:30:54 -0400

    lint

--- octopus_deploy/tests/test_e2e.py
+++ octopus_deploy/tests/test_e2e.py
@@ -3,6 +3,7 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
 
+
 @pytest.mark.e2e
 def test_e2e(dd_agent_check, instance):
     aggregator = dd_agent_check(instance, rate=True)
