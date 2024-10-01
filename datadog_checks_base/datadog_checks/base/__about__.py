c36aa5bdea67175b9c8fd582c5c198f25ce6e2e3
Kyle-Neale <kyle.neale@datadoghq.com>
2024-09-30 20:22:12 -0400

    other small fixes

--- datadog_checks_base/datadog_checks/base/__about__.py
+++ datadog_checks_base/datadog_checks/base/__about__.py
@@ -1,8 +1,4 @@
 # (C) Datadog, Inc. 2018-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-<<<<<<< HEAD
 __version__ = "37.0.0"
-=======
-__version__ = "36.16.0"
->>>>>>> 5f23e184b4 (Release `datadog_checks_base` on 7.58.x (#18730))
