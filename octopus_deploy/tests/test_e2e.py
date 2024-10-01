14e851add573394068589156db409a64f4b655ad
Sarah Witt <sarah.witt@datadoghq.com>
2024-10-01 14:53:42 -0400

    lint

--- octopus_deploy/tests/test_e2e.py
+++ octopus_deploy/tests/test_e2e.py
@@ -2,7 +2,9 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
+
 from .constants import ALL_METRICS
+
 
 @pytest.mark.e2e
 def test_e2e(dd_agent_check, instance):
@@ -11,4 +13,3 @@ def test_e2e(dd_agent_check, instance):
     aggregator.assert_metric('octopus_deploy.api.can_connect', 1, tags=['space_name:Default'])
     for metric in ALL_METRICS:
         aggregator.assert_metric(metric)
-
