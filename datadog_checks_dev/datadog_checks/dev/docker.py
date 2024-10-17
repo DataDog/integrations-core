06236da7439864cf953276962e515c87a016b1df
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2024-10-17 12:29:53 +0200

    remove -f

--- datadog_checks_dev/datadog_checks/dev/docker.py
+++ datadog_checks_dev/datadog_checks/dev/docker.py
@@ -247,7 +247,7 @@ class ComposeFileUp(LazyFunction):
             return run_command(self.command, check=True)
         except SubprocessError as e:
             compose_error = str(e)
-            log_command = ['docker', 'compose', '-f', self.compose_file, 'logs']
+            log_command = ['docker', 'compose', self.compose_file, 'logs']
             compose_logs = run_command(log_command, check=True)
             raise SubprocessError(f"compose error: {compose_error} |  compose logs: {compose_logs}")
 
