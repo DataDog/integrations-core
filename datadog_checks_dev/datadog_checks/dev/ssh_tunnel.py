6e200d46b8b4d76c507f25f717f37498594b90c1
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-10-18 13:05:58 +0200

    send signal to kill process

--- datadog_checks_dev/datadog_checks/dev/ssh_tunnel.py
+++ datadog_checks_dev/datadog_checks/dev/ssh_tunnel.py
@@ -4,6 +4,7 @@
 from __future__ import absolute_import
 
 import os
+import signal
 import subprocess
 from contextlib import contextmanager
 
@@ -139,5 +140,5 @@ class KillProcess(LazyFunction):
         with TempDir(self.temp_name) as temp_dir:
             with open(os.path.join(temp_dir, self.pid_file)) as pid_file:
                 pid = int(pid_file.read())
-                os.kill(pid)
+                os.kill(pid, signal.SIGKILL if os.name == 'posix' else signal.SIGTERM)
                 return 0
