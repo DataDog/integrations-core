f4e9a008f86664b9e1825ae3c6acd4d580b4f9c0
Zhengda Lu <zhengda.lu@datadoghq.com>
2024-10-28 14:18:43 +0000

    remove documentdb prefix for consistency

--- mongo/datadog_checks/mongo/config_models/instance.py
+++ mongo/datadog_checks/mongo/config_models/instance.py
@@ -25,8 +25,8 @@ class Aws(BaseModel):
         arbitrary_types_allowed=True,
         frozen=True,
     )
-    documentdb_cluster_identifier: Optional[str] = None
-    documentdb_instance_endpoint: Optional[str] = None
+    cluster_identifier: Optional[str] = None
+    instance_endpoint: Optional[str] = None
 
 
 class Field(BaseModel):
