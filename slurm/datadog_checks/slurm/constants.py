50329c63792e3934198228e9a6b24a6148e30a12
steveny91 <steven.yuen@datadoghq.com>
2024-10-31 10:17:48 -0400

    license headers

--- slurm/datadog_checks/slurm/constants.py
+++ slurm/datadog_checks/slurm/constants.py
@@ -1,3 +1,6 @@
+# (C) Datadog, Inc. 2024-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
 SINFO_PARTITION_PARAMS = ["-hO", "Partition:|,NodeList:|,CPUs:|,Available:|,Memory:|,Cluster:|,NodeAIOT:|,StateLong:"]
 SINFO_NODE_PARAMS = ["-hNO", "PartitionName:|,Available:|,NodeList:|,NodeAIOT:|,Memory:|,Cluster:"]
 SINFO_ADDITIONAL_NODE_PARAMS = "|,CPUsLoad:|,FreeMem:|,Disk:|,StateLong:|,Reason:|,features_act:|,Threads:"
