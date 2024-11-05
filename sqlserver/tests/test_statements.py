e6ab6ef9266a9081cce1f51639a58c4763422dde
Alex Weisberger <alex.weisberger@datadoghq.com>
2024-11-04 20:01:55 -0500

    Ad lookback_window param.

--- sqlserver/tests/test_statements.py
+++ sqlserver/tests/test_statements.py
@@ -1020,6 +1020,15 @@ def test_metrics_lookback_multiplier(instance_docker):
     check.statement_metrics._load_raw_query_metrics_rows(mock_cursor)
     mock_cursor.execute.assert_called_with(ANY, (6,))
 
+@pytest.mark.unit
+def test_metrics_lookback_window_config(instance_docker):
+    instance_docker['query_metrics'] = {'lookback_window': 86400}
+    check = SQLServer(CHECK_NAME, {}, [instance_docker])
+    _, mock_cursor = _mock_database_list()
+
+    check.statement_metrics._load_raw_query_metrics_rows(mock_cursor)
+    mock_cursor.execute.assert_called_with(ANY, (86400,))
+
 
 @pytest.mark.flaky
 @pytest.mark.integration
