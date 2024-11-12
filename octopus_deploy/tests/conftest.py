1a9f2e45b5f0a327c4119e5ac28984fec6d064a2
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2024-11-12 19:15:17 +0100

    fixed instance fixture

--- octopus_deploy/tests/conftest.py
+++ octopus_deploy/tests/conftest.py
@@ -35,7 +35,7 @@ def dd_environment():
 
 @pytest.fixture
 def instance():
-    return {'octopus_endpoint': 'http://localhost:80/api', 'space': 'Default'}
+    return INSTANCE
 
 
 def get_json_value_from_file(file_path):
