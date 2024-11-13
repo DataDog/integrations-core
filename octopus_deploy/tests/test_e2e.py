ba95a7c2ab221eba6e1095baf6a691f719070141
Jose Manuel Almaza <josemanuel.almaza@datadoghq.com>
2024-11-13 10:32:56 +0100

    added some e2e asserts

--- octopus_deploy/tests/test_e2e.py
+++ octopus_deploy/tests/test_e2e.py
@@ -11,5 +11,5 @@ def test_e2e(dd_agent_check, instance):
     aggregator = dd_agent_check(instance)
 
     aggregator.assert_metric('octopus_deploy.api.can_connect', 1, tags=[])
-    for metric in ALL_METRICS:
-        aggregator.assert_metric(metric)
+    aggregator.assert_no_duplicate_all()
+    aggregator.assert_all_metrics_covered()
