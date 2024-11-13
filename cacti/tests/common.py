bb57f3b1419c5f0fe50cfdbdf309e25eefbd73ae
Enrico Donnici <enrico.donnici@datadoghq.com>
2024-11-13 11:27:04 +0100

    use bash -c with export and commands in same line

--- cacti/tests/common.py
+++ cacti/tests/common.py
@@ -27,12 +27,9 @@ INSTANCE_INTEGRATION = {
 E2E_METADATA = {
     'start_commands': [
         'apt-get update',
-        "echo 'DEBIAN_FRONTEND=noninteractive' > .env",
-        "echo 'CC=x86_64-linux-gnu-gcc' >> .env",
-        "source .env",
+        "bash -c 'export DEBIAN_FRONTEND=noninteractive && apt-get install rrdtool git librrd-dev build-essential -y'",
         'apt-get install rrdtool git librrd-dev build-essential -y',
-        # 'export CC=aarch64-linux-gnu-gcc',
-        'pip install rrdtool',
+        "bash -c 'export CC=x86_64-linux-gnu-gcc && pip install rrdtool'",
         # 'git clone https://github.com/nubtron/python-rrdtool.git',
         # 'cd python-rrdtool',
         # 'python setup.py install',
