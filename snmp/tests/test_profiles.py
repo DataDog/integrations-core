d27d92ff71dfc6b66777e6bf0d20b62cf37d2641
Jean-Edouard Dupau <jeanedouard.dupau@datadoghq.com>
2024-11-18 18:08:41 +0100

    fix test cisco asa

--- snmp/tests/test_profiles.py
+++ snmp/tests/test_profiles.py
@@ -2141,12 +2141,14 @@ def assert_cisco_asa(aggregator, profile):
     rtt_indexes = [1, 7, 10, 13, 15, 18, 20]
     rtt_types = [22, 21, 17, 6, 20, 8, 16]
     rtt_states = [3, 1, 6, 4, 6, 1, 6]
+    rtt_senses = [13, 30, 2, 21, 19, 20, 27]
     rtt_gauges = ['rttMonLatestRttOperCompletionTime', 'rttMonLatestRttOperSense', 'rttMonCtrlOperTimeoutOccurred']
     for i in range(len(rtt_indexes)):
         tags = [
             "rtt_index:{}".format(rtt_indexes[i]),
             "rtt_type:{}".format(rtt_types[i]),
             "rtt_state:{}".format(rtt_states[i]),
+            "rtt_sense:{}".format(rtt_senses[i]),
         ] + common_tags
         for rtt in rtt_gauges:
             aggregator.assert_metric('snmp.{}'.format(rtt), metric_type=aggregator.GAUGE, tags=tags)
