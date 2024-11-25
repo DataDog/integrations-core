2e61b74a9bcb545fc58817de7d38397348c0c100
Enrico Donnici <enrico.donnici@datadoghq.com>
2024-11-25 10:16:21 +0100

    Clean up integrate-with-python.

--- .github/workflows/scripts/python-build/integrate-with-python.ps1
+++ .github/workflows/scripts/python-build/integrate-with-python.ps1
@@ -13,21 +13,6 @@ param (
 Set-StrictMode -Version 3
 $ErrorActionPreference = "Stop"
 
-$Msys2Bin = "$Msys2Path\usr\bin\bash.exe"
-
-if (!(Test-Path $Msys2Bin)) {
-    throw "MSYS2 not found at path: $Msys2Path"
-}
-
-function Invoke-Msys2 {
-    param (
-        [string] $Command
-    )
-    & $Msys2Bin -e -l -c $Command
-    if (!$?) { throw "Command failed: $Command" }
-
-}
-
 # Updates the OpenSSL version in get_externals.bats
 function Update-OpenSSLVersionInBatch {
     param (
