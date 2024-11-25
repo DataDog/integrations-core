ce6acac8364e63b6d996e7e7a3a96b4162da45ea
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2024-11-25 10:54:21 +0100

    Bump dependencies for checking and fixing code style

--- ddev/src/ddev/plugin/external/hatch/environment_collector.py
+++ ddev/src/ddev/plugin/external/hatch/environment_collector.py
@@ -144,7 +144,7 @@ class DatadogChecksEnvironmentCollector(EnvironmentCollectorInterface):
             },
             # We pin deps in order to make CI more stable/reliable.
             'dependencies': [
-                'black==24.2.0',
+                'black==24.10.0',
                 'ruff==0.3.3',
                 # Keep in sync with: /datadog_checks_base/pyproject.toml
                 'pydantic==2.7.3',
