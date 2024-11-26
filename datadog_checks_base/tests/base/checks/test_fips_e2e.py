a7bf8e2d2cd7af6b716dc0bd7db2ea24c142032f
David Kirov <david.kirov@datadoghq.com>
2024-11-26 14:11:10 +0100

    Fix typo

--- datadog_checks_base/tests/base/checks/test_fips_e2e.py
+++ datadog_checks_base/tests/base/checks/test_fips_e2e.py
@@ -15,7 +15,7 @@ from datadog_checks.dev import docker_run, get_here
 from datadog_checks.dev.conditions import WaitForPortListening
 
 HERE = get_here()
-EMBEDDED = os.path.join(HERE, '..', '..', 'fixtures', 'fips', 'embeded')
+EMBEDDED = os.path.join(HERE, '..', '..', 'fixtures', 'fips', 'embedded')
 FIPS_SERVER_PORT = 8443
 
 
