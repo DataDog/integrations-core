ad86924d93c3e1e5fa03074008224ec2de7645bb
HadhemiDD <hadhemi.samaali@datadoghq.com>
2024-12-04 13:03:44 +0100

    add tests

---
+++ duckdb/tests/common.py
@@ -0,0 +1,17 @@
+# (C) Datadog, Inc. 2024-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+
+from datadog_checks.dev import get_docker_hostname, get_here
+
+HERE = get_here()
+HOST = get_docker_hostname()
+PORT = 5000
+
+CONFIG = {
+    'host': HOST,
+    'port': PORT,
+    'database': 'data/duckdb.db',
+    'read_only': False,
+}
