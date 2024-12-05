96e0ca32ba2b53479bb0dfeb23c892b3be90d99c
mphanias <pmokrala@aerospike.com>
2024-12-05 13:19:29 +0530

    aerospike-agent-and-dashboards
    added set metrics to metadata to check

--- aerospike/tests/common.py
+++ aerospike/tests/common.py
@@ -162,12 +162,12 @@ TPS_METRICS = [
 ]
 
 LEGACY_SET_METRICS = [
-    'aerospike_sets_tombstones',
+    'tombstones',
     'aerospike_sets_memory_data_bytes',
     'aerospike_sets_truncate_lut',
     'aerospike_sets_objects',
     'aerospike_sets_stop_writes_count',
-    'aerospike_sets_disable_eviction',
+    'disable_eviction',
 ]
 
 SET_METRICS = ['enable_index', 'index_populating', 'sindexes']
