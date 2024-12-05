84649c28b8982eedfd806b96f2c0d180c7c8a900
HadhemiDD <hadhemi.samaali@datadoghq.com>
2024-12-05 17:43:51 +0100

    test

--- duckdb/tests/conftest.py
+++ duckdb/tests/conftest.py
@@ -2,13 +2,30 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
+import os
+from copy import deepcopy
 
+import duckdb
+from datadog_checks.dev import LazyFunction, docker_run
+
+from . import common
+
+class InitializeDB(LazyFunction):
+    def __init__(self, config):
+        self.conn_info = common.connection_options_from_config(config)
+
+    def __call__(self):
+        with duckdb.connect(**self.conn_info) as conn:
+            cur = conn.cursor()
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    yield
+    compose_file = os.path.join(common.HERE, 'docker', 'docker-compose.yaml')
+
+    with docker_run(compose_file, log_patterns=['Duckdb is now running'], conditions=[InitializeDB(common.CONFIG)]):
+        yield common.CONFIG
 
 
 @pytest.fixture
 def instance():
-    return {}
+    return deepcopy(common.CONFIG)
