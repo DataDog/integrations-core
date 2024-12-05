01f51018afdd5b2cf0a8112b691178fc3b4bb1ad
mphanias <pmokrala@aerospike.com>
2024-12-05 14:50:18 +0530

    aerospike-agent-and-dashboard
    removed unnecessary spaces

--- aerospike/tests/test_aerospike.py
+++ aerospike/tests/test_aerospike.py
@@ -127,7 +127,7 @@ def _test_check(aggregator):
             aggregator.assert_metric("aerospike.set.{}".format(metric))
     else:
         for metric in LEGACY_SET_METRICS:
-            print(" ---> ","aerospike.set.{}".format(metric))
+            print(" ---> ", "aerospike.set.{}".format(metric))
             aggregator.assert_metric("aerospike.set.{}".format(metric))
 
     for metric in INDEXES_METRICS:
