4a741873f5465f933427f6647ec51e53e0c2baad
David Kirov <david.kirov@datadoghq.com>
2024-12-06 15:39:09 +0100

    Make md5 tests pass

--- datadog_checks_base/tests/fips/test_fips.py
+++ datadog_checks_base/tests/fips/test_fips.py
@@ -10,6 +10,9 @@ import pytest
 
 from datadog_checks.base.utils.fips import enable_fips
 
+PATH_TO_OPENSSL_CONF = os.getenv("PATH_TO_OPENSSL_CONF")
+PATH_TO_OPENSSL_MODULES = os.getenv("PATH_TO_OPENSSL_MODULES")
+
 
 @pytest.fixture(scope="function")
 def clean_environment():
@@ -30,7 +33,7 @@ def test_ssl_md5_before_fips(clean_environment):
 def test_ssl_md5_after_fips(clean_environment):
     import ssl
 
-    enable_fips()
+    enable_fips(path_to_openssl_conf=PATH_TO_OPENSSL_CONF,path_to_openssl_modules=PATH_TO_OPENSSL_MODULES)
     with pytest.raises(ssl.SSLError, match='No cipher can be selected.'):
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         ctx.set_ciphers("MD5")
@@ -47,7 +50,7 @@ def test_cryptography_md5_after_fips(clean_environment):
     from cryptography.exceptions import InternalError
     from cryptography.hazmat.primitives import hashes
 
-    enable_fips()
+    enable_fips(path_to_openssl_conf=PATH_TO_OPENSSL_CONF,path_to_openssl_modules=PATH_TO_OPENSSL_MODULES)
     with pytest.raises(InternalError, match='Unknown OpenSSL error.'):
         hashes.Hash(hashes.MD5())
 
