78be68e9ab69dd5c3af1eb9634d3291404371766
Steven Yuen <steven.yuen@datadoghq.com>
2024-12-08 07:46:25 -0500

    Sy/fix flaky downloader test (#19220)
    * fix CI for failing downloader test
    
    * add one more exclude
    
    * tokumx and lint

--- datadog_checks_downloader/tests/test_downloader.py
+++ datadog_checks_downloader/tests/test_downloader.py
@@ -53,6 +53,9 @@ EXCLUDED_INTEGRATIONS = [
     "datadog-dd-cluster-agent",  # excluding this since actual integration is called `datadog-cluster-agent`
     "datadog-kubernetes",  # excluding this since `kubernetes` check is Agent v5 only
     "datadog-go-metro",  # excluding this since `go-metro` check is Agent v5 only
+    "datadog-agent-metrics",  # excluding this since `agent-metrics` check is Agent v5 only
+    "datadog-amazon-kafka",  # excluding this since `amazon-kafka` wasn't an official release
+    "datadog-tokumx",  # excluding this since `tokumx` was dropped in py3
 ]
 
 # Specific integration versions released for the last time by a revoked developer but not shipped anymore.
