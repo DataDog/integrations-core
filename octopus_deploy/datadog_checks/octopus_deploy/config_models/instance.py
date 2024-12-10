28835b7d15eeedb15f4fb23d3ff396d2b2a9bdbf
Sarah Witt <sarah.witt@datadoghq.com>
2024-12-10 16:35:48 -0500

    sync models

--- octopus_deploy/datadog_checks/octopus_deploy/config_models/instance.py
+++ octopus_deploy/datadog_checks/octopus_deploy/config_models/instance.py
@@ -37,7 +37,7 @@ class Environments(BaseModel):
     exclude: Optional[tuple[str, ...]] = None
     include: Optional[tuple[Union[str, MappingProxyType[str, Any]], ...]] = None
     interval: Optional[int] = None
-    limit: Optional[int] = Field(None, description='Maximum number of projects to be processed.\n')
+    limit: Optional[int] = Field(None, description='Maximum number of environments to be processed.\n')
 
 
 class MetricPatterns(BaseModel):
