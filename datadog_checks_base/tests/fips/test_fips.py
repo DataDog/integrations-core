285fa3b2314a3706c715f0658cab125efe8a14f1
David Kirov <david.kirov@datadoghq.com>
2024-12-20 21:52:58 +0100

    Merge integration tests and use monkeypatch in setup fixture

--- datadog_checks_base/tests/fips/test_fips.py
+++ datadog_checks_base/tests/fips/test_fips.py
@@ -1,9 +1,9 @@
 # (C) Datadog, Inc. 2024-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-import os
 from typing import Any  # noqa: F401
 
+import os
 import pytest
 
 from datadog_checks.base.utils.fips import enable_fips
@@ -13,14 +13,40 @@ PATH_TO_OPENSSL_MODULES = os.getenv("PATH_TO_OPENSSL_MODULES")
 
 
 @pytest.fixture(scope="function")
-def clean_environment():
-    os.environ["GOFIPS"] = "0"
-    os.environ["OPENSSL_CONF"] = ""
-    os.environ["OPENSSL_MODULES"] = ""
+def clean_environment(monkeypatch):
+    monkeypatch.setenv("GOFIPS", "0")
+    monkeypatch.setenv("OPENSSL_CONF", "")
+    monkeypatch.setenv("OPENSSL_MODULES", "")
     yield
 
 
+@pytest.mark.fips_off
+def test_ssl_md5_before_fips(clean_environment):
+    """
+    MD5 cipher should be available through ssl before enabling FIPS mode.
+    """
+    import ssl
+
+    ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    ctx.set_ciphers("MD5")
+    assert True
+
+
+@pytest.mark.fips_off
+def test_cryptography_md5_before_fips(clean_environment):
+    """
+    MD5 cipher should be available through cryptography before enabling FIPS mode.
+    """
+    from cryptography.hazmat.primitives import hashes
+
+    assert hashes.Hash(hashes.MD5())
+
+
+@pytest.mark.fips_on
 def test_ssl_md5_after_fips(clean_environment):
+    """
+    MD5 cipher should not be available through ssl after enabling FIPS mode.
+    """
     import ssl
 
     enable_fips(path_to_openssl_conf=PATH_TO_OPENSSL_CONF, path_to_openssl_modules=PATH_TO_OPENSSL_MODULES)
@@ -29,7 +55,11 @@ def test_ssl_md5_after_fips(clean_environment):
         ctx.set_ciphers("MD5")
 
 
+@pytest.mark.fips_on
 def test_cryptography_md5_after_fips(clean_environment):
+    """
+    MD5 cipher should not be available through cryptography after enabling FIPS mode.
+    """
     from cryptography.exceptions import InternalError
     from cryptography.hazmat.primitives import hashes
 
