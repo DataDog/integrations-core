0dea1a016bb8db4ffb1eb070a4b5808628732fdf
DhruvaPatel-crest <dhruva.patel@crestdata.ai>
2025-01-10 14:50:55 +0530

    Added e2e test files

---
+++ sonatype_nexus/tests/test_e2e.py
@@ -0,0 +1,18 @@
+# (C) Datadog, Inc. 2025-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+import pytest
+
+from .constants import E2E_METRICS
+
+
+@pytest.mark.e2e
+def test_e2e(dd_agent_check, instance):
+    aggregator = dd_agent_check(instance)
+
+    aggregator.assert_metric('sonatype_nexus.status.available_cpus_health', 1, tags=[{"sonatype_host": "localhost", "host":"http://localhost:8081"}])
+    for metric in E2E_METRICS:
+        aggregator.assert_metric(metric)
+    aggregator.assert_no_duplicate_all()
+    aggregator.assert_all_metrics_covered()
