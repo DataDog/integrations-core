ce7f4339884881e82f59be232fde69f16cd6282f
DhruvaPatel-crest <dhruva.patel@crestdata.ai>
2025-01-10 14:57:08 +0530

    solved linting error

--- sonatype_nexus/tests/test_e2e.py
+++ sonatype_nexus/tests/test_e2e.py
@@ -11,7 +11,11 @@ from .constants import E2E_METRICS
 def test_e2e(dd_agent_check, instance):
     aggregator = dd_agent_check(instance)
 
-    aggregator.assert_metric('sonatype_nexus.status.available_cpus_health', 1, tags=[{"sonatype_host": "localhost", "host":"http://localhost:8081"}])
+    aggregator.assert_metric(
+        'sonatype_nexus.status.available_cpus_health', 
+        1, 
+        tags=[{"sonatype_host": "localhost", "host":"http://localhost:8081"}]
+    )
     for metric in E2E_METRICS:
         aggregator.assert_metric(metric)
     aggregator.assert_no_duplicate_all()
