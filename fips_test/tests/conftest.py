b25a37b53c0dc2a0e854f650f1dfe432d1dd4368
David Kirov <david.kirov@datadoghq.com>
2025-01-15 15:17:27 +0100

    Add E2E tests

--- fips_test/tests/conftest.py
+++ fips_test/tests/conftest.py
@@ -1,14 +1,42 @@
 # (C) Datadog, Inc. 2025-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+
 import pytest
+
+from datadog_checks.dev import docker_run
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+CA_CERT = os.path.join(HERE, 'compose', 'ca.crt')
+CA_CERT_MOUNT_PATH = '/tmp/ca.crt'
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    yield
+    with docker_run(os.path.join(HERE, 'compose', 'docker-compose.yml'), build=True, sleep=20):
+        e2e_metadata = {'docker_volumes': ['{}:{}'.format(CA_CERT, CA_CERT_MOUNT_PATH)]}
+        yield instance_e2e_fips, instance_e2e_non_fips, e2e_metadata
 
 
-@pytest.fixture
-def instance():
-    return {}
+@pytest.fixture(scope='session')
+def instance_e2e_fips():
+    return {
+        'server': 'https://localhost',
+        'port': 8443,
+        'tls_ca_cert': CA_CERT_MOUNT_PATH,
+        'tls_verify': False,
+        'tls_validate_hostname': False,
+    }
+
+
+@pytest.fixture(scope='session')
+def instance_e2e_non_fips():
+    return {
+        'server': 'https://localhost',
+        'port': 9443,
+        'tls_ca_cert': CA_CERT_MOUNT_PATH,
+        'tls_verify': False,
+        'tls_validate_hostname': False,
+    }
---
+++ fips_test/tests/test_e2e.py
@@ -0,0 +1,40 @@
+# (C) Datadog, Inc. 2025-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+from typing import Any  # noqa: F401
+
+import pytest
+
+from datadog_checks.fips_test import FIPSTestCheck
+from datadog_checks.fips_test.const import (
+    SERVICE_CHECK_HTTP,
+    SERVICE_CHECK_RMI,
+    SERVICE_CHECK_SOCKET,
+    SERVICE_CHECK_SSH,
+)
+
+
+@pytest.mark.e2e
+@pytest.mark.fips_on
+def test_connection_fips(clean_fips_environment, dd_environment, dd_agent_check, instance_e2e_fips):
+    """
+    Connection to the FIPS servers after enabling FIPS mode should succeed.
+    """
+    aggregator = dd_agent_check(instance_e2e_fips)
+    aggregator.assert_service_check(SERVICE_CHECK_HTTP, status=FIPSTestCheck.OK, count=1)
+    aggregator.assert_service_check(SERVICE_CHECK_RMI, status=FIPSTestCheck.OK, count=1)
+    aggregator.assert_service_check(SERVICE_CHECK_SOCKET, status=FIPSTestCheck.OK, count=1)
+    aggregator.assert_service_check(SERVICE_CHECK_SSH, status=FIPSTestCheck.OK, count=1)
+
+
+@pytest.mark.e2e
+@pytest.mark.fips_on
+def test_connection_non_fips(clean_fips_environment, dd_environment, dd_agent_check, instance_e2e_non_fips):
+    """
+    Connection to the non-FIPS servers after enabling FIPS mode should fail.
+    """
+    aggregator = dd_agent_check(instance_e2e_non_fips)
+    aggregator.assert_service_check(SERVICE_CHECK_HTTP, status=FIPSTestCheck.CRITICAL, count=1)
+    aggregator.assert_service_check(SERVICE_CHECK_RMI, status=FIPSTestCheck.CRITICAL, count=1)
+    aggregator.assert_service_check(SERVICE_CHECK_SOCKET, status=FIPSTestCheck.CRITICAL, count=1)
+    aggregator.assert_service_check(SERVICE_CHECK_SSH, status=FIPSTestCheck.CRITICAL, count=1)
