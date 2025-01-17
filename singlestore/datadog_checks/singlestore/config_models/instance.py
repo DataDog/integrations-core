d96237f7b975f1c6efbc94d001cdb2aa1e98fab9
Steven Yuen <steven.yuen@datadoghq.com>
2025-01-16 21:12:13 -0400

    Sy/cipher templates (#19334)
    * add tls_ciphers to templates
    
    * changelog
    
    * update templates
    
    * fix http_check template
    
    * changelogs
    
    * fix models
    
    * bump dcd (#19408)
    
    (cherry picked from commit 7e99ee587f3ad637ff6b14b78fccc01a2d64b84b)

--- singlestore/datadog_checks/singlestore/config_models/instance.py
+++ singlestore/datadog_checks/singlestore/config_models/instance.py
@@ -63,6 +63,7 @@ class InstanceConfig(BaseModel):
     tags: Optional[tuple[str, ...]] = None
     tls_ca_cert: Optional[str] = None
     tls_cert: Optional[str] = None
+    tls_ciphers: Optional[tuple[str, ...]] = None
     tls_private_key: Optional[str] = None
     tls_private_key_password: Optional[str] = None
     tls_validate_hostname: Optional[bool] = None
