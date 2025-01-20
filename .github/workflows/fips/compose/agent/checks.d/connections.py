3fae3bfa8a6421b9cc23403865743cde994f1ef0
David Kirov <david.kirov@datadoghq.com>
2025-01-20 14:27:18 +0100

    Fix workflow

--- .github/workflows/fips/compose/agent/checks.d/connections.py
+++ .github/workflows/fips/compose/agent/checks.d/connections.py
@@ -3,4 +3,10 @@ from checks import AgentCheck
 
 class HelloCheck(AgentCheck):
     def check(self, instance):
-        self.gauge('hello.world', 1)
+        try:
+            self.http.get(instance.get('http_endpoint'))
+        except Exception as e:
+            self.gauge('http_status', 1)
+            print(f"Exception when trying to connect to {instance.get('http_endpoint')}: {e}")
+        else:
+            self.gauge('https_status', 0)
--- .github/workflows/fips/tests/test_connections.py
+++ .github/workflows/fips/tests/test_connections.py
@@ -1,2 +1,6 @@
+import subprocess
+
+
 def test_connections():
+    subprocess.run(["docker", "exec", "compose-agent-1", "agent", "check", "connections", "--json"], check=True)
     pass
