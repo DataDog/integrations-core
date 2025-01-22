ab21f4ff4bd0ed5fa47525787b84556c30281c54
David Kirov <david.kirov@datadoghq.com>
2025-01-22 17:42:25 +0100

    Add json logic to test

--- .github/workflows/fips/tests/test_connections.py
+++ .github/workflows/fips/tests/test_connections.py
@@ -1,6 +1,10 @@
 import subprocess
+import json
 
 
 def test_connections():
-    subprocess.run(["docker", "exec", "compose-agent-1", "agent", "check", "connections", "--json"], check=True)
-    pass
+    result = subprocess.run(["docker", "exec", "compose-agent-1", "agent", "check", "connections", "--json"], check=True, capture_output=True)
+    check_json = json.loads(result.stdout)
+    submitted_metrics = check_json[0]['aggregator']['metrics']
+    for metric_json in submitted_metrics:
+        assert metric_json["points"][-1][-1] == 0
