f3c51b712c019cabf5236a9937cb44fbd0c5ea27
David Kirov <david.kirov@datadoghq.com>
2025-01-24 15:42:00 +0100

    Handle message header for check --json

--- .github/workflows/fips/tests/test_connections.py
+++ .github/workflows/fips/tests/test_connections.py
@@ -19,7 +19,12 @@ def _parse_json(payload):
     Convert agent check json to dict of metric_name: value.
     """
     parsed_json = {}
-    for instance, suffix in zip(payload, ("", "_fips")):
+    # The payload may start with a message like "Multiple instances found". 
+    # Therefore, we need to split the payload and load the json objects.
+    if not payload.startswith(b'['):
+        payload = b'['+payload.split(b'[', maxsplit=1)[1]
+    check_json = json.loads(payload)
+    for instance, suffix in zip(check_json, ("", "_fips")):
         submitted_metrics = instance['aggregator']['metrics']
         for metric_json in submitted_metrics:
             parsed_json[metric_json["metric"]+suffix] = int(metric_json["points"][-1][-1])
@@ -28,7 +33,6 @@ def _parse_json(payload):
 
 def test_connections():
     result = subprocess.run(["docker", "exec", "compose-agent-1", "agent", "check", "connections", "--json"], check=True, capture_output=True)
-    check_json = json.loads(result.stdout)
-    parsed_json = _parse_json(check_json)
+    parsed_json = _parse_json(result.stdout)
 
     assert parsed_json == FIPS_AGENT if AGENT_FIPS_MODE else REGULAR_AGENT
