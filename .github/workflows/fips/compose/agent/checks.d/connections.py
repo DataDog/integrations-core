9f1ea219285c909ef466d2b280d93f91b7a4f895
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2025-01-24 11:50:07 +0100

    wip: tests and metrics

--- .github/workflows/fips/compose/agent/checks.d/connections.py
+++ .github/workflows/fips/compose/agent/checks.d/connections.py
@@ -6,8 +6,8 @@ class HelloCheck(AgentCheck):
         try:
             self.http.get(instance.get('http_endpoint'), verify=False)
         except Exception as e:
-            self.gauge('http_status', 1)
+            self.gauge('http_status', 0)
             self.log.warn(f"Exception when trying to connect to {instance.get('http_endpoint')}: {e}")
             raise RuntimeError(f"Exception when trying to connect to {instance.get('http_endpoint')}: {e}")
         else:
-            self.gauge('https_status', 0)
+            self.gauge('https_status', 1)
--- .github/workflows/fips/tests/test_connections.py
+++ .github/workflows/fips/tests/test_connections.py
@@ -2,9 +2,27 @@ import subprocess
 import json
 
 
-def test_connections():
+REGULAR_AGENT = {
+    'http_status': 1,
+    'http_status_fips_server': 1,
+}
+FIPS_AGENT = {
+    'http_status': 0,
+    'http_status_fips_server': 1,
+}
+
+
+def _parse_json(payload):
+    """
+    Convert agent check json to dict of metric_name: value.
+    """
+
+
+def test_connections_regular_agent():
     result = subprocess.run(["docker", "exec", "compose-agent-1", "agent", "check", "connections", "--json"], check=True, capture_output=True)
     check_json = json.loads(result.stdout)
     submitted_metrics = check_json[0]['aggregator']['metrics']
     for metric_json in submitted_metrics:
         assert metric_json["points"][-1][-1] == 0
+
+    assert parsed_json == REGULAR_AGENT
