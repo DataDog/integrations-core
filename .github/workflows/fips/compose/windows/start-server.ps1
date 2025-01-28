ba502da9f829bcc5469409bbd41edc7283dc326a
David Kirov <david.kirov@datadoghq.com>
2025-01-28 12:03:10 +0100

    Fix windows compose file

--- .github/workflows/fips/compose/windows/start-server.ps1
+++ .github/workflows/fips/compose/windows/start-server.ps1
@@ -1,6 +1,6 @@
-if (!(Test-Path -Path "./ca.crt") -or !(Test-Path -Path "./ca.key")) {
+if (!(Test-Path -Path "C:/app/ca.crt") -or !(Test-Path -Path "C:/app/ca.key")) {
     Write-Host "Generating self-signed certificate..."
-    & openssl req -x509 -newkey rsa:2048 -keyout ca.key -out ca.crt -days 365 -nodes -subj "/CN=localhost"
+    & openssl req -x509 -newkey rsa:2048 -keyout C:/app/ca.key -out C:/app/ca.crt -days 365 -nodes -subj "/CN=localhost"
 }
 
 param(
@@ -10,8 +10,8 @@ param(
 Write-Host "Starting OpenSSL server on port 443 with cipher $Cipher..."
 & openssl s_server `
     -accept 443 `
-    -cert ca.crt `
-    -key ca.key `
+    -cert C:/app/ca.crt `
+    -key C:/app/ca.key `
     -cipher $Cipher `
     -no_tls1_3 `
     -WWW
