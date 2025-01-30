3b76aa849342e0466a3308c91402938025b7c969
Jennifer Chen <jennifer.chen@datadoghq.com>
2025-01-30 13:51:29 -0500

    Collect local fallback telemetry

--- datadog_cluster_agent/datadog_checks/datadog_cluster_agent/check.py
+++ datadog_cluster_agent/datadog_checks/datadog_cluster_agent/check.py
@@ -37,6 +37,8 @@ DEFAULT_METRICS = {
     'autoscaling_workload_vertical_scaling_received_requests': 'autoscaling.workload.vertical_scaling_received_requests',  # noqa: E501
     'autoscaling_workload_store_load_entities': 'autoscaling.workload.store_load_entities',
     'autoscaling_workload_store_job_queue_length': 'autoscaling.workload.store_job_queue_length',
+    'autoscaling_workload_local_horizontal_scaling_recommended_replicas': 'autoscaling.workload.local.horizontal_scaling_recommended_replicas',
+    'autoscaling_workload_local_horizontal_utilization_pct': 'autoscaling.workload.local.horizontal_utilization_pct',
     'aggregator__flush': 'aggregator.flush',
     'aggregator__processed': 'aggregator.processed',
     'api_requests': 'api_requests',
