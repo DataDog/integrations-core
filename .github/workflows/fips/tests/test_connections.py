34f86223f61305022aae5a776de841b1606c2871
David Kirov <david.kirov@datadoghq.com>
2025-02-03 17:29:42 +0100

    Fix windows compose

--- .github/workflows/fips/tests/test_connections.py
+++ .github/workflows/fips/tests/test_connections.py
@@ -6,7 +6,7 @@ REGULAR_AGENT = {
     'http_status_fips': 1,
     'ssh_status_fips': 1,
     'http_status': 1,
-    'ssh_status': 0,
+    'ssh_status': 0,  # paramiko does not support non FIPS ciphers OOTB
 }
 FIPS_AGENT = {
     'http_status_fips': 1,
