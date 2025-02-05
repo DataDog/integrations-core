0b2cab701ff8192d8de0dcb0e891ff08102d5d45
David Kirov <david.kirov@datadoghq.com>
2025-02-05 13:57:24 +0100

    Add workaround for Windows

--- .github/workflows/fips/tests/test_libraries.py
+++ .github/workflows/fips/tests/test_libraries.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 from conftest import AGENT_FIPS_MODE, parse_json
 
@@ -11,9 +12,10 @@ FIPS_AGENT = {
     'cryptography_status': 0,
 }
 
+WORKAROUND = ("-e", "OPENSSL_CONF=/opt/datadog-agent/embedded/ssl/openssl.cnf", "-e", "OPENSSL_MODULES=/opt/datadog-agent/embedded/lib/ossl-modules") if os.environ.get("RUNNER_OS") == "Linux" else ("-e", "OPENSSL_CONF=C:/Program Files/Datadog/Datadog Agent/embedded3/ssl/openssl.cnf", "-e", "OPENSSL_MODULES=C:/Program Files/Datadog/Datadog Agent/embedded3/lib/ossl-modules")
 
 def test_libraries():
-    result = subprocess.run(["docker", "exec", "-e", "OPENSSL_CONF=/opt/datadog-agent/embedded/ssl/openssl.cnf", "-e", "OPENSSL_MODULES=/opt/datadog-agent/embedded/lib/ossl-modules", "compose-agent-1", "agent", "check", "libraries", "--json"], check=True, capture_output=True)
+    result = subprocess.run(["docker", "exec", *WORKAROUND, "compose-agent-1", "agent", "check", "libraries", "--json"], check=True, capture_output=True)
     parsed_json = parse_json(result.stdout)
     expected_json = FIPS_AGENT if AGENT_FIPS_MODE else REGULAR_AGENT
 
