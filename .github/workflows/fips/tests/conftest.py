b43d49a7ad51be49f3d793ea1ab2ffc101ac0451
David Kirov <david.kirov@datadoghq.com>
2025-02-05 17:33:35 +0100

    Combine tests

--- .github/workflows/fips/tests/conftest.py
+++ .github/workflows/fips/tests/conftest.py
@@ -2,13 +2,31 @@ import json
 import os
 
 AGENT_FIPS_MODE = 1 if "fips" in os.getenv("AGENT_TYPE") else 0
+REGULAR_AGENT = set((
+    ('http_status_fips', 1),
+    ('ssh_status_fips', 1),
+    ('http_status', 1),
+    ('ssh_status', 0),  # paramiko does not support non FIPS ciphers OOTB
+    ('ssl_status', 1),
+    ('cryptography_status', 1),
+    ))
+FIPS_AGENT = set((
+    ('http_status_fips', 1),
+    ('ssh_status_fips', 1),
+    ('http_status', 0),
+    ('ssh_status', 0),
+    ('ssl_status', 0),
+    ('cryptography_status', 0),
+    ))
+
+WORKAROUND = ("-e", "OPENSSL_CONF=/opt/datadog-agent/embedded/ssl/openssl.cnf", "-e", "OPENSSL_MODULES=/opt/datadog-agent/embedded/lib/ossl-modules") if os.environ.get("RUNNER_OS") == "Linux" else ("-e", "OPENSSL_CONF=C:/Program Files/Datadog/Datadog Agent/embedded3/ssl/openssl.cnf", "-e", "OPENSSL_MODULES=C:/Program Files/Datadog/Datadog Agent/embedded3/lib/ossl-modules")
 
 
-def parse_json(payload):
+def parse_json(payload) -> set:
     """
-    Convert agent check json to dict of metric_name: value.
+    Convert agent check json to a set of (metric_name, value).
     """
-    parsed_json = {}
+    parsed_json = set()
     # The payload may start with a message like "Multiple instances found". 
     # Therefore, we need to split the payload and load the json objects.
     if not payload.startswith(b'['):
@@ -17,6 +35,6 @@ def parse_json(payload):
     # The suffixes depend on the order of the defined instances in the config.
     for instance, suffix in zip(check_json, ("", "_fips")):
         for metric_json in instance['aggregator']['metrics']:
-            parsed_json[metric_json["metric"]+suffix] = int(metric_json["points"][-1][-1])
+            parsed_json.add((metric_json["metric"]+suffix,int(metric_json["points"][-1][-1])))
     return parsed_json
 
---
+++ .github/workflows/fips/tests/test_all.py
@@ -0,0 +1,20 @@
+import subprocess
+
+from conftest import (AGENT_FIPS_MODE, FIPS_AGENT, REGULAR_AGENT, WORKAROUND,
+                      parse_json)
+
+
+def test_connections():
+    result = subprocess.run(["docker", "exec", "compose-agent-1", "agent", "check", "connections", "--json"], check=True, capture_output=True)
+    parsed_json = parse_json(result.stdout)
+    expected_json = FIPS_AGENT if AGENT_FIPS_MODE else REGULAR_AGENT
+
+    assert parsed_json == (parsed_json & expected_json)
+
+
+def test_libraries():
+    result = subprocess.run(["docker", "exec", *WORKAROUND, "compose-agent-1", "agent", "check", "libraries", "--json"], check=True, capture_output=True)
+    parsed_json = parse_json(result.stdout)
+    expected_json = FIPS_AGENT if AGENT_FIPS_MODE else REGULAR_AGENT
+
+    assert parsed_json == (parsed_json & expected_json)
