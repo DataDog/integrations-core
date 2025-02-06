506a8c12aa1517665bf370e596139519efea3670
Jennifer Chen <jennifer.chen@datadoghq.com>
2025-02-06 15:57:06 -0500

    Add telemetry metric for when local fallback is enabled

--- datadog_cluster_agent/datadog_checks/datadog_cluster_agent/check.py
+++ datadog_cluster_agent/datadog_checks/datadog_cluster_agent/check.py
@@ -39,6 +39,7 @@ DEFAULT_METRICS = {
     'autoscaling_workload_store_job_queue_length': 'autoscaling.workload.store_job_queue_length',
     'autoscaling_workload_local_horizontal_scaling_recommended_replicas': 'autoscaling.workload.local.horizontal_scaling_recommended_replicas',
     'autoscaling_workload_local_horizontal_utilization_pct': 'autoscaling.workload.local.horizontal_utilization_pct',
+    'autoscaling_workload_local_fallback_enabled': 'autoscaling.workload.local.fallback_enabled',
     'aggregator__flush': 'aggregator.flush',
     'aggregator__processed': 'aggregator.processed',
     'api_requests': 'api_requests',
