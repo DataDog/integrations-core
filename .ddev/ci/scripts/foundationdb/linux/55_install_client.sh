fb89ee8de34a6703b9db96afec7617a8461e63d5
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2025-02-13 19:58:15 +0100

    Make sure foundationdb client stays in sync in CI

--- .ddev/ci/scripts/foundationdb/linux/55_install_client.sh
+++ .ddev/ci/scripts/foundationdb/linux/55_install_client.sh
@@ -5,15 +5,21 @@
 
 set -ex
 
+FOUNDATIONDB_VERSION="6.3.15"
+foundationdb_version_from_pyproject_toml=$(grep -e "foundationdb==" foundationdb/pyproject.toml | cut -f 3 -d = | cut -f 1 -d '"')
+if [ ${foundationdb_version_from_pyproject_toml} != ${FOUNDATIONDB_VERSION}]; then
+    echo "foundationdb/pyproject has version ${foundationdb_version_from_pyproject_toml} but ${0} is installing version ${FOUNDATIONDB_VERSION}. Make sure they're in sync."
+    exit 1
+fi
 TMP_DIR=/tmp/fdb
-FDB_URL=https://github.com/apple/foundationdb/releases/download/6.3.15/foundationdb-clients_6.3.15-1_amd64.deb
+FDB_URL=https://github.com/apple/foundationdb/releases/download/${FOUNDATIONDB_VERSION}/foundationdb-clients_${FOUNDATIONDB_VERSION}-1_amd64.deb
 
 mkdir -p $TMP_DIR
 pushd $TMP_DIR
 
 curl --verbose -LO $FDB_URL
 
-sudo dpkg -i foundationdb-clients_6.3.15-1_amd64.deb
+sudo dpkg -i foundationdb-clients_${FOUNDATIONDB_VERSION}-1_amd64.deb
 sudo apt-get install -f
 
 popd
