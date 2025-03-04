916507bf031d9eb9da9dfd60383d184f2fc30351
David Kirov <david.kirov@datadoghq.com>
2025-03-04 16:04:39 +0100

    Add metric mapping and update check.py

--- velero/datadog_checks/velero/check.py
+++ velero/datadog_checks/velero/check.py
@@ -1,7 +1,9 @@
 # (C) Datadog, Inc. 2025-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+
 from datadog_checks.base import OpenMetricsBaseCheckV2  # noqa: F401
+from datadog_checks.velero.metrics import METRIC_MAP, RENAME_LABELS_MAP
 
 
 class VeleroCheck(OpenMetricsBaseCheckV2):
@@ -10,3 +12,9 @@ class VeleroCheck(OpenMetricsBaseCheckV2):
 
     def __init__(self, name, init_config, instances):
         super(VeleroCheck, self).__init__(name, init_config, instances)
+
+    def get_default_config(self):
+        return {
+            'metrics': [METRIC_MAP],
+            'rename_labels': RENAME_LABELS_MAP,
+        }
