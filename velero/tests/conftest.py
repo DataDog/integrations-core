fbba2cb62345adfcd05ee6408170b046d55f6472
David Kirov <david.kirov@datadoghq.com>
2025-03-05 11:29:24 +0100

    Add Caddy environment and test

--- velero/tests/conftest.py
+++ velero/tests/conftest.py
@@ -2,12 +2,24 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
+import logging
 
-from .common import MOCKED_INSTANCE
+from .common import MOCKED_INSTANCE, COMPOSE_FILE
+from datadog_checks.dev import docker_run
+from datadog_checks.dev.conditions import CheckEndpoints
+
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    yield
+    compose_file = COMPOSE_FILE
+    conditions = [
+        CheckEndpoints(MOCKED_INSTANCE["openmetrics_endpoint"]),
+    ]
+    logging.info(conditions)
+    with docker_run(compose_file, conditions=conditions):
+        yield {
+            'instances': [MOCKED_INSTANCE],
+        }
 
 
 @pytest.fixture
