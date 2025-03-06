ca22cfa80dce64313e3b0a7eb0f07bf9d4676449
akaila-crest <abhi.kaila@crestdata.ai>
2025-03-06 19:14:20 +0530

    Update conftest.py

--- silverstripe_cms/tests/conftest.py
+++ silverstripe_cms/tests/conftest.py
@@ -1,11 +1,11 @@
 # (C) Datadog, Inc. 2025-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-
 import os
 from copy import deepcopy
 
 import pytest
+
 from datadog_checks.dev import docker_run
 
 from .common import COMPOSE, INSTANCE
