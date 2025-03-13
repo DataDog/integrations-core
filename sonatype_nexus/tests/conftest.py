3ff0d9e8134d5065a609f3e8b6d9bcf76d6f25a5
Akshit Vaid <akshit.vaid@crestdata.ai>
2025-03-13 09:55:07 +0530

    Add e2e tests

--- sonatype_nexus/tests/conftest.py
+++ sonatype_nexus/tests/conftest.py
@@ -1,14 +1,29 @@
 # (C) Datadog, Inc. 2025-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+from copy import deepcopy
+
 import pytest
 
+from datadog_checks.dev import docker_run
 
-@pytest.fixture(scope="session")
+from .common import COMPOSE, INSTANCE
+
+
+@pytest.fixture(scope='session')
 def dd_environment():
-    yield
+    compose_file = os.path.join(COMPOSE, 'docker-compose.yaml')
+
+    with docker_run(
+        compose_file,
+        build=True,
+        sleep=30,
+    ):
+        instance = INSTANCE.copy()
+        yield instance
 
 
 @pytest.fixture
 def instance():
-    return {}
+    return deepcopy(INSTANCE)
