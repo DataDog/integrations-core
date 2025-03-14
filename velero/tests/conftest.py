d98aacc83f6f1a62d2a3b7a9c2ba31ce573a6118
David Kirov <david.kirov@datadoghq.com>
2025-03-14 14:46:23 +0100

    Make E2E env functional

--- velero/tests/conftest.py
+++ velero/tests/conftest.py
@@ -2,12 +2,12 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import os
-import time
 from contextlib import ExitStack
 
 import pytest
 
-from datadog_checks.dev import run_command
+from datadog_checks.dev import TempDir, run_command
+from datadog_checks.dev.fs import path_join
 from datadog_checks.dev.kind import kind_run
 from datadog_checks.dev.kube_port_forward import port_forward
 
@@ -18,66 +18,31 @@ KIND_DIR = os.path.join(HERE, 'kind')
 
 
 def setup_velero():
-    """Set up Velero and MinIO in the Kind cluster."""
-    run_command(['kubectl', 'create', 'namespace', 'velero'], check=True)
+    """Set up Velero, MinIO and Nginx in the Kind cluster."""
+    # Apply MinIO deployment
+    run_command(['kubectl', 'apply', '-f', os.path.join(KIND_DIR, 'minio.yaml'), '--wait'], check=True)
 
-    # Deploy MinIO
-    run_command(['kubectl', 'apply', '-f', os.path.join(KIND_DIR, 'minio.yaml')], check=True)
+    # Apply Nginx deployment
+    run_command(['kubectl', 'apply', '-f', os.path.join(KIND_DIR, 'nginx.yaml'), '--wait'], check=True)
 
-    for attempt in range(12):
-        try:
-            run_command(
-                ['kubectl', 'wait', '--for=condition=ready', 'pod', '-l', 'app=minio', '-n', 'velero', '--timeout=60s'],
-                check=True,
-            )
-            break
-        except Exception:
-            if attempt == 11:
-                raise Exception("Timed out waiting for MinIO pod to exist")
-            time.sleep(5)
+    # Add Velero Helm repo
+    run_command(['helm', 'repo', 'add', 'vmware-tanzu', 'https://vmware-tanzu.github.io/helm-charts'], check=True)
 
-    # Install Velero
+    # Install Velero using Helm
     run_command(
         [
-            'velero',
+            'helm',
             'install',
-            '--provider',
-            'aws',
-            '--plugins',
-            'velero/velero-plugin-for-aws:v1.7.1',
-            '--bucket',
             'velero',
-            '--secret-file',
-            os.path.join(KIND_DIR, 'credentials-velero'),
-            '--use-volume-snapshots=false',
-            '--use-node-agent',
-            '--backup-location-config',
-            'region=minio,s3ForcePathStyle=true,s3Url=http://minio.velero.svc:9000',
+            'vmware-tanzu/velero',
+            '--namespace',
+            'velero',
+            '--values',
+            os.path.join(KIND_DIR, 'velero-values.yaml'),
+            '--wait',
         ],
         check=True,
     )
-
-    for attempt in range(12):
-        try:
-            run_command(
-                [
-                    'kubectl',
-                    'wait',
-                    '--for=condition=ready',
-                    'pod',
-                    '-l',
-                    'deploy=velero',
-                    '-n',
-                    'velero',
-                    '--timeout=60s',
-                ],
-                check=True,
-            )
-            break
-        except Exception:
-            if attempt == 11:
-                raise Exception("Timed out waiting for Velero pod to exist")
-            time.sleep(5)
 
 
 def get_instances(velero_host, velero_port, node_agent_host, node_agent_port):
@@ -99,15 +64,20 @@ def get_instances(velero_host, velero_port, node_agent_host, node_agent_port):
 def dd_environment():
     kind_config = os.path.join(KIND_DIR, 'kind-config.yaml')
 
-    with kind_run(
-        conditions=[setup_velero],
-        kind_config=kind_config,
-    ) as kubeconfig:
-        with ExitStack() as stack:
-            ip_ports = [
-                stack.enter_context(port_forward(kubeconfig, 'velero', PORT, ressource, name))
-                for ressource, name in [('deployment', 'velero'), ('daemonset', 'node-agent')]
-            ]
+    with TempDir('helm_dir') as helm_dir:
+        with kind_run(
+            conditions=[setup_velero],
+            kind_config=kind_config,
+            env_vars={
+                "HELM_CACHE_HOME": path_join(helm_dir, 'Caches'),
+                "HELM_CONFIG_HOME": path_join(helm_dir, 'Preferences'),
+            },
+        ) as kubeconfig:
+            with ExitStack() as stack:
+                ip_ports = [
+                    stack.enter_context(port_forward(kubeconfig, 'velero', PORT, ressource, name))
+                    for ressource, name in [('deployment', 'velero'), ('daemonset', 'node-agent')]
+                ]
 
         instances = get_instances(ip_ports[0][0], ip_ports[0][1], ip_ports[1][0], ip_ports[1][1])
 
