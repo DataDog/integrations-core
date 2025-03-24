2f11a0d521d5431b6d39140d3774c2f373c93487
steveny91 <steven.yuen@datadoghq.com>
2025-03-24 16:05:35 -0400

    lint

--- ibm_mq/tests/test_ibm_mq_unit.py
+++ ibm_mq/tests/test_ibm_mq_unit.py
@@ -289,9 +289,7 @@ def test_queue_manager_process_direct_ssl(instance):
         pytest.param(
             'America/New_York', 'America/New_York', tz.gettz('America/New_York'), None, True, id='valid-timezone-object'
         ),
-        pytest.param(
-            'Europe/London', 'Europe/London', tz.UTC, None, False, id='another-valid-timezone-string'
-        ),
+        pytest.param('Europe/London', 'Europe/London', tz.UTC, None, False, id='another-valid-timezone-string'),
         pytest.param(
             'Europe/London', 'Europe/London', tz.gettz('Europe/London'), None, True, id='another-valid-timezone-object'
         ),
