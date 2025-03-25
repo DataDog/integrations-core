b52ead1a3fef69f7feccc3a874f19270a380fc98
Kyle-Neale <kyle.neale@datadoghq.com>
2025-03-25 15:35:03 -0400

    Add Celery end to end environment

--- celery/tests/common.py
+++ celery/tests/common.py
@@ -1,1 +1,36 @@
 
+import os
+
+from datadog_checks.dev import get_docker_hostname, get_here
+
+HERE = get_here()
+HOST = get_docker_hostname()
+PORT = 5555
+
+
+def get_fixture_path(filename):
+    return os.path.join(HERE, 'fixtures', filename)
+
+
+MOCKED_INSTANCE = {
+    'openmetrics_endpoint': f"http://{HOST}:{PORT}/metrics",
+    'tags': ['test:tag'],
+}
+
+COMPOSE_FILE = os.path.join(HERE, 'docker', 'docker-compose.yaml')
+
+STANDALONE_TEST_METRICS = {
+    # Flower event metrics
+    'flower_events_total': 'events.total',
+    'flower_events_created': 'events.created',
+    
+    # Task metrics
+    'flower_task_runtime_seconds': 'task.runtime.seconds',
+    'flower_task_runtime_seconds_created': 'task.runtime.created',
+    'flower_task_prefetch_time_seconds': 'task.prefetch_time.seconds',
+    
+    # Worker metrics
+    'flower_worker_prefetched_tasks': 'worker.prefetched_tasks',
+    'flower_worker_online': 'worker.online',
+    'flower_worker_number_of_currently_executing_tasks': 'worker.executing_tasks',
+}
--- celery/tests/conftest.py
+++ celery/tests/conftest.py
@@ -1,14 +1,37 @@
-# (C) Datadog, Inc. 2025-present
-# All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
+import copy
 import pytest
+
+from datadog_checks.dev import docker_run
+from datadog_checks.dev.conditions import CheckDockerLogs
+
+from . import common
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    yield
+    """
+    Start docker-compose environment before running tests and tear it down afterward.
+    """
+    compose_file = common.COMPOSE_FILE
+    
+    with docker_run(
+        compose_file,
+        conditions=[
+            CheckDockerLogs('docker-redis-standalone-1', 'Ready to accept connections tcp'),
+        ],
+        env_vars={
+            'DD_LOGS_ENABLED': 'true',
+            'DD_CONTAINER_COLLECT_ALL': 'true',
+        },
+    ):
+        yield {
+            'instances': [common.MOCKED_INSTANCE]
+        }
 
-
-@pytest.fixture
+@pytest.fixture(scope='session')
 def instance():
-    return {}
+    """
+    Return a default instance used for the integration.
+    """
+    return copy.deepcopy(common.MOCKED_INSTANCE)
+
