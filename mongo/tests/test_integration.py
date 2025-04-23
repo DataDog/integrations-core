e9a390a9f813d249005647565de49dd718a91078
aldrick.castro <aldrick.castro@d>
2025-04-23 17:45:43 -0400

    Still don't know why it works 😅

--- mongo/tests/test_integration.py
+++ mongo/tests/test_integration.py
@@ -1256,18 +1256,10 @@ def test_integration_skip_free_storage_metrics(instance_integration, aggregator,
     instance_integration['free_storage_metrics'] = False
     mongo_check = check(instance_integration)
 
-    with mock_pymongo("replica-primary"):
+    with mock_pymongo("standalone"):
         dd_run_check(mongo_check)
 
-    replica_tags = [
-        'replset_name:replset',
-        'replset_state:primary',
-        'replset_me:replset-data-0.mongo.default.svc.cluster.local:27017',
-        'hosting_type:self-hosted',
-        'replset_nodetype:ELECTABLE',
-        'replset_workloadtype:OPERATIONAL',
-    ]
     metrics_categories = [
         'dbstats-non-free-storage',
     ]
-    assert_metrics(mongo_check, aggregator, metrics_categories, replica_tags)
+    assert_metrics(mongo_check, aggregator, metrics_categories, ['hosting_type:self-hosted'])
