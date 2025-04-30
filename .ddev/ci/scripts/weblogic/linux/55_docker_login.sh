1721b61172cc3412c07d7af5cc9b02763a5b8e3e
steveny91 <steven.yuen@datadoghq.com>
2025-04-30 11:17:46 -0400

    test oracle token change

--- .ddev/ci/scripts/weblogic/linux/55_docker_login.sh
+++ .ddev/ci/scripts/weblogic/linux/55_docker_login.sh
@@ -5,7 +5,7 @@ set +x
 
 echo "[INFO] Docker login"
 for i in 2 4 8 16 32; do
-  echo "$ORACLE_DOCKER_PASSWORD" | docker login container-registry.oracle.com --username "$ORACLE_DOCKER_USERNAME" --password-stdin && break
+  echo "$ORACLE_DOCKER_PASSWORD_2" | docker login container-registry.oracle.com --username "$ORACLE_DOCKER_USERNAME" --password-stdin && break
   echo "[INFO] Wait $i seconds and retry docker login"
   sleep $i
 done
