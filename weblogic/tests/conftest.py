4333232ccd53fd103c46171077b54a438a114af4
Sarah Witt <sarah.witt@datadoghq.com>
2025-05-13 17:23:02 -0400

    update conditions

--- weblogic/tests/conftest.py
+++ weblogic/tests/conftest.py
@@ -22,9 +22,9 @@ def dd_environment(instance):
     with docker_run(
         compose_file=compose_file,
         env_vars={'PROPERTIES_DIR': properties_dir},
-        sleep=60,
+        sleep=100,
         build=True,
-        attempts=2,
+        attempts=3,
     ):
         yield instance, {'use_jmx': True}
 
