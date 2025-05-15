92d03d60ab687c169c366e560a21a55841b144e9
tirthraj.chaudhari <tirthraj.chaudhari@crestdatasys.com>
2025-05-15 19:45:23 +0530

    Add licence headers in utils.py file

--- mac_audit_logs/datadog_checks/mac_audit_logs/utils.py
+++ mac_audit_logs/datadog_checks/mac_audit_logs/utils.py
@@ -1,3 +1,6 @@
+# (C) Datadog, Inc. 2025-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
 from datetime import datetime, timedelta, timezone
 
 from . import constants
