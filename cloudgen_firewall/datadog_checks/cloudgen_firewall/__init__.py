a0781c70ce91bb09d1c620c0dc57569e574cb794
vinodkumar-sacumen <vinod.kumar@sacumen.com>
2025-06-02 13:37:49 +0530

    created integration with type --logs

--- cloudgen_firewall/datadog_checks/cloudgen_firewall/__init__.py
+++ cloudgen_firewall/datadog_checks/cloudgen_firewall/__init__.py
@@ -2,6 +2,5 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .__about__ import __version__
-from .check import CloudgenFirewallCheck
 
-__all__ = ['__version__', 'CloudgenFirewallCheck']
+__all__ = ['__version__']
