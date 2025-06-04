564feea52f9fe0cfbc8187a48251c8daad2dcf5a
Sarah Witt <sarah.witt@datadoghq.com>
2025-06-04 17:09:34 -0400

    Add version metadata check

--- proxmox/datadog_checks/proxmox/check.py
+++ proxmox/datadog_checks/proxmox/check.py
@@ -4,67 +4,30 @@
 from typing import Any  # noqa: F401
 
 from datadog_checks.base import AgentCheck  # noqa: F401
-
-# from datadog_checks.base.utils.db import QueryManager
-# from requests.exceptions import ConnectionError, HTTPError, InvalidURL, Timeout
-# from json import JSONDecodeError
+from datadog_checks.proxmox.config_models import ConfigMixin
 
 
-class ProxmoxCheck(AgentCheck):
-
-    # This will be the prefix of every metric the integration sends
+class ProxmoxCheck(AgentCheck, ConfigMixin):
     __NAMESPACE__ = 'proxmox'
 
     def __init__(self, name, init_config, instances):
         super(ProxmoxCheck, self).__init__(name, init_config, instances)
+        self.check_initializations.append(self._parse_config)
 
-        # Use self.instance to read the check configuration
-        # self.url = self.instance.get("url")
-
-        # If the check is going to perform SQL queries you should define a query manager here.
-        # More info at
-        # https://datadoghq.dev/integrations-core/base/databases/#datadog_checks.base.utils.db.core.QueryManager
-        # sample_query = {
-        #     "name": "sample",
-        #     "query": "SELECT * FROM sample_table",
-        #     "columns": [
-        #         {"name": "metric", "type": "gauge"}
-        #     ],
-        # }
-        # self._query_manager = QueryManager(self, self.execute_query, queries=[sample_query])
-        # self.check_initializations.append(self._query_manager.compile_queries)
+    def _parse_config(self):
+        self.base_tags = [f"proxmox_server:{self.config.proxmox_server}"]
+        if self.config.tags:
+            self.base_tags.extend(self.config.tags)
 
     def check(self, _):
-        # type: (Any) -> None
-        # The following are useful bits of code to help new users get started.
+        try:
+            response = self.http.get(f"{self.config.proxmox_server}/version")
+            response.raise_for_status()
+            response_json = response.json()
+            version = response_json.get("data", {}).get("version")
+            self.set_metadata('version', version)
+            self.gauge("api.up", 1, tags=self.base_tags)
 
-        # Perform HTTP Requests with our HTTP wrapper.
-        # More info at https://datadoghq.dev/integrations-core/base/http/
-        # try:
-        #     response = self.http.get(self.url)
-        #     response.raise_for_status()
-        #     response_json = response.json()
-
-        # except (HTTPError, InvalidURL, ConnectionError, Timeout) as e:
-        #     self.log.debug("Could not connect", exc_info=True)
-
-        # except JSONDecodeError as e:
-        #    self.log.debug("Could not parse JSON", exc_info=True)
-
-        # except ValueError as e:
-        #    self.log.debug("Unexpected value", exc_info=True)
-
-        # This is how you submit metrics
-        # There are different types of metrics that you can submit (gauge, event).
-        # More info at https://datadoghq.dev/integrations-core/base/api/#datadog_checks.base.checks.base.AgentCheck
-        # self.gauge("test", 1.23, tags=['foo:bar'])
-
-        # Perform database queries using the Query Manager
-        # self._query_manager.execute()
-
-        # This is how you use the persistent cache. This cache file based and persists across agent restarts.
-        # If you need an in-memory cache that is persisted across runs
-        # You can define a dictionary in the __init__ method.
-        # self.write_persistent_cache("key", "value")
-        # value = self.read_persistent_cache("key")
-        pass
+        except Exception as e:
+            self.log.error("Encountered an Exception when hitting the Proxmox API %s", e)
+            self.gauge("api.up", 0, tags=self.base_tags)
--- proxmox/tests/test_unit.py
+++ proxmox/tests/test_unit.py
@@ -1,19 +1,55 @@
 # (C) Datadog, Inc. 2025-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-
 from typing import Any, Callable, Dict  # noqa: F401
+
+import pytest
 
 from datadog_checks.base import AgentCheck  # noqa: F401
 from datadog_checks.base.stubs.aggregator import AggregatorStub  # noqa: F401
-from datadog_checks.dev.utils import get_metadata_metrics
+from datadog_checks.dev.http import MockResponse
 from datadog_checks.proxmox import ProxmoxCheck
 
 
-def test_check(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
+@pytest.mark.usefixtures('mock_http_get')
+def test_api_up(dd_run_check, datadog_agent, aggregator, instance):
     check = ProxmoxCheck('proxmox', {}, [instance])
     dd_run_check(check)
+    aggregator.assert_metric("proxmox.api.up", 1, tags=['proxmox_server:http://localhost:8006/api2/json', 'testing'])
 
-    aggregator.assert_all_metrics_covered()
-    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+
+@pytest.mark.parametrize(
+    ('mock_http_get'),
+    [
+        pytest.param(
+            {'http_error': {'/api2/json/version': MockResponse(status_code=500)}},
+            id='500',
+        ),
+        pytest.param(
+            {'http_error': {'/api2/json/version': MockResponse(status_code=404)}},
+            id='404',
+        ),
+    ],
+    indirect=['mock_http_get'],
+)
+@pytest.mark.usefixtures('mock_http_get')
+def test_api_down(dd_run_check, datadog_agent, aggregator, instance):
+    check = ProxmoxCheck('proxmox', {}, [instance])
+    dd_run_check(check)
+    aggregator.assert_metric("proxmox.api.up", 0, tags=['proxmox_server:http://localhost:8006/api2/json', 'testing'])
+
+
+@pytest.mark.usefixtures('mock_http_get')
+def test_version_metadata(dd_run_check, datadog_agent, aggregator, instance):
+    check = ProxmoxCheck('proxmox', {}, [instance])
+    check.check_id = 'test:123'
+    dd_run_check(check)
+
+    version_metadata = {
+        'version.scheme': 'semver',
+        'version.major': '8',
+        'version.minor': '4',
+        'version.patch': '1',
+        'version.raw': '8.4.1',
+    }
+    datadog_agent.assert_metadata('test:123', version_metadata)
