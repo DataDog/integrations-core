64d076538c8a7b8b0ded9c63a0f524a3ff296d1b
Juanpe Araque <juanpedro.araque@datadoghq.com>
2025-06-11 15:47:25 +0200

    Restructure datadog_checks_base using modern namespace packages

--- datadog_checks_base/datadog_checks/checks/prometheus_check/__init__.py
+++
@@ -1,4 +0,0 @@
-# (C) Datadog, Inc. 2016-present
-# All rights reserved
-# Licensed under Simplified BSD License (see LICENSE)
-from ..prometheus import PrometheusCheck, PrometheusFormat, UnknownFormatError
