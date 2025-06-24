9f7eadb0b8bb33679a8fbdc8058993c582874ebe
David Kirov <david.kirov@datadoghq.com>
2025-06-24 11:21:14 +0200

    Lustre implementation WIP 2

--- lustre/datadog_checks/lustre/check.py
+++ lustre/datadog_checks/lustre/check.py
@@ -4,11 +4,43 @@
 from typing import Any  # noqa: F401
 
 from datadog_checks.base import AgentCheck  # noqa: F401
-from datadog_checks.lustre.common import LCTL_PATH, IGNORED_JOBSTATS_METRICS, MDS_JOBSTATS_PARAM_REGEX, OSS_JOBSTATS_PARAM_REGEX
 import subprocess
 import yaml
+from dataclasses import dataclass
 
-    
+OSS_JOBSTATS_PARAM_REGEX = 'ost.*.job_stats'
+MDS_JOBSTATS_PARAM_REGEX = 'obdfilter.*.job_stats'
+
+IGNORED_STATS = {
+    'snapshot_time',
+    'start_time',
+    'elapsed_time',
+}
+
+@dataclass
+class LustreParam:
+    regex: str
+    node_types: tuple[str, ...]
+    wildcards: tuple[str, ...] = ()
+    prefix: str = ''
+
+DEFAULT_PARAMS = [
+    LustreParam(regex='llite.*.stats', node_types=('client',), wildcards=('component_uuid',), prefix='filesystem'),
+]
+
+EXTRA_PARAMS = [
+    LustreParam(regex='mds.MDS.mdt.stats', node_types=('mds',), prefix='mds.mdt'),
+    LustreParam(regex='mdt.*.exports.*.stats', node_types=('mds',), wildcards=('component_name','nid'), prefix='mds.mdt.exports'),
+    LustreParam(regex='mdc.*.stats', node_types=('client',), wildcards=('component_uuid',), prefix='mdc'),
+    LustreParam(regex='ldlm.services.*.stats', node_types=('client', 'mds', 'oss'), wildcards=('ldlm_service',), prefix='ldlm.services'),
+    LustreParam(regex='ldlm.namespaces.*.pool.stats', node_types=('client', 'mds', 'oss'), wildcards=('component_uuid',), prefix='ldlm.namespaces.pool'),
+    LustreParam(regex='mgs.MGS.exports.*.stats', node_types=('mds',), wildcards=('component_name', 'nid'), prefix='mgs.exports'),
+    LustreParam(regex='ost.OSS.oss.stats', node_types=('oss',), prefix='ost.oss'),
+    LustreParam(regex='osc.*.stats', node_types=('client',), wildcards=('component_uuid',), prefix='osc'),
+    LustreParam(regex='obdfilter.*.exports.*.stats', node_types=('oss',), wildcards=('component_name', 'nid'), prefix='obdfilter.exports'),
+    LustreParam(regex='obdfilter.*.stats', node_types=('oss',), wildcards=('component_name',), prefix='obdfilter'),
+]
+
 class LustreCheck(AgentCheck):
 
     __NAMESPACE__ = 'lustre'
@@ -17,18 +49,43 @@ class LustreCheck(AgentCheck):
         super(LustreCheck, self).__init__(name, init_config, instances)
 
         self.filesystems = self.instance.get("filesystems", [])
-        self.node_type = self.instance.get("node_type", self.get_node_type())
         self.lctl_path = self.instance.get("lctl_path", "/usr/sbin/lctl")
         self.lnetctl_path = self.instance.get("lnetctl_path", "/usr/sbin/lnetctl")
+        self.lnetctl_detailed = self.instance.get("enable_lnetctl_detailed", False)
+        self.lnetctl_health = self.instance.get("enable_lnetctl_health", False)
+        self.node_type = self.instance.get("node_type", self.get_node_type())
+
+        self.param_list = DEFAULT_PARAMS
+        if self.instance.get("enable_extra_params", False):
+            self.param_list += EXTRA_PARAMS
+
 
 
     def check(self, _):
         self.submit_jobstats_metrics(self.node_type)
+        # self.submit_lnet_metrics()
+        # self.submit_lnet_local_ni_metrics()
+        # self.submit_lnet_peer_ni_metrics()
 
     def get_node_type(self):
         '''
         Determine the host type from the command line.
         '''
+        self.log.debug('Determining node type...')
+        cmd = f'{self.lctl_path} dl'
+        try:
+            output_lines = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout.splitlines()
+            # Example line:   0 UP osd-ldiskfs lustre-OST0001-osd lustre-OST0001-osd_UUID 5
+            devices = [line.strip().split()[2] for line in output_lines]
+            if 'mdt' in devices:
+                return 'mds'
+            elif 'ost' in devices:
+                return 'oss'
+            else:
+                return 'client'
+        except Exception as e:
+            self.log.error(f'Failed to determine node type: {e}')
+            return 'client'
 
     def lctl_list_param(self, param_regex):
         '''
@@ -41,34 +98,52 @@ class LustreCheck(AgentCheck):
             self.log.error(f'Failed to list parameters for {param_regex}: {e}')
             return []
 
-    def lctl_get_param(self, param):
+    def lctl_get_param(self, param_regex):
         '''
         Get the value of a parameter from the command line.
         '''
-        cmd = f'{self.lctl_path} get_param -ny {param}'
+        cmd = f'{self.lctl_path} get_param -ny {param_regex}'
         try:
             return subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
         except Exception as e:
-            self.log.error(f'Failed to get data for {param}: {e}')
+            self.log.error(f'Failed to get data for {param_regex}: {e}')
             return ''
 
-    def submit_jobstats_metrics(self, component_type):
+    def lnet_get_stats(self, stats_type):
+        '''
+        Show the lnet stats from the command line.
+        '''
+        cmd = f'sudo {self.lnetctl_path} {stats_type} show -v 3'
+        try:
+            return subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
+        except Exception as e:
+            self.log.error(f'Failed to show lnet stats: {e}')
+            return ''
+
+    def submit_jobstats_metrics(self):
         '''
         Submit the jobstats metrics to Datadog.
         '''
-        jobstats_params = self.get_jobstats_params_list(component_type)
+        jobstats_params = self.get_jobstats_params_list(self.node_type)
         for jobstats_param in jobstats_params:
-            component_name = jobstats_param.split('.')[1]
+            component_name = jobstats_param.split('.')[1]  # For example: lustre-MDT0000
             if self.filesystems and not any(component_name.startswith(filesystem) for filesystem in self.filesystems):
                 continue
-            jobstats_metrics = self.get_jobstats_metrics(jobstats_param)
+            jobstats_metrics = self.get_jobstats_metrics(jobstats_param)['job_stats']
             for job in jobstats_metrics:
                 job_id = job['job_id']
                 for metric_name, metric_values in job.items():
-                    if metric_name in IGNORED_JOBSTATS_METRICS:
+                    if not isinstance(metric_values, dict):
                         continue
                     for metric_type, metric_value in metric_values.items():
-                        self.gauge(f'lustre.jobstats.{metric_name}.{metric_type}', metric_value, tags=[f'component_type:{component_type}', f'component_name:{component_name}', f'job_id:{job_id}'])
+                        if metric_type == 'unit':
+                            continue
+                        if metric_type == 'hist':
+                            # TODO: Handle histogram metrics if needed
+                            continue 
+                        self.log.warning(f'Jobstats metric {metric_name} with type {metric_type} has value {metric_value}')
+                        print(f'lustre.jobstats.{metric_name}.{metric_type}')
+                        self.gauge(f'jobstats.{metric_name}.{metric_type}', metric_value, tags=[f'component_type:{self.node_type}', f'component_name:{component_name}', f'job_id:{job_id}'])
 
 
     def get_jobstats_params_list(self, component_type):
@@ -94,28 +169,115 @@ class LustreCheck(AgentCheck):
         '''
         jobstats_output = self.lctl_get_param(jobstats_param)
         try:
-            return yaml.safe_load(jobstats_output)['job_stats']
+            return yaml.safe_load(jobstats_output)
         except KeyError:
             self.log.debug(f'No jobstats metrics found for {jobstats_param}')
-            return []
+            return {}
     
-    def submit_lnet_metrics(self):
+    def submit_lnet_stats_metrics(self):
         '''
-        Submit the lnet metrics to Datadog.
+        Submit the lnet metrics.
         '''
-        lnet_stats = self.get_lnet_stats()
-        for lnet_stat in lnet_stats:
-            self.gauge(f'lustre.net.{lnet_stat}', lnet_stats[lnet_stat], tags=[f'node_type:{self.node_type}'])
+        lnet_metrics = self.get_lnet_metrics('stats')['statistics']
+        for metric in lnet_metrics:
+            self.gauge(f'net.{metric}', lnet_metrics[metric], tags=[f'node_type:{self.node_type}'])
 
-    def get_lnet_stats(self):
+    def get_lnet_metrics(self, stats_type='stats'):
         '''
         Get the lnet stats from the command line.
         '''
-        cmd = f'sudo {self.lnetctl_path} stats show'
-        lnet_stats = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
+        lnet_stats = self.lnet_get_stats(stats_type)
         try:
-            return yaml.safe_load(lnet_stats)['statistics']
-        except ValueError:
+            return yaml.safe_load(lnet_stats)
+        except (KeyError, ValueError):
             self.log.debug(f'No lnet stats found')
-            return ''
+            return {}
     
+    def submit_lnet_local_ni_metrics(self):
+        '''
+        Submit the lnet local ni metrics.
+        '''
+        lnet_local_stats = self.get_lnet_metrics('net')['net']
+        for net in lnet_local_stats:
+            net_type = net.get('net type')
+            for ni in net.get('local NI(s)', []):
+                nid = ni.get('nid')
+                tags = [f'net_type:{net_type}', f'nid:{nid}']
+                for stats_group_name, stats_group in ni.items():
+                    if not isinstance(stats_group, dict):
+                        continue
+                    for metric_name, metric_value in stats_group.items():
+                        if isinstance(metric_value, int):
+                            self.gauge(f'net.local.{stats_group_name.replace(" ", "_")}.{metric_name.replace(" ", "_")}', metric_value, tags=tags)
+
+    def submit_lnet_peer_ni_metrics(self):
+        '''
+        Submit the lnet peer ni metrics.
+        '''
+        lnet_peer_stats = self.get_lnet_metrics('peer')['peer']
+        for peer in lnet_peer_stats:
+            nid = peer.get('primary nid')
+            for ni in peer.get('peer ni', []):
+                peer_nid = ni.get('nid')
+                tags = [f'nid:{nid}', f'peer_nid:{peer_nid}']
+                for stats_group_name, stats_group in ni.items():
+                    if not isinstance(stats_group, dict):
+                        continue
+                    for metric_name, metric_value in stats_group.items():
+                        if isinstance(metric_value, int):
+                            self.gauge(f'net.peer.{stats_group_name.replace(" ", "_")}.{metric_name.replace(" ", "_")}', metric_value, tags=tags)
+
+    def submit_general_stats(self):
+        '''
+        Submit general stats.
+        '''
+        for param in self.param_list: 
+            if not param.regex.endswith('.stats'):
+                continue
+            raw_stats = self.lctl_get_param(param.regex)
+            parsed_stats = self.parse_stats(raw_stats)
+            for stat_name, stat_value in parsed_stats.items():
+                if isinstance(stat_value, int):
+                    self.gauge(f'general.{stat_name}', stat_value, tags=[f'node_type:{self.node_type}'])
+                    continue
+                if not isinstance(stat_value, dict):
+                    self.log.debug(f'Unexpected stat value for {stat_name}: {stat_value}')
+                    continue
+                for metric_type, metric_value in stat_value.items():
+                    if isinstance(metric_value, int):
+                        self.gauge(f'general.{stat_name}.{metric_type}', metric_value, tags=[f'node_type:{self.node_type}'])
+                    else:
+                        self.log.debug(f'Unexpected metric value for {stat_name}.{metric_type}: {metric_value}')
+
+
+    def parse_stats(self, raw_stats):
+        '''
+        Parse the raw stats into a dictionary.
+        '''
+        stats = {}
+        for line in raw_stats.splitlines():
+            # elapsed_time              2068792.478877751 secs.nsecs
+            # req_waittime              83 samples [usecs] 11 40 1493 32135
+            # req_qdepth                83 samples [reqs] 0 0 0 0
+            # cancel                    253 samples [locks] 1 1 253
+            # tgtreg                    2 samples [reqs]
+            if not line.strip():
+                continue
+            parts = line.split()
+            if len(parts) < 2:
+                continue
+            stat_name = parts[0]
+            if stat_name in IGNORED_STATS:
+                continue
+            try:
+                stat_value = {"count": int(parts[1]), "unit": parts[3].strip('[]')}
+                stat_types = ('min', 'max', 'sum', 'sumsq')
+                for i, value in enumerate(parts[4:]):
+                    stat_value[stat_types[i]] = int(value)
+                if len(parts) > 8:
+                    self.log.debug(f'Unexpected format for stat "{line}"')
+            except ValueError:
+                self.log.debug(f'Could not parse stat value for "{line}"')
+                continue
+            stats[stat_name] = stat_value
+        return stats
--- lustre/tests/test_unit.py
+++ lustre/tests/test_unit.py
@@ -8,13 +8,36 @@ from datadog_checks.base import AgentCheck  # noqa: F401
 from datadog_checks.base.stubs.aggregator import AggregatorStub  # noqa: F401
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.lustre import LustreCheck
+from datadog_checks.lustre.metrics import LNET_LOCAL_METRICS, LNET_PEER_METRICS, LNET_STATS_METRICS
 from datadog_checks.dev import get_here
+import os
+import mock
+import pytest
 
 HERE = get_here()
-FIXTURES_DIR = HERE / 'fixtures'
+FIXTURES_DIR = os.path.join(HERE, 'fixtures')
+
+@pytest.fixture
+def mock_client():
+    with mock.patch.object(LustreCheck, 'get_node_type', return_value='client'):
+        yield
+
+@pytest.fixture
+def mock_mds():
+    with mock.patch.object(LustreCheck, 'get_node_type', return_value='mds'):
+        yield
+
+@pytest.fixture
+def mock_oss():
+    with mock.patch.object(LustreCheck, 'get_node_type', return_value='oss'):
+        yield
+
+@pytest.fixture
+def disable_subprocess():
+    with mock.patch('datadog_checks.lustre.check.subprocess.run'):
+        yield
 
 def test_check(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
     check = LustreCheck('lustre', {}, [instance])
     dd_run_check(check)
 
@@ -22,17 +45,39 @@ def test_check(dd_run_check, aggregator, instance):
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
 
 
-def test_emits_critical_service_check_when_service_is_down(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
-    check = LustreCheck('lustre', {}, [instance])
-    dd_run_check(check)
-    aggregator.assert_service_check('lustre.can_connect', LustreCheck.CRITICAL)
+def test_jobstats(aggregator, instance, mock_oss, disable_subprocess):
+    with mock.patch.object(LustreCheck, 'lctl_list_param') as mock_list_param:
+        with mock.patch.object(LustreCheck, 'lctl_get_param') as mock_get_param:
+            mock_list_param.return_value = ['obdfilter.lustre-OST0001.job_stats']
+            with open(os.path.join(FIXTURES_DIR, 'oss_jobstats.txt'), 'r') as f:
+                mock_get_param.return_value = f.read()
+                check = LustreCheck('lustre', {}, [instance])
+                check.submit_jobstats_metrics()
+    # TODO: Add assertions to verify the job stats metrics
 
+def test_lnet_stats(aggregator, instance, mock_client):
+    with mock.patch.object(LustreCheck, 'lnet_get_stats') as mock_get:
+        with open(os.path.join(FIXTURES_DIR, 'all_lnet_stats.txt'), 'r') as f:
+            mock_get.return_value = f.read()
+            check = LustreCheck('lustre', {}, [instance])
+            check.submit_lnet_stats_metrics()
+    for metric in LNET_STATS_METRICS:
+        aggregator.assert_metric(metric)
 
-def test_jobstats(dd_run_check, aggregator, instance):
-    # type: (Callable[[AgentCheck, bool], None], AggregatorStub, Dict[str, Any]) -> None
-    check = LustreCheck('lustre', {}, [instance])
-    dd_run_check(check)
+def test_lnet_local(aggregator, instance, mock_client):
+    with mock.patch.object(LustreCheck, 'lnet_get_stats') as mock_get:
+        with open(os.path.join(FIXTURES_DIR, 'all_lnet_net.txt'), 'r') as f:
+            mock_get.return_value = f.read()
+            check = LustreCheck('lustre', {}, [instance])
+            check.submit_lnet_local_ni_metrics()
+    for metric in LNET_LOCAL_METRICS:
+        aggregator.assert_metric(metric)
 
-    # Assert that job stats metrics are collected
-
+def test_lnet_peer(aggregator, instance, mock_client):
+    with mock.patch.object(LustreCheck, 'lnet_get_stats') as mock_get:
+        with open(os.path.join(FIXTURES_DIR, 'all_lnet_peer.txt'), 'r') as f:
+            mock_get.return_value = f.read()
+            check = LustreCheck('lustre', {}, [instance])
+            check.submit_lnet_peer_ni_metrics()
+    for metric in LNET_PEER_METRICS:
+        aggregator.assert_metric(metric)
