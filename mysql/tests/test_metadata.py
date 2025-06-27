225fecedf3c64ca22fba85dfda3972f0671609b3
Zhengda Lu <zhengda.lu@datadoghq.com>
2025-06-27 13:40:57 +0000

    fix tests

--- mysql/tests/test_metadata.py
+++ mysql/tests/test_metadata.py
@@ -716,9 +716,6 @@ def test_schemas_collection_truncated(aggregator, dd_run_check, dbm_instance):
 
 @pytest.mark.unit
 def test_schemas_collection_config(dbm_instance):
-    check = MySql(common.CHECK_NAME, {}, instances=[dbm_instance])
-    assert check.config.schema_config == {}
-
     dbm_instance['schemas_collection'] = {"enabled": True, "max_execution_time": 0}
     check = MySql(common.CHECK_NAME, {}, instances=[dbm_instance])
     assert check._config.schema_config == {"enabled": True, "max_execution_time": 0}
