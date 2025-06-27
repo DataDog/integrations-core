d2b4bd8639b92cf8001f2a8368f7217e02a9c388
Zhengda Lu <zhengda.lu@datadoghq.com>
2025-06-27 14:48:17 +0000

    fix deprecation version

--- sqlserver/datadog_checks/sqlserver/config_models/deprecations.py
+++ sqlserver/datadog_checks/sqlserver/config_models/deprecations.py
@@ -5,7 +5,7 @@
 
 def instance():
     return {
-        'deadlocks_collection': {'Agent version': '7.59.0', 'Migration': 'Use `collect_deadlocks` instead.'},
-        'schemas_collection': {'Agent version': '7.59.0', 'Migration': 'Use `collect_schemas` instead.'},
-        'xe_collection': {'Agent version': '7.59.0', 'Migration': 'Use `collect_xe` instead.'},
+        'deadlocks_collection': {'Agent version': '7.69.0', 'Migration': 'Use `collect_deadlocks` instead.'},
+        'schemas_collection': {'Agent version': '7.69.0', 'Migration': 'Use `collect_schemas` instead.'},
+        'xe_collection': {'Agent version': '7.69.0', 'Migration': 'Use `collect_xe` instead.'},
     }
