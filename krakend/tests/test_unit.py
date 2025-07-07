a265f4654419f44a52a5591aa86283921c3c4828
Juanpe Araque <juanpedro.araque@datadoghq.com>
2025-07-07 15:25:42 +0200

    Fix typo in license headers

--- krakend/tests/test_unit.py
+++ krakend/tests/test_unit.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2025-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+
 from collections.abc import Callable
 from pathlib import Path
 
