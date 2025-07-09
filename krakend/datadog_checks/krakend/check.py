7ca7934eb82041788df22e547ec5859d4890d9ae
Juanpe Araque <juanpedro.araque@datadoghq.com>
2025-07-09 17:31:51 +0200

    Rollback usage of decorator until it is fixed

--- krakend/datadog_checks/krakend/check.py
+++ krakend/datadog_checks/krakend/check.py
@@ -1,15 +1,49 @@
 # (C) Datadog, Inc. 2025-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from __future__ import annotations
+
 from collections import ChainMap
+from typing import TYPE_CHECKING
 
 from datadog_checks.base import OpenMetricsBaseCheckV2
-from datadog_checks.base.checks.openmetrics.v2.scraper import OpenMetricsScraper, decorators
+from datadog_checks.base.checks.openmetrics.v2.scraper import OpenMetricsScraper
 from datadog_checks.base.types import InstanceType
 
 from .metrics import METRICS_MAP, RENAME_LABELS_MAP
 
+if TYPE_CHECKING:
+    from typing import Mapping
+
+    from prometheus_client.metrics_core import Metric
+
+    from datadog_checks.base.checks.base import AgentCheck
+
 HTTP_STATUS_CODE_TAG = "http_response_status_code"
+
+
+class HttpCodeClassScraper(OpenMetricsScraper):
+    def __init__(self, check: AgentCheck, config: Mapping):
+        super().__init__(check, config)
+
+    def consume_metrics(self, runtime_data):
+        metrics = super().consume_metrics(runtime_data)
+        for metric in metrics:
+            yield HttpCodeClassScraper.inject_code_class(metric)
+
+    @staticmethod
+    def inject_code_class(metric: Metric):
+        # Patch all samples to add the code_class tag if 'code' is a 3-digit HTTP code
+        for sample in metric.samples:
+            if (
+                (code := sample.labels.get(HTTP_STATUS_CODE_TAG))
+                and isinstance(code, str)
+                and len(code) == 3
+                and code.isdigit()
+            ):
+                sample.labels['code_class'] = f"{code[0]}XX"
+
+        return metric
 
 
 class KrakendCheck(OpenMetricsBaseCheckV2):
@@ -18,12 +52,9 @@ class KrakendCheck(OpenMetricsBaseCheckV2):
     DEFAULT_METRIC_LIMIT = 0
 
     def create_scraper(self, config: InstanceType):
-        return decorators.WithHttpCodeClass(
-            OpenMetricsScraper(self, self.get_config_with_defaults(config)),
-            http_status_tag=HTTP_STATUS_CODE_TAG,
-        )
+        return HttpCodeClassScraper(self, self.get_config_with_defaults(config))
 
-    def get_config_with_defaults(self, config: InstanceType):
+    def get_config_with_defaults(self, config: InstanceType) -> Mapping:
         # If the user does not provide a value for go_metrics or process_metrics,
         # assume they want whatever behvaior has been set in KrakenD
         go_metrics = config.get("go_metrics", True)
