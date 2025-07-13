9df460e6baa90ad85367d6354f581ec9e9f41a98
NouemanKHAL <noueman.khalikine@datadoghq.com>
2025-07-13 13:02:20 +0200

    delete test_version_metadata from the e2e test

--- glusterfs/tests/test_e2e.py
+++ glusterfs/tests/test_e2e.py
@@ -23,19 +23,3 @@ def test_e2e(dd_agent_check, config):
 
     aggregator.assert_service_check("glusterfs.cluster.health", GlusterfsCheck.OK)
     aggregator.assert_all_metrics_covered()
-
-
-def test_version_metadata(dd_agent_check, datadog_agent, config):
-    dd_agent_check(config)
-    if GLUSTER_VERSION == "7.1":
-        version_metadata = {
-            'version.raw': "7.1",
-            'version.scheme': 'glusterfs',
-            'version.major': 7,
-            'version.minor': 1,
-        }
-        datadog_agent.assert_metadata('', version_metadata)
-        datadog_agent.assert_metadata_count(4)
-
-    else:
-        pytest.skip("Unsupported glusterfs version: {}".format(GLUSTER_VERSION))
