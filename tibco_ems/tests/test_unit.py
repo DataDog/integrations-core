01df43e708ccaa66abf3acd5ae784992a08a7ba2
Sarah Witt <sarah.witt@datadoghq.com>
2025-07-23 13:14:21 -0400

    Add logging and stop storing all tags

--- tibco_ems/tests/test_unit.py
+++ tibco_ems/tests/test_unit.py
@@ -2,13 +2,10 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
-from typing import Any, Callable, Dict  # noqa: F401
 from unittest.mock import MagicMock
 
 import pytest
 
-from datadog_checks.base import AgentCheck  # noqa: F401
-from datadog_checks.base.stubs.aggregator import AggregatorStub  # noqa: F401
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.tibco_ems import TibcoEMSCheck
 
