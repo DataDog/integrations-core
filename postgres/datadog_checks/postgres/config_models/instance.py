10c5a1404843ac6432ce44afb47d3fec9da28659
Seth Samuel <seth.samuel@datadoghq.com>
2025-07-28 15:13:07 -0400

    Fix defaults

--- postgres/datadog_checks/postgres/config_models/instance.py
+++ postgres/datadog_checks/postgres/config_models/instance.py
@@ -292,21 +292,21 @@ class InstanceConfig(BaseModel):
     use_global_custom_queries: Optional[str] = None
     username: Optional[str] = None
 
-    # @model_validator(mode='before')
-    # def _initial_validation(cls, values):
-    #     return validation.core.initialize_config(getattr(validators, 'initialize_instance', identity)(values))
+    @model_validator(mode='before')
+    def _initial_validation(cls, values):
+        return validation.core.initialize_config(getattr(validators, 'initialize_instance', identity)(values))
 
-    # @field_validator('*', mode='before')
-    # def _validate(cls, value, info):
-    #     field = cls.model_fields[info.field_name]
-    #     field_name = field.alias or info.field_name
-    #     if info.context and field_name in info.context['configured_fields']:
-    #         value = getattr(validators, f'instance_{info.field_name}', identity)(value, field=field)
-    #     else:
-    #         value = getattr(defaults, f'instance_{info.field_name}', lambda: value)()
+    @field_validator('*', mode='before')
+    def _validate(cls, value, info):
+        field = cls.model_fields[info.field_name]
+        field_name = field.alias or info.field_name
+        if info.context and field_name in info.context['configured_fields']:
+            value = getattr(validators, f'instance_{info.field_name}', identity)(value, field=field)
+        elif not value:
+            value = getattr(defaults, f'instance_{info.field_name}', lambda: value)()
 
-    #     return validation.utils.make_immutable(value)
+        return validation.utils.make_immutable(value)
 
-    # @model_validator(mode='after')
-    # def _final_validation(cls, model):
-    #     return validation.core.check_model(getattr(validators, 'check_instance', identity)(model))
+    @model_validator(mode='after')
+    def _final_validation(cls, model):
+        return validation.core.check_model(getattr(validators, 'check_instance', identity)(model))
