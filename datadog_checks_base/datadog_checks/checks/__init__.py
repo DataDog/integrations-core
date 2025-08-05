00a32abff8c651746caa3aa8c6a8b13801216f9b
Juanpe Araque <juanpedro.araque@datadoghq.com>
2025-08-05 16:14:31 +0200

    Clean up checks to keep only the minimum temporary packages

--- datadog_checks_base/datadog_checks/checks/__init__.py
+++ datadog_checks_base/datadog_checks/checks/__init__.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 # ruff: noqa
-from ..base.checks import *
-from ..base.errors import CheckException
-from . import AgentCheck
+from datadog_checks.base.checks import *
+from datadog_checks.base.errors import CheckException
+from datadog_checks.base.checks.base import AgentCheck
+from datadog_checks.base.checks import libs, openmetrics, prometheus, win, base, network
--- datadog_checks_base/datadog_checks/checks/win/winpdh_stub.py
+++
@@ -1,5 +0,0 @@
-# (C) Datadog, Inc. 2013-present
-# All rights reserved
-# Licensed under Simplified BSD License (see LICENSE)
-# ruff: noqa
-from ...base.checks.win.winpdh_stub import *
