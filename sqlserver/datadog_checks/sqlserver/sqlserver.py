02d7e4682518857880387be43f5661bcca887621
sangeetashivaji <sangeeta.shivajirao@datadoghq.com>
2025-08-06 09:33:48 -0400

    Update ddagenthostname for other integrations

--- sqlserver/datadog_checks/sqlserver/sqlserver.py
+++ sqlserver/datadog_checks/sqlserver/sqlserver.py
@@ -235,6 +235,7 @@ class SQLServer(AgentCheck):
         """
         self.tag_manager.set_tag("database_hostname", self.database_hostname, replace=True)
         self.tag_manager.set_tag("database_instance", self.database_identifier, replace=True)
+        self.tag_manager.set_tag("ddagenthostname", self.agent_hostname, replace=True)
 
     def set_resource_tags(self):
         if self.cloud_metadata.get("gcp") is not None:
