403966bc41d7a3a39ccfb107469f2ebc6bfa9caa
Piotr Wolski <piotr.wolski42@gmail.com>
2025-08-06 12:51:03 -0600

    lower topic names before comparing

--- kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
+++ kafka_consumer/datadog_checks/kafka_consumer/kafka_consumer.py
@@ -416,7 +416,7 @@ class KafkaCheck(AgentCheck):
     def data_streams_live_message(self, highwater_offsets, cluster_id):
         monitored_topics = None
         for cfg in self.config.live_messages_configs:
-            monitored_topics = monitored_topics or {topic for (topic, _) in highwater_offsets.keys()}
+            monitored_topics = monitored_topics or {topic.lower() for (topic, _) in highwater_offsets.keys()}
             kafka = cfg['kafka']
             topic = kafka["topic"]
             partition = kafka["partition"]
@@ -432,7 +432,7 @@ class KafkaCheck(AgentCheck):
                 continue
             if not cluster or not cluster_id or cluster.lower() != cluster_id.lower():
                 continue
-            if topic not in monitored_topics:
+            if topic.lower() not in monitored_topics:
                 self.log.debug('Skipping live messages for topic %s because it is not monitored by this check', topic)
                 continue
             start_offsets = resolve_start_offsets(highwater_offsets, topic, partition, start_offset, n_messages)
