54f936e1deade39b9959d5daac8ccec62b4e89c0
Seth Samuel <seth.samuel@datadoghq.com>
2025-08-11 10:28:06 -0400

    Lint

--- postgres/datadog_checks/postgres/postgres.py
+++ postgres/datadog_checks/postgres/postgres.py
@@ -1118,7 +1118,6 @@ class PostgreSql(AgentCheck):
                 hostname=self.reported_hostname,
                 raw=True,
             )
-            
         else:
             self.service_check(
                 self.SERVICE_CHECK_NAME,
