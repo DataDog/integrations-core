011b2d597c5933bedd64987141ecb7a453a7852f
NouemanKHAL <noueman.khalikine@datadoghq.com>
2025-08-29 15:56:13 +0200

    fix exporting DBCheck class

--- datadog_checks_base/datadog_checks/base/checks/__init__.pyi
+++ datadog_checks_base/datadog_checks/base/checks/__init__.pyi
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from .base import AgentCheck
+from .db import DBCheck
 from .network import EventType, NetworkCheck, Status
 
-__all__ = ['AgentCheck', 'EventType', 'NetworkCheck', 'Status']
+__all__ = ['AgentCheck', 'DBCheck', 'EventType', 'NetworkCheck', 'Status']
