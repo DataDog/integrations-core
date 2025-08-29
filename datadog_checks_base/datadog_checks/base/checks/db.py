b54f1c4f1cf5dcdb222b81cff82172a838ea8874
NouemanKHAL <noueman.khalikine@datadoghq.com>
2025-08-29 15:57:14 +0200

    cleanup DBCheck typing and redundant raw_event check

--- datadog_checks_base/datadog_checks/base/checks/db.py
+++ datadog_checks_base/datadog_checks/base/checks/db.py
@@ -6,37 +6,17 @@ from . import AgentCheck
 
 
 class DBCheck(AgentCheck):
-    def database_monitoring_query_sample(self, raw_event):
-        # type: (str) -> None
-        if raw_event is None:
-            return
-
+    def database_monitoring_query_sample(self, raw_event: str):
         self.event_platform_event(self, raw_event, "dbm-samples")
 
-    def database_monitoring_query_metrics(self, raw_event):
-        # type: (str) -> None
-        if raw_event is None:
-            return
-
+    def database_monitoring_query_metrics(self, raw_event: str):
         self.event_platform_event(self, raw_event, "dbm-metrics")
 
-    def database_monitoring_query_activity(self, raw_event):
-        # type: (str) -> None
-        if raw_event is None:
-            return
-
+    def database_monitoring_query_activity(self, raw_event: str):
         self.event_platform_event(self, raw_event, "dbm-activity")
 
-    def database_monitoring_metadata(self, raw_event):
-        # type: (str) -> None
-        if raw_event is None:
-            return
-
+    def database_monitoring_metadata(self, raw_event: str):
         self.event_platform_event(self, raw_event, "dbm-metadata")
 
-    def database_monitoring_health(self, raw_event):
-        # type: (str) -> None
-        if raw_event is None:
-            return
-
+    def database_monitoring_health(self, raw_event: str):
         self.event_platform_event(self, raw_event, "dbm-health")
