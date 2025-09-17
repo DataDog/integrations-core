ca4f2033499f8205b82cf29555c987816ca33d88
NouemanKHAL <noueman.khalikine@datadoghq.com>
2025-09-17 11:26:21 +0200

    working nutanix.health.up metric

--- nutanix/tests/conftest.py
+++ nutanix/tests/conftest.py
@@ -3,6 +3,14 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import pytest
 
+AWS_INSTANCE = {
+    "pc_ip": "nutanix-prism-central-nlb-7160b14f9d5c530e.elb.us-east-1.amazonaws.com",
+    "pc_port": 9440,
+    "username": "admin",
+    "password": "uyp2ZFW9qat4dxn-rza",
+    "tls_verify": False,
+}
+
 
 @pytest.fixture(scope='session')
 def dd_environment():
@@ -12,3 +20,8 @@ def dd_environment():
 @pytest.fixture
 def instance():
     return {}
+
+
+@pytest.fixture
+def aws_instance():
+    return AWS_INSTANCE
