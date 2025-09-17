bccad632758bf7be072bad98947c812369efc0b6
Lucia Sanchez Bella <lucia.sanchezbella@datadoghq.com>
2025-09-17 17:09:55 +0200

    change metric name

--- ddev/src/ddev/cli/size/utils/common_funcs.py
+++ ddev/src/ddev/cli/size/utils/common_funcs.py
@@ -866,7 +866,7 @@ def send_metrics_to_dd(
     for item in modules:
         metrics.append(
             {
-                "metric": f"{metric_name}.size_status",
+                "metric": f"{metric_name}.size",
                 "type": "gauge",
                 "points": [(timestamp, item["Size_Bytes"])],
                 "tags": [
