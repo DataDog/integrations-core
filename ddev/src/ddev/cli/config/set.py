113b0124ebc4c617e09e2f8b97dd3c1857aadcc3
Lucia Sanchez Bella <lucia.sanchezbella@datadoghq.com>
2025-09-23 17:42:23 +0200

    quiet try

--- ddev/src/ddev/cli/config/set.py
+++ ddev/src/ddev/cli/config/set.py
@@ -55,8 +55,9 @@ def validate_final_config(app: Application, overrides: bool, config: dict[str, A
 @click.argument('key')
 @click.argument('value', required=False)
 @click.option('--overrides', is_flag=True, help='Set the value in the local config file (.ddev.toml)')
+@click.option('--quiet', is_flag=True, help='Do not display the new setting')
 @click.pass_obj
-def set_value(app: Application, key: str, value: str | None, overrides: bool):
+def set_value(app: Application, key: str, value: str | None, overrides: bool, quiet: bool):
     """
     Assign values to config file entries. If the value is omitted,
     you will be prompted, with the input hidden if it is sensitive.
@@ -133,5 +134,6 @@ def set_value(app: Application, key: str, value: str | None, overrides: bool):
 
     from rich.syntax import Syntax
 
-    app.display_success('New setting:')
-    app.output(Syntax(rendered_changed, 'toml', background_color='default'))
+    if not quiet:
+        app.display_success('New setting:')
+        app.output(Syntax(rendered_changed, 'toml', background_color='default'))
