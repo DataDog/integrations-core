fab49b16d2185e51b1c0864fb870a78cd646fd45
Seth Samuel <seth.samuel@datadoghq.com>
2025-09-30 15:41:29 -0400

    Make tests fast

--- postgres/tests/conftest.py
+++ postgres/tests/conftest.py
@@ -63,12 +63,13 @@ def dd_environment(e2e_instance):
     compose_file = 'docker-compose.yaml'
     if float(POSTGRES_VERSION) >= 10.0:
         compose_file = 'docker-compose-replication.yaml'
-    with docker_run(
-        os.path.join(HERE, 'compose', compose_file),
-        conditions=[WaitFor(connect_to_pg)],
-        env_vars={"POSTGRES_IMAGE": POSTGRES_IMAGE, "POSTGRES_LOCALE": POSTGRES_LOCALE},
-    ):
-        yield e2e_instance, E2E_METADATA
+    return e2e_instance, E2E_METADATA
+    # with docker_run(
+    #     os.path.join(HERE, 'compose', compose_file),
+    #     conditions=[WaitFor(connect_to_pg)],
+    #     env_vars={"POSTGRES_IMAGE": POSTGRES_IMAGE, "POSTGRES_LOCALE": POSTGRES_LOCALE},
+    # ):
+    #     yield e2e_instance, E2E_METADATA
 
 
 @pytest.fixture
