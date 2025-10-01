d740b72920e09bdfa6193272a358a5d2caeee749
Zhengda Lu <zhengda.lu@datadoghq.com>
2025-10-01 18:45:04 +0000

    fix tests

--- sqlserver/tests/test_statements.py
+++ sqlserver/tests/test_statements.py
@@ -400,7 +400,9 @@ def test_statement_metrics_and_plans(
                 cursor, SQL_SERVER_QUERY_METRICS_COLUMNS
             )
 
-    expected_instance_tags.add("sqlserver_servername:{}".format(check.static_info_cache.get(STATIC_INFO_SERVERNAME)))
+    expected_instance_tags.add(
+        "sqlserver_servername:{}".format(check.static_info_cache.get(STATIC_INFO_SERVERNAME, "").lower())
+    )
     expected_instance_tags_with_db = expected_instance_tags | {"db:{}".format(database)}
 
     # dbm-metrics
