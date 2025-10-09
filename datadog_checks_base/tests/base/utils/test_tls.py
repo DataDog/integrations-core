19c1d88729aa33b435098d7eb76cdbded83d87ca
David Kirov <david.kirov@datadoghq.com>
2025-10-09 17:49:23 +0200

    Rename changelog

--- datadog_checks_base/tests/base/utils/test_tls.py
+++ datadog_checks_base/tests/base/utils/test_tls.py
@@ -1,10 +1,10 @@
 # (C) Datadog, Inc. 2019-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import logging
 import os
 import ssl
 from ssl import SSLContext
-import logging
 
 import pytest
 from mock import MagicMock, patch  # noqa: F401
