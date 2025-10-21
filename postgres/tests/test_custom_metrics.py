a32934f4b99bf619b50dcfc5a33c8568aaed43d1
Alex Torres <alejandro.torres@datadoghq.com>
2025-10-21 18:10:37 -0400

    [dbm] Fix logic for only_custom_queries parameter (#21708)
    * fix logic for only_custom_queries parameter
    
    * fix custom queries test
    
    * add changelog
    
    * run ddev test format

--- postgres/tests/test_custom_metrics.py
+++ postgres/tests/test_custom_metrics.py
@@ -141,3 +141,35 @@ def test_only_instance_custom_queries(aggregator, pg_instance, dd_run_check, int
 
         aggregator.assert_metric('custom.num', value=value, tags=custom_tags + ['query:custom'])
         aggregator.assert_metric('global_custom.num', value=value, tags=custom_tags + ['query:global_custom'], count=0)
+
+
+@pytest.mark.integration
+@pytest.mark.usefixtures('dd_environment')
+def test_only_custom_queries(aggregator, pg_instance, dd_run_check, integration_check):
+    pg_instance.update(
+        {
+            'only_custom_queries': True,
+            'custom_queries': [
+                {
+                    'metric_prefix': 'custom',
+                    'query': "SELECT letter, num FROM (VALUES (97, 'a'), (98, 'b'), (99, 'c')) AS t (num,letter)",
+                    'columns': [{'name': 'customtag', 'type': 'tag'}, {'name': 'num', 'type': 'gauge'}],
+                    'tags': ['query:custom'],
+                },
+            ],
+        }
+    )
+    postgres_check = integration_check(pg_instance)
+    dd_run_check(postgres_check)
+    tags = _get_expected_tags(postgres_check, pg_instance, with_db=True)
+
+    for tag in ('a', 'b', 'c'):
+        value = ord(tag)
+        custom_tags = [f'customtag:{tag}']
+        custom_tags.extend(tags)
+
+        aggregator.assert_metric('custom.num', value=value, tags=custom_tags + ['query:custom'])
+
+    running_tags = _get_expected_tags(postgres_check, pg_instance)
+    aggregator.assert_metric('postgresql.running', count=1, value=1, tags=running_tags)
+    aggregator.assert_all_metrics_covered()
