ea69ef122ae705fc16c6e2e3fe866d0353969754
Eric Weaver <eric.weaver@datadoghq.com>
2025-10-24 10:38:42 -0400

    Filter out errors triggered by monitoring user

--- mysql/datadog_checks/mysql/mysql.py
+++ mysql/datadog_checks/mysql/mysql.py
@@ -440,7 +440,9 @@ class MySql(AgentCheck):
         if self.global_variables.performance_schema_enabled:
             queries.extend([QUERY_USER_CONNECTIONS])
             if not self.is_mariadb and self.version.version_compatible((8, 0, 0)) and self._config.dbm_enabled:
-                queries.extend([QUERY_ERRORS_RAISED])
+                error_query = QUERY_ERRORS_RAISED.copy()
+                error_query['query'] = error_query['query'].format(user=self._config.user)
+                queries.extend([error_query])
         if self._index_metrics.include_index_metrics:
             queries.extend(self._index_metrics.queries)
         self._runtime_queries_cached = self._new_query_executor(queries)
