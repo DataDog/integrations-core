bf7e82af3e51e142ee131e720a370c57b6387369
Seth Samuel <seth.samuel@datadoghq.com>
2025-10-24 09:04:55 -0400

    AI Fixes

--- datadog_checks_base/datadog_checks/base/utils/db/schemas.py
+++ datadog_checks_base/datadog_checks/base/utils/db/schemas.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, TypedDict
 
-import orjson as json
+from datadog_checks.base.utils.format.json import json
 
 from .utils import now_ms
 
