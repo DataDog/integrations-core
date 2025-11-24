a17d65d1cdceae77747d84b34e983c161870b975
steveny91 <steven.yuen@datadoghq.com>
2025-11-24 13:23:16 -0500

    lint

--- ibm_mq/datadog_checks/ibm_mq/collectors/queue_metric_collector.py
+++ ibm_mq/datadog_checks/ibm_mq/collectors/queue_metric_collector.py
@@ -266,7 +266,7 @@ class QueueMetricCollector(object):
             for queue_info in response:
                 usage = KNOWN_USAGES.get(queue_info.get(pymqi.CMQC.MQIA_USAGE), 'unknown')
                 enriched_tags.append('queue_usage:{}'.format(usage))
- 
+
                 # Add queue description as tag if enabled
                 if self.config.add_description_tags and pymqi.CMQC.MQCA_Q_DESC in queue_info:
                     queue_desc = to_string(queue_info[pymqi.CMQC.MQCA_Q_DESC]).strip()
@@ -275,7 +275,7 @@ class QueueMetricCollector(object):
                             queue_desc = normalize_desc_tag(queue_desc)
                         if queue_desc:
                             enriched_tags.append('queue_desc:{}'.format(queue_desc))
- 
+
                 self._submit_queue_stats(queue_info, queue_name, enriched_tags)
         finally:
             if pcf is not None:
