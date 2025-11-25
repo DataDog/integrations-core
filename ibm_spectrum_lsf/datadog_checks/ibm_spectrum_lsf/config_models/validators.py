eb7a101e2035d231580eb04942bb09947b57b410
Sarah Witt <sarah.witt@datadoghq.com>
2025-11-24 23:24:04 -0500

    make a set

--- ibm_spectrum_lsf/datadog_checks/ibm_spectrum_lsf/config_models/validators.py
+++ ibm_spectrum_lsf/datadog_checks/ibm_spectrum_lsf/config_models/validators.py
@@ -5,7 +5,7 @@
 
 def initialize_instance(values, **kwargs):
     if 'metric_sources' in values:
-        valid_options = [
+        valid_options = {
             'lsclusters',
             'lshosts',
             'bhosts',
@@ -15,7 +15,7 @@ def initialize_instance(values, **kwargs):
             'bjobs',
             'lsload_gpu',
             'bhosts_gpu',
-        ]
+        }
         for val in values['metric_sources']:
             if val not in valid_options:
                 raise ValueError(f'Invalid metric source: {val}')
