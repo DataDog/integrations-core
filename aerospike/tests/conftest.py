bee5a6f832f4fd3b51d2f38db0adf80f8491f719
Ilia Kurenkov <ilia.kurenkov@datadoghq.com>
2025-12-19 17:28:48 +0100

    remove unused import

--- aerospike/tests/conftest.py
+++ aerospike/tests/conftest.py
@@ -6,7 +6,6 @@ from copy import deepcopy
 import aerospike
 import pytest
 
-from datadog_checks.base.utils.platform import Platform
 from datadog_checks.dev.conditions import WaitFor
 from datadog_checks.dev.docker import CheckDockerLogs, docker_run
 
