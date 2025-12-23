41c1c928883146e164f1d7924f9efdf1c95a2483
NouemanKHAL <noueman.khalikine@datadoghq.com>
2025-12-23 16:14:35 +0100

    inject provider as property

--- datadog_checks_base/datadog_checks/base/checks/base.py
+++ datadog_checks_base/datadog_checks/base/checks/base.py
@@ -193,6 +193,7 @@ class AgentCheck(object):
         instance = instances[0] if instances else None
 
         self.check_id = ''
+        self.provider = ''
         self.name = name  # type: str
         self.init_config = init_config  # type: InitConfigType
         self.agentConfig = agentConfig  # type: AgentConfigType
@@ -214,7 +215,7 @@ class AgentCheck(object):
         logger = logging.getLogger('{}.{}'.format(__name__, self.name))
         self.log = CheckLoggingAdapter(logger, self)
 
-        self.log.debug(f"provider: {kwargs.get('provider', '')}")
+        self.log.debug(f"provider: {self.provider}")
 
 
         metric_patterns = self.instance.get('metric_patterns', {}) if instance else {}
