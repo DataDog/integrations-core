7ddfd8c747d708b36e4328fa8cf865bad114eff6
NouemanKHAL <noueman.khalikine@datadoghq.com>
2025-12-26 19:01:00 +0100

    add secure_field property in the spec and the models

--- datadog_checks_dev/datadog_checks/dev/tooling/configuration/consumers/model/model_info.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/configuration/consumers/model/model_info.py
@@ -15,6 +15,8 @@ class ModelInfo:
         self.defaults_file_lines: List[str] = []
         self.validator_data = []
         self.deprecation_data = defaultdict(dict)
+        # List of (normalized_option_name, original_option_name) for fields marked as secure_field
+        self.secure_fields: List[tuple[str, str]] = []
 
     def update(self, section_model):
         """
@@ -24,6 +26,16 @@ class ModelInfo:
         self.defaults_file_lines.extend(section_model.defaults_file_lines)
         self.validator_data.extend(section_model.validator_data)
         self.deprecation_data.update(section_model.deprecation_data)
+        self.secure_fields.extend(section_model.secure_fields)
+
+    def add_secure_field(self, normalized_option_name: str, original_option_name: str):
+        """
+        Add a field to the secure fields list.
+
+        :param normalized_option_name: Normalized option name (used in code)
+        :param original_option_name: Original option name (used as field alias)
+        """
+        self.secure_fields.append((normalized_option_name, original_option_name))
 
     def add_type_validators(self, type_data: dict, option_name: str, normalized_option_name: str) -> List[str]:
         """
