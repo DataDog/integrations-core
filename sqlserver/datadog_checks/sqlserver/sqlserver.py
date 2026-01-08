5db8a368c84f8adcc7daa445b660e506a4850097
Jason Petersen <jason.petersen@datadoghq.com>
2026-01-08 09:40:43 -0700

    Improve use stmt escaping (#22277)
    * Improve escaping of db name in USE (sqlserver)
    
    * Update USE statement tests
    
    * Formatting fixes
    
    * Fix incorrect import
    
    Co-authored-by: Eric Weaver <eweaver755@gmail.com>
    
    * Add changelog line
    
    * Formatting fixes
    
    * Add comments clarifying need for escaping
    
    ---------
    
    Co-authored-by: Eric Weaver <eweaver755@gmail.com>

--- sqlserver/datadog_checks/sqlserver/sqlserver.py
+++ sqlserver/datadog_checks/sqlserver/sqlserver.py
@@ -95,7 +95,6 @@ from datadog_checks.sqlserver.const import (
     STATIC_INFO_SERVERNAME,
     STATIC_INFO_VERSION,
     STATIC_INFO_YEAR,
-    SWITCH_DB_STATEMENT,
     VALID_METRIC_TYPES,
     expected_sys_databases_columns,
 )
@@ -828,7 +827,8 @@ class SQLServer(DatabaseCheck):
                 for db in self.databases:
                     check_err_message = "Database {} connection service check failed: {}"
                     try:
-                        cursor.execute(SWITCH_DB_STATEMENT.format(db.name))
+                        switch_db_statement = construct_use_statement(db.name)
+                        cursor.execute(switch_db_statement)
                         cursor.execute(DATABASE_SERVICE_CHECK_QUERY)
                         cursor.fetchall()
                         self.handle_service_check(AgentCheck.OK, self.connection.get_host_with_port(), db.name, False)
@@ -843,7 +843,8 @@ class SQLServer(DatabaseCheck):
                         )
                         continue
                 # Switch DB back to MASTER
-                cursor.execute(SWITCH_DB_STATEMENT.format(self.connection.DEFAULT_DATABASE))
+                switch_db_statement = construct_use_statement(self.connection.DEFAULT_DATABASE)
+                cursor.execute(switch_db_statement)
 
     def get_databases(self):
         engine_edition = self.static_info_cache.get(STATIC_INFO_ENGINE_EDITION)
