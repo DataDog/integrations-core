e61d957d5031130a33020202c26bb61c28f4df5f
Juanpe Araque <juanpedro.araque@datadoghq.com>
2026-01-22 11:21:17 +0100

    Improve our CI visibility reporting and streamline test-target (#22355)
    * Use official datadog GH action to monitor CI visibility
    
    * Add changelog
    
    * Add some debugging for failing tests
    
    * Remove action and ensure ddtrace is a depdnency of the default environment
    
    * Revert changes for debugging in test_agent_packages
    
    * Remove profiling
    
    * Remove trace analytics
    
    * Disable subprocess tracing
    
    * Try removing git metadta gathering
    
    * Debug messages to understand git issues
    
    * Fix formattingh
    
    * Fix error in debug code
    
    * Fix formatting
    
    * Make sure we unshallwo the repo
    
    * Also unshallwo for windows setup for ddev
    
    * Add test environment name to differentiate between e2e and unit tests
    
    * Update documentation
    
    * Add ci visibility to e2e tests as well
    
    * Try reducing the size of test-target
    
    * Streamline test-target to reduce the object footprint on actions
    
    * Ensure python version set to 3.13 when input is an empty string
    
    * Ensure python version is updatable through the py upgrade script
    
    * Print actual ddev command run when executing tests
    
    * Update ddev env test command to respect trace enabled option
    
    * Fix update_py_config script patterns
    
    * Fix AppEnvVar for enable tracing
    
    * Modify integration tag to target
    
    * Tag jobs with the same tags as we tag tests
    
    * Add target_env tag to the test workflow
    
    * Make bash script more robust
    
    * Avoid running flaky tests in pr-all
    
    * Fix handling of pytest args
    
    * Address comments

---
+++ .github/workflows/scripts/run-e2e-tests.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Run E2E tests for test-target workflow
+# This script is called from test-target.yml to reduce YAML size
+
+# Required environment variables:
+# INPUT_REPO, INPUT_TARGET, INPUT_PLATFORM, INPUT_PYTEST_ARGS
+# INPUT_TARGET_ENV (optional) - environment to run tests against
+# INPUT_IS_LATEST (optional) - set to "true" for latest version tests
+# INPUT_SESSION_NAME (optional) - custom session name
+
+export DD_TEST_SESSION_NAME="${INPUT_SESSION_NAME:-e2e-tests}"
+
+# Set tracing flag
+# TODO: SQL Server on Windows crashes when tracing is enabled with error File Windows fatal exception: access violation
+if [[ "$INPUT_REPO" == 'core' && ( "$INPUT_TARGET" != 'sqlserver' || "$INPUT_PLATFORM" != 'windows' ) ]]; then
+  export DDEV_TEST_ENABLE_TRACING="1"
+else
+  export DDEV_TEST_ENABLE_TRACING="0"
+fi
+
+# Set E2E flags based on repo (latest always uses --base --new-env)
+if [[ "$INPUT_REPO" == 'core' || "$INPUT_IS_LATEST" == 'true' ]]; then
+  E2E_FLAGS="--base --new-env"
+else
+  E2E_FLAGS="--dev"
+fi
+
+# Build target arguments
+# For latest: INPUT_TARGET already contains "target:latest", no separate env
+# For regular: INPUT_TARGET is just the target, INPUT_TARGET_ENV is the env
+if [[ -n "$INPUT_TARGET_ENV" ]]; then
+  TARGET_ARGS="$INPUT_TARGET $INPUT_TARGET_ENV"
+else
+  TARGET_ARGS="$INPUT_TARGET"
+fi
+
+# Set positional arguments ($@) based on inputs
+# This is done to ensure we handle pytest args correctly.
+if [[ -n "$INPUT_PYTEST_ARGS" ]]; then
+  # Use eval to correctly parse quoted arguments (e.g. -m "not flaky")
+  eval "set -- $INPUT_PYTEST_ARGS"
+elif [[ "$INPUT_IS_LATEST" == 'true' ]]; then
+  # Latest version without pytest args: include 'all' explicitly
+  set -- "all"
+else
+  # Clear positional arguments
+  set --
+fi
+
+set +e
+set -x
+ddev env test $E2E_FLAGS --junit $TARGET_ARGS -- "$@" -k "not fips"
+exit_code=$?
+set +x
+
+if [[ $exit_code -eq 5 ]]; then
+  echo "No tests were collected."
+  exit 0
+else
+  exit $exit_code
+fi
---
+++ .github/workflows/scripts/run-unit-integration-tests.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Run Unit & Integration tests for test-target workflow
+# This script is called from test-target.yml to reduce YAML size
+
+# Required environment variables:
+# INPUT_REPO, INPUT_TARGET, INPUT_PLATFORM, INPUT_MINIMUM_BASE_PACKAGE
+# INPUT_TARGET_STR, INPUT_PYTEST_ARGS
+
+# Set tracing flag
+# TODO: SQL Server on Windows crashes when tracing is enabled with error File Windows fatal exception: access violation
+if [[ "$INPUT_REPO" == 'core' && ( "$INPUT_TARGET" != 'sqlserver' || "$INPUT_PLATFORM" != 'windows' ) ]]; then
+  export DDEV_TEST_ENABLE_TRACING="1"
+else
+  export DDEV_TEST_ENABLE_TRACING="0"
+fi
+
+# Set test session name and extra flags based on minimum-base-package
+if [[ "$INPUT_MINIMUM_BASE_PACKAGE" == 'true' ]]; then
+  export DD_TEST_SESSION_NAME="minimum-base-package-tests"
+  EXTRA_FLAGS="--compat --recreate"
+  COV_FLAG=""
+  VERBOSE_FLAG=""
+else
+  export DD_TEST_SESSION_NAME="unit-tests"
+  EXTRA_FLAGS=""
+  COV_FLAG="--cov"
+  VERBOSE_FLAG="-v"
+fi
+
+# Parse INPUT_PYTEST_ARGS with proper quote handling using eval
+# This allows passing args like '-m "not flaky"' correctly
+if [[ -n "$INPUT_PYTEST_ARGS" ]]; then
+  eval "set -- $INPUT_PYTEST_ARGS"
+else
+  set --
+fi
+
+set +e
+set -x
+ddev $VERBOSE_FLAG test $EXTRA_FLAGS $COV_FLAG --junit "$INPUT_TARGET_STR" -- "$@" -k "not fips"
+exit_code=$?
+set +x
+
+if [[ "$INPUT_PYTEST_ARGS" == '-m flaky' && $exit_code -eq 5 ]]; then
+  # Flaky test count can be zero, this is done to avoid pipeline failure
+  echo "No tests were collected."
+  exit 0
+else
+  exit $exit_code
+fi
