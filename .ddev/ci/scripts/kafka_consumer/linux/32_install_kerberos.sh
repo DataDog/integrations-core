4f6634eda0ccbfc07d939cb83edadb9ca69238d6
Piotr Wolski <piotr.wolski42@gmail.com>
2026-01-27 20:06:09 -0700

    update libraries

--- .ddev/ci/scripts/kafka_consumer/linux/32_install_kerberos.sh
+++ .ddev/ci/scripts/kafka_consumer/linux/32_install_kerberos.sh
@@ -3,6 +3,8 @@
 set -ex
 
 sudo apt update
+# Upgrade libc6 to ensure sys/random.h is available (required by librdkafka 2.13.0)
+sudo apt install -y --no-install-recommends libc6 libc-dev
 sudo apt install -y --no-install-recommends build-essential libkrb5-dev wget software-properties-common lsb-release gcc make python3 python3-pip python3-dev libsasl2-modules-gssapi-mit krb5-user
 
 # Install librdkafka from source since no binaries are available for the distribution we use on the CI:
