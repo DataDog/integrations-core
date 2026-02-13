a00e2d91e8deaeedb0a536c054bebf1e407947a5
mwdd146980 <morgan.wang@datadoghq.com>
2026-02-13 14:09:54 -0500

    refactor(datadog_checks_base): use mock_http_response in test_proxy
    Replace mock_requests_get and direct Session.get patches with mock_http_response.

--- datadog_checks_base/tests/base/utils/http/test_proxy.py
+++ datadog_checks_base/tests/base/utils/http/test_proxy.py
@@ -2,26 +2,14 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from collections import OrderedDict
-from io import BytesIO
 from unittest import mock
 
 import pytest
-from requests import Response, Session
 
 from datadog_checks.base.utils.http import RequestsWrapper
 from datadog_checks.dev import EnvVars
 
 pytestmark = [pytest.mark.unit]
-
-
-@pytest.fixture(scope="module")
-def mock_requests_get():
-    with mock.patch.object(Session, 'get', autospec=True) as mock_get:
-        response = Response()
-        response.status_code = 200
-        response.raw = BytesIO("Proxied request successful!".encode('utf-8'))
-        mock_get.return_value = response
-        yield mock_get
 
 
 def test_config_default():
@@ -108,18 +96,19 @@ def test_config_proxy_skip_init_config():
     [('http://www.google.com', 'HTTP_PROXY'), ('https://www.google.com', 'HTTPS_PROXY')],
     ids=['http', 'https'],
 )
-def test_proxy_env_vars_skip(mock_requests_get: mock.MagicMock, url: str, env_var: str):
+def test_proxy_env_vars_skip(mock_http_response, url: str, env_var: str):
     instance = {'skip_proxy': True}
     init_config = {}
     http = RequestsWrapper(instance, init_config)
 
     expected_proxies = {'http': '', 'https': ''}
 
+    mock_get = mock_http_response('')
     with EnvVars({env_var: 'http://1.2.3.4:567'}):
         http.get(url)
 
         # Since skip is true, we inject empty proxies to the call
-        actual_proxies = mock_requests_get.call_args[1]['proxies']
+        actual_proxies = mock_get.call_args[1]['proxies']
         assert actual_proxies == expected_proxies
 
 
@@ -128,14 +117,15 @@ def test_proxy_env_vars_skip(mock_requests_get: mock.MagicMock, url: str, env_va
     [('http://www.google.com', 'HTTP_PROXY'), ('https://www.google.com', 'HTTPS_PROXY')],
     ids=['http', 'https'],
 )
-def test_proxy_env_vars_override_skip_fail(mock_requests_get: mock.MagicMock, url: str, env_var: str):
+def test_proxy_env_vars_override_skip_fail(mock_http_response, url: str, env_var: str):
     instance = {'skip_proxy': True}
     init_config = {}
     http = RequestsWrapper(instance, init_config)
 
+    mock_get = mock_http_response('')
     with EnvVars({env_var: 'http://1.2.3.4:567'}):
         http.get(url, timeout=1, proxies=None)
-        actual_proxies = mock_requests_get.call_args[1]['proxies']
+        actual_proxies = mock_get.call_args[1]['proxies']
 
         # Even with skip true, we ignore it to call get with the proxies supplied to the get method
         assert actual_proxies is None
@@ -157,7 +147,7 @@ def test_proxy_env_vars_override_skip_fail(mock_requests_get: mock.MagicMock, ur
         pytest.param({}, True, id='without_no_proxy'),
     ],
 )
-def test_no_proxy_bypass(mock_requests_get: mock.MagicMock, url: str, no_proxies: dict, should_proxy: bool):
+def test_no_proxy_bypass(mock_http_response, url: str, no_proxies: dict, should_proxy: bool):
     proxies = {'http': 'http://1.2.3.4:567', 'https': 'https://1.2.3.4:567'}
     instance = {'proxy': proxies | no_proxies}
     init_config = {}
@@ -166,12 +156,13 @@ def test_no_proxy_bypass(mock_requests_get: mock.MagicMock, url: str, no_proxies
     # Validate that the proxies are injected appropriately
     expected_proxies = proxies if should_proxy else {'http': '', 'https': ''}
 
+    mock_get = mock_http_response('')
     http.get(url)
-    actual_proxies = mock_requests_get.call_args[1]['proxies']
+    actual_proxies = mock_get.call_args[1]['proxies']
     assert actual_proxies == expected_proxies
 
 
-def test_no_proxy_uris_coverage():
+def test_no_proxy_uris_coverage(mock_http_response):
     http = RequestsWrapper({}, {})
 
     # Coverage is not smart enough to detect that looping an empty
@@ -181,6 +172,7 @@ def test_no_proxy_uris_coverage():
     http.no_proxy_uris.__iter__ = lambda self, *args, **kwargs: iter([])
     http.no_proxy_uris.__bool__ = lambda self, *args, **kwargs: True
 
+    mock_http_response('')
     http.get('https://www.google.com')
 
 
@@ -223,24 +215,24 @@ def test_no_proxy_uris_coverage():
         ),
     ],
 )
-def test_proxy_passes_right_params_to_requests(proxy, expected_proxy, url):
+def test_proxy_passes_right_params_to_requests(mock_http_response, proxy, expected_proxy, url):
     instance = {'proxy': proxy}
     init_config = {}
 
     http = RequestsWrapper(instance, init_config)
 
-    with mock.patch('requests.Session.get') as mock_get:
-        http.get(url)
+    mock_get = mock_http_response('')
+    http.get(url)
 
-        call_args = {
-            'auth': None,
-            'cert': None,
-            'headers': OrderedDict(
-                [('User-Agent', 'Datadog Agent/0.0.0'), ('Accept', '*/*'), ('Accept-Encoding', 'gzip, deflate')]
-            ),
-            'proxies': expected_proxy,
-            'timeout': (10.0, 10.0),
-            'verify': True,
-            'allow_redirects': True,
-        }
-        mock_get.assert_called_with(url, **call_args)
+    call_args = {
+        'auth': None,
+        'cert': None,
+        'headers': OrderedDict(
+            [('User-Agent', 'Datadog Agent/0.0.0'), ('Accept', '*/*'), ('Accept-Encoding', 'gzip, deflate')]
+        ),
+        'proxies': expected_proxy,
+        'timeout': (10.0, 10.0),
+        'verify': True,
+        'allow_redirects': True,
+    }
+    mock_get.assert_called_with(url, **call_args)
