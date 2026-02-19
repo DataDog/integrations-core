19493bb51d8e6203789bf80c291a7d344a347b1c
Austin Bergstrom <austin.bergstrom@datadoghq.com>
2026-02-19 15:04:53 -0500

    ddev test --fmt sqlserver

--- sqlserver/datadog_checks/sqlserver/database_metrics/database_memory_metrics.py
+++ sqlserver/datadog_checks/sqlserver/database_metrics/database_memory_metrics.py
@@ -63,7 +63,5 @@ class SqlserverDatabaseMemoryMetrics(SqlserverDatabaseMetricsBase):
 
     def __repr__(self) -> str:
         return (
-            f"{self.__class__.__name__}("
-            f"enabled={self.enabled}, "
-            f"collection_interval={self.collection_interval})"
+            f"{self.__class__.__name__}(" f"enabled={self.enabled}, " f"collection_interval={self.collection_interval})"
         )
