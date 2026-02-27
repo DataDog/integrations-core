51eae2874c95f36b0e4a01859e64115b3d558caf
Juanpe Araque <juanpedro.araque@datadoghq.com>
2026-02-27 15:38:32 +0100

    Move RENAME_LABELS_MAP to check.py and delete metrics.py
    Move the small RENAME_LABELS_MAP constant into check.py and remove
    metrics.py entirely. Write rename_labels to the defaults layer of the
    ChainMap via result.maps[-1] to match the original behavior.

--- krakend/datadog_checks/krakend/metrics.py
+++
@@ -1,8 +0,0 @@
-# (C) Datadog, Inc. 2025-present
-# All rights reserved
-# Licensed under a 3-clause BSD style license (see LICENSE)
-
-RENAME_LABELS_MAP = {
-    "service_version": "krakend.service_version",
-    "service_name": "krakend.service_name",
-}
