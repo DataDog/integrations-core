a6979fe24c13323e0e11ae4fbb8170eb5dac8465
Luis Orofino <luis.orofino@datadoghq.com>
2026-03-13 15:28:34 +0100

    separated ToolProtocol from base and changed __init__

---
+++ ddev/src/ddev/ai/tools/protocol.py
@@ -0,0 +1,20 @@
+# (C) Datadog, Inc. 2026-present
+# All rights reserved
+# Licensed under a 3-clause BSD style license (see LICENSE)
+
+from typing import Protocol
+
+from anthropic.types import ToolParam
+
+from .types import ToolResult
+
+
+class ToolProtocol[TInput](Protocol):
+    @property
+    def name(self) -> str: ...
+    @property
+    def description(self) -> str: ...
+    @property
+    def definition(self) -> ToolParam: ...
+    async def run(self, raw: dict[str, object]) -> ToolResult: ...
+    async def __call__(self, tool_input: TInput) -> ToolResult: ...
