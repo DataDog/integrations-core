b4d9462934c7e53da958c849fbd0dd4cb42b476d
Luis Orofino <luis.orofino@datadoghq.com>
2026-03-18 17:33:11 +0100

    init with nothing inside

--- ddev/src/ddev/ai/tools/__init__.py
+++ ddev/src/ddev/ai/tools/__init__.py
@@ -1,23 +1,3 @@
 # (C) Datadog, Inc. 2026-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-
-from ddev.ai.tools.core.base import BaseTool
-from ddev.ai.tools.core.protocol import ToolProtocol
-from ddev.ai.tools.core.registry import ToolRegistry
-from ddev.ai.tools.core.types import ToolResult
-from ddev.ai.tools.shell.grep import GrepTool
-from ddev.ai.tools.shell.list_files import ListFilesTool
-from ddev.ai.tools.shell.mkdir import MkdirTool
-from ddev.ai.tools.shell.read_file import ReadFileTool
-
-__all__ = [
-    "BaseTool",
-    "ToolProtocol",
-    "ToolRegistry",
-    "ToolResult",
-    "GrepTool",
-    "ListFilesTool",
-    "MkdirTool",
-    "ReadFileTool",
-]
