c4a059061ada8c2db58be6083964cc7fcbb078b5
steveny91 <steven.yuen@datadoghq.com>
2026-03-19 01:38:25 -0400

    fix tests

--- datadog_checks_base/tests/base/checks/openmetrics/test_v2/scraper/test_aggregator.py
+++ datadog_checks_base/tests/base/checks/openmetrics/test_v2/scraper/test_aggregator.py
@@ -1,119 +1,115 @@
 # (C) Datadog, Inc. 2025-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-from prometheus_client.samples import Sample
+from ..utils import get_check
 
-from datadog_checks.base.checks.openmetrics.v2.scraper.aggregator import (
-    aggregate_sample_data,
-    should_aggregate,
-)
+GAUGE_PAYLOAD = """
+# HELP cars_in_lot The current number of cars in the lot
+# TYPE cars_in_lot gauge
+cars_in_lot{make="honda", model="civic", color="#FF5733"} 5
+cars_in_lot{make="honda", model="civic", color="#4CAF50"} 3
+cars_in_lot{make="honda", model="civic", color="#4287f5"} 4
+cars_in_lot{make="toyota", model="corolla", color="#FFC300"} 2
+cars_in_lot{make="toyota", model="corolla", color="#900C3F"} 3
+cars_in_lot{make="toyota", model="corolla", color="#DAF7A6"} 1
+""".strip()
 
-# --- should_aggregate ---
+COUNTER_PAYLOAD = """
+# HELP car_counter_total The number of cars seen coming into lot
+# TYPE car_counter_total counter
+car_counter_total{make="honda", model="civic", color="#FF5733"} 120
+car_counter_total{make="honda", model="civic", color="#4CAF50"} 95
+car_counter_total{make="honda", model="civic", color="#4287f5"} 88
+car_counter_total{make="toyota", model="corolla", color="#FFC300"} 70
+car_counter_total{make="toyota", model="corolla", color="#900C3F"} 55
+car_counter_total{make="toyota", model="corolla", color="#DAF7A6"} 60
+""".strip()
 
-
-def test_should_aggregate_given_empty_exclude_labels_returns_false():
-    assert should_aggregate(set()) is False
-
-
-def test_should_aggregate_given_nonempty_exclude_labels_returns_true():
-    assert should_aggregate({'color'}) is True
-
-
-# --- gauge aggregation ---
-
-
-def test_aggregate_gauge_given_colliding_tags_returns_summed_value():
-    sample_data = [
-        (Sample('cars_in_lot', {'make': 'honda'}, 5.0), ['make:honda'], ''),
-        (Sample('cars_in_lot', {'make': 'honda'}, 3.0), ['make:honda'], ''),
-        (Sample('cars_in_lot', {'make': 'honda'}, 4.0), ['make:honda'], ''),
-    ]
-    result = list(aggregate_sample_data(iter(sample_data), 'gauge'))
-    assert len(result) == 1
-    assert result[0][0].value == 12.0
-    assert result[0][1] == ['make:honda']
-
-
-def test_aggregate_gauge_given_no_collisions_returns_all_samples():
-    sample_data = [
-        (Sample('cars_in_lot', {'make': 'honda'}, 5.0), ['make:honda'], ''),
-        (Sample('cars_in_lot', {'make': 'toyota'}, 3.0), ['make:toyota'], ''),
-    ]
-    result = list(aggregate_sample_data(iter(sample_data), 'gauge'))
-    assert len(result) == 2
-    values = {r[0].value for r in result}
-    assert values == {5.0, 3.0}
+HISTOGRAM_PAYLOAD = """
+# HELP request_duration A histogram of request durations
+# TYPE request_duration histogram
+request_duration_bucket{handler="api", color="red", le="0.1"} 10
+request_duration_bucket{handler="api", color="blue", le="0.1"} 5
+request_duration_bucket{handler="api", color="red", le="1.0"} 20
+request_duration_bucket{handler="api", color="blue", le="1.0"} 12
+request_duration_bucket{handler="api", color="red", le="+Inf"} 25
+request_duration_bucket{handler="api", color="blue", le="+Inf"} 15
+request_duration_sum{handler="api", color="red"} 100.5
+request_duration_sum{handler="api", color="blue"} 50.3
+request_duration_count{handler="api", color="red"} 25
+request_duration_count{handler="api", color="blue"} 15
+""".strip()
 
 
-# --- counter aggregation ---
+def test_gauge_given_exclude_labels_returns_summed_values(aggregator, dd_run_check, mock_http_response):
+    mock_http_response(GAUGE_PAYLOAD)
+    check = get_check({'metrics': ['.+'], 'exclude_labels': ['color']})
+    dd_run_check(check)
+
+    aggregator.assert_metric(
+        'test.cars_in_lot',
+        12.0,
+        metric_type=aggregator.GAUGE,
+        tags=['endpoint:test', 'make:honda', 'model:civic'],
+    )
+    aggregator.assert_metric(
+        'test.cars_in_lot',
+        6.0,
+        metric_type=aggregator.GAUGE,
+        tags=['endpoint:test', 'make:toyota', 'model:corolla'],
+    )
+    aggregator.assert_all_metrics_covered()
 
 
-def test_aggregate_counter_given_colliding_tags_returns_summed_value():
-    sample_data = [
-        (Sample('car_counter_total', {'make': 'honda'}, 120.0), ['make:honda'], ''),
-        (Sample('car_counter_total', {'make': 'honda'}, 95.0), ['make:honda'], ''),
-        (Sample('car_counter_total', {'make': 'honda'}, 88.0), ['make:honda'], ''),
-    ]
-    result = list(aggregate_sample_data(iter(sample_data), 'counter'))
-    assert len(result) == 1
-    assert result[0][0].value == 303.0
+def test_gauge_given_no_exclude_labels_returns_individual_values(aggregator, dd_run_check, mock_http_response):
+    mock_http_response(GAUGE_PAYLOAD)
+    check = get_check({'metrics': ['.+']})
+    dd_run_check(check)
+
+    aggregator.assert_metric(
+        'test.cars_in_lot',
+        5.0,
+        tags=['endpoint:test', 'make:honda', 'model:civic', 'color:#FF5733'],
+    )
+    aggregator.assert_metric(
+        'test.cars_in_lot',
+        3.0,
+        tags=['endpoint:test', 'make:honda', 'model:civic', 'color:#4CAF50'],
+    )
 
 
-# --- histogram and summary pass through unchanged ---
+def test_counter_given_exclude_labels_submits_summed_value(aggregator, dd_run_check, mock_http_response):
+    mock_http_response(COUNTER_PAYLOAD)
+    check = get_check({'metrics': ['.+'], 'exclude_labels': ['color']})
+    dd_run_check(check)
+
+    aggregator.assert_metric(
+        'test.car_counter.count',
+        303.0,
+        metric_type=aggregator.MONOTONIC_COUNT,
+        tags=['endpoint:test', 'make:honda', 'model:civic'],
+    )
+    aggregator.assert_metric(
+        'test.car_counter.count',
+        185.0,
+        metric_type=aggregator.MONOTONIC_COUNT,
+        tags=['endpoint:test', 'make:toyota', 'model:corolla'],
+    )
 
 
-def test_aggregate_histogram_given_colliding_tags_passes_through_unchanged():
-    sample_data = [
-        (Sample('req_bucket', {'le': '10'}, 5.0), ['le:10'], ''),
-        (Sample('req_bucket', {'le': '10'}, 3.0), ['le:10'], ''),
-    ]
-    result = list(aggregate_sample_data(iter(sample_data), 'histogram'))
-    assert len(result) == 2
-    assert result[0][0].value == 5.0
-    assert result[1][0].value == 3.0
+def test_histogram_given_exclude_labels_passes_through_unchanged(aggregator, dd_run_check, mock_http_response):
+    mock_http_response(HISTOGRAM_PAYLOAD)
+    check = get_check({'metrics': ['.+'], 'exclude_labels': ['color']})
+    dd_run_check(check)
+    dd_run_check(check)
 
-
-def test_aggregate_summary_given_colliding_tags_passes_through_unchanged():
-    sample_data = [
-        (Sample('latency_sum', {}, 1000.0), [], ''),
-        (Sample('latency_sum', {}, 500.0), [], ''),
-    ]
-    result = list(aggregate_sample_data(iter(sample_data), 'summary'))
-    assert len(result) == 2
-
-
-# --- hostname grouping ---
-
-
-def test_aggregate_gauge_given_different_hostnames_returns_separate_groups():
-    sample_data = [
-        (Sample('cpu', {}, 10.0), ['app:web'], 'host-a'),
-        (Sample('cpu', {}, 20.0), ['app:web'], 'host-b'),
-    ]
-    result = list(aggregate_sample_data(iter(sample_data), 'gauge'))
-    assert len(result) == 2
-
-
-# --- tag ordering ---
-
-
-def test_aggregate_gauge_given_unsorted_tags_groups_correctly():
-    sample_data = [
-        (Sample('m', {}, 1.0), ['b:2', 'a:1'], ''),
-        (Sample('m', {}, 2.0), ['a:1', 'b:2'], ''),
-    ]
-    result = list(aggregate_sample_data(iter(sample_data), 'gauge'))
-    assert len(result) == 1
-    assert result[0][0].value == 3.0
-
-
-# --- unrecognized metric type passes through ---
-
-
-def test_aggregate_given_unknown_metric_type_passes_through():
-    sample_data = [
-        (Sample('info_metric', {}, 1.0), ['app:web'], ''),
-        (Sample('info_metric', {}, 1.0), ['app:web'], ''),
-    ]
-    result = list(aggregate_sample_data(iter(sample_data), 'info'))
-    assert len(result) == 2
+    aggregator.assert_metric(
+        'test.request_duration.count',
+        metric_type=aggregator.MONOTONIC_COUNT,
+        tags=['endpoint:test', 'handler:api'],
+    )
+    aggregator.assert_metric(
+        'test.request_duration.sum',
+        metric_type=aggregator.MONOTONIC_COUNT,
+        tags=['endpoint:test', 'handler:api'],
+    )
