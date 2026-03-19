6d38db7e1df939b68853370d2f543d7fc88a5209
NouemanKHAL <noueman.khalikine@datadoghq.com>
2026-03-19 18:05:32 +0100

    remove ValueError catch from base.py

--- datadog_checks_base/datadog_checks/base/checks/base.py
+++ datadog_checks_base/datadog_checks/base/checks/base.py
@@ -642,8 +642,6 @@ class AgentCheck(object):
                     value = config.get(field_name)
                     if value is not None:
                         check_field_trusted_provider(field_name, value, security_config)
-            except ValueError as e:
-                raise ConfigurationError(str(e)) from None
             except ValidationError as e:
                 errors = e.errors()
                 num_errors = len(errors)
