94c9ff52131903a27eb1d952973489ab6789c4d1
Phil Lee <philip.lee@datadoghq.com>
2026-03-30 11:36:39 -0400

    Add missing ABOUTME comments to __init__.py and __about__.py

--- nifi/datadog_checks/nifi/__about__.py
+++ nifi/datadog_checks/nifi/__about__.py
@@ -1,3 +1,6 @@
+# ABOUTME: Version metadata for the NiFi integration package.
+# ABOUTME: Single source of truth for the package version string.
+
 # (C) Datadog, Inc. 2026-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
--- nifi/datadog_checks/nifi/__init__.py
+++ nifi/datadog_checks/nifi/__init__.py
@@ -1,3 +1,6 @@
+# ABOUTME: Entry point for the NiFi Datadog integration package.
+# ABOUTME: Exposes NifiCheck and package version for agent discovery.
+
 # (C) Datadog, Inc. 2026-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
