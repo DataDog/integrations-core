4cb9bb5e1230da4093aee679ccc07eb6a52436e5
steveny91 <steven.yuen@datadoghq.com>
2026-04-08 15:22:27 -0400

    test validations

--- postgres/datadog_checks/postgres/__init__.py
+++ postgres/datadog_checks/postgres/__init__.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
+from datadog_checks.checks import AgentCheck  # deliberately wrong import path for CI testing
 from .__about__ import __version__
 from .postgres import PostgreSql
 
