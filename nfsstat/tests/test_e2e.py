ea5fa6b32f5eb8704353ea6fab6797cce4896c2b
sarah-witt <sarah.witt@datadoghq.com>
2026-04-10 13:06:11 -0400

    Add test for embedded nfsiostat

--- nfsstat/tests/test_e2e.py
+++ nfsstat/tests/test_e2e.py
@@ -5,7 +5,7 @@ import pytest
 
 from datadog_checks.dev.utils import get_metadata_metrics
 
-from .common import CONFIG, METRICS
+from .common import CONFIG, CONFIG_BUNDLED_BINARY, METRICS
 
 
 @pytest.mark.e2e
@@ -17,3 +17,11 @@ def test_e2e(dd_agent_check):
 
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+
+
+@pytest.mark.e2e
+def test_e2e_bundled_binary_no_mounts(dd_agent_check):
+    """Verify the agent's bundled nfsiostat binary exists and the check handles no NFS mounts gracefully."""
+    aggregator = dd_agent_check(CONFIG_BUNDLED_BINARY)
+
+    aggregator.assert_all_metrics_covered()
