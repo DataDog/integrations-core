c5cf3ae93742c8af4da969ed50f904f30da5a16f
sarah-witt <sarah.witt@datadoghq.com>
2026-04-14 11:47:01 -0400

    Add auth token config

--- dell_powerflex/datadog_checks/dell_powerflex/config_models/defaults.py
+++ dell_powerflex/datadog_checks/dell_powerflex/config_models/defaults.py
@@ -8,6 +8,14 @@
 #     ddev -x validate models -s <INTEGRATION_NAME>
 
 
+def instance_allow_redirects():
+    return True
+
+
+def instance_auth_type():
+    return 'basic'
+
+
 def instance_disable_generic_tags():
     return False
 
@@ -20,5 +28,53 @@ def instance_enable_legacy_tags_normalization():
     return True
 
 
+def instance_kerberos_auth():
+    return 'disabled'
+
+
+def instance_kerberos_delegate():
+    return False
+
+
+def instance_kerberos_force_initiate():
+    return False
+
+
+def instance_log_requests():
+    return False
+
+
 def instance_min_collection_interval():
     return 15
+
+
+def instance_persist_connections():
+    return False
+
+
+def instance_request_size():
+    return 16
+
+
+def instance_skip_proxy():
+    return False
+
+
+def instance_timeout():
+    return 10
+
+
+def instance_tls_ignore_warning():
+    return False
+
+
+def instance_tls_use_host_header():
+    return False
+
+
+def instance_tls_verify():
+    return True
+
+
+def instance_use_legacy_auth_encoding():
+    return True
