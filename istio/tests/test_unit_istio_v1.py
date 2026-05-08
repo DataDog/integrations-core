ce89c1420f8e81516563a5f6b1d5da4fa04a4c49
mwdd146980 <morgan.wang@datadoghq.com>
2026-05-08 16:15:49 -0400

    Migrate linkerd + istio v1 tests off requests_mock to mock_openmetrics_http

--- istio/tests/test_unit_istio_v1.py
+++ istio/tests/test_unit_istio_v1.py
@@ -1,22 +1,22 @@
 # (C) Datadog, Inc. 2024-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
-import requests_mock
-
+from datadog_checks.base.utils.http_testing import MockHTTPResponse
 from datadog_checks.istio import Istio
 
 from . import common
 from .utils import _assert_tags_excluded, get_response
 
 
-def test_legacy_istiod(aggregator, dd_run_check):
+def test_legacy_istiod(aggregator, dd_run_check, mock_openmetrics_http):
     """
     Test the istiod deployment endpoint for v1.5+ check for OpenMetricsV1 implementation
     """
+    mock_openmetrics_http.get.return_value = MockHTTPResponse(
+        content=get_response('1.5', 'istiod.txt'), headers={'Content-Type': 'text/plain'}
+    )
     check = Istio('istio', {}, [common.MOCK_LEGACY_ISTIOD_INSTANCE])
-    with requests_mock.Mocker() as metric_request:
-        metric_request.get('http://localhost:8080/metrics', text=get_response('1.5', 'istiod.txt'))
-        dd_run_check(check)
+    dd_run_check(check)
 
     for metric in common.ISTIOD_METRICS:
         aggregator.assert_metric(metric)
@@ -24,15 +24,16 @@ def test_legacy_istiod(aggregator, dd_run_check):
     aggregator.assert_all_metrics_covered()
 
 
-def test_legacy_proxy_mesh(aggregator, dd_run_check):
+def test_legacy_proxy_mesh(aggregator, dd_run_check, mock_openmetrics_http):
     """
     Test proxy mesh check for OpenMetricsV1 implementation
     """
+    mock_openmetrics_http.get.return_value = MockHTTPResponse(
+        content=get_response('1.5', 'istio-proxy.txt'), headers={'Content-Type': 'text/plain'}
+    )
     check = Istio(common.CHECK_NAME, {}, [common.MOCK_LEGACY_MESH_INSTANCE])
+    dd_run_check(check)
 
-    with requests_mock.Mocker() as metric_request:
-        metric_request.get('http://localhost:15090/metrics', text=get_response('1.5', 'istio-proxy.txt'))
-        dd_run_check(check)
     for metric in common.LEGACY_MESH_METRICS + common.MESH_MERICS_1_5:
         aggregator.assert_metric(metric)
 
@@ -41,19 +42,19 @@ def test_legacy_proxy_mesh(aggregator, dd_run_check):
     aggregator.assert_all_metrics_covered()
 
 
-def test_legacy_proxy_mesh_exclude(aggregator, dd_run_check):
+def test_legacy_proxy_mesh_exclude(aggregator, dd_run_check, mock_openmetrics_http):
     """
     Test proxy mesh check for OpenMetricsV1 implementation
     """
+    mock_openmetrics_http.get.return_value = MockHTTPResponse(
+        content=get_response('1.5', 'istio-proxy.txt'), headers={'Content-Type': 'text/plain'}
+    )
     exclude_tags = ['destination_app', 'destination_principal']
     instance = common.MOCK_LEGACY_MESH_INSTANCE
     instance['exclude_labels'] = exclude_tags
 
     check = Istio(common.CHECK_NAME, {}, [instance])
-
-    with requests_mock.Mocker() as metric_request:
-        metric_request.get('http://localhost:15090/metrics', text=get_response('1.5', 'istio-proxy.txt'))
-        dd_run_check(check)
+    dd_run_check(check)
 
     for metric in common.LEGACY_MESH_METRICS + common.MESH_MERICS_1_5:
         aggregator.assert_metric(metric)
@@ -63,13 +64,13 @@ def test_legacy_proxy_mesh_exclude(aggregator, dd_run_check):
     aggregator.assert_all_metrics_covered()
 
 
-def test_legacy_version_metadata(datadog_agent, dd_run_check):
+def test_legacy_version_metadata(datadog_agent, dd_run_check, mock_openmetrics_http):
+    mock_openmetrics_http.get.return_value = MockHTTPResponse(
+        content=get_response('1.5', 'istiod.txt'), headers={'Content-Type': 'text/plain'}
+    )
     check = Istio(common.CHECK_NAME, {}, [common.MOCK_LEGACY_ISTIOD_INSTANCE])
     check.check_id = 'test:123'
-
-    with requests_mock.Mocker() as metric_request:
-        metric_request.get('http://localhost:8080/metrics', text=get_response('1.5', 'istiod.txt'))
-        dd_run_check(check)
+    dd_run_check(check)
 
     # Use version mocked from istiod 1.5 fixture
     MOCK_VERSION = '1.5.0'
