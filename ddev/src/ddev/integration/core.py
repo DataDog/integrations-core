a434d1b850a591f7852cbd77156af6d42bf64620
Juanpe Araque <juanpedro.araque@datadoghq.com>
2026-05-10 23:59:00 +0200

    Add example_config to Integration and require changelog when it changes

--- ddev/src/ddev/integration/core.py
+++ ddev/src/ddev/integration/core.py
@@ -69,7 +69,11 @@ class Integration:
                     yield Path(root, f)
 
     def requires_changelog_entry(self, path: Path) -> bool:
-        return self.package_directory in path.parents or (self.is_package and path == (self.path / 'pyproject.toml'))
+        return (
+            self.package_directory in path.parents
+            or (self.is_package and path == (self.path / 'pyproject.toml'))
+            or path == self.example_config
+        )
 
     @property
     def release_tag_pattern(self) -> str:
@@ -142,6 +146,10 @@ class Integration:
     def config_spec(self) -> Path:
         relative_path = self.manifest.get('/assets/integration/configuration/spec', 'assets/configuration/spec.yaml')
         return self.path / relative_path
+
+    @cached_property
+    def example_config(self) -> Path:
+        return self.package_directory / 'data' / 'conf.yaml.example'
 
     @cached_property
     def minimum_base_package_version(self) -> str | None:
