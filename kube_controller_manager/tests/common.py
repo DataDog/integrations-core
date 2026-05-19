52302cf96415d63674b843e6410736c0f0cea515
mwdd146980 <morgan.wang@datadoghq.com>
2026-05-19 16:34:44 -0400

    kube_controller_manager: address review pass 1 findings
    - Drop changelog entry (AGENTS.md: test-only PRs don't need one)
    - Add type hints to make_mock_metrics

--- kube_controller_manager/tests/common.py
+++ kube_controller_manager/tests/common.py
@@ -2,6 +2,7 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import os
+from unittest.mock import MagicMock
 
 from datadog_checks.base.utils.http_testing import MockHTTPResponse
 from datadog_checks.dev import get_here
@@ -9,7 +10,7 @@ from datadog_checks.dev import get_here
 HERE = get_here()
 
 
-def make_mock_metrics(mock_openmetrics_http, fixture_filename):
+def make_mock_metrics(mock_openmetrics_http: MagicMock, fixture_filename: str) -> MagicMock:
     f_name = os.path.join(HERE, 'fixtures', fixture_filename)
     mock_openmetrics_http.get.return_value = MockHTTPResponse(file_path=f_name, headers={'Content-Type': 'text/plain'})
     return mock_openmetrics_http
