e76b1d2aed7c28f09d6338cea1c8e0de599fea1c
Juanpe Araque <juanpedro.araque@datadoghq.com>
2026-05-20 14:29:18 +0100

    Backport n8n fixes to 7.79.x (#23764)
    * Port n8n from 7.80.x
    
    * Remove unused Dockerfile after the fix
    
    * Update requirements-agent-release.txt

--- n8n/tests/test_e2e.py
+++ n8n/tests/test_e2e.py
@@ -1,13 +1,29 @@
 # (C) Datadog, Inc. 2026-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+from typing import Any, Callable
+
+import pytest
+
 from datadog_checks.dev.utils import assert_service_checks
 
+from . import common
 
-def test_check_n8n_e2e(dd_agent_check, instance):
-    aggregator = dd_agent_check(instance, rate=True)
 
-    # Assert the readiness check metric is present with status_code tag
-    aggregator.assert_metric('n8n.readiness.check', value=1, tags=["status_code:200"], at_least=1)
+@pytest.mark.e2e
+def test_check_n8n_e2e(
+    dd_agent_check: Callable[..., Any],
+):
+    aggregator = dd_agent_check(rate=True)
 
+    aggregator.assert_metric('n8n.readiness.check', value=1, tags=['status_code:200', 'n8n_process:main'], at_least=1)
+    # Worker also exposes /healthz/readiness via QUEUE_HEALTH_CHECK_ACTIVE on its own port.
+    aggregator.assert_metric('n8n.readiness.check', value=1, tags=['status_code:200', 'n8n_process:worker'], at_least=1)
+
+    aggregator.assert_metrics_using_metadata(
+        common.get_metadata_metrics_for_version(exclude_rare=True),
+        check_submission_type=True,
+        check_symmetric_inclusion=True,
+        exclude=list(common.RARE_EVENT_METRIC_NAMES),
+    )
     assert_service_checks(aggregator)
