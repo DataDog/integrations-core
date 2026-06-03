054194c3ac19dbf569b323f5ab8ffa7df6be63ea
Guillermo Julián <guillermo.julian@datadoghq.com>
2026-06-03 10:31:37 +0000

    Update metadata

--- kueue/tests/test_unit.py
+++ kueue/tests/test_unit.py
@@ -10,8 +10,29 @@ from datadog_checks.base import AgentCheck  # noqa: F401
 from datadog_checks.base.stubs.aggregator import AggregatorStub  # noqa: F401
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.kueue import KueueCheck
+from datadog_checks.kueue.check import OTHER_RESOURCE_NAME, RESOURCE_NAME_MAP
+from datadog_checks.kueue.metrics import LOCAL_QUEUE_METRIC_MAP, METRIC_MAP, RESOURCE_METRIC_MAP
 
 from .common import TEST_METRICS, get_fixture_path
+
+
+def test_mapped_metrics_are_in_metadata():
+    mapped_metrics = set()
+    mapped_metrics.update(_metadata_metric_name(metric_name) for metric_name in METRIC_MAP.values())
+    mapped_metrics.update(_metadata_metric_name(metric_name) for metric_name in LOCAL_QUEUE_METRIC_MAP.values())
+
+    resource_names = {OTHER_RESOURCE_NAME, *RESOURCE_NAME_MAP.values()}
+    for metric_name in RESOURCE_METRIC_MAP.values():
+        mapped_metrics.update(_metadata_metric_name(f'{metric_name}.{resource_name}') for resource_name in resource_names)
+
+    assert mapped_metrics.issubset(set(get_metadata_metrics()))
+
+
+def _metadata_metric_name(metric_name):
+    if isinstance(metric_name, dict):
+        metric_name = metric_name['name']
+
+    return f'kueue.{metric_name}'
 
 
 def test_check(dd_run_check, aggregator, instance, mock_http_response):
