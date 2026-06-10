f079d2b8ef41b045086967e4fb10676a272a86cc
Guillermo Julián <guillermo.julian@datadoghq.com>
2026-06-10 10:46:43 +0000

    Document Kueue GC summary metrics

--- kueue/tests/test_e2e.py
+++ kueue/tests/test_e2e.py
@@ -4,7 +4,6 @@
 
 import pytest
 
-from datadog_checks.base.constants import ServiceCheck
 from datadog_checks.dev.utils import assert_service_checks, get_metadata_metrics
 
 
@@ -12,11 +11,7 @@ from datadog_checks.dev.utils import assert_service_checks, get_metadata_metrics
 def test_e2e(dd_agent_check):
     aggregator = dd_agent_check()
 
-    aggregator.assert_metrics_using_metadata(
-        get_metadata_metrics(),
-        check_submission_type=True,
-        exclude=['kueue.go.gc.duration.seconds.quantile'],
-    )
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics(), check_submission_type=True)
 
     for metric in (
         'kueue.build_info',
@@ -34,5 +29,4 @@ def test_e2e(dd_agent_check):
     for metric in ('kueue.cluster_queue.info', 'kueue.cluster_queue.status'):
         aggregator.assert_metric_has_tag(metric, 'kueue_cluster_queue:cluster-queue')
 
-    aggregator.assert_service_check('kueue.openmetrics.health', ServiceCheck.OK, count=1)
     assert_service_checks(aggregator)
