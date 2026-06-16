a5e64a9bc68517896a7515fe7c22335b5d9f7d4b
sarah-witt <sarah.witt@datadoghq.com>
2026-06-16 15:50:18 -0400

    setup check with dd_run_check

--- envoy/tests/test_bench.py
+++ envoy/tests/test_bench.py
@@ -1,4 +1,4 @@
-# (C) Datadog, Inc. 2026-present
+# (C) Datadog, Inc. 2025-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 
@@ -6,13 +6,13 @@ from datadog_checks.envoy import Envoy
 
 from .common import DEFAULT_INSTANCE, get_fixture_path
 
+OPENMETRICS_FIXTURE = get_fixture_path('./openmetrics/openmetrics.txt')
 
-def test_openmetrics_check(benchmark, mock_http_response):
-    mock_http_response(file_path=get_fixture_path('./openmetrics/openmetrics.txt'))
+
+def test_openmetrics_check(benchmark, dd_run_check, mock_http_response):
+    mock_http_response(file_path=OPENMETRICS_FIXTURE)
 
     c = Envoy('envoy', {}, [DEFAULT_INSTANCE])
-
-    # Run once to get any first-run overhead out of the way.
-    c.check(DEFAULT_INSTANCE)
+    dd_run_check(c)
 
     benchmark(c.check, DEFAULT_INSTANCE)
