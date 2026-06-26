a44e01a21cf17d67c7bcb299dce99c457b9d7ac0
Vincent Whitchurch <vincent.whitchurch@datadoghq.com>
2026-06-26 08:24:42 +0000

    regen

--- krakend/datadog_checks/krakend/config_models/discovery.py
+++ krakend/datadog_checks/krakend/config_models/discovery.py
@@ -20,7 +20,7 @@ from datadog_checks.krakend.config_models.shared import SharedConfig
 
 def _generated_candidates(service: Service) -> Iterator[dict[str, Any]]:
     shared = SharedConfig.model_validate({}, context={'configured_fields': frozenset()}).model_dump(
-        mode='json', exclude_none=True
+        by_alias=True, mode='json', exclude_none=True
     )
     # discovery[0]: from_ports
     for port in candidate_ports(service, [9090]):
@@ -30,7 +30,7 @@ def _generated_candidates(service: Service) -> Iterator[dict[str, Any]]:
         }
         instance = InstanceConfig.model_validate(
             instance_data, context={'configured_fields': frozenset(instance_data)}
-        ).model_dump(mode='json', exclude_none=True)
+        ).model_dump(by_alias=True, mode='json', exclude_none=True)
         yield {'init_config': shared, 'instances': [instance]}
 
 
