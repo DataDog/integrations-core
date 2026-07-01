0e3ce53144451459151386263a862a56f3d59258
sarah-witt <sarah.witt@datadoghq.com>
2026-07-01 15:09:39 -0400

    format and handle quotes

--- datadog_checks_base/datadog_checks/base/checks/openmetrics/v2/scraper/base_scraper.py
+++ datadog_checks_base/datadog_checks/base/checks/openmetrics/v2/scraper/base_scraper.py
@@ -13,10 +13,9 @@ from typing import List  # noqa: F401
 from prometheus_client import Metric
 from prometheus_client.openmetrics.parser import text_fd_to_metric_families as parse_openmetrics
 from prometheus_client.parser import text_fd_to_metric_families as parse_prometheus
-
-import datadog_checks.base.checks.openmetrics.parser_optimizations  # noqa: F401
 from requests.exceptions import ConnectionError
 
+import datadog_checks.base.checks.openmetrics.parser_optimizations  # noqa: F401
 from datadog_checks.base.agent import datadog_agent
 from datadog_checks.base.checks.openmetrics.v2.first_scrape_handler import first_scrape_handler
 from datadog_checks.base.checks.openmetrics.v2.labels import LabelAggregator, get_label_normalizer
