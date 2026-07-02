4de81a3b42805ea4d16fa5f9506c91e708ff5fb1
Vincent Whitchurch <vincent.whitchurch@datadoghq.com>
2026-07-02 07:42:10 +0000

    tests

--- ddev/tests/cli/env/test_agent.py
+++ ddev/tests/cli/env/test_agent.py
@@ -38,6 +38,38 @@ def test_not_trigger_run(ddev, data_dir, mocker):
     invoke.assert_called_once_with(['status'], env_vars=None)
 
 
+def test_env_vars(ddev, data_dir, mocker):
+    invoke = mocker.patch('ddev.e2e.agent.docker.DockerAgent.invoke')
+
+    integration = 'postgres'
+    environment = 'py3.12'
+    env_data = EnvDataStorage(data_dir).get(integration, environment)
+    env_data.write_metadata({})
+
+    result = ddev('env', 'agent', integration, environment, '--env', 'FOO=bar', '--env', 'BAZ=qux', 'status')
+
+    assert result.exit_code == 0, result.output
+    assert not result.output
+
+    invoke.assert_called_once_with(['status'], env_vars={'FOO': 'bar', 'BAZ': 'qux'})
+
+
+def test_env_vars_trigger_run(ddev, data_dir, mocker):
+    invoke = mocker.patch('ddev.e2e.agent.docker.DockerAgent.invoke')
+
+    integration = 'postgres'
+    environment = 'py3.12'
+    env_data = EnvDataStorage(data_dir).get(integration, environment)
+    env_data.write_metadata({})
+
+    result = ddev('env', 'agent', integration, environment, '--env', 'FOO=bar', 'check', integration, '-l', 'debug')
+
+    assert result.exit_code == 0, result.output
+    assert not result.output
+
+    invoke.assert_called_once_with(['check', integration, '-l', 'debug'], env_vars={'FOO': 'bar'})
+
+
 def test_trigger_run(ddev, data_dir, mocker):
     invoke = mocker.patch('ddev.e2e.agent.docker.DockerAgent.invoke')
 
