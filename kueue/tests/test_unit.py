449e60a35f628cdb3746c91afaff5c36449d64e6
Guillermo Julián <guillermo.julian@datadoghq.com>
2026-07-06 10:46:37 +0000

    Add Kueue topology domain metrics.

--- kueue/tests/test_unit.py
+++ kueue/tests/test_unit.py
@@ -10,7 +10,9 @@ from datadog_checks.base.stubs import tagger
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.kueue import KueueCheck
 from datadog_checks.kueue.check import OTHER_RESOURCE_NAME, RESOURCE_NAME_MAP
+from datadog_checks.kueue.kube_client import to_mutable_config
 from datadog_checks.kueue.metrics import LOCAL_QUEUE_METRIC_MAP, METRIC_MAP, RESOURCE_METRIC_MAP
+from datadog_checks.kueue.topology import DomainStat, build_node_gpu_usage, iter_domain_stats
 
 from .common import EXPECTED_METRIC_TAGS, UNIT_METRICS, get_fixture_path
 
@@ -51,6 +53,7 @@ def test_check(dd_run_check, aggregator, instance, mock_http_response):
     mock_http_response(file_path=get_fixture_path('metrics.txt'))
 
     check = KueueCheck('kueue', {}, [{**instance, 'collect_workload_events': False}])
+    check.kube_client = FakeKubernetesAPIClient(**topology_fixtures())
     dd_run_check(check)
 
     for metric in UNIT_METRICS:
@@ -77,7 +80,9 @@ def test_queue_tagger_tags(dd_run_check, aggregator, instance, mock_http_respons
         }
     )
 
-    check = KueueCheck('kueue', {}, [{**instance, 'collect_workload_events': False}])
+    check = KueueCheck(
+        'kueue', {}, [{**instance, 'collect_workload_events': False, 'collect_topology_domain_metrics': False}]
+    )
     dd_run_check(check)
 
     aggregator.assert_metric_has_tag('kueue.pending_workloads', 'cluster_queue_tag:value')
@@ -100,7 +105,9 @@ def test_queue_tagger_tags_are_scoped(dd_run_check, aggregator, instance, mock_h
         }
     )
 
-    check = KueueCheck('kueue', {}, [{**instance, 'collect_workload_events': False}])
+    check = KueueCheck(
+        'kueue', {}, [{**instance, 'collect_workload_events': False, 'collect_topology_domain_metrics': False}]
+    )
     dd_run_check(check)
 
     go_goroutines_tags = _get_metric_tags(aggregator, 'kueue.go.goroutines')
@@ -119,7 +126,9 @@ def test_resource_name_map(dd_run_check, aggregator, instance, mock_http_respons
         },
     }
 
-    check = KueueCheck('kueue', {}, [{**instance, 'collect_workload_events': False}])
+    check = KueueCheck(
+        'kueue', {}, [{**instance, 'collect_workload_events': False, 'collect_topology_domain_metrics': False}]
+    )
     dd_run_check(check)
 
     aggregator.assert_metric('kueue.cluster_queue.resource_usage.cpu')
@@ -142,6 +151,15 @@ def test_empty_instance(dd_run_check):
         dd_run_check(check)
 
 
+def test_to_mutable_config_converts_immutable_config_model_values():
+    value = {'clusters': ({'cluster': {'server': 'https://example.com'}, 'name': 'cluster'},), 'contexts': ()}
+
+    assert to_mutable_config(value) == {
+        'clusters': [{'cluster': {'server': 'https://example.com'}, 'name': 'cluster'}],
+        'contexts': [],
+    }
+
+
 def test_workload_events_config_can_be_parsed_before_check(instance):
     check = KueueCheck('kueue', {}, [instance])
 
@@ -151,17 +169,45 @@ def test_workload_events_config_can_be_parsed_before_check(instance):
 
 
 class FakeKubernetesAPIClient:
-    def __init__(self, *workload_snapshots):
+    def __init__(
+        self,
+        *workload_snapshots,
+        nodes=None,
+        pods=None,
+        resource_flavors=None,
+        topologies=None,
+    ):
         self.workload_snapshots = list(workload_snapshots)
         self.list_workloads_namespaces = []
+        self.nodes = nodes or []
+        self.pods = pods or []
+        self.resource_flavors = resource_flavors or []
+        self.topologies = topologies or []
 
     def list_workloads(self, namespace=None):
         self.list_workloads_namespaces.append(namespace)
         return self.workload_snapshots.pop(0)
 
+    def list_nodes(self):
+        return self.nodes
+
+    def list_pods(self):
+        return self.pods
+
+    def list_resource_flavors(self):
+        return self.resource_flavors
+
+    def list_topologies(self):
+        return self.topologies
+
 
 def load_workloads(name):
     with open(get_fixture_path(f'workloads/{name}.json')) as f:
+        return json.load(f)
+
+
+def load_topology_fixture(name):
+    with open(get_fixture_path(f'topology/{name}.json')) as f:
         return json.load(f)
 
 
@@ -184,6 +230,124 @@ def without_admission(workloads):
     for workload in workloads:
         workload['status'].pop('admission', None)
     return workloads
+
+
+def topology_fixtures():
+    return {
+        'nodes': load_topology_fixture('nodes'),
+        'pods': load_topology_fixture('pods'),
+        'resource_flavors': load_topology_fixture('resourceflavors'),
+        'topologies': load_topology_fixture('topologies'),
+    }
+
+
+def test_node_gpu_usage_counts_only_bound_non_terminal_gpu_pods():
+    usage = build_node_gpu_usage(load_topology_fixture('pods'))
+
+    assert usage == {
+        'node-a1': 3,
+        'node-b1': 4,
+        'node-other-flavor': 6,
+    }
+
+
+def test_iter_domain_stats_groups_domains_by_topology_path_and_classifies_usage():
+    stats = list(iter_domain_stats(**topology_fixtures()))
+
+    assert (
+        DomainStat(
+            flavor='tas-flavor',
+            level='cloud.provider.com/topology-block',
+            total=2,
+            available=0,
+            fully_used=1,
+            partially_used=1,
+        )
+        in stats
+    )
+    assert (
+        DomainStat(
+            flavor='tas-flavor',
+            level='cloud.provider.com/topology-rack',
+            total=2,
+            available=0,
+            fully_used=1,
+            partially_used=1,
+        )
+        in stats
+    )
+    assert (
+        DomainStat(
+            flavor='tas-flavor',
+            level='kubernetes.io/hostname',
+            total=3,
+            available=1,
+            fully_used=1,
+            partially_used=1,
+        )
+        in stats
+    )
+    assert (
+        DomainStat(
+            flavor='other-flavor',
+            level='zone',
+            total=1,
+            available=0,
+            fully_used=0,
+            partially_used=1,
+        )
+        in stats
+    )
+    assert (
+        DomainStat(
+            flavor='other-flavor',
+            level='kubernetes.io/hostname',
+            total=1,
+            available=0,
+            fully_used=0,
+            partially_used=1,
+        )
+        in stats
+    )
+    assert all(stat.flavor not in {'no-topology-flavor', 'missing-topology-flavor'} for stat in stats)
+
+
+def test_iter_domain_stats_empty_node_labels_match_all_nodes_with_complete_topology_labels():
+    stats = list(iter_domain_stats(**topology_fixtures()))
+
+    assert (
+        DomainStat(
+            flavor='all-nodes-flavor',
+            level='kubernetes.io/hostname',
+            total=3,
+            available=1,
+            fully_used=1,
+            partially_used=1,
+        )
+        in stats
+    )
+
+
+def test_topology_domain_metrics(dd_run_check, aggregator, instance, mock_http_response):
+    mock_http_response(file_path=get_fixture_path('metrics.txt'))
+    tagger.set_tags({'kueue_resource_flavor://tas-flavor': ['resource_flavor_tag:value']})
+    check = KueueCheck('kueue', {}, [instance])
+    check.kube_client = FakeKubernetesAPIClient(load_workloads('pending'), **topology_fixtures())
+
+    dd_run_check(check)
+
+    expected_tags = [
+        'test:tag',
+        'kueue_resource_flavor:tas-flavor',
+        'domain:kubernetes.io/hostname',
+        'domain_short:hostname',
+        'resource_flavor_tag:value',
+    ]
+    aggregator.assert_metric('kueue.topology_domains.total', value=3, tags=expected_tags)
+    aggregator.assert_metric('kueue.topology_domains.available', value=1, tags=expected_tags)
+    aggregator.assert_metric('kueue.topology_domains.fully_used', value=1, tags=expected_tags)
+    aggregator.assert_metric('kueue.topology_domains.partially_used', value=1, tags=expected_tags)
+    tagger.assert_called('kueue_resource_flavor://tas-flavor', tagger.ORCHESTRATOR)
 
 
 def test_workload_events_suppress_first_poll(dd_run_check, aggregator, instance, mock_http_response):
