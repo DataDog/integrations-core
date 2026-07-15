5cad39f65c587e8282ac7cc06313304d316c8bcc
Marta Vicente Navarro <marta.vicentenavarro@datadoghq.com>
2026-07-15 17:37:29 +0200

    Add config discovery support to envoy
    Adds a from_ports discovery strategy covering both the OpenMetrics and
    legacy stats_url dispatch modes on port 8001, regenerates config
    models, wires E2E discovery metadata, and adds discovery E2E tests.

--- envoy/tests/test_e2e.py
+++ envoy/tests/test_e2e.py
@@ -4,6 +4,7 @@
 
 import pytest
 
+from datadog_checks.dev.docker import assert_all_discovery_candidates_stable
 from datadog_checks.dev.utils import get_metadata_metrics
 from datadog_checks.envoy import Envoy
 
@@ -43,3 +44,33 @@ def test_e2e(dd_agent_check, exercise_envoy):
     aggregator.assert_service_check(
         'envoy.openmetrics.health', Envoy.OK, tags=['endpoint:{}'.format(DEFAULT_INSTANCE['openmetrics_endpoint'])]
     )
+
+
+@pytest.mark.e2e
+def test_e2e_discovery(dd_agent_check_discovery, exercise_envoy):
+    aggregator = dd_agent_check_discovery(rate=True)
+
+    for metric in (
+        PROMETHEUS_METRICS
+        + LOCAL_RATE_LIMIT_METRICS
+        + CONNECTION_LIMIT_METRICS
+        + TLS_INSPECTOR_METRICS
+        + ADAPTIVE_CONCURRENCY_PROMETHEUS_METRICS
+    ):
+        formatted_metric = "envoy.{}".format(metric)
+        if metric in FLAKY_METRICS:
+            aggregator.assert_metric(formatted_metric, at_least=0)
+            continue
+        aggregator.assert_metric(formatted_metric)
+
+    aggregator.assert_all_metrics_covered()
+    aggregator.assert_metrics_using_metadata(get_metadata_metrics())
+    # discovery can't know the endpoint ahead of time, and Autodiscovery-injected
+    # container tags (docker_image, image_id, etc.) would break an exact tag match,
+    # so the endpoint tag isn't asserted here, unlike in test_e2e above.
+    aggregator.assert_service_check('envoy.openmetrics.health', Envoy.OK)
+
+
+@pytest.mark.e2e
+def test_e2e_discovery_all_candidates(dd_agent_check):
+    assert_all_discovery_candidates_stable(dd_agent_check, Envoy, compose_service='front-envoy')
