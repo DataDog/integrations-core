f0e59d21c82242ea3e3daca61ee858712c7e1c70
Vincent Whitchurch <vincent.whitchurch@datadoghq.com>
2026-07-17 13:16:02 +0000

    Fix traffic server discovery E2E assertions

--- traffic_server/tests/test_e2e.py
+++ traffic_server/tests/test_e2e.py
@@ -1,6 +1,7 @@
 # (C) Datadog, Inc. 2022-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import re
 
 import pytest
 
@@ -9,11 +10,37 @@ from datadog_checks.traffic_server import TrafficServerCheck
 
 from .common import EXPECTED_METRICS
 
+DISCOVERY_METRICS_PATTERN = re.compile(
+    '|'.join(
+        (
+            'traffic_server.node.*',
+            'traffic_server.process.http.code.*',
+            'traffic_server.process.http(s|2)?.*current',
+            'traffic_server.process.http(s|2)?.*_requests',
+            'traffic_server.process.http(s|2)?.*_connections',
+            'traffic_server.process.http.transaction_.*',
+            'traffic_server.process.hostdb.*',
+            'traffic_server.process.dns.*',
+            'traffic_server.process.traffic_server.memory.rss',
+            'traffic_server.process.eventloop.*',
+            'traffic_server.process.ssl.user_agent_session.*',
+            'traffic_server.process.ssl.ssl_error.*',
+            'traffic_server.process.ssl.user_agent_sessions',
+            'traffic_server.process.cache.total.*',
+            'traffic_server.process.cache.volume.dir.*',
+            'traffic_server.process.cache.volume.percent_full',
+            'traffic_server.process.cache.volume.bytes.*',
+            'traffic_server.process.cache.volume.ram_cache.*',
+        )
+    )
+)
+DISCOVERY_EXPECTED_METRICS = tuple(metric for metric in EXPECTED_METRICS if DISCOVERY_METRICS_PATTERN.search(metric))
 
-def assert_metrics(aggregator, tags=None):
+
+def assert_metrics(aggregator, *, expected_metrics=EXPECTED_METRICS, tags=None):
     aggregator.assert_service_check('traffic_server.can_connect', TrafficServerCheck.OK)
 
-    for metric_name in EXPECTED_METRICS:
+    for metric_name in expected_metrics:
         aggregator.assert_metric(metric_name, at_least=1)
         for tag in tags or []:
             aggregator.assert_metric_has_tag(metric_name, tag)
@@ -31,7 +58,7 @@ def test_e2e(dd_agent_check, instance, aggregator):
 def test_e2e_discovery(dd_agent_check_discovery):
     aggregator = dd_agent_check_discovery(rate=True)
 
-    assert_metrics(aggregator)
+    assert_metrics(aggregator, expected_metrics=DISCOVERY_EXPECTED_METRICS)
 
 
 @pytest.mark.e2e
