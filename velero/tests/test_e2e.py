21b3e683cd5151efd3d716be00d20621dbf60c99
Enrico Donnici <enrico.donnici@datadoghq.com>
2026-07-22 10:53:28 +0000

    Run Velero discovery E2E with Kubernetes Agent

--- velero/tests/test_e2e.py
+++ velero/tests/test_e2e.py
@@ -8,10 +8,7 @@ from datadog_checks.base.constants import ServiceCheck
 from .common import OPTIONAL_METRICS, TEST_METRICS
 
 
-@pytest.mark.e2e
-def test_check_velero_e2e(dd_agent_check):
-    aggregator = dd_agent_check(rate=True)
-
+def assert_metrics(aggregator):
     for metric, _ in TEST_METRICS.items():
         if metric in OPTIONAL_METRICS:
             aggregator.assert_metric(name=metric, at_least=0)
@@ -19,3 +16,14 @@ def test_check_velero_e2e(dd_agent_check):
             aggregator.assert_metric(name=metric, at_least=1)
 
     aggregator.assert_service_check('velero.openmetrics.health', ServiceCheck.OK)
+
+
+@pytest.mark.e2e
+def test_check_velero_e2e(dd_agent_check):
+    assert_metrics(dd_agent_check(rate=True))
+
+
+@pytest.mark.e2e
+def test_check_velero_discovery_e2e(dd_agent_check_discovery):
+    aggregator = dd_agent_check_discovery(check_rate=True, discovery_min_instances=2)
+    assert_metrics(aggregator)
