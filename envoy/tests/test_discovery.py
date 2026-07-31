5b86051eda4d0d7840516eb9e02bd8008ff994d4
Marta Vicente Navarro <vicentenavarromarta@gmail.com>
2026-07-31 14:21:26 +0200

    remove comment line
    Co-authored-by: Marta Vicente Navarro <vicentenavarromarta@gmail.com>

--- envoy/tests/test_discovery.py
+++ envoy/tests/test_discovery.py
@@ -45,8 +45,7 @@ def test_openmetrics_endpoint_disables_server_info_for_all_candidates() -> None:
 
 
 def test_ipv6_host_is_bracketed_in_generated_endpoint() -> None:
-    # Service.host brackets IPv6 hosts itself on interpolation (datadog_checks.base.utils.discovery);
-    # this integration no longer needs to repair the URL.
+    # Service.host brackets IPv6 hosts itself on interpolation (datadog_checks.base.utils.discovery).
     service = Service(
         id='envoy',
         host='fd00::1',
