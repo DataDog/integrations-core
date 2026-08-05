de17b9e695f8a9ed372f7755966f6283837b425f
Juanpe Araque <juanpedro.araque@datadoghq.com>
2026-08-05 16:20:03 +0200

    Add multiple Kubernetes port forwarding

--- datadog_checks_dev/datadog_checks/dev/kube_port_forward.py
+++ datadog_checks_dev/datadog_checks/dev/kube_port_forward.py
@@ -4,15 +4,26 @@
 from __future__ import absolute_import
 
 import os
-from contextlib import contextmanager
+from contextlib import ExitStack, contextmanager
+from dataclasses import dataclass
+from typing import Iterator
 
+from .conditions import WaitForPortListening
 from .env import environment_run
 from .fs import chdir
 from .ssh_tunnel import KillProcess, run_background_command
 from .structures import LazyFunction, TempDir
-from .utils import find_free_port, get_ip
+from .utils import find_free_port, find_free_ports, get_ip
 
 PID_FILE = 'kubectl.pid'
+
+
+@dataclass(frozen=True)
+class PortForwardConfig:
+    namespace: str
+    remote_port: int
+    resource: str
+    resource_name: str
 
 
 def _build_temp_key(namespace, deployment, remote_port):
@@ -20,9 +31,16 @@ def _build_temp_key(namespace, deployment, remote_port):
 
 
 @contextmanager
-def port_forward(kubeconfig, namespace, remote_port, resource, resource_name):
+def port_forward(
+    kubeconfig: str,
+    namespace: str,
+    remote_port: int,
+    resource: str,
+    resource_name: str,
+    local_port: int | None = None,
+) -> Iterator[tuple[str, int]]:
     """Use `kubectl` to forward a remote port locally."""
-    set_up = PortForwardUp(kubeconfig, namespace, remote_port, resource, resource_name)
+    set_up = PortForwardUp(kubeconfig, namespace, remote_port, resource, resource_name, local_port)
     key = _build_temp_key(namespace, resource_name, remote_port)
     tear_down = KillProcess(key, PID_FILE)
 
@@ -30,15 +48,47 @@ def port_forward(kubeconfig, namespace, remote_port, resource, resource_name):
         yield result
 
 
+@contextmanager
+def multiple_port_forward(kubeconfig: str, configs: list[PortForwardConfig]) -> Iterator[list[tuple[str, int]]]:
+    """Forward multiple remote ports to distinct local ports."""
+    ip = get_ip()
+    local_ports = find_free_ports(ip, len(configs))
+
+    with ExitStack() as stack:
+        forwards = [
+            stack.enter_context(
+                port_forward(
+                    kubeconfig,
+                    config.namespace,
+                    config.remote_port,
+                    config.resource,
+                    config.resource_name,
+                    local_port,
+                )
+            )
+            for config, local_port in zip(configs, local_ports, strict=True)
+        ]
+        yield forwards
+
+
 class PortForwardUp(LazyFunction):
     """Setup `kubectl port-forward`."""
 
-    def __init__(self, kubeconfig, namespace, remote_port, resource, resource_name):
+    def __init__(
+        self,
+        kubeconfig: str,
+        namespace: str,
+        remote_port: int,
+        resource: str,
+        resource_name: str,
+        local_port: int | None = None,
+    ) -> None:
         self.kubeconfig = kubeconfig
         self.namespace = namespace
         self.remote_port = remote_port
         self.resource = resource
         self.resource_name = resource_name
+        self.local_port = local_port
 
     def __call__(self):
         key = _build_temp_key(self.namespace, self.resource_name, self.remote_port)
@@ -46,7 +96,7 @@ class PortForwardUp(LazyFunction):
             # Run in the temp dir to put kube cache files there
             with chdir(temp_dir):
                 ip = get_ip()
-                local_port = find_free_port(ip)
+                local_port = self.local_port if self.local_port is not None else find_free_port(ip)
                 command = [
                     'kubectl',
                     'port-forward',
@@ -61,4 +111,5 @@ class PortForwardUp(LazyFunction):
                 env = os.environ.copy()
                 env['KUBECONFIG'] = self.kubeconfig
                 run_background_command(command, os.path.join(temp_dir, PID_FILE), env=env)
+                WaitForPortListening(ip, local_port)()
                 return ip, local_port
