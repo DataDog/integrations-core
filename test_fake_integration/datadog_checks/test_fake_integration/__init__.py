52b016f453190deacd26ec9f3ce78289ec7c2b32
sarah-witt <sarah.witt@datadoghq.com>
2026-08-11 11:35:18 -0400

    rename to test

--- test_fake_integration/datadog_checks/test_fake_integration/__init__.py
+++
@@ -1,4 +0,0 @@
-from datadog_checks.base import AgentCheck
-from datadog_checks.test_fake_integration.check import TestFakeIntegrationCheck
-
-__all__ = ["TestFakeIntegrationCheck"]
--- test_fake_integration_2/datadog_checks/test_fake_integration_2/__init__.py
+++
@@ -1,4 +0,0 @@
-from datadog_checks.base import AgentCheck
-from datadog_checks.test_fake_integration_2.check import TestFakeIntegration2Check
-
-__all__ = ["TestFakeIntegration2Check"]
