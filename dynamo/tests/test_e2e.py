64cce83c6520a280a7b2aec61e308a25ebbacc1a
Piotr WOLSKI <piotr.wolski@datadoghq.com>
2026-08-27 13:02:04 -0600

    fix(dynamo): pass explicit instances dict to dd_agent_check in e2e test

--- dynamo/tests/test_e2e.py
+++ dynamo/tests/test_e2e.py
@@ -6,6 +6,6 @@ from datadog_checks.dev.utils import assert_service_checks
 
 
 def test_check_dynamo_e2e(dd_agent_check, frontend_instance, worker_instance):
-    aggregator = dd_agent_check([frontend_instance, worker_instance], rate=True)
+    aggregator = dd_agent_check({'instances': [frontend_instance, worker_instance]}, rate=True)
     aggregator.assert_service_check('dynamo.openmetrics.health', ServiceCheck.OK, count=2)
     assert_service_checks(aggregator)
