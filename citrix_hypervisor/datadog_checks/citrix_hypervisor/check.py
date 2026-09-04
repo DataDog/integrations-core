dbcd48eb0b3af940ba8a4aac2473f0bbe69a400d
mwdd146980 <morgan.wang@datadoghq.com>
2026-09-04 15:50:14 +0000

    Revert "Update Citrix Hypervisor response type"
    This reverts commit 9a06dc9fb6e9f1b808fd6ec4a109e000a167334e.

--- citrix_hypervisor/datadog_checks/citrix_hypervisor/check.py
+++ citrix_hypervisor/datadog_checks/citrix_hypervisor/check.py
@@ -14,10 +14,10 @@ from datadog_checks.base.utils.http import requests
 from .metrics import build_metric
 
 if TYPE_CHECKING:
-    from datadog_checks.base.utils.http_protocol import HTTPResponse
+    from datadog_checks.base.utils.http import ResponseWrapper
 
 
-def _safely_process_metrics_response(data: HTTPResponse) -> dict[str, dict]:
+def _safely_process_metrics_response(data: ResponseWrapper) -> dict[str, dict]:
     # Responses can be wrongly formatted for older versions of the hypervisor,
     # it's missing double quotes " around the field names. Use yaml to parse the response
     # if json parsing fails.
