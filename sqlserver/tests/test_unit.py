108b2c5d4db33f25f25edc138d7ead578fdc0061
Jason Petersen <jason.petersen@datadoghq.com>
2026-09-11 14:06:28 -0600

    Run async collectors in default metrics tests

--- sqlserver/tests/test_unit.py
+++ sqlserver/tests/test_unit.py
@@ -1255,7 +1255,7 @@ def test_set_default_driver_conf_linux():
 
 
 @windows_ci
-def test_check_local(aggregator, dd_run_check, init_config, instance_docker):
+def test_check_local(aggregator, dd_run_check, init_config, instance_docker, run_database_metrics_synchronously):
     sqlserver_check = SQLServer(CHECK_NAME, init_config, [instance_docker])
     dd_run_check(sqlserver_check)
     check_tags = sqlserver_check._config.tags + [
