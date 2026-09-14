0097d53e91f30fa1f8d03c1e8d058611c474e130
Piotr WOLSKI <piotr.wolski@datadoghq.com>
2026-09-14 12:45:24 -0600

    Fix vLLM test import ordering

--- vllm/tests/test_unit.py
+++ vllm/tests/test_unit.py
@@ -9,8 +9,8 @@ import pytest
 from datadog_checks.base.constants import ServiceCheck
 from datadog_checks.dev.http import MockResponse
 from datadog_checks.dev.utils import get_metadata_metrics
-from datadog_checks.vllm import vLLMCheck
 from datadog_checks.vllm import check as vllm_check
+from datadog_checks.vllm import vLLMCheck
 
 from .common import METRICS_MOCK, get_fixture_path
 
