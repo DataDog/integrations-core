ece398b81ff3d595d9ad2e20b94fc9f067aa11bd
Philip Lee <philip.lee@datadoghq.com>
2026-09-17 09:44:36 -0400

    [milvus] strengthen tests to lift mutation score

--- milvus/tests/test_unit.py
+++ milvus/tests/test_unit.py
@@ -9,16 +9,18 @@ from datadog_checks.milvus import MilvusCheck
 
 from .common import STANDALONE_TEST_METRICS, get_fixture_path
 
+pytestmark = pytest.mark.unit
+
 
 def test_check(dd_run_check, aggregator, instance, mock_http_response):
-    mock_http_response(file_path=get_fixture_path('milvus_payload.txt'))
+    mock_http_response(file_path=get_fixture_path("milvus_payload.txt"))
 
-    check = MilvusCheck('milvus', {}, [instance])
+    check = MilvusCheck("milvus", {}, [instance])
     dd_run_check(check)
 
     for metric, metric_type in STANDALONE_TEST_METRICS.items():
         aggregator.assert_metric(metric, metric_type=aggregator.METRIC_ENUM_MAP[metric_type])
-        aggregator.assert_metric_has_tag(metric, 'test:tag')
+        aggregator.assert_metric_has_tag(metric, "test:tag")
 
     aggregator.assert_all_metrics_covered()
     aggregator.assert_metrics_using_metadata(get_metadata_metrics())
@@ -27,17 +29,22 @@ def test_check(dd_run_check, aggregator, instance, mock_http_response):
 def test_empty_instance(dd_run_check):
     with pytest.raises(
         Exception,
-        match='InstanceConfig`:\nopenmetrics_endpoint\n  Field required',
+        match="InstanceConfig`:\nopenmetrics_endpoint\n  Field required",
     ):
-        check = MilvusCheck('milvus', {}, [{}])
+        check = MilvusCheck("milvus", {}, [{}])
         dd_run_check(check)
 
 
 def test_custom_validation(dd_run_check):
-    endpoint = 'milvus:2112/metrics'
+    endpoint = "milvus:2112/metrics"
     with pytest.raises(
         Exception,
-        match='openmetrics_endpoint: {} is incorrectly configured'.format(endpoint),
+        match="openmetrics_endpoint: {} is incorrectly configured".format(endpoint),
     ):
-        check = MilvusCheck('milvus', {}, [{'openmetrics_endpoint': endpoint}])
+        check = MilvusCheck("milvus", {}, [{"openmetrics_endpoint": endpoint}])
         dd_run_check(check)
+
+
+def test_default_metric_limit_is_zero(instance):
+    check = MilvusCheck("milvus", {}, [instance])
+    assert check.DEFAULT_METRIC_LIMIT == 0
